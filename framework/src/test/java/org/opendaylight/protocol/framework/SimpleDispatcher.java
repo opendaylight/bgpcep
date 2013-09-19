5b84bc890ad9d34436dc84611e61128a743b65f2
Robert Varga <rovarga@cisco.com>
2013-09-19 08:45:40 +0200

    Fix the dispatcher not being threadsafe
    It turns out that the createServer/createClient calls actually need a
    initializer which is protocol-specific and that listener needs to be
    more than an abstract method, as there is protocol-specific state which
    needs to be maintained.
    
    Change-Id: Ia70c01183b23f79f7f1d8376cbe04c6af2f549d1
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- framework/src/test/java/org/opendaylight/protocol/framework/SimpleDispatcher.java
+++ framework/src/test/java/org/opendaylight/protocol/framework/SimpleDispatcher.java
@@ -1,32 +1,52 @@
 package org.opendaylight.protocol.framework;
 
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.Promise;
+
+import java.net.InetSocketAddress;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 
-public class SimpleDispatcher<M, S extends ProtocolSession<?>, L extends SessionListener<?, ?, ?>> extends
-AbstractDispatcher<S, L> {
+public class SimpleDispatcher extends AbstractDispatcher<SimpleSession, SimpleSessionListener> {
 
 	private static final Logger logger = LoggerFactory.getLogger(SimpleDispatcher.class);
 
-	private final SessionNegotiatorFactory<M, S, L> negotiatorFactory;
+	private final SessionNegotiatorFactory<SimpleMessage, SimpleSession, SimpleSessionListener> negotiatorFactory;
 	private final ProtocolHandlerFactory<?> factory;
 
-	public SimpleDispatcher(final SessionNegotiatorFactory<M, S, L> negotiatorFactory, final ProtocolHandlerFactory<?> factory,
-			final Promise<S> promise) {
+	private final class SimplePipelineInitializer implements PipelineInitializer<SimpleSession> {
+		final SessionListenerFactory<SimpleSessionListener> listenerFactory;
+
+		SimplePipelineInitializer(final SessionListenerFactory<SimpleSessionListener> listenerFactory) {
+			this.listenerFactory = Preconditions.checkNotNull(listenerFactory);
+		}
+
+		@Override
+		public void initializeChannel(final SocketChannel channel, final Promise<SimpleSession> promise) {
+			channel.pipeline().addLast(factory.getDecoders());
+			channel.pipeline().addLast("negotiator", negotiatorFactory.getSessionNegotiator(listenerFactory, channel, promise));
+			channel.pipeline().addLast(factory.getEncoders());
+			logger.debug("initialization completed for channel {}", channel);
+		}
+
+	}
+
+	public SimpleDispatcher(final SessionNegotiatorFactory<SimpleMessage, SimpleSession, SimpleSessionListener> negotiatorFactory, final ProtocolHandlerFactory<?> factory,
+			final Promise<SimpleSession> promise) {
 		this.negotiatorFactory = Preconditions.checkNotNull(negotiatorFactory);
 		this.factory = Preconditions.checkNotNull(factory);
 	}
 
-	@Override
-	public void initializeChannel(final SocketChannel ch, final Promise<S> promise, final SessionListenerFactory<L> lfactory) {
-		ch.pipeline().addLast(this.factory.getDecoders());
-		ch.pipeline().addLast("negotiator", this.negotiatorFactory.getSessionNegotiator(lfactory, ch, promise));
-		ch.pipeline().addLast(this.factory.getEncoders());
-		logger.debug("initialization completed for channel {}", ch);
+	public Future<SimpleSession> createClient(final InetSocketAddress address, final ReconnectStrategy strategy, final SessionListenerFactory<SimpleSessionListener> listenerFactory) {
+		return super.createClient(address, strategy, new SimplePipelineInitializer(listenerFactory));
+	}
+
+	public ChannelFuture createServer(final InetSocketAddress address, final SessionListenerFactory<SimpleSessionListener> listenerFactory) {
+		return super.createServer(address, new SimplePipelineInitializer(listenerFactory));
 	}
 }
--- pcep/testtool/src/test/java/org/opendaylight/protocol/pcep/testtool/PCCMock.java
+++ pcep/testtool/src/test/java/org/opendaylight/protocol/pcep/testtool/PCCMock.java
@@ -10,6 +10,7 @@ package org.opendaylight.protocol.pcep.testtool;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.util.HashedWheelTimer;
 import io.netty.util.concurrent.DefaultPromise;
+import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GlobalEventExecutor;
 import io.netty.util.concurrent.Promise;
 
@@ -20,6 +21,7 @@ import org.opendaylight.protocol.framework.AbstractDispatcher;
 import org.opendaylight.protocol.framework.NeverReconnectStrategy;
 import org.opendaylight.protocol.framework.ProtocolHandlerFactory;
 import org.opendaylight.protocol.framework.ProtocolSession;
+import org.opendaylight.protocol.framework.ReconnectStrategy;
 import org.opendaylight.protocol.framework.SessionListener;
 import org.opendaylight.protocol.framework.SessionListenerFactory;
 import org.opendaylight.protocol.framework.SessionNegotiatorFactory;
@@ -47,11 +49,15 @@ AbstractDispatcher<S, L> {
 		this.factory = Preconditions.checkNotNull(factory);
 	}
 
-	@Override
-	public void initializeChannel(final SocketChannel ch, final Promise<S> promise, final SessionListenerFactory<L> listenerFactory) {
-		ch.pipeline().addLast(this.factory.getDecoders());
-		ch.pipeline().addLast("negotiator", this.negotiatorFactory.getSessionNegotiator(listenerFactory, ch, promise));
-		ch.pipeline().addLast(this.factory.getEncoders());
+	public Future<S> createClient(final InetSocketAddress address, final ReconnectStrategy strategy, final SessionListenerFactory<L> listenerFactory) {
+		return super.createClient(address, strategy, new PipelineInitializer<S>() {
+			@Override
+			public void initializeChannel(final SocketChannel ch, final Promise<S> promise) {
+				ch.pipeline().addLast(factory.getDecoders());
+				ch.pipeline().addLast("negotiator", negotiatorFactory.getSessionNegotiator(listenerFactory, ch, promise));
+				ch.pipeline().addLast(factory.getEncoders());
+			}
+		});
 	}
 
 	public static void main(final String[] args) throws Exception {
