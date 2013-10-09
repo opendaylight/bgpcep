7857529fcf1728f66173f5e9eb2e1f6bec68d8a4
Robert Varga <rovarga@cisco.com>
2013-10-09 13:19:30 +0200

    WIP: add ipv4/ipv6/linkstate buidlers
    Change-Id: I5442d4f6a39cf3bb4f7b155ff04f22cc7a9f629e
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ topology/provider-bgp/src/main/java/org/opendaylight/bgpcep/topology/provider/bgp/impl/Ipv6ReachabilityTopologyBuilder.java
@@ -0,0 +1,12 @@
+package org.opendaylight.bgpcep.topology.provider.bgp.impl;
+
+import org.opendaylight.bgpcep.topology.provider.bgp.LocRIBListener;
+import org.opendaylight.controller.sal.binding.api.data.DataChangeEvent;
+import org.opendaylight.controller.sal.binding.api.data.DataModification;
+
+final class Ipv6ReachabilityTopologyBuilder implements LocRIBListener {
+	@Override
+	public void onLocRIBChange(final DataModification trans, final DataChangeEvent event) {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ topology/provider-bgp/src/main/java/org/opendaylight/bgpcep/topology/provider/bgp/impl/LinkstateTopologyBuilder.java
@@ -0,0 +1,16 @@
+package org.opendaylight.bgpcep.topology.provider.bgp.impl;
+
+import org.opendaylight.bgpcep.topology.provider.bgp.LocRIBListener;
+import org.opendaylight.controller.sal.binding.api.data.DataChangeEvent;
+import org.opendaylight.controller.sal.binding.api.data.DataModification;
+
+public class LinkstateTopologyBuilder implements LocRIBListener {
+
+	@Override
+	public void onLocRIBChange(DataModification trans, DataChangeEvent event)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
