3049ba5a51eb3b4d86a93e57fad16e6fa65763dc
Robert Varga <rovarga@cisco.com>
2013-10-11 09:08:02 +0200

    BUG-113: Start rework of ParameterRegistry
    Change-Id: I4d04707fdb7a8dfbfbc3f92e6290f4883b1bbfb5
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/ParameterUtil.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.parser.spi;
+
+import com.google.common.primitives.UnsignedBytes;
+
+public final class ParameterUtil {
+	private ParameterUtil() {
+
+	}
+
+	public static byte[] formatParameter(final int type, final byte[] value) {
+		final byte[] bytes = new byte[2 + value.length];
+		bytes[0] = UnsignedBytes.checkedCast(type);
+		bytes[1] = UnsignedBytes.checkedCast(value.length);
+		System.arraycopy(value, 0, bytes, 2, value.length);
+		return bytes;
+	}
+}
