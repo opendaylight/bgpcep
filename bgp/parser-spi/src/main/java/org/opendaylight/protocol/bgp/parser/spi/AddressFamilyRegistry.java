071f75312272531fc95f4f01c3c6cc40d0c31165
Robert Varga <rovarga@cisco.com>
2013-10-11 11:27:37 +0200

    BUG-113: Convert ParserUtil into AFI/SAFI registries
    Change-Id: Ifdedf1a072be70f8e0797aeb9f8f562955216519
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/AddressFamilyRegistry.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.parser.spi;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.AddressFamily;
+
+public interface AddressFamilyRegistry {
+	public AutoCloseable registerAddressFamily(Class<? extends AddressFamily> clazz, int number);
+
+	public Class<? extends AddressFamily> classForFamily(int number);
+	public Integer numberForClass(Class<? extends AddressFamily> clazz);
+}
