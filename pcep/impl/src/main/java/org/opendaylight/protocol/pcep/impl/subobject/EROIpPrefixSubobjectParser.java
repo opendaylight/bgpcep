b6ceaff026f743871204cbb2ca7ac2f04d1234bb
Robert Varga <rovarga@cisco.com>
2013-10-15 15:09:46 +0200

    BUG-47: label model
    Change-Id: I7768eda3967f12526be2817fbc9f68e7efc84824
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/subobject/EROIpPrefixSubobjectParser.java
+++ pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/subobject/EROIpPrefixSubobjectParser.java
@@ -7,16 +7,16 @@
  */
 package org.opendaylight.protocol.pcep.impl.subobject;
 
-import org.opendaylight.protocol.util.ByteArray;
 import org.opendaylight.protocol.concepts.Ipv4Util;
 import org.opendaylight.protocol.concepts.Ipv6Util;
 import org.opendaylight.protocol.pcep.PCEPDeserializerException;
 import org.opendaylight.protocol.pcep.spi.SubobjectParser;
 import org.opendaylight.protocol.pcep.spi.SubobjectSerializer;
+import org.opendaylight.protocol.util.ByteArray;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpPrefix;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev130820.CSubobject;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev130820.IpPrefixSubobject;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev130820.route.subobjects.subobject.type.IpPrefixBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev130820.basic.explicit.route.subobjects.subobject.type.IpPrefixBuilder;
 
 import com.google.common.primitives.UnsignedBytes;
 
@@ -24,16 +24,16 @@ import com.google.common.primitives.UnsignedBytes;
  * Parser for {@link IpPrefixSubobject}
  */
 public class EROIpPrefixSubobjectParser implements SubobjectParser, SubobjectSerializer {
-	
+
 	public static final int TYPE = 32;
-	
+
 	public static final int IP4_F_LENGTH = 4;
 	public static final int PREFIX4_F_LENGTH = 1;
 
 	public static final int PREFIX4_F_OFFSET = IP4_F_LENGTH;
 
 	public static final int CONTENT4_LENGTH = PREFIX4_F_OFFSET + PREFIX4_F_LENGTH + 1; //added reserved field of size 1 byte
-	
+
 	public static final int IP_F_LENGTH = 16;
 	public static final int PREFIX_F_LENGTH = 1;
 
@@ -42,9 +42,11 @@ public class EROIpPrefixSubobjectParser implements SubobjectParser, SubobjectSer
 
 	public static final int CONTENT_LENGTH = PREFIX_F_OFFSET + PREFIX_F_LENGTH + 1; // added reserved field of size 1 byte
 
-	public IpPrefixSubobject parseSubobject(byte[] buffer) throws PCEPDeserializerException {
-		if (buffer == null || buffer.length == 0)
+	@Override
+	public IpPrefixSubobject parseSubobject(final byte[] buffer) throws PCEPDeserializerException {
+		if (buffer == null || buffer.length == 0) {
 			throw new IllegalArgumentException("Array of bytes is mandatory. Can't be null or empty.");
+		}
 
 		if (buffer.length == CONTENT4_LENGTH) {
 			final int length = UnsignedBytes.toInt(buffer[PREFIX4_F_OFFSET]);
@@ -52,14 +54,17 @@ public class EROIpPrefixSubobjectParser implements SubobjectParser, SubobjectSer
 		} else if (buffer.length == CONTENT_LENGTH) {
 			final int length = UnsignedBytes.toInt(buffer[PREFIX_F_OFFSET]);
 			return new IpPrefixBuilder().setIpPrefix(new IpPrefix(Ipv6Util.prefixForBytes(ByteArray.subByte(buffer, 0, IP_F_LENGTH), length))).build();
-		} else
+		} else {
 			throw new PCEPDeserializerException("Wrong length of array of bytes. Passed: " + buffer.length + ";");
+		}
 	}
 
-	public byte[] serializeSubobject(CSubobject subobject) {
-		if (!(subobject instanceof IpPrefixSubobject))
+	@Override
+	public byte[] serializeSubobject(final CSubobject subobject) {
+		if (!(subobject instanceof IpPrefixSubobject)) {
 			throw new IllegalArgumentException("Unknown subobject instance. Passed " + subobject.getClass()
 					+ ". Needed IpPrefixSubobject.");
+		}
 
 		final IpPrefixSubobject specObj = (IpPrefixSubobject) subobject;
 		final IpPrefix prefix = specObj.getIpPrefix();
@@ -72,8 +77,8 @@ public class EROIpPrefixSubobjectParser implements SubobjectParser, SubobjectSer
 			final byte[] retBytes = new byte[CONTENT4_LENGTH];
 			ByteArray.copyWhole(prefix.getIpv4Prefix().getValue().getBytes(), retBytes, 0);
 			return retBytes;
-		//retBytes[PREFIX_F_OFFSET] = ByteArray.intToBytes(prefix.getLength())[Integer.SIZE / Byte.SIZE - 1];
-		} 
+			//retBytes[PREFIX_F_OFFSET] = ByteArray.intToBytes(prefix.getLength())[Integer.SIZE / Byte.SIZE - 1];
+		}
 		return new byte[0];
 	}
 
