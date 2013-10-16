d1b50dbdbf2dcec11976e080c28e921af826381b
Robert Varga <rovarga@cisco.com>
2013-10-16 22:47:44 +0200

    Fixup pcep-api imports
    Change-Id: Ia9b78be96d9dfb46999c63c3f8fc208ac74b16e6
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- integration-tests/src/test/java/org/opendaylight/protocol/integration/PcepTesttoolBundleTest.java
+++ integration-tests/src/test/java/org/opendaylight/protocol/integration/PcepTesttoolBundleTest.java
@@ -14,7 +14,7 @@ import com.google.common.collect.Lists;
 public final class PcepTesttoolBundleTest extends AbstractBundleTest {
 	@Override
 	protected Collection<String> prerequisiteBundles() {
-		return Lists.newArrayList("concepts", "framework", "pcep-api", "pcep-impl", "util");
+		return Lists.newArrayList("concepts", "framework", "pcep-api", "pcep-impl", "rsvp-api", "util");
 	}
 
 	@Override
