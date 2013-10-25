ba76f730bfb239f582faf718ba85095bbe6644ee
Robert Varga <rovarga@cisco.com>
2013-10-25 15:56:28 +0200

    BUG-109: fix model and introduce provider
    Change-Id: I699a75d9ce4ef8a390eea6aedd22eae3390751a7
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ topology/tunnel-provider-pcep/src/main/java/org/opendaylight/bgpcep/topology/tunnel/provider/pcep/BundleActivator.java
@@ -0,0 +1,22 @@
+package org.opendaylight.bgpcep.topology.tunnel.provider.pcep;
+
+import org.opendaylight.controller.sal.binding.api.AbstractBindingAwareProvider;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.data.DataProviderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+
+public final class BundleActivator extends AbstractBindingAwareProvider {
+	private static final Logger LOG = LoggerFactory.getLogger(BundleActivator.class);
+
+	@Override
+	public void onSessionInitiated(final ProviderContext session) {
+		final DataProviderService dps = Preconditions.checkNotNull(session.getSALService(DataProviderService.class));
+
+		// FIXME: migrate to config subsystem
+		final TunnelTopologyExporter tte = new TunnelTopologyExporter(dps, null);
+		tte.addTargetTopology(null);
+	}
+}
