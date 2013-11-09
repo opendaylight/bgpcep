8b68ac817071960679eafbf909e5b02dfd770966
Robert Varga <rovarga@cisco.com>
2013-11-09 09:08:45 +0100

    BUG-191: provide a way for executors to provide scheduling feedback
    The idea is that the executor is hooked onto the ListenableFuture,
    kicking in if it succeeds. Should the insn be cancelled, the future
    will be, too.
    
    The notification part is handled internally until the Future succeeds --
    once it does, the executor can either start immediately executing, after
    checking with the Instruction, or inform it why it delayed execution.
    
    Once execution completes, the executor pushes the appropriate details
    into the instruction.
    
    Change-Id: I9dd9c29c876b890cadf842941c1e59e0f394cf84
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ pcep/topology-spi/src/main/java/org/opendaylight/bgpcep/pcep/topology/spi/AbstractInstructionExecutor.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.pcep.topology.spi;
+
+import org.opendaylight.bgpcep.programming.spi.Instruction;
+import org.opendaylight.bgpcep.programming.spi.InstructionScheduler;
+import org.opendaylight.bgpcep.programming.spi.SchedulerException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.programming.rev130930.InstructionStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.programming.rev130930.SubmitInstructionInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.programming.rev130930.submit.instruction.output.result.FailureCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.programming.rev130930.submit.instruction.output.result.FailureCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.OperationResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+/**
+ *
+ */
+public abstract class AbstractInstructionExecutor implements FutureCallback<Instruction> {
+	private static final Logger LOG = LoggerFactory.getLogger(AbstractInstructionExecutor.class);
+	private final SubmitInstructionInput input;
+
+	protected AbstractInstructionExecutor(final SubmitInstructionInput input) {
+		this.input = Preconditions.checkNotNull(input);
+	}
+
+	public final SubmitInstructionInput getInput() {
+		return input;
+	}
+
+	public static FailureCase schedule(final InstructionScheduler scheduler, final AbstractInstructionExecutor fwd) {
+		final ListenableFuture<Instruction> s;
+
+		try {
+			s = scheduler.scheduleInstruction(fwd.getInput());
+		} catch (SchedulerException e) {
+			LOG.info("Instuction {} failed to schedule", e);
+			return new FailureCaseBuilder().setFailure(e.getFailure()).build();
+		}
+
+		Futures.addCallback(s, fwd);
+		return null;
+	}
+
+	protected abstract ListenableFuture<OperationResult> invokeOperation();
+
+	@Override
+	public void onSuccess(final Instruction insn) {
+		if (insn.checkedExecutionStart()) {
+			final ListenableFuture<OperationResult> s = invokeOperation();
+			Futures.addCallback(s, new FutureCallback<OperationResult>() {
+				@Override
+				public void onSuccess(final OperationResult result) {
+					if (result.getFailure() != null) {
+						switch (result.getFailure()) {
+						case Failed:
+						case NoAck:
+							insn.executionCompleted(InstructionStatus.Failed, null);
+							break;
+						case Unsent:
+							insn.executionCompleted(InstructionStatus.Cancelled, null);
+							break;
+						}
+					} else {
+						insn.executionCompleted(InstructionStatus.Successful, null);
+					}
+				}
+
+				@Override
+				public void onFailure(final Throwable t) {
+					insn.executionCompleted(InstructionStatus.Failed, null);
+				}
+			});
+		}
+	}
+
+	@Override
+	public void onFailure(final Throwable t) {
+		LOG.debug("Instruction {} cancelled", input, t);
+	}
+}
---
+++ programming/spi/src/main/java/org/opendaylight/bgpcep/programming/spi/Instruction.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.programming.spi;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.programming.rev130930.InstructionStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.programming.rev130930.instruction.status.changed.Details;
+
+/**
+ *
+ */
+public interface Instruction {
+	/**
+	 * Instruction executors are required to call this method prior to
+	 * starting executing on the instruction. Implementations of this method
+	 * are required to transition into Executing state and return true, or
+	 * into Cancelled state and return false.
+	 * 
+	 * @return Indication whether the instruction execution should proceed.
+	 */
+	boolean checkedExecutionStart();
+
+	/**
+	 * Instruction executors can inform about execution hold ups which
+	 * prevent an otherwise-ready instruction from executing by calling this
+	 * method. It is recommended they check the return of this method to
+	 * detect if a cancellation occurred asynchronously.
+	 * 
+	 * @param details Details which execution is held up
+	 * @return Indication whether the instruction execution should proceed.
+	 *         If this method returns false, all subsequent calls to this
+	 *         method as well as {@link checkedExecutionStart()} will return
+	 *         false.
+	 */
+	boolean executionHeldUp(Details details);
+
+	/**
+	 * Instruction executors are required to call this method when execution
+	 * has finished to provide the execution result to the end.
+	 * 
+	 * @param status Execution result
+	 * @param details Execution result details
+	 */
+	void executionCompleted(InstructionStatus status, Details details);
+}
