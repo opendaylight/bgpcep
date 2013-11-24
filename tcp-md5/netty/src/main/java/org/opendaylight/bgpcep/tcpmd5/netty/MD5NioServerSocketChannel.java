62cb610f1b492aad5df246ee7bc3fee0c58147e1
Robert Varga <robert.varga@pantheon.sk>
2013-11-24 20:25:55 +0100

    Initial code drop of Netty.io integration
    This is binding glue allows for integration of the base TCP-MD5 code
    with Netty.io library.
    
    Change-Id: Id574736e4f3f8e21f75e79591190120b4495943b
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

---
+++ tcp-md5/netty/src/main/java/org/opendaylight/bgpcep/tcpmd5/netty/MD5NioServerSocketChannel.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2013 Robert Varga. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.tcpmd5.netty;
+
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import org.opendaylight.bgpcep.tcpmd5.MD5ServerSocketChannel;
+
+/**
+ * {@link NioServerSocketChannel} enabled with support for TCP MD5 Signature
+ * option.
+ */
+public class MD5NioServerSocketChannel extends NioServerSocketChannel {
+	private final MD5ServerSocketChannelConfig config;
+	private final MD5ServerSocketChannel channel;
+
+	public MD5NioServerSocketChannel() {
+		super();
+		this.channel = new MD5ServerSocketChannel(javaChannel());
+		this.config = new ProxyMD5ServerSocketChannelConfig(super.config(), channel);
+	}
+
+	@Override
+	public MD5ServerSocketChannelConfig config() {
+		return this.config;
+	}
+
+	@Override
+	protected MD5ServerSocketChannel javaChannel() {
+		return channel;
+	}
+}
---
+++ tcp-md5/netty/src/main/java/org/opendaylight/bgpcep/tcpmd5/netty/MD5NioSocketChannel.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2013 Robert Varga. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.tcpmd5.netty;
+
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import org.opendaylight.bgpcep.tcpmd5.MD5SocketChannel;
+
+/**
+ * {@link NioSocketChannel} enabled with support for TCP MD5 Signature
+ * option.
+ */
+public class MD5NioSocketChannel extends NioSocketChannel {
+	private final MD5SocketChannelConfig config;
+	private final MD5SocketChannel channel;
+
+	public MD5NioSocketChannel() {
+		super();
+		this.channel = new MD5SocketChannel(javaChannel());
+		this.config = new ProxyMD5SocketChannelConfig(super.config(), channel);
+	}
+
+	@Override
+	public MD5SocketChannelConfig config() {
+		return this.config;
+	}
+
+	@Override
+	protected MD5SocketChannel javaChannel() {
+		return channel;
+	}
+}
