a4ae8226e393810117fb989bccff78f92c98ab71
Robert Varga <robert.varga@pantheon.sk>
2013-11-24 20:25:55 +0100

    Initial code drop of Netty.io integration
    This is binding glue allows for integration of the base TCP-MD5 code
    with Netty.io library.
    
    Change-Id: Id574736e4f3f8e21f75e79591190120b4495943b
    Signed-off-by: Robert Varga <robert.varga@pantheon.sk>

---
+++ tcp-md5/netty/src/main/java/org/opendaylight/bgpcep/tcpmd5/netty/MD5ChannelOption.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2013 Robert Varga. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.tcpmd5.netty;
+
+import io.netty.channel.ChannelOption;
+
+/**
+ * TCP MD5 Signature {@link ChannelOption}.
+ */
+public final class MD5ChannelOption extends ChannelOption<byte[]> {
+	/**
+	 * Singleton instance of TCP MD5 Signature ChannelOption.
+	 */
+	public static final MD5ChannelOption TCP_MD5SIG = new MD5ChannelOption("TCP_MD5SIG");
+
+	private MD5ChannelOption(final String name) {
+		super(name);
+	}
+
+	@Override
+	public void validate(final byte[] value) {
+		// null value is allowed
+	}
+}
---
+++ tcp-md5/netty/src/main/java/org/opendaylight/bgpcep/tcpmd5/netty/MD5SocketChannelConfig.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2013 Robert Varga. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.tcpmd5.netty;
+
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.MessageSizeEstimator;
+import io.netty.channel.RecvByteBufAllocator;
+import io.netty.channel.socket.SocketChannelConfig;
+
+/**
+ * {@link SocketChannelConfig} augmented with TCP MD5 Signature option support.
+ */
+public interface MD5SocketChannelConfig extends SocketChannelConfig {
+	byte[] getMD5SignatureKey();
+
+	MD5SocketChannelConfig setMD5SignatureKey(byte[] key);
+
+	@Override
+	MD5SocketChannelConfig setTcpNoDelay(boolean tcpNoDelay);
+
+	@Override
+	MD5SocketChannelConfig setSoLinger(int soLinger);
+
+	@Override
+	MD5SocketChannelConfig setSendBufferSize(int sendBufferSize);
+
+	@Override
+	MD5SocketChannelConfig setReceiveBufferSize(int receiveBufferSize);
+
+	@Override
+	MD5SocketChannelConfig setKeepAlive(boolean keepAlive);
+
+	@Override
+	MD5SocketChannelConfig setTrafficClass(int trafficClass);
+
+	@Override
+	MD5SocketChannelConfig setReuseAddress(boolean reuseAddress);
+
+	@Override
+	MD5SocketChannelConfig setPerformancePreferences(int connectionTime, int latency, int bandwidth);
+
+	@Override
+	MD5SocketChannelConfig setAllowHalfClosure(boolean allowHalfClosure);
+
+	@Override
+	MD5SocketChannelConfig setConnectTimeoutMillis(int connectTimeoutMillis);
+
+	@Override
+	MD5SocketChannelConfig setMaxMessagesPerRead(int maxMessagesPerRead);
+
+	@Override
+	MD5SocketChannelConfig setWriteSpinCount(int writeSpinCount);
+
+	@Override
+	MD5SocketChannelConfig setAllocator(ByteBufAllocator allocator);
+
+	@Override
+	MD5SocketChannelConfig setRecvByteBufAllocator(RecvByteBufAllocator allocator);
+
+	@Override
+	MD5SocketChannelConfig setAutoRead(boolean autoRead);
+
+	@Override
+	MD5SocketChannelConfig setMessageSizeEstimator(MessageSizeEstimator estimator);
+}
