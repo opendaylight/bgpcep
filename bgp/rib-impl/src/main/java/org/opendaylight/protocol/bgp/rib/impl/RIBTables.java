e36eb697fdae7536ae04056f1c59729a9889608d
Robert Varga <rovarga@cisco.com>
2013-11-27 16:50:17 +0100

    Handle legacy NLRI in messages
    Change-Id: Ia000427b882b2b6563550b3c6657559b5c063ac4
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/RIBTables.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/RIBTables.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.opendaylight.protocol.bgp.rib.spi.AdjRIBsIn;
+import org.opendaylight.protocol.bgp.rib.spi.AdjRIBsInFactory;
 import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionConsumerContext;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.PathAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.TablesKey;
@@ -26,16 +27,17 @@ final class RIBTables {
 	}
 
 	public synchronized AdjRIBsIn getOrCreate(final TablesKey key) {
-		final AdjRIBsIn table;
-
-		if (!tables.containsKey(key)) {
-			table = registry.getAdjRIBsInFactory(key.getAfi(), key.getSafi()).createAdjRIBsIn(comparator, key);
-			tables.put(key, table);
-		} else {
-			table = tables.get(key);
+		if (tables.containsKey(key)) {
+			return tables.get(key);
 		}
 
+		final AdjRIBsInFactory f = registry.getAdjRIBsInFactory(key.getAfi(), key.getSafi());
+		if (f == null) {
+			return null;
+		}
+
+		final AdjRIBsIn table = Preconditions.checkNotNull(f.createAdjRIBsIn(comparator, key));
+		tables.put(key, table);
 		return table;
 	}
-
 }
