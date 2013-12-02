8a8ef89a9b83f173e73616b4306f7e370305ae33
Robert Varga <rovarga@cisco.com>
2013-12-02 19:56:20 +0100

    BUG-193: shut the topology provider down correctly
    Change-Id: I6c832b6452721f33cd5d3cf977733b2b30744af2
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPTopologyProvider.java
+++ pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPTopologyProvider.java
@@ -9,27 +9,35 @@ package org.opendaylight.bgpcep.pcep.topology.provider;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutionException;
 
 import org.opendaylight.bgpcep.programming.spi.InstructionScheduler;
 import org.opendaylight.bgpcep.topology.DefaultTopologyReference;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 import org.opendaylight.controller.sal.binding.api.data.DataProviderService;
 import org.opendaylight.protocol.pcep.PCEPDispatcher;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.programming.rev131106.NetworkTopologyPcepProgrammingService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.NetworkTopologyPcepService;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 
 public final class PCEPTopologyProvider extends DefaultTopologyReference implements AutoCloseable {
+	private static final Logger LOG = LoggerFactory.getLogger(PCEPTopologyProvider.class);
+	private final RpcRegistration<NetworkTopologyPcepProgrammingService> network;
+	private final RpcRegistration<NetworkTopologyPcepService> element;
 	private final ServerSessionManager manager;
-	private final TopologyProgramming network;
-	private final TopologyRPCs element;
 	private final Channel channel;
 
 	private PCEPTopologyProvider(final Channel channel, final InstanceIdentifier<Topology> topology, final ServerSessionManager manager,
-			final TopologyRPCs element, final TopologyProgramming network) {
+			final RpcRegistration<NetworkTopologyPcepService> element, final RpcRegistration<NetworkTopologyPcepProgrammingService> network) {
 		super(topology);
 		this.channel = Preconditions.checkNotNull(channel);
 		this.manager = Preconditions.checkNotNull(manager);
@@ -38,20 +46,45 @@ public final class PCEPTopologyProvider extends DefaultTopologyReference impleme
 	}
 
 	public static PCEPTopologyProvider create(final PCEPDispatcher dispatcher, final InetSocketAddress address,
-			final InstructionScheduler scheduler, final DataProviderService dataService, final InstanceIdentifier<Topology> topology)
-			throws InterruptedException, ExecutionException {
+			final InstructionScheduler scheduler, final DataProviderService dataService, final RpcProviderRegistry rpcRegistry,
+			final InstanceIdentifier<Topology> topology)
+					throws InterruptedException, ExecutionException {
 
 		final ServerSessionManager manager = new ServerSessionManager(dataService, topology);
-		final TopologyRPCs element = new TopologyRPCs(manager);
-		final TopologyProgramming network = new TopologyProgramming(scheduler, manager);
 		final ChannelFuture f = dispatcher.createServer(address, manager);
 		f.get();
+
+		final RpcRegistration<NetworkTopologyPcepService> element = rpcRegistry.addRpcImplementation(NetworkTopologyPcepService.class, new TopologyRPCs(manager));
+		final RpcRegistration<NetworkTopologyPcepProgrammingService> network = rpcRegistry.addRpcImplementation(NetworkTopologyPcepProgrammingService.class, new TopologyProgramming(scheduler, manager));
+
 		return new PCEPTopologyProvider(f.channel(), topology, manager, element, network);
 	}
 
 	@Override
-	public void close() throws Exception {
-		this.channel.close();
-		// FIXME: BUG-193: close other stuff
+	public void close() {
+		LOG.debug("Closing server channel {}", channel);
+
+		channel.close().addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(final ChannelFuture f) throws Exception {
+				LOG.debug("Server channel {} closed", f.channel());
+
+				try {
+					network.close();
+				} catch (Exception e) {
+					LOG.error("Failed to unregister network-level RPCs", e);
+				}
+				try {
+					element.close();
+				} catch (Exception e) {
+					LOG.error("Failed to unregister element-level RPCs", e);
+				}
+				try {
+					manager.close();
+				} catch (InterruptedException | ExecutionException e) {
+					LOG.error("Failed to shutdown session manager", e);
+				}
+			}
+		});
 	}
 }
