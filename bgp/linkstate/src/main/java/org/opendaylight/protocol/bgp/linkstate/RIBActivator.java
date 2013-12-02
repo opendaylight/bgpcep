8699ad69bc1ee4ce2cb8491a16fbe3e0df89a68b
Robert Varga <rovarga@cisco.com>
2013-12-02 23:29:31 +0100

    Implement proper activator shutdown
    Change-Id: I14dcfd8015dc5ace84c3ce36c5accedb55ea4950
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/RIBActivator.java
+++ bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/RIBActivator.java
@@ -8,47 +8,28 @@
 package org.opendaylight.protocol.bgp.linkstate;
 
 import java.util.Comparator;
+import java.util.List;
 
 import org.opendaylight.controller.sal.binding.api.data.DataModificationTransaction;
+import org.opendaylight.protocol.bgp.rib.spi.AbstractRIBExtensionProviderActivator;
 import org.opendaylight.protocol.bgp.rib.spi.AdjRIBsIn;
 import org.opendaylight.protocol.bgp.rib.spi.AdjRIBsInFactory;
-import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderActivator;
 import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderContext;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.LinkstateAddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.LinkstateSubsequentAddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.PathAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.TablesKey;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 
-public final class RIBActivator implements RIBExtensionProviderActivator {
-	private static final Logger LOG = LoggerFactory.getLogger(RIBActivator.class);
-	private AutoCloseable reg;
-
+public final class RIBActivator extends AbstractRIBExtensionProviderActivator {
 	@Override
-	public void startRIBExtensionProvider(final RIBExtensionProviderContext context) {
-		Preconditions.checkState(reg == null);
-
-		reg = context.registerAdjRIBsInFactory(LinkstateAddressFamily.class, LinkstateSubsequentAddressFamily.class, new AdjRIBsInFactory() {
+	protected List<AutoCloseable> startRIBExtensionProviderImpl(final RIBExtensionProviderContext context) {
+		return Lists.newArrayList(context.registerAdjRIBsInFactory(LinkstateAddressFamily.class, LinkstateSubsequentAddressFamily.class, new AdjRIBsInFactory() {
 			@Override
 			public AdjRIBsIn createAdjRIBsIn(final DataModificationTransaction trans, final Comparator<PathAttributes> comparator, final TablesKey key) {
 				return new LinkstateAdjRIBsIn(trans, comparator, key);
 			}
-		});
-	}
-
-	@Override
-	public void stopRIBExtensionProvider() {
-		if (reg != null) {
-			try {
-				reg.close();
-			} catch (Exception e) {
-				LOG.warn("Failed to unregister Linkstate extension", e);
-			} finally {
-				reg = null;
-			}
-		}
+		}));
 	}
 }
--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/RIBActivator.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/RIBActivator.java
@@ -8,66 +8,37 @@
 package org.opendaylight.protocol.bgp.rib.impl;
 
 import java.util.Comparator;
+import java.util.List;
 
 import org.opendaylight.controller.sal.binding.api.data.DataModificationTransaction;
+import org.opendaylight.protocol.bgp.rib.spi.AbstractRIBExtensionProviderActivator;
 import org.opendaylight.protocol.bgp.rib.spi.AdjRIBsIn;
 import org.opendaylight.protocol.bgp.rib.spi.AdjRIBsInFactory;
-import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderActivator;
 import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderContext;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.PathAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.TablesKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv4AddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv6AddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.UnicastSubsequentAddressFamily;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 
-public final class RIBActivator implements RIBExtensionProviderActivator {
-	private static final Logger LOG = LoggerFactory.getLogger(RIBActivator.class);
-	private AutoCloseable v4reg, v6reg;
+public final class RIBActivator extends AbstractRIBExtensionProviderActivator {
 
 	@Override
-	public void startRIBExtensionProvider(final RIBExtensionProviderContext context) {
-		Preconditions.checkState(v4reg == null);
-		Preconditions.checkState(v6reg == null);
-
-		v4reg = context.registerAdjRIBsInFactory(Ipv4AddressFamily.class, UnicastSubsequentAddressFamily.class, new AdjRIBsInFactory() {
-			@Override
-			public AdjRIBsIn createAdjRIBsIn(final DataModificationTransaction trans, final Comparator<PathAttributes> comparator, final TablesKey key) {
-				return new Ipv4AdjRIBsIn(trans, comparator, key);
-			}
-		});
-
-		v6reg = context.registerAdjRIBsInFactory(Ipv6AddressFamily.class, UnicastSubsequentAddressFamily.class, new AdjRIBsInFactory() {
-			@Override
-			public AdjRIBsIn createAdjRIBsIn(final DataModificationTransaction trans, final Comparator<PathAttributes> comparator, final TablesKey key) {
-				return new Ipv6AdjRIBsIn(trans, comparator, key);
-			}
-		});
-
-	}
-
-	@Override
-	public void stopRIBExtensionProvider() {
-		if (v4reg != null) {
-			try {
-				v4reg.close();
-			} catch (Exception e) {
-				LOG.warn("Failed to unregister IPv4 extension", e);
-			} finally {
-				v4reg = null;
-			}
-		}
-		if (v6reg != null) {
-			try {
-				v6reg.close();
-			} catch (Exception e) {
-				LOG.warn("Failed to unregister IPv6 extension", e);
-			} finally {
-				v6reg = null;
-			}
-		}
+	protected List<AutoCloseable> startRIBExtensionProviderImpl(final RIBExtensionProviderContext context) {
+		return Lists.newArrayList(
+				context.registerAdjRIBsInFactory(Ipv4AddressFamily.class, UnicastSubsequentAddressFamily.class, new AdjRIBsInFactory() {
+					@Override
+					public AdjRIBsIn createAdjRIBsIn(final DataModificationTransaction trans, final Comparator<PathAttributes> comparator, final TablesKey key) {
+						return new Ipv4AdjRIBsIn(trans, comparator, key);
+					}
+				}),
+				context.registerAdjRIBsInFactory(Ipv6AddressFamily.class, UnicastSubsequentAddressFamily.class, new AdjRIBsInFactory() {
+					@Override
+					public AdjRIBsIn createAdjRIBsIn(final DataModificationTransaction trans, final Comparator<PathAttributes> comparator, final TablesKey key) {
+						return new Ipv6AdjRIBsIn(trans, comparator, key);
+					}
+				}));
 	}
 }
