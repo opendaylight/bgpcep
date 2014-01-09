947419c1d65c05ec60e990eaf7205b2ca42684e7
Maros Marsalek <mmarsale@cisco.com>
2014-01-09 12:52:25 +0100

    Added routed rpcs to pcep-topology|tunnel-provider
    Also added integration(pax-exam) test to test the routing
    
    Change-Id: I385f99ecc0fbb9c3cee0a555c87335f3080e2412
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

---
+++ integration-tests/src/test/java/org/opendaylight/protocol/integration/pcep/AbstractPcepOsgiTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.integration.pcep;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yangtools.yang.binding.RpcService;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.options.DefaultCompositeOption;
+import org.ops4j.pax.exam.util.Filter;
+import org.osgi.framework.BundleContext;
+
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+
+public class AbstractPcepOsgiTest {
+	@Inject
+	@Filter(timeout=60*1000)
+	BindingAwareBroker broker;
+	@Inject
+	BundleContext bundleContext;
+
+	public BindingAwareBroker getBroker() {
+		return broker;
+	}
+
+	public void setBroker(BindingAwareBroker broker) {
+		this.broker = broker;
+	}
+
+	public BundleContext getBundleContext() {
+		return bundleContext;
+	}
+
+	public void setBundleContext(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+
+	@Configuration
+	public Option[] config() {
+		return options(mavenBundle("org.slf4j", "slf4j-api")
+				.versionAsInProject(), //
+//				mavenBundle("org.slf4j", "log4j-over-slf4j").versionAsInProject(), //
+				mavenBundle("ch.qos.logback", "logback-core").versionAsInProject(), //
+				mavenBundle("ch.qos.logback", "logback-classic").versionAsInProject(), //
+
+				pcepModules(), //
+				systemProperty("osgi.bundles.defaultStartLevel").value("4"),
+
+				TestHelper.mdSalCoreBundles(),
+
+				TestHelper.bindingAwareSalBundles(),
+				TestHelper.configMinumumBundles(),
+				// BASE Models
+				TestHelper.baseModelBundles(),
+				TestHelper.flowCapableModelBundles(),
+				TestHelper.junitAndMockitoBundles());
+	}
+
+	private Option pcepModules() {
+		return new DefaultCompositeOption(mavenBundle("org.opendaylight.yangtools.model", "ietf-topology").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "pcep-topology-api").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "pcep-tunnel-api").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "pcep-api").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "topology-api").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "topology-tunnel-api").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "programming-topology-api").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "programming-tunnel-api").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "concepts").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "util").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "rsvp-api").versionAsInProject(), //
+				mavenBundle("org.opendaylight.bgpcep", "programming-api").versionAsInProject());
+	}
+
+	abstract class AbstractTestProvider implements BindingAwareProvider {
+
+		@Override
+		public Collection<? extends RpcService> getImplementations() {
+			return Collections.emptySet();
+		}
+
+		@Override
+		public Collection<? extends ProviderFunctionality> getFunctionality() {
+			return Collections.emptySet();
+		}
+
+		@Override
+		public void onSessionInitialized(BindingAwareBroker.ConsumerContext session) {
+			// Noop
+
+		}
+
+	}
+
+	TopologyKey getTopologyId(String id) {
+		return new TopologyKey(new org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId(id));
+	}
+}
