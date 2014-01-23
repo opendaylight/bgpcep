8d32e357694b1d52280412c97a3c5d2a3304e717
Robert Varga <rovarga@cisco.com>
2014-01-23 13:18:43 +0100

    Make base pcep-topology-api independent of stateful
    Change-Id: I0da583e0c51932baf5559828aa5e9fbcb2fe2296
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/Stateful07TopologySessionListener.java
+++ pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/Stateful07TopologySessionListener.java
@@ -8,22 +8,27 @@
 package org.opendaylight.bgpcep.pcep.topology.provider;
 
 import java.net.InetAddress;
+import java.nio.ByteBuffer;
 import java.util.Collections;
 
 import javax.annotation.concurrent.GuardedBy;
 
-import org.opendaylight.controller.md.sal.common.api.TransactionStatus;
 import org.opendaylight.controller.sal.binding.api.data.DataModificationTransaction;
 import org.opendaylight.protocol.pcep.PCEPSession;
 import org.opendaylight.protocol.pcep.PCEPSessionListener;
-import org.opendaylight.protocol.pcep.TerminationReason;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.crabbe.initiated.rev131126.PcinitiateBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.crabbe.initiated.rev131126.pcinitiate.message.PcinitiateMessageBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.crabbe.initiated.rev131126.pcinitiate.message.pcinitiate.message.RequestsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.Arguments1;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.OperationalStatus;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.PcrptMessage;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.PcupdBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.PlspId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.ReportedLsp1;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.ReportedLsp1Builder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.SrpIdNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.StatefulTlv1;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.StatefulTlv1Builder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.SymbolicPathName;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.Tlvs2;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.lsp.object.Lsp;
@@ -48,22 +53,17 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.pcep.client.attributes.PathComputationClientBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.pcep.client.attributes.path.computation.client.ReportedLsp;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.pcep.client.attributes.path.computation.client.ReportedLspBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.pcep.client.attributes.path.computation.client.ReportedLspKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.pcep.client.attributes.path.computation.client.StatefulTlvBuilder;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.InstanceIdentifierBuilder;
-import org.opendaylight.yangtools.yang.common.RpcResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.JdkFutureAdapters;
 import com.google.common.util.concurrent.ListenableFuture;
 
-final class Stateful07TopologySessionListener extends AbstractTopologySessionListener<SrpIdNumber, PlspId, SymbolicPathName> implements PCEPSessionListener {
+final class Stateful07TopologySessionListener extends AbstractTopologySessionListener<SrpIdNumber, PlspId> implements PCEPSessionListener {
 	private static final Logger LOG = LoggerFactory.getLogger(Stateful07TopologySessionListener.class);
 
 	/**
@@ -81,12 +81,14 @@ final class Stateful07TopologySessionListener extends AbstractTopologySessionLis
 		final InetAddress peerAddress = session.getRemoteAddress();
 
 		final Tlvs tlvs = session.getRemoteTlvs();
-		if (tlvs.getAugmentation(Tlvs2.class) != null) {
-			final Stateful stateful = tlvs.getAugmentation(Tlvs2.class).getStateful();
+		final Tlvs2 tlv = tlvs.getAugmentation(Tlvs2.class);
+		if (tlv != null) {
+			final Stateful stateful = tlv.getStateful();
 			if (stateful != null) {
 				pccBuilder.setReportedLsp(Collections.<ReportedLsp> emptyList());
-				pccBuilder.setStatefulTlv(new StatefulTlvBuilder().setStateful(stateful).build());
 				pccBuilder.setStateSync(PccSyncState.InitialResync);
+				pccBuilder.setStatefulTlv(
+						new StatefulTlvBuilder().addAugmentation(StatefulTlv1.class, new StatefulTlv1Builder(tlv).build()).build());
 			} else {
 				LOG.debug("Peer {} does not advertise stateful TLV", peerAddress);
 			}
@@ -95,21 +97,13 @@ final class Stateful07TopologySessionListener extends AbstractTopologySessionLis
 		}
 	}
 
-	private InstanceIdentifierBuilder<ReportedLsp> lspIdentifier(final ReportedLspKey key) {
-		return pccIdentifier().child(ReportedLsp.class, key);
-	}
-
 	@Override
-	public synchronized void onMessage(final PCEPSession session, final Message message) {
+	protected synchronized boolean onMessage(final DataModificationTransaction trans, final Message message) {
 		if (!(message instanceof PcrptMessage)) {
-			LOG.info("Unhandled message {} on session {}", message, session);
-			session.sendMessage(UNHANDLED_MESSAGE_ERROR);
+			return true;
 		}
 
 		final org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.pcrpt.message.PcrptMessage rpt = ((PcrptMessage) message).getPcrptMessage();
-
-		final DataModificationTransaction trans = this.serverSessionManager.beginTransaction();
-
 		for (final Reports r : rpt.getReports()) {
 			final Lsp lsp = r.getLsp();
 
@@ -118,7 +112,8 @@ final class Stateful07TopologySessionListener extends AbstractTopologySessionLis
 				continue;
 			}
 
-			final ReportedLspBuilder rlb = new ReportedLspBuilder(r);
+			final ReportedLspBuilder rlb = new ReportedLspBuilder();
+			rlb.addAugmentation(ReportedLsp1.class, new ReportedLsp1Builder(r).build());
 			boolean solicited = false;
 
 			final Srp srp = r.getSrp();
@@ -150,56 +145,24 @@ final class Stateful07TopologySessionListener extends AbstractTopologySessionLis
 			}
 
 			final PlspId id = lsp.getPlspId();
-			if (lsp.isRemove()) {
-				final SymbolicPathName name = removeLsp(id);
-				if (name != null) {
-					trans.removeOperationalData(lspIdentifier(new ReportedLspKey(name)).build());
-				}
-
-				LOG.debug("LSP {} removed", lsp);
-			} else {
-				SymbolicPathName name = getLsp(id);
-				if (name == null) {
-					LOG.debug("PLSPID {} not known yet, looking for a symbolic name", id);
-
-					final org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.lsp.object.lsp.Tlvs tlvs = r.getLsp().getTlvs();
-					if (tlvs != null && tlvs.getSymbolicPathName() != null) {
-						name = tlvs.getSymbolicPathName().getPathName();
-						addLsp(id, name);
-					} else {
-						LOG.error("PLSPID {} seen for the first time, not reporting the LSP", id);
-						continue;
-					}
-				}
-
-				Preconditions.checkState(name != null);
-				rlb.setKey(new ReportedLspKey(name));
-
-				// If this is an unsolicited update. We need to make sure we retain the metadata already present
-				if (solicited) {
-					updateLspMetadata(name, rlb.getMetadata());
+			if (!lsp.isRemove()) {
+				final org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.lsp.object.lsp.Tlvs tlvs = r.getLsp().getTlvs();
+				final String name;
+				if (tlvs != null && tlvs.getSymbolicPathName() != null) {
+					name = Charsets.UTF_8.decode(ByteBuffer.wrap(tlvs.getSymbolicPathName().getPathName().getValue())).toString();
 				} else {
-					rlb.setMetadata(getLspMetadata(name));
+					name = null;
 				}
 
-				trans.putOperationalData(lspIdentifier(rlb.getKey()).build(), rlb.build());
-
+				updateLsp(trans, id, name, rlb, solicited);
 				LOG.debug("LSP {} updated", lsp);
+			} else {
+				removeLsp(trans, id);
+				LOG.debug("LSP {} removed", lsp);
 			}
 		}
 
-		Futures.addCallback(JdkFutureAdapters.listenInPoolThread(trans.commit()), new FutureCallback<RpcResult<TransactionStatus>>() {
-			@Override
-			public void onSuccess(final RpcResult<TransactionStatus> result) {
-				LOG.trace("Internal state for session {} updated successfully", session);
-			}
-
-			@Override
-			public void onFailure(final Throwable t) {
-				LOG.error("Failed to update internal state for session {}, closing it", session, t);
-				session.close(TerminationReason.Unknown);
-			}
-		});
+		return false;
 	}
 
 	@GuardedBy("this")
@@ -210,7 +173,7 @@ final class Stateful07TopologySessionListener extends AbstractTopologySessionLis
 	@Override
 	public synchronized ListenableFuture<OperationResult> addLsp(final AddLspArgs input) {
 		// Make sure there is no such LSP
-		final InstanceIdentifier<ReportedLsp> lsp = lspIdentifier(new ReportedLspKey(input.getName())).build();
+		final InstanceIdentifier<ReportedLsp> lsp = lspIdentifier(input.getName()).build();
 		if (serverSessionManager.readOperationalData(lsp) != null) {
 			LOG.debug("Node {} already contains lsp {} at {}", input.getNode(), input.getName(), lsp);
 			return OperationResults.UNSENT.future();
@@ -221,7 +184,7 @@ final class Stateful07TopologySessionListener extends AbstractTopologySessionLis
 		rb.fieldsFrom(input.getArguments());
 		rb.setSrp(new SrpBuilder().setOperationId(nextRequest()).setProcessingRule(Boolean.TRUE).build());
 		rb.setLsp(new LspBuilder().setAdministrative(input.getArguments().isAdministrative()).setDelegate(Boolean.TRUE).setTlvs(
-				new TlvsBuilder().setSymbolicPathName(new SymbolicPathNameBuilder().setPathName(input.getName()).build()).build()).build());
+				new TlvsBuilder().setSymbolicPathName(new SymbolicPathNameBuilder().setPathName(new SymbolicPathName(input.getName().getBytes(Charsets.UTF_8))).build()).build()).build());
 
 		final PcinitiateMessageBuilder ib = new PcinitiateMessageBuilder(MESSAGE_HEADER);
 		ib.setRequests(ImmutableList.of(rb.build()));
@@ -234,17 +197,20 @@ final class Stateful07TopologySessionListener extends AbstractTopologySessionLis
 	@Override
 	public synchronized ListenableFuture<OperationResult> removeLsp(final RemoveLspArgs input) {
 		// Make sure the LSP exists, we need it for PLSP-ID
-		final InstanceIdentifier<ReportedLsp> lsp = lspIdentifier(new ReportedLspKey(input.getName())).build();
+		final InstanceIdentifier<ReportedLsp> lsp = lspIdentifier(input.getName()).build();
 		final ReportedLsp rep = serverSessionManager.readOperationalData(lsp);
 		if (rep == null) {
 			LOG.debug("Node {} does not contain LSP {}", input.getNode(), input.getName());
 			return OperationResults.UNSENT.future();
 		}
 
+		final ReportedLsp1 ra = rep.getAugmentation(ReportedLsp1.class);
+		Preconditions.checkState(ra != null);
+
 		// Build the request and send it
 		final RequestsBuilder rb = new RequestsBuilder();
 		rb.setSrp(new SrpBuilder().setOperationId(nextRequest()).setProcessingRule(Boolean.TRUE).build());
-		rb.setLsp(new LspBuilder().setRemove(Boolean.TRUE).setPlspId(rep.getLsp().getPlspId()).setDelegate(Boolean.TRUE).build());
+		rb.setLsp(new LspBuilder().setRemove(Boolean.TRUE).setPlspId(ra.getLsp().getPlspId()).setDelegate(Boolean.TRUE).build());
 
 		final PcinitiateMessageBuilder ib = new PcinitiateMessageBuilder(MESSAGE_HEADER);
 		ib.setRequests(ImmutableList.of(rb.build()));
@@ -254,17 +220,20 @@ final class Stateful07TopologySessionListener extends AbstractTopologySessionLis
 	@Override
 	public synchronized ListenableFuture<OperationResult> updateLsp(final UpdateLspArgs input) {
 		// Make sure the LSP exists
-		final InstanceIdentifier<ReportedLsp> lsp = lspIdentifier(new ReportedLspKey(input.getName())).build();
+		final InstanceIdentifier<ReportedLsp> lsp = lspIdentifier(input.getName()).build();
 		final ReportedLsp rep = serverSessionManager.readOperationalData(lsp);
 		if (rep == null) {
 			LOG.debug("Node {} does not contain LSP {}", input.getNode(), input.getName());
 			return OperationResults.UNSENT.future();
 		}
 
+		final ReportedLsp1 ra = rep.getAugmentation(ReportedLsp1.class);
+		Preconditions.checkState(ra != null);
+
 		// Build the PCUpd request and send it
 		final UpdatesBuilder rb = new UpdatesBuilder();
 		rb.setSrp(new SrpBuilder().setOperationId(nextRequest()).setProcessingRule(Boolean.TRUE).build());
-		rb.setLsp(new LspBuilder().setPlspId(rep.getLsp().getPlspId()).setDelegate(Boolean.TRUE).build());
+		rb.setLsp(new LspBuilder().setPlspId(ra.getLsp().getPlspId()).setDelegate(Boolean.TRUE).build());
 		final PathBuilder pb = new PathBuilder();
 		rb.setPath(pb.setEro(input.getArguments().getEro()).build());
 
@@ -276,16 +245,28 @@ final class Stateful07TopologySessionListener extends AbstractTopologySessionLis
 
 	@Override
 	public synchronized ListenableFuture<OperationResult> ensureLspOperational(final EnsureLspOperationalInput input) {
+		OperationalStatus op = null;
+		final Arguments1 aa = input.getArguments().getAugmentation(Arguments1.class);
+		if (aa != null) {
+			op = aa.getOperational();
+		}
+
 		// Make sure the LSP exists
-		final InstanceIdentifier<ReportedLsp> lsp = lspIdentifier(new ReportedLspKey(input.getName())).build();
-		LOG.debug("Checking if LSP {} has operational state {}", lsp, input.getArguments().getOperational());
+		final InstanceIdentifier<ReportedLsp> lsp = lspIdentifier(input.getName()).build();
+		LOG.debug("Checking if LSP {} has operational state {}", lsp, op);
 		final ReportedLsp rep = serverSessionManager.readOperationalData(lsp);
 		if (rep == null) {
 			LOG.debug("Node {} does not contain LSP {}", input.getNode(), input.getName());
 			return OperationResults.UNSENT.future();
 		}
 
-		if (rep.getLsp().getOperational().equals(input.getArguments().getOperational())) {
+		final ReportedLsp1 ra = rep.getAugmentation(ReportedLsp1.class);
+		if (ra == null) {
+			LOG.warn("Node {} LSP {} does not contain data", input.getNode(), input.getName());
+			return OperationResults.UNSENT.future();
+		}
+
+		if (ra.getLsp().getOperational().equals(op)) {
 			return OperationResults.SUCCESS.future();
 		} else {
 			return OperationResults.UNSENT.future();
