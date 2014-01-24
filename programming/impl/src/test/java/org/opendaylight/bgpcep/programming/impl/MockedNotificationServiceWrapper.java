6471f1d114964209031311b489eb8733844cc35f
Dana Kutenicsova <dkutenic@cisco.com>
2014-01-24 09:55:12 +0100

    Added more copyright headers.
    Change-Id: I57847eb1712dea936bcf79e035fe3ba5fe0b979b
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- programming/impl/src/test/java/org/opendaylight/bgpcep/programming/impl/MockedNotificationServiceWrapper.java
+++ programming/impl/src/test/java/org/opendaylight/bgpcep/programming/impl/MockedNotificationServiceWrapper.java
@@ -1,15 +1,22 @@
-/**
- * @author Maros Marsalek
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
  *
- * 01 2014
- *
- * Copyright (c) 2012 by Cisco Systems, Inc.
- * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 package org.opendaylight.bgpcep.programming.impl;
 
-import com.google.common.collect.Lists;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
 import junit.framework.Assert;
+
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
@@ -18,48 +25,42 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.programm
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.programming.rev130930.InstructionStatusChanged;
 import org.opendaylight.yangtools.yang.binding.Notification;
 
-import java.util.List;
-
-import static junit.framework.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
+import com.google.common.collect.Lists;
 
 final class MockedNotificationServiceWrapper {
-	private List<Notification> publishedNotifications;
+	private final List<Notification> publishedNotifications;
 
 	MockedNotificationServiceWrapper() {
-		publishedNotifications = Lists.newArrayList();
+		this.publishedNotifications = Lists.newArrayList();
 	}
 
 	NotificationProviderService getMockedNotificationService() {
-		NotificationProviderService mockedNotificationService = mock(NotificationProviderService.class);
+		final NotificationProviderService mockedNotificationService = mock(NotificationProviderService.class);
 
 		doAnswer(new Answer() {
 			@Override
-			public Object answer(InvocationOnMock invocation) throws Throwable {
-				Object notif = invocation.getArguments()[0];
+			public Object answer(final InvocationOnMock invocation) throws Throwable {
+				final Object notif = invocation.getArguments()[0];
 				assertTrue(Notification.class.isAssignableFrom(notif.getClass()));
-				publishedNotifications.add((Notification) notif);
+				MockedNotificationServiceWrapper.this.publishedNotifications.add((Notification) notif);
 				return null;
 			}
 		}).when(mockedNotificationService).publish(any(Notification.class));
 		return mockedNotificationService;
 	}
 
-	void assertNotificationsCount(int count) {
-		assertEquals(count, publishedNotifications.size());
+	void assertNotificationsCount(final int count) {
+		assertEquals(count, this.publishedNotifications.size());
 	}
 
-	public void assertInstructionStatusChangedNotification(int idx, InstructionId id, InstructionStatus status) {
-		assertTrue(InstructionStatusChanged.class.isAssignableFrom(publishedNotifications.get(idx).getClass()));
-		InstructionStatusChanged firstNotification = (InstructionStatusChanged) publishedNotifications.get(idx);
+	public void assertInstructionStatusChangedNotification(final int idx, final InstructionId id, final InstructionStatus status) {
+		assertTrue(InstructionStatusChanged.class.isAssignableFrom(this.publishedNotifications.get(idx).getClass()));
+		final InstructionStatusChanged firstNotification = (InstructionStatusChanged) this.publishedNotifications.get(idx);
 		assertInstructionStatusChangedNotification(id, status, firstNotification);
 	}
 
-	private void assertInstructionStatusChangedNotification(InstructionId id, InstructionStatus status,
-			InstructionStatusChanged firstNotification) {
+	private void assertInstructionStatusChangedNotification(final InstructionId id, final InstructionStatus status,
+			final InstructionStatusChanged firstNotification) {
 		Assert.assertEquals(id, firstNotification.getId());
 		Assert.assertEquals(status, firstNotification.getStatus());
 	}
