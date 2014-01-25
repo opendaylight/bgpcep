d393e9f5dc333830dd927eed9ac41458d09246b5
Robert Varga <rovarga@cisco.com>
2014-01-25 17:05:47 +0100

    Rework BGP configuration
    This patch reworks the structure of BGP configuration so that it follows
    the logical organization of the RIB.
    
    The core change is the creation of BGPPeer as a full-blown configuration
    item. This encapsulates the previous proposal configuration and has the
    inverse relationship with the RIB: the RIB is its dependency and it
    pulls things like the local AS and BGP identifier from it.
    
    Futher improvement is the introduction of bgp-table-type, which allows
    explicit listing of tables which should be advertized in the BGP Open
    message as well as specification of which tables should be tracked in
    the local RIB.
    
    Change-Id: I905759a3d9e780394579238d48ff0aee009a5544
    Signed-off-by: Robert Varga <rovarga@cisco.com>
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/Ipv6AdjRIBsIn.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/Ipv6AdjRIBsIn.java
@@ -7,14 +7,11 @@
  */
 package org.opendaylight.protocol.bgp.rib.impl;
 
-import java.util.Comparator;
-
 import org.opendaylight.controller.sal.binding.api.data.DataModificationTransaction;
 import org.opendaylight.protocol.bgp.rib.RibReference;
 import org.opendaylight.protocol.bgp.rib.spi.AbstractAdjRIBsIn;
 import org.opendaylight.protocol.bgp.rib.spi.Peer;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Prefix;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.PathAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.destination.destination.type.destination.ipv6._case.DestinationIpv6;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.update.path.attributes.MpReachNlri;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.update.path.attributes.MpUnreachNlri;
@@ -28,8 +25,8 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 final class Ipv6AdjRIBsIn extends AbstractAdjRIBsIn<Ipv6Prefix, Ipv6Route> {
-	Ipv6AdjRIBsIn(final DataModificationTransaction trans, final RibReference rib, final Comparator<PathAttributes> comparator, final TablesKey key) {
-		super(trans, rib, comparator, key);
+	Ipv6AdjRIBsIn(final DataModificationTransaction trans, final RibReference rib, final TablesKey key) {
+		super(trans, rib, key);
 	}
 
 	@Override
