0aebad4f7640f3e1a50a1815f2d7a24a3c921edf
Robert Varga <rovarga@cisco.com>
2014-01-25 17:05:47 +0100

    Rework BGP configuration
    This patch reworks the structure of BGP configuration so that it follows
    the logical organization of the RIB.
    
    The core change is the creation of BGPPeer as a full-blown configuration
    item. This encapsulates the previous proposal configuration and has the
    inverse relationship with the RIB: the RIB is its dependency and it
    pulls things like the local AS and BGP identifier from it.
    
    Futher improvement is the introduction of bgp-table-type, which allows
    explicit listing of tables which should be advertized in the BGP Open
    message as well as specification of which tables should be tracked in
    the local RIB.
    
    Change-Id: I905759a3d9e780394579238d48ff0aee009a5544
    Signed-off-by: Robert Varga <rovarga@cisco.com>
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

--- bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/RIBActivator.java
+++ bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/RIBActivator.java
@@ -7,7 +7,6 @@
  */
 package org.opendaylight.protocol.bgp.linkstate;
 
-import java.util.Comparator;
 import java.util.List;
 
 import org.opendaylight.controller.sal.binding.api.data.DataModificationTransaction;
@@ -18,7 +17,6 @@ import org.opendaylight.protocol.bgp.rib.spi.AdjRIBsInFactory;
 import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderContext;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.LinkstateAddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.LinkstateSubsequentAddressFamily;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.PathAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.TablesKey;
 
 import com.google.common.collect.Lists;
@@ -31,11 +29,10 @@ public final class RIBActivator extends AbstractRIBExtensionProviderActivator {
 	protected List<AutoCloseable> startRIBExtensionProviderImpl(final RIBExtensionProviderContext context) {
 		return Lists.newArrayList(context.registerAdjRIBsInFactory(LinkstateAddressFamily.class, LinkstateSubsequentAddressFamily.class,
 				new AdjRIBsInFactory() {
-					@Override
-					public AdjRIBsIn createAdjRIBsIn(final DataModificationTransaction trans, final RibReference rib,
-							final Comparator<PathAttributes> comparator, final TablesKey key) {
-						return new LinkstateAdjRIBsIn(trans, rib, comparator, key);
-					}
-				}));
+			@Override
+			public AdjRIBsIn createAdjRIBsIn(final DataModificationTransaction trans, final RibReference rib, final TablesKey key) {
+				return new LinkstateAdjRIBsIn(trans, rib, key);
+			}
+		}));
 	}
 }
