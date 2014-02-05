65a24c67c1978f0668ab1ab99f17f4c4d6f4e36c
Robert Varga <rovarga@cisco.com>
2014-02-05 20:54:28 +0100

    Introduce AbstractPCEPSessionProposalFactory for reuse
    Change-Id: Iaa4cf995a28b1a940c03e0f0cd0e99fe4b7a1ecf
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- pcep/ietf-stateful07/src/main/java/org/opendaylight/protocol/pcep/ietf/initiated00/Stateful07SessionProposalFactory.java
+++ pcep/ietf-stateful07/src/main/java/org/opendaylight/protocol/pcep/ietf/initiated00/Stateful07SessionProposalFactory.java
@@ -5,62 +5,39 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.protocol.pcep.impl;
+package org.opendaylight.protocol.pcep.ietf.initiated00;
 
 import java.net.InetSocketAddress;
 
-import org.opendaylight.protocol.pcep.PCEPSessionProposalFactory;
+import org.opendaylight.protocol.pcep.spi.AbstractPCEPSessionProposalFactory;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.crabbe.initiated.rev131126.Stateful1;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.crabbe.initiated.rev131126.Stateful1Builder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.Tlvs2;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.Tlvs2Builder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.stateful.capability.tlv.StatefulBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.open.object.Open;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.open.object.OpenBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.open.object.open.TlvsBuilder;
 
-public class PCEPSessionProposalFactoryImpl implements PCEPSessionProposalFactory {
-
-	private final int keepAlive, deadTimer;
+public class Stateful07SessionProposalFactory extends AbstractPCEPSessionProposalFactory {
 
 	private final boolean stateful, active, instant;
 
-	public PCEPSessionProposalFactoryImpl(final int deadTimer, final int keepAlive, final boolean stateful, final boolean active,
+	public Stateful07SessionProposalFactory(final int deadTimer, final int keepAlive, final boolean stateful, final boolean active,
 			final boolean instant) {
-		this.deadTimer = deadTimer;
-		this.keepAlive = keepAlive;
+		super(deadTimer, keepAlive);
 		this.stateful = stateful;
 		this.active = active;
 		this.instant = instant;
 	}
 
 	@Override
-	public Open getSessionProposal(final InetSocketAddress address, final int sessionId) {
-		final TlvsBuilder builder = new TlvsBuilder();
-		if (PCEPSessionProposalFactoryImpl.this.stateful) {
+	protected void addTlvs(final InetSocketAddress address, final TlvsBuilder builder) {
+		if (Stateful07SessionProposalFactory.this.stateful) {
 			builder.addAugmentation(
 					Tlvs2.class,
 					new Tlvs2Builder().setStateful(
 							new StatefulBuilder().setLspUpdateCapability(this.active).addAugmentation(Stateful1.class,
 									new Stateful1Builder().setInitiation(this.instant).build()).build()).build()).build();
 		}
-		final OpenBuilder oBuilder = new OpenBuilder();
-		oBuilder.setSessionId((short) sessionId);
-		if (PCEPSessionProposalFactoryImpl.this.keepAlive != 0) {
-			oBuilder.setKeepalive((short) PCEPSessionProposalFactoryImpl.this.keepAlive);
-		}
-		if (PCEPSessionProposalFactoryImpl.this.deadTimer != 0) {
-			oBuilder.setDeadTimer((short) PCEPSessionProposalFactoryImpl.this.deadTimer);
-		}
-		return oBuilder.setTlvs(builder.build()).build();
-	}
-
-	public int getKeepAlive() {
-		return this.keepAlive;
-	}
-
-	public int getDeadTimer() {
-		return this.deadTimer;
 	}
 
 	public boolean isStateful() {
