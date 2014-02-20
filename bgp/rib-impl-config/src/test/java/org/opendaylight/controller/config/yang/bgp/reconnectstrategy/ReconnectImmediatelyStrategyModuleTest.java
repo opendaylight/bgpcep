01c3e28fb8a58867ac17914fe2184cee4cbaa71b
Tomas Olvecky <tolvecky@cisco.com>
2014-02-20 09:44:33 +0100

    Fix tests in rib-impl-config to use 'singleton' name for global event executor.
    Change-Id: Ic9609026145edd2850dd9ac7266eb43c83a97b1a
    Signed-off-by: Tomas Olvecky <tolvecky@cisco.com>
    (cherry picked from commit 39c53df12341c77b21fc3e92558dd6167baa55c0)

--- bgp/rib-impl-config/src/test/java/org/opendaylight/controller/config/yang/bgp/reconnectstrategy/ReconnectImmediatelyStrategyModuleTest.java
+++ bgp/rib-impl-config/src/test/java/org/opendaylight/controller/config/yang/bgp/reconnectstrategy/ReconnectImmediatelyStrategyModuleTest.java
@@ -7,13 +7,6 @@
  */
 package org.opendaylight.controller.config.yang.bgp.reconnectstrategy;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import javax.management.InstanceAlreadyExistsException;
-import javax.management.InstanceNotFoundException;
-import javax.management.ObjectName;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.opendaylight.controller.config.api.ConflictingVersionException;
@@ -27,9 +20,16 @@ import org.opendaylight.controller.config.yang.reconnectstrategy.AbstractReconne
 import org.opendaylight.controller.config.yang.reconnectstrategy.ReconnectImmediatelyStrategyModuleFactory;
 import org.opendaylight.controller.config.yang.reconnectstrategy.ReconnectImmediatelyStrategyModuleMXBean;
 
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.InstanceNotFoundException;
+import javax.management.ObjectName;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class ReconnectImmediatelyStrategyModuleTest extends AbstractConfigTest {
 
-	private final String instanceName = "immediately";
+	private final String instanceName = GlobalEventExecutorModuleFactory.SINGLETON_NAME;
 
 	private ReconnectImmediatelyStrategyModuleFactory factory;
 
@@ -49,7 +49,7 @@ public class ReconnectImmediatelyStrategyModuleTest extends AbstractConfigTest {
 		try {
 			ConfigTransactionJMXClient transaction = configRegistryClient
 					.createTransaction();
-			createInstance(transaction, this.factory.getImplementationName(), instanceName, null, this.executorFactory.getImplementationName());
+			createInstance(transaction, this.factory.getImplementationName(), instanceName, null);
 			transaction.validateConfig();
 			fail();
 		} catch (ValidationException e) {
@@ -63,7 +63,7 @@ public class ReconnectImmediatelyStrategyModuleTest extends AbstractConfigTest {
 		try {
 			ConfigTransactionJMXClient transaction = configRegistryClient
 					.createTransaction();
-			createInstance(transaction, this.factory.getImplementationName(), instanceName, -1, this.executorFactory.getImplementationName());
+			createInstance(transaction, this.factory.getImplementationName(), instanceName, -1);
 			transaction.validateConfig();
 			fail();
 		} catch (ValidationException e) {
@@ -75,7 +75,7 @@ public class ReconnectImmediatelyStrategyModuleTest extends AbstractConfigTest {
 	public void testCreateBean() throws Exception {
 		ConfigTransactionJMXClient transaction = configRegistryClient
 				.createTransaction();
-		createInstance(transaction, this.factory.getImplementationName(), instanceName, 500, this.executorFactory.getImplementationName());
+		createInstance(transaction, this.factory.getImplementationName(), instanceName, 500);
 		transaction.validateConfig();
 		CommitStatus status = transaction.commit();
 		assertBeanCount(1, factory.getImplementationName());
@@ -87,7 +87,7 @@ public class ReconnectImmediatelyStrategyModuleTest extends AbstractConfigTest {
 			ConflictingVersionException, ValidationException {
 		ConfigTransactionJMXClient transaction = configRegistryClient
 				.createTransaction();
-		createInstance(transaction, this.factory.getImplementationName(), instanceName, 100, this.executorFactory.getImplementationName());
+		createInstance(transaction, this.factory.getImplementationName(), instanceName, 100);
 		transaction.commit();
 		transaction = configRegistryClient.createTransaction();
 		assertBeanCount(1, factory.getImplementationName());
@@ -102,7 +102,7 @@ public class ReconnectImmediatelyStrategyModuleTest extends AbstractConfigTest {
 			InstanceNotFoundException {
 		ConfigTransactionJMXClient transaction = configRegistryClient
 				.createTransaction();
-		createInstance(transaction, this.factory.getImplementationName(), instanceName, 500, this.executorFactory.getImplementationName());
+		createInstance(transaction, this.factory.getImplementationName(), instanceName, 500);
 		transaction.commit();
 		transaction = configRegistryClient.createTransaction();
 		assertBeanCount(1, factory.getImplementationName());
@@ -120,15 +120,14 @@ public class ReconnectImmediatelyStrategyModuleTest extends AbstractConfigTest {
 	public static ObjectName createInstance(
 			final ConfigTransactionJMXClient transaction,
 			final String moduleName,
-			final String instanceName, final Integer timeout,
-			final String executorModuleName) throws InstanceAlreadyExistsException {
+			final String instanceName, final Integer timeout) throws InstanceAlreadyExistsException {
 		ObjectName nameCreated = transaction.createModule(
 				moduleName, instanceName);
 		ReconnectImmediatelyStrategyModuleMXBean mxBean = transaction
 				.newMBeanProxy(nameCreated,
 						ReconnectImmediatelyStrategyModuleMXBean.class);
 		mxBean.setTimeout(timeout);
-		mxBean.setExecutor(GlobalEventExecutorUtil.createInstance(transaction, executorModuleName, "global-event-executor"));
+		mxBean.setExecutor(GlobalEventExecutorUtil.createOrGetInstance(transaction));
 		return nameCreated;
 	}
 
