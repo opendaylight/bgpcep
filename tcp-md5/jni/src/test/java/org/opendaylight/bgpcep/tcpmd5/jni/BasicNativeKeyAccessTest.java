7cb317ffac7069e4b62cc0c71a6381123f68b0d6
Robert Varga <rovarga@cisco.com>
2014-04-24 21:43:35 +0200

    BUG-634: add connecticity test suite
    This patch adds two tests, which test both the positive (matching key)
    and negative (mismatched key) scenarios.
    
    Change-Id: Ibee31baa126a54d9cff4d905febad46a812bea21
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- tcp-md5/jni/src/test/java/org/opendaylight/bgpcep/tcpmd5/jni/BasicNativeKeyAccessTest.java
+++ tcp-md5/jni/src/test/java/org/opendaylight/bgpcep/tcpmd5/jni/BasicNativeKeyAccessTest.java
@@ -26,8 +26,8 @@ import org.opendaylight.bgpcep.tcpmd5.KeyMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NativeKeyAccessTest {
-	private static final Logger LOG = LoggerFactory.getLogger(NativeKeyAccessTest.class);
+public class BasicNativeKeyAccessTest {
+	private static final Logger LOG = LoggerFactory.getLogger(BasicNativeKeyAccessTest.class);
 	private static final byte[] KEY1 = new byte[] { 1 };
 	private static final byte[] KEY2 = new byte[] { 2, 3 };
 
@@ -58,13 +58,13 @@ public class NativeKeyAccessTest {
 	}
 
 	@Before
-	public void initialize() throws IOException {
+	public void setup() throws IOException {
 		factory = NativeKeyAccessFactory.getInstance();
 		channel = SocketChannel.open();
 	}
 
 	@After
-	public void shutdown() throws IOException {
+	public void teardown() throws IOException {
 		channel.close();
 	}
 
