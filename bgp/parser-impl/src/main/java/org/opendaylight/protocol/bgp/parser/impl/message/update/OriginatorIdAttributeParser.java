3ae48c7d42a24e88fce444bdbea2162e226c08a7
Martin Bobak <mbobak@cisco.com>
2014-05-16 13:20:40 +0200

    - added originatorId and clusterId serialization to BGPUPdateMessageParser - serialization of Nlri attributes added - attribute_flags and attribute_type added to all attribute serializers - attribute descriptors <attribute type, attribute length, attribute   value> unified by AttributeDescriptor interface - added tests for Update message serialization
    Change-Id: I722dbe6d42f7173bbf62dff5b3b32e5b17321211
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/OriginatorIdAttributeParser.java
+++ bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/OriginatorIdAttributeParser.java
@@ -9,25 +9,36 @@ package org.opendaylight.protocol.bgp.parser.impl.message.update;
 
 import com.google.common.base.Preconditions;
 import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bgp.parser.spi.AttributeDescriptor;
 import org.opendaylight.protocol.bgp.parser.spi.AttributeParser;
-import org.opendaylight.protocol.bgp.parser.spi.AttributeSerializer;
 import org.opendaylight.protocol.concepts.Ipv4Util;
 import org.opendaylight.protocol.util.ByteArray;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.update.PathAttributesBuilder;
-import org.opendaylight.yangtools.yang.binding.DataObject;
 
-public final class OriginatorIdAttributeParser implements AttributeParser, AttributeSerializer {
-	public static final int TYPE = 9;
+public final class OriginatorIdAttributeParser implements AttributeParser,AttributeDescriptor {
 
-	private static final int ORIGINATOR_LENGTH = 4;
+    public static final int TYPE = 9;
+    public static final int ATTR_FLAGS = 128;
+	public static final int ATTR_LENGTH = 4;
 
 	@Override
 	public void parseAttribute(final ByteBuf buffer, final PathAttributesBuilder builder) {
-		Preconditions.checkArgument(buffer.readableBytes() == ORIGINATOR_LENGTH, "Length of byte array for ORIGINATOR_ID should be %s, but is %s", ORIGINATOR_LENGTH, buffer.readableBytes());
-		builder.setOriginatorId(Ipv4Util.addressForBytes(ByteArray.readBytes(buffer, ORIGINATOR_LENGTH)));
+		Preconditions.checkArgument(buffer.readableBytes() == ATTR_LENGTH, "Length of byte array for ORIGINATOR_ID should be %s, but is %s", ATTR_LENGTH, buffer.readableBytes());
+		builder.setOriginatorId(Ipv4Util.addressForBytes(ByteArray.readBytes(buffer, ATTR_LENGTH)));
 	}
 
     @Override
-    public void serializeAttribute(DataObject attribute, ByteBuf byteAggregator) {
+    public int getType() {
+        return TYPE;
+    }
+
+    @Override
+    public int getFlags() {
+        return ATTR_FLAGS;
+    }
+
+    @Override
+    public int getLength() {
+        return ATTR_LENGTH;
     }
 }
