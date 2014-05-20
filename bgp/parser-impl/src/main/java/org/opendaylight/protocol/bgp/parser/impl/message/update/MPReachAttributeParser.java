33f09227dd77ae3b20e7d9e9068b0afb16b409ee
Martin Bobak <mbobak@cisco.com>
2014-05-20 16:57:08 +0200

    Bug 611 - Update message serialization - multi protocol attributes serialization for update message
    Change-Id: I710b87a67c9f19bd9ed9391dab32ed1522188a05
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/MPReachAttributeParser.java
+++ bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/MPReachAttributeParser.java
@@ -8,21 +8,26 @@
 package org.opendaylight.protocol.bgp.parser.impl.message.update;
 
 import com.google.common.base.Preconditions;
+import com.google.common.primitives.UnsignedBytes;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
 import org.opendaylight.protocol.bgp.parser.BGPError;
 import org.opendaylight.protocol.bgp.parser.BGPParsingException;
 import org.opendaylight.protocol.bgp.parser.spi.AttributeParser;
 import org.opendaylight.protocol.bgp.parser.spi.AttributeSerializer;
 import org.opendaylight.protocol.bgp.parser.spi.NlriRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.update.PathAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.update.PathAttributesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.PathAttributes1;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.PathAttributes1Builder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.update.path.attributes.MpReachNlri;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 
 public final class MPReachAttributeParser implements AttributeParser,AttributeSerializer {
 	public static final int TYPE = 14;
-
+    public static final int ATTR_FLAGS = 128;
+    public static final int ATTR_LENGTH = 2;
 	private final NlriRegistry reg;
 
 	public MPReachAttributeParser(final NlriRegistry reg) {
@@ -41,6 +46,20 @@ public final class MPReachAttributeParser implements AttributeParser,AttributeSe
 
     @Override
     public void serializeAttribute(DataObject attribute, ByteBuf byteAggregator) {
-        //TODO implement this
+        PathAttributes pathAttributes = (PathAttributes) attribute;
+        if (pathAttributes.getAugmentation(PathAttributes1.class) == null) {
+            return;
+        }
+        PathAttributes1 pathAttributes1 = pathAttributes.getAugmentation(PathAttributes1.class);
+        MpReachNlri mpReachNlri = pathAttributes1.getMpReachNlri();
+
+        ByteBuf reachBuffer = Unpooled.buffer();
+        this.reg.serializeMpReach(mpReachNlri, reachBuffer);
+
+        byteAggregator.writeByte(UnsignedBytes.checkedCast(ATTR_FLAGS));
+        byteAggregator.writeByte(UnsignedBytes.checkedCast(TYPE));
+        byteAggregator.writeByte(UnsignedBytes.checkedCast(reachBuffer.writerIndex()));
+        byteAggregator.writeBytes(reachBuffer);
+
     }
 }
