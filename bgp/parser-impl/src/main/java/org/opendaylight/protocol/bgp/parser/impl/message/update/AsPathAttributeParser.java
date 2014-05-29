bb2af51772d6aba4e61dd2318418cc3ac23e67b4
Martin Bobak <mbobak@cisco.com>
2014-05-29 15:40:23 +0200

    Bug 611 - Update message serialization BGP Update message serializer - serializers use ByteBuf instead of byte[] - added update message path attributes serialization - AttributeSerializer expects ByteBuf as method param - MessageSerializer uses ByteBuf as method param
    Change-Id: Ic7bb94e1ab20980a90967ef0a8b00ea17d04db69
    Signed-off-by: Martin Bobak <mbobak@cisco.com>

--- bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/AsPathAttributeParser.java
+++ bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/AsPathAttributeParser.java
@@ -10,32 +10,38 @@ package org.opendaylight.protocol.bgp.parser.impl.message.update;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.primitives.UnsignedBytes;
-
 import io.netty.buffer.ByteBuf;
-
 import java.util.List;
-
 import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
 import org.opendaylight.protocol.bgp.parser.BGPError;
 import org.opendaylight.protocol.bgp.parser.BGPParsingException;
 import org.opendaylight.protocol.bgp.parser.impl.message.update.AsPathSegmentParser.SegmentType;
 import org.opendaylight.protocol.bgp.parser.spi.AttributeParser;
+import org.opendaylight.protocol.bgp.parser.spi.AttributeSerializer;
 import org.opendaylight.protocol.util.ReferenceCache;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.AsNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.PathAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.AsPath;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.AsPathBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.as.path.Segments;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.as.path.SegmentsBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.update.PathAttributesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.as.path.segment.c.segment.AListCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.as.path.segment.c.segment.AListCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.as.path.segment.c.segment.ASetCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.as.path.segment.c.segment.ASetCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.as.path.segment.c.segment.a.list._case.AListBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.as.path.segment.c.segment.a.list._case.a.list.AsSequence;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.as.path.segment.c.segment.a.set._case.ASetBuilder;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public final class AsPathAttributeParser implements AttributeParser {
+
+public final class AsPathAttributeParser implements AttributeParser, AttributeSerializer {
     public static final int TYPE = 2;
     private final ReferenceCache refCache;
+    private static final Logger LOG = LoggerFactory.getLogger(AsPathAttributeParser.class);
 
     public AsPathAttributeParser(final ReferenceCache refCache) {
         this.refCache = Preconditions.checkNotNull(refCache);
@@ -44,13 +50,12 @@ public final class AsPathAttributeParser implements AttributeParser {
     /**
      * Parses AS_PATH from bytes.
      *
-     * @param bytes byte array to be parsed
+     * @param buffer bytes to be parsed
      * @return new ASPath object
      * @throws BGPDocumentedException if there is no AS_SEQUENCE present (mandatory)
      * @throws BGPParsingException
      */
-    private static AsPath parseAsPath(final ReferenceCache refCache, final ByteBuf buffer) throws BGPDocumentedException,
-            BGPParsingException {
+    private static AsPath parseAsPath(final ReferenceCache refCache, final ByteBuf buffer) throws BGPDocumentedException, BGPParsingException {
         final List<Segments> ases = Lists.newArrayList();
         boolean isSequence = false;
         while (buffer.isReadable()) {
@@ -83,8 +88,27 @@ public final class AsPathAttributeParser implements AttributeParser {
     }
 
     @Override
-    public void parseAttribute(final ByteBuf buffer, final PathAttributesBuilder builder) throws BGPDocumentedException,
-            BGPParsingException {
+    public void parseAttribute(final ByteBuf buffer, final PathAttributesBuilder builder) throws BGPDocumentedException, BGPParsingException {
         builder.setAsPath(parseAsPath(this.refCache, buffer));
+    }
+
+    @Override
+    public void serializeAttribute(DataObject attribute, ByteBuf byteAggregator) {
+        PathAttributes pathAttributes = (PathAttributes) attribute;
+        if (pathAttributes.getAsPath() == null) {
+            return;
+        }
+        AsPath asPath = pathAttributes.getAsPath();
+        for (Segments segments : asPath.getSegments()) {
+            if (segments.getCSegment() instanceof AListCase) {
+                AListCase listCase = (AListCase) segments.getCSegment();
+                AsPathSegmentParser.serializeAsSequence(listCase, byteAggregator);
+            } else if (segments.getCSegment() instanceof ASetCase) {
+                ASetCase set = (ASetCase) segments.getCSegment();
+                AsPathSegmentParser.serializeAsSet(set, byteAggregator);
+            } else {
+                LOG.warn("CSegment class is neither AListCase nor ASetCase.");
+            }
+        }
     }
 }
