4f770b0b56b7266bdcf87f4f1fde6dfc2bc478b6
Robert Varga <rovarga@cisco.com>
2014-05-29 13:23:01 +0200

    Revert "BUG-634: Fix .so not being included in the .jar"
    This reverts commit 902dcb5542f52663ccab12ca591ea2993946266b.
    
    Change-Id: I549b07858ca4d3b5aae6c4bde1d9bd22d6240dc1
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- tcp-md5/jni/src/main/java/org/opendaylight/bgpcep/tcpmd5/jni/NativeKeyAccess.java
+++ tcp-md5/jni/src/main/java/org/opendaylight/bgpcep/tcpmd5/jni/NativeKeyAccess.java
@@ -8,14 +8,7 @@
 package org.opendaylight.bgpcep.tcpmd5.jni;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.channels.Channel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,56 +20,16 @@ import com.google.common.base.Preconditions;
  * directly to the underlying operating system.
  */
 public final class NativeKeyAccess implements KeyAccess {
-	private static final String LIBNAME = "libtcpmd5-jni.so";
 	private static final Logger LOG = LoggerFactory.getLogger(NativeKeyAccess.class);
-	private static boolean AVAILABLE = false;
-
-	private static InputStream getLibraryStream() {
-		return Preconditions.checkNotNull(NativeKeyAccess.class.getResourceAsStream('/' + LIBNAME),
-				String.format("Failed to open library resource %s", LIBNAME));
-	}
 
 	static {
-		final Set<PosixFilePermission> perms = PosixFilePermissions.fromString("rwxr-x---");
+		NarSystem.loadLibrary();
 
-		try (final InputStream is = getLibraryStream()) {
-			try {
-				final Path p = Files.createTempFile(LIBNAME, null, PosixFilePermissions.asFileAttribute(perms));
-
-				LOG.info("Copying {} to {}", is, p);
-
-				try {
-					Files.copy(is, p, StandardCopyOption.REPLACE_EXISTING);
-
-					try {
-						Runtime.getRuntime().load(p.toString());
-
-						LOG.info("Library {} loaded", p);
-
-						int rt = NarSystem.runUnitTests();
-						if (rt == 0) {
-							LOG.warn("Run-time initialization failed");
-						} else {
-							LOG.debug("Run-time found {} supported channel classes", rt);
-							AVAILABLE = true;
-						}
-					} catch (RuntimeException e) {
-						LOG.error("Failed to load native library", e);
-					}
-				} catch (IOException e) {
-					LOG.error("Failed to extract native library", e);
-				} finally {
-					try {
-						Files.deleteIfExists(p);
-					} catch (IOException e) {
-						LOG.warn("Failed to remove temporary file", e);
-					}
-				}
-			} catch (IOException e2) {
-				LOG.error("Failed to create temporary file {}", LIBNAME, e2);
-			}
-		} catch (IOException e1) {
-			LOG.error("Failed to find native library {}", LIBNAME, e1);
+		int rt = NarSystem.runUnitTests();
+		if (rt == 0) {
+			LOG.warn("Run-time initialization failed");
+		} else {
+			LOG.debug("Run-time found {} supported channel classes", rt);
 		}
 	}
 
@@ -91,31 +44,15 @@ public final class NativeKeyAccess implements KeyAccess {
 	}
 
 	public static KeyAccess create(final Channel channel) {
-		if (!AVAILABLE) {
-			LOG.debug("Native library not available");
+		if (isClassSupported0(channel.getClass())) {
+			return new NativeKeyAccess(channel);
+		} else {
 			return null;
 		}
-
-		if (!isClassSupported0(channel.getClass())) {
-			LOG.debug("No support available for class {}", channel.getClass());
-			return null;
-		}
-
-		return new NativeKeyAccess(channel);
 	}
 
 	public static boolean isAvailableForClass(final Class<?> clazz) {
-		if (!AVAILABLE) {
-			LOG.debug("Native library not available");
-			return false;
-		}
-
-		if (!isClassSupported0(clazz)) {
-			LOG.debug("No support available for class {}", clazz);
-			return false;
-		}
-
-		return true;
+		return isClassSupported0(clazz);
 	}
 
 	@Override
