605a71a1d2bb7fbb697e38e8d7d786462d8cf658
Milos Fabian <milfabia@cisco.com>
2014-06-24 10:02:27 +0200

    Bug-1180: Segment routing message parsers/serilizers
    -added Segment Routing messages parsers/serializers
    	-extends stateful07 message parsers and PcRepMessage parser
    	-junit tests
    -added parsers/serializers activator
    
    Change-Id: I531b36cd0fc48a30fbfbe8b596a1faa0f1945fb0
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing02/SrEroUtil.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.pcep.segment.routing02;
+
+import org.opendaylight.protocol.pcep.spi.PCEPErrors;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.lsp.setup.type._01.rev140507.PathSetupTypeTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing._02.rev140506.SrEroSubobject;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.explicit.route.object.Ero;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.explicit.route.object.ero.Subobject;
+
+public class SrEroUtil {
+
+    private static final int MPLS_LABEL_MIN_VALUE = 16;
+
+    protected static PCEPErrors validateSrEroSubobjects(final Ero ero) {
+        if(ero.getSubobject() == null || ero.getSubobject().isEmpty()) {
+            return null;
+        }
+        for(final Subobject subobject : ero.getSubobject()) {
+            if(!(subobject.getSubobjectType() instanceof SrEroSubobject)) {
+                return PCEPErrors.NON_IDENTICAL_ERO_SUBOBJECTS;
+            }
+            final SrEroSubobject srEroSubobject  = (SrEroSubobject) subobject.getSubobjectType();
+            if(srEroSubobject.getFlags().isM() && srEroSubobject.getSid() < MPLS_LABEL_MIN_VALUE) {
+                return PCEPErrors.BAD_LABEL_VALUE;
+            }
+        }
+        return null;
+    }
+
+    protected static boolean isPst(final PathSetupTypeTlv tlv) {
+        if(tlv != null && tlv.getPathSetupType() != null) {
+            if(tlv.getPathSetupType().isPst()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
