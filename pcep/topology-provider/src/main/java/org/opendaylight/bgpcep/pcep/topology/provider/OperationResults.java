8011e6bf093f97c2c70c1085388cb2139f32a301
Robert Varga <rovarga@cisco.com>
2014-06-26 14:26:07 +0200

    BUG-670: enable reporting of PCEP errors
    Adds a place where errors reported by a PCC can be stored.
    
    Change-Id: Id235effcf6069b66b9f3f3cae19d0cb495ee332f
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/OperationResults.java
+++ pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/OperationResults.java
@@ -7,43 +7,64 @@
  */
 package org.opendaylight.bgpcep.pcep.topology.provider;
 
+import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.FailureType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.OperationResult;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev131024.operation.result.Errors;
 import org.opendaylight.yangtools.yang.binding.DataContainer;
 
 /**
  *
  */
-enum OperationResults implements OperationResult {
-    NOACK {
-        @Override
-        public FailureType getFailure() {
-            return FailureType.NoAck;
-        }
+final class OperationResults implements OperationResult {
+    static final OperationResults NOACK = new OperationResults(FailureType.NoAck);
+    static final OperationResults SUCCESS = new OperationResults(null);
+    static final OperationResults UNSENT = new OperationResults(FailureType.Unsent);
 
-    },
-    SUCCESS {
-        @Override
-        public FailureType getFailure() {
-            return null;
-        }
-    },
-    UNSENT {
-        @Override
-        public FailureType getFailure() {
-            return FailureType.Unsent;
-        }
-    };
+    private final List<Errors> errors;
+    private final FailureType failure;
 
-    @Override
-    public Class<? extends DataContainer> getImplementedInterface() {
-        return OperationResult.class;
+    private OperationResults(final FailureType type) {
+        this(type, Collections.<Errors>emptyList());
+    }
+
+    private OperationResults(final FailureType failure, final List<Errors> errors) {
+        this.failure = failure;
+        this.errors = Preconditions.checkNotNull(errors);
     }
 
     ListenableFuture<OperationResult> future() {
         return Futures.<OperationResult> immediateFuture(this);
+    }
+
+    public static OperationResults create(final FailureType failure, final List<Errors> errors) {
+        Preconditions.checkNotNull(failure);
+        return new OperationResults(failure, errors);
+    }
+
+    public static OperationResults create(final FailureType failure, final Errors... errors) {
+        return create(failure, Arrays.asList(errors));
+    }
+
+    @Override
+    public FailureType getFailure() {
+        return failure;
+    }
+
+    @Override
+    public List<Errors> getErrors() {
+        return errors;
+
+    }
+    @Override
+    public Class<? extends DataContainer> getImplementedInterface() {
+        return OperationResult.class;
     }
 }
