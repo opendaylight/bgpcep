4546a4148aa5410cdad8799eefd52d0529f4e5ba
Maros Marsalek <mmarsale@cisco.com>
2014-07-04 15:05:06 +0200

    BUG-338 Allow incomming BGP connections.
    Added BGPPeerRegistry that holds all configured BGP peers.
    Client/Server SessionNegotiators retrieve BGPPeers(SessionListeners) from the BGPPeerRegistry.
    Added StrictBGPPeerRegistry (default implementation of BGPPeerRegistry) that allows only 1 session with BGP peer. If second session is being established, the registry drops either already established or new BGP session (depending on higher source BGP ID).
    
    Added 2 config attributes to BGPPeer:
    - initiate connection: true to start BGP connection from ODL. False if just configure BGP peer to accept session from remote device.
    - peer registry: dependency on BGPPeerRegistry. Peer registers itself into this registry.
    
    * new attributes are optional to preserve backwards compatibility.
    
    Change-Id: I2f7a606db5196fed49094d49efbb570c4d0fbecb
    Signed-off-by: Maros Marsalek <mmarsale@cisco.com>

---
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/spi/BGPServerDispatcher.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl.spi;
+
+import io.netty.channel.ChannelFuture;
+import java.net.InetSocketAddress;
+import org.opendaylight.bgpcep.tcpmd5.KeyMapping;
+
+/**
+ * Dispatcher class for creating BGP server.
+ */
+public interface BGPServerDispatcher {
+
+    ChannelFuture createServer(BGPPeerRegistry peerRegistry, InetSocketAddress address, BGPSessionValidator sessionValidator);
+
+    /**
+     * Create new BGP server bound to provided socket address.
+     */
+    ChannelFuture createServer(BGPPeerRegistry peerRegistry, InetSocketAddress address, BGPSessionValidator sessionValidator, KeyMapping keys);
+}
