5e5290e1d4da278961efd8c388880a414a5f1449
Dana Kutenicsova <dkutenic@cisco.com>
2014-07-06 17:43:56 +0200

    BUG-730 : cleaned up bgp-parser-impl
    Change-Id: I478cc65158c58a7e92adce3de17dc265de779034
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/open/MultiProtocolCapabilityHandler.java
+++ bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/open/MultiProtocolCapabilityHandler.java
@@ -8,11 +8,10 @@
 package org.opendaylight.protocol.bgp.parser.impl.message.open;
 
 import com.google.common.base.Preconditions;
-import com.google.common.primitives.UnsignedBytes;
 
 import io.netty.buffer.ByteBuf;
-
 import io.netty.buffer.Unpooled;
+
 import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
 import org.opendaylight.protocol.bgp.parser.BGPParsingException;
 import org.opendaylight.protocol.bgp.parser.spi.AddressFamilyRegistry;
@@ -33,6 +32,8 @@ public final class MultiProtocolCapabilityHandler implements CapabilityParser, C
     private final AddressFamilyRegistry afiReg;
     private final SubsequentAddressFamilyRegistry safiReg;
 
+    private static final int RESERVED = 1;
+
     public MultiProtocolCapabilityHandler(final AddressFamilyRegistry afiReg, final SubsequentAddressFamilyRegistry safiReg) {
         this.afiReg = Preconditions.checkNotNull(afiReg);
         this.safiReg = Preconditions.checkNotNull(safiReg);
@@ -46,29 +47,31 @@ public final class MultiProtocolCapabilityHandler implements CapabilityParser, C
             throw new BGPParsingException("Address Family Identifier: '" + afiVal + "' not supported.");
         }
         // skip reserved
-        buffer.skipBytes(1);
-        final int safiVal = UnsignedBytes.toInt(buffer.readByte());
+        buffer.skipBytes(RESERVED);
+        final int safiVal = buffer.readUnsignedByte();
         final Class<? extends SubsequentAddressFamily> safi = this.safiReg.classForFamily(safiVal);
         if (safi == null) {
             throw new BGPParsingException("Subsequent Address Family Identifier: '" + safiVal + "' not supported.");
         }
         return new MultiprotocolCaseBuilder().setMultiprotocolCapability(
-                new MultiprotocolCapabilityBuilder().setAfi(afi).setSafi(safi).build()).build();
+            new MultiprotocolCapabilityBuilder().setAfi(afi).setSafi(safi).build()).build();
     }
 
     @Override
     public void serializeCapability(final CParameters capability, ByteBuf byteAggregator) {
         final MultiprotocolCase mp = (MultiprotocolCase) capability;
-
+        final ByteBuf capBuffer = Unpooled.buffer();
         final Class<? extends AddressFamily> afi = mp.getMultiprotocolCapability().getAfi();
         final Integer afival = this.afiReg.numberForClass(afi);
         Preconditions.checkArgument(afival != null, "Unhandled address family " + afi);
+        capBuffer.writeShort(afival);
 
         final Class<? extends SubsequentAddressFamily> safi = mp.getMultiprotocolCapability().getSafi();
         final Integer safival = this.safiReg.numberForClass(safi);
         Preconditions.checkArgument(safival != null, "Unhandled subsequent address family " + safi);
+        capBuffer.writeZero(RESERVED);
+        capBuffer.writeByte(safival);
 
-        CapabilityUtil.formatCapability(CODE, Unpooled.wrappedBuffer(new byte[] { UnsignedBytes.checkedCast(afival / 256),
-            UnsignedBytes.checkedCast(afival % 256), 0, UnsignedBytes.checkedCast(safival) }), byteAggregator);
+        CapabilityUtil.formatCapability(CODE, capBuffer, byteAggregator);
     }
 }
