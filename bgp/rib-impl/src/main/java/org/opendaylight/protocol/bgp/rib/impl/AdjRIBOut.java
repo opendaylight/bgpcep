f4ef271c1d968f5188d45e1770b1c383d724b4f6
Robert Varga <rovarga@cisco.com>
2014-07-17 19:07:03 +0200

    BUG-1116: WIP: introduce AdjRIBOut as a class
    This is a thread-safe class through which we can funnel updates destined
    towards a peer. Each peer needs to have one such instance, which is
    registered with a LocRib listener, which pushes updates into it. These
    updates then need to be consumed by the netty threadpool.
    
    Change-Id: I79ace174c78f0653b057b0bad1c685ecd5e9d0cb
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRIBOut.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl;
+
+import com.google.common.base.Preconditions;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.Update;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+class AdjRIBOut extends AbstractAdjRIBOut<Object, DataObject> {
+    private final BGPSessionImpl session;
+
+    AdjRIBOut(final BGPSessionImpl session) {
+        this.session = Preconditions.checkNotNull(session);
+    }
+
+    @Override
+    protected void wantWrite() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected boolean writePDU(final Object key, final DataObject value) {
+        final Update update = null; // value;
+
+        session.sendMessage(update);
+        return session.isWritable();
+    }
+}
