c11fefe791bc53409bf1e80fda8c54a0689b8f50
Dana Kutenicsova <dkutenic@cisco.com>
2014-07-27 20:08:53 +0200

    Fixed various findbugs issues in util.
    Change-Id: I5e31d46ed509596284764cb7a1eba63399789dff
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- util/src/main/java/org/opendaylight/protocol/util/PCEPHexDumpParser.java
+++ util/src/main/java/org/opendaylight/protocol/util/PCEPHexDumpParser.java
@@ -60,13 +60,13 @@ public final class PCEPHexDumpParser {
             // next chars are final length, ending with '.'
             final int lengthIdx = idx + LENGTH.length();
             final int messageIdx = content.indexOf('.', lengthIdx);
-            final int length = Integer.valueOf(content.substring(lengthIdx, messageIdx));
+            final int length = Integer.parseInt(content.substring(lengthIdx, messageIdx));
             final int messageEndIdx = messageIdx + (length * 2) + 1;    // dot
 
             // Assert that message is longer than minimum 4(header.length == 4)
             // If length in PCEP message would be 0, loop would never end
             Preconditions.checkArgument(length >= MINIMAL_LENGTH, "Invalid message at index " + idx + ", length atribute is lower than "
-                    + MINIMAL_LENGTH);
+                + MINIMAL_LENGTH);
 
             final String hexMessage = content.substring(messageIdx + 1, messageEndIdx); // dot
             byte[] message = null;
