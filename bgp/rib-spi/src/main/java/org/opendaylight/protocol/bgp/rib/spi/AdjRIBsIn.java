b6b7f6b1fd50644907819532fd880b562270280e
Dana Kutenicsova <dkutenic@cisco.com>
2014-08-31 16:26:17 +0200

    BUG-1116 : application peer API
    Change-Id: I518938143019acb485ca897a8cecbc52a3d5f568
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/AdjRIBsIn.java
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/AdjRIBsIn.java
@@ -10,17 +10,53 @@ package org.opendaylight.protocol.bgp.rib.spi;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.Update;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.update.PathAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.update.path.attributes.MpReachNlri;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.update.path.attributes.MpReachNlriBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.update.path.attributes.MpUnreachNlri;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.Route;
 
 public interface AdjRIBsIn<K, V extends Route> {
-    void addRoutes(AdjRIBsTransaction trans, Peer peer, MpReachNlri nlri, PathAttributes attributes);
 
-    void removeRoutes(AdjRIBsTransaction trans, Peer peer, MpUnreachNlri nlri);
+    /**
+     * Adds routes to this adjacency rib.
+     * @param trans data-store transaction
+     * @param peer advertising peer
+     * @param nlri routes
+     * @param attributes route attributes
+     */
+    void addRoutes(final AdjRIBsTransaction trans, final Peer peer, final MpReachNlri nlri, final PathAttributes attributes);
 
-    void clear(AdjRIBsTransaction trans, Peer peer);
+    /**
+     * Removes routes from this adjacency rib.
+     * @param trans data-store transaction
+     * @param peer advertising peer
+     * @param nlri routes
+     */
+    void removeRoutes(final AdjRIBsTransaction trans, final Peer peer, final MpUnreachNlri nlri);
 
-    void markUptodate(AdjRIBsTransaction trans, Peer peer);
+    /**
+     * Clears adjacency rib tables.
+     * @param trans data-store transaction
+     * @param peer advertising peer
+     */
+    void clear(final AdjRIBsTransaction trans, final Peer peer);
 
+    /**
+     * Marks true or false the state of this adjacency rib.
+     * @param trans data-store transaction
+     * @param peer advertising peer
+     */
+    void markUptodate(final AdjRIBsTransaction trans, final Peer peer);
+
+    /**
+     * Transform an advertised data object into the corresponding NLRI in MP_REACH attribute.
+     * @param builder MP_REACH attribute builder
+     * @param data Data object
+     */
+    void addAdvertisement(final MpReachNlriBuilder builder, final V data);
+
+    /**
+     * Creates end-of-rib message for this adjacency rib.
+     * @return BGP Update message
+     */
     Update endOfRib();
 }
