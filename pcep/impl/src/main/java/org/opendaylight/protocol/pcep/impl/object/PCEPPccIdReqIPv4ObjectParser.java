a1b3aec57da3c39acc4fe4683275eec4c3473948
Milos Fabian <milfabia@cisco.com>
2014-10-27 10:37:16 +0100

    Bug-2226: RFC5886 - New objects parsers/seriazers
    -Implementation of parsers and serializers for
    new objects defined in RFC5886
    
    -ref.: https://tools.ietf.org/html/rfc5886
    
    Change-Id: I8c9c6038058f3dbdf19253c6284cd29619237359
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/object/PCEPPccIdReqIPv4ObjectParser.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.pcep.impl.object;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.pcep.spi.PCEPDeserializerException;
+import org.opendaylight.protocol.util.Ipv4Util;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.Object;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.ObjectHeader;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.pcc.id.req.object.PccIdReqBuilder;
+
+/**
+ * Parser for {@link PccId} with IPv4 address
+ * @see https://tools.ietf.org/html/rfc5886#section-4.2
+ */
+public class PCEPPccIdReqIPv4ObjectParser extends AbstractPccIdReqObjectParser {
+
+    @Override
+    public Object parseObject(final ObjectHeader header, final ByteBuf buffer) throws PCEPDeserializerException {
+        Preconditions.checkArgument(buffer != null && buffer.isReadable(), "Array of bytes is mandatory. Can't be null or empty.");
+        final PccIdReqBuilder builder = new PccIdReqBuilder();
+        builder.setIpAddress(new IpAddress(Ipv4Util.addressForByteBuf(buffer)));
+        return builder.build();
+    }
+
+}
