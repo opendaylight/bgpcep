c1b3bafa501b0bc974411b5f2dd07a4da9e22f8f
Milos Fabian <milfabia@cisco.com>
2014-10-27 10:37:16 +0100

    Bug-2226: RFC5886 - New objects parsers/seriazers
    -Implementation of parsers and serializers for
    new objects defined in RFC5886
    
    -ref.: https://tools.ietf.org/html/rfc5886
    
    Change-Id: I8c9c6038058f3dbdf19253c6284cd29619237359
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/object/AbstractPceIdObjectParser.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.pcep.impl.object;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.opendaylight.protocol.pcep.spi.ObjectParser;
+import org.opendaylight.protocol.pcep.spi.ObjectSerializer;
+import org.opendaylight.protocol.pcep.spi.ObjectUtil;
+import org.opendaylight.protocol.util.ByteBufWriteUtil;
+import org.opendaylight.protocol.util.Ipv4Util;
+import org.opendaylight.protocol.util.Ipv6Util;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.Object;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.pce.id.object.PceId;
+
+public abstract class AbstractPceIdObjectParser implements ObjectParser, ObjectSerializer {
+
+    public static final int CLASS = 25;
+
+    public static final int IPV4_TYPE = 1;
+    public static final int IPV6_TYPE = 2;
+
+    @Override
+    public void serializeObject(final Object object, final ByteBuf buffer) {
+        Preconditions.checkArgument(object instanceof PceId, "Wrong instance of PCEPObject. Passed %s. Needed PccIdReqObject.", object.getClass());
+        final PceId pceId = (PceId) object;
+        if (pceId.getIpAddress().getIpv4Address() != null) {
+            final ByteBuf body = Unpooled.buffer(Ipv4Util.IP4_LENGTH);
+            ByteBufWriteUtil.writeIpv4Address(pceId.getIpAddress().getIpv4Address(), body);
+            ObjectUtil.formatSubobject(IPV4_TYPE, CLASS, object.isProcessingRule(), object.isIgnore(), body, buffer);
+        } else if (pceId.getIpAddress().getIpv6Address() != null) {
+            final ByteBuf body = Unpooled.buffer(Ipv6Util.IPV6_LENGTH);
+            ByteBufWriteUtil.writeIpv6Address(pceId.getIpAddress().getIpv6Address(), body);
+            ObjectUtil.formatSubobject(IPV6_TYPE, CLASS, object.isProcessingRule(), object.isIgnore(), body, buffer);
+        }
+    }
+
+}
