a5a7215d712716bfc7ba03426e151cd80bc9871f
Dana Kutenicsova <dkutenic@cisco.com>
2014-11-25 16:34:44 +0100

    Refactor linkstate.
    Change-Id: I9ce95ef9d704e9e2a183121c35ba5973d2ec308e
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

---
+++ bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/nlri/LinkNlriParser.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.linkstate.nlri;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import org.opendaylight.protocol.bgp.linkstate.TlvUtil;
+import org.opendaylight.protocol.bgp.parser.BGPParsingException;
+import org.opendaylight.protocol.util.Ipv4Util;
+import org.opendaylight.protocol.util.Ipv6Util;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.Ipv4InterfaceIdentifier;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.Ipv6InterfaceIdentifier;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.TopologyIdentifier;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.linkstate.destination.c.linkstate.destination.LinkDescriptors;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.linkstate.destination.c.linkstate.destination.LinkDescriptorsBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class LinkNlriParser {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LinkNlriParser.class);
+
+    /* Link Descriptor TLVs */
+    private static final int LINK_LR_IDENTIFIERS = 258;
+    private static final int IPV4_IFACE_ADDRESS = 259;
+    private static final int IPV4_NEIGHBOR_ADDRESS = 260;
+    private static final int IPV6_IFACE_ADDRESS = 261;
+    private static final int IPV6_NEIGHBOR_ADDRESS = 262;
+
+    static LinkDescriptors parseLinkDescriptors(final ByteBuf buffer) throws BGPParsingException {
+        final LinkDescriptorsBuilder builder = new LinkDescriptorsBuilder();
+        while (buffer.isReadable()) {
+            final int type = buffer.readUnsignedShort();
+            final int length = buffer.readUnsignedShort();
+            final ByteBuf value = buffer.slice(buffer.readerIndex(), length);
+            LOG.trace("Parsing Link Descriptor: {}", ByteBufUtil.hexDump(value));
+            switch (type) {
+            case LINK_LR_IDENTIFIERS:
+                builder.setLinkLocalIdentifier(value.readUnsignedInt());
+                builder.setLinkRemoteIdentifier(value.readUnsignedInt());
+                LOG.debug("Parsed link local {} remote {} Identifiers.", builder.getLinkLocalIdentifier(),
+                    builder.getLinkRemoteIdentifier());
+                break;
+            case IPV4_IFACE_ADDRESS:
+                final Ipv4InterfaceIdentifier lipv4 = new Ipv4InterfaceIdentifier(Ipv4Util.addressForByteBuf(value));
+                builder.setIpv4InterfaceAddress(lipv4);
+                LOG.debug("Parsed IPv4 interface address {}.", lipv4);
+                break;
+            case IPV4_NEIGHBOR_ADDRESS:
+                final Ipv4InterfaceIdentifier ripv4 = new Ipv4InterfaceIdentifier(Ipv4Util.addressForByteBuf(value));
+                builder.setIpv4NeighborAddress(ripv4);
+                LOG.debug("Parsed IPv4 neighbor address {}.", ripv4);
+                break;
+            case IPV6_IFACE_ADDRESS:
+                final Ipv6InterfaceIdentifier lipv6 = new Ipv6InterfaceIdentifier(Ipv6Util.addressForByteBuf(value));
+                builder.setIpv6InterfaceAddress(lipv6);
+                LOG.debug("Parsed IPv6 interface address {}.", lipv6);
+                break;
+            case IPV6_NEIGHBOR_ADDRESS:
+                final Ipv6InterfaceIdentifier ripv6 = new Ipv6InterfaceIdentifier(Ipv6Util.addressForByteBuf(value));
+                builder.setIpv6NeighborAddress(ripv6);
+                LOG.debug("Parsed IPv6 neighbor address {}.", ripv6);
+                break;
+            case TlvUtil.MULTI_TOPOLOGY_ID:
+                final TopologyIdentifier topId = new TopologyIdentifier(value.readUnsignedShort() & TlvUtil.TOPOLOGY_ID_OFFSET);
+                builder.setMultiTopologyId(topId);
+                LOG.debug("Parsed topology identifier {}.", topId);
+                break;
+            default:
+                throw new BGPParsingException("Link Descriptor not recognized, type: " + type);
+            }
+            buffer.skipBytes(length);
+        }
+        LOG.trace("Finished parsing Link descriptors.");
+        return builder.build();
+    }
+
+    static void serializeLinkDescriptors(final LinkDescriptors descriptors, final ByteBuf buffer) {
+        if (descriptors.getLinkLocalIdentifier() != null && descriptors.getLinkRemoteIdentifier() != null) {
+            final ByteBuf identifierBuf = Unpooled.buffer();
+            identifierBuf.writeInt(descriptors.getLinkLocalIdentifier().intValue());
+            identifierBuf.writeInt(descriptors.getLinkRemoteIdentifier().intValue());
+            TlvUtil.writeTLV(LINK_LR_IDENTIFIERS, identifierBuf, buffer);
+        }
+        if (descriptors.getIpv4InterfaceAddress() != null) {
+            TlvUtil.writeTLV(IPV4_IFACE_ADDRESS, Ipv4Util.byteBufForAddress(descriptors.getIpv4InterfaceAddress()), buffer);
+        }
+        if (descriptors.getIpv4NeighborAddress() != null) {
+            TlvUtil.writeTLV(IPV4_NEIGHBOR_ADDRESS, Ipv4Util.byteBufForAddress(descriptors.getIpv4NeighborAddress()), buffer);
+        }
+        if (descriptors.getIpv6InterfaceAddress() != null) {
+            TlvUtil.writeTLV(IPV6_IFACE_ADDRESS, Ipv6Util.byteBufForAddress(descriptors.getIpv6InterfaceAddress()), buffer);
+        }
+        if (descriptors.getIpv6NeighborAddress() != null) {
+            TlvUtil.writeTLV(IPV6_NEIGHBOR_ADDRESS, Ipv6Util.byteBufForAddress(descriptors.getIpv6NeighborAddress()), buffer);
+        }
+        if (descriptors.getMultiTopologyId() != null) {
+            TlvUtil.writeTLV(TlvUtil.MULTI_TOPOLOGY_ID, Unpooled.copyShort(descriptors.getMultiTopologyId().getValue()), buffer);
+        }
+    }
+}
---
+++ bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/nlri/PrefixNlriParser.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.linkstate.nlri;
+
+import com.google.common.primitives.UnsignedBytes;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import org.opendaylight.protocol.bgp.linkstate.TlvUtil;
+import org.opendaylight.protocol.bgp.parser.BGPParsingException;
+import org.opendaylight.protocol.util.ByteArray;
+import org.opendaylight.protocol.util.Ipv4Util;
+import org.opendaylight.protocol.util.Ipv6Util;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpPrefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.OspfRouteType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.TopologyIdentifier;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.linkstate.destination.c.linkstate.destination.PrefixDescriptors;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev131125.linkstate.destination.c.linkstate.destination.PrefixDescriptorsBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class PrefixNlriParser {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PrefixNlriParser.class);
+
+    /* Prefix Descriptor TLVs */
+    private static final int OSPF_ROUTE_TYPE = 264;
+    private static final int IP_REACHABILITY = 265;
+
+    static PrefixDescriptors parsePrefixDescriptors(final ByteBuf buffer, final boolean ipv4) throws BGPParsingException {
+        final PrefixDescriptorsBuilder builder = new PrefixDescriptorsBuilder();
+        while (buffer.isReadable()) {
+            final int type = buffer.readUnsignedShort();
+            final int length = buffer.readUnsignedShort();
+            final ByteBuf value = buffer.slice(buffer.readerIndex(), length);
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("Parsing Prefix Descriptor: {}", ByteBufUtil.hexDump(value));
+            }
+            switch (type) {
+            case TlvUtil.MULTI_TOPOLOGY_ID:
+                final TopologyIdentifier topologyId = new TopologyIdentifier(value.readShort() & TlvUtil.TOPOLOGY_ID_OFFSET);
+                builder.setMultiTopologyId(topologyId);
+                LOG.trace("Parsed Topology Identifier: {}", topologyId);
+                break;
+            case OSPF_ROUTE_TYPE:
+                final int rt = value.readByte();
+                final OspfRouteType routeType = OspfRouteType.forValue(rt);
+                if (routeType == null) {
+                    throw new BGPParsingException("Unknown OSPF Route Type: " + rt);
+                }
+                builder.setOspfRouteType(routeType);
+                LOG.trace("Parser RouteType: {}", routeType);
+                break;
+            case IP_REACHABILITY:
+                IpPrefix prefix = null;
+                final int prefixLength = value.readByte();
+                final int size = prefixLength / Byte.SIZE + ((prefixLength % Byte.SIZE == 0) ? 0 : 1);
+                if (size != value.readableBytes()) {
+                    LOG.debug("Expected length {}, actual length {}.", size, value.readableBytes());
+                    throw new BGPParsingException("Illegal length of IP reachability TLV: " + (value.readableBytes()));
+                }
+                prefix = (ipv4) ? new IpPrefix(Ipv4Util.prefixForBytes(ByteArray.readBytes(value, size), prefixLength)):
+                    new IpPrefix(Ipv6Util.prefixForBytes(ByteArray.readBytes(value, size), prefixLength));
+                builder.setIpReachabilityInformation(prefix);
+                LOG.trace("Parsed IP reachability info: {}", prefix);
+                break;
+            default:
+                throw new BGPParsingException("Prefix Descriptor not recognized, type: " + type);
+            }
+            buffer.skipBytes(length);
+        }
+        LOG.debug("Finished parsing Prefix descriptors.");
+        return builder.build();
+    }
+
+    static void serializePrefixDescriptors(final PrefixDescriptors descriptors, final ByteBuf buffer) {
+        if (descriptors.getMultiTopologyId() != null) {
+            TlvUtil.writeTLV(TlvUtil.MULTI_TOPOLOGY_ID, Unpooled.copyShort(descriptors.getMultiTopologyId().getValue()), buffer);
+        }
+        if (descriptors.getOspfRouteType() != null) {
+            TlvUtil.writeTLV(OSPF_ROUTE_TYPE,
+                Unpooled.wrappedBuffer(new byte[] {UnsignedBytes.checkedCast(descriptors.getOspfRouteType().getIntValue()) }), buffer);
+        }
+        if (descriptors.getIpReachabilityInformation() != null) {
+            final IpPrefix prefix = descriptors.getIpReachabilityInformation();
+            byte[] prefixBytes = null;
+            if (prefix.getIpv4Prefix() != null) {
+                prefixBytes = Ipv4Util.bytesForPrefixBegin(prefix.getIpv4Prefix());
+            } else if (prefix.getIpv6Prefix() != null) {
+                prefixBytes = Ipv6Util.bytesForPrefixBegin(prefix.getIpv6Prefix());
+            }
+            TlvUtil.writeTLV(IP_REACHABILITY, Unpooled.wrappedBuffer(prefixBytes), buffer);
+        }
+    }
+}
