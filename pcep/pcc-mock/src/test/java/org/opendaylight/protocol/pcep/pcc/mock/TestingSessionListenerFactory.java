b5b7322a80495a232cd542ef50c9ecadd139e2bd
Milos Fabian <milfabia@cisco.com>
2015-02-09 15:37:35 +0100

    Bug-2208: pcc-mock code refactoring
    -removed unnecessary code
    -switched to use immediate reconnecting strategy
    -refactored client dispatcher and main class
    -improved junit tests
    
    Change-Id: I3d44ad88a5bf81212443bd0079ad82719e367b1c
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ pcep/pcc-mock/src/test/java/org/opendaylight/protocol/pcep/pcc/mock/TestingSessionListenerFactory.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.pcep.pcc.mock;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.concurrent.GuardedBy;
+import org.opendaylight.protocol.pcep.PCEPSession;
+import org.opendaylight.protocol.pcep.PCEPSessionListener;
+import org.opendaylight.protocol.pcep.PCEPSessionListenerFactory;
+
+public class TestingSessionListenerFactory implements PCEPSessionListenerFactory {
+
+    @GuardedBy("this")
+    private List<TestingSessionListener> sessionListeners = new ArrayList<>();
+
+    @Override
+    public PCEPSessionListener getSessionListener() {
+        final TestingSessionListener sessionListener = new TestingSessionListener();
+        this.sessionListeners.add(sessionListener);
+        return sessionListener;
+    }
+
+    public TestingSessionListener getSessionListenerByRemoteAddress(final InetAddress ipAddress) {
+        for (final TestingSessionListener sessionListener : this.sessionListeners) {
+            if (sessionListener.isUp()) {
+                final PCEPSession session = sessionListener.getSession();
+                if (session.getRemoteAddress().equals(ipAddress)) {
+                    return sessionListener;
+                }
+            }
+        }
+        return null;
+    }
+}
