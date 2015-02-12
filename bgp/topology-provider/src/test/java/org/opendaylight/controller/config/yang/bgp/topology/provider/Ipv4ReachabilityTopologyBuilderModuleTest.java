3d37f626d6a0daf258feabb08e610d44ecec6fed
Ladislav Borak <lborak@cisco.com>
2015-02-12 09:47:12 +0100

    Bug 2227 - API for RFC7311
    - added new metric into network_concepts.yang
    
    - added new path attribute represents AIGP into bgp_message.yang
    
    Change-Id: Ie82066f1bc7f7cb241438a80ac21ecfd57b5763d
    Signed-off-by: Ladislav Borak <lborak@cisco.com>

--- bgp/topology-provider/src/test/java/org/opendaylight/controller/config/yang/bgp/topology/provider/Ipv4ReachabilityTopologyBuilderModuleTest.java
+++ bgp/topology-provider/src/test/java/org/opendaylight/controller/config/yang/bgp/topology/provider/Ipv4ReachabilityTopologyBuilderModuleTest.java
@@ -39,6 +39,8 @@ public class Ipv4ReachabilityTopologyBuilderModuleTest extends AbstractRIBImplMo
         final List<String> paths = super.getYangModelsPaths();
         paths.add("/META-INF/yang/network-topology@2013-10-21.yang");
         paths.add("/META-INF/yang/l3-unicast-igp-topology@2013-10-21.yang");
+        paths.add("/META-INF/yang/bgp-message.yang");
+        paths.add("/META-INF/yang/network-topology.yang");
         return paths;
     }
 
