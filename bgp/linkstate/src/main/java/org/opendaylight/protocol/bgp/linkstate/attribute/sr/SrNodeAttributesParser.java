f2010340f49679d68b7882d8ee14cb1a0d630de9
Dana Kutenicsova <dkutenic@cisco.com>
2015-02-15 22:40:19 +0100

    BUG-608 : implemented parsers/serializers for SID Label Binding TLV and sub-tlvs
    Change-Id: I49d0e7fe2effaef56eef690e23ed8a177a0f722b
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/attribute/sr/SrNodeAttributesParser.java
+++ bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/attribute/sr/SrNodeAttributesParser.java
@@ -8,32 +8,235 @@
 package org.opendaylight.protocol.bgp.linkstate.attribute.sr;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
+import org.opendaylight.protocol.bgp.linkstate.TlvUtil;
 import org.opendaylight.protocol.util.ByteArray;
+import org.opendaylight.protocol.util.ByteBufWriteUtil;
+import org.opendaylight.protocol.util.Ipv4Util;
+import org.opendaylight.protocol.util.Ipv6Util;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpPrefix;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.node.state.SrAlgorithm;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.node.state.SrAlgorithmBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.node.state.SrCapabilities;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.node.state.SrCapabilitiesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.node.state.SrSidLabel;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.node.state.SrSidLabelBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.Algorithm;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.SidLabel;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.SidLabelBinding.SidLabelFlags;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.SrCapabilities.Flags;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.Weight;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.label.binding.SubTlvs;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.label.binding.SubTlvsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.SubtlvType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.EroMetricCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.EroMetricCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ip4EroBackupCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ip4EroBackupCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ip4EroCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ip4EroCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ipv6EroBackupCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ipv6EroBackupCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ipv6EroCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ipv6EroCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.SidLabelCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.SidLabelCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.UnnumberedEroBackupCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.UnnumberedEroBackupCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.UnnumberedEroCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.UnnumberedEroCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.concepts.rev131125.TeMetric;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class SrNodeAttributesParser {
 
-    private static final int FLAGS_SIZE = 1;
+    private static final Logger LOG = LoggerFactory.getLogger(SrNodeAttributesParser.class);
 
-    private static final int SID_TLV_TYPE = 1;
+    private static final int FLAGS_SIZE = 1;
 
     /* SR Capabilities flags */
     private static final int IPV4 = 7;
     private static final int IPV6 = 6;
 
+    /* SID Label flags */
+    private static final int AFI = 7;
+    private static final int MIRROR = 6;
+
+    /* SID Label Tlv types */
+    private static final int SID_TLV_TYPE = 1;
+    private static final int ERO_METRIC = 2;
+    private static final int ERO_IPV4 = 3;
+    private static final int ERO_IPV6 = 4;
+    private static final int UNNUMBERED_ERO = 5;
+    private static final int BACKUP_IPV4 = 6;
+    private static final int BACKUP_IPV6 = 7;
+    private static final int UNNUMBERED_BACKUP_ERO = 8;
+
+    private static final int UNNUMBERED_4_SIZE = 8;
+
+    private static List<SubTlvs> parseSidSubtlvs(final ByteBuf buffer) {
+        final List<SubTlvs> subs = new ArrayList<>();
+        while (buffer.isReadable()) {
+            final int type = buffer.readUnsignedByte();
+            final int length = buffer.readUnsignedByte();
+            final ByteBuf value = buffer.readSlice(length);
+            SubtlvType sub = null;
+            switch (type) {
+            case SID_TLV_TYPE:
+                sub = new SidLabelCaseBuilder().setSid(new SidLabel(ByteArray.readAllBytes(value))).build();
+                break;
+            case ERO_METRIC:
+                sub = new EroMetricCaseBuilder().setEroMetric(new TeMetric(value.readUnsignedInt())).build();
+                break;
+            case ERO_IPV4:
+                final Ip4EroCaseBuilder ero4 = new Ip4EroCaseBuilder().setLoose(value.readUnsignedByte() != 0);
+                ero4.setAddress(new IpAddress(Ipv4Util.addressForByteBuf(value)));
+                sub = ero4.build();
+                break;
+            case ERO_IPV6:
+                final Ipv6EroCaseBuilder ero6 = new Ipv6EroCaseBuilder().setLoose(value.readUnsignedByte() != 0);
+                ero6.setAddress(new IpAddress(Ipv6Util.addressForByteBuf(value)));
+                sub = ero6.build();
+                break;
+            case UNNUMBERED_ERO:
+                final UnnumberedEroCaseBuilder un = new UnnumberedEroCaseBuilder().setLoose(value.readUnsignedByte() != 0);
+                if (value.readableBytes() == UNNUMBERED_4_SIZE) {
+                    un.setRouterId(ByteArray.readBytes(value, Ipv4Util.IP4_LENGTH));
+                } else {
+                    un.setRouterId(ByteArray.readBytes(value, Ipv6Util.IPV6_LENGTH));
+                }
+                un.setInterfaceId(value.readUnsignedInt());
+                sub = un.build();
+                break;
+            case BACKUP_IPV4:
+                final Ip4EroBackupCaseBuilder erob4 = new Ip4EroBackupCaseBuilder().setLoose(value.readUnsignedByte() != 0);
+                erob4.setAddress(new IpAddress(Ipv4Util.addressForByteBuf(value)));
+                sub = erob4.build();
+                break;
+            case BACKUP_IPV6:
+                final Ipv6EroBackupCaseBuilder erob6 = new Ipv6EroBackupCaseBuilder().setLoose(value.readUnsignedByte() != 0);
+                erob6.setAddress(new IpAddress(Ipv6Util.addressForByteBuf(value)));
+                sub = erob6.build();
+                break;
+            case UNNUMBERED_BACKUP_ERO:
+                final UnnumberedEroBackupCaseBuilder unb = new UnnumberedEroBackupCaseBuilder().setLoose(value.readUnsignedByte() != 0);
+                if (value.readableBytes() == UNNUMBERED_4_SIZE) {
+                    unb.setRouterId(ByteArray.readBytes(value, Ipv4Util.IP4_LENGTH));
+                } else {
+                    unb.setRouterId(ByteArray.readBytes(value, Ipv6Util.IPV6_LENGTH));
+                }
+                unb.setInterfaceId(value.readUnsignedInt());
+                sub = unb.build();
+                break;
+            default:
+                LOG.debug("Unknown SID Label Subtlv found, type {}", type);
+                // we don't want to add null sub-tlv
+            }
+            subs.add(new SubTlvsBuilder().setSubtlvType(sub).build());
+        }
+        return subs;
+    }
+
     public static SrSidLabel parseSidLabelBinding(final ByteBuf buffer) {
-        // TODO Auto-generated method stub
-        return null;
+        final SrSidLabelBuilder builder = new SrSidLabelBuilder();
+        final BitSet flags = BitSet.valueOf(ByteArray.readBytes(buffer, FLAGS_SIZE));
+        builder.setSidLabelFlags(new SidLabelFlags(flags.get(AFI), flags.get(MIRROR)));
+        builder.setWeight(new Weight(buffer.readUnsignedByte()));
+        builder.setValueRange(buffer.readUnsignedShort());
+        final int length = buffer.readUnsignedByte();
+        IpPrefix prefix = null;
+        if (length == Ipv4Util.IP4_LENGTH) {
+            prefix = new IpPrefix(Ipv4Util.prefixForByteBuf(buffer));
+        } else {
+            prefix = new IpPrefix(Ipv6Util.prefixForByteBuf(buffer));
+        }
+        builder.setFecPrefix(prefix);
+        builder.setSubTlvs(parseSidSubtlvs(buffer));
+        return builder.build();
+    }
+
+    private static byte serializeLoose(final boolean loose) {
+        return loose ? (byte)128 : 0;
+    }
+
+    private static void serializeSidSubtlvs(final List<SubTlvs> subTlvs, final ByteBuf buffer) {
+        if (subTlvs == null) {
+            return;
+        }
+        for (final SubTlvs sub : subTlvs) {
+            final SubtlvType type = sub.getSubtlvType();
+            if (type instanceof SidLabelCase) {
+                TlvUtil.writeSrTLV(SID_TLV_TYPE, Unpooled.wrappedBuffer(((SidLabelCase)type).getSid().getValue()), buffer);
+            } else if (type instanceof EroMetricCase) {
+                final ByteBuf b = Unpooled.buffer();
+                ByteBufWriteUtil.writeUnsignedInt(((EroMetricCase)type).getEroMetric().getValue(), b);
+                TlvUtil.writeSrTLV(ERO_METRIC, b, buffer);
+            } else if (type instanceof Ip4EroCase) {
+                final ByteBuf b = Unpooled.buffer(Ipv4Util.IP4_LENGTH + FLAGS_SIZE);
+                final Ip4EroCase ero = (Ip4EroCase)type;
+                b.writeByte(serializeLoose(ero.isLoose()));
+                ByteBufWriteUtil.writeIpv4Address(ero.getAddress().getIpv4Address(), b);
+                TlvUtil.writeSrTLV(ERO_IPV4, b, buffer);
+            } else if (type instanceof Ipv6EroCase) {
+                final ByteBuf b = Unpooled.buffer(Ipv6Util.IPV6_LENGTH + FLAGS_SIZE);
+                final Ipv6EroCase ero = (Ipv6EroCase)type;
+                b.writeByte(serializeLoose(ero.isLoose()));
+                ByteBufWriteUtil.writeIpv6Address(ero.getAddress().getIpv6Address(), b);
+                TlvUtil.writeSrTLV(ERO_IPV6, b, buffer);
+            } else if (type instanceof UnnumberedEroCase) {
+                final UnnumberedEroCase ero = (UnnumberedEroCase)type;
+                final ByteBuf b = Unpooled.buffer();
+                b.writeByte(serializeLoose(ero.isLoose()));
+                b.writeBytes(ero.getRouterId());
+                ByteBufWriteUtil.writeUnsignedInt(ero.getInterfaceId(), b);
+                TlvUtil.writeSrTLV(UNNUMBERED_ERO, b, buffer);
+            } else if (type instanceof Ip4EroBackupCase) {
+                final ByteBuf b = Unpooled.buffer(Ipv4Util.IP4_LENGTH + FLAGS_SIZE);
+                final Ip4EroBackupCase ero = (Ip4EroBackupCase)type;
+                b.writeByte(serializeLoose(ero.isLoose()));
+                ByteBufWriteUtil.writeIpv4Address(ero.getAddress().getIpv4Address(), b);
+                TlvUtil.writeSrTLV(BACKUP_IPV4, b, buffer);
+            } else if (type instanceof Ipv6EroBackupCase) {
+                final ByteBuf b = Unpooled.buffer(Ipv6Util.IPV6_LENGTH + FLAGS_SIZE);
+                final Ipv6EroBackupCase ero = (Ipv6EroBackupCase)type;
+                b.writeByte(serializeLoose(ero.isLoose()));
+                ByteBufWriteUtil.writeIpv6Address(ero.getAddress().getIpv6Address(), b);
+                TlvUtil.writeSrTLV(BACKUP_IPV6, b, buffer);
+            } else if (type instanceof UnnumberedEroBackupCase) {
+                final UnnumberedEroBackupCase ero = (UnnumberedEroBackupCase)type;
+                final ByteBuf b = Unpooled.buffer();
+                b.writeByte(serializeLoose(ero.isLoose()));
+                b.writeBytes(ero.getRouterId());
+                ByteBufWriteUtil.writeUnsignedInt(ero.getInterfaceId(), b);
+                TlvUtil.writeSrTLV(UNNUMBERED_BACKUP_ERO, b, buffer);
+            }
+        }
+    }
+
+    public static void serializeSidLabelBinding(final SrSidLabel binding, final ByteBuf buffer) {
+        final SidLabelFlags flags = binding.getSidLabelFlags();
+        final BitSet bs = new BitSet(FLAGS_SIZE);
+        if (flags.isAddressFamily() != null) {
+            bs.set(AFI, flags.isAddressFamily());
+        }
+        if (flags.isMirrorContext() != null) {
+            bs.set(MIRROR, flags.isMirrorContext());
+        }
+        buffer.writeBytes(bs.toByteArray());
+        buffer.writeByte(binding.getWeight().getValue());
+        buffer.writeShort(binding.getValueRange());
+        final IpPrefix prefix = binding.getFecPrefix();
+        if (prefix.getIpv4Prefix() != null) {
+            buffer.writeBytes(Ipv4Util.bytesForPrefixBegin(prefix.getIpv4Prefix()));
+        } else {
+            buffer.writeBytes(Ipv6Util.bytesForPrefixBegin(prefix.getIpv6Prefix()));
+        }
+        serializeSidSubtlvs(binding.getSubTlvs(), buffer);
     }
 
     public static SrCapabilities parseSrCapabilities(final ByteBuf buffer) {
