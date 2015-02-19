7cbf534400977d7555cb3218d8be9631f2c1168a
Dana Kutenicsova <dkutenic@cisco.com>
2015-02-19 13:30:23 +0100

    Added some unit tests.
    Change-Id: I4fbcf0775956087b089db136327e1bbbe4ec348e
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

---
+++ bgp/flowspec/src/test/java/org/opendaylight/protocol/bgp/flowspec/ActivatorTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.flowspec;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderContext;
+import org.opendaylight.protocol.bgp.parser.spi.pojo.SimpleBGPExtensionProviderContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.flowspec.rev150114.FlowspecSubsequentAddressFamily;
+
+public class ActivatorTest {
+
+
+    @Test
+    public void testActivator() throws Exception {
+        final FSActivator act = new FSActivator();
+        final BGPExtensionProviderContext context = new SimpleBGPExtensionProviderContext();
+        assertNull(context.getSubsequentAddressFamilyRegistry().classForFamily(133));
+        act.start(context);
+        assertEquals(FlowspecSubsequentAddressFamily.class, context.getSubsequentAddressFamilyRegistry().classForFamily(133));
+        act.close();
+    }
+}
