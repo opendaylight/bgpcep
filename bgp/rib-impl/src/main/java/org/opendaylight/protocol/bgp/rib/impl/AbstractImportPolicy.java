5650dc1ca9b048179449230531f3fa2711db74ea
Robert Varga <rovarga@cisco.com>
2015-02-26 15:18:39 +0100

    BUG-2383: rework RIB inbound
    Each Peer (like BGPPeer) performs updates the adj-rib-in via a dedicated
    AdjRibInWriter instance. That class, along with RIBSupport and TableContext,
    is responsible for translating the Binding-Aware messages coming from
    the message parser into Binding-Independent form within the datastore.
    
    Changes are then propagated via DataChangeNotifications into
    InboundPolicyEnforcer, which performs any munging of attributes required
    and checks if the route is eligible for inclusion in the local RIB. If
    it is, the route is output into the effective-rib-in hosted in the
    datastore.
    
    From there it is picked up by BestPathSelection, which compares the
    effective routes from multiple peers and installs the best one into the
    local RIB.
    
    Change-Id: Ie23746b2fc44499f42d346595562b498c19fd112
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AbstractImportPolicy.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl;
+
+import java.util.EnumMap;
+import java.util.Map;
+import javax.annotation.Nullable;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.PeerRole;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+/**
+ * Defines the internal hooks invoked when a new route appears.
+ */
+abstract class AbstractImportPolicy {
+    // Invoked on routes which we get from outside of our home AS
+    private static final class FromExternalImportPolicy extends AbstractImportPolicy {
+        @Override
+        ContainerNode effectiveAttributes(final ContainerNode attributes) {
+            // FIXME: filter all non-transitive attributes
+            // FIXME: but that may end up hurting our informedness
+            return attributes;
+        }
+    }
+
+    // Invoked on routes which we get from our normal home AS peers.
+    private static class FromInternalImportPolicy extends AbstractImportPolicy {
+        @Override
+        ContainerNode effectiveAttributes(final ContainerNode attributes) {
+            // FIXME: Implement filtering according to <a href="http://tools.ietf.org/html/rfc4456#section-8"/>.
+            return attributes;
+        }
+    }
+
+    /**
+     * Invoked on routes which we get from our reflector peers. This is a special-case of
+     * FromInternalImportPolicy.
+     *
+     */
+    private static final class FromReflectorClientImportPolicy extends FromInternalImportPolicy {
+        // FIXME: override if necessary
+    }
+
+    private static final Map<PeerRole, AbstractImportPolicy> POLICIES;
+
+    static {
+        final Map<PeerRole, AbstractImportPolicy> p = new EnumMap<PeerRole, AbstractImportPolicy>(PeerRole.class);
+        p.put(PeerRole.Ebgp, new FromExternalImportPolicy());
+        p.put(PeerRole.Ibgp, new FromInternalImportPolicy());
+        p.put(PeerRole.RrClient, new FromReflectorClientImportPolicy());
+        POLICIES = p;
+    }
+
+    static AbstractImportPolicy forRole(final PeerRole peerRole) {
+        return POLICIES.get(peerRole);
+    }
+
+    /**
+     * Transform incoming attributes according to policy.
+     *
+     * @param attributes received attributes
+     * @return Filtered attributes, or null if the advertisement should be ignored.
+     */
+    abstract @Nullable ContainerNode effectiveAttributes(@Nullable ContainerNode attributes);
+}
---
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/RIBSupport.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.spi;
+
+import java.util.Collection;
+import javax.annotation.Nonnull;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+public interface RIBSupport {
+    /**
+     * Return the table-type-specific empty routes container, as augmented into the
+     * bgp-rib model under /rib/tables/routes choice node. This needs to include all
+     * the skeleton nodes under which the individual routes will be stored.
+     *
+     * @return Protocol-specific case in the routes choice, may not be null.
+     */
+    @Nonnull ChoiceNode emptyRoutes();
+    @Nonnull NodeIdentifier routeAttributes();
+
+    @Nonnull Collection<Class<? extends DataObject>> cacheableAttributeObjects();
+    @Nonnull Collection<Class<? extends DataObject>> cacheableNlriObjects();
+    void deleteRoutes(@Nonnull DOMDataWriteTransaction tx, @Nonnull YangInstanceIdentifier tableId, @Nonnull ContainerNode nlri);
+    void putRoutes(@Nonnull DOMDataWriteTransaction tx, @Nonnull YangInstanceIdentifier tableId, @Nonnull ContainerNode nlri, @Nonnull ContainerNode attributes);
+
+}
