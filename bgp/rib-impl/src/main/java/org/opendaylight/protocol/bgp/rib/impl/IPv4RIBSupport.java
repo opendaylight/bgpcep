d22ab1319106ce8dad50214429c305be5eff06cc
Robert Varga <rovarga@cisco.com>
2015-02-26 15:18:39 +0100

    BUG-2383: rework RIB inbound
    Each Peer (like BGPPeer) performs updates the adj-rib-in via a dedicated
    AdjRibInWriter instance. That class, along with RIBSupport and TableContext,
    is responsible for translating the Binding-Aware messages coming from
    the message parser into Binding-Independent form within the datastore.
    
    Changes are then propagated via DataChangeNotifications into
    InboundPolicyEnforcer, which performs any munging of attributes required
    and checks if the route is eligible for inclusion in the local RIB. If
    it is, the route is output into the effective-rib-in hosted in the
    datastore.
    
    From there it is picked up by BestPathSelection, which compares the
    effective routes from multiple peers and installs the best one into the
    local RIB.
    
    Change-Id: Ie23746b2fc44499f42d346595562b498c19fd112
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/IPv4RIBSupport.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.destination.destination.type.DestinationIpv4Case;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.tables.Routes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.tables.routes.ipv4.routes._case.Ipv4Routes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.tables.routes.ipv4.routes._case.ipv4.routes.Ipv4Route;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.route.Attributes;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+
+final class IPv4RIBSupport extends AbstractIPRIBSupport {
+    private static final IPv4RIBSupport SINGLETON = new IPv4RIBSupport();
+    private final ChoiceNode emptyRoutes = Builders.choiceBuilder()
+            .withNodeIdentifier(new NodeIdentifier(Routes.QNAME))
+            .addChild(Builders.containerBuilder()
+                .withNodeIdentifier(new NodeIdentifier(Ipv4Routes.QNAME))
+                .withChild(ImmutableNodes.mapNodeBuilder(Ipv4Route.QNAME).build()).build()).build();
+    private final NodeIdentifier destination = new NodeIdentifier(DestinationIpv4Case.QNAME);
+    private final NodeIdentifier routes = new NodeIdentifier(Ipv4Routes.QNAME);
+    private final NodeIdentifier route = new NodeIdentifier(Ipv4Route.QNAME);
+    private final NodeIdentifier attributes = new NodeIdentifier(QName.create(Ipv4Route.QNAME, Attributes.QNAME.getLocalName()));
+
+    private static final QName PREFIX_QNAME = QName.create(Ipv4Route.QNAME, "prefix");
+
+    private IPv4RIBSupport() {
+
+    }
+
+    static IPv4RIBSupport getInstance() {
+        return SINGLETON;
+    }
+
+    @Override
+    public ChoiceNode emptyRoutes() {
+        return emptyRoutes;
+    }
+
+    @Override
+    public NodeIdentifier routeAttributes() {
+        return attributes;
+    }
+
+    @Override
+    protected NodeIdentifier destinationIdentifier() {
+        return destination;
+    }
+
+    @Override
+    protected NodeIdentifier routeIdentifier() {
+        return route;
+    }
+
+    @Override
+    protected NodeIdentifier routesIdentifier() {
+        return routes;
+    }
+}
