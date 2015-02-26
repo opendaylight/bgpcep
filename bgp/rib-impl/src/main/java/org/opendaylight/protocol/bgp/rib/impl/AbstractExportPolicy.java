82912ee2c7d083cc62cf14e41e9eef03fefdd313
Robert Varga <rovarga@cisco.com>
2015-02-26 15:18:39 +0100

    BUG-2383: rework RIB inbound
    Each Peer (like BGPPeer) performs updates the adj-rib-in via a dedicated
    AdjRibInWriter instance. That class, along with RIBSupport and TableContext,
    is responsible for translating the Binding-Aware messages coming from
    the message parser into Binding-Independent form within the datastore.
    
    Changes are then propagated via DataChangeNotifications into
    InboundPolicyEnforcer, which performs any munging of attributes required
    and checks if the route is eligible for inclusion in the local RIB. If
    it is, the route is output into the effective-rib-in hosted in the
    datastore.
    
    From there it is picked up by BestPathSelection, which compares the
    effective routes from multiple peers and installs the best one into the
    local RIB.
    
    Change-Id: Ie23746b2fc44499f42d346595562b498c19fd112
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AbstractExportPolicy.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl;
+
+import com.google.common.collect.Maps;
+import java.util.EnumMap;
+import java.util.Map;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.PeerRole;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+/**
+ * Defines the internal hooks invoked when a new route appears.
+ */
+abstract class AbstractExportPolicy {
+    // Invoked on routes which we send outside of our home AS
+    private static final class ToExternalExportPolicy extends AbstractExportPolicy {
+        @Override
+        ContainerNode effectiveAttributes(final PeerRole sourceRole, final ContainerNode attributes) {
+            // FIXME: filter all non-transitive attributes
+            // FIXME: but that may end up hurting our informedness
+            // FIXME: prepend local AS to add our AS into AS_PATH
+
+            switch (sourceRole) {
+            case Ebgp:
+                // eBGP -> eBGP, propagate
+                return attributes;
+            case Ibgp:
+                // Non-Client iBGP -> eBGP, propagate
+                return attributes;
+            case RrClient:
+                // Client iBGP -> eBGP, propagate
+                return attributes;
+            }
+            return attributes;
+        }
+    }
+
+    // Invoked on routes which we send to our normal home AS peers.
+    private static class ToInternalExportPolicy extends AbstractExportPolicy {
+        protected static ContainerNode prependClusterId(final ContainerNode attributes) {
+            // FIXME: prepend local CLUSTER_ID to CLUSTER_LIST
+            return attributes;
+        }
+
+        // Attributes when reflecting a route
+        protected static ContainerNode toClientAttributes(final ContainerNode attributes) {
+            // TODO: (defensiveness) verify ORIGINATOR_ID (should have been set)
+
+            return prependClusterId(attributes);
+        }
+
+        @Override
+        ContainerNode effectiveAttributes(final PeerRole sourceRole, final ContainerNode attributes) {
+            // Implement filtering according to <a ref="http://tools.ietf.org/html/rfc4456#section-8"/>.
+
+            switch (sourceRole) {
+            case Ebgp:
+                // eBGP -> Non-Client iBGP, propagate
+                return attributes;
+
+            case Ibgp:
+                // Non-Client iBGP -> Non-Client iBGP, block
+                return null;
+
+            case RrClient:
+                // Client iBGP -> Non-Client iBGP, reflect
+                return toClientAttributes(attributes);
+            }
+            return attributes;
+        }
+    }
+
+    /**
+     * Invoked on routes which we send to our reflector peers. This is a special-case of
+     * FromInternalImportPolicy.
+     */
+    private static final class ToReflectorClientExportPolicy extends AbstractExportPolicy {
+        @Override
+        ContainerNode effectiveAttributes(final PeerRole sourceRole, final ContainerNode attributes) {
+            switch (sourceRole) {
+            case Ebgp:
+                // eBGP -> Client iBGP, propagate
+                return attributes;
+            case Ibgp:
+                // Non-Client iBGP -> Client iBGP, reflect
+                // FIXME: add ORIGINATOR_ID
+
+                return ToInternalExportPolicy.prependClusterId(attributes);
+            case RrClient:
+                // Client iBGP -> Client iBGP, reflect
+                return ToInternalExportPolicy.toClientAttributes(attributes);
+            }
+
+            throw new IllegalStateException("Unhandled source role " + sourceRole);
+        }
+    }
+
+    static final Map<PeerRole, AbstractExportPolicy> POLICIES;
+
+    static {
+        final Map<PeerRole, AbstractExportPolicy> p = new EnumMap<PeerRole, AbstractExportPolicy>(PeerRole.class);
+        p.put(PeerRole.Ebgp, new ToExternalExportPolicy());
+        p.put(PeerRole.Ibgp, new ToInternalExportPolicy());
+        p.put(PeerRole.RrClient, new ToReflectorClientExportPolicy());
+        POLICIES = Maps.immutableEnumMap(p);
+    }
+
+    static AbstractExportPolicy forRole(final PeerRole peerRole) {
+        return POLICIES.get(peerRole);
+    }
+
+    /**
+     * Transform outgoing attributes according to policy.
+     *
+     * @param sourceRole role of the peer which originated the routes
+     * @param attributes outgoing attributes
+     * @return Filtered attributes, or null if the advertisement should be ignored.
+     */
+    abstract ContainerNode effectiveAttributes(PeerRole sourceRole, ContainerNode attributes);
+}
