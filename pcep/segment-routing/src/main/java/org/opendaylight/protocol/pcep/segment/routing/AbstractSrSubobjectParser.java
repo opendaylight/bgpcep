f502a27e5848dffb50c99216499f1e84c9118020
Ladislav Borak <lborak@cisco.com>
2015-03-11 16:02:13 +0100

    Bug 2803 - Update SR
    - updated API to confirm with
      https://tools.ietf.org/html/draft-ietf-pce-segment-routing-01
    
    - added new error type
    
    - added abstract class with parsing and serializing SrSubobject
    
    Change-Id: Ic3b9d5a4b0f6bd8332211bfc670c2ff2c2dc9c95
    Signed-off-by: Ladislav Borak <lborak@cisco.com>
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/AbstractSrSubobjectParser.java
+++ pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/AbstractSrSubobjectParser.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- */
 package org.opendaylight.protocol.pcep.segment.routing;
 
 import static org.opendaylight.protocol.util.ByteBufWriteUtil.writeIpv4Address;
@@ -12,7 +5,6 @@ import static org.opendaylight.protocol.util.ByteBufWriteUtil.writeIpv6Address;
 import static org.opendaylight.protocol.util.ByteBufWriteUtil.writeUnsignedByte;
 import static org.opendaylight.protocol.util.ByteBufWriteUtil.writeUnsignedInt;
 
-import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -34,78 +26,97 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.seg
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev150112.sr.subobject.nai.UnnumberedAdjacencyBuilder;
 import org.opendaylight.yangtools.yang.binding.DataContainer;
 
-final class SrSubobjectParserUtil {
+public abstract class AbstractSrSubobjectParser   {
 
-    public static final int MINIMAL_LENGTH = 4;
-    public static final int BITSET_LENGTH = 8;
+    protected static final int MINIMAL_LENGTH = 4;
+    protected static final int BITSET_LENGTH = 8;
+    protected static final int M_FLAG_POSITION = 7;
+    protected static final int C_FLAG_POSITION = 6;
+    protected static final int S_FLAG_POSITION = 5;
+    protected static final int F_FLAG_POSITION = 4;
+    protected static final int MPLS_LABEL_OFFSET = 12;
 
     private static final int SID_TYPE_BITS_OFFSET = 4;
 
-    private SrSubobjectParserUtil() {
-        throw new UnsupportedOperationException();
-    }
+    public ByteBuf serializeSubobject(final SrSubobject srSubobject) {
+        final ByteBuf buffer = Unpooled.buffer(MINIMAL_LENGTH);
+        // sid type
+        writeUnsignedByte((short)(srSubobject.getSidType().getIntValue() << SID_TYPE_BITS_OFFSET), buffer);
 
-    public static ByteBuf serializeSrSubobject(final SrSubobject srSubobject, final BitArray bits) {
-        Preconditions.checkArgument(srSubobject.getNai() != null || srSubobject.getSid() != null,
-                "Both SID and NAI are absent in SR subobject.");
-        final ByteBuf body = Unpooled.buffer(MINIMAL_LENGTH);
-        writeUnsignedByte((short)(srSubobject.getSidType().getIntValue() << SID_TYPE_BITS_OFFSET), body);
-        bits.toByteBuf(body);
-
-        if (srSubobject.getSid() != null) {
-            writeUnsignedInt(srSubobject.getSid(), body);
+        final BitArray bits = new BitArray(BITSET_LENGTH);
+        bits.set(M_FLAG_POSITION, srSubobject.isMFlag());
+        bits.set(C_FLAG_POSITION, srSubobject.isCFlag());
+        if (srSubobject.getSid() == null) {
+            bits.set(S_FLAG_POSITION, Boolean.TRUE);
         }
+        if (srSubobject.getNai() == null) {
+            bits.set(F_FLAG_POSITION, Boolean.TRUE);
+        }
+        // bits
+        bits.toByteBuf(buffer);
+        // sid
+        Preconditions.checkArgument(srSubobject.getNai() != null || srSubobject.getSid() != null, "Both SID and NAI are absent in SR subobject.");
+        if (srSubobject.getSid() != null) {
+            if (srSubobject.isMFlag()) {
+                writeUnsignedInt(srSubobject.getSid() << MPLS_LABEL_OFFSET, buffer);
+            } else {
+                writeUnsignedInt(srSubobject.getSid(), buffer);
+            }
+        }
+        // nai
         final Nai nai = srSubobject.getNai();
         if (nai != null) {
             switch (srSubobject.getSidType()) {
             case Ipv4NodeId:
-                writeIpv4Address(((IpNodeId) nai).getIpAddress().getIpv4Address(), body);
+                writeIpv4Address(((IpNodeId) nai).getIpAddress().getIpv4Address(), buffer);
                 break;
             case Ipv6NodeId:
-                writeIpv6Address(((IpNodeId) nai).getIpAddress().getIpv6Address(), body);
+                writeIpv6Address(((IpNodeId) nai).getIpAddress().getIpv6Address(), buffer);
                 break;
             case Ipv4Adjacency:
-                writeIpv4Address(((IpAdjacency) nai).getLocalIpAddress().getIpv4Address(), body);
-                writeIpv4Address(((IpAdjacency) nai).getRemoteIpAddress().getIpv4Address(), body);
+                writeIpv4Address(((IpAdjacency) nai).getLocalIpAddress().getIpv4Address(), buffer);
+                writeIpv4Address(((IpAdjacency) nai).getRemoteIpAddress().getIpv4Address(), buffer);
                 break;
             case Ipv6Adjacency:
-                writeIpv6Address(((IpAdjacency) nai).getLocalIpAddress().getIpv6Address(), body);
-                writeIpv6Address(((IpAdjacency) nai).getRemoteIpAddress().getIpv6Address(), body);
+                writeIpv6Address(((IpAdjacency) nai).getLocalIpAddress().getIpv6Address(), buffer);
+                writeIpv6Address(((IpAdjacency) nai).getRemoteIpAddress().getIpv6Address(), buffer);
                 break;
             case Unnumbered:
                 final UnnumberedAdjacency unnumbered = (UnnumberedAdjacency) nai;
-                ByteBufWriteUtil.writeUnsignedInt(unnumbered.getLocalNodeId(), body);
-                ByteBufWriteUtil.writeUnsignedInt(unnumbered.getLocalInterfaceId(), body);
-                ByteBufWriteUtil.writeUnsignedInt(unnumbered.getRemoteNodeId(), body);
-                ByteBufWriteUtil.writeUnsignedInt(unnumbered.getRemoteInterfaceId(), body);
+                ByteBufWriteUtil.writeUnsignedInt(unnumbered.getLocalNodeId(), buffer);
+                ByteBufWriteUtil.writeUnsignedInt(unnumbered.getLocalInterfaceId(), buffer);
+                ByteBufWriteUtil.writeUnsignedInt(unnumbered.getRemoteNodeId(), buffer);
+                ByteBufWriteUtil.writeUnsignedInt(unnumbered.getRemoteInterfaceId(), buffer);
                 break;
             default:
                 break;
             }
         }
-        return body;
+        return buffer;
     }
 
-    public static SrSubobject parseSrSubobject(final ByteBuf buffer, final Function<BitArray, Void> getFlags, final int fPosition, final int sPosition)
-            throws PCEPDeserializerException {
+    protected final SrSubobject parseSrSubobject(final ByteBuf buffer) throws PCEPDeserializerException {
         final int sidTypeByte = buffer.readByte() >> SID_TYPE_BITS_OFFSET;
         final SidType sidType = SidType.forValue(sidTypeByte);
-
         final BitArray bitSet = BitArray.valueOf(buffer.readByte());
-        getFlags.apply(bitSet);
-        final boolean f = bitSet.get(fPosition);
-        final boolean s = bitSet.get(sPosition);
+        final boolean f = bitSet.get(F_FLAG_POSITION);
+        final boolean s = bitSet.get(S_FLAG_POSITION);
+        final boolean c = bitSet.get(C_FLAG_POSITION);
+        final boolean m = bitSet.get(M_FLAG_POSITION);
 
         if (f && s) {
             throw new PCEPDeserializerException("Both SID and NAI are absent in SR subobject.");
         }
-        final Long sid;
+        Long tmp;
         if (!s) {
-            sid = buffer.readUnsignedInt();
+            tmp = buffer.readUnsignedInt();
+            if (m) {
+                tmp = tmp >> MPLS_LABEL_OFFSET;
+            }
         } else {
-            sid = null;
+            tmp = null;
         }
-
+        final Long sid = tmp;
         final Nai nai;
 
         if (sidType != null && !f) {
@@ -140,20 +151,33 @@ final class SrSubobjectParserUtil {
         } else {
             nai = null;
         }
-
         return new SrSubobject() {
+
             @Override
             public Class<? extends DataContainer> getImplementedInterface() {
                 return SrSubobject.class;
             }
+
+            @Override
+            public Boolean isMFlag() {
+                return m;
+            }
+
+            @Override
+            public Boolean isCFlag() {
+                return c;
+            }
+
             @Override
             public SidType getSidType() {
                 return sidType;
             }
+
             @Override
             public Long getSid() {
                 return sid;
             }
+
             @Override
             public Nai getNai() {
                 return nai;
