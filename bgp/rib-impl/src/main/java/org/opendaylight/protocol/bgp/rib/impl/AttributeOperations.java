aaddf267c363ab7f7eb40ebe88ea8e263f30147a
Robert Varga <rovarga@cisco.com>
2015-03-11 12:26:58 +0100

    BUG-2383: split out policies into separate files
    Static policies kwill not work, as some of them require additional
    arguments to perform their role. Split them out, so they can be evolved
    independently.
    
    Change-Id: I33decf294c53391d4d705014adfd62b6d5ffa34d
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AttributeOperations.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl;
+
+import com.google.common.base.Optional;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.AsPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.ClusterId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.OriginatorId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.as.path.Segments;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.ClusterIdentifier;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.as.path.segment.CSegment;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.as.path.segment.c.segment.a.list._case.AList;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yangtools.yang.common.QNameModule;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.DataContainerChild;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
+import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeAttrBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.ListNodeBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility class for working with route attributes in binding independent form. An instance
+ * is always bound to a certain namespace, so it maintains cached attribute identifiers.
+ */
+final class AttributeOperations {
+    private static final Logger LOG = LoggerFactory.getLogger(AttributeOperations.class);
+    private static final LoadingCache<QNameModule, AttributeOperations> ATTRIBUTES_CACHE = CacheBuilder.newBuilder().weakKeys().weakValues().build(
+        new CacheLoader<QNameModule, AttributeOperations>() {
+            @Override
+            public AttributeOperations load(final QNameModule key) throws Exception {
+                return new AttributeOperations(key);
+            }
+        });
+
+    private final Iterable<PathArgument> originatorIdPath;
+    private final Iterable<PathArgument> clusterListPath;
+    private final NodeIdentifier originatorIdContainer;
+    private final NodeIdentifier originatorIdLeaf;
+    private final NodeIdentifier clusterListContainer;
+    private final NodeIdentifier clusterListLeaf;
+    private final NodeIdentifier asPathContainer;
+    private final NodeIdentifier asPathSegments;
+    private final NodeIdentifier asPathChoice;
+    private final Iterable<PathArgument> asPathPath;
+    private final NodeIdentifier asPathId;
+    private final NodeIdentifier asPathList;
+
+    private AttributeOperations(final QNameModule namespace) {
+        this.asPathContainer = new NodeIdentifier(QName.cachedReference(QName.create(namespace, AsPath.QNAME.getLocalName())));
+        this.asPathSegments = new NodeIdentifier(QName.cachedReference(QName.create(namespace, Segments.QNAME.getLocalName())));
+        this.asPathChoice = new NodeIdentifier(QName.cachedReference(QName.create(namespace, CSegment.QNAME.getLocalName())));
+        this.asPathList = new NodeIdentifier(QName.cachedReference(QName.create(namespace, AList.QNAME.getLocalName())));
+        this.asPathId = new NodeIdentifier(QName.cachedReference(QName.create(namespace, "as")));
+        this.asPathPath = ImmutableList.<PathArgument>of(asPathContainer, asPathSegments, asPathChoice, asPathId);
+
+        this.clusterListContainer = new NodeIdentifier(QName.cachedReference(QName.create(namespace, ClusterId.QNAME.getLocalName())));
+        this.clusterListLeaf = new NodeIdentifier(QName.cachedReference(QName.create(namespace, "cluster")));
+        this.clusterListPath = ImmutableList.<PathArgument>of(clusterListContainer, clusterListLeaf);
+        this.originatorIdContainer = new NodeIdentifier(QName.cachedReference(QName.create(namespace, OriginatorId.QNAME.getLocalName())));
+        this.originatorIdLeaf = new NodeIdentifier(QName.cachedReference(QName.create(namespace, "originator")));
+        this.originatorIdPath = ImmutableList.<PathArgument>of(originatorIdContainer, originatorIdLeaf);
+    }
+
+    static AttributeOperations getInstance(final ContainerNode attributes) {
+        return ATTRIBUTES_CACHE.getUnchecked(attributes.getNodeType().getModule());
+    }
+
+    ContainerNode exportedAttributes(final ContainerNode attributes, final Long localAs) {
+        // First filter out non-transitive attributes
+        // FIXME: removes MULTI_EXIT_DISC, too.
+        final ContainerNode transitives = transitiveAttributes(attributes);
+
+        // FIXME: allow transitiveAttributes() only add to the builder
+        final DataContainerNodeAttrBuilder<NodeIdentifier, ContainerNode> b = Builders.containerBuilder(transitives);
+        b.withNodeIdentifier(transitives.getIdentifier());
+
+        // Create a new AS_PATH builder
+        final ListNodeBuilder<Object, LeafSetEntryNode<Object>> apb = Builders.leafSetBuilder();
+        apb.withNodeIdentifier(asPathId);
+
+        // prepend local AS into AS_PATH, as per https://tools.ietf.org/html/rfc4271#section-5.1.2
+        apb.withChild(Builders.leafSetEntryBuilder().withValue(localAs).build());
+
+        // FIXME: munge apb into the attributes structure
+
+        return b.build();
+    }
+
+
+    // Attributes when reflecting a route
+    ContainerNode reflectedAttributes(final ContainerNode attributes, final Ipv4Address originatorId, final ClusterIdentifier clusterId) {
+        final DataContainerNodeAttrBuilder<NodeIdentifier, ContainerNode> b = Builders.containerBuilder(attributes);
+
+        // Create a new CLUSTER_LIST builder
+        final ListNodeBuilder<Object, LeafSetEntryNode<Object>> clb = Builders.leafSetBuilder();
+        clb.withNodeIdentifier(clusterListLeaf);
+
+        // prepend local CLUSTER_ID
+        clb.withChild(Builders.leafSetEntryBuilder().withValue(clusterId).build());
+
+        // if there was a CLUSTER_LIST attribute, add all other entries
+        final Optional<NormalizedNode<?, ?>> maybeClusterList = NormalizedNodes.findNode(attributes, clusterListPath);
+        if (maybeClusterList.isPresent()) {
+            final NormalizedNode<?, ?> clusterList = maybeClusterList.get();
+            if (clusterList instanceof LeafSetNode) {
+                // FIXME: can we get rid of this cast?
+                for (LeafSetEntryNode<Object> e : ((LeafSetNode<Object>)clusterList).getValue()) {
+                    clb.addChild(e);
+                }
+            } else {
+                LOG.warn("Ignoring malformed CLUSTER_LIST {}", clusterList);
+            }
+        } else {
+            LOG.debug("Creating fresh CLUSTER_LIST attribute");
+        }
+
+        // Now wrap it in a container and add it to attributes
+        final DataContainerNodeAttrBuilder<NodeIdentifier, ContainerNode> cb = Builders.containerBuilder();
+        cb.withNodeIdentifier(clusterListContainer);
+        cb.withChild(clb.build());
+        b.withChild(cb.build());
+
+        // add ORIGINATOR_ID if not present
+        final Optional<NormalizedNode<?, ?>> maybeOriginatorId = NormalizedNodes.findNode(attributes, originatorIdPath);
+        if (!maybeOriginatorId.isPresent()) {
+            final DataContainerNodeAttrBuilder<NodeIdentifier, ContainerNode> oib = Builders.containerBuilder();
+            oib.withNodeIdentifier(originatorIdContainer);
+            oib.withChild(ImmutableNodes.leafNode(originatorIdLeaf, originatorId.getValue()));
+            b.withChild(oib.build());
+        }
+
+        return b.build();
+    }
+
+    private boolean isTransitiveAttribute(final DataContainerChild<? extends PathArgument, ?> child) {
+        // FIXME: perform a filtering operation
+        return true;
+    }
+
+    /**
+     * Filter out all non-transitive attributes.
+     *
+     * @param attributes Input attributes
+     * @return Output attributes, transitive only.
+     */
+    ContainerNode transitiveAttributes(final ContainerNode attributes) {
+        final DataContainerNodeAttrBuilder<NodeIdentifier, ContainerNode> b = Builders.containerBuilder();
+        b.withNodeIdentifier(attributes.getIdentifier());
+
+        // We want to reuse attributes as much as possible, so the result of the loop
+        // indicates whether we performed a modification. If we have not modified the
+        // attributes, we can reuse them.
+        boolean modified = false;
+        for (DataContainerChild<? extends PathArgument, ?> child : attributes.getValue()) {
+            if (isTransitiveAttribute(child)) {
+                b.withChild(child);
+            } else {
+                modified = true;
+            }
+        }
+
+        return modified ? b.build() : attributes;
+    }
+
+    LeafSetNode<?> getClusterList(final ContainerNode attributes) {
+        final Optional<NormalizedNode<?, ?>> maybeClusterList = NormalizedNodes.findNode(attributes, clusterListPath);
+        if (maybeClusterList.isPresent()) {
+            final NormalizedNode<?, ?> clusterList = maybeClusterList.get();
+            if (clusterList instanceof LeafSetNode) {
+                return (LeafSetNode<?>) clusterList;
+            }
+
+            LOG.warn("Unexpected CLUSTER_LIST node {}, ignoring it", clusterList);
+        }
+
+        return null;
+    }
+
+    Object getOriginatorId(final ContainerNode attributes) {
+        final Optional<NormalizedNode<?, ?>> maybeOriginatorId = NormalizedNodes.findNode(attributes, originatorIdPath);
+        if (!maybeOriginatorId.isPresent()) {
+            LOG.debug("No ORIGINATOR_ID present");
+            return null;
+        }
+
+        final NormalizedNode<?, ?> originatorId = maybeOriginatorId.get();
+        if (originatorId instanceof LeafNode) {
+            return ((LeafNode<?>)originatorId).getValue();
+        }
+
+        LOG.warn("Unexpected ORIGINATOR_ID node {}, ignoring it", originatorId);
+        return null;
+    }
+
+}
