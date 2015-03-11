3b56bbf0ce48feadb81902bce9ce6aa7e8c99e0d
Robert Varga <rovarga@cisco.com>
2015-03-11 12:26:58 +0100

    BUG-2383: split out policies into separate files
    Static policies kwill not work, as some of them require additional
    arguments to perform their role. Split them out, so they can be evolved
    independently.
    
    Change-Id: I33decf294c53391d4d705014adfd62b6d5ffa34d
    Signed-off-by: Robert Varga <rovarga@cisco.com>

---
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/FromInternalImportPolicy.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl;
+
+import com.google.common.base.Preconditions;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.ClusterIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.LeafSetNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Invoked on routes which we get from our normal home AS peers.
+ */
+class FromInternalImportPolicy extends AbstractImportPolicy {
+    private static final Logger LOG = LoggerFactory.getLogger(FromInternalImportPolicy.class);
+    private final ClusterIdentifier clusterIdentifier;
+    private final Ipv4Address bgpIdentifier;
+
+    FromInternalImportPolicy(final Ipv4Address bgpIdentifier, final ClusterIdentifier clusterIdentifier) {
+        this.bgpIdentifier = Preconditions.checkNotNull(bgpIdentifier);
+        this.clusterIdentifier = Preconditions.checkNotNull(clusterIdentifier);
+    }
+
+    @Override
+    ContainerNode effectiveAttributes(final ContainerNode attributes) {
+        final AttributeOperations oper = AttributeOperations.getInstance(attributes);
+
+        /*
+         * This is an implementation of https://tools.ietf.org/html/rfc4456#section-8
+         *
+         * We first check the ORIGINATOR_ID, if present. If it matches our BGP identifier,
+         * we filter the route.
+         */
+        final Object originatorId = oper.getOriginatorId(attributes);
+        if (bgpIdentifier.getValue().equals(originatorId)) {
+            LOG.debug("Filtering route with our ORIGINATOR_ID {}", bgpIdentifier);
+            return null;
+        }
+
+        /*
+         * Second we check CLUSTER_LIST, if present. If it contains our CLUSTER_ID, we issue
+         * a warning and ignore the route.
+         */
+        final LeafSetNode<?> clusterList = oper.getClusterList(attributes);
+        if (clusterList != null) {
+            for (LeafSetEntryNode<?> node : clusterList.getValue()) {
+                if (clusterIdentifier.getValue().equals(node.getValue())) {
+                    LOG.info("Received a route with our CLUSTER_ID {} in CLUSTER_LIST {}, filtering it", clusterIdentifier.getValue(), clusterList);
+                    return null;
+                }
+            }
+        }
+
+        return attributes;
+    }
+}
---
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/ToExternalExportPolicy.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl;
+
+import com.google.common.base.Preconditions;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.PeerRole;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+/**
+ * Invoked on routes which we send outside of our home AS.
+ */
+final class ToExternalExportPolicy extends AbstractExportPolicy {
+    private final Long localAs;
+
+    ToExternalExportPolicy(final Long localAs) {
+        this.localAs = Preconditions.checkNotNull(localAs);
+    }
+
+    @Override
+    ContainerNode effectiveAttributes(final PeerRole sourceRole, final ContainerNode attributes) {
+        final ContainerNode ret = AttributeOperations.getInstance(attributes).exportedAttributes(attributes, localAs);
+
+        switch (sourceRole) {
+        case Ebgp:
+            // eBGP -> eBGP, propagate
+            return ret;
+        case Ibgp:
+            // Non-Client iBGP -> eBGP, propagate
+            return ret;
+        case RrClient:
+            // Client iBGP -> eBGP, propagate
+            return ret;
+        default:
+            throw new IllegalArgumentException("Unhandled source role " + sourceRole);
+        }
+    }
+}
