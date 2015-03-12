313c7b05da66b425249bc97beac28c9e087d21be
Robert Varga <rovarga@cisco.com>
2015-03-12 20:13:43 +0100

    BUG-2383: wire up AdjRibInWriter
    Modify AdjRibInWriter lifecycle to fit with BGPPeer actions. Also make
    sure we pick up the transaction chain from RIB.
    
    Change-Id: Ide7b7bf3073d5a106608d555577535a079ed2b37
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibInWriter.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibInWriter.java
@@ -15,15 +15,22 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import javax.annotation.Nonnull;
 import javax.annotation.concurrent.NotThreadSafe;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
 import org.opendaylight.controller.md.sal.dom.api.DOMTransactionChain;
 import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionConsumerContext;
 import org.opendaylight.protocol.bgp.rib.spi.RIBSupport;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.PathAttributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.update.path.attributes.MpReachNlri;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.update.path.attributes.MpUnreachNlri;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.BgpRib;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.PeerRole;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.bgp.rib.Rib;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.bgp.rib.RibKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.bgp.rib.rib.Peer;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.bgp.rib.rib.peer.AdjRibIn;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.Tables;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.TablesKey;
@@ -36,8 +43,10 @@ import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdent
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
 import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
 import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.api.DataContainerNodeBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,36 +58,67 @@ import org.slf4j.LoggerFactory;
 final class AdjRibInWriter {
     private static final Logger LOG = LoggerFactory.getLogger(AdjRibInWriter.class);
 
-    // FIXME: is there a utility method to construct this?
-    private static final ContainerNode EMPTY_ADJRIBIN = Builders.containerBuilder().withNodeIdentifier(new NodeIdentifier(AdjRibIn.QNAME)).addChild(ImmutableNodes.mapNodeBuilder(Tables.QNAME).build()).build();
     private static final LeafNode<Boolean> ATTRIBUTES_UPTODATE_FALSE = ImmutableNodes.leafNode(QName.create(Attributes.QNAME, "uptodate"), Boolean.FALSE);
     private static final LeafNode<Boolean> ATTRIBUTES_UPTODATE_TRUE = ImmutableNodes.leafNode(ATTRIBUTES_UPTODATE_FALSE.getNodeType(), Boolean.TRUE);
-    private static final QName AFI_QNAME = QName.create(Tables.QNAME, "afi");
-    private static final QName SAFI_QNAME = QName.create(Tables.QNAME, "safi");
+    private static final QName RIB_ID_QNAME = QName.cachedReference(QName.create(Rib.QNAME, "id"));
+    private static final QName AFI_QNAME = QName.cachedReference(QName.create(Tables.QNAME, "afi"));
+    private static final QName SAFI_QNAME = QName.cachedReference(QName.create(Tables.QNAME, "safi"));
+    private static final QName PEER_ID_QNAME = QName.cachedReference(QName.create(Peer.QNAME, "peer-id"));
+    private static final QName PEER_ROLE_QNAME = QName.cachedReference(QName.create(Peer.QNAME, "peer-role"));
+    private static final NodeIdentifier ADJRIBIN = new NodeIdentifier(AdjRibIn.QNAME);
+    private static final NodeIdentifier PEER_ID = new NodeIdentifier(PEER_ID_QNAME);
+    private static final NodeIdentifier PEER_ROLE = new NodeIdentifier(PEER_ROLE_QNAME);
+    private static final NodeIdentifier TABLES = new NodeIdentifier(Tables.QNAME);
+
+    // FIXME: is there a utility method to construct this?
+    private static final ContainerNode EMPTY_ADJRIBIN = Builders.containerBuilder().withNodeIdentifier(ADJRIBIN).addChild(ImmutableNodes.mapNodeBuilder(Tables.QNAME).build()).build();
 
     private final Map<TablesKey, TableContext> tables;
-    private final YangInstanceIdentifier adjRibInRoot;
+    private final YangInstanceIdentifier tablesRoot;
+    private final YangInstanceIdentifier ribPath;
     private final DOMTransactionChain chain;
+    private final Ipv4Address peerId = null;
+    private final String role;
 
     /*
      * FIXME: transaction chain has to be instantiated in caller, so it can terminate us when it fails.
      */
-    private AdjRibInWriter(final DOMTransactionChain chain, final YangInstanceIdentifier adjRibInRoot, final Map<TablesKey, TableContext> tables) {
+    private AdjRibInWriter(final YangInstanceIdentifier ribPath, final DOMTransactionChain chain, final String role, final YangInstanceIdentifier tablesRoot, final Map<TablesKey, TableContext> tables) {
+        this.ribPath = Preconditions.checkNotNull(ribPath);
         this.chain = Preconditions.checkNotNull(chain);
-        this.adjRibInRoot = Preconditions.checkNotNull(adjRibInRoot);
         this.tables = Preconditions.checkNotNull(tables);
+        this.role = Preconditions.checkNotNull(role);
+        this.tablesRoot = tablesRoot;
     }
 
-    static AdjRibInWriter create(final DOMTransactionChain chain, final YangInstanceIdentifier peer) {
-        // Not used often, no need to optimize it via builder
-        final YangInstanceIdentifier adjRibInRoot = peer.node(EMPTY_ADJRIBIN.getIdentifier());
+    // We could use a codec, but this should be fine, too
+    private static String roleString(final PeerRole role) {
+        switch (role) {
+        case Ebgp:
+            return "ebgp";
+        case Ibgp:
+            return "ibgp";
+        case RrClient:
+            return "rr-client";
+        default:
+            throw new IllegalArgumentException("Unhandled role " + role);
+        }
+    }
 
-        // Create top-level AdjRibIn with an empty table list
-        final DOMDataWriteTransaction tx = chain.newWriteOnlyTransaction();
-        tx.put(LogicalDatastoreType.OPERATIONAL, adjRibInRoot, EMPTY_ADJRIBIN);
-        tx.submit();
+    /**
+     * Create a new writer using a transaction chain.
+     *
+     * @param role peer's role
+     * @param chain transaction chain
+     * @return A fresh writer instance
+     */
+    static AdjRibInWriter create(@Nonnull final RibKey key, @Nonnull final PeerRole role, @Nonnull final DOMTransactionChain chain) {
+        final InstanceIdentifierBuilder b = YangInstanceIdentifier.builder();
+        b.node(BgpRib.QNAME);
+        b.node(Rib.QNAME);
+        b.nodeWithKey(Rib.QNAME, RIB_ID_QNAME, key.getId().getValue());
 
-        return new AdjRibInWriter(chain, adjRibInRoot, Collections.<TablesKey, TableContext>emptyMap());
+        return new AdjRibInWriter(b.build(), chain, roleString(role), null, Collections.<TablesKey, TableContext>emptyMap());
     }
 
     /**
@@ -86,23 +126,46 @@ final class AdjRibInWriter {
      * Empty tables are created for new entries and old tables are deleted. Once this
      * method returns, the old instance must not be reasonably used.
      *
+     * @param newPeerId new peer BGP identifier
+     * @param registry RIB extension registry
      * @param tableTypes New tables, must not be null
      * @return New writer
      */
-    AdjRibInWriter changeTableTypes(final RIBExtensionConsumerContext registry, final Set<TablesKey> tableTypes) {
-        if (tableTypes.equals(tables.keySet())) {
-            return this;
-        }
-
+    AdjRibInWriter transform(final Ipv4Address newPeerId, final RIBExtensionConsumerContext registry, final Set<TablesKey> tableTypes) {
         final DOMDataWriteTransaction tx = chain.newWriteOnlyTransaction();
 
-        // Wipe tables which are not present in the new types
-        for (Entry<TablesKey, TableContext> e : tables.entrySet()) {
-            if (!tableTypes.contains(e.getKey())) {
-                e.getValue().removeTable(tx);
+        final YangInstanceIdentifier newTablesRoot;
+        if (!newPeerId.equals(this.peerId)) {
+            if (peerId != null) {
+                // Wipe old peer data completely
+                tx.delete(LogicalDatastoreType.OPERATIONAL, ribPath.node(Peer.QNAME).node(new NodeIdentifierWithPredicates(Peer.QNAME, PEER_ID_QNAME, peerId.getValue())));
+            }
+
+            // Install new empty peer structure
+            final NodeIdentifierWithPredicates peerKey = new NodeIdentifierWithPredicates(Peer.QNAME, PEER_ID_QNAME, newPeerId.getValue());
+            final YangInstanceIdentifier newPeerPath = ribPath.node(Peer.QNAME).node(peerKey);
+
+            final DataContainerNodeBuilder<NodeIdentifierWithPredicates, MapEntryNode> pb = Builders.mapEntryBuilder();
+            pb.withNodeIdentifier(peerKey);
+            pb.withChild(ImmutableNodes.leafNode(PEER_ID, newPeerId.getValue()));
+            pb.withChild(ImmutableNodes.leafNode(PEER_ROLE, role));
+            pb.withChild(EMPTY_ADJRIBIN);
+
+            tx.put(LogicalDatastoreType.OPERATIONAL, newPeerPath, pb.build());
+
+            newTablesRoot = newPeerPath.node(EMPTY_ADJRIBIN.getIdentifier()).node(TABLES);
+        } else {
+            newTablesRoot = tablesRoot;
+
+            // Wipe tables which are not present in the new types
+            for (Entry<TablesKey, TableContext> e : tables.entrySet()) {
+                if (!tableTypes.contains(e.getKey())) {
+                    e.getValue().removeTable(tx);
+                }
             }
         }
 
+        // Now create new table instances, potentially creating their empty entries
         final Builder<TablesKey, TableContext> tb = ImmutableMap.builder();
         for (TablesKey k : tableTypes) {
             TableContext ctx = tables.get(k);
@@ -114,7 +177,7 @@ final class AdjRibInWriter {
                 }
 
                 // We will use table keys very often, make sure they are optimized
-                final InstanceIdentifierBuilder idb = YangInstanceIdentifier.builder(adjRibInRoot);
+                final InstanceIdentifierBuilder idb = YangInstanceIdentifier.builder(newTablesRoot);
 
                 // FIXME: use codec to translate the key
                 final Map<QName, Object> keyValues = ImmutableMap.<QName, Object>of(AFI_QNAME, BindingReflections.getQName(k.getAfi()), SAFI_QNAME, BindingReflections.getQName(k.getSafi()));
@@ -132,7 +195,7 @@ final class AdjRibInWriter {
 
         tx.submit();
 
-        return new AdjRibInWriter(chain, adjRibInRoot, tb.build());
+        return new AdjRibInWriter(ribPath, chain, role, newTablesRoot, tb.build());
     }
 
     /**
