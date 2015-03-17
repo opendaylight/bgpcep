0f353d0573eb2cb18e2af4efaed2e580e4e8e324
Ladislav Borak <lborak@cisco.com>
2015-03-17 15:03:04 +0100

    SR Update
    - added abstract class
    Signed-off-by: Ladislav Borak <lborak@cisco.com>
    Change-Id: I1fa997355ba056d8d2a8b31d3008a297c5783993

--- pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/SrEroTypeSrSubobjectParser.java
+++ pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/SrEroTypeSrSubobjectParser.java
@@ -7,8 +7,6 @@
  */
 package org.opendaylight.protocol.pcep.segment.routing;
 
-import static org.opendaylight.protocol.pcep.segment.routing.SrSubobjectParserUtil.BITSET_LENGTH;
-
 import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import io.netty.buffer.ByteBuf;
@@ -22,15 +20,9 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.seg
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.explicit.route.object.ero.Subobject;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.explicit.route.object.ero.SubobjectBuilder;
 
-public class SrEroTypeSrSubobjectParser implements EROSubobjectParser, EROSubobjectSerializer {
+public class SrEroTypeSrSubobjectParser extends AbstractSrSubobjectParser implements EROSubobjectParser, EROSubobjectSerializer {
 
     public static final int TYPE = 5;
-
-    private static final int M_FLAG_POSITION = 7;
-    private static final int C_FLAG_POSITION = 6;
-    private static final int S_FLAG_POSITION = 5;
-    private static final int F_FLAG_POSITION = 4;
-    private static final int MPLS_LABEL_OFFSET = 12;
 
     @Override
     public void serializeSubobject(final Subobject subobject, final ByteBuf buffer) {
@@ -39,20 +31,7 @@ public class SrEroTypeSrSubobjectParser implements EROSubobjectParser, EROSubobj
                         .getClass());
 
         final SrSubobject srSubobject = (SrSubobject) subobject.getSubobjectType();
-        final SrEroTypeBuilder srEroTypeBuilder = new SrEroTypeBuilder(srSubobject);
-        if (srSubobject.isMFlag() != null && srSubobject.isMFlag() && srSubobject.getSid() != null) {
-            srEroTypeBuilder.setSid(srSubobject.getSid() << MPLS_LABEL_OFFSET);
-        }
-        final BitArray bits = new BitArray(BITSET_LENGTH);
-        bits.set(M_FLAG_POSITION, srSubobject.isMFlag());
-        bits.set(C_FLAG_POSITION, srSubobject.isCFlag());
-        if (srSubobject.getSid() == null) {
-            bits.set(S_FLAG_POSITION, Boolean.TRUE);
-        }
-        if (srSubobject.getNai() == null) {
-            bits.set(F_FLAG_POSITION, Boolean.TRUE);
-        }
-        final ByteBuf body = SrSubobjectParserUtil.serializeSrSubobject(srEroTypeBuilder.build(), bits);
+        final ByteBuf body = serializeSubobject(srSubobject);
         EROSubobjectUtil.formatSubobject(TYPE, subobject.isLoose(), body, buffer);
     }
 
@@ -64,7 +43,7 @@ public class SrEroTypeSrSubobjectParser implements EROSubobjectParser, EROSubobj
             throw new PCEPDeserializerException("Wrong length of array of bytes. Passed: " + buffer.readableBytes() + ";");
         }
         final BitArray flags = new BitArray(BITSET_LENGTH);
-        final SrSubobject srSubobject = SrSubobjectParserUtil.parseSrSubobject(buffer, new Function<BitArray, Void>() {
+        final SrSubobject srSubobject = parseSrSubobject(buffer, new Function<BitArray, Void>() {
             @Override
             public Void apply(final BitArray input) {
                 flags.set(C_FLAG_POSITION, input.get(C_FLAG_POSITION));
@@ -73,9 +52,9 @@ public class SrEroTypeSrSubobjectParser implements EROSubobjectParser, EROSubobj
             }
         }, F_FLAG_POSITION, S_FLAG_POSITION, C_FLAG_POSITION, M_FLAG_POSITION);
         final SrEroTypeBuilder srEroTypeBuilder = new SrEroTypeBuilder(srSubobject);
-        srEroTypeBuilder.setCFlag(flags.get(C_FLAG_POSITION));
-        srEroTypeBuilder.setMFlag(flags.get(M_FLAG_POSITION));
-        if (srEroTypeBuilder.isMFlag() != null && srEroTypeBuilder.isMFlag() && srEroTypeBuilder.getSid() != null) {
+        srEroTypeBuilder.setCFlag(srSubobject.isCFlag());
+        srEroTypeBuilder.setMFlag(srSubobject.isMFlag());
+        if (srEroTypeBuilder.isCFlag() && srEroTypeBuilder.isMFlag() && srEroTypeBuilder.getSid() != null) {
             srEroTypeBuilder.setSid(srEroTypeBuilder.getSid() >> MPLS_LABEL_OFFSET);
         }
         final SubobjectBuilder subobjectBuilder = new SubobjectBuilder();
@@ -83,5 +62,4 @@ public class SrEroTypeSrSubobjectParser implements EROSubobjectParser, EROSubobj
         subobjectBuilder.setSubobjectType(srEroTypeBuilder.build());
         return subobjectBuilder.build();
     }
-
 }
--- pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/SrRroTypeSrSubobjectParser.java
+++ pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/SrRroTypeSrSubobjectParser.java
@@ -8,9 +8,6 @@
 
 package org.opendaylight.protocol.pcep.segment.routing;
 
-import static org.opendaylight.protocol.pcep.segment.routing.SrSubobjectParserUtil.BITSET_LENGTH;
-import static org.opendaylight.protocol.pcep.segment.routing.SrSubobjectParserUtil.MINIMAL_LENGTH;
-
 import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import io.netty.buffer.ByteBuf;
@@ -24,37 +21,17 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.seg
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.reported.route.object.rro.Subobject;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.reported.route.object.rro.SubobjectBuilder;
 
-public class SrRroTypeSrSubobjectParser implements RROSubobjectParser, RROSubobjectSerializer {
+public class SrRroTypeSrSubobjectParser extends AbstractSrSubobjectParser implements RROSubobjectParser, RROSubobjectSerializer {
 
     public static final int TYPE = 6;
 
-    private static final int M_FLAG_POSITION = 7;
-    private static final int C_FLAG_POSITION = 6;
-    private static final int S_FLAG_POSITION = 5;
-    private static final int F_FLAG_POSITION = 4;
-    private static final int MPLS_LABEL_OFFSET = 12;
-
     @Override
-    public void serializeSubobject(final Subobject subobject, final ByteBuf buffer) {
+    public void serializeSubobject(Subobject subobject, ByteBuf buffer) {
         Preconditions.checkArgument(subobject.getSubobjectType() instanceof SrSubobject,
                 "Unknown subobject instance. Passed %s. Needed SrSubobject.", subobject.getSubobjectType()
                         .getClass());
-
         final SrSubobject srSubobject = (SrSubobject) subobject.getSubobjectType();
-        final SrRroTypeBuilder builder = new SrRroTypeBuilder(srSubobject);
-        if (srSubobject.isCFlag() != null && srSubobject.isMFlag() && srSubobject.getSid() != null) {
-            builder.setSid(srSubobject.getSid() << MPLS_LABEL_OFFSET);
-        }
-        final BitArray bits = new BitArray(BITSET_LENGTH);
-        bits.set(M_FLAG_POSITION, srSubobject.isMFlag());
-        bits.set(C_FLAG_POSITION, srSubobject.isCFlag());
-        if (srSubobject.getSid() == null) {
-            bits.set(S_FLAG_POSITION, Boolean.TRUE);
-        }
-        if (srSubobject.getNai() == null) {
-            bits.set(F_FLAG_POSITION, Boolean.TRUE);
-        }
-        final ByteBuf body = SrSubobjectParserUtil.serializeSrSubobject(builder.build(), bits);
+        final ByteBuf body = serializeSubobject(srSubobject);
         RROSubobjectUtil.formatSubobject(TYPE, body, buffer);
     }
 
@@ -66,7 +43,7 @@ public class SrRroTypeSrSubobjectParser implements RROSubobjectParser, RROSubobj
             throw new PCEPDeserializerException("Wrong length of array of bytes. Passed: " + buffer.readableBytes() + ";");
         }
         final BitArray flags = new BitArray(BITSET_LENGTH);
-        final SrSubobject srSubobject = SrSubobjectParserUtil.parseSrSubobject(buffer, new Function<BitArray, Void>() {
+        final SrSubobject srSubobject = parseSrSubobject(buffer, new Function<BitArray, Void>() {
             @Override
             public Void apply(final BitArray input) {
                 flags.set(C_FLAG_POSITION, input.get(C_FLAG_POSITION));
@@ -75,13 +52,14 @@ public class SrRroTypeSrSubobjectParser implements RROSubobjectParser, RROSubobj
             }
         }, F_FLAG_POSITION, S_FLAG_POSITION, C_FLAG_POSITION, M_FLAG_POSITION);
         final SrRroTypeBuilder srRroTypeBuilder = new SrRroTypeBuilder(srSubobject);
-        srRroTypeBuilder.setCFlag(flags.get(C_FLAG_POSITION));
-        srRroTypeBuilder.setMFlag(flags.get(M_FLAG_POSITION));
-        if (srRroTypeBuilder.isMFlag() != null && srRroTypeBuilder.isMFlag() && srRroTypeBuilder.getSid() != null) {
+        srRroTypeBuilder.setCFlag(srSubobject.isCFlag());
+        srRroTypeBuilder.setMFlag(srSubobject.isMFlag());
+        if (srRroTypeBuilder.isCFlag() && srRroTypeBuilder.isMFlag() && srRroTypeBuilder.getSid() != null) {
             srRroTypeBuilder.setSid(srRroTypeBuilder.getSid() >> MPLS_LABEL_OFFSET);
         }
         final SubobjectBuilder subobjectBuilder = new SubobjectBuilder();
         subobjectBuilder.setSubobjectType(srRroTypeBuilder.build());
         return subobjectBuilder.build();
     }
+
 }
