f8a918d6d242af64ae38970cb1616da8312e6ced
Ladislav Borak <lborak@cisco.com>
2015-03-23 14:00:10 +0100

    Bug 2803 - Update SR
    Change-Id: I40f9fe9c80176a39f32b24c9387994bdc57ef476
    Signed-off-by: Ladislav Borak <lborak@cisco.com>

--- pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/SegmentRoutingActivator.java
+++ pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/SegmentRoutingActivator.java
@@ -10,11 +10,27 @@ package org.opendaylight.protocol.pcep.segment.routing;
 
 import com.google.common.collect.Lists;
 import java.util.List;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.AddLspSrEroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.AddLspSrRroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.PcinitiateMessageSrEroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.PcinitiateMessageSrRroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.PcrptMessageSrEroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.PcrptMessageSrRroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.PcupdMessageSrEroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.PcupdMessageSrRroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.RepliesFailureMessageSrRroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.RepliesSuccessMessageSrEroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.RepliesSuccessMessageSrRroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.ReportedLspSrEroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.ReportedLspSrRroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.RequestMessageRROSrRroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.RequestMessageReportedRouteSrRroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.UpdateLspSrEroSubobjectParser;
+import org.opendaylight.protocol.pcep.segment.routing.parsers.UpdateLspSrRroSubobjectParser;
 import org.opendaylight.protocol.pcep.spi.PCEPExtensionProviderContext;
 import org.opendaylight.protocol.pcep.spi.TlvRegistry;
 import org.opendaylight.protocol.pcep.spi.VendorInformationTlvRegistry;
 import org.opendaylight.protocol.pcep.spi.pojo.AbstractPCEPExtensionProviderActivator;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev150112.add.lsp.input.arguments.ero.subobject.subobject.type.SrEroType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev150112.add.lsp.input.arguments.rro.subobject.subobject.type.SrRroType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev150112.sr.pce.capability.tlv.SrPceCapability;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.open.object.Open;
@@ -34,9 +50,29 @@ public class SegmentRoutingActivator extends AbstractPCEPExtensionProviderActiva
         regs.add(context.registerTlvSerializer(SrPceCapability.class, new SrPceCapabilityTlvParser()));
 
         /* Subobjects */
-        regs.add(context.registerEROSubobjectParser(SrEroSubobjectParser.TYPE, new SrEroSubobjectParser()));
-        regs.add(context.registerEROSubobjectSerializer(SrEroType.class, new SrEroSubobjectParser()));
-        regs.add(context.registerRROSubobjectParser(SrRroSubobjectParser.TYPE, new SrRroSubobjectParser()));
+
+        /* Ero-type */
+        regs.add(context.registerEROSubobjectParser(RepliesSuccessMessageSrEroSubobjectParser.TYPE, new RepliesSuccessMessageSrEroSubobjectParser()));
+        regs.add(context.registerEROSubobjectParser(PcinitiateMessageSrEroSubobjectParser.TYPE, new PcinitiateMessageSrEroSubobjectParser()));
+        regs.add(context.registerEROSubobjectParser(PcrptMessageSrEroSubobjectParser.TYPE, new PcrptMessageSrEroSubobjectParser()));
+        regs.add(context.registerEROSubobjectParser(PcupdMessageSrEroSubobjectParser.TYPE, new PcupdMessageSrEroSubobjectParser()));
+        regs.add(context.registerEROSubobjectParser(AddLspSrEroSubobjectParser.TYPE, new AddLspSrEroSubobjectParser()));
+        regs.add(context.registerEROSubobjectParser(UpdateLspSrEroSubobjectParser.TYPE, new UpdateLspSrEroSubobjectParser()));
+        regs.add(context.registerEROSubobjectParser(ReportedLspSrEroSubobjectParser.TYPE, new ReportedLspSrEroSubobjectParser()));
+
+        /* Rro-type */
+        regs.add(context.registerRROSubobjectParser(RequestMessageReportedRouteSrRroSubobjectParser.TYPE, new RequestMessageReportedRouteSrRroSubobjectParser()));
+        regs.add(context.registerRROSubobjectParser(RequestMessageRROSrRroSubobjectParser.TYPE, new RequestMessageRROSrRroSubobjectParser()));
+        regs.add(context.registerRROSubobjectParser(RepliesFailureMessageSrRroSubobjectParser.TYPE, new RepliesFailureMessageSrRroSubobjectParser()));
+        regs.add(context.registerRROSubobjectParser(RepliesSuccessMessageSrRroSubobjectParser.TYPE, new RepliesSuccessMessageSrRroSubobjectParser()));
+        regs.add(context.registerRROSubobjectParser(PcinitiateMessageSrRroSubobjectParser.TYPE, new PcinitiateMessageSrRroSubobjectParser()));
+        regs.add(context.registerRROSubobjectParser(PcrptMessageSrRroSubobjectParser.TYPE, new PcrptMessageSrRroSubobjectParser()));
+        regs.add(context.registerRROSubobjectParser(PcupdMessageSrRroSubobjectParser.TYPE, new PcupdMessageSrRroSubobjectParser()));
+        regs.add(context.registerRROSubobjectParser(AddLspSrRroSubobjectParser.TYPE, new AddLspSrRroSubobjectParser()));
+        regs.add(context.registerRROSubobjectParser(UpdateLspSrRroSubobjectParser.TYPE, new UpdateLspSrRroSubobjectParser()));
+        regs.add(context.registerRROSubobjectParser(ReportedLspSrRroSubobjectParser.TYPE, new ReportedLspSrRroSubobjectParser()));
+
+        /* serializers for Ero-type and Rro-type */
         regs.add(context.registerRROSubobjectSerializer(SrRroType.class, new SrRroSubobjectParser()));
 
         /* Objects */
