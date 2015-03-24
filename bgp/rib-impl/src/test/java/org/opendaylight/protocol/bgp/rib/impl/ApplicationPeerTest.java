57817fc20ba8004bc7bf7581bef7f3b1dd2725e3
Dana Kutenicsova <dkutenic@cisco.com>
2015-03-24 11:24:59 +0100

    Added wiring of Attributes codecs to RibSupportContext
    Provided code which adds resolution of Attributes codec
    to RibSupportContext, which is shared context between
    tables of same type for one instance of BGPRibImpl.
    
    Codecs are still not loaded since we are missing
    access to SchemaService in runtime.
    
    Change-Id: I43ee02c7cbab1cc9c20d659bf83454e04cf07cf6
    Signed-off-by: Tony Tkacik <ttkacik@cisco.com>
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- bgp/rib-impl/src/test/java/org/opendaylight/protocol/bgp/rib/impl/ApplicationPeerTest.java
+++ bgp/rib-impl/src/test/java/org/opendaylight/protocol/bgp/rib/impl/ApplicationPeerTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.CheckedFuture;
 import io.netty.channel.Channel;
@@ -29,6 +30,7 @@ import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
+import javassist.ClassPool;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -112,11 +114,20 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.type
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.UnicastSubsequentAddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.concepts.rev131125.IsoSystemIdentifier;
 import org.opendaylight.yangtools.binding.data.codec.api.BindingCodecTreeFactory;
+import org.opendaylight.yangtools.binding.data.codec.gen.impl.DataObjectSerializerGenerator;
+import org.opendaylight.yangtools.binding.data.codec.gen.impl.StreamWriterGenerator;
+import org.opendaylight.yangtools.binding.data.codec.impl.BindingNormalizedNodeCodecRegistry;
+import org.opendaylight.yangtools.sal.binding.generator.api.ClassLoadingStrategy;
+import org.opendaylight.yangtools.sal.binding.generator.impl.ModuleInfoBackedContext;
+import org.opendaylight.yangtools.sal.binding.generator.util.BindingRuntimeContext;
+import org.opendaylight.yangtools.sal.binding.generator.util.JavassistUtils;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.binding.Notification;
+import org.opendaylight.yangtools.yang.binding.util.BindingReflections;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaContext;
 
 public class ApplicationPeerTest {
 
@@ -149,7 +160,6 @@ public class ApplicationPeerTest {
     @Mock
     DOMTransactionChain domChain;
 
-    @Mock
     BindingCodecTreeFactory codecFactory;
 
     @Mock
@@ -189,6 +199,9 @@ public class ApplicationPeerTest {
     @Before
     public void setUp() throws InterruptedException, ExecutionException, ReadFailedException {
         MockitoAnnotations.initMocks(this);
+        ModuleInfoBackedContext strategy = createClassLoadingStrategy();
+        SchemaContext schemaContext = strategy.tryToCreateSchemaContext().get();
+        codecFactory = createCodecFactory(strategy,schemaContext);
         final List<BgpTableType> localTables = new ArrayList<>();
         this.routes = new ArrayList<>();
         localTables.add(new BgpTableTypeImpl(Ipv4AddressFamily.class, UnicastSubsequentAddressFamily.class));
@@ -233,11 +246,31 @@ public class ApplicationPeerTest {
         this.r = new RIBImpl(new RibId("test"), new AsNumber(5L), new Ipv4Address("127.0.0.1"),
             context , this.dispatcher, this.tcpStrategyFactory, this.codecFactory, this.tcpStrategyFactory, this.dps, this.dom, localTables);
         this.peer = new ApplicationPeer(new ApplicationRibId("t"), new Ipv4Address("127.0.0.1"), this.r);
+        this.r.onGlobalContextUpdated(schemaContext);
         final ReadOnlyTransaction readTx = Mockito.mock(ReadOnlyTransaction.class);
         Mockito.doReturn(readTx).when(this.dps).newReadOnlyTransaction();
         final CheckedFuture<Optional<DataObject>, ReadFailedException> readFuture = Mockito.mock(CheckedFuture.class);
         Mockito.doReturn(Optional.<DataObject>absent()).when(readFuture).checkedGet();
         Mockito.doReturn(readFuture).when(readTx).read(Mockito.eq(LogicalDatastoreType.OPERATIONAL), Mockito.any(InstanceIdentifier.class));
+    }
+
+    private BindingCodecTreeFactory createCodecFactory(ClassLoadingStrategy str, SchemaContext ctx) {
+        DataObjectSerializerGenerator generator = StreamWriterGenerator.create(JavassistUtils.forClassPool(ClassPool.getDefault()));
+        BindingNormalizedNodeCodecRegistry codec = new BindingNormalizedNodeCodecRegistry(generator);
+        codec.onBindingRuntimeContextUpdated(BindingRuntimeContext.create(str, ctx));
+        return codec;
+    }
+
+    private ModuleInfoBackedContext createClassLoadingStrategy() {
+        ModuleInfoBackedContext ctx = ModuleInfoBackedContext.create();
+
+        try {
+            ctx.registerModuleInfo(BindingReflections.getModuleInfo(LinkstateRoute.class));
+            ctx.registerModuleInfo(BindingReflections.getModuleInfo(Ipv4Route.class));
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+        return ctx;
     }
 
     @After
