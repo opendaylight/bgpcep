bc3730401ba90a5cd31dd8f2501066e4d94de693
Dana Kutenicsova <dkutenic@cisco.com>
2015-03-27 11:13:08 +0100

    BUG-2383 : wire AdjRibOutListener to BGPPeer
    Change-Id: Idcec38e59a2c4b60c291400b6baff6b67f38e0f3
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibOutListener.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibOutListener.java
@@ -8,11 +8,20 @@
 package org.opendaylight.protocol.bgp.rib.impl;
 
 import com.google.common.base.Preconditions;
+import com.google.common.primitives.UnsignedInteger;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.annotation.concurrent.NotThreadSafe;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataTreeChangeListener;
 import org.opendaylight.protocol.bgp.rib.spi.RIBSupport;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.UpdateBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.PeerId;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifierWithPredicates;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.PathArgument;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
 
 /**
@@ -23,23 +32,48 @@ import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
 final class AdjRibOutListener implements DOMDataTreeChangeListener {
     private final ChannelOutputLimiter session;
     private final RIBSupport ribSupport;
+    private final Map<PathArgument, RouteEntry> routeEntries = new HashMap<>();
 
     AdjRibOutListener(final RIBSupport ribSupport, final ChannelOutputLimiter session) {
         this.ribSupport = Preconditions.checkNotNull(ribSupport);
         this.session = Preconditions.checkNotNull(session);
     }
 
-    @Override
-    public void onDataTreeChanged(final Collection<DataTreeCandidate> changes) {
-        for (DataTreeCandidate tc : changes) {
-            final UpdateBuilder ub = new UpdateBuilder();
-
-            // FIXME: fill the structure
-
-            session.write(ub.build());
-        }
-
-        session.flush();
+    static AdjRibOutListener create(final RIBSupport ribSupport, final ChannelOutputLimiter session) {
+        return new AdjRibOutListener(ribSupport, session);
     }
 
+    @Override
+    public void onDataTreeChanged(final Collection<DataTreeCandidate> changes) {
+        // FIXME: refactor to avoid code duplications
+        final Map<RouteUpdateKey, RouteEntry> toUpdate = new HashMap<>();
+        for (final DataTreeCandidate tc : changes) {
+            final YangInstanceIdentifier path = tc.getRootPath();
+            final PathArgument routeId = path.getLastPathArgument();
+            final NodeIdentifierWithPredicates peerKey = IdentifierUtils.peerKey(path);
+            final PeerId peerId = IdentifierUtils.peerId(peerKey);
+            final UnsignedInteger routerId = RouterIds.routerIdForPeerId(peerId);
+
+            RouteEntry entry = this.routeEntries.get(routeId);
+            if (tc.getRootNode().getDataAfter().isPresent()) {
+                if (entry == null) {
+                    entry = new RouteEntry();
+                    this.routeEntries.put(routeId, entry);
+                }
+                entry.addRoute(routerId, (ContainerNode) tc.getRootNode().getDataAfter().get());
+            } else if (entry != null && entry.removeRoute(routerId)) {
+                this.routeEntries.remove(routeId);
+                entry = null;
+            }
+            toUpdate.put(new RouteUpdateKey(peerId, routeId), entry);
+        }
+        for (final Entry<RouteUpdateKey, RouteEntry> update : toUpdate.entrySet()) {
+            final UpdateBuilder ub = new UpdateBuilder();
+
+            // FIXME: fill the structure (use codecs)
+
+            this.session.write(ub.build());
+        }
+        this.session.flush();
+    }
 }
