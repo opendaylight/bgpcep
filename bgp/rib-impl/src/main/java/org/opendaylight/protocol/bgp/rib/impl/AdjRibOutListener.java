32582045e503701f90f8f9d5a7bd25e0baa94853
Dana Kutenicsova <dkutenic@cisco.com>
2015-03-27 11:13:08 +0100

    BUG-2383 : wire AdjRibOutListener to BGPPeer
    Change-Id: Idcec38e59a2c4b60c291400b6baff6b67f38e0f3
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibOutListener.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibOutListener.java
@@ -14,6 +14,9 @@ import org.opendaylight.controller.md.sal.dom.api.DOMDataTreeChangeListener;
 import org.opendaylight.protocol.bgp.rib.spi.RIBSupport;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.UpdateBuilder;
 import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
+import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidateNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Instantiated for each peer and table, listens on a particular peer's adj-rib-out,
@@ -21,6 +24,9 @@ import org.opendaylight.yangtools.yang.data.api.schema.tree.DataTreeCandidate;
  */
 @NotThreadSafe
 final class AdjRibOutListener implements DOMDataTreeChangeListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AdjRibOutListener.class);
+
     private final ChannelOutputLimiter session;
     private final RIBSupport ribSupport;
 
@@ -29,17 +35,25 @@ final class AdjRibOutListener implements DOMDataTreeChangeListener {
         this.session = Preconditions.checkNotNull(session);
     }
 
-    @Override
-    public void onDataTreeChanged(final Collection<DataTreeCandidate> changes) {
-        for (DataTreeCandidate tc : changes) {
-            final UpdateBuilder ub = new UpdateBuilder();
-
-            // FIXME: fill the structure
-
-            session.write(ub.build());
-        }
-
-        session.flush();
+    static AdjRibOutListener create(final RIBSupport ribSupport, final ChannelOutputLimiter session) {
+        return new AdjRibOutListener(ribSupport, session);
     }
 
+    @Override
+    public void onDataTreeChanged(final Collection<DataTreeCandidate> changes) {
+        LOG.debug("Data change received for AdjRibOut {}", changes);
+        for (final DataTreeCandidate tc : changes) {
+            for (final DataTreeCandidateNode child : tc.getRootNode().getChildNodes()) {
+                for (final DataTreeCandidateNode route : this.ribSupport.changedRoutes(child)) {
+                    LOG.debug("AdjRibOut parsing route {}", route);
+                    final UpdateBuilder ub = new UpdateBuilder();
+
+                    // FIXME: fill the structure (use codecs)
+
+                    this.session.write(ub.build());
+                }
+            }
+        }
+        this.session.flush();
+    }
 }
