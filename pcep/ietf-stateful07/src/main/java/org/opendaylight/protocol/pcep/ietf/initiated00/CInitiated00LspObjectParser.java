bb6ed4ec8fd0986ec2df07ec98e9130b5d3215be
Dana Kutenicsova <dkutenic@cisco.com>
2015-03-28 23:16:53 +0100

    Simplified inheritance within stateful module.
    - extracted parsing and serializing of flags
    into separate methods, so they can be overridden
    
    Change-Id: I0c602fa91d89a2c84e2a5b4be3e8e865c80a410e
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- pcep/ietf-stateful07/src/main/java/org/opendaylight/protocol/pcep/ietf/initiated00/CInitiated00LspObjectParser.java
+++ pcep/ietf-stateful07/src/main/java/org/opendaylight/protocol/pcep/ietf/initiated00/CInitiated00LspObjectParser.java
@@ -7,27 +7,16 @@
  */
 package org.opendaylight.protocol.pcep.ietf.initiated00;
 
-import static org.opendaylight.protocol.util.ByteBufWriteUtil.writeMedium;
-
-import com.google.common.base.Preconditions;
-import com.google.common.primitives.UnsignedBytes;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import org.opendaylight.protocol.pcep.ietf.stateful07.Stateful07LspObjectParser;
-import org.opendaylight.protocol.pcep.spi.ObjectUtil;
-import org.opendaylight.protocol.pcep.spi.PCEPDeserializerException;
 import org.opendaylight.protocol.pcep.spi.TlvRegistry;
 import org.opendaylight.protocol.pcep.spi.VendorInformationTlvRegistry;
 import org.opendaylight.protocol.util.BitArray;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.crabbe.initiated.rev131126.Lsp1;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.crabbe.initiated.rev131126.Lsp1Builder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.OperationalStatus;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.PlspId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.lsp.object.Lsp;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.lsp.object.LspBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.lsp.object.lsp.TlvsBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.Object;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.ObjectHeader;
 
 /**
  * Parser for {@link Lsp}
@@ -41,13 +30,7 @@ public class CInitiated00LspObjectParser extends Stateful07LspObjectParser {
     }
 
     @Override
-    public Lsp parseObject(final ObjectHeader header, final ByteBuf bytes) throws PCEPDeserializerException {
-        Preconditions.checkArgument(bytes != null && bytes.isReadable(), "Array of bytes is mandatory. Can't be null or empty.");
-        final LspBuilder builder = new LspBuilder();
-        builder.setIgnore(header.isIgnore());
-        builder.setProcessingRule(header.isProcessingRule());
-        final int[] plspIdRaw = new int[] { bytes.readUnsignedByte(), bytes.readUnsignedByte(), bytes.getUnsignedByte(2), };
-        builder.setPlspId(new PlspId((long) ((plspIdRaw[0] << FLAGS_SIZE) | (plspIdRaw[1] << FOUR_BITS_SHIFT) | (plspIdRaw[2] >> FOUR_BITS_SHIFT))));
+    protected void parseFlags(final LspBuilder builder, final ByteBuf bytes) {
         final BitArray flags = BitArray.valueOf(bytes, FLAGS_SIZE);
         builder.setDelegate(flags.get(DELEGATE));
         builder.setSync(flags.get(SYNC));
@@ -59,19 +42,10 @@ public class CInitiated00LspObjectParser extends Stateful07LspObjectParser {
         s |= (flags.get(OPERATIONAL + 1) ? 1 : 0) << 1;
         s |= (flags.get(OPERATIONAL) ? 1 : 0) << 2;
         builder.setOperational(OperationalStatus.forValue(s));
-        final TlvsBuilder b = new TlvsBuilder();
-        parseTlvs(b, bytes.slice());
-        builder.setTlvs(b.build());
-        return builder.build();
     }
 
     @Override
-    public void serializeObject(final Object object, final ByteBuf buffer) {
-        Preconditions.checkArgument(object instanceof Lsp, "Wrong instance of PCEPObject. Passed %s. Needed LspObject.", object.getClass());
-        final Lsp specObj = (Lsp) object;
-        final ByteBuf body = Unpooled.buffer();
-        Preconditions.checkArgument(specObj.getPlspId() != null, "PLSP-ID not present");
-        writeMedium(specObj.getPlspId().getValue().intValue() << FOUR_BITS_SHIFT, body);
+    protected BitArray serializeFlags(final Lsp specObj) {
         final BitArray flags = new BitArray(FLAGS_SIZE);
         flags.set(DELEGATE, specObj.isDelegate());
         flags.set(REMOVE, specObj.isRemove());
@@ -80,15 +54,6 @@ public class CInitiated00LspObjectParser extends Stateful07LspObjectParser {
         if (specObj.getAugmentation(Lsp1.class) != null) {
             flags.set(CREATE_FLAG_OFFSET, specObj.getAugmentation(Lsp1.class).isCreate());
         }
-        byte op = 0;
-        if (specObj.getOperational() != null) {
-            op = UnsignedBytes.checkedCast(specObj.getOperational().getIntValue());
-            op = (byte) (op << FOUR_BITS_SHIFT);
-        }
-        final byte[] res = flags.array();
-        res[res.length -1] = (byte) (res[res.length -1] | op);
-        body.writeByte(res[res.length -1]);
-        serializeTlvs(specObj.getTlvs(), body);
-        ObjectUtil.formatSubobject(TYPE, CLASS, object.isProcessingRule(), object.isIgnore(), body, buffer);
+        return flags;
     }
 }
