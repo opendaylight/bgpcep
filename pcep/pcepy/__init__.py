80a09dbe7864e75922554731585872c2d7d56e40
Vratko Polak <vrpolak@cisco.com>
2015-04-09 13:52:51 +0200

    DO NOT MERGE: Change to test whether verify-python job works.
    This fixes the pep8 violation from previous patch.
    
    Change-Id: Icfdb4037d809071ef1d3e1b7ba90fb73ab4bd72a
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- pcep/pcepy/__init__.py
+++ pcep/pcepy/__init__.py
@@ -6,3 +6,5 @@
 # terms of the Eclipse Public License v1.0 which accompanies this distribution,
 # and is available at http://www.eclipse.org/legal/epl-v10.html
 #
+
+# Correct amount of empty lines.
