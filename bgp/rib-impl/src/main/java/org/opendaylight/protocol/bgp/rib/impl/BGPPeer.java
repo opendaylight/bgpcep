466d4585e52e77c1a9360e20286427e5e8f6142d
Ladislav Borak <lborak@cisco.com>
2015-04-20 13:46:57 +0200

    Bug 2963 - Make BGP peer role configurable
    - added support for BGP peer role configurable
    
    Change-Id: I756c013538a3c10a509c48ab1b69cefac2c033da
    Signed-off-by: Ladislav Borak <lborak@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/BGPPeer.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/BGPPeer.java
@@ -87,14 +87,16 @@ public class BGPPeer implements ReusableBGPPeer, Peer, AutoCloseable, BGPPeerRun
     private AdjRibInWriter ribWriter;
 
     public BGPPeer(final String name, final RIB rib) {
+        this(name, rib, null);
+    }
+
+    public BGPPeer(final String name, final RIB rib, final Short role) {
         this.rib = Preconditions.checkNotNull(rib);
         this.name = name;
 
         final DOMTransactionChain chain = rib.createPeerChain(this);
-        // FIXME: make this configurable
-        final PeerRole role = PeerRole.Ibgp;
-
-        this.ribWriter = AdjRibInWriter.create(rib.getYangRibId(), role, chain);
+        final PeerRole tempRole = role != null ? PeerRole.forValue(role) : PeerRole.Ibgp;
+        this.ribWriter = AdjRibInWriter.create(rib.getYangRibId(), tempRole, chain);
     }
 
     @Override
