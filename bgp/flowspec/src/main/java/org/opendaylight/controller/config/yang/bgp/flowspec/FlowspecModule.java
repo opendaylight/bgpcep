10baec4d2dd75ecba7fb9afad8dabe5cabdb7e22
Dana Kutenicsova <dkutenic@cisco.com>
2015-04-23 13:23:19 +0200

    BUG-2571 : created configuration for Flowspec
    Implemented configuration and registration for
    parser and RIB for Flowspec support.
    
    Change-Id: I46158998f4b7fb6de6c3fc018e02df2c150104dd
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

---
+++ bgp/flowspec/src/main/java/org/opendaylight/controller/config/yang/bgp/flowspec/FlowspecModule.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.config.yang.bgp.flowspec;
+
+import org.opendaylight.protocol.bgp.flowspec.BGPActivator;
+import org.opendaylight.protocol.bgp.flowspec.RIBActivator;
+import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderActivator;
+import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderContext;
+import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderActivator;
+import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderContext;
+
+public class FlowspecModule extends org.opendaylight.controller.config.yang.bgp.flowspec.AbstractFlowspecModule {
+    public FlowspecModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public FlowspecModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, final org.opendaylight.controller.config.yang.bgp.flowspec.FlowspecModule oldModule, final java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        final class FlowspecExtension implements AutoCloseable, BGPExtensionProviderActivator, RIBExtensionProviderActivator {
+            private final BGPExtensionProviderActivator bgpact = new BGPActivator();
+            private final RIBExtensionProviderActivator ribact = new RIBActivator();
+
+            @Override
+            public void close() {
+                if (this.bgpact != null) {
+                    this.bgpact.stop();
+                }
+                if (this.ribact != null) {
+                    this.ribact.stopRIBExtensionProvider();
+                }
+            }
+
+            @Override
+            public void startRIBExtensionProvider(final RIBExtensionProviderContext context) {
+                this.ribact.startRIBExtensionProvider(context);
+            }
+
+            @Override
+            public void stopRIBExtensionProvider() {
+                this.ribact.stopRIBExtensionProvider();
+            }
+
+            @Override
+            public void start(final BGPExtensionProviderContext context) {
+                this.bgpact.start(context);
+            }
+
+            @Override
+            public void stop() {
+                this.bgpact.stop();
+            }
+        }
+        return new FlowspecExtension();
+    }
+}
