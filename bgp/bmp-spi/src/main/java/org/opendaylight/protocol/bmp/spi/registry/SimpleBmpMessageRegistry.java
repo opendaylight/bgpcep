457cc085bef03c8c4e1ee11754f39c30cfb62e63
Milos Fabian <milfabia@cisco.com>
2015-05-13 08:48:49 +0200

    WIP - Bug 3075: BMP SPI
    -BMP messages parser/serializer and registry abstraction
    
    Change-Id: I21e8477a268404712265e6b8fc4ba5a1aff73ce4
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/bmp-spi/src/main/java/org/opendaylight/protocol/bmp/spi/registry/SimpleBmpMessageRegistry.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.spi.registry;
+
+import static org.opendaylight.protocol.bmp.spi.parser.BmpMessageConstants.BMP_VERSION;
+import static org.opendaylight.protocol.bmp.spi.parser.BmpMessageConstants.COMMON_HEADER_LENGTH;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bmp.spi.parser.BmpMessageParser;
+import org.opendaylight.protocol.bmp.spi.parser.BmpMessageSerializer;
+import org.opendaylight.protocol.concepts.HandlerRegistry;
+import org.opendaylight.protocol.util.Values;
+import org.opendaylight.yangtools.yang.binding.DataContainer;
+import org.opendaylight.yangtools.yang.binding.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleBmpMessageRegistry implements BmpMessageRegistry {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleBmpMessageRegistry.class);
+
+    private final HandlerRegistry<DataContainer, BmpMessageParser, BmpMessageSerializer> handlers = new HandlerRegistry<>();
+
+    @Override
+    public AutoCloseable registerBmpMessageParser(final int messageType, final BmpMessageParser parser) {
+        Preconditions.checkArgument(messageType >= 0 && messageType <= Values.UNSIGNED_BYTE_MAX_VALUE);
+        return this.handlers.registerParser(messageType, parser);
+    }
+
+    @Override
+    public AutoCloseable registerBmpMessageSerializer(final Class<? extends Notification> msgClass, final BmpMessageSerializer serializer) {
+        return this.handlers.registerSerializer(msgClass, serializer);
+    }
+
+    @Override
+    public Notification parseMessage(final ByteBuf buffer) {
+        final int messageType = parseMessageHeader(buffer);
+        Preconditions.checkArgument(messageType >= 0 && messageType <= Values.UNSIGNED_BYTE_MAX_VALUE);
+        final BmpMessageParser parser = this.handlers.getParser(messageType);
+        if (parser == null) {
+            LOG.warn("BMP parser for message type {} is not registered.", messageType);
+            return null;
+        }
+        return parser.parseMessage(buffer);
+    }
+
+    @Override
+    public void serializeMessage(final Notification message, final ByteBuf buffer) {
+        final BmpMessageSerializer serializer = this.handlers.getSerializer(message.getImplementedInterface());
+        if (serializer == null) {
+            LOG.warn("BMP serializer for message type {} is not registered.", message.getClass());
+            return;
+        }
+        serializer.serializeMessage(message, buffer);
+    }
+
+    private int parseMessageHeader(final ByteBuf buffer) {
+        //TODO precondition messages
+        Preconditions.checkArgument(buffer != null && buffer.isReadable());
+        Preconditions.checkArgument(buffer.readableBytes() >= COMMON_HEADER_LENGTH);
+        final short messageVersion = buffer.readUnsignedByte();
+        Preconditions.checkState(messageVersion == BMP_VERSION);
+        final long messageLength = buffer.readUnsignedInt();
+        final short msgType = buffer.readUnsignedByte();
+        Preconditions.checkArgument(buffer.readableBytes() == messageLength - COMMON_HEADER_LENGTH);
+        return msgType;
+    }
+
+}
