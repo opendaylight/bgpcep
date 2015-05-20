7e02430f882d6a2123b865c4a48d3c260c9c8284
Claudio D. Gasparini <cgaspari@cisco.com>
2015-05-20 15:27:13 +0200

    WIP Bug-3075 BMP dispatcher and session
    Change-Id: Ie141f0cd9dfccc4f808160d072ead6def3332280
    Signed-off-by: Milos Fabian <milfabia@cisco.com>
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/bmp-api/src/main/java/org/opendaylight/protocol/bmp/api/BmpSession.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.api;
+
+import io.netty.util.concurrent.Future;
+
+import java.net.InetAddress;
+
+import org.opendaylight.protocol.framework.ProtocolSession;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+public interface BmpSession extends ProtocolSession<Notification> {
+    /**
+     * @param message message to be sent
+     * @return Future promise which will be succeed when the message is enqueued in the socket.
+     */
+    Future<Void> sendMessage(Notification message);
+
+    /**
+     * 
+     * @return
+     */
+    InetAddress getRemoteAddress();
+}
---
+++ bgp/bmp-impl/src/main/java/org/opendaylight/protocol/bmp/impl/BmpSessionImpl.java
@@ -0,0 +1,120 @@
+package org.opendaylight.protocol.bmp.impl;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.util.concurrent.Future;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+import javax.annotation.concurrent.GuardedBy;
+
+import org.opendaylight.protocol.bmp.api.BmpSession;
+import org.opendaylight.protocol.bmp.api.BmpSessionListener;
+import org.opendaylight.protocol.framework.AbstractProtocolSession;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.InitiationMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.Termination;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.TerminationMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.TerminationMessageBuilder;
+import org.opendaylight.yangtools.yang.binding.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BmpSessionImpl extends AbstractProtocolSession<Notification> implements BmpSession {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BmpSessionImpl.class);
+
+    private final BmpSessionListener listener;
+
+    private final Channel channel;
+
+    @GuardedBy("this")
+    private State state = State.UP;
+
+    public BmpSessionImpl(final BmpSessionListener listener, final Channel channel) {
+        this.listener = listener;
+        this.channel = channel;
+    }
+
+    @Override
+    protected void handleMessage(final Notification msg) {
+        if (this.state == State.INITIATED) {
+            this.listener.onMessage(this, msg);
+            if (msg instanceof TerminationMessage) {
+                this.close();
+            }
+        } else {
+            if (msg instanceof InitiationMessage) {
+                this.state = State.INITIATED;
+                this.listener.onMessage(this, msg);
+            } else {
+                LOG.trace("Session closed, initialization message was expected", msg);
+                close();
+            }
+        }
+    }
+
+    @Override
+    protected void endOfInput() {
+        if (this.state == State.UP) {
+            this.listener.onSessionDown(this, new IOException("End of input detected. Close the session."));
+        }
+    }
+
+    @Override
+    protected void sessionUp() {
+        this.listener.onSessionUp(this);
+    }
+
+    @Override
+    public void close() {
+        LOG.info("Closing session: {}", this);
+        this.sendMessage(new TerminationMessageBuilder().setReason(Termination.Reason.forValue(1)).build());
+
+        if (this.state != State.IDLE) {
+            this.channel.close();
+            this.state = State.IDLE;
+        }
+    }
+
+    @Override
+    public Future<Void> sendMessage(final Notification message) {
+        final ChannelFuture f = this.channel.writeAndFlush(message);
+
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(final ChannelFuture arg) {
+                if (arg.isSuccess()) {
+                    LOG.trace("Message sent to socket: {}", message);
+                } else {
+                    LOG.debug("Message not sent: {}", message, arg.cause());
+                }
+            }
+        });
+
+        return f;
+    }
+
+    @Override
+    public InetAddress getRemoteAddress() {
+        return ((InetSocketAddress) this.channel.remoteAddress()).getAddress();
+    }
+
+    public enum State {
+        /**
+         * The session has been completely established.
+         */
+        UP,
+        /**
+         * The session is half-alive
+         */
+        INITIATED,
+        /**
+         * The session has been closed. It will not be resurrected.
+         */
+        IDLE
+    }
+
+}
