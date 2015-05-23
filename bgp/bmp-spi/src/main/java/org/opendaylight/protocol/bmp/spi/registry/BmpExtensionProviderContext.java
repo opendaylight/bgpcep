832e990d4f426d14e8b1e4aafa87aaeae2d15200
Milos Fabian <milfabia@cisco.com>
2015-05-24 00:49:08 +0200

    Bug-3075: BMP messages abstraction and registers
    -abstracted common and per-peer header and TLVs parsing/serialization
    -implemented BMP message and TLV registers
    -BMP extension provider and consumer
    
    Change-Id: I21e8477a268404712265e6b8fc4ba5a1aff73ce4
    Signed-off-by: Milos Fabian <milfabia@cisco.com>
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/bmp-spi/src/main/java/org/opendaylight/protocol/bmp/spi/registry/BmpExtensionProviderContext.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.spi.registry;
+
+import org.opendaylight.protocol.bmp.spi.parser.BmpTlvParser;
+import org.opendaylight.protocol.bmp.spi.parser.BmpTlvSerializer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.Tlv;
+
+public interface BmpExtensionProviderContext extends BmpMessageRegistrator, BmpExtensionConsumerContext {
+
+    AutoCloseable registerBmpStatisticsTlvParser(int tlvType, BmpTlvParser parser);
+
+    AutoCloseable registerBmpStatisticsTlvSerializer(Class<? extends Tlv> tlvClass, BmpTlvSerializer serializer);
+
+    AutoCloseable registerBmpInitiationTlvParser(int tlvType, BmpTlvParser parser);
+
+    AutoCloseable registerBmpInitiationTlvSerializer(Class<? extends Tlv> tlvClass, BmpTlvSerializer serializer);
+
+    AutoCloseable registerBmpTerminationTlvParser(int tlvType, BmpTlvParser parser);
+
+    AutoCloseable registerBmpTerminationTlvSerializer(Class<? extends Tlv> tlvClass, BmpTlvSerializer serializer);
+
+}
---
+++ bgp/bmp-spi/src/main/java/org/opendaylight/protocol/bmp/spi/registry/SimpleBmpExtensionProviderContext.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.spi.registry;
+
+import org.opendaylight.protocol.bmp.spi.parser.BmpMessageParser;
+import org.opendaylight.protocol.bmp.spi.parser.BmpMessageSerializer;
+import org.opendaylight.protocol.bmp.spi.parser.BmpTlvParser;
+import org.opendaylight.protocol.bmp.spi.parser.BmpTlvSerializer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.Tlv;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+public class SimpleBmpExtensionProviderContext implements BmpExtensionConsumerContext, BmpExtensionProviderContext {
+
+    private final BmpMessageRegistry bmpMessageRegistry = new SimpleBmpMessageRegistry();
+    private final BmpTlvRegistry bmpStatisticsTlvRegistry = new SimpleBmpTlvRegistry();
+    private final BmpTlvRegistry bmpInitiationTlvRegistry = new SimpleBmpTlvRegistry();
+    private final BmpTlvRegistry bmpTerminationTlvRegistry = new SimpleBmpTlvRegistry();
+
+    @Override
+    public AutoCloseable registerBmpMessageParser(final int messageType, final BmpMessageParser parser) {
+        return this.bmpMessageRegistry.registerBmpMessageParser(messageType, parser);
+    }
+
+    @Override
+    public AutoCloseable registerBmpMessageSerializer(final Class<? extends Notification> messageClass, final BmpMessageSerializer serializer) {
+        return this.bmpMessageRegistry.registerBmpMessageSerializer(messageClass, serializer);
+    }
+
+    @Override
+    public BmpMessageRegistry getBmpMessageRegistry() {
+        return this.bmpMessageRegistry;
+    }
+
+    @Override
+    public AutoCloseable registerBmpStatisticsTlvParser(final int tlvType, final BmpTlvParser parser) {
+        return this.bmpStatisticsTlvRegistry.registerBmpTlvParser(tlvType, parser);
+    }
+
+    @Override
+    public AutoCloseable registerBmpStatisticsTlvSerializer(final Class<? extends Tlv> tlvClass, final BmpTlvSerializer serializer) {
+        return this.bmpStatisticsTlvRegistry.registerBmpTlvSerializer(tlvClass, serializer);
+    }
+
+    @Override
+    public AutoCloseable registerBmpInitiationTlvParser(final int tlvType, final BmpTlvParser parser) {
+        return this.bmpInitiationTlvRegistry.registerBmpTlvParser(tlvType, parser);
+    }
+
+    @Override
+    public AutoCloseable registerBmpInitiationTlvSerializer(final Class<? extends Tlv> tlvClass, final BmpTlvSerializer serializer) {
+        return this.bmpInitiationTlvRegistry.registerBmpTlvSerializer(tlvClass, serializer);
+    }
+
+    @Override
+    public AutoCloseable registerBmpTerminationTlvParser(final int tlvType, final BmpTlvParser parser) {
+        return this.bmpTerminationTlvRegistry.registerBmpTlvParser(tlvType, parser);
+    }
+
+    @Override
+    public AutoCloseable registerBmpTerminationTlvSerializer(final Class<? extends Tlv> tlvClass, final BmpTlvSerializer serializer) {
+        return this.bmpTerminationTlvRegistry.registerBmpTlvSerializer(tlvClass, serializer);
+    }
+
+    @Override
+    public BmpTlvRegistry getBmpStatisticsTlvRegistry() {
+        return this.bmpStatisticsTlvRegistry;
+    }
+
+    @Override
+    public BmpTlvRegistry getBmpInitiationTlvRegistry() {
+        return this.bmpInitiationTlvRegistry;
+    }
+
+    @Override
+    public BmpTlvRegistry getBmpTerminationTlvRegistry() {
+        return this.bmpTerminationTlvRegistry;
+    }
+
+}
