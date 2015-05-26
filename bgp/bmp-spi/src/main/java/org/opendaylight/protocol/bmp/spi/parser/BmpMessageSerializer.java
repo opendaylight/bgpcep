0514d654ad3af5937d7eee05a27261425c7dcb75
Milos Fabian <milfabia@cisco.com>
2015-05-26 09:40:22 +0200

    Bug-3075: BMP parser/serializer and registry APIs
    -Java API for BMP message and TLV parsers/serializers
    -Java API for BMP registry and extension consumer/provider
    
    Change-Id: I4ea6fe6418b964bcf8976d85724d120efb128a5f
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/bmp-spi/src/main/java/org/opendaylight/protocol/bmp/spi/parser/BmpMessageSerializer.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.spi.parser;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+public interface BmpMessageSerializer {
+
+    void serializeMessage(final Notification message, final ByteBuf buffer);
+
+}
