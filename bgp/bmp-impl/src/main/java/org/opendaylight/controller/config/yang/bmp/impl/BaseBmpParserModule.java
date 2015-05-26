b86651c8bceec9f4601d883f100c3c494eeee62d
Milos Fabian <milfabia@cisco.com>
2015-05-26 11:40:06 +0200

    Bug-3075: BMP activator
    -registers all message and TLV parsers/serializers
    -integrate BMP activator with config subystem
    -create instance of activator, consume BGP extensions
    
    Change-Id: I745fac383ff6c7e9d275ce94ed426b2751aacec6
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/bmp-impl/src/main/java/org/opendaylight/controller/config/yang/bmp/impl/BaseBmpParserModule.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.config.yang.bmp.impl;
+
+import org.opendaylight.protocol.bmp.impl.BmpActivator;
+
+public class BaseBmpParserModule extends org.opendaylight.controller.config.yang.bmp.impl.AbstractBaseBmpParserModule {
+    public BaseBmpParserModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public BaseBmpParserModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.bmp.impl.BaseBmpParserModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        return new BmpActivator(getBgpExtensionsDependency().getMessageRegistry());
+    }
+
+}
