d7a50c8e26ea271e5ea12ac737f33bbdd3742bb1
Milos Fabian <milfabia@cisco.com>
2015-05-26 11:27:10 +0200

    Bug-3075: BMP message parsers/serializers implementation
    Change-Id: Ie0b8ee9c0086ffcebff9d5d767d87c198673f9fd
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/bmp-impl/src/main/java/org/opendaylight/protocol/bmp/impl/message/InitiationHandler.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.impl.message;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bmp.spi.parser.AbstractBmpMessageWithTlvParser;
+import org.opendaylight.protocol.bmp.spi.parser.BmpDeserializationException;
+import org.opendaylight.protocol.bmp.spi.registry.BmpTlvRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.InitiationMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.InitiationMessageBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.Tlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.description.tlv.DescriptionTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.initiation.Tlvs;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.initiation.TlvsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.name.tlv.NameTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.string.informations.StringInformation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.string.informations.StringInformationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.string.tlv.StringTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.string.tlv.StringTlvBuilder;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+/**
+ * Created by cgasparini on 13.5.2015.
+ */
+public class InitiationHandler extends AbstractBmpMessageWithTlvParser<TlvsBuilder> {
+
+    public InitiationHandler(final BmpTlvRegistry tlvRegistry) {
+        super(tlvRegistry);
+    }
+
+    private static final int MESSAGE_TYPE = 4;
+
+    @Override
+    public void serializeMessageBody(final Notification message, final ByteBuf buffer) {
+        Preconditions.checkArgument(message instanceof InitiationMessage, "Incorrect instance of BGP message. The Initiation Message is expected.");
+        final InitiationMessage initiation = (InitiationMessage) message;
+        serializeTlvs(initiation.getTlvs(), buffer);
+    }
+
+    @Override
+    public Notification parseMessageBody(final ByteBuf bytes) throws BmpDeserializationException {
+        final InitiationMessageBuilder initiationBuilder = new InitiationMessageBuilder();
+        final TlvsBuilder tlvsBuilder = new TlvsBuilder();
+        tlvsBuilder.setStringInformation(ImmutableList.<StringInformation>of());
+        parseTlvs(tlvsBuilder, bytes);
+
+        if (tlvsBuilder.getDescriptionTlv() == null || tlvsBuilder.getDescriptionTlv().getDescription() == null) {
+            throw new BmpDeserializationException("Inclusion of sysDescr TLV is mandatory.");
+        }
+        if (tlvsBuilder.getNameTlv() == null || tlvsBuilder.getNameTlv().getName() == null) {
+            throw new BmpDeserializationException("Inclusion of sysName TLV is mandatory.");
+        }
+
+        return initiationBuilder.setTlvs(tlvsBuilder.build()).build();
+    }
+
+    @Override
+    public int getBmpMessageType() {
+        return MESSAGE_TYPE;
+    }
+
+    private void serializeTlvs(final Tlvs tlvs, final ByteBuf output) {
+        serializeTlv(tlvs.getNameTlv(), output);
+        serializeTlv(tlvs.getDescriptionTlv(), output);
+        if (tlvs.getStringInformation() != null) {
+            for (final StringInformation stringInfo : tlvs.getStringInformation()) {
+                if (stringInfo.getStringTlv() != null) {
+                    serializeTlv(stringInfo.getStringTlv(), output);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void addTlv(final TlvsBuilder builder, final Tlv tlv) {
+        if (tlv instanceof DescriptionTlv) {
+            builder.setDescriptionTlv((DescriptionTlv) tlv);
+        } else if (tlv instanceof NameTlv) {
+            builder.setNameTlv((NameTlv) tlv);
+        } else if (tlv instanceof StringTlv) {
+            builder.setStringInformation(ImmutableList.<StringInformation>builder()
+                    .addAll(builder.getStringInformation())
+                    .add(new StringInformationBuilder().setStringTlv(new StringTlvBuilder((StringTlv) tlv).build()).build()).build());
+        }
+    }
+}
---
+++ bgp/bmp-impl/src/main/java/org/opendaylight/protocol/bmp/impl/message/RouteMonitoringMessageHandler.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.impl.message;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
+import org.opendaylight.protocol.bgp.parser.BGPParsingException;
+import org.opendaylight.protocol.bgp.parser.spi.MessageRegistry;
+import org.opendaylight.protocol.bmp.spi.parser.AbstractBmpPerPeerMessageParser;
+import org.opendaylight.protocol.bmp.spi.parser.BmpDeserializationException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.UpdateBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.UpdateMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.RouteMonitoringMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.RouteMonitoringMessageBuilder;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+/**
+ * Created by cgasparini on 13.5.2015.
+ */
+public class RouteMonitoringMessageHandler extends AbstractBmpPerPeerMessageParser<RouteMonitoringMessageBuilder> {
+
+    private static final int MESSAGE_TYPE = 0;
+
+    public RouteMonitoringMessageHandler(final MessageRegistry bgpMssageRegistry) {
+        super(bgpMssageRegistry);
+    }
+
+    @Override
+    public void serializeMessageBody(final Notification message, final ByteBuf buffer) {
+        super.serializeMessageBody(message, buffer);
+        Preconditions.checkArgument(message instanceof RouteMonitoringMessage, "An instance of RouteMonitoringMessage is required");
+        final RouteMonitoringMessage routeMonitor = (RouteMonitoringMessage) message;
+        getBgpMessageRegistry().serializeMessage(new UpdateBuilder(routeMonitor.getUpdate()).build(), buffer);
+    }
+
+    @Override
+    public Notification parseMessageBody(final ByteBuf bytes) throws BmpDeserializationException {
+        final RouteMonitoringMessageBuilder routeMonitor = new RouteMonitoringMessageBuilder().setPeerHeader(parsePerPeerHeader(bytes));
+        try {
+            final Notification message = getBgpMessageRegistry().parseMessage(bytes);
+            Preconditions.checkNotNull(message, "UpdateMessage may not be null");
+            Preconditions.checkArgument(message instanceof UpdateMessage, "An instance of UpdateMessage is required");
+            final UpdateMessage updateMessage = (UpdateMessage) message;
+            final org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.route.monitoring
+                .message.Update update = new org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message
+                .rev150512.route.monitoring.message.UpdateBuilder(updateMessage).build();
+            routeMonitor.setUpdate(update);
+        } catch (final BGPDocumentedException | BGPParsingException e) {
+            throw new BmpDeserializationException("Error while parsing Update Message.", e);
+        }
+
+        return routeMonitor.build();
+    }
+
+    @Override
+    public int getBmpMessageType() {
+        return MESSAGE_TYPE;
+    }
+}
