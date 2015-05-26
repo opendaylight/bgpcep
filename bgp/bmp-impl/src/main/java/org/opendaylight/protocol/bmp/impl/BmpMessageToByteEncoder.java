9e4290ae161eb8f8d41e848ae671e44cca2cbc35
Milos Fabian <milfabia@cisco.com>
2015-05-26 11:41:23 +0200

    Bug-3075: netty encoder/decored for BMP messages
    -integrate message registry with netty pipeline handlers
    
    Change-Id: I8207b57ba7d859ff0a877c8eb6d5e4d7ddbc93ef
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/bmp-impl/src/main/java/org/opendaylight/protocol/bmp/impl/BmpMessageToByteEncoder.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.impl;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import org.opendaylight.protocol.bmp.spi.registry.BmpMessageRegistry;
+import org.opendaylight.yangtools.yang.binding.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Sharable
+public class BmpMessageToByteEncoder extends MessageToByteEncoder<Notification> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BmpMessageToByteEncoder.class);
+
+    private final BmpMessageRegistry registry;
+
+    public BmpMessageToByteEncoder(final BmpMessageRegistry registry) {
+        this.registry = Preconditions.checkNotNull(registry);
+    }
+
+    @Override
+    protected void encode(final ChannelHandlerContext ctx, final Notification message, final ByteBuf out) throws Exception {
+        LOG.trace("Encoding message: {}", message);
+        this.registry.serializeMessage(message, out);
+        LOG.debug("Message sent to output: {}", message);
+    }
+
+}
