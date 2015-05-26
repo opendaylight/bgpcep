e903a80dfe2f618601cd577c5330232e0e657889
Milos Fabian <milfabia@cisco.com>
2015-05-26 09:48:59 +0200

    Bug-3075: BMP parsers/serializers and registry abstraction
    -implemented common functionality for BMP parsers/serializers
    -implemented simple registr, extension provider and consumer
    
    Change-Id: Ie3b042d8b20b2f25d15c40bea529a45a4f9db1e4
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/bmp-spi/src/main/java/org/opendaylight/protocol/bmp/spi/parser/AbstractBmpMessageWithTlvParser.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.spi.parser;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import org.opendaylight.protocol.bmp.spi.registry.BmpTlvRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.Tlv;
+import org.opendaylight.yangtools.concepts.Builder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract  class AbstractBmpMessageWithTlvParser<T extends Builder<?>> extends AbstractBmpMessageParser {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractBmpMessageWithTlvParser.class);
+
+    private final BmpTlvRegistry tlvRegistry;
+
+    public AbstractBmpMessageWithTlvParser(final BmpTlvRegistry tlvRegistry) {
+        this.tlvRegistry = tlvRegistry;
+    }
+
+    protected final void parseTlvs(final T builder, final ByteBuf bytes) throws BmpDeserializationException {
+        Preconditions.checkArgument(bytes != null, "Array of bytes is mandatory. Can't be null.");
+        if (!bytes.isReadable()) {
+            return;
+        }
+        while (bytes.isReadable()) {
+            final int type = bytes.readUnsignedShort();
+            final int length = bytes.readUnsignedShort();
+            if (length > bytes.readableBytes()) {
+                throw new BmpDeserializationException("Wrong length specified. Passed: " + length + "; Expected: <= " + bytes.readableBytes()
+                    + ".");
+            }
+            final ByteBuf tlvBytes = bytes.readSlice(length);
+            LOG.trace("Parsing BMP TLV : {}", ByteBufUtil.hexDump(tlvBytes));
+
+            final Tlv tlv = this.tlvRegistry.parseTlv(type, tlvBytes);
+            if(tlv != null) {
+                LOG.trace("Parsed BMP TLV {}.", tlv);
+                addTlv(builder, tlv);
+            }
+        }
+    }
+
+    protected final void serializeTlv(final Tlv tlv, final ByteBuf buffer) {
+        Preconditions.checkNotNull(tlv, "BMP TLV is mandatory.");
+        LOG.trace("Serializing BMP TLV {}", tlv);
+        this.tlvRegistry.serializeTlv(tlv, buffer);
+        LOG.trace("Serialized BMP TLV : {}.", ByteBufUtil.hexDump(buffer));
+    }
+
+    protected void addTlv(final T builder, final Tlv tlv) {
+        //no TLVs by default
+    }
+
+}
---
+++ bgp/bmp-spi/src/main/java/org/opendaylight/protocol/bmp/spi/parser/AbstractBmpPerPeerMessageParser.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.spi.parser;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bgp.parser.spi.MessageRegistry;
+import org.opendaylight.protocol.bmp.spi.registry.BmpTlvRegistry;
+import org.opendaylight.protocol.util.BitArray;
+import org.opendaylight.protocol.util.ByteBufWriteUtil;
+import org.opendaylight.protocol.util.Ipv4Util;
+import org.opendaylight.protocol.util.Ipv6Util;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.AsNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev100924.Timestamp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.AdjRibInType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.PeerType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.peer.header.PeerHeader;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.peer.header.PeerHeaderBuilder;
+import org.opendaylight.yangtools.concepts.Builder;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+public abstract class AbstractBmpPerPeerMessageParser<T  extends Builder<?>> extends AbstractBmpMessageWithTlvParser<T> {
+
+    private static final int L_FLAG_POS = 1;
+    private static final int V_FLAG_POS = 0;
+    private static final int FLAGS_SIZE = 8;
+    private static final int PEER_DISTINGUISHER_SIZE = 8;
+    private static final int PER_PEER_HEADER_SIZE = 32;
+
+    private final MessageRegistry bgpMssageRegistry;
+
+    public AbstractBmpPerPeerMessageParser(final MessageRegistry bgpMssageRegistry) {
+        this(bgpMssageRegistry, null);
+    }
+
+    public AbstractBmpPerPeerMessageParser(final MessageRegistry bgpMssageRegistry, final BmpTlvRegistry tlvRegistry) {
+        super(tlvRegistry);
+        this.bgpMssageRegistry = Preconditions.checkNotNull(bgpMssageRegistry);
+    }
+
+    @Override
+    public void serializeMessageBody(final Notification message, final ByteBuf buffer) {
+        if (message instanceof org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.PeerHeader) {
+            final org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.PeerHeader messageWithPerPeerHeader =
+                (org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.PeerHeader) message;
+            serializePerPeerHeader(messageWithPerPeerHeader.getPeerHeader(), buffer);
+        }
+    }
+
+    protected final PeerHeader parsePerPeerHeader(final ByteBuf bytes) {
+        Preconditions.checkArgument(bytes.readableBytes() >= PER_PEER_HEADER_SIZE);
+        final PeerHeaderBuilder phBuilder = new PeerHeaderBuilder();
+        phBuilder.setType(PeerType.forValue(bytes.readByte()));
+        final BitArray flags = BitArray.valueOf(bytes, FLAGS_SIZE);
+        phBuilder.setAdjRibInType(AdjRibInType.forValue(flags.get(L_FLAG_POS) ? 1 : 0));
+        phBuilder.setIpv4(!flags.get(V_FLAG_POS));
+        if (phBuilder.getType().equals(PeerType.L3vpn)) {
+            phBuilder.setDistinguisher(PeerDistinguisherUtil.parsePeerDistingisher(bytes));
+        } else {
+            bytes.skipBytes(PEER_DISTINGUISHER_SIZE);
+        }
+        if (phBuilder.isIpv4()) {
+            bytes.skipBytes(Ipv6Util.IPV6_LENGTH - Ipv4Util.IP4_LENGTH);
+            phBuilder.setAddress(new IpAddress(Ipv4Util.addressForByteBuf(bytes)));
+        } else {
+            phBuilder.setAddress(new IpAddress(Ipv6Util.addressForByteBuf(bytes)));
+        }
+        phBuilder.setAs(new AsNumber(bytes.readUnsignedInt()));
+        phBuilder.setBgpId(Ipv4Util.addressForByteBuf(bytes));
+        phBuilder.setTimestampSec(new Timestamp(bytes.readUnsignedInt()));
+        phBuilder.setTimestampMicro(new Timestamp(bytes.readUnsignedInt()));
+        return phBuilder.build();
+    }
+
+    protected void serializePerPeerHeader(final PeerHeader peerHeader, final ByteBuf output) {
+        Preconditions.checkArgument(peerHeader != null, "Per-peer header cannot be null.");
+        output.writeByte(peerHeader.getType().getIntValue());
+        final BitArray flags = new BitArray(FLAGS_SIZE);
+        flags.set(L_FLAG_POS, peerHeader.getAdjRibInType().getIntValue() == 0 ? false : true);
+        flags.set(V_FLAG_POS, !peerHeader.isIpv4());
+        flags.toByteBuf(output);
+        if (peerHeader.getType().equals(PeerType.L3vpn)) {
+            PeerDistinguisherUtil.serializePeerDistinguisher(peerHeader.getDistinguisher(), output);
+        } else {
+            output.writeZero(PEER_DISTINGUISHER_SIZE);
+        }
+        if (peerHeader.isIpv4()) {
+            output.writeZero(Ipv6Util.IPV6_LENGTH - Ipv4Util.IP4_LENGTH);
+            ByteBufWriteUtil.writeIpv4Address(peerHeader.getAddress().getIpv4Address(), output);
+        } else {
+            ByteBufWriteUtil.writeIpv6Address(peerHeader.getAddress().getIpv6Address(), output);
+        }
+        ByteBufWriteUtil.writeUnsignedInt(peerHeader.getAs().getValue(), output);
+        ByteBufWriteUtil.writeIpv4Address(peerHeader.getBgpId(), output);
+        if (peerHeader.getTimestampSec() != null) {
+            ByteBufWriteUtil.writeUnsignedInt(peerHeader.getTimestampSec().getValue(), output);
+        } else {
+            output.writeZero(ByteBufWriteUtil.INT_BYTES_LENGTH);
+        }
+        if (peerHeader.getTimestampMicro() != null) {
+            ByteBufWriteUtil.writeUnsignedInt(peerHeader.getTimestampMicro().getValue(), output);
+        } else {
+            output.writeZero(ByteBufWriteUtil.INT_BYTES_LENGTH);
+        }
+    }
+
+    protected final MessageRegistry getBgpMessageRegistry() {
+        return this.bgpMssageRegistry;
+    }
+}
