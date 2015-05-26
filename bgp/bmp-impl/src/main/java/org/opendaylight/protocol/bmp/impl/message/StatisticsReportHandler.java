587f76609429b3f4a64ec762b0435a45caa66c49
Milos Fabian <milfabia@cisco.com>
2015-05-26 11:27:10 +0200

    Bug-3075: BMP message parsers/serializers implementation
    Change-Id: Ie0b8ee9c0086ffcebff9d5d767d87c198673f9fd
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/bmp-impl/src/main/java/org/opendaylight/protocol/bmp/impl/message/StatisticsReportHandler.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.impl.message;
+
+import static org.opendaylight.protocol.util.ByteBufWriteUtil.INT_BYTES_LENGTH;
+import static org.opendaylight.protocol.util.ByteBufWriteUtil.writeUnsignedInt;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.opendaylight.protocol.bgp.parser.spi.MessageRegistry;
+import org.opendaylight.protocol.bmp.spi.parser.AbstractBmpPerPeerMessageParser;
+import org.opendaylight.protocol.bmp.spi.parser.BmpDeserializationException;
+import org.opendaylight.protocol.bmp.spi.registry.BmpTlvRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.StatsReportsMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.StatsReportsMessageBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.Tlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.Tlvs;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.TlvsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.tlvs.AdjRibsInRoutesTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.tlvs.DuplicatePrefixAdvertisementsTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.tlvs.DuplicateWithdrawsTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.tlvs.InvalidatedAsConfedLoopTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.tlvs.InvalidatedAsPathLoopTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.tlvs.InvalidatedClusterListLoopTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.tlvs.InvalidatedOriginatorIdTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.tlvs.LocRibRoutesTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.stat.tlvs.RejectedPrefixesTlv;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+/**
+ * Created by cgasparini on 13.5.2015.
+ */
+public class StatisticsReportHandler extends AbstractBmpPerPeerMessageParser<TlvsBuilder> {
+
+    private static final int MESSAGE_TYPE = 1;
+
+    public StatisticsReportHandler(final MessageRegistry bgpMssageRegistry, final BmpTlvRegistry tlvRegistry) {
+        super(bgpMssageRegistry, tlvRegistry);
+    }
+
+    @Override
+    public void serializeMessageBody(final Notification message, final ByteBuf buffer) {
+        super.serializeMessageBody(message, buffer);
+        Preconditions.checkArgument(message instanceof StatsReportsMessage, "An instance of Statistics Reports message is required");
+        final StatsReportsMessage statsReport = (StatsReportsMessage) message;
+        serializeTlvs(statsReport.getTlvs(), buffer);
+    }
+
+    @Override
+    public Notification parseMessageBody(final ByteBuf bytes) throws BmpDeserializationException {
+        final StatsReportsMessageBuilder statReport = new StatsReportsMessageBuilder().setPeerHeader(parsePerPeerHeader(bytes));
+        final TlvsBuilder tlvsBuilder = new TlvsBuilder();
+        bytes.skipBytes(INT_BYTES_LENGTH);
+        parseTlvs(tlvsBuilder, bytes);
+
+        return statReport.setTlvs(tlvsBuilder.build()).build();
+    }
+
+    @Override
+    public int getBmpMessageType() {
+        return MESSAGE_TYPE;
+    }
+
+    protected void serializeTlvs(final Tlvs tlvs, final ByteBuf output) {
+        final AtomicInteger counter = new AtomicInteger(0);
+        final ByteBuf tlvsBuffer = Unpooled.buffer();
+        serializeStatTlv(tlvs.getRejectedPrefixesTlv(), tlvsBuffer, counter);
+        serializeStatTlv(tlvs.getDuplicatePrefixAdvertisementsTlv(), tlvsBuffer, counter);
+        serializeStatTlv(tlvs.getDuplicateWithdrawsTlv(), tlvsBuffer, counter);
+        serializeStatTlv(tlvs.getInvalidatedClusterListLoopTlv(), tlvsBuffer, counter);
+        serializeStatTlv(tlvs.getInvalidatedAsPathLoopTlv(), tlvsBuffer, counter);
+        serializeStatTlv(tlvs.getInvalidatedOriginatorIdTlv(), tlvsBuffer, counter);
+        serializeStatTlv(tlvs.getInvalidatedAsConfedLoopTlv(), tlvsBuffer, counter);
+        serializeStatTlv(tlvs.getAdjRibsInRoutesTlv(), tlvsBuffer, counter);
+        serializeStatTlv(tlvs.getLocRibRoutesTlv(), tlvsBuffer, counter);
+        writeUnsignedInt(counter.longValue(), output);
+        output.writeBytes(tlvsBuffer);
+    }
+
+    private void serializeStatTlv(final Tlv tlv, final ByteBuf tlvsBuffer, final AtomicInteger counter) {
+        if (tlv != null) {
+            counter.incrementAndGet();
+            serializeTlv(tlv, tlvsBuffer);
+        }
+    }
+
+    @Override
+    protected void addTlv(final TlvsBuilder builder, final Tlv tlv) {
+        if (tlv instanceof AdjRibsInRoutesTlv) {
+            builder.setAdjRibsInRoutesTlv((AdjRibsInRoutesTlv) tlv);
+        } else if (tlv instanceof DuplicatePrefixAdvertisementsTlv) {
+            builder.setDuplicatePrefixAdvertisementsTlv((DuplicatePrefixAdvertisementsTlv) tlv);
+        } else if (tlv instanceof DuplicateWithdrawsTlv) {
+            builder.setDuplicateWithdrawsTlv((DuplicateWithdrawsTlv) tlv);
+        } else if (tlv instanceof InvalidatedAsConfedLoopTlv) {
+            builder.setInvalidatedAsConfedLoopTlv((InvalidatedAsConfedLoopTlv) tlv);
+        } else if (tlv instanceof InvalidatedAsPathLoopTlv) {
+            builder.setInvalidatedAsPathLoopTlv((InvalidatedAsPathLoopTlv) tlv);
+        } else if (tlv instanceof InvalidatedClusterListLoopTlv) {
+            builder.setInvalidatedClusterListLoopTlv((InvalidatedClusterListLoopTlv) tlv);
+        } else if (tlv instanceof InvalidatedOriginatorIdTlv) {
+            builder.setInvalidatedOriginatorIdTlv((InvalidatedOriginatorIdTlv) tlv);
+        } else if (tlv instanceof LocRibRoutesTlv) {
+            builder.setLocRibRoutesTlv((LocRibRoutesTlv) tlv);
+        } else if (tlv instanceof RejectedPrefixesTlv) {
+            builder.setRejectedPrefixesTlv((RejectedPrefixesTlv) tlv);
+        }
+    }
+
+}
---
+++ bgp/bmp-impl/src/main/java/org/opendaylight/protocol/bmp/impl/message/TerminationHandler.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.impl.message;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bmp.spi.parser.AbstractBmpMessageWithTlvParser;
+import org.opendaylight.protocol.bmp.spi.parser.BmpDeserializationException;
+import org.opendaylight.protocol.bmp.spi.registry.BmpTlvRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.TerminationMessage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.TerminationMessageBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.Tlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.reason.tlv.ReasonTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.string.informations.StringInformation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.string.informations.StringInformationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.string.tlv.StringTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.string.tlv.StringTlvBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.termination.Tlvs;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.termination.TlvsBuilder;
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+/**
+ * Created by cgasparini on 13.5.2015.
+ */
+public class TerminationHandler extends AbstractBmpMessageWithTlvParser<TlvsBuilder> {
+
+    public TerminationHandler(final BmpTlvRegistry tlvRegistry) {
+        super(tlvRegistry);
+    }
+
+    private static final int MESSAGE_TYPE = 5;
+
+    @Override
+    public Notification parseMessageBody(final ByteBuf bytes) throws BmpDeserializationException {
+        final TerminationMessageBuilder terminationMessage = new TerminationMessageBuilder();
+        final TlvsBuilder tlvsBuilder = new TlvsBuilder();
+        tlvsBuilder.setStringInformation(ImmutableList.<StringInformation>of());
+        parseTlvs(tlvsBuilder, bytes);
+        if (tlvsBuilder.getReasonTlv() == null || tlvsBuilder.getReasonTlv().getReason() == null) {
+            throw new BmpDeserializationException("Inclusion of Reason TLV is mandatory.");
+        }
+        return terminationMessage.setTlvs(tlvsBuilder.build()).build();
+    }
+
+
+    @Override
+    public void serializeMessageBody(final Notification message, final ByteBuf buffer) {
+        Preconditions.checkArgument(message instanceof TerminationMessage, "An instance of Termination message is required");
+        final TerminationMessage terminationMsg = (TerminationMessage) message;
+        serializeTlvs(terminationMsg.getTlvs(), buffer);
+    }
+
+    @Override
+    public int getBmpMessageType() {
+        return MESSAGE_TYPE;
+    }
+
+    protected void serializeTlvs(final Tlvs tlvs, final ByteBuf output) {
+        serializeTlv(tlvs.getReasonTlv(), output);
+        if (tlvs.getStringInformation() != null) {
+            for (final StringInformation stringInfo : tlvs.getStringInformation()) {
+                if (stringInfo.getStringTlv() != null) {
+                    serializeTlv(stringInfo.getStringTlv(), output);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void addTlv(final TlvsBuilder builder, final Tlv tlv) {
+        if (tlv instanceof ReasonTlv) {
+            builder.setReasonTlv((ReasonTlv) tlv);
+        } else if (tlv instanceof StringTlv) {
+            builder.setStringInformation(ImmutableList.<StringInformation>builder()
+                    .addAll(builder.getStringInformation())
+                    .add(new StringInformationBuilder().setStringTlv(new StringTlvBuilder((StringTlv) tlv).build()).build()).build());
+        }
+    }
+}
