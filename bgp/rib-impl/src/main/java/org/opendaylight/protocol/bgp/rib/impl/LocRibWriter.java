d59426aa0a8f993c50ea8069140b8c6780b3a907
Dana Kutenicsova <dkutenic@cisco.com>
2015-05-29 09:09:10 +0200

    BUG-3419 : fix calling get() on Absent value
    When BGP shuts down, LocRIB receives and uptodate
    change with no data attached causing an Exception.
    Fix this by checking existence of the data.
    
    Change-Id: Ibe1aeb8e7f02136be2c83be516e47833c1d8146a
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/LocRibWriter.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/LocRibWriter.java
@@ -157,17 +157,19 @@ final class LocRibWriter implements AutoCloseable, DOMDataTreeChangeListener {
 
     private void update(final DOMDataWriteTransaction tx, final Collection<DataTreeCandidate> changes,
         final Map<RouteUpdateKey, AbstractRouteEntry> toUpdate) {
-
         for (final DataTreeCandidate tc : changes) {
+            LOG.debug("Modification type {}", tc.getRootNode().getModificationType());
             final YangInstanceIdentifier path = tc.getRootPath();
             final NodeIdentifierWithPredicates peerKey = IdentifierUtils.peerKey(path);
             final PeerId peerId = IdentifierUtils.peerId(peerKey);
             final UnsignedInteger routerId = RouterIds.routerIdForPeerId(peerId);
             for (final DataTreeCandidateNode child : tc.getRootNode().getChildNodes()) {
                 if ((Attributes.QNAME).equals(child.getIdentifier().getNodeType())) {
-                    // putting uptodate attribute in
-                    LOG.trace("Uptodate found for {}", child.getDataAfter());
-                    tx.put(LogicalDatastoreType.OPERATIONAL, this.locRibTarget.node(child.getIdentifier()), child.getDataAfter().get());
+                    if (child.getDataAfter().isPresent()) {
+                        // putting uptodate attribute in
+                        LOG.trace("Uptodate found for {}", child.getDataAfter());
+                        tx.put(LogicalDatastoreType.OPERATIONAL, this.locRibTarget.node(child.getIdentifier()), child.getDataAfter().get());
+                    }
                     continue;
                 }
                 for (final DataTreeCandidateNode route : this.ribSupport.changedRoutes(child)) {
