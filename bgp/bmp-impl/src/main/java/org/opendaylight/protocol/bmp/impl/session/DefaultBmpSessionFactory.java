fd8aadcdab41aa9c8827a85ea54514253f82bfae
Milos Fabian <milfabia@cisco.com>
2015-06-09 09:12:41 +0200

    Bug-3075: BMP session implementation
    -basic channel reading/message handling
    -delegating messages to application layer
    -error handling, end of input detection
    -simple session factory
    
    Change-Id: I827c44413a4cc7cfa6066ceb2c63b8d9c85c49bc
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/bmp-impl/src/main/java/org/opendaylight/protocol/bmp/impl/session/DefaultBmpSessionFactory.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.impl.session;
+
+import io.netty.channel.Channel;
+import org.opendaylight.protocol.bmp.api.BmpSession;
+import org.opendaylight.protocol.bmp.api.BmpSessionFactory;
+import org.opendaylight.protocol.bmp.api.BmpSessionListenerFactory;
+
+public class DefaultBmpSessionFactory implements BmpSessionFactory {
+
+    @Override
+    public BmpSession getSession(final Channel channel, final BmpSessionListenerFactory sessionListenerFactory) {
+        return new BmpSessionImpl(sessionListenerFactory.getSessionListener(), channel);
+    }
+
+}
