31b72102d131a838106dff7ae2616d23b5d21fb5
Milos Fabian <milfabia@cisco.com>
2015-06-09 09:12:41 +0200

    Bug-3075: BMP session implementation
    -basic channel reading/message handling
    -delegating messages to application layer
    -error handling, end of input detection
    -simple session factory
    
    Change-Id: I827c44413a4cc7cfa6066ceb2c63b8d9c85c49bc
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/bmp-impl/src/test/java/org/opendaylight/protocol/bmp/impl/session/BmpTestSessionListener.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.impl.session;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+import org.opendaylight.protocol.bmp.api.BmpSession;
+import org.opendaylight.protocol.bmp.api.BmpSessionListener;
+import org.opendaylight.yangtools.yang.binding.Notification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by cgasparini on 21.5.2015.
+ */
+public class BmpTestSessionListener implements BmpSessionListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BmpTestSessionListener.class);
+    private List<Notification> messages = Lists.newArrayList();
+    private boolean up = false;
+
+    public boolean isUp () {
+        return up;
+    }
+
+    public List<Notification> getListMsg() {
+        return this.messages;
+    }
+
+    @Override
+    public void onMessage(final BmpSession session, final Notification message) {
+        LOG.debug("Received message: {} {}", message.getClass(), message);
+        this.messages.add(message);
+    }
+
+    @Override
+    public synchronized void onSessionUp(final BmpSession session) {
+        LOG.debug("Session up.");
+        this.up = true;
+    }
+
+    @Override
+    public void onSessionDown(final BmpSession session, final Exception e) {
+        LOG.debug("Session down.", e);
+        this.up = false;
+    }
+}
+
+
