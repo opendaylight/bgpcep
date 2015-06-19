6db0a0f17aeebae7c2d7db67aa9b4c26b97688d2
Dana Kutenicsova <dkutenic@cisco.com>
2015-06-19 13:03:42 +0200

    Created new classes for serializing/deserializing only.
    Codecs and RibSupportContext should be separate
    concepts.
    
    Change-Id: I953c36526775105bbee12d3530984a5ee1d9925c
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

---
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/spi/CodecsRegistry.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl.spi;
+
+import org.opendaylight.protocol.bgp.rib.impl.Codecs;
+import org.opendaylight.protocol.bgp.rib.spi.RIBSupport;
+
+/**
+ * Registry for Codec classes
+ *
+ */
+public interface CodecsRegistry {
+
+    /**
+     * Return the Codecs class registered for given RIBSupport.
+     *
+     * @param ribSupport associated with Codecs class
+     * @return Codecs
+     */
+    Codecs getCodecs(final RIBSupport ribSupport);
+}
