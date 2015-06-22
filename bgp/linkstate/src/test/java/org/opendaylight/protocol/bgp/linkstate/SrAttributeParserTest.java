0db565df276c1c31131bfd3d35da55d9c4021d5e
Iveta Halanova <iveta.halanova@pantheon.sk>
2015-06-22 15:06:52 +0200

    Fix SrNodeAttributesParser + test
    expanded tests by parsering serialized outputs
    fixed seralization, parsering
    
    Change-Id: I8382ab97d1b19791ba5395a69d1a9873be6cf8e5
    Signed-off-by: Iveta Halanova <iveta.halanova@pantheon.sk>

--- bgp/linkstate/src/test/java/org/opendaylight/protocol/bgp/linkstate/SrAttributeParserTest.java
+++ bgp/linkstate/src/test/java/org/opendaylight/protocol/bgp/linkstate/SrAttributeParserTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2015 Cisco diffs, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -9,8 +9,8 @@ package org.opendaylight.protocol.bgp.linkstate;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-
 import com.google.common.collect.Lists;
+import com.google.common.primitives.Bytes;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import java.lang.reflect.Constructor;
@@ -27,6 +27,7 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpPrefix;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Prefix;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Address;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Prefix;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.link.state.SrAdjId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.link.state.SrAdjIdBuilder;
@@ -49,37 +50,96 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segm
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.label.binding.SubTlvs;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.label.binding.SubTlvsBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.EroMetricCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ipv4EroBackupCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ipv4EroCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ipv6EroBackupCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.Ipv6EroCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.SidLabelCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.UnnumberedEroBackupCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev150206.sid.sub.tlvs.subtlv.type.UnnumberedEroCaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.concepts.rev131125.IsoSystemIdentifier;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.concepts.rev131125.TeMetric;
 
 public class SrAttributeParserTest {
 
+    private static final byte[] ipv6aBytes = { 0x20, 1, 0x0d, (byte) 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+    private static final Ipv6Address ipv6a = new Ipv6Address("2001:db8::1");
+    private static final byte[] ipv6bBytes = { 0x20, 1, 0x0d, (byte) 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 };
+    private static final Ipv6Address ipv6b = new Ipv6Address("2001:db8::2");
+    private static final byte[] fecIpv6Bytes = { 0x20, 1, 0, 0, 0, 0, 0, 0 };
+    private static final Ipv6Prefix fecIpv6 = new Ipv6Prefix("2001::/64");
+
+    /*
+      0                   1                   2                   3
+      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     |      Type     |     Length    |     Flags     |     Weight    |
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     |            Range              | Prefix Length |  FEC Prefix   |
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     //           FEC Prefix (continued, variable)                  //
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     |                    SubTLVs (variable)                         |
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+     tools.ietf.org/html/draft-ietf-isis-segment-routing-extensions-02#section-2.4
+     */
+
     @Test
     public void testSidLabelBindingv4() {
-        final byte[] bytes = { (byte)0xC0, 24, 0, 42, 8, 127, 1, 4, 1, 2, 3, 4, 2, 4, 0, 0, 0, 8, 3, 5,(byte)0x80, 10, 0, 0, 1, 5, 9, (byte)0x80, 1, 2, 3, 4, 0, 0, 1, (byte)0xF4 };
+        // expected bytes according to format above
+        final byte[] sidExpectedBytes = { (byte)0xC0, 24,
+            0, 42,
+            32, 127, 0, 0, 0,
+            1, 4, 1, 2, 3, 4,
+            2, 1, 8,
+            3, 5, (byte)0x80, 10, 0, 0, 1,
+            6, 5, (byte)0x80, 10, 0, 0, 2,
+            5, 9, (byte)0x80, 1, 2, 3, 4, 0, 0, 1, (byte)0xF4};
+        final byte[] expectedBytes = Bytes.concat(new byte[] {(byte)0x95, (byte)sidExpectedBytes.length}, sidExpectedBytes);
+
         final List<SubTlvs> subs = new ArrayList<>();
         subs.add(new SubTlvsBuilder().setSubtlvType(new SidLabelCaseBuilder().setSid(new SidLabel(new byte[] {1,2,3,4})).build()).build());
         subs.add(new SubTlvsBuilder().setSubtlvType(new EroMetricCaseBuilder().setEroMetric(new TeMetric((long) 8)).build()).build());
         subs.add(new SubTlvsBuilder().setSubtlvType(new Ipv4EroCaseBuilder().setLoose(true).setAddress(new IpAddress(new Ipv4Address("10.0.0.1"))).build()).build());
+        subs.add(new SubTlvsBuilder().setSubtlvType(new Ipv4EroBackupCaseBuilder().setLoose(true).setAddress(new IpAddress(new Ipv4Address("10.0.0.2"))).build()).build());
         subs.add(new SubTlvsBuilder().setSubtlvType(new UnnumberedEroCaseBuilder().setLoose(true).setRouterId(new byte[] {1,2,3,4}).setInterfaceId((long)500).build()).build());
-        final SrSidLabel b4 = new SrSidLabelBuilder().setSidLabelFlags(new SidLabelFlags(Boolean.TRUE, Boolean.TRUE))
-            .setWeight(new Weight((short) 24)).setFecPrefix(new IpPrefix(new Ipv4Prefix("127.0.0.0/8"))).setValueRange(42).setSubTlvs(subs).build();
-        final ByteBuf b = Unpooled.buffer();
-        SrNodeAttributesParser.serializeSidLabelBinding(b4, b);
-        assertArrayEquals(bytes, ByteArray.readAllBytes(b));
+        final SrSidLabel srSidLabelToSerialize = new SrSidLabelBuilder().setSidLabelFlags(new SidLabelFlags(Boolean.TRUE, Boolean.TRUE))
+            .setWeight(new Weight((short) 24)).setFecPrefix(new IpPrefix(new Ipv4Prefix("127.0.0.0/32"))).setValueRange(42).setSubTlvs(subs).build();
+
+        final ByteBuf serializedSrSidLabel = Unpooled.buffer();
+        SrNodeAttributesParser.serializeSidLabelBinding(srSidLabelToSerialize, serializedSrSidLabel);
+        assertArrayEquals(expectedBytes, ByteArray.getAllBytes(serializedSrSidLabel));
+
+        final SrSidLabel parsedSrSidLabel = SrNodeAttributesParser.parseSidLabelBinding(serializedSrSidLabel);
+        assertEquals(srSidLabelToSerialize, parsedSrSidLabel);
     }
 
     @Test
     public void testSidLabelBindingv6() {
-        final byte[] bytes = { (byte)0xC0, 24, 0, 42, 8, (byte)0x20 };
-        final SrSidLabel b4 = new SrSidLabelBuilder().setSidLabelFlags(new SidLabelFlags(Boolean.TRUE, Boolean.TRUE))
-            .setWeight(new Weight((short) 24)).setFecPrefix(new IpPrefix(new Ipv6Prefix("2001::1/8"))).setValueRange(42).build();
-        final ByteBuf b = Unpooled.buffer();
-        SrNodeAttributesParser.serializeSidLabelBinding(b4, b);
-        assertArrayEquals(bytes, ByteArray.readAllBytes(b));
+        // expected bytes according to format above
+        final byte[] sidExpectedBytes = Bytes.concat(new byte[] {
+            (byte)0xC0, 24,
+            0, 42,
+            (byte)(fecIpv6Bytes.length * Byte.SIZE) }, fecIpv6Bytes, new byte[] {
+                4, (byte)(ipv6aBytes.length+1), (byte)0x80}, ipv6aBytes, new byte[] {
+                    7, (byte)(ipv6bBytes.length+1), (byte)0x80}, ipv6bBytes, new byte[] {
+                        8, 21, (byte)0x80, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 , 0, 0, 1, (byte)0xF4}); // UnnumberedEro: type 5, length 20 for ipv6 1 + 8 + 8 + 3B
+        final byte[] expectedBytes = Bytes.concat(new byte[] {(byte)0x95, (byte)sidExpectedBytes.length}, sidExpectedBytes);
+
+        final List<SubTlvs> subs = new ArrayList<>();
+        subs.add(new SubTlvsBuilder().setSubtlvType(new Ipv6EroCaseBuilder().setLoose(true).setAddress(new IpAddress(ipv6a)).build()).build());
+        subs.add(new SubTlvsBuilder().setSubtlvType(new Ipv6EroBackupCaseBuilder().setLoose(true).setAddress(new IpAddress(ipv6b)).build()).build());
+        subs.add(new SubTlvsBuilder().setSubtlvType(new UnnumberedEroBackupCaseBuilder().setLoose(true).setRouterId(new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}).setInterfaceId((long)500).build()).build());
+        final SrSidLabel srSidLabelToSerialize = new SrSidLabelBuilder().setSidLabelFlags(new SidLabelFlags(Boolean.TRUE, Boolean.TRUE))
+            .setWeight(new Weight((short) 24)).setFecPrefix(new IpPrefix(fecIpv6)).setValueRange(42).setSubTlvs(subs).build();
+
+        final ByteBuf serializedSrSidLabel = Unpooled.buffer();
+        SrNodeAttributesParser.serializeSidLabelBinding(srSidLabelToSerialize, serializedSrSidLabel);
+        assertArrayEquals(expectedBytes, ByteArray.getAllBytes(serializedSrSidLabel));
+
+        final SrSidLabel parsedSrSidLabel = SrNodeAttributesParser.parseSidLabelBinding(serializedSrSidLabel);
+        assertEquals(srSidLabelToSerialize, parsedSrSidLabel);
     }
 
     @Test
