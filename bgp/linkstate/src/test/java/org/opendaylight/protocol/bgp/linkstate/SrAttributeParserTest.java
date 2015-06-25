049cb6ef0c84230ddccd4b330d501da36a7757b8
Iveta Halanova <iveta.halanova@pantheon.sk>
2015-06-25 14:05:15 +0200

    Bug-2848: BGP-LS EPE extension: Peer Sid
    Parsering/serializing for Peer-SID, Peer-Set-SID
    
    Change-Id: I56b4c2c131c205fa727f4261fc9cd06d4b39dd63
    Signed-off-by: Iveta Halanova <iveta.halanova@pantheon.sk>

--- bgp/linkstate/src/test/java/org/opendaylight/protocol/bgp/linkstate/SrAttributeParserTest.java
+++ bgp/linkstate/src/test/java/org/opendaylight/protocol/bgp/linkstate/SrAttributeParserTest.java
@@ -183,7 +183,7 @@ public class SrAttributeParserTest {
         final byte[] sidLabel = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
         final SrAdjId srAdjId = new SrAdjIdBuilder().setFlags(new AdjacencyFlags(Boolean.TRUE, Boolean.FALSE, Boolean.TRUE, Boolean.FALSE, Boolean.TRUE)).setWeight(new Weight((short) 10)).setSid(new SidLabel(sidLabel)).build();
         final ByteBuf serializedData = SrLinkAttributesParser.serializeAdjacencySegmentIdentifier(srAdjId);
-        assertEquals(srAdjId, SrLinkAttributesParser.parseAdjacencySegmentIdentifier(Unpooled.wrappedBuffer(tested)));
+        assertEquals(srAdjId, new SrAdjIdBuilder(SrLinkAttributesParser.parseAdjacencySegmentIdentifier(Unpooled.wrappedBuffer(tested))).build());
         assertArrayEquals(tested, ByteArray.readAllBytes(serializedData));
     }
 
