4bc6fc8e32736206e0467623a043226dc9b7e6b6
Iveta Halanova <iveta.halanova@pantheon.sk>
2015-07-02 08:04:35 +0200

    Test coverage: bmp.spi.parser
    Tests expansion
    
    Change-Id: I5a0c470fb7f40bd098f72e182c0537a657f52bea
    Signed-off-by: Iveta Halanova <iveta.halanova@pantheon.sk>

--- bgp/bmp-spi/src/test/java/org/opendaylight/protocol/bmp/spi/parser/AbstractBmpPerPeerMessageParserTest.java
+++ bgp/bmp-spi/src/test/java/org/opendaylight/protocol/bmp/spi/parser/AbstractBmpPerPeerMessageParserTest.java
@@ -10,7 +10,6 @@ package org.opendaylight.protocol.bmp.spi.parser;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import org.junit.Before;
@@ -22,9 +21,15 @@ import org.opendaylight.protocol.util.ByteArray;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.AsNumber;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Address;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev100924.Timestamp;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.AdjRibInType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.DistinguisherType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.PeerType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.PeerUpNotification;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.PeerUpNotificationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.peer.Distinguisher;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.peer.DistinguisherBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.peer.header.PeerHeader;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.peer.header.PeerHeaderBuilder;
 import org.opendaylight.yangtools.concepts.Builder;
@@ -36,12 +41,21 @@ import org.opendaylight.yangtools.yang.binding.Notification;
 public class AbstractBmpPerPeerMessageParserTest {
     protected BGPExtensionProviderContext ctx;
     private AbstractBmpPerPeerMessageParser<?> parser;
+    final byte[] ipv6MsgWithDistinguishergBytes = {
+        (byte) 0x01, (byte) 0xc0,
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x05, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x03, //Distinguisher
+        (byte) 0x20, (byte) 0x01, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, //IPV6
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x01, //2001::01
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0xA8,  //AS 168
+        (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x02,  //Peer BGP ID 1.1.1.2
+        0, 0, 0, 0, 0, 0, 0, 0
+    };
 
     @Before
     public void setUp() {
         this.ctx = ServiceLoaderBGPExtensionProviderContext.getSingletonInstance();
         final MessageRegistry msgRegistry = this.ctx.getMessageRegistry();
-        parser = new AbstractBmpPerPeerMessageParser<Builder<?>>(msgRegistry) {
+        this.parser = new AbstractBmpPerPeerMessageParser<Builder<?>>(msgRegistry) {
             @Override
             public Notification parseMessageBody(final ByteBuf bytes) {
                 return null;
@@ -79,8 +93,51 @@ public class AbstractBmpPerPeerMessageParserTest {
         phBuilder.setTimestampMicro(new Timestamp(16909060L));
         assertEquals(perHeader, phBuilder.build());
 
-        ByteBuf aggregator = Unpooled.buffer();
+        final ByteBuf aggregator = Unpooled.buffer();
         this.parser.serializePerPeerHeader(perHeader, aggregator);
         assertArrayEquals(msgBytes, ByteArray.getAllBytes(aggregator));
+    }
+
+    @Test
+    public void testPerPeerHeaderIpv6() {
+
+        final PeerHeader perHeader = this.parser.parsePerPeerHeader(Unpooled.wrappedBuffer(this.ipv6MsgWithDistinguishergBytes));
+
+        final PeerHeaderBuilder phBuilder = new PeerHeaderBuilder();
+        phBuilder.setType(PeerType.L3vpn);
+        final Distinguisher dist = new DistinguisherBuilder().setDistinguisher("5:3").setDistinguisherType(DistinguisherType.Type0).build();
+        phBuilder.setDistinguisher(dist);
+        phBuilder.setAdjRibInType(AdjRibInType.forValue(1));
+        phBuilder.setIpv4(false);
+        phBuilder.setAddress(new IpAddress(new Ipv6Address("2001::1")));
+        phBuilder.setAs(new AsNumber(168L));
+        phBuilder.setBgpId(new Ipv4Address("1.1.1.2"));
+        phBuilder.setTimestampSec(new Timestamp(0L));
+        phBuilder.setTimestampMicro(new Timestamp(0L));
+
+        assertEquals(perHeader, phBuilder.build());
+
+        final ByteBuf aggregator = Unpooled.buffer();
+        phBuilder.setTimestampSec(null);
+        phBuilder.setTimestampMicro(null);
+        this.parser.serializePerPeerHeader(phBuilder.build(), aggregator);
+        assertArrayEquals(this.ipv6MsgWithDistinguishergBytes, ByteArray.getAllBytes(aggregator));
+    }
+
+    @Test
+    public void testBgpMessageRegistry() {
+        final MessageRegistry msgRegistry = this.ctx.getMessageRegistry();
+        assertEquals(msgRegistry, this.parser.getBgpMessageRegistry());
+    }
+
+    @Test
+    public void testSerializeMessageBody() {
+        final PeerHeader perHeader = this.parser.parsePerPeerHeader(Unpooled.wrappedBuffer(this.ipv6MsgWithDistinguishergBytes));
+
+        final PeerUpNotification peerNotif = new PeerUpNotificationBuilder().setPeerHeader(perHeader).build();
+
+        final ByteBuf aggregator = Unpooled.buffer();
+        this.parser.serializeMessageBody(peerNotif, aggregator);
+        assertArrayEquals(this.ipv6MsgWithDistinguishergBytes, ByteArray.getAllBytes(aggregator));
     }
 }
