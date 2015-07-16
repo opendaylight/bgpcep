ad17ae33fe0edc921d8e7f76025c0f64657d53a0
Iveta Halanova <iveta.halanova@pantheon.sk>
2015-07-16 15:23:09 +0200

    PCEP TLVs extensions
        - yang model augmented by LSP-DB-VERSION and SPEAKER-ENTITY-ID
        - parsers/serializers implementation
    
    Change-Id: I07d2da05f27a5fdb1fe87ebdb58793ab59a08012
    Signed-off-by: Iveta Halanova <iveta.halanova@pantheon.sk>

---
+++ pcep/ietf-stateful07/src/main/java/org/opendaylight/protocol/pcep/sync/optimizations/SyncOptimizationsLspObjectParser.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.pcep.sync.optimizations;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.pcep.ietf.stateful07.Stateful07LspObjectParser;
+import org.opendaylight.protocol.pcep.spi.TlvRegistry;
+import org.opendaylight.protocol.pcep.spi.VendorInformationTlvRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.pcep.sync.optimizations.rev150714.LspDbVersionTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.pcep.sync.optimizations.rev150714.Tlvs1;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.lsp.object.lsp.Tlvs;
+
+public class SyncOptimizationsLspObjectParser extends Stateful07LspObjectParser {
+
+    public SyncOptimizationsLspObjectParser(final TlvRegistry tlvReg, final VendorInformationTlvRegistry viTlvReg) {
+        super(tlvReg, viTlvReg);
+    }
+
+    @Override
+    public void serializeTlvs(final Tlvs tlvs, final ByteBuf body) {
+        if (tlvs == null) {
+            return;
+        }
+        if (tlvs.getLspErrorCode() != null) {
+            serializeTlv(tlvs.getLspErrorCode(), body);
+        }
+        if (tlvs.getLspIdentifiers() != null) {
+            serializeTlv(tlvs.getLspIdentifiers(), body);
+        }
+        if (tlvs.getRsvpErrorSpec() != null) {
+            serializeTlv(tlvs.getRsvpErrorSpec(), body);
+        }
+        if (tlvs.getSymbolicPathName() != null) {
+            serializeTlv(tlvs.getSymbolicPathName(), body);
+        }
+        if (tlvs.getVsTlv() != null) {
+            serializeTlv(tlvs.getVsTlv(), body);
+        }
+        serializeVendorInformationTlvs(tlvs.getVendorInformationTlv(), body);
+        if (tlvs.getPathBinding() != null) {
+            serializeTlv(tlvs.getPathBinding(), body);
+        }
+        serializeAugmentation(tlvs.getAugmentation(Tlvs1.class), body);
+    }
+
+    private void serializeAugmentation(final Tlvs1 tlv, final ByteBuf body) {
+        Preconditions.checkArgument(tlv instanceof LspDbVersionTlv, "TLV object is not instance of LspDbVersionTlv.");
+        final LspDbVersionTlv dbVersion = tlv;
+        new LspDbVersionTlvParser().serializeTlv(dbVersion.getLspDbVersion(), body);
+    }
+}
