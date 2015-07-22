8f1709a9f4b1a45e6dc3b44d9327700f513247b0
Iveta Halanova <iveta.halanova@pantheon.sk>
2015-07-22 08:57:21 +0200

    Sonar warning fix: too long method
    Change-Id: I4fdc2f39e465fa0266e4d202fbf5f7b69a2de4fa
    Signed-off-by: Iveta Halanova <iveta.halanova@pantheon.sk>

--- bgp/rib-mock/src/main/java/org/opendaylight/protocol/bgp/rib/mock/EventBusRegistration.java
+++ bgp/rib-mock/src/main/java/org/opendaylight/protocol/bgp/rib/mock/EventBusRegistration.java
@@ -83,88 +83,79 @@ final class EventBusRegistration extends AbstractListenerRegistration<BGPSession
                     }
                 }
             }
-
-            listener.onSessionUp(new BGPSession() {
-
-                private static final long AS = 30L;
-
-                @Override
-                public void channelRegistered(final ChannelHandlerContext channelHandlerContext) throws Exception {
-
-                }
-
-                @Override
-                public void channelUnregistered(final ChannelHandlerContext channelHandlerContext) throws Exception {
-
-                }
-
-                @Override
-                public void channelActive(final ChannelHandlerContext channelHandlerContext) throws Exception {
-
-                }
-
-                @Override
-                public void channelInactive(final ChannelHandlerContext channelHandlerContext) throws Exception {
-
-                }
-
-                @Override
-                public void channelRead(final ChannelHandlerContext channelHandlerContext, final Object o) throws Exception {
-
-                }
-
-                @Override
-                public void channelReadComplete(final ChannelHandlerContext channelHandlerContext) throws Exception {
-
-                }
-
-                @Override
-                public void userEventTriggered(final ChannelHandlerContext channelHandlerContext, final Object o) throws Exception {
-
-                }
-
-                @Override
-                public void channelWritabilityChanged(final ChannelHandlerContext channelHandlerContext) throws Exception {
-
-                }
-
-                @Override
-                public void handlerAdded(final ChannelHandlerContext channelHandlerContext) throws Exception {
-
-                }
-
-                @Override
-                public void handlerRemoved(final ChannelHandlerContext channelHandlerContext) throws Exception {
-
-                }
-
-                @Override
-                public void exceptionCaught(final ChannelHandlerContext channelHandlerContext, final Throwable throwable) throws Exception {
-
-                }
-
-                @Override
-                public void close() {
-                    LOG.debug("Session {} closed", this);
-                }
-
-                @Override
-                public Set<BgpTableType> getAdvertisedTableTypes() {
-                    return tts;
-                }
-
-                @Override
-                public Ipv4Address getBgpId() {
-                    return new Ipv4Address("127.0.0.1");
-                }
-
-                @Override
-                public AsNumber getAsNumber() {
-                    return new AsNumber(AS);
-                }
-            });
+            activateSession(listener, tts);
         } else if (!(message instanceof Keepalive)) {
             listener.onMessage(null, message);
         }
+    }
+
+    private static void activateSession(final BGPSessionListener listener, final Set<BgpTableType> tts) {
+        listener.onSessionUp(new BGPSession() {
+            private static final long AS = 30L;
+
+            @Override
+            public void channelRegistered(final ChannelHandlerContext channelHandlerContext) throws Exception {
+            }
+
+            @Override
+            public void channelUnregistered(final ChannelHandlerContext channelHandlerContext) throws Exception {
+            }
+
+            @Override
+            public void channelActive(final ChannelHandlerContext channelHandlerContext) throws Exception {
+            }
+
+            @Override
+            public void channelInactive(final ChannelHandlerContext channelHandlerContext) throws Exception {
+            }
+
+            @Override
+            public void channelRead(final ChannelHandlerContext channelHandlerContext, final Object o) throws Exception {
+            }
+
+            @Override
+            public void channelReadComplete(final ChannelHandlerContext channelHandlerContext) throws Exception {
+            }
+
+            @Override
+            public void userEventTriggered(final ChannelHandlerContext channelHandlerContext, final Object o) throws Exception {
+            }
+
+            @Override
+            public void channelWritabilityChanged(final ChannelHandlerContext channelHandlerContext) throws Exception {
+            }
+
+            @Override
+            public void handlerAdded(final ChannelHandlerContext channelHandlerContext) throws Exception {
+            }
+
+            @Override
+            public void handlerRemoved(final ChannelHandlerContext channelHandlerContext) throws Exception {
+            }
+
+            @Override
+            public void exceptionCaught(final ChannelHandlerContext channelHandlerContext, final Throwable throwable) throws Exception {
+            }
+
+            @Override
+            public void close() {
+                LOG.debug("Session {} closed", this);
+            }
+
+            @Override
+            public Set<BgpTableType> getAdvertisedTableTypes() {
+                return tts;
+            }
+
+            @Override
+            public Ipv4Address getBgpId() {
+                return new Ipv4Address("127.0.0.1");
+            }
+
+            @Override
+            public AsNumber getAsNumber() {
+                return new AsNumber(AS);
+            }
+        });
     }
 }
