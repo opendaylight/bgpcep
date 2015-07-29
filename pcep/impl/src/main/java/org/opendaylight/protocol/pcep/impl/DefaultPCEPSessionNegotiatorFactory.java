b16be46eb6d1ea7b27d4e8bea088f3ccee91bf1b
Iveta Halanova <iveta.halanova@pantheon.sk>
2015-07-29 14:14:04 +0200

    BUG-139: WIP
    -configurable capabilities
    -peer proposal
    
    Change-Id: Id9a6cf7f6cd24823430084dcaf10f60a70ea42f2
    Signed-off-by: Milos Fabian <milfabia@cisco.com>
    Signed-off-by: Iveta Halanova <iveta.halanova@pantheon.sk>

--- pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/DefaultPCEPSessionNegotiatorFactory.java
+++ pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/DefaultPCEPSessionNegotiatorFactory.java
@@ -10,28 +10,31 @@ package org.opendaylight.protocol.pcep.impl;
 import com.google.common.base.Preconditions;
 import io.netty.channel.Channel;
 import io.netty.util.concurrent.Promise;
+import java.net.InetSocketAddress;
 import org.opendaylight.controller.config.yang.pcep.impl.Tls;
+import org.opendaylight.protocol.pcep.PCEPPeerProposal;
 import org.opendaylight.protocol.pcep.PCEPSessionListener;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.open.object.Open;
+import org.opendaylight.protocol.pcep.PCEPSessionProposalFactory;
 
 public final class DefaultPCEPSessionNegotiatorFactory extends AbstractPCEPSessionNegotiatorFactory {
-    private final Open localPrefs;
+    private final PCEPSessionProposalFactory spf;
     private final int maxUnknownMessages;
     private final Tls tlsConfiguration;
 
-    public DefaultPCEPSessionNegotiatorFactory(final Open localPrefs, final int maxUnknownMessages) {
-        this(localPrefs, maxUnknownMessages, null);
+    public DefaultPCEPSessionNegotiatorFactory(final PCEPSessionProposalFactory spf, final int maxUnknownMessages) {
+        this(spf, maxUnknownMessages, null);
     }
 
-    public DefaultPCEPSessionNegotiatorFactory(final Open localPrefs, final int maxUnknownMessages, final Tls tlsConfiguration) {
-        this.localPrefs = Preconditions.checkNotNull(localPrefs);
+    public DefaultPCEPSessionNegotiatorFactory(final PCEPSessionProposalFactory spf, final int maxUnknownMessages, final Tls tlsConfiguration) {
+        this.spf = Preconditions.checkNotNull(spf);
         this.maxUnknownMessages = maxUnknownMessages;
         this.tlsConfiguration = tlsConfiguration;
     }
 
     @Override
     protected AbstractPCEPSessionNegotiator createNegotiator(final Promise<PCEPSessionImpl> promise, final PCEPSessionListener listener,
-            final Channel channel, final short sessionId) {
-        return new DefaultPCEPSessionNegotiator(promise, channel, listener, sessionId, this.maxUnknownMessages, this.localPrefs, this.tlsConfiguration);
+            final Channel channel, final short sessionId, final PCEPPeerProposal peerProposal) {
+        return new DefaultPCEPSessionNegotiator(promise, channel, listener, sessionId, this.maxUnknownMessages,
+                this.spf.getSessionProposal((InetSocketAddress)channel.remoteAddress(), sessionId, peerProposal), this.tlsConfiguration);
     }
 }
