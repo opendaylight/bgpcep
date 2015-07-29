21af5ef1c1cf1bb15f2751dcd5952671d5954be9
Iveta Halanova <iveta.halanova@pantheon.sk>
2015-07-29 14:14:04 +0200

    BUG-139: PCEP capabilities refactor
    Created pcep-capability indentity in exchange for factory of stateful07 session proposal.
    PCEPCapability is implemented for stateful07 as PCEPStatefulCapability and for segment
    routing as PCEPSegmentRoutingCapability.
    Capabilities are add via TLVs to Open message.
    Code preparation for including LS DB version number in Open message.
    Unit tests updates and corretion of other influenced code.
    
    Change-Id: Id9a6cf7f6cd24823430084dcaf10f60a70ea42f2
    Signed-off-by: Milos Fabian <milfabia@cisco.com>
    Signed-off-by: Iveta Halanova <iveta.halanova@pantheon.sk>

--- pcep/api/src/main/java/org/opendaylight/protocol/pcep/PCEPSessionProposalFactory.java
+++ pcep/api/src/main/java/org/opendaylight/protocol/pcep/PCEPSessionProposalFactory.java
@@ -8,7 +8,6 @@
 package org.opendaylight.protocol.pcep;
 
 import java.net.InetSocketAddress;
-
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.open.object.Open;
 
 /**
@@ -25,4 +24,14 @@ public interface PCEPSessionProposalFactory {
      * @return specific session proposal
      */
     Open getSessionProposal(InetSocketAddress address, int sessionId);
+
+    /**
+     *
+     * @param address serves as constraint, so that factory is able to return different proposals for different
+     *        addresses
+     * @param sessionId is used for creation of PCEPOpenObject
+     * @param peerProposal for including information from peer to our Open message
+     * @return specific session proposal
+     */
+    Open getSessionProposal(InetSocketAddress address, int sessionId, PCEPPeerProposal peerProposal);
 }
