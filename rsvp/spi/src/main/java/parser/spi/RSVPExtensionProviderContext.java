a95646a6b6a2d5f126841c395b25eb0f5d826558
Claudio D. Gasparini <cgaspari@cisco.com>
2015-08-18 19:16:56 +0200

    BUG 2230: Parser SPI
    Change-Id: I3f3ae65776fd9c7487c7d684a6ce7bf728d036ed
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ rsvp/spi/src/main/java/parser/spi/RSVPExtensionProviderContext.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package parser.spi;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.RsvpTeObject;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.basic.explicit.route.subobjects.SubobjectType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.LabelType;
+
+public interface RSVPExtensionProviderContext extends RSVPExtensionConsumerContext {
+    void registerRsvpObjectParser(int classNum, int cType, RSVPTeObjectParser parser);
+
+    void registerRsvpObjectSerializer(Class<? extends RsvpTeObject> objectClass, int cType, RSVPTeObjectSerializer serializer);
+
+    AutoCloseable registerXROSubobjectSerializer(Class<? extends SubobjectType> subobjectClass, XROSubobjectSerializer serializer);
+
+    AutoCloseable registerXROSubobjectParser(int subobjectType, XROSubobjectParser parser);
+
+    AutoCloseable registerRROSubobjectSerializer(Class<? extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.record.route.subobjects.SubobjectType> subobjectClass, RROSubobjectSerializer serializer);
+
+    AutoCloseable registerRROSubobjectParser(int subobjectType, RROSubobjectParser parser);
+
+    AutoCloseable registerEROSubobjectSerializer(Class<? extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.basic.explicit.route.subobjects.SubobjectType> subobjectClass, EROSubobjectSerializer serializer);
+
+    AutoCloseable registerEROSubobjectParser(int subobjectType, EROSubobjectParser parser);
+
+    AutoCloseable registerLabelSerializer(Class<? extends LabelType> labelClass, LabelSerializer serializer);
+
+    AutoCloseable registerLabelParser(int cType, LabelParser parser);
+}
