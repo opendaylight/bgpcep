dd9d326e6b12de86111bf1bdd90952601c426b5e
Dana Kutenicsova <dkutenic@cisco.com>
2015-08-21 15:36:05 +0200

    Refactoring: flowspec operands
    Created a hierarchy of operand classes and removed
    all operand-related code from FSNlriParser.
    
    Change-Id: Ia41d22b16ab7e75fe9959df6a24b558721b0ff2f
    Signed-off-by: Dana Kutenicsova <dkutenic@cisco.com>

---
+++ bgp/flowspec/src/main/java/org/opendaylight/protocol/bgp/flowspec/BitmaskOperandParser.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.flowspec;
+
+import com.google.common.annotations.VisibleForTesting;
+import io.netty.buffer.ByteBuf;
+import java.util.Set;
+import org.opendaylight.protocol.util.BitArray;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.flowspec.rev150807.BitmaskOperand;
+
+/**
+ * Parser class for BitmaskOperand.
+ */
+public final class BitmaskOperandParser extends AbstractOperandParser<BitmaskOperand> {
+
+    public static final BitmaskOperandParser INSTANCE;
+
+    static {
+        INSTANCE = new BitmaskOperandParser();
+    }
+
+    @VisibleForTesting
+    static final String MATCH_VALUE = "match";
+    @VisibleForTesting
+    static final String NOT_VALUE = "not";
+
+    private static final int NOT = 6;
+    private static final int MATCH = 7;
+
+    private BitmaskOperandParser() {
+
+    }
+
+    @Override
+    public BitmaskOperand create(final Set<String> opValues) {
+        return new BitmaskOperand(opValues.contains(AND_BIT_VALUE), opValues.contains(END_OF_LIST_VALUE), opValues.contains(MATCH_VALUE), opValues.contains(NOT_VALUE));
+    }
+
+    @Override
+    public void serialize(final BitmaskOperand op, final int length, final ByteBuf buffer) {
+        final BitArray bs = new BitArray(OPERAND_LENGTH);
+        bs.set(END_OF_LIST, op.isEndOfList());
+        bs.set(AND_BIT, op.isAndBit());
+        bs.set(MATCH, op.isMatch());
+        bs.set(NOT, op.isNot());
+        final byte len = (byte) (Integer.numberOfTrailingZeros(length) << LENGTH_SHIFT);
+        buffer.writeByte(bs.toByte() | len);
+    }
+
+    @Override
+    public BitmaskOperand parse(final byte op) {
+        final BitArray bs = BitArray.valueOf(op);
+        return new BitmaskOperand(bs.get(AND_BIT), bs.get(END_OF_LIST), bs.get(MATCH), bs.get(NOT));
+    }
+
+    @Override
+    public String toString(final BitmaskOperand op, final boolean isFirst) {
+        final StringBuilder buffer = new StringBuilder();
+        if (!op.isAndBit() && !isFirst) {
+            buffer.append("or ");
+        }
+        if (op.isAndBit()) {
+            buffer.append("and ");
+        }
+        if (op.isMatch()) {
+            buffer.append("does ");
+            if (op.isNot()) {
+                buffer.append("not ");
+            }
+            buffer.append("match ");
+        } else if (op.isNot()) {
+            buffer.append("is not ");
+        }
+        return buffer.toString();
+    }
+}
---
+++ bgp/flowspec/src/main/java/org/opendaylight/protocol/bgp/flowspec/NumericOneByteOperandParser.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.flowspec;
+
+import io.netty.buffer.ByteBuf;
+import java.util.List;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.flowspec.rev150807.NumericOneByteValue;
+
+/**
+ * Parser class for NumericOneByteValues.
+ */
+public final class NumericOneByteOperandParser extends AbstractNumericOperandParser<NumericOneByteValue> {
+
+    public static final NumericOneByteOperandParser INSTANCE;
+
+    static {
+        INSTANCE = new NumericOneByteOperandParser();
+    }
+
+    private NumericOneByteOperandParser() {
+
+    }
+
+    /**
+     * Serializes Flowspec component type that has maximum of 1B sized value field and numeric operand.
+     *
+     * @param list of operands to be serialized
+     * @param nlriByteBuf where the operands will be serialized
+     */
+    @Override
+    public final <T extends NumericOneByteValue> void serialize(final List<T> list, final ByteBuf nlriByteBuf) {
+        for (final T operand : list) {
+            super.serialize(operand.getOp(), 1, nlriByteBuf);
+            Util.writeShortest(operand.getValue(), nlriByteBuf);
+        }
+    }
+
+    // TODO: duplicate code with NumericTwoByteValue
+    @Override
+    public final<T extends NumericOneByteValue> String toString(final List<T> list) {
+        final StringBuilder buffer = new StringBuilder();
+        boolean isFirst = true;
+        for (final T item : list) {
+            buffer.append(super.toString(item.getOp(), isFirst));
+            buffer.append(item.getValue());
+            buffer.append(' ');
+            if (isFirst) {
+                isFirst = false;
+            }
+        }
+        return buffer.toString();
+    }
+}
