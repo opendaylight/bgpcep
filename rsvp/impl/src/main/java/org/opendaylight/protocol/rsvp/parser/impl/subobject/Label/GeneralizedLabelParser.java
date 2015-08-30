c11b5a905e066e09aff9b7645742c1ec1d829b5b
Claudio D. Gasparini <cgaspari@cisco.com>
2015-08-30 14:57:09 +0200

    BUG  2230: RSVP Subobjets
    -Implentation of Label Subobjects parsers/serializers
    -Implentation of ASNumber parser/serializer
    -Implentation of Label Subobjects parsers/serializers tests
    
    Change-Id: I348f241c73fa5928f20762356e45b7f55486a8a0
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ rsvp/impl/src/main/java/org/opendaylight/protocol/rsvp/parser/impl/subobject/Label/GeneralizedLabelParser.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.rsvp.parser.impl.subobject.Label;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.opendaylight.protocol.rsvp.parser.spi.LabelParser;
+import org.opendaylight.protocol.rsvp.parser.spi.LabelSerializer;
+import org.opendaylight.protocol.rsvp.parser.spi.RSVPParsingException;
+import org.opendaylight.protocol.util.ByteArray;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.LabelType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.label.type.GeneralizedLabelCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.label.type.GeneralizedLabelCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.label.type.generalized.label._case.GeneralizedLabel;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.label.type.generalized.label._case.GeneralizedLabelBuilder;
+
+/**
+ * Parser for {@link GeneralizedLabelCase}
+ */
+public class GeneralizedLabelParser implements LabelParser, LabelSerializer {
+
+    public static final int CTYPE = 2;
+
+    @Override
+    public final LabelType parseLabel(final ByteBuf buffer) throws RSVPParsingException {
+        Preconditions.checkArgument(buffer != null && buffer.isReadable(), "Array of bytes is mandatory. Can't be null or empty.");
+        return new GeneralizedLabelCaseBuilder().setGeneralizedLabel(
+            new GeneralizedLabelBuilder().setGeneralizedLabel(ByteArray.readAllBytes(buffer)).build()).build();
+    }
+
+    @Override
+    public final void serializeLabel(final boolean unidirectional, final boolean global, final LabelType subobject,
+                                     final ByteBuf buffer) {
+        Preconditions.checkArgument(subobject instanceof GeneralizedLabelCase, "Unknown Label Subobject instance. Passed {}. Needed GeneralizedLabelCase.", subobject.getClass());
+        final GeneralizedLabel gl = ((GeneralizedLabelCase) subobject).getGeneralizedLabel();
+        Preconditions.checkArgument(gl != null, "GeneralizedLabel is mandatory.");
+        final ByteBuf body = Unpooled.wrappedBuffer(gl.getGeneralizedLabel());
+        LabelUtil.formatLabel(CTYPE, unidirectional, global, body, buffer);
+    }
+}
---
+++ rsvp/impl/src/main/java/org/opendaylight/protocol/rsvp/parser/impl/subobject/Label/Type1LabelParser.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.rsvp.parser.impl.subobject.Label;
+
+import static org.opendaylight.protocol.util.ByteBufWriteUtil.writeUnsignedInt;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.opendaylight.protocol.rsvp.parser.spi.LabelParser;
+import org.opendaylight.protocol.rsvp.parser.spi.LabelSerializer;
+import org.opendaylight.protocol.rsvp.parser.spi.RSVPParsingException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.LabelType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.label.type.Type1LabelCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.label.type.Type1LabelCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.label.type.type1.label._case.Type1Label;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.label.subobject.label.type.type1.label._case.Type1LabelBuilder;
+
+/**
+ * Parser for {@link Type1LabelCase}
+ */
+public class Type1LabelParser implements LabelParser, LabelSerializer {
+
+    public static final int CTYPE = 1;
+
+    public static final int LABEL_LENGTH = 4;
+
+    @Override
+    public LabelType parseLabel(final ByteBuf buffer) throws RSVPParsingException {
+        Preconditions.checkArgument(buffer != null && buffer.isReadable(), "Array of bytes is mandatory. Can't be null or empty.");
+        if (buffer.readableBytes() != LABEL_LENGTH) {
+            throw new RSVPParsingException("Wrong length of array of bytes. Passed: " + buffer.readableBytes() + "; Expected: "
+                + LABEL_LENGTH + ".");
+        }
+        return new Type1LabelCaseBuilder().setType1Label(new Type1LabelBuilder().setType1Label(buffer.readUnsignedInt()).build()).build();
+    }
+
+    @Override
+    public void serializeLabel(final boolean unidirectional, final boolean global, final LabelType subobject, final ByteBuf buffer) {
+        Preconditions.checkArgument(subobject instanceof Type1LabelCase, "Unknown Label Subobject instance. Passed {}. Needed Type1LabelCase.", subobject.getClass());
+        final ByteBuf body = Unpooled.buffer(LABEL_LENGTH);
+        final Type1Label type1Label = ((Type1LabelCase) subobject).getType1Label();
+        Preconditions.checkArgument(type1Label != null, "Type1Label is mandatory.");
+        writeUnsignedInt(type1Label.getType1Label(), body);
+        LabelUtil.formatLabel(CTYPE, unidirectional, global, body, buffer);
+    }
+}
