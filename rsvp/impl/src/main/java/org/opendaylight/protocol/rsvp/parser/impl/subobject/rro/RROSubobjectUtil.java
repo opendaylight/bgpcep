c40085ac8d622d4632b8e7060a2725dcb7586a67
Claudio D. Gasparini <cgaspari@cisco.com>
2015-08-30 14:48:47 +0200

    BUG 2230: RSVP RRO Subobjects
    -Implementation of RRO subobjects parsers/serializers
    
    Change-Id: I06284a94601f0e022c59f323d6d38d47dcd4af3a
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ rsvp/impl/src/main/java/org/opendaylight/protocol/rsvp/parser/impl/subobject/rro/RROSubobjectUtil.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.rsvp.parser.impl.subobject.rro;
+
+import io.netty.buffer.ByteBuf;
+
+class RROSubobjectUtil {
+    private static final int HEADER_SIZE = 2;
+
+    private RROSubobjectUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void formatSubobject(final int type, final ByteBuf body, final ByteBuf buffer) {
+        buffer.writeByte(type);
+        buffer.writeByte(body.writerIndex() + HEADER_SIZE);
+        buffer.writeBytes(body);
+    }
+}
