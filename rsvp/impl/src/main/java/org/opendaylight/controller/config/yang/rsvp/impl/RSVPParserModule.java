b0734992f17e158f51999a7f4974100491067ecf
Claudio D. Gasparini <cgaspari@cisco.com>
2015-08-30 14:05:12 +0200

    BUG 2230: RSVP Impl Extension Service
    Change-Id: I5c7cbf0f205494dd6c0f05353d01a634431db8ed
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ rsvp/impl/src/main/java/org/opendaylight/controller/config/yang/rsvp/impl/RSVPParserModule.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.config.yang.rsvp.impl;
+
+import org.opendaylight.protocol.rsvp.parser.impl.RSVPActivator;
+
+public class RSVPParserModule extends org.opendaylight.controller.config.yang.rsvp.impl.AbstractRSVPParserModule {
+    public RSVPParserModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public RSVPParserModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.rsvp.impl.RSVPParserModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        return new RSVPActivator();
+    }
+
+}
