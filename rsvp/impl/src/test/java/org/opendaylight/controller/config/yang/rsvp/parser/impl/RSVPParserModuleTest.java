14687437b65dac183c72f71353f2049c9e41bdca
Claudio D. Gasparini <cgaspari@cisco.com>
2015-08-30 14:05:12 +0200

    BUG 2230: RSVP Impl Extension Service
    -Implement RSVP Parser extension service yang model
    -Implement RSVP Parser extension service
    -Implement RSVP Parser extension service Test
    
    Change-Id: I5c7cbf0f205494dd6c0f05353d01a634431db8ed
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ rsvp/impl/src/test/java/org/opendaylight/controller/config/yang/rsvp/parser/impl/RSVPParserModuleTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.controller.config.yang.rsvp.parser.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.controller.config.api.jmx.CommitStatus;
+import org.opendaylight.controller.config.manager.impl.AbstractConfigTest;
+import org.opendaylight.controller.config.manager.impl.factoriesresolver.HardcodedModuleFactoriesResolver;
+import org.opendaylight.controller.config.util.ConfigTransactionJMXClient;
+import org.opendaylight.controller.config.yang.rsvp.impl.RSVPParserModuleFactory;
+import org.opendaylight.controller.config.yang.rsvp.spi.SimpleRSVPExtensionProviderContextModuleFactory;
+
+public class RSVPParserModuleTest extends AbstractConfigTest {
+    private static final String FACTORY_NAME = RSVPParserModuleFactory.NAME;
+    private static final String INSTANCE_NAME = "rsvp-parser-impl";
+
+    @Before
+    public void setUp() throws Exception {
+        super.initConfigTransactionManagerImpl(new HardcodedModuleFactoriesResolver(this.mockedContext, new
+            RSVPParserModuleFactory(), new SimpleRSVPExtensionProviderContextModuleFactory()));
+    }
+
+    @Test
+    public void testCreateBean() throws Exception {
+        final CommitStatus status = createInstance();
+        assertBeanCount(1, FACTORY_NAME);
+        assertStatus(status, 1, 0, 0);
+    }
+
+    @Test
+    public void testReusingOldInstance() throws Exception {
+        createInstance();
+        final ConfigTransactionJMXClient transaction = this.configRegistryClient.createTransaction();
+        assertBeanCount(1, FACTORY_NAME);
+        final CommitStatus status = transaction.commit();
+        assertBeanCount(1, FACTORY_NAME);
+        assertStatus(status, 0, 0, 1);
+    }
+
+    private CommitStatus createInstance() throws Exception {
+        final ConfigTransactionJMXClient transaction = this.configRegistryClient.createTransaction();
+        transaction.createModule(FACTORY_NAME, INSTANCE_NAME);
+        return transaction.commit();
+    }
+}
