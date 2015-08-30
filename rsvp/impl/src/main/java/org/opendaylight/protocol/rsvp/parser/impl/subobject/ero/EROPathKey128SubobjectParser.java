366e981daa98dfb380bfb2455ac9ecfd1595eb4c
Claudio D. Gasparini <cgaspari@cisco.com>
2015-08-30 14:28:45 +0200

    BUG 2230: RSVP ERO Subobjects
    -RSVP ERO Subobjects parsers/serializers
    
    Change-Id: I921d6d165667a6290846ca8994aead0b0e6cee4c
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ rsvp/impl/src/main/java/org/opendaylight/protocol/rsvp/parser/impl/subobject/ero/EROPathKey128SubobjectParser.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.rsvp.parser.impl.subobject.ero;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.rsvp.parser.spi.EROSubobjectParser;
+import org.opendaylight.protocol.rsvp.parser.spi.RSVPParsingException;
+import org.opendaylight.protocol.rsvp.parser.spi.subobjects.CommonPathKeyParser;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.explicit.route.object.ero.subobject.subobject.type.PathKeyCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.explicit.route.subobjects.list.SubobjectContainer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.explicit.route.subobjects.list.SubobjectContainerBuilder;
+
+/**
+ * Parser for { PathKey}
+ */
+public class EROPathKey128SubobjectParser extends CommonPathKeyParser implements EROSubobjectParser{
+
+    public static final int TYPE = 65;
+
+    protected static final int PCE128_ID_F_LENGTH = 16;
+
+    private static final int CONTENT128_LENGTH = 2 + PCE128_ID_F_LENGTH;
+
+    @Override
+    public SubobjectContainer parseSubobject(final ByteBuf buffer, final boolean loose) throws RSVPParsingException {
+        Preconditions.checkArgument(buffer != null && buffer.isReadable(), "Array of bytes is mandatory. Can't be null or empty.");
+        if (buffer.readableBytes() != CONTENT128_LENGTH) {
+            throw new RSVPParsingException("Wrong length of array of bytes. Passed: " + buffer.readableBytes() + "; Expected: >"
+                + CONTENT128_LENGTH + ".");
+        }
+        final SubobjectContainerBuilder builder = new SubobjectContainerBuilder();
+        builder.setLoose(loose);
+        builder.setSubobjectType(new PathKeyCaseBuilder().setPathKey(parsePathKey(PCE128_ID_F_LENGTH, buffer)).build());
+        return builder.build();
+    }
+}
