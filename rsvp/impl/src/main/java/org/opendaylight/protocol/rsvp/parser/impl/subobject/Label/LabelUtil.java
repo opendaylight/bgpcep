9b213b53ee17996ef9f9201fd63a86e3ef76c192
Claudio D. Gasparini <cgaspari@cisco.com>
2015-08-30 14:57:09 +0200

    BUG  2230: RSVP Subobjets
    -Implentation of Label Subobjects parsers/serializers
    -Implentation of ASNumber parser/serializer
    -Implentation of Label Subobjects parsers/serializers tests
    
    Change-Id: I348f241c73fa5928f20762356e45b7f55486a8a0
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ rsvp/impl/src/main/java/org/opendaylight/protocol/rsvp/parser/impl/subobject/Label/LabelUtil.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.rsvp.parser.impl.subobject.Label;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.util.BitArray;
+
+public final class LabelUtil {
+
+    private static final int FLAGS_SIZE = 8;
+
+    private static final int UNIDIRECTIONAL = 0;
+    private static final int GLOBAL = 7;
+
+    private LabelUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void formatLabel(final int type, final Boolean unidirectional, final Boolean global, final ByteBuf body, final ByteBuf buffer) {
+        final BitArray reserved = new BitArray(FLAGS_SIZE);
+        reserved.set(UNIDIRECTIONAL, unidirectional);
+        reserved.set(GLOBAL, global);
+        reserved.toByteBuf(buffer);
+        buffer.writeByte(type);
+        buffer.writeBytes(body);
+    }
+}
