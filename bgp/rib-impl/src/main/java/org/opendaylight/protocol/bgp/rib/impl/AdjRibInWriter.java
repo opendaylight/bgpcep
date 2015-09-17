8344beeebac4dc791e2537be1113d148b8bd08a5
Claudio D. Gasparini <cgaspari@cisco.com>
2015-09-17 14:06:58 +0200

    BUG-4070: Define new specific Peer role for App Peer.
    Currently App Peer is defined a iBGP, therefore it doesnt
    advertise to iBGP peers. Setting it as an RR, means that
    it will advertise ORIGINATOR_ID and CLUSTER_ID, which
    is undesirable.
    Instead we create a new role for App Peer, INTERNAL.
    Which will act as an RR-client but without advertise
    undesirable fields ORIGINATOR_ID and CLUSTER_ID.
    
    Change-Id: I1385158806838623824773c3031556fefa2a0262
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibInWriter.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibInWriter.java
@@ -97,6 +97,8 @@ final class AdjRibInWriter {
             return "ibgp";
         case RrClient:
             return "rr-client";
+        case Internal:
+            return "internal";
         default:
             throw new IllegalArgumentException("Unhandled role " + role);
         }
