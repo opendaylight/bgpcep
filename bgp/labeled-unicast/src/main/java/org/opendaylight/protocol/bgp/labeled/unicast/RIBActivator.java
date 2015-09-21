75dc2405853fc55c5b9a172ffd0ea1bd4a12b9d5
Milos Fabian <milfabia@cisco.com>
2015-09-21 16:55:23 +0200

    Bug-3904: BGP-LU RIB support
    -implemented RIB support for BGP-LU routes
    -support ipv4/ipv6 afi routes
    -activator registers new rib support
    
    Change-Id: I30762465979f2dbc82e778bf74a1906b3fdcab82
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/labeled-unicast/src/main/java/org/opendaylight/protocol/bgp/labeled/unicast/RIBActivator.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.labeled.unicast;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.protocol.bgp.rib.spi.AbstractRIBExtensionProviderActivator;
+import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.labeled.unicast.rev150525.LabeledUnicastSubsequentAddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv4AddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv6AddressFamily;
+
+public class RIBActivator extends AbstractRIBExtensionProviderActivator {
+
+    @Override
+    protected List<AutoCloseable> startRIBExtensionProviderImpl(
+            final RIBExtensionProviderContext context) {
+        final List<AutoCloseable> regs = new ArrayList<>();
+        regs.add(context.registerRIBSupport(Ipv4AddressFamily.class, LabeledUnicastSubsequentAddressFamily.class, new LabeledUnicastRIBSupport(Ipv4AddressFamily.class)));
+        regs.add(context.registerRIBSupport(Ipv6AddressFamily.class, LabeledUnicastSubsequentAddressFamily.class, new LabeledUnicastRIBSupport(Ipv6AddressFamily.class)));
+        return regs;
+    }
+}
