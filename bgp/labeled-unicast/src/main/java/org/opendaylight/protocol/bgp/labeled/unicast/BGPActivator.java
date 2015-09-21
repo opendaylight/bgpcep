faa41e6cc2dcf41e1160b7bb40625d764c077077
Milos Fabian <milfabia@cisco.com>
2015-09-21 16:50:35 +0200

    Bug-3904: BGP-LU NLRI encoder/decoder
    -implementation of new NLRI parser/serializers (Ipv4 and Ipv6 prefix support)
    -junit tests
    -bgp activator register handlers
    
    Change-Id: Icf1fef1477d031bebc9a1247f9f4396c3cbb1a4b
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/labeled-unicast/src/main/java/org/opendaylight/protocol/bgp/labeled/unicast/BGPActivator.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.labeled.unicast;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.protocol.bgp.parser.spi.AbstractBGPExtensionProviderActivator;
+import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.labeled.unicast.rev150525.LabeledUnicastSubsequentAddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.labeled.unicast.rev150525.labeled.unicast.routes.LabeledUnicastRoutes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv4AddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv6AddressFamily;
+
+public final class BGPActivator extends AbstractBGPExtensionProviderActivator {
+
+    private static final int LABELED_UNICAST_SAFI = 4;
+
+    @Override
+    protected List<AutoCloseable> startImpl(final BGPExtensionProviderContext context) {
+        final List<AutoCloseable> regs = new ArrayList<>();
+        final LUNlriParser luNlriParser = new LUNlriParser();
+
+        regs.add(context.registerSubsequentAddressFamily(LabeledUnicastSubsequentAddressFamily.class, LABELED_UNICAST_SAFI));
+
+        regs.add(context.registerNlriParser(Ipv4AddressFamily.class, LabeledUnicastSubsequentAddressFamily.class, luNlriParser));
+        regs.add(context.registerNlriParser(Ipv6AddressFamily.class, LabeledUnicastSubsequentAddressFamily.class, luNlriParser));
+
+        regs.add(context.registerNlriSerializer(LabeledUnicastRoutes.class, luNlriParser));
+
+        return regs;
+    }
+
+}
