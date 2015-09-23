771033ddbdedcc88269d25374c2898920259b964
Milos Fabian <milfabia@cisco.com>
2015-09-23 11:53:42 +0200

    BUG-4197: BGP OpenConfig API
    BGP OpenConfig yang models as defined in https://tools.ietf.org/html/draft-ietf-idr-bgp-model-00
    Defines yang data model for configuration and managing BGP.
    Added "odl-openconfig-extensions" Yang data model to support missing AFI/SAFI
    
    Change-Id: I1a59a1c36f6f81b94058cc201115174e41fdfc8b
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/openconfig-api/src/main/java/org/opendaylight/yang/gen/v1/http/openconfig/net/yang/bgp/types/rev150515/RrClusterIdTypeBuilder.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+
+package org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev150515;
+
+import com.google.common.primitives.UnsignedInts;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
+ * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is:
+ * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
+ * loss of user code.
+ *
+ */
+public class RrClusterIdTypeBuilder {
+
+    private static final Pattern IPV4_PATTERN = Pattern.compile("^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(%[\\p{N}\\p{L}]+)?$");
+
+    public static RrClusterIdType getDefaultInstance(final java.lang.String defaultValue) {
+        final Matcher ipv4Matcher = IPV4_PATTERN.matcher(defaultValue);
+        if (ipv4Matcher.matches()) {
+            return new RrClusterIdType(new Ipv4Address(defaultValue));
+        } else {
+            try {
+                final long parseUnsignedInt = UnsignedInts.parseUnsignedInt(defaultValue);
+                return new RrClusterIdType(parseUnsignedInt);
+            } catch (final NumberFormatException e) {
+                throw new IllegalArgumentException("Cannot create RrClusterIdType from " + defaultValue);
+            }
+        }
+    }
+
+}
---
+++ bgp/openconfig-api/src/test/java/org/opendaylight/yang/gen/v1/http/openconfig/net/yang/bgp/policy/rev150515/routing/policy/policy/definitions/policy/definition/statements/statement/actions/bgp/actions/set/community/set/community/method/InlineCommunitiesBuilderTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.policy.rev150515.routing.policy.policy.definitions.policy.definition.statements.statement.actions.bgp.actions.set.community.set.community.method;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.policy.rev150515.routing.policy.policy.definitions.policy.definition.statements.statement.actions.bgp.actions.set.community.set.community.method.Inline.Communities;
+
+public class InlineCommunitiesBuilderTest {
+
+    @Test
+    public void testValid() {
+        final Communities communities = InlineCommunitiesBuilder.getDefaultInstance("123");
+        Assert.assertEquals(123L, communities.getBgpStdCommunityType().getUint32().longValue());
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testInvalid() {
+        InlineCommunitiesBuilder.getDefaultInstance("abc");
+    }
+
+}
