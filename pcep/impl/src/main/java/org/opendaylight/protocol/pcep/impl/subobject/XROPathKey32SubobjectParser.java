88e5c5e006d479827be000b14692ac49b3d4cb3d
Claudio D. Gasparini <cgaspari@cisco.com>
2015-09-25 11:10:11 +0200

    BUG 4181: Fix PCEP Parsers/Serializers
    Objects with more than one Ctype are not serialized
    correctly.
    This required update pcep-types.yang model, and register correctly
    parser/serializers for:
    -PathKEy Object /subobject
    -EndPoint Object
    
    Change-Id: I4518830b606a644a5673568e33f362f30958a51a
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/subobject/XROPathKey32SubobjectParser.java
+++ pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/subobject/XROPathKey32SubobjectParser.java
@@ -59,9 +59,13 @@ public class XROPathKey32SubobjectParser implements XROSubobjectParser, XROSubob
         Preconditions.checkArgument(subobject.getSubobjectType() instanceof PathKeyCase, "Unknown subobject instance. Passed %s. Needed PathKey.", subobject.getSubobjectType().getClass());
         final org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.explicit.route.object.ero.subobject.subobject.type.path.key._case.PathKey pk = ((PathKeyCase) subobject.getSubobjectType()).getPathKey();
         final ByteBuf body = Unpooled.buffer();
+        Preconditions.checkArgument(pk.getPceId() != null, "PceId is mandatory.");
+        if(pk.getPceId().getBinary().length == 16) {
+            XROPathKey128SubobjectParser.serializeSubobject(subobject,buffer);
+        }
         Preconditions.checkArgument(pk.getPathKey() != null, "PathKey is mandatory.");
         writeUnsignedShort(pk.getPathKey().getValue(), body);
-        Preconditions.checkArgument(pk.getPceId() != null, "PceId is mandatory.");
+        Preconditions.checkArgument(pk.getPceId().getBinary().length == 4, "PceId 32 Bit required.");
         body.writeBytes(pk.getPceId().getBinary());
         XROSubobjectUtil.formatSubobject(TYPE, subobject.isMandatory(), body, buffer);
     }
