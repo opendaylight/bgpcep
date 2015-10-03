7381172338c778ec16f48e6bdd05fc9284bfb338
Milos Fabian <milfabia@cisco.com>
2015-10-03 02:18:28 +0200

    BUG-4173: BGP OpenConfig SPI
    Defines the APIs to track a BGP configuration modules status changes and
    BGP OpenConfig data writers consumed as a service injected by config subsystem into
    BGP configuration module.
    The goal is to react on changes in the BGP configuration module instances - the configuration
    modules are mapped to the OpenConfig BGP data model instances and kept in a datastore.
    
    Change-Id: Ic38d5049d4d7e2f51f05a0e44e28626fc48e7ab7
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/openconfig-spi/src/main/java/org/opendaylight/protocol/bgp/openconfig/spi/BGPGlobalProvider.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.openconfig.spi;
+
+import java.util.List;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.AsNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.BgpTableType;
+
+/**
+ * Provides put/delete operations of the BGP OpenConfig Global configuration, based on BGP RIB configuration module instance.
+ *
+ */
+public interface BGPGlobalProvider {
+
+    /**
+     * Transform input parameters to BGP OpenConfig Global instance and writes the new/changed configuration.
+     * @param instanceName BGP RIB configuration module instance name
+     * @param localAs Local Autonomous System Number
+     * @param bgpRibId BGP RIB identifier
+     * @param clusterId BGP Cluster Identifier
+     * @param list
+     */
+    void writeGlobal(String instanceName, AsNumber localAs, Ipv4Address bgpRibId, Ipv4Address clusterId, List<BgpTableType> list);
+
+    /**
+     * Delete BGP OpenConfig Global configuration instance.
+     * @param instanceName BGP RIB configuration module instance name
+     */
+    void removeGlobal(String instanceName);
+
+}
