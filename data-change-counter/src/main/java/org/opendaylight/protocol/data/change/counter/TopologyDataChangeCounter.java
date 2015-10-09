b2b7e2cc70c2335f7162c9e480ef28de2373bc74
Iveta Halanova <ihalanov@cisco.com>
2015-10-09 15:34:30 +0200

    BUG-4364: data change counter transaction chain
    to avoid logging spam
    
    Change-Id: Ia59091f71edfb69d0fd3860086634112c4de0226
    Signed-off-by: Iveta Halanova <ihalanov@cisco.com>

--- data-change-counter/src/main/java/org/opendaylight/protocol/data/change/counter/TopologyDataChangeCounter.java
+++ data-change-counter/src/main/java/org/opendaylight/protocol/data/change/counter/TopologyDataChangeCounter.java
@@ -9,11 +9,15 @@
 package org.opendaylight.protocol.data.change.counter;
 
 import java.util.concurrent.atomic.AtomicLong;
+import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChain;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionChainListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.data.change.counter.rev140815.DataChangeCounter;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.data.change.counter.rev140815.DataChangeCounterBuilder;
 import org.opendaylight.yangtools.yang.binding.DataObject;
@@ -21,7 +25,7 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TopologyDataChangeCounter implements DataChangeListener {
+public class TopologyDataChangeCounter implements DataChangeListener, TransactionChainListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(TopologyDataChangeCounter.class);
 
@@ -29,17 +33,19 @@ public class TopologyDataChangeCounter implements DataChangeListener {
             .builder(DataChangeCounter.class).build();
 
     private final DataBroker dataBroker;
-    private AtomicLong count;
+    private final BindingTransactionChain chain;
+    private final AtomicLong count;
 
     public TopologyDataChangeCounter(final DataBroker dataBroker) {
         this.dataBroker = dataBroker;
+        this.chain = this.dataBroker.createTransactionChain(this);
         this.count = new AtomicLong(0);
         putCount(this.count.get());
         LOG.debug("Data change counter initiated");
     }
 
     @Override
-    public void onDataChanged(AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
+    public void onDataChanged(final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change) {
         putCount(this.count.incrementAndGet());
         LOG.debug("Data change #{}", this.count.get());
     }
@@ -48,14 +54,25 @@ public class TopologyDataChangeCounter implements DataChangeListener {
         final WriteTransaction wTx = this.dataBroker.newWriteOnlyTransaction();
         wTx.delete(LogicalDatastoreType.OPERATIONAL, IID);
         wTx.submit();
+        this.chain.close();
         LOG.debug("Data change counter removed");
     }
 
     private void putCount(final long count) {
-        final WriteTransaction wTx = this.dataBroker.newWriteOnlyTransaction();
-        wTx.put(LogicalDatastoreType.OPERATIONAL, IID, new DataChangeCounterBuilder().setCount(count)
-                .build());
+        final WriteTransaction wTx = this.chain.newWriteOnlyTransaction();
+        wTx.put(LogicalDatastoreType.OPERATIONAL, IID, new DataChangeCounterBuilder().setCount(count).build());
         wTx.submit();
+    }
+
+    @Override
+    public void onTransactionChainFailed(final TransactionChain<?, ?> chain, final AsyncTransaction<?, ?> transaction, final Throwable cause) {
+        chain.close();
+        LOG.warn("Transaction chain failure. Transaction: {}", transaction, cause);
+    }
+
+    @Override
+    public void onTransactionChainSuccessful(final TransactionChain<?, ?> chain) {
+        LOG.debug("Transaction chain successful. {}", chain);
     }
 
 }
