6b63a93536a155e3cabedebc56df047b0c0f3add
Claudio D. Gasparini <cgaspari@cisco.com>
2015-10-30 15:01:01 +0100

    BGPCEP MD5 Password Fix
    Password were checked in case if null
    but not in case of empty.
    Fix by creating an Optional from password where in
    case of null or empty will be absent.
    
    Change-Id: I67033deeccd5f634a73d941d0c3da23e8dfb644d
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- bgp/openconfig-spi/src/test/java/org/opendaylight/protocol/bgp/openconfig/spi/pojo/BGPPeerInstanceConfigurationTest.java
+++ bgp/openconfig-spi/src/test/java/org/opendaylight/protocol/bgp/openconfig/spi/pojo/BGPPeerInstanceConfigurationTest.java
@@ -20,6 +20,7 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.BgpTableType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.PeerRole;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.tcpmd5.cfg.rev140427.Rfc2385Key;
 
 public class BGPPeerInstanceConfigurationTest {
 
@@ -30,7 +31,7 @@ public class BGPPeerInstanceConfigurationTest {
     private static final AsNumber AS_NUMBER = new AsNumber(72L);
 
     private final BGPPeerInstanceConfiguration config = new BGPPeerInstanceConfiguration(INSTANCE_NAME, HOST, PORT, HOLD_TIMER, PeerRole.Ibgp,
-            Boolean.FALSE, Collections.<BgpTableType>emptyList(), AS_NUMBER, null);
+            Boolean.FALSE, Collections.<BgpTableType>emptyList(), AS_NUMBER, Optional.<Rfc2385Key>absent());
 
     @Test
     public final void testGetHost() {
