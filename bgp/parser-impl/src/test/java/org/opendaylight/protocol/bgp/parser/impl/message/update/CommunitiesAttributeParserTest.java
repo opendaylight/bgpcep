ddd7b5dfc2395cc6e4f164e5fd41364ef9d32fa8
Claudio D. Gasparini <cgaspari@cisco.com>
2015-11-11 13:23:46 +0100

    BUG-4622: Sending Empty Optional Attributes
    Some of the parsers of Optional Attributes
    only take in account the possibility of null
    attribute and not of an empty one.
    Fix by check it and dont parse them in case of
    empty or null.
    
    Change-Id: I34d801a001ed167e18646e228b9b9dfc03f7c882
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/parser-impl/src/test/java/org/opendaylight/protocol/bgp/parser/impl/message/update/CommunitiesAttributeParserTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.parser.impl.message.update;
+
+import com.google.common.collect.Lists;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import java.util.List;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.opendaylight.protocol.bgp.parser.spi.pojo.ServiceLoaderBGPExtensionProviderContext;
+import org.opendaylight.protocol.util.ByteArray;
+import org.opendaylight.protocol.util.NoopReferenceCache;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.Attributes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.AttributesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.path.attributes.attributes.Communities;
+
+public class CommunitiesAttributeParserTest extends TestCase {
+
+    private static final byte[] CommunitiesBytes = {(byte) 0xC0, (byte) 0x08, (byte) 0x10,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0x1,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0x2,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0x3,
+        (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0x10};
+
+    public void testCommunitiesAttributeParser() throws Exception {
+        final List<Communities> comms = Lists.newArrayList();
+        comms.add((Communities) CommunityUtil.NO_EXPORT);
+        comms.add((Communities) CommunityUtil.NO_ADVERTISE);
+        comms.add((Communities) CommunityUtil.NO_EXPORT_SUBCONFED);
+        comms.add((Communities) CommunityUtil.create(NoopReferenceCache.getInstance(), 0xFFFF, 0xFF10));
+
+        final AttributesBuilder paBuilder = new AttributesBuilder();
+        paBuilder.setCommunities(comms);
+
+        final ByteBuf actual = Unpooled.buffer();
+        ServiceLoaderBGPExtensionProviderContext.getSingletonInstance().getAttributeRegistry()
+            .serializeAttribute(paBuilder.build(), actual);
+        Assert.assertArrayEquals(CommunitiesBytes, ByteArray.getAllBytes(actual));
+        final Attributes attributeOut = ServiceLoaderBGPExtensionProviderContext.getSingletonInstance().getAttributeRegistry()
+            .parseAttributes(actual);
+        Assert.assertEquals(comms,attributeOut.getCommunities());
+    }
+
+    public void testParseEmptyListAttribute() {
+        final List<Communities> comms = Lists.newArrayList();
+        final ByteBuf actual = Unpooled.buffer();
+        ServiceLoaderBGPExtensionProviderContext.getSingletonInstance().getAttributeRegistry()
+            .serializeAttribute(new AttributesBuilder().setCommunities(comms).build(), actual);
+        Assert.assertEquals(Unpooled.buffer(), actual);
+    }
+
+    public void testParseEmptyAttribute() {
+        final ByteBuf actual = Unpooled.buffer();
+        ServiceLoaderBGPExtensionProviderContext.getSingletonInstance().getAttributeRegistry()
+            .serializeAttribute(new AttributesBuilder().build(), actual);
+        Assert.assertEquals(Unpooled.buffer(), actual);
+    }
+}
