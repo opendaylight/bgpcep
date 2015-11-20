7dec0b169aef00424d3d4d1069e9e87bca326994
Claudio D. Gasparini <cgaspari@cisco.com>
2015-11-20 12:34:36 +0100

    Fix multiple Sonar Issues
    -Refactor Tunnel programming.
    -Class misspelling name.
    -Rename packet.
    
    Change-Id: Ib737585a098d00f2c1b6a03543ff84d704099578
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/controller/config/yang/bgp/rib/impl/Rev130715Util.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/controller/config/yang/bgp/rib/impl/Rev130715Util.java
@@ -13,9 +13,13 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv6Address;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
 
-public  final class Rev130715Uitl {
+public final class Rev130715Util {
+    private Rev130715Util() {
+        throw new UnsupportedOperationException();
+    }
+
     public static Ipv4Address getIpv4Address(final org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address ipv4) {
-        if(ipv4 == null) {
+        if (ipv4 == null) {
             return null;
         }
         return new Ipv4Address(ipv4.getValue());
@@ -30,7 +34,7 @@ public  final class Rev130715Uitl {
     }
 
     public static IpAddress getIpvAddress(final org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress hostWithoutValue) {
-        if(hostWithoutValue.getIpv4Address() != null) {
+        if (hostWithoutValue.getIpv4Address() != null) {
             return new IpAddress(new Ipv4Address(hostWithoutValue.getIpv4Address().getValue()));
         }
         return new IpAddress(new Ipv6Address(hostWithoutValue.getIpv6Address().getValue()));
