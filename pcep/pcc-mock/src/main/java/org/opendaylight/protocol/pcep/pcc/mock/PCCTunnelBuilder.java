44d92fcd8fb6ebbdc5db65238a3d7bdc1f98f9e3
Claudio D. Gasparini <cgaspari@cisco.com>
2015-11-28 22:33:05 +0100

    BUG-2208: PCC-MOCK Stateful Sync Opt
    Update pcc-mock to support:
    +State Synchronization Avoidance.
    +Incremental Synchronization
    +PCE Triggered Initial Synchronization
    +PCE Triggered Lsp Resynchronization
    +PCE Triggered Full DB Resynchronization
    Input
    --state-sync-avoidance 10 5 5
    --incremental-sync-procedure 10 5 5
    --triggered-initial-sync
    --triggered-re-sync
    +DBVersion number after PCC reconnection
    +Seconds to wait until disconnect
    +Seconds to wait until PCC reconnects
    
    TODO:
    +When resync modify lsp, so it can be check that it has been reload
    
    Change-Id: I61f673177a086d969b6f097733351413b71ea8e2
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ pcep/pcc-mock/src/main/java/org/opendaylight/protocol/pcep/pcc/mock/PCCTunnelBuilder.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.pcep.pcc.mock;
+
+import static org.opendaylight.protocol.pcep.pcc.mock.MsgBuilderUtil.createPath;
+
+import com.google.common.collect.Lists;
+import java.util.HashMap;
+import java.util.Map;
+import org.opendaylight.protocol.pcep.pcc.mock.api.LspType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpPrefix;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.PlspId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.explicit.route.object.ero.Subobject;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.explicit.route.object.ero.SubobjectBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.basic.explicit.route.subobjects.subobject.type.IpPrefixCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.basic.explicit.route.subobjects.subobject.type.ip.prefix._case.IpPrefixBuilder;
+
+public class PCCTunnelBuilder {
+    private static final Subobject DEFAULT_ENDPOINT_HOP = getDefaultEROEndpointHop();
+    private static final String ENDPOINT_ADDRESS = "1.1.1.1";
+    private static final String ENDPOINT_PREFIX = ENDPOINT_ADDRESS + "/32";
+    public static final int PCC_DELEGATION = -1;
+
+    public static Map<PlspId, PCCTunnel> createTunnels(String address, final int lsps) {
+        Map<PlspId, PCCTunnel> tunnels = new HashMap<>();
+        for (int i = 1; i <= lsps; i++) {
+            final PCCTunnel tunnel = new PCCTunnel(MsgBuilderUtil.getDefaultPathName(address, i), PCC_DELEGATION, LspType.PCC_LSP,
+                createPath(Lists.newArrayList(DEFAULT_ENDPOINT_HOP)));
+            tunnels.put(new PlspId((long) i), tunnel);
+        }
+        return tunnels;
+    }
+
+    private static Subobject getDefaultEROEndpointHop() {
+        final SubobjectBuilder builder = new SubobjectBuilder();
+        builder.setLoose(false);
+        builder.setSubobjectType(new IpPrefixCaseBuilder().setIpPrefix(new IpPrefixBuilder().setIpPrefix(
+            new IpPrefix(new Ipv4Prefix(ENDPOINT_PREFIX))).build()).build());
+        return builder.build();
+    }
+}
