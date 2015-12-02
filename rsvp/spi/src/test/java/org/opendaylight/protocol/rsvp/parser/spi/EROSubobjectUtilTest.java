5d4d7ba93aafa03b5d305050aa25e1006887c67c
Iveta Halanova <ihalanov@cisco.com>
2015-12-02 13:55:22 +0100

    Test coverage: rsvp-spi/subobjects
    Change-Id: I29400140d6e5cb5846473896a20151e3a57fbdc5
    Signed-off-by: Iveta Halanova <ihalanov@cisco.com>

---
+++ rsvp/spi/src/test/java/org/opendaylight/protocol/rsvp/parser/spi/EROSubobjectUtilTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.rsvp.parser.spi;
+
+import static org.junit.Assert.assertArrayEquals;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import org.junit.Test;
+
+public class EROSubobjectUtilTest {
+
+    @Test(expected=UnsupportedOperationException.class)
+    public void testPrivateConstructor() throws Throwable {
+        final Constructor<EROSubobjectUtil> c = EROSubobjectUtil.class.getDeclaredConstructor();
+        c.setAccessible(true);
+        try {
+            c.newInstance();
+        } catch (final InvocationTargetException e) {
+            throw e.getCause();
+        }
+    }
+
+    @Test
+    public void testFormatSubobject1() {
+        final byte[] array = new byte[] {2, 3};
+        final byte[] expected = new byte[] {(byte)0x81, 4, 2, 3};
+        final ByteBuf body = Unpooled.copiedBuffer(array);
+        final ByteBuf aggregator = Unpooled.buffer(4);
+        EROSubobjectUtil.formatSubobject(1, Boolean.TRUE, body, aggregator);
+        assertArrayEquals(expected, aggregator.array());
+    }
+
+    @Test
+    public void testFormatSubobject2() {
+        final byte[] array = new byte[] {2, 3};
+        final byte[] expected = new byte[] {1, 4, 2, 3};
+        final ByteBuf body = Unpooled.copiedBuffer(array);
+        final ByteBuf aggregator = Unpooled.buffer(4);
+        EROSubobjectUtil.formatSubobject(1, Boolean.FALSE, body, aggregator);
+        assertArrayEquals(expected, aggregator.array());
+    }
+
+    @Test
+    public void testFormatSubobject3() {
+        final byte[] array = new byte[] {2, 3};
+        final byte[] expected = new byte[] {1, 4, 2, 3};
+        final ByteBuf body = Unpooled.copiedBuffer(array);
+        final ByteBuf aggregator = Unpooled.buffer(4);
+        EROSubobjectUtil.formatSubobject(1, null, body, aggregator);
+        assertArrayEquals(expected, aggregator.array());
+    }
+}
