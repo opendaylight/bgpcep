af146e2e2ee0f4bb4808ba99a39e40d9130e2cad
Iveta Halanova <ihalanov@cisco.com>
2015-12-15 14:51:57 +0100

    BUG-4740: No reconnect after negotiation failure
    Change-Id: I31a152bcfc52f98a9e95a8a75b26d42d2da8a014
    Signed-off-by: Iveta Halanova <ihalanov@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/protocol/BGPReconnectPromise.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/protocol/BGPReconnectPromise.java
@@ -127,7 +127,8 @@ public class BGPReconnectPromise<S extends BGPSession> extends DefaultPromise<Vo
             }
 
             if (!this.promise.isInitialConnectFinished()) {
-                LOG.debug("Connection to {} was dropped during negotiation, reattempting", this.promise.address);
+                LOG.debug("Connection to {} was dropped during negotiation", this.promise.address);
+                return;
             }
 
             LOG.debug("Reconnecting after connection to {} was dropped", this.promise.address);
