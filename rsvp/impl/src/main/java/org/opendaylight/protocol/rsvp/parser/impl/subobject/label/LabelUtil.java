2124359d01e1d0d404766542290f694068682b79
Claudio D. Gasparini <cgaspari@cisco.com>
2015-12-30 23:48:55 +0100

    Rename Label package name to match the regular expression
    Change-Id: I31996acbcca5b3e247c977a3615f2b4050670488
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- rsvp/impl/src/main/java/org/opendaylight/protocol/rsvp/parser/impl/subobject/label/LabelUtil.java
+++ rsvp/impl/src/main/java/org/opendaylight/protocol/rsvp/parser/impl/subobject/label/LabelUtil.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.protocol.rsvp.parser.impl.subobject.Label;
+package org.opendaylight.protocol.rsvp.parser.impl.subobject.label;
 
 import io.netty.buffer.ByteBuf;
 import org.opendaylight.protocol.util.BitArray;
