4812eea33e87250f0664d22dfe5f6d6903a849c7
Claudio D. Gasparini <cgaspari@cisco.com>
2016-01-05 16:59:27 +0100

    Cancel PCEPSessionNegotiator Timers on session down
    On session goes down PCEPSessionNegotiator Timers need to
    be canceled
    
    Change-Id: I82091efb6e8ffcda992f08bc3fdcbf1cf63a1ab5
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- pcep/pcc-mock/src/main/java/org/opendaylight/protocol/pcep/pcc/mock/PCCDispatcherImpl.java
+++ pcep/pcc-mock/src/main/java/org/opendaylight/protocol/pcep/pcc/mock/PCCDispatcherImpl.java
@@ -93,8 +93,8 @@ public final class PCCDispatcherImpl implements PccDispatcher, AutoCloseable {
         setChannelFactory(b, keys);
         b.option(ChannelOption.SO_KEEPALIVE, true);
         b.option(ChannelOption.MAX_MESSAGES_PER_READ, 1);
-        final PCCReconnectPromise promise = new PCCReconnectPromise(remoteAddress,
-                reconnectTime == -1 ? getNeverReconnectStrategyFactory() : getTimedReconnectStrategyFactory(reconnectTime), b);
+        final ReconnectStrategyFactory reconnectStrategy = reconnectTime == -1 ? getNeverReconnectStrategyFactory() : getTimedReconnectStrategyFactory(reconnectTime);
+        final PCCReconnectPromise promise = new PCCReconnectPromise(remoteAddress, reconnectStrategy, b);
         final ChannelInitializer<SocketChannel> channelInitializer = new ChannelInitializer<SocketChannel>() {
             @Override
             protected void initChannel(final SocketChannel ch) throws Exception {
@@ -110,6 +110,7 @@ public final class PCCDispatcherImpl implements PccDispatcher, AutoCloseable {
 
                         if (!promise.isInitialConnectFinished()) {
                             LOG.debug("Connection to {} was dropped during negotiation, reattempting", remoteAddress);
+                            return;
                         }
                         LOG.debug("Reconnecting after connection to {} was dropped", remoteAddress);
                         PCCDispatcherImpl.this.createClient(remoteAddress, reconnectTime, listenerFactory, negotiatorFactory,
