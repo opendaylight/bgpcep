526c716e2392312bfd875e60addb79e6ba2b0504
Milos Fabian <milfabia@cisco.com>
2016-01-11 02:10:52 +0100

    Bug 4921 - Support Bandwidth Usage object
    Support Bandwidth-Usage object as defined in
    https://tools.ietf.org/html/draft-dhody-pce-stateful-pce-auto-bandwidth-06#section-5.3.2
    The object is used in PcRpt message report the real-time
    bandwidth usage of a TE LSP.
    Make object-type configurable, since no codepoint is assigned by IANA.
    By default object-type=5.
    
    Change-Id: I8aacf86cee3f647ad47a0dc01a861b16f89a62c5
    Signed-off-by: Milos Fabian <milfabia@cisco.com>
    (cherry picked from commit 5bd37a2832c88475435300ffe46836cb895309c4)

---
+++ pcep/auto-bandwidth-extension/src/main/java/org/opendaylight/protocol/pcep/auto/bandwidth/extension/Activator.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.pcep.auto.bandwidth.extension;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.protocol.pcep.spi.PCEPExtensionProviderContext;
+import org.opendaylight.protocol.pcep.spi.pojo.AbstractPCEPExtensionProviderActivator;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.pcep.auto.bandwidth.rev160109.bandwidth.usage.object.BandwidthUsage;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev131222.Pcrpt;
+
+public class Activator extends AbstractPCEPExtensionProviderActivator {
+
+    private final int bandwidthUsageObjectType;
+
+    public Activator(final int bandwidthUsageObjectType) {
+        this.bandwidthUsageObjectType = bandwidthUsageObjectType;
+    }
+
+    @Override
+    protected List<AutoCloseable> startImpl(final PCEPExtensionProviderContext context) {
+        final List<AutoCloseable> regs = new ArrayList<>();
+
+        final BandwidthUsageObjectCodec bandwidthUsageObjectCodec = new BandwidthUsageObjectCodec(this.bandwidthUsageObjectType);
+        regs.add(context.registerObjectParser(BandwidthUsageObjectCodec.CLASS, bandwidthUsageObjectCodec.getType(), bandwidthUsageObjectCodec));
+        regs.add(context.registerObjectSerializer(BandwidthUsage.class, bandwidthUsageObjectCodec));
+
+        final PcRptMessageCodec pcRptMessageCodec = new PcRptMessageCodec(context.getObjectHandlerRegistry());
+        regs.add(context.registerMessageParser(PcRptMessageCodec.TYPE, pcRptMessageCodec));
+        regs.add(context.registerMessageSerializer(Pcrpt.class, pcRptMessageCodec));
+
+        return regs;
+    }
+
+}
