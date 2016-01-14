b28fe05892c1497b49410a0042bc8a58f43b486e
Jozef Behran <jbehran@cisco.com>
2016-01-14 11:10:27 +0100

    Fixed play.py broken by the "big cleanup change"
    Change-Id: Ifc8150825e14e262eef272f0fd18f56ae746917b
    Signed-off-by: Jozef Behran <jbehran@cisco.com>

--- test/tools/fastbgp/play.py
+++ test/tools/fastbgp/play.py
@@ -815,16 +815,16 @@ class MessageGenerator(object):
                 "\x02"  # AS segment type (AS_SEQUENCE)
                 "\x01"  # AS segment length (1)
             )
-            my_AS = struct.pack(">I", my_autonomous_system)
-            path_attributes_hex += my_AS  # AS segment (4 bytes)
+            my_as_hex = struct.pack(">I", my_autonomous_system)
+            path_attributes_hex += my_as_hex  # AS segment (4 bytes)
             path_attributes_hex += (
                 "\x40"  # Flags ("Well-Known")
                 "\x03"  # Type (NEXT_HOP)
                 "\x04"  # Length (4)
             )
-            next_hop = struct.pack(">I", int(next_hop))
+            next_hop_hex = struct.pack(">I", int(next_hop))
             path_attributes_hex += (
-                next_hop  # IP address of the next hop (4 bytes)
+                next_hop_hex  # IP address of the next hop (4 bytes)
             )
         else:
             path_attributes_hex = ""
