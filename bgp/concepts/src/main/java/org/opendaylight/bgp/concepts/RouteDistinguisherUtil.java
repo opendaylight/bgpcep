1fa40a21f4336486002c847c21899206c11d4e52
Iveta Halanova <ihalanov@cisco.com>
2016-01-27 15:19:40 +0100

    BUG-5104:  Defined Route Distinguisher type
    added new type into yang model - route-distinguisher
    implemented Util class with parser and serializer.
    
    Change-Id: Ic8b4f7e1fa5d6ff3ff0021f54fb43e67aa12b908
    Signed-off-by: Iveta Halanova <ihalanov@cisco.com>

---
+++ bgp/concepts/src/main/java/org/opendaylight/bgp/concepts/RouteDistinguisherUtil.java
@@ -0,0 +1,100 @@
+package org.opendaylight.bgp.concepts;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.util.Ipv4Util;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.AsNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.RouteDistinguisher;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.route.distinguisher.AdministratorSubfield;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.route.distinguisher.administrator.subfield.AsNumberCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.route.distinguisher.administrator.subfield.AsNumberCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.route.distinguisher.administrator.subfield.Ipv4Case;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.route.distinguisher.administrator.subfield.Ipv4CaseBuilder;
+import org.opendaylight.yangtools.yang.binding.DataContainer;
+
+/**
+ * Utility class for of RouteDistinguisher serialization and parsing.
+ * https://tools.ietf.org/html/rfc4364#section-4
+ */
+public final class RouteDistinguisherUtil {
+
+    private static final int AS_2BYTE_TYPE = 0;
+    private static final int IPV4_TYPE = 1;
+    private static final int AS_4BYTE_TYPE = 2;
+
+    private RouteDistinguisherUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Serializes route distinguisher according to type and writes into ByteBuf.
+     *
+     * @param distinquisher
+     * @param byteAggregator
+     */
+    public static void serializeRouteDistinquisher(final RouteDistinguisher distinquisher, final ByteBuf byteAggregator) {
+        final int type = distinquisher.getType();
+        byteAggregator.writeShort(type);
+        switch (type) {
+            case AS_2BYTE_TYPE:
+                byteAggregator.writeShort(((AsNumberCase) distinquisher.getAdministratorSubfield()).getAsNumber().getValue().intValue());
+                byteAggregator.writeInt(distinquisher.getAssignedNumberSubfield().intValue());
+            break;
+            case IPV4_TYPE:
+                byteAggregator.writeBytes(Ipv4Util.byteBufForAddress(((Ipv4Case) distinquisher.getAdministratorSubfield()).getIpv4Address()));
+                byteAggregator.writeShort(distinquisher.getAssignedNumberSubfield().intValue());
+            break;
+            case AS_4BYTE_TYPE:
+                byteAggregator.writeInt(((AsNumberCase) distinquisher.getAdministratorSubfield()).getAsNumber().getValue().intValue());
+                byteAggregator.writeShort(distinquisher.getAssignedNumberSubfield().intValue());
+            break;
+        }
+    }
+
+    /**
+     * Parses three types of route distinguisher from given ByteBuf.
+     *
+     * @param buffer
+     * @return RouteDistinguisher
+     */
+    public static RouteDistinguisher parseRouteDistinguisher(final ByteBuf buffer) {
+        final int type = buffer.readUnsignedShort();
+        final AdministratorSubfield administratorSubfield;
+        final Long assignedNumberSubfield;
+        switch (type) {
+            case AS_2BYTE_TYPE:
+                administratorSubfield = new AsNumberCaseBuilder().setAsNumber(new AsNumber(new Long(buffer.readUnsignedShort()))).build();
+                assignedNumberSubfield = new Long(buffer.readUnsignedInt());
+            break;
+            case IPV4_TYPE:
+                administratorSubfield = new Ipv4CaseBuilder().setIpv4Address(Ipv4Util.addressForByteBuf(buffer)).build();
+                assignedNumberSubfield = new Long(buffer.readUnsignedShort());
+            break;
+            case AS_4BYTE_TYPE:
+                administratorSubfield = new AsNumberCaseBuilder().setAsNumber(new AsNumber(new Long(buffer.readUnsignedInt()))).build();
+                assignedNumberSubfield = new Long(buffer.readUnsignedShort());
+            break;
+            default:
+                administratorSubfield = null;
+                assignedNumberSubfield = null;
+            break;
+        }
+        return new RouteDistinguisher() {
+            @Override
+            public Class<? extends DataContainer> getImplementedInterface() {
+                return RouteDistinguisher.class;
+            }
+            @Override
+            public Integer getType() {
+                return type;
+            }
+            @Override
+            public Long getAssignedNumberSubfield() {
+                return assignedNumberSubfield;
+            }
+            @Override
+            public AdministratorSubfield getAdministratorSubfield() {
+                return administratorSubfield;
+            }
+        };
+    }
+}
