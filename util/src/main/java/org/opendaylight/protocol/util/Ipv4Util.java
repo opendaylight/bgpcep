1e7f1e7e560e2221f77af424450cc54788edfc7f
Robert Varga <rovarga@cisco.com>
2016-02-18 17:31:59 +0100

    BUG-2825: use IetfInetUtil to instantiate addresses
    Rather than brewing our own strings from bytes, use the utility methods
    provided with the model. Also use a single instance for empty prefix.
    
    Change-Id: Idfbfc7f2374b8d0205f1d0e3407e3e347242dbc9
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- util/src/main/java/org/opendaylight/protocol/util/Ipv4Util.java
+++ util/src/main/java/org/opendaylight/protocol/util/Ipv4Util.java
@@ -10,47 +10,29 @@ package org.opendaylight.protocol.util;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.net.InetAddresses;
-import com.google.common.primitives.Bytes;
 import com.google.common.primitives.UnsignedBytes;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IetfInetUtil;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Prefix;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Address;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
 
 /**
  * Util class for creating generated Ipv4Address.
  */
 public final class Ipv4Util {
+    public static final int IP4_LENGTH = 4;
+    private static final Ipv4Prefix EMPTY_PREFIX = new Ipv4Prefix("0.0.0.0/0");
 
     private Ipv4Util() {
         throw new UnsupportedOperationException();
-    }
-
-    public static final int IP4_LENGTH = 4;
-
-    /**
-     * Converts byte array to Inet4Address.
-     *
-     * @param bytes to be converted
-     * @return InetAddress instance
-     * @throws IllegalArgumentException if {@link UnknownHostException} is thrown.
-     */
-    private static InetAddress getAddress(final byte[] bytes) {
-        try {
-            return Inet4Address.getByAddress(bytes);
-        } catch (final UnknownHostException e) {
-            throw new IllegalArgumentException("Failed to construct IPv4 address", e);
-        }
     }
 
     /**
@@ -60,7 +42,7 @@ public final class Ipv4Util {
      * @return Ipv4Address
      */
     public static Ipv4Address addressForByteBuf(final ByteBuf buffer) {
-        return new Ipv4Address(InetAddresses.toAddrString(getAddress(ByteArray.readBytes(buffer, IP4_LENGTH))));
+        return IetfInetUtil.INSTANCE.ipv4AddressFor(ByteArray.readBytes(buffer, IP4_LENGTH));
     }
 
     /**
@@ -79,9 +61,14 @@ public final class Ipv4Util {
      * @return byte array
      */
     public static byte[] bytesForAddress(final Ipv4Address address) {
-        final InetAddress a = InetAddresses.forString(address.getValue());
-        Preconditions.checkArgument(a instanceof Inet4Address);
-        return a.getAddress();
+        return IetfInetUtil.INSTANCE.ipv4AddressBytes(address);
+    }
+
+    private static int prefixBitsToBytes(final int bits) {
+        if (bits % Byte.SIZE != 0) {
+            return (bits / Byte.SIZE) + 1;
+        }
+        return bits / Byte.SIZE;
     }
 
     /**
@@ -91,11 +78,7 @@ public final class Ipv4Util {
      * @return
      */
     public static int getPrefixLengthBytes(final String prefix) {
-        final int bits = Ipv4Util.getPrefixLength(prefix);
-        if (bits % Byte.SIZE != 0) {
-            return (bits / Byte.SIZE) + 1;
-        }
-        return bits / Byte.SIZE;
+        return prefixBitsToBytes(Ipv4Util.getPrefixLength(prefix));
     }
 
     /**
@@ -105,12 +88,7 @@ public final class Ipv4Util {
      * @return byte array with prefix length at the end
      */
     public static byte[] bytesForPrefix(final Ipv4Prefix prefix) {
-        final String p = prefix.getValue();
-        final int sep = p.indexOf('/');
-        final InetAddress a = InetAddresses.forString(p.substring(0, sep));
-        Preconditions.checkArgument(a instanceof Inet4Address);
-        final byte[] bytes = a.getAddress();
-        return Bytes.concat(bytes, new byte[] { Byte.valueOf(p.substring(sep + 1, p.length())) });
+        return IetfInetUtil.INSTANCE.ipv4PrefixToBytes(prefix);
     }
 
     /**
@@ -121,16 +99,20 @@ public final class Ipv4Util {
      * @return byte array with the prefix length at the beginning
      */
     public static byte[] bytesForPrefixBegin(final Ipv4Prefix prefix) {
-        final String p = prefix.getValue();
-        final int length = getPrefixLength(p);
-        if (length == 0) {
+        final byte[] addrWithPrefix = bytesForPrefix(prefix);
+        return prefixedBytes(addrWithPrefix[IP4_LENGTH], addrWithPrefix);
+    }
+
+    static byte[] prefixedBytes(final byte prefixBits, final byte[] address) {
+        if (prefixBits != 0) {
+            final int prefixBytes = prefixBitsToBytes(Byte.toUnsignedInt(prefixBits));
+            final byte[] ret = new byte[prefixBytes + 1];
+            ret[0] = prefixBits;
+            System.arraycopy(address, 0, ret, 1, prefixBytes);
+            return ret;
+        } else {
             return new byte[] { 0 };
         }
-        final int sep = p.indexOf('/');
-        final InetAddress a = InetAddresses.forString(p.substring(0, sep));
-        Preconditions.checkArgument(a instanceof Inet4Address);
-        final byte[] bytes = a.getAddress();
-        return Bytes.concat(new byte[] { UnsignedBytes.checkedCast(length) }, ByteArray.subByte(bytes, 0 , getPrefixLengthBytes(p)));
     }
 
     /**
@@ -143,8 +125,7 @@ public final class Ipv4Util {
     public static Ipv4Prefix prefixForBytes(final byte[] bytes, final int length) {
         Preconditions.checkArgument(length <= bytes.length * Byte.SIZE);
         final byte[] tmp = Arrays.copyOfRange(bytes, 0, IP4_LENGTH);
-        final InetAddress a = getAddress(tmp);
-        return new Ipv4Prefix(InetAddresses.toAddrString(a) + '/' + length);
+        return IetfInetUtil.INSTANCE.ipv4PrefixFor(tmp, length);
     }
 
     /**
@@ -158,7 +139,7 @@ public final class Ipv4Util {
         final int prefixLength = bytes.readByte();
         final int size = prefixLength / Byte.SIZE + ((prefixLength % Byte.SIZE == 0) ? 0 : 1);
         Preconditions.checkArgument(size <= bytes.readableBytes(), "Illegal length of IP prefix: " + (bytes.readableBytes()));
-        return Ipv4Util.prefixForBytes(ByteArray.readBytes(bytes, size), prefixLength);
+        return prefixForBytes(ByteArray.readBytes(bytes, size), prefixLength);
     }
 
     /**
@@ -178,7 +159,7 @@ public final class Ipv4Util {
             byteOffset += 1;
             // if length == 0, default route will be added
             if (bitLength == 0) {
-                list.add(new Ipv4Prefix("0.0.0.0/0"));
+                list.add(EMPTY_PREFIX);
                 continue;
             }
             final int byteCount = (bitLength % Byte.SIZE != 0) ? (bitLength / Byte.SIZE) + 1 : bitLength / Byte.SIZE;
@@ -207,11 +188,7 @@ public final class Ipv4Util {
      * @return IpAddress
      */
     public static IpAddress getIpAddress(final InetAddress inetAddress) {
-        final String address = InetAddresses.toAddrString(inetAddress);
-        if (inetAddress instanceof Inet4Address) {
-            return new IpAddress(new Ipv4Address(address));
-        }
-        return new IpAddress(new Ipv6Address(address));
+        return IetfInetUtil.INSTANCE.ipAddressFor(inetAddress);
     }
 
     /**
