868782084de141554f8a6e9d7aa6a1b4e3d6ae6f
Milos Fabian <milfabia@cisco.com>
2016-03-02 14:59:56 +0100

    Bug 4827: Add PathId utility class
    The Utility class provides functions
    for optional writing of path-id value
    into a ByteBuf.
    
    Change-Id: I65235c7aab9d60df9054ecd475481f21e1c430a8
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/parser-spi/src/test/java/org/opendaylight/protocol/bgp/parser/spi/PathIdUtilTest.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.parser.spi;
+
+import static org.opendaylight.protocol.bgp.parser.spi.PathIdUtil.NII;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.protocol.util.ByteBufWriteUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.PathId;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeSchemaAwareBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableLeafNodeBuilder;
+
+public class PathIdUtilTest {
+
+    private ByteBuf buffer;
+
+    @Before
+    public void setUp() {
+        this.buffer = Unpooled.buffer();
+    }
+
+    @Test
+    public void testWritePathIdNull() {
+        PathIdUtil.writePathId(null, this.buffer);
+        Assert.assertEquals(0, this.buffer.readableBytes());
+    }
+
+    @Test
+    public void testWritePathIdZero() {
+        PathIdUtil.writePathId(new PathId(0l), this.buffer);
+        Assert.assertEquals(0, this.buffer.readableBytes());
+    }
+
+    @Test
+    public void testWritePathId() {
+        PathIdUtil.writePathId(new PathId(10l), this.buffer);
+        Assert.assertEquals(Integer.BYTES, this.buffer.readableBytes());
+    }
+
+    @Test
+    public void testReadPathId() {
+        final long expected = 10L;
+        ByteBufWriteUtil.writeUnsignedInt(expected, this.buffer);
+        final PathId pathId = PathIdUtil.readPathId(this.buffer);
+        Assert.assertEquals(expected, pathId.getValue().longValue());
+    }
+
+    @Test
+    public void testExtractPathId() {
+        final long pathIdValue = 0;
+        final ContainerNode cont = ImmutableContainerNodeSchemaAwareBuilder.create().withNodeIdentifier(NII).
+            addChild(new ImmutableLeafNodeBuilder<>().withNodeIdentifier(NII).withValue(pathIdValue).build()).build();
+        Assert.assertEquals(pathIdValue, PathIdUtil.extractPathId(cont).longValue());
+    }
+
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testReadPathIdBufferNull() {
+        PathIdUtil.readPathId(null);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testReadPathIdBufferEmpty() {
+        PathIdUtil.readPathId(this.buffer);
+    }
+
+}
