04775363e24b1e540e2f9aa2f2aea2f564a6d046
Milos Fabian <milfabia@cisco.com>
2016-03-02 14:59:56 +0100

    Bug 4827: Add PathId utility class
    The Utility class provides functions
    for optional writing of path-id value
    into a ByteBuf.
    
    Change-Id: I65235c7aab9d60df9054ecd475481f21e1c430a8
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/PathIdUtil.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.parser.spi;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.util.ByteBufWriteUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.PathId;
+
+public final class PathIdUtil {
+
+    private PathIdUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Writes path-id value into the buffer when
+     * the path-id is not null or does not equal to zero.
+     *
+     * @param pathId The NLRI Path Identifier.
+     * @param buffer The ByteBuf where path-id value can be written.
+     */
+    public static void writePathId(final PathId pathId, final ByteBuf buffer) {
+        if (pathId != null && pathId.getValue() != 0) {
+            ByteBufWriteUtil.writeUnsignedInt(pathId.getValue(), buffer);
+        }
+    }
+
+}
