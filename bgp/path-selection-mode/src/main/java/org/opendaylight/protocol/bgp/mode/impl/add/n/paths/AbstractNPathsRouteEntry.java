23ac26d026943e81949eb0181227b3bcb04b83fe
Claudio D. Gasparini <cgaspari@cisco.com>
2016-03-05 22:38:16 +0100

    BUG-4827: Introduce Advertise N Paths Selection Mode
    Introduce Advertise N Paths Selection Mode
    
    Change-Id: Ia92570fe60e73a0a34d20668aea21322c1746d76
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/path-selection-mode/src/main/java/org/opendaylight/protocol/bgp/mode/impl/add/n/paths/AbstractNPathsRouteEntry.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.mode.impl.add.n.paths;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.protocol.bgp.mode.impl.add.AddPathAbstractRouteEntry;
+import org.opendaylight.protocol.bgp.mode.impl.add.AddPathBestPath;
+import org.opendaylight.protocol.bgp.mode.impl.add.RouteKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+abstract class AbstractNPathsRouteEntry extends AddPathAbstractRouteEntry {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractNPathsRouteEntry.class);
+
+    private final long nBestPaths;
+
+    AbstractNPathsRouteEntry(final Long nBestPaths) {
+        this.nBestPaths = nBestPaths;
+    }
+
+    @Override
+    public final boolean selectBest(final long localAs) {
+        final List<AddPathBestPath> newBestPathList = new ArrayList<>();
+        final List<RouteKey> keyList = this.offsets.getRouteKeysList();
+        final long maxSearch = this.nBestPaths < this.offsets.size() && this.nBestPaths != 0 ? this.nBestPaths : this.offsets.size();
+        for (long i = 0; i < maxSearch; ++i) {
+            final AddPathBestPath newBest = selectBest(localAs, keyList);
+            newBestPathList.add(newBest);
+            keyList.remove(newBest.getRouteKey());
+        }
+
+        this.bestPathRemoved = new ArrayList<>(this.bestPath);
+        if (this.bestPathRemoved.removeAll(newBestPathList) || !this.bestPath.equals(newBestPathList)) {
+            this.bestPath = newBestPathList;
+            LOG.trace("Actual Best {}, removed best {}", this.bestPath, this.bestPathRemoved);
+            return true;
+        }
+        return false;
+    }
+}
