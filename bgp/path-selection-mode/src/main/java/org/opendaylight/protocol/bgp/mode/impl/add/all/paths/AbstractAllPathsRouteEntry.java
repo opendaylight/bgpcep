dca18826b01cebe70c2137925b4f98b385074901
Claudio D. Gasparini <cgaspari@cisco.com>
2016-03-08 14:15:38 +0100

    BUG-4827: Introduce Avertise All Paths Selecetion Mode
    Introduce Avertise All Paths Selecetion Mode
    
    Change-Id: I744cf2722828234203fe3830a753ec1e0259c9ee
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/path-selection-mode/src/main/java/org/opendaylight/protocol/bgp/mode/impl/add/all/paths/AbstractAllPathsRouteEntry.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.mode.impl.add.all.paths;
+
+import com.google.common.base.Preconditions;
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.protocol.bgp.mode.api.BestPathState;
+import org.opendaylight.protocol.bgp.mode.impl.BestPathStateImpl;
+import org.opendaylight.protocol.bgp.mode.impl.add.AddPathAbstractRouteEntry;
+import org.opendaylight.protocol.bgp.mode.impl.add.AddPathBestPath;
+import org.opendaylight.protocol.bgp.mode.impl.add.RouteKey;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+abstract class AbstractAllPathsRouteEntry extends AddPathAbstractRouteEntry {
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractAllPathsRouteEntry.class);
+
+    @Override
+    public final boolean selectBest(final long localAs) {
+        final List<AddPathBestPath> newBestPathList = new ArrayList<>();
+        final List<RouteKey> keyList = this.offsets.getRouteKeysList();
+
+        if(!keyList.isEmpty()) {
+            /* we set the best path first on List for not supported Add path cases*/
+            final AddPathBestPath newBest = selectBest(localAs, keyList);
+            newBestPathList.add(newBest);
+            keyList.remove(newBest.getRouteKey());
+            /*we add the rest of path, regardless in what order they are, since this is all path case */
+            for (RouteKey key : keyList) {
+                final int offset = this.offsets.offsetOf(key);
+                final ContainerNode attributes = this.offsets.getValue(this.values, offset);
+                Preconditions.checkNotNull(key.getRouteId(), "Router ID may not be null");
+                if (attributes != null) {
+                    final BestPathState state = new BestPathStateImpl(attributes);
+                    final AddPathBestPath bestPath = new AddPathBestPath(state, key, offset, this.offsets.getValue(this.pathsId, offset));
+                    newBestPathList.add(bestPath);
+                }
+            }
+        }
+
+        this.bestPathRemoved = new ArrayList<>(this.bestPath);
+        if (this.bestPathRemoved.removeAll(newBestPathList) || !this.bestPath.equals(newBestPathList)) {
+            this.bestPath = newBestPathList;
+            LOG.trace("Actual Best {}, removed best {}", this.bestPath, this.bestPathRemoved);
+            return true;
+        }
+        return false;
+    }
+
+
+}
