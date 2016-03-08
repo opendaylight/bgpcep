3e993d1ec0f00a3cb2ea2a75652b30a4dd81d06e
Claudio D. Gasparini <cgaspari@cisco.com>
2016-03-08 14:15:38 +0100

    BUG-4827: Introduce Avertise All Paths Selecetion Mode
    Introduce Avertise All Paths Selecetion Mode
    
    Change-Id: I744cf2722828234203fe3830a753ec1e0259c9ee
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/path-selection-mode/src/main/java/org/opendaylight/protocol/bgp/mode/impl/add/all/paths/ComplexRouteEntry.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.mode.impl.add.all.paths;
+
+import com.google.common.primitives.UnsignedInteger;
+import org.opendaylight.protocol.bgp.mode.api.BestPath;
+import org.opendaylight.protocol.bgp.mode.impl.OffsetMap;
+import org.opendaylight.protocol.bgp.mode.impl.add.AddPathBestPath;
+import org.opendaylight.protocol.bgp.mode.impl.add.RouteKey;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+final class ComplexRouteEntry extends AbstractAllPathsRouteEntry {
+    private static final MapEntryNode[] EMPTY_VALUES = new MapEntryNode[0];
+    private MapEntryNode[] values = EMPTY_VALUES;
+
+    @Override
+    public boolean removeRoute(final UnsignedInteger routerId, final long remotePathId) {
+        final RouteKey key = new RouteKey(routerId, remotePathId);
+        final OffsetMap<RouteKey> map = getOffsets();
+        final int offset = map.offsetOf(key);
+        final boolean ret = removeRoute(key, offset);
+        this.values = map.removeValue(this.values, offset);
+        return ret;
+    }
+
+    @Override
+    public MapEntryNode createValue(final YangInstanceIdentifier.PathArgument routeId, final BestPath path) {
+        final OffsetMap<RouteKey> map = getOffsets();
+        final int offset = map.offsetOf(((AddPathBestPath) path).getRouteKey());
+        return map.getValue(this.values, offset);
+    }
+
+    @Override
+    public int addRoute(final UnsignedInteger routerId, final Long remotePathId, final YangInstanceIdentifier.NodeIdentifier attII, final NormalizedNode<?, ?> data) {
+        final OffsetMap<RouteKey> oldMap = getOffsets();
+        final int offset = addRoute(new RouteKey(routerId, remotePathId), attII, data);
+        final OffsetMap<RouteKey> newMap = getOffsets();
+
+        if (!newMap.equals(oldMap)) {
+            this.values = newMap.expand(oldMap, this.values, offset);
+        }
+
+        newMap.setValue(this.values, offset, data);
+        return offset;
+    }
+}
