7c42979324b70a90b3306b4cf6c00a7b2a08b0c3
Omair <of8826@att.com>
2016-03-14 13:12:55 -0700

    BUG 5031: Bring order into the BGP-LS encoders/decoders by introducing simple codecs and their registers for BGP-LS's NLRI types, to make it more extensible.
    Change-Id: Idd0740db3d3dc1828b5d5459a75bf0799b70d6ef
    Signed-off-by: Omair <of8826@att.com>

--- bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/BGPActivator.java
+++ bgp/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/BGPActivator.java
@@ -11,13 +11,27 @@ import java.util.ArrayList;
 import java.util.List;
 import org.opendaylight.protocol.bgp.linkstate.attribute.LinkstateAttributeParser;
 import org.opendaylight.protocol.bgp.linkstate.attribute.next.hop.LinkstateNextHopParser;
+import org.opendaylight.protocol.bgp.linkstate.nlri.LinkNlriParser;
 import org.opendaylight.protocol.bgp.linkstate.nlri.LinkstateNlriParser;
+import org.opendaylight.protocol.bgp.linkstate.nlri.NodeNlriParser;
+import org.opendaylight.protocol.bgp.linkstate.nlri.PrefixIpv4NlriParser;
+import org.opendaylight.protocol.bgp.linkstate.nlri.PrefixIpv6NlriParser;
+import org.opendaylight.protocol.bgp.linkstate.nlri.PrefixNlriSerializer;
+import org.opendaylight.protocol.bgp.linkstate.nlri.SimpleNlriTypeRegistry;
+import org.opendaylight.protocol.bgp.linkstate.nlri.TeLspIpv4NlriParser;
+import org.opendaylight.protocol.bgp.linkstate.nlri.TeLspIpv6NlriParser;
+import org.opendaylight.protocol.bgp.linkstate.nlri.TeLspNlriSerializer;
 import org.opendaylight.protocol.bgp.parser.spi.AbstractBGPExtensionProviderActivator;
 import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderContext;
 import org.opendaylight.protocol.rsvp.parser.spi.RSVPTeObjectRegistry;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.Attributes1;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.LinkstateAddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.LinkstateSubsequentAddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.NlriType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.linkstate.object.type.LinkCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.linkstate.object.type.NodeCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.linkstate.object.type.PrefixCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.linkstate.object.type.TeLspCase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.linkstate.routes.LinkstateRoutes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.MplsLabeledVpnSubsequentAddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.next.hop.c.next.hop.Ipv4NextHopCase;
@@ -52,6 +66,8 @@ public final class BGPActivator extends AbstractBGPExtensionProviderActivator {
     protected List<AutoCloseable> startImpl(final BGPExtensionProviderContext context) {
         final List<AutoCloseable> regs = new ArrayList<>();
 
+        final SimpleNlriTypeRegistry nlriTypeReg = SimpleNlriTypeRegistry.getInstance();
+
         regs.add(context.registerAddressFamily(LinkstateAddressFamily.class, LINKSTATE_AFI));
         regs.add(context.registerSubsequentAddressFamily(LinkstateSubsequentAddressFamily.class, LINKSTATE_SAFI));
 
@@ -66,6 +82,32 @@ public final class BGPActivator extends AbstractBGPExtensionProviderActivator {
         regs.add(context.registerAttributeSerializer(Attributes1.class, new LinkstateAttributeParser(this.ianaLinkstateAttributeType, this.rsvpTeObjectRegistry)));
         final LinkstateAttributeParser linkstateAttributeParser = new LinkstateAttributeParser(this.ianaLinkstateAttributeType, this.rsvpTeObjectRegistry);
         regs.add(context.registerAttributeParser(linkstateAttributeParser.getType(), linkstateAttributeParser));
+
+        final NodeNlriParser nodeParser = new NodeNlriParser();
+        regs.add(nlriTypeReg.registerNlriTypeParser(NlriType.Node, nodeParser));
+        regs.add(nlriTypeReg.registerNlriTypeSerializer(NodeCase.class, nodeParser));
+
+        final LinkNlriParser linkParser = new LinkNlriParser();
+        regs.add(nlriTypeReg.registerNlriTypeParser(NlriType.Link, linkParser));
+        regs.add( nlriTypeReg.registerNlriTypeSerializer(LinkCase.class, linkParser));
+
+        final PrefixIpv4NlriParser ipv4PrefParser = new PrefixIpv4NlriParser();
+        regs.add(nlriTypeReg.registerNlriTypeParser(NlriType.Ipv4Prefix, ipv4PrefParser));
+
+        final PrefixIpv6NlriParser ipv6PrefParser = new PrefixIpv6NlriParser();
+        regs.add(nlriTypeReg.registerNlriTypeParser(NlriType.Ipv6Prefix, ipv6PrefParser));
+
+        final PrefixNlriSerializer prefixSerializer = new PrefixNlriSerializer();
+        regs.add(nlriTypeReg.registerNlriTypeSerializer(PrefixCase.class, prefixSerializer));
+
+        final TeLspIpv4NlriParser telSPipv4Parser = new TeLspIpv4NlriParser();
+        regs.add(nlriTypeReg.registerNlriTypeParser(NlriType.Ipv4TeLsp, telSPipv4Parser));
+
+        final TeLspIpv6NlriParser telSPipv6Parser = new TeLspIpv6NlriParser();
+        regs.add(nlriTypeReg.registerNlriTypeParser(NlriType.Ipv6TeLsp, telSPipv6Parser));
+
+        final TeLspNlriSerializer telSpSerializer = new TeLspNlriSerializer();
+        regs.add(nlriTypeReg.registerNlriTypeSerializer(TeLspCase.class, telSpSerializer));
 
         return regs;
     }
