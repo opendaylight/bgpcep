ffb173c4fd08b386c903311e9139d62980f93876
Kevin Wang <kwang@brocade.com>
2016-03-16 15:47:47 -0700

    Disable the original TestHelper and import the new MD-SAL it base classes
    Change-Id: Ic2e626a2bc56b2cda0e776fc42d1b8bbbe88b6ed
    Signed-off-by: Kevin Wang <kevixw@gmail.com>

--- integration-tests/src/test/java/org/opendaylight/protocol/integration/pcep/AbstractPcepOsgiTest.java
+++ integration-tests/src/test/java/org/opendaylight/protocol/integration/pcep/AbstractPcepOsgiTest.java
@@ -7,28 +7,43 @@
  */
 package org.opendaylight.protocol.integration.pcep;
 
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.systemPackages;
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
-import javax.inject.Inject;
+import org.opendaylight.controller.mdsal.it.base.AbstractMdsalTestBase;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
-import org.opendaylight.controller.test.sal.binding.it.TestHelper;
+//import org.opendaylight.controller.test.sal.binding.it.TestHelper;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.options.DefaultCompositeOption;
+import org.ops4j.pax.exam.options.MavenUrlReference;
 import org.ops4j.pax.exam.util.Filter;
 import org.osgi.framework.BundleContext;
 
-//FIXME: merge with org.opendaylight.controller.test.sal.binding.it.AbstractTest ?
-public class AbstractPcepOsgiTest {
-    @Inject
+import javax.inject.Inject;
+
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemPackages;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class AbstractPcepOsgiTest extends AbstractMdsalTestBase {
+    protected static final Logger LOG = LoggerFactory.getLogger(AbstractPcepOsgiTest.class);
+    /*@Inject
     @Filter(timeout = 120 * 1000)
     BindingAwareBroker broker;
     @Inject
-    BundleContext bundleContext;
+   BundleContext bundleContext;
 
     public BindingAwareBroker getBroker() {
         return this.broker;
@@ -45,28 +60,55 @@ public class AbstractPcepOsgiTest {
     public void setBundleContext(final BundleContext bundleContext) {
         this.bundleContext = bundleContext;
     }
+*/
 
-    @Configuration
-    public Option[] config() {
-        return options(
-                // TestHelper.getLoggingBundles(), //
+    @Override
+    public String getModuleName() {
+        return "odl-pcep-api-cfg";
+    }
 
-                pcepModules(), //
-                systemProperty("osgi.bundles.defaultStartLevel").value("4"),
-                systemProperty("pax.exam.osgi.unresolved.fail").value("true"),
-                systemPackages("sun.nio.ch"),
+    @Override
+    public String getInstanceName() {
+        return "bgpcep-default";
+    }
 
-                mavenBundle("org.slf4j", "slf4j-api").versionAsInProject(), //
-                mavenBundle("org.slf4j", "log4j-over-slf4j").versionAsInProject(), //
-                mavenBundle("ch.qos.logback", "logback-core").versionAsInProject(), //
-                mavenBundle("ch.qos.logback", "logback-classic").versionAsInProject(),
-                mavenBundle("openexi", "nagasena").versionAsInProject(),
-                mavenBundle("com.github.romix", "java-concurrent-hash-trie-map").versionAsInProject(),
+    @Override
+    public MavenUrlReference getFeatureRepo() {
+        return maven().groupId("org.opendaylight.bgpcep").artifactId("features-bgp")
+                .classifier("features").type("xml").versionAsInProject();
+    }
 
-                TestHelper.mdSalCoreBundles(),
+    @Override
+    public String getFeatureName() {
+        return "odl-bgpcep-bgp";
+    }
+//
+//    @Configuration
+//    public Option[] config() {
+//        return options(
+//                // TestHelper.getLoggingBundles(), //
+//
+//                pcepModules(), //
+//                systemProperty("osgi.bundles.defaultStartLevel").value("4"),
+//                systemProperty("pax.exam.osgi.unresolved.fail").value("true"),
+//                systemPackages("sun.nio.ch"),
+//
+//                mavenBundle("org.slf4j", "slf4j-api").versionAsInProject(), //
+//                mavenBundle("org.slf4j", "log4j-over-slf4j").versionAsInProject(), //
+//                mavenBundle("ch.qos.logback", "logback-core").versionAsInProject(), //
+//                mavenBundle("ch.qos.logback", "logback-classic").versionAsInProject(),
+//                mavenBundle("openexi", "nagasena").versionAsInProject(),
+//                mavenBundle("com.github.romix", "java-concurrent-hash-trie-map").versionAsInProject(),
+//
+//                TestHelper.mdSalCoreBundles(),
+//
+//                TestHelper.bindingAwareSalBundles(), TestHelper.configMinumumBundles(), TestHelper.baseModelBundles(),
+//                TestHelper.junitAndMockitoBundles(), TestHelper.protocolFrameworkBundles());
+//    }
 
-                TestHelper.bindingAwareSalBundles(), TestHelper.configMinumumBundles(), TestHelper.baseModelBundles(),
-                TestHelper.junitAndMockitoBundles(), TestHelper.protocolFrameworkBundles());
+    @Test
+    public void testBgpecpFeatureLoad() {
+        Assert.assertTrue(true);
     }
 
     private Option pcepModules() {
@@ -90,10 +132,10 @@ public class AbstractPcepOsgiTest {
                 mavenBundle("org.opendaylight.bgpcep", "rsvp-api").versionAsInProject(), //
                 mavenBundle("org.opendaylight.bgpcep", "programming-api").versionAsInProject());
     }
-
-    abstract class AbstractTestProvider implements BindingAwareProvider {
-
-    }
+//
+//    abstract class AbstractTestProvider implements BindingAwareProvider {
+//
+//    }
 
     TopologyKey getTopologyId(final String id) {
         return new TopologyKey(new org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId(id));
