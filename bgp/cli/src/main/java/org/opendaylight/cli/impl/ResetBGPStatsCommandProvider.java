5bb448bce4ad85c072b78d9374de1dcc079c99a2
Icaro Camelo <icamelo@inocybe.com>
2016-03-30 18:41:54 -0400

    WIP: BGP statistics CLI
    Change-Id: Ie1f7d2acead057f2123b4c98f37468bebe0a32f9
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

---
+++ bgp/cli/src/main/java/org/opendaylight/cli/impl/ResetBGPStatsCommandProvider.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2016 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.cli.impl;
+
+import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
+import javax.management.ObjectName;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.opendaylight.cli.utils.BgpCliUtils;
+
+@Command(scope = "bgp", name = "reset-stats", description = "Reset BGP stats.")
+public class ResetBGPStatsCommandProvider extends OsgiCommandSupport {
+
+    @Override
+    protected Object doExecute() throws Exception {
+        PrintStream out = session.getConsole();
+        ObjectName mbeanName = new ObjectName(BgpCliUtils.BGP_MBEANS_NAME);
+
+        try {
+            ManagementFactory.getPlatformMBeanServer().invoke(mbeanName, BgpCliUtils.BGP_MBEANS_RESET_STATS_OPERATION, null, null);
+        } catch (Exception e) {
+            out.println("Error getting platform MBeanServer. \n");
+            out.println(e.getMessage());
+            return null;
+        }
+
+        out.print("BGP Statistics reset!");
+        return null;
+    }
+
+}
+
