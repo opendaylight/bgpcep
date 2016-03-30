cdf25966ab149b5fb81a51b37ba6a1e478d500ce
Icaro Camelo <icamelo@inocybe.com>
2016-03-30 18:41:54 -0400

    BGP statistics CLI
    Change-Id: Ie1f7d2acead057f2123b4c98f37468bebe0a32f9
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

---
+++ bgp/cli/src/main/java/org/opendaylight/cli/impl/ResetBGPStatsCommandProvider.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2016 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.cli.impl;
+
+import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
+import javax.management.JMX;
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.opendaylight.cli.utils.BgpCliUtils;
+import org.opendaylight.controller.config.yang.bgp.rib.impl.BGPPeerRuntimeMXBean;
+
+@Command(scope = "bgp", name = "reset-stats", description = "Reset BGP stats.")
+public class ResetBGPStatsCommandProvider extends OsgiCommandSupport {
+
+    @Override
+    protected Object doExecute() throws Exception {
+        PrintStream out = session.getConsole();
+        ObjectName managedBeanName = new ObjectName(BgpCliUtils.BGP_MBEANS_NAME);
+
+        final MBeanServerConnection conn;
+        final BGPPeerRuntimeMXBean proxy;
+        try{
+            conn =
+                    ManagementFactory.getPlatformMBeanServer();
+            proxy = JMX.newMXBeanProxy(conn, managedBeanName, BGPPeerRuntimeMXBean.class);
+        } catch (Exception e) {
+            out.println("Error getting platform MBeanServer. \n");
+            out.println(e.getMessage());
+            return null;
+        }
+
+        proxy.resetStats();
+        out.print("BGP Statistics reset!\n");
+        return null;
+    }
+
+}
+
---
+++ bgp/cli/src/main/java/org/opendaylight/cli/utils/BgpCliUtils.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2016 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.cli.utils;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import javax.management.JMException;
+import org.apache.karaf.shell.table.ShellTable;
+import org.opendaylight.controller.config.yang.bgp.rib.impl.BgpSessionState;
+
+public final class BgpCliUtils{
+    public static final String BGP_MBEANS_NAME = "org.opendaylight.controller:instanceName=example-bgp-peer,type=RuntimeBean,moduleFactoryName=bgp-peer";
+
+    public static void displayAll(final BgpSessionState bgpSessionState, final PrintStream printStream) throws IOException, JMException {
+        final ShellTable table = new ShellTable();
+        table.column("Name").alignLeft();
+        table.column("Value").alignLeft();
+        table.addRow().addContent("HoldtimeCurrent", bgpSessionState.getHoldtimeCurrent());
+        table.addRow().addContent("KeepaliveCurrent", bgpSessionState.getKeepaliveCurrent());
+        table.addRow().addContent("SessionDuration", bgpSessionState.getSessionDuration());
+        table.addRow().addContent("SessionState", bgpSessionState.getSessionState());
+
+        //Messages Stats
+        table.addRow().addContent("MessagesStats.ErrorMsgsSent", bgpSessionState.getMessagesStats().getErrorMsgs().getErrorSent().getCount());
+        table.addRow().addContent("MessagesStats.ErrorMsgsReceived", bgpSessionState.getMessagesStats().getErrorMsgs().getErrorReceived().getCount());
+        table.addRow().addContent("MessagesStats.KeepAliveMsgsSent", bgpSessionState.getMessagesStats().getKeepAliveMsgs().getSent().getCount());
+        table.addRow().addContent("MessagesStats.KeepAliveMsgsReceived", bgpSessionState.getMessagesStats().getKeepAliveMsgs().getReceived().getCount());
+        table.addRow().addContent("MessagesStats.TotalMsgsSent", bgpSessionState.getMessagesStats().getTotalMsgs().getSent().getCount());
+        table.addRow().addContent("MessagesStats.TotalMsgsReceived", bgpSessionState.getMessagesStats().getTotalMsgs().getReceived().getCount());
+        table.addRow().addContent("MessagesStats.UpdateMsgsSent", bgpSessionState.getMessagesStats().getUpdateMsgs().getSent().getCount());
+        table.addRow().addContent("MessagesStats.UpdateMsgsReceived", bgpSessionState.getMessagesStats().getUpdateMsgs().getReceived().getCount());
+
+        //Peer Preferences
+        table.addRow().addContent("PeerPreferences.AddPathCapability", bgpSessionState.getPeerPreferences().getAddPathCapability());
+        table.addRow().addContent("PeerPreferences.Address", bgpSessionState.getPeerPreferences().getAddress());
+        table.addRow().addContent("PeerPreferences.AS", bgpSessionState.getPeerPreferences().getAs());
+        table.addRow().addContent("PeerPreferences.BgpExtendedMessageCapability", bgpSessionState.getPeerPreferences().getBgpExtendedMessageCapability());
+        table.addRow().addContent("PeerPreferences.BgpId", bgpSessionState.getPeerPreferences().getBgpId());
+        table.addRow().addContent("PeerPreferences.FourOctetAsCapability", bgpSessionState.getPeerPreferences().getFourOctetAsCapability());
+        table.addRow().addContent("PeerPreferences.GrCapability", bgpSessionState.getPeerPreferences().getGrCapability());
+        table.addRow().addContent("PeerPreferences.Holdtime", bgpSessionState.getPeerPreferences().getHoldtime());
+        table.addRow().addContent("PeerPreferences.Port", bgpSessionState.getPeerPreferences().getPort());
+        table.addRow().addContent("PeerPreferences.RouteRefreshCapability", bgpSessionState.getPeerPreferences().getRouteRefreshCapability());
+
+        //Speaker Preferences
+        table.addRow().addContent("SpeakerPreferences.AddPathCapability", bgpSessionState.getSpeakerPreferences().getAddPathCapability());
+        table.addRow().addContent("SpeakerPreferences.Address", bgpSessionState.getSpeakerPreferences().getAddress());
+        table.addRow().addContent("SpeakerPreferences.AS", bgpSessionState.getSpeakerPreferences().getAs());
+        table.addRow().addContent("SpeakerPreferences.BgpExtendedMessageCapability", bgpSessionState.getSpeakerPreferences().getBgpExtendedMessageCapability());
+        table.addRow().addContent("SpeakerPreferences.BgpId", bgpSessionState.getSpeakerPreferences().getBgpId());
+        table.addRow().addContent("SpeakerPreferences.FourOctetAsCapability", bgpSessionState.getSpeakerPreferences().getFourOctetAsCapability());
+        table.addRow().addContent("SpeakerPreferences.GrCapability", bgpSessionState.getSpeakerPreferences().getGrCapability());
+        table.addRow().addContent("SpeakerPreferences.Holdtime", bgpSessionState.getSpeakerPreferences().getHoldtime());
+        table.addRow().addContent("SpeakerPreferences.Port", bgpSessionState.getSpeakerPreferences().getPort());
+        table.addRow().addContent("SpeakerPreferences.RouteRefreshCapability", bgpSessionState.getSpeakerPreferences().getRouteRefreshCapability());
+
+        table.print(printStream);
+    }
+}
