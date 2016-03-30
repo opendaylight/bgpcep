37f91e3d498cfa9ec36a43ffd106e66e8e729189
Icaro Camelo <icamelo@inocybe.com>
2016-03-30 18:41:54 -0400

    WIP: BGP statistics CLI
    Change-Id: Ie1f7d2acead057f2123b4c98f37468bebe0a32f9
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

---
+++ bgp/cli/src/main/java/org/opendaylight/cli/impl/ShowBGPStatsCommandProvider.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2016 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.cli.impl;
+
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.opendaylight.cli.utils.BgpCliUtils;
+
+import javax.management.ObjectName;
+import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
+
+
+@Command(scope = "bgp", name = "show-stats", description = "Shows BGP stats.")
+public class ShowBGPStatsCommandProvider extends OsgiCommandSupport {
+
+    @Argument(index = 0, name = "ipAddress", description = "IP Address", required = true, multiValued = false)
+    String ipAddress;
+
+    @Override
+    protected Object doExecute() throws Exception {
+        PrintStream out = session.getConsole();
+
+        ObjectName managedBeanName = new ObjectName(BgpCliUtils.BGP_MBEANS_NAME);
+
+        // Read attribute
+        Object message = null;
+
+        try{
+            message = ManagementFactory.getPlatformMBeanServer()
+                    .getAttribute(managedBeanName, BgpCliUtils.BGP_MBEANS_GET_STATS_OPERATION);
+        } catch (Exception e) {
+            out.println("Error getting platform MBeanServer. \n");
+            out.println(e.getStackTrace());
+        }
+
+        if (message == null){
+            out.println("No BgpPeerState found.");
+        }
+
+        final StringBuilder result = new StringBuilder();
+        BgpCliUtils.BGPStatisticsData state = BgpCliUtils.parseMessage(message);
+
+        if (state != null) {
+            result.append("Hold time: " + state.getHoldtimeCurrent());
+            result.append("\n");
+            result.append("Keep alive: " + state.getKeepAliveCurrent());
+            result.append("\n");
+            result.append("Session state: " + state.getSessionState());
+            result.append("\n");
+            result.append("Peer Address: " + state.getPeerAddress());
+            result.append("\n");
+            result.append("Peer AS: " + state.getPeerAs());
+            result.append("\n");
+            result.append("Gr Capability " + state.getPeerGrCapability());
+            result.append("\n");
+            result.append("Speaker Address: " + state.getSpeakerAddress());
+            result.append("\n");
+            result.append("Speaker Port: " + state.getSpeakerPort());
+            result.append("\n");
+            result.append("Total Msgs received: " + state.getTotalMsgsReceived());
+            result.append("\n");
+            result.append("Total Msgs sent: " + state.getTotalMsgsSent());
+            result.append("\n");
+            out.print(result.toString());
+        }
+        return null;
+    }
+
+}
