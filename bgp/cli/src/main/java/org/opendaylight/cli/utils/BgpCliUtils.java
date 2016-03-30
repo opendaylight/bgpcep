92508219cf5bd833e0cac849d73fc3e55b04d97d
Icaro Camelo <icamelo@inocybe.com>
2016-03-30 18:41:54 -0400

    WIP: BGP statistics CLI
    Change-Id: Ie1f7d2acead057f2123b4c98f37468bebe0a32f9
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

---
+++ bgp/cli/src/main/java/org/opendaylight/cli/utils/BgpCliUtils.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2016 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.cli.utils;
+
+public final class BgpCliUtils{
+    public static final String BGP_MBEANS_GET_STATS_OPERATION = "BgpPeerState";
+    public static final String BGP_MBEANS_NAME = "org.opendaylight.controller:instanceName=example-bgp-peer";
+    public static final String BGP_MBEANS_RESET_STATS_OPERATION = " resetStats";
+}
