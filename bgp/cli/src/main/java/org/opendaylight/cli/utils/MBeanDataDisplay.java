9ba4d4a04e8cbd80463636cffd7401d3caa71803
Icaro Camelo <icamelo@inocybe.com>
2016-03-30 18:41:54 -0400

    WIP: BGP statistics CLI
    Change-Id: Ie1f7d2acead057f2123b4c98f37468bebe0a32f9
    Signed-off-by: Icaro Camelo <icamelo@inocybe.com>

---
+++ bgp/cli/src/main/java/org/opendaylight/cli/utils/MBeanDataDisplay.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2007 Sun Microsystems, Inc.  All Rights Reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.cli.utils;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.management.JMException;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanInfo;
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+
+/*
+\* Class MBeanDataDisplay
+\* @author Sun Microsystems, Inc.
+\*/
+public class MBeanDataDisplay {
+
+    /*
+    \* A logger for this class.
+    \*\*/
+    private static final Logger LOG =
+            Logger.getLogger(MBeanDataDisplay.class.getName());
+
+    final MBeanServerConnection server;
+    final TextDataDisplay dataDisplay = new TextDataDisplay();
+
+    /*\* Creates a new instance of MBeanDataDisplay \*/
+    public MBeanDataDisplay(MBeanServerConnection conn) {
+        this.server = conn;
+    }
+
+    StringBuffer writeAttribute(StringBuffer buffer,
+                                String prefix, ObjectName mbean,
+                                MBeanAttributeInfo info, Object value) {
+        buffer.append(prefix).append("# ").append(info.getDescription())
+                .append("\\n");
+        return dataDisplay.write(buffer, prefix, info.getName(), value);
+    }
+
+    public StringBuffer write(StringBuffer buffer, String prefix, ObjectName mbean)
+            throws IOException, JMException {
+        final MBeanInfo info = server.getMBeanInfo(mbean);
+        buffer.append(prefix).append("MBean: ").append(mbean).append("\\n");
+        buffer.append(prefix).append("{\\n");
+        final String attrPrefix = prefix + "   ";
+        final MBeanAttributeInfo[] attributes = info.getAttributes();
+        for (MBeanAttributeInfo attr : attributes) {
+            Object toWrite = null;
+            try {
+                toWrite = server.getAttribute(mbean, attr.getName());
+            } catch (Exception x) {
+                toWrite = x;
+            }
+            writeAttribute(buffer, attrPrefix, mbean, attr, toWrite);
+            buffer.append("\\n");
+        }
+        buffer.append(prefix).append("}\\n");
+        return buffer;
+    }
+
+    public String toString(String prefix, ObjectName mbean)
+            throws IOException, JMException {
+        return write(new StringBuffer(), prefix, mbean).toString();
+    }
+
+    public String toString(ObjectName mbean)
+            throws IOException, JMException {
+        return toString("", mbean);
+    }
+}
---
+++ bgp/cli/src/main/java/org/opendaylight/cli/utils/TextDataDisplay.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2007 Sun Microsystems, Inc.  All Rights Reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.cli.utils;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.TabularData;
+
+public class TextDataDisplay {
+
+    //Creates a new instance of TextDataDisplay
+    public TextDataDisplay() {
+    }
+
+    public StringBuffer write(StringBuffer buffer, String prefix,
+                              String name, Object data) {
+        if (data == null) return writeSimple(buffer, prefix, name, null, true);
+        if (data.getClass().isArray())
+            return writeArray(buffer, prefix, name, data);
+        if (data instanceof CompositeData)
+            return writeCompositeData(buffer, prefix, name, (CompositeData) data);
+        if (data instanceof TabularData)
+            return writeTabularData(buffer, prefix, name, (TabularData) data);
+        if (data instanceof Map)
+            return writeMap(buffer, prefix, name, (Map) data);
+        if (data instanceof Collection) {
+            return writeArray(buffer, prefix, name, ((Collection) data).toArray());
+        }
+        return writeSimple(buffer, prefix, name, data, true);
+    }
+
+    String toString(Object data) {
+        if (data == null) return "null";
+        else return data.toString();
+    }
+
+    StringBuffer writeSimple(StringBuffer buffer, String prefix,
+                             String name, Object data, boolean writeline) {
+        buffer.append(prefix).append(name).append("=").append(toString(data));
+        if (writeline) buffer.append("\\n");
+        return buffer;
+    }
+
+    StringBuffer writeArray(StringBuffer buffer, String prefix,
+                            String name, Object array) {
+        if (array == null)
+            return writeSimple(buffer, prefix, name, null, true);
+        final int length = Array.getLength(array);
+        for (int i = 0; i < length; i++) {
+            final Object data = Array.get(array, i);
+            write(buffer, prefix, name + "[" + i + "]", data);
+        }
+        return buffer;
+    }
+
+    StringBuffer writeCompositeData(StringBuffer buffer,
+                                    String prefix, String name, CompositeData data) {
+        if (data == null)
+            return writeSimple(buffer, prefix, name, null, true);
+        writeSimple(buffer, prefix, name, "CompositeData(" +
+                data.getCompositeType().getTypeName() + ")", true);
+        buffer.append(prefix).append("{").append("\\n");
+        final String fieldprefix = prefix + " ";
+        for (String key : data.getCompositeType().keySet()) {
+            write(buffer, fieldprefix, name + "." + key, data.get(key));
+        }
+        buffer.append(prefix).append("}").append("\\n");
+        return buffer;
+    }
+
+    StringBuffer writeTabularData(StringBuffer buffer,
+                                  String prefix, String name, TabularData data) {
+        if (data == null)
+            return writeSimple(buffer, prefix, name, null, true);
+        writeSimple(buffer, prefix, name, "TabularData(" +
+                data.getTabularType().getTypeName() + ")", true);
+        final List<String> keyNames = data.getTabularType().getIndexNames();
+        final int indexCount = keyNames.size();
+        for (Object keys : data.keySet()) {
+            final Object[] keyValues = ((List<?>) keys).toArray();
+            final StringBuilder b = new StringBuilder(name);
+            b.append("[");
+            for (int i = 0; i < indexCount; i++) {
+                if (i > 0) b.append(", ");
+                b.append(keyNames.get(i) + "=" + keyValues[i]);
+            }
+            b.append("]");
+            writeCompositeData(buffer, prefix, b.toString(), data.get(keyValues));
+            b.append("\\n");
+        }
+        return buffer;
+    }
+
+    StringBuffer writeMap(StringBuffer buffer,
+                          String prefix, String name, Map<Object, Object> data) {
+        if (data == null)
+            return writeSimple(buffer, prefix, name, null, true);
+        writeSimple(buffer, prefix, name, "java.util.Map", true);
+        for (Entry<Object, Object> e : data.entrySet()) {
+            write(buffer, prefix, name + "[" + e.getKey() + "]", e.getValue());
+        }
+        return buffer;
+    }
+
+}
