ced1821f914998db1ce1cb756af6e49084343b91
Kevin Wang <kevixw@gmail.com>
2016-04-01 00:17:13 +0000

    Add support of IPv6 VPN ext (RFC4659) for BGP-MPLS
    Change-Id: Iaebcb20ed461667626f13c44e2011610212f8893
    Signed-off-by: Kevin Wang <kevixw@gmail.com>

---
+++ bgp/l3vpn/src/main/java/org/opendaylight/protocol/bgp/l3vpn/ipv6/RibIpv6Activator.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.l3vpn.ipv6;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.protocol.bgp.rib.spi.AbstractRIBExtensionProviderActivator;
+import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv6AddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.MplsLabeledVpnSubsequentAddressFamily;
+
+/**
+ * @author Kevin Wang
+ */
+public class RibIpv6Activator extends AbstractRIBExtensionProviderActivator {
+
+    @Override
+    protected List<AutoCloseable> startRIBExtensionProviderImpl(final RIBExtensionProviderContext context) {
+        final List<AutoCloseable> regs = new ArrayList<>();
+        regs.add(context.registerRIBSupport(Ipv6AddressFamily.class, MplsLabeledVpnSubsequentAddressFamily.class, new VpnIpv6RIBSupport()));
+        return regs;
+    }
+
+}
