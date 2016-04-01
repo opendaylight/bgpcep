cc68ec065244ea474512b7aaf73907ab9be3ddfe
Kevin Wang <kevixw@gmail.com>
2016-04-01 00:17:13 +0000

    Add support of IPv6 VPN ext (RFC4659) for BGP-MPLS
    Change-Id: Iaebcb20ed461667626f13c44e2011610212f8893
    Signed-off-by: Kevin Wang <kevixw@gmail.com>

--- bgp/l3vpn/src/main/java/org/opendaylight/controller/config/yang/bgp/vpn/ipv6/VpnIpv6Module.java
+++ bgp/l3vpn/src/main/java/org/opendaylight/controller/config/yang/bgp/vpn/ipv6/VpnIpv6Module.java
@@ -1,25 +1,25 @@
 /*
- * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.controller.config.yang.bgp.vpn.ipv4;
+package org.opendaylight.controller.config.yang.bgp.vpn.ipv6;
 
-import org.opendaylight.protocol.bgp.l3vpn.ipv4.BgpIpv4Activator;
-import org.opendaylight.protocol.bgp.l3vpn.ipv4.RibIpv4Activator;
+import org.opendaylight.protocol.bgp.l3vpn.ipv6.BgpIpv6Activator;
+import org.opendaylight.protocol.bgp.l3vpn.ipv6.RibIpv6Activator;
 import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderActivator;
 import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderContext;
 import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderActivator;
 import org.opendaylight.protocol.bgp.rib.spi.RIBExtensionProviderContext;
 
-public class VpnIpv4Module extends AbstractVpnIpv4Module {
-    public VpnIpv4Module(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+public class VpnIpv6Module extends org.opendaylight.controller.config.yang.bgp.vpn.ipv6.AbstractVpnIpv6Module {
+    public VpnIpv6Module(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
 
-    public VpnIpv4Module(final org.opendaylight.controller.config.api.ModuleIdentifier identifier, final org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, final org.opendaylight.controller.config.yang.bgp.vpn.ipv4.VpnIpv4Module oldModule, final java.lang.AutoCloseable oldInstance) {
+    public VpnIpv6Module(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.bgp.vpn.ipv6.VpnIpv6Module oldModule, java.lang.AutoCloseable oldInstance) {
         super(identifier, dependencyResolver, oldModule, oldInstance);
     }
 
@@ -30,26 +30,31 @@ public class VpnIpv4Module extends AbstractVpnIpv4Module {
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        final class VpnIpv4Extension implements AutoCloseable, BGPExtensionProviderActivator, RIBExtensionProviderActivator {
-            private final BGPExtensionProviderActivator bgpact = new BgpIpv4Activator();
-            private final RIBExtensionProviderActivator ribact = new RibIpv4Activator();
+        final class VpnIpv6Extension implements AutoCloseable, BGPExtensionProviderActivator, RIBExtensionProviderActivator {
+            private final BGPExtensionProviderActivator bgpact = new BgpIpv6Activator();
+            private final RIBExtensionProviderActivator ribact = new RibIpv6Activator();
+
             @Override
             public void startRIBExtensionProvider(
                 final RIBExtensionProviderContext context) {
                 this.ribact.startRIBExtensionProvider(context);
             }
+
             @Override
             public void stopRIBExtensionProvider() {
                 this.ribact.stopRIBExtensionProvider();
             }
+
             @Override
             public void start(final BGPExtensionProviderContext context) {
                 this.bgpact.start(context);
             }
+
             @Override
             public void stop() {
                 this.bgpact.stop();
             }
+
             @Override
             public void close() throws Exception {
                 if (this.bgpact != null) {
@@ -60,7 +65,7 @@ public class VpnIpv4Module extends AbstractVpnIpv4Module {
                 }
             }
         }
-        return new VpnIpv4Extension();
+        return new VpnIpv6Extension();
     }
 
 }
--- bgp/l3vpn/src/main/java/org/opendaylight/protocol/bgp/l3vpn/ipv4/VpnIpv4RIBSupport.java
+++ bgp/l3vpn/src/main/java/org/opendaylight/protocol/bgp/l3vpn/ipv4/VpnIpv4RIBSupport.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.protocol.bgp.l3vpn;
+package org.opendaylight.protocol.bgp.l3vpn.ipv4;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableCollection;
