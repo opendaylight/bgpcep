de46ee0e8220e83712cbf2c14788e30cf1b59f16
Claudio D. Gasparini <cgaspari@cisco.com>
2016-04-05 11:21:16 +0200

    BUG-4826: Evpn ESI Handlers
    Evpn ESI Handlers
    https://tools.ietf.org/html/rfc7432#section-5
    
    Change-Id: I10fce0720ed60e7f37f416e7af741c61950afa85
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/impl/esi/types/ASGenParser.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.evpn.impl.esi.types;
+
+import static org.opendaylight.protocol.bgp.evpn.impl.esi.types.EsiModelUtil.extractAS;
+import static org.opendaylight.protocol.bgp.evpn.impl.esi.types.EsiModelUtil.extractLD;
+import static org.opendaylight.protocol.util.ByteBufWriteUtil.writeUnsignedInt;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.AsNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.EsiType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.Esi;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.esi.AsGeneratedCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.esi.AsGeneratedCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.esi.as.generated._case.AsGenerated;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.esi.as.generated._case.AsGeneratedBuilder;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+final class ASGenParser extends AbstractEsiType {
+
+    @Override
+    public void serializeBody(final Esi esi, final ByteBuf body) {
+        Preconditions.checkArgument(esi instanceof AsGeneratedCase, "Unknown esi instance. Passed %s. Needed AsGeneratedCase.", esi.getClass());
+        final AsGenerated asGen = ((AsGeneratedCase) esi).getAsGenerated();
+        writeUnsignedInt(asGen.getAs().getValue(), body);
+        writeUnsignedInt(asGen.getLocalDiscriminator(), body);
+        body.writeZero(ZERO_BYTE);
+    }
+
+    @Override
+    protected EsiType getType() {
+        return EsiType.AsGenerated;
+    }
+
+    @Override
+    public Esi serializeEsi(final ContainerNode esi) {
+        final AsGeneratedBuilder builder = new AsGeneratedBuilder();
+        builder.setLocalDiscriminator(extractLD(esi));
+        builder.setAs(extractAS(esi));
+        return new AsGeneratedCaseBuilder().setAsGenerated(builder.build()).build();
+    }
+
+    @Override
+    public Esi parseEsi(final ByteBuf buffer) {
+        final AsGenerated asGen = new AsGeneratedBuilder().setAs(new AsNumber(buffer.readUnsignedInt()))
+            .setLocalDiscriminator(buffer.readUnsignedInt()).build();
+        return new AsGeneratedCaseBuilder().setAsGenerated(asGen).build();
+    }
+}
---
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/spi/EsiSerializer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.evpn.spi;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.Esi;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+public interface EsiSerializer {
+    /**
+     * Serialize Ethernet Segment Identifier
+     *
+     * @param esi Ethernet Segment Identifier
+     * @param buffer write in Bytebuf encoded ESI body
+     */
+    void serializeEsi(Esi esi, ByteBuf buffer);
+
+    /**
+     * Serialize Ethernet Segment Identifier Model
+     *
+     * @param esi Ethernet Segment Identifier Model
+     * @return Ethernet Segment Identifier
+     */
+    Esi serializeEsi(ContainerNode esi);
+}
