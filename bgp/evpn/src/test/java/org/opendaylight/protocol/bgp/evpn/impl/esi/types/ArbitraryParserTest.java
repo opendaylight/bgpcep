9b1ea21c23771b99cc47d3fce27305af2b819419
Claudio D. Gasparini <cgaspari@cisco.com>
2016-04-05 14:57:16 +0200

    BUG-4826: Evpn ESI type Handlers test
    Evpn ESI Handlers test implementation
    
    Change-Id: I311e23b4b024c28f008d8b1142d2e73129c9d393
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/evpn/src/test/java/org/opendaylight/protocol/bgp/evpn/impl/esi/types/ArbitraryParserTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.evpn.impl.esi.types;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.opendaylight.protocol.bgp.evpn.impl.ModelTestUtil.createContBuilder;
+import static org.opendaylight.protocol.bgp.evpn.impl.ModelTestUtil.createValueBuilder;
+import static org.opendaylight.protocol.bgp.evpn.impl.esi.types.EsiModelUtil.ARB_NID;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.protocol.util.ByteArray;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.Esi;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.esi.ArbitraryCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.esi.ArbitraryCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.esi.AsGeneratedCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.esi.arbitrary._case.Arbitrary;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.esi.arbitrary._case.ArbitraryBuilder;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+public final class ArbitraryParserTest extends AbstractParserTest {
+    private static final byte[] ARB_VALUE = {(byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06, (byte) 0x07,
+        (byte) 0x08, (byte) 0x09};
+    private static final byte[] ARB_RESULT = {(byte) 0x00, (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05, (byte) 0x06,
+        (byte) 0x07, (byte) 0x08, (byte) 0x09};
+    private ArbitraryParser parser;
+
+    @Before
+    public void setUp() {
+        this.parser = new ArbitraryParser();
+    }
+
+    @Test
+    public void parserTest() {
+        final ByteBuf buff = Unpooled.buffer(VALUE_SIZE);
+
+        final ArbitraryCase acb = new ArbitraryCaseBuilder().setArbitrary(new ArbitraryBuilder().setArbitrary(ARB_VALUE).build()).build();
+        this.parser.serializeEsi(acb, buff);
+        assertArrayEquals(ARB_RESULT, ByteArray.getAllBytes(buff));
+
+        final Esi acResult = this.parser.parseEsi(Unpooled.wrappedBuffer(ARB_VALUE));
+        assertEquals(acb, acResult);
+
+        final ContainerNode cont = createContBuilder(new NodeIdentifier(Arbitrary.QNAME))
+            .addChild(createValueBuilder(ARB_VALUE, ARB_NID).build()).build();
+        final Esi acmResult = this.parser.serializeEsi(cont);
+        assertEquals(acb, acmResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void wrongCaseTest() {
+        this.parser.serializeEsi(new AsGeneratedCaseBuilder().build(), null);
+    }
+}
