1d49f4cba3ed16359f9ae4ee7577f3cd4a905d4c
Claudio D. Gasparini <cgaspari@cisco.com>
2016-04-05 11:22:59 +0200

    BUG-4826: Evpn Extended communities handlers
    Evpn Extended communities handlers
    https://tools.ietf.org/html/rfc7432#section-7.5
    
    Change-Id: I20328e15b60a885b64a9ffbf4ac34eb6fe7a23f8
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/BGPActivator.java
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/BGPActivator.java
@@ -10,15 +10,43 @@ package org.opendaylight.protocol.bgp.evpn;
 import java.util.ArrayList;
 import java.util.List;
 import org.opendaylight.protocol.bgp.evpn.impl.esi.types.ESIActivator;
+import org.opendaylight.protocol.bgp.evpn.impl.extended.communities.DefaultGatewayExtCom;
+import org.opendaylight.protocol.bgp.evpn.impl.extended.communities.ESILabelExtCom;
+import org.opendaylight.protocol.bgp.evpn.impl.extended.communities.ESImpRouteTargetExtCom;
+import org.opendaylight.protocol.bgp.evpn.impl.extended.communities.MACMobExtCom;
 import org.opendaylight.protocol.bgp.parser.spi.AbstractBGPExtensionProviderActivator;
 import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderContext;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.evpn.routes.evpn.routes.evpn.route.attributes.extended.communities.extended.community.DefaultGatewayExtendedCommunityCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.update.attributes.extended.communities.extended.community.EsImportRouteExtendedCommunityCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.update.attributes.extended.communities.extended.community.EsiLabelExtendedCommunityCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.update.attributes.extended.communities.extended.community.MacMobilityExtendedCommunityCase;
 
-public final class BGPActivator extends AbstractBGPExtensionProviderActivator {
+
+public class BGPActivator extends AbstractBGPExtensionProviderActivator {
     @Override
     protected List<AutoCloseable> startImpl(final BGPExtensionProviderContext context) {
         final List<AutoCloseable> regs = new ArrayList<>();
 
+        registerExtendedCommunities(context, regs);
         ESIActivator.registerEsiTypeParsers(regs);
         return regs;
+    }
+
+    private void registerExtendedCommunities(final BGPExtensionProviderContext context, final List<AutoCloseable> regs) {
+        final DefaultGatewayExtCom defGEC = new DefaultGatewayExtCom();
+        regs.add(context.registerExtendedCommunityParser(defGEC.getType(true), defGEC.getSubType(), defGEC));
+        regs.add(context.registerExtendedCommunitySerializer(DefaultGatewayExtendedCommunityCase.class, defGEC));
+
+        final ESILabelExtCom esiLEC = new ESILabelExtCom();
+        regs.add(context.registerExtendedCommunityParser(esiLEC.getType(true), esiLEC.getSubType(), esiLEC));
+        regs.add(context.registerExtendedCommunitySerializer(EsiLabelExtendedCommunityCase.class, esiLEC));
+
+        final ESImpRouteTargetExtCom esImpEC = new ESImpRouteTargetExtCom();
+        regs.add(context.registerExtendedCommunityParser(esImpEC.getType(true), esImpEC.getSubType(), esImpEC));
+        regs.add(context.registerExtendedCommunitySerializer(EsImportRouteExtendedCommunityCase.class, esImpEC));
+
+        final MACMobExtCom macEC = new MACMobExtCom();
+        regs.add(context.registerExtendedCommunityParser(macEC.getType(true), macEC.getSubType(), macEC));
+        regs.add(context.registerExtendedCommunitySerializer(MacMobilityExtendedCommunityCase.class, macEC));
     }
 }
---
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/impl/extended/communities/DefaultGatewayExtCom.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.evpn.impl.extended.communities;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
+import org.opendaylight.protocol.bgp.parser.BGPParsingException;
+import org.opendaylight.protocol.bgp.parser.spi.extended.community.AbstractOpaqueExtendedCommunity;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321._default.gateway.extended.community.DefaultGatewayExtendedCommunityBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.evpn.routes.evpn.routes.evpn.route.attributes.extended.communities.extended.community.DefaultGatewayExtendedCommunityCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.evpn.routes.evpn.routes.evpn.route.attributes.extended.communities.extended.community.DefaultGatewayExtendedCommunityCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.extended.community.ExtendedCommunity;
+
+public final class DefaultGatewayExtCom extends AbstractOpaqueExtendedCommunity {
+    private static final int SUBTYPE = 13;
+    private static final int RESERVED = 6;
+
+    @Override
+    public ExtendedCommunity parseExtendedCommunity(final ByteBuf buffer) throws BGPDocumentedException, BGPParsingException {
+        Preconditions.checkArgument(buffer.readableBytes() == RESERVED, "Wrong length of array of bytes. Passed: %s .", buffer.readableBytes());
+        buffer.skipBytes(RESERVED);
+        return new DefaultGatewayExtendedCommunityCaseBuilder().setDefaultGatewayExtendedCommunity(
+            new DefaultGatewayExtendedCommunityBuilder().build()).build();
+    }
+
+    @Override
+    public void serializeExtendedCommunity(final ExtendedCommunity extendedCommunity, final ByteBuf byteAggregator) {
+        Preconditions.checkArgument(extendedCommunity instanceof DefaultGatewayExtendedCommunityCase,
+            "The extended community %s is not DefaultGatewayExtendedCommunityCase type.", extendedCommunity);
+        byteAggregator.writeZero(RESERVED);
+    }
+
+    @Override
+    public int getSubType() {
+        return SUBTYPE;
+    }
+}
