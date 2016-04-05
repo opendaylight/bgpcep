2ea59129f01d1ee551fddc7aa645792d4a1b38d0
Claudio D. Gasparini <cgaspari@cisco.com>
2016-04-05 11:27:35 +0200

    BUG-4826: BGP Evpn Nlri's registry
    Implementation of registry for handle Nlri's.
    
    Change-Id: If71959b5c37f5c3b62268c25c1c10469010a4c08
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/spi/EvpnRegistry.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.evpn.spi;
+
+import io.netty.buffer.ByteBuf;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.NlriType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.evpn.EvpnChoice;
+import org.opendaylight.yangtools.yang.data.api.schema.ChoiceNode;
+
+public interface EvpnRegistry {
+    /**
+     * Decode input buffer to BGP Evpn.
+     *
+     * @param type Nlri Type
+     * @param buffer encoded EvpnChoice body in Bytebuf
+     * @return EvpnChoice
+     */
+    EvpnChoice parseEvpn(@Nonnull NlriType type, @Nonnull ByteBuf buffer);
+
+    /**
+     * Encode input BGP Evpn to output buffer
+     *
+     * @param evpn EvpnChoice
+     * @param common encoded common Evpn
+     * @return encoded EvpnChoice body in Bytebuf
+     */
+    ByteBuf serializeEvpn(@Nonnull EvpnChoice evpn, @Nonnull ByteBuf common);
+
+    /**
+     * Decode Evpn Model to Evpn.
+     *
+     * @param evpnChoice ChoiceNode containing Evpn
+     * @return EvpnChoice
+     */
+    EvpnChoice serializeEvpnModel(@Nonnull ChoiceNode evpnChoice);
+
+    /**
+     * Create Route key from Evpn model
+     *
+     * @param evpnChoice ChoiceNode containing Evpn
+     * @return EvpnChoice
+     */
+    EvpnChoice serializeEvpnRouteKey(@Nonnull ChoiceNode evpnChoice);
+}
---
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/spi/EvpnSerializer.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.evpn.spi;
+
+import io.netty.buffer.ByteBuf;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.evpn.EvpnChoice;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+public interface EvpnSerializer {
+    /**
+     * @param evpn Evpn
+     * @param buffer Encode common Evpn parts to output buffer
+     * @return Encode Evpn to output buffer
+     */
+    ByteBuf serializeEvpn(@Nonnull EvpnChoice evpn, @Nonnull ByteBuf buffer);
+
+    /**
+     * Serialize Evpn
+     *
+     * @param evpn ChoiceNode containing Evpn
+     * @return Evpn
+     */
+    EvpnChoice serializeEvpnModel(@Nonnull ContainerNode evpn);
+
+    /**
+     * create Route key from Evpn model
+     *
+     * @param evpn ContainerNode containing Evpn
+     * @return Evpn
+     */
+    EvpnChoice createRouteKey(@Nonnull ContainerNode evpn);
+}
