19543be926c772bfa108420635f62dba831842b5
Claudio D. Gasparini <cgaspari@cisco.com>
2016-04-05 11:22:59 +0200

    WIP BUG-4826: Evpn Extended communities handlers
    Evpn Extended communities handlers
    https://tools.ietf.org/html/rfc7432#section-7.5
    
    Change-Id: I20328e15b60a885b64a9ffbf4ac34eb6fe7a23f8
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/impl/extended/communities/ESILabelExtCom.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.evpn.impl.extended.communities;
+
+import static org.opendaylight.protocol.util.MplsLabelUtil.byteBufForMplsLabel;
+import static org.opendaylight.protocol.util.MplsLabelUtil.mplsLabelForByteBuf;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
+import org.opendaylight.protocol.bgp.parser.BGPParsingException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.label.extended.community.EsiLabelExtendedCommunity;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.label.extended.community.EsiLabelExtendedCommunityBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.evpn.routes.evpn.routes.evpn.route.attributes.extended.communities.extended.community.EsiLabelExtendedCommunityCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.evpn.routes.evpn.routes.evpn.route.attributes.extended.communities.extended.community.EsiLabelExtendedCommunityCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.extended.community.ExtendedCommunity;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.concepts.rev131125.MplsLabel;
+
+public final class ESILabelExtCom extends AbstractExtendedCommunities {
+    private static final int SUBTYPE = 1;
+    private static final int RESERVED = 2;
+
+    @Override
+    public ExtendedCommunity parseExtendedCommunity(final ByteBuf buffer) throws BGPDocumentedException, BGPParsingException {
+        final boolean singleActive = buffer.readBoolean();
+        buffer.skipBytes(RESERVED);
+        final MplsLabel label = mplsLabelForByteBuf(buffer);
+        return new EsiLabelExtendedCommunityCaseBuilder().setEsiLabelExtendedCommunity(
+            new EsiLabelExtendedCommunityBuilder().setEsiLabel(label).setSingleActiveMode(singleActive).build()).build();
+    }
+
+    @Override
+    public void serializeExtendedCommunity(final ExtendedCommunity extendedCommunity, final ByteBuf byteAggregator) {
+        Preconditions.checkArgument(extendedCommunity instanceof EsiLabelExtendedCommunityCase,
+            "The extended community %s is not EsiLabelExtendedCommunityCaseCase type.", extendedCommunity);
+        final EsiLabelExtendedCommunity extCom = ((EsiLabelExtendedCommunityCase) extendedCommunity).getEsiLabelExtendedCommunity();
+        byteAggregator.writeBoolean(extCom.isSingleActiveMode());
+        byteAggregator.writeZero(RESERVED);
+        byteAggregator.writeBytes(byteBufForMplsLabel(extCom.getEsiLabel()));
+    }
+
+    @Override
+    public int getSubType() {
+        return SUBTYPE;
+    }
+}
---
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/impl/extended/communities/MACMobExtCom.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.evpn.impl.extended.communities;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
+import org.opendaylight.protocol.bgp.parser.BGPParsingException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.evpn.routes.evpn.routes.evpn.route.attributes.extended.communities.extended.community.MacMobilityExtendedCommunityCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.evpn.routes.evpn.routes.evpn.route.attributes.extended.communities.extended.community.MacMobilityExtendedCommunityCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.mac.mobility.extended.community.MacMobilityExtendedCommunity;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.mac.mobility.extended.community.MacMobilityExtendedCommunityBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.extended.community.ExtendedCommunity;
+
+public final class MACMobExtCom extends AbstractExtendedCommunities {
+    private static final int SUBTYPE = 1;
+    private static final int RESERVED = 1;
+
+    @Override
+    public ExtendedCommunity parseExtendedCommunity(final ByteBuf buffer) throws BGPDocumentedException, BGPParsingException {
+        final boolean isStatic = buffer.readBoolean();
+        buffer.skipBytes(RESERVED);
+        final long seqNumber = buffer.readUnsignedInt();
+        return new MacMobilityExtendedCommunityCaseBuilder().setMacMobilityExtendedCommunity(new MacMobilityExtendedCommunityBuilder()
+            .setStatic(isStatic).setSeqNumber(seqNumber).build()).build();
+    }
+
+    @Override
+    public void serializeExtendedCommunity(final ExtendedCommunity extendedCommunity, final ByteBuf byteAggregator) {
+        Preconditions.checkArgument(extendedCommunity instanceof MacMobilityExtendedCommunityCase,
+            "The extended community %s is not MacMobilityExtendedCommunityCase type.", extendedCommunity);
+        final MacMobilityExtendedCommunity extCom = ((MacMobilityExtendedCommunityCase) extendedCommunity).getMacMobilityExtendedCommunity();
+        byteAggregator.writeBoolean(extCom.isStatic());
+        byteAggregator.writeZero(RESERVED);
+        byteAggregator.writeInt(extCom.getSeqNumber().intValue());
+    }
+
+    @Override
+    public int getSubType() {
+        return SUBTYPE;
+    }
+}
