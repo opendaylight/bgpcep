348d188016ffd07bcf69e4bd018ba0470723468d
Claudio D. Gasparini <cgaspari@cisco.com>
2016-04-05 11:57:10 +0200

    WIP BUG-4826: Evpn ESI Registry
    Evpn ESI Registry Implementation
    
    Change-Id: I50ee6ddf717fe1553c7b57197ee91e43ad91f349
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/spi/pojo/SimpleEsiTypeRegistry.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.evpn.spi.pojo;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import java.util.HashMap;
+import java.util.Map;
+import org.opendaylight.protocol.bgp.evpn.spi.EsiParser;
+import org.opendaylight.protocol.bgp.evpn.spi.EsiRegistry;
+import org.opendaylight.protocol.bgp.evpn.spi.EsiSerializer;
+import org.opendaylight.protocol.concepts.HandlerRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.EsiType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.esi.Esi;
+import org.opendaylight.yangtools.yang.binding.DataContainer;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+public final class SimpleEsiTypeRegistry implements EsiRegistry {
+    private static final SimpleEsiTypeRegistry SINGLETON = new SimpleEsiTypeRegistry();
+    private static final int CONTENT_LENGTH = 10;
+    private static final int ESI_LENGTH = 9;
+    private final HandlerRegistry<DataContainer, EsiParser, EsiSerializer> handlers = new HandlerRegistry<>();
+    private final Map<Class<? extends Esi>, EsiSerializer> modelSerializer = new HashMap<>();
+
+    private SimpleEsiTypeRegistry() {
+    }
+
+    public static SimpleEsiTypeRegistry getInstance() {
+        return SINGLETON;
+    }
+
+    public AutoCloseable registerEsiParser(final EsiType esiType, final EsiParser parser) {
+        return this.handlers.registerParser(esiType.getIntValue(), parser);
+    }
+
+    public AutoCloseable registerEsiSerializer(final Class<? extends Esi> esiType, final EsiSerializer serializer) {
+        this.modelSerializer.put(esiType, serializer);
+        return this.handlers.registerSerializer(esiType, serializer);
+    }
+
+    @Override
+    public Esi parseEsi(final ByteBuf buffer) {
+        Preconditions.checkArgument(buffer != null && buffer.isReadable(), "Array of bytes is mandatory. Can't be null or empty.");
+        Preconditions.checkArgument(buffer.readableBytes() == CONTENT_LENGTH, "Wrong length of array of bytes. Passed: " + buffer.readableBytes() + ";");
+
+        final EsiParser parser = this.handlers.getParser(EsiType.forValue(buffer.readByte()).getIntValue());
+        if (parser == null) {
+            return null;
+        }
+        return parser.parseEsi(buffer.readSlice(ESI_LENGTH));
+    }
+
+    @Override
+    public void serializeEsi(final Esi esi, final ByteBuf buffer) {
+        final EsiSerializer serializer = this.handlers.getSerializer(esi.getImplementedInterface());
+        if (serializer == null) {
+            return;
+        }
+        serializer.serializeEsi(esi, buffer);
+    }
+
+    @Override
+    public Esi serializeEsiModel(final Class<? extends Esi> esiType, final ContainerNode esiChoice) {
+        return this.modelSerializer.get(esiType).serializeEsi(esiChoice);
+    }
+}
