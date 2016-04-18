bc86e5eb151ec1004d1dd8c7b36b71e8972464e9
Iveta Halanova <ihalanov@cisco.com>
2016-04-18 10:26:20 +0200

    BUG-5441: BGP SR prefix SID extension
    ref.: https://tools.ietf.org/html/draft-ietf-idr-bgp-prefix-sid-02
    
    Updated and added yang modules in parser, LU and inet projects.
    Implemented individual handlers and registered.
    Created parser, serialized and registry interfaces.
    Updated and add unit tests.
    
    Change-Id: If39bfc6c06bf65687d20caa800e9a891ce11a12e
    Signed-off-by: Iveta Halanova <ihalanov@cisco.com>

--- bgp/labeled-unicast/src/main/java/org/opendaylight/protocol/bgp/labeled/unicast/BGPActivator.java
+++ bgp/labeled-unicast/src/main/java/org/opendaylight/protocol/bgp/labeled/unicast/BGPActivator.java
@@ -15,6 +15,8 @@ import org.opendaylight.protocol.bgp.parser.spi.AbstractBGPExtensionProviderActi
 import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderContext;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.labeled.unicast.rev150525.LabeledUnicastSubsequentAddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.labeled.unicast.rev150525.labeled.unicast.routes.LabeledUnicastRoutes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.labeled.unicast.rev150525.update.attributes.bgp.prefix.sid.bgp.prefix.sid.tlvs.bgp.prefix.sid.tlv.LuLabelIndexTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.labeled.unicast.rev150525.update.attributes.bgp.prefix.sid.bgp.prefix.sid.tlvs.bgp.prefix.sid.tlv.LuOriginatorSrgbTlv;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv4AddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv6AddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.next.hop.c.next.hop.Ipv4NextHopCase;
@@ -26,19 +28,26 @@ public final class BGPActivator extends AbstractBGPExtensionProviderActivator {
 
     @Override
     protected List<AutoCloseable> startImpl(final BGPExtensionProviderContext context) {
-        final List<AutoCloseable> regs = new ArrayList<>(4);
+        final List<AutoCloseable> regs = new ArrayList<>(8);
         final LUNlriParser luNlriParser = new LUNlriParser();
 
         regs.add(context.registerSubsequentAddressFamily(LabeledUnicastSubsequentAddressFamily.class, LABELED_UNICAST_SAFI));
 
-        Ipv4NextHopParserSerializer ipv4NextHopParser = new Ipv4NextHopParserSerializer();
-        Ipv6NextHopParserSerializer ipv6NextHopParser = new Ipv6NextHopParserSerializer();
+        final Ipv4NextHopParserSerializer ipv4NextHopParser = new Ipv4NextHopParserSerializer();
+        final Ipv6NextHopParserSerializer ipv6NextHopParser = new Ipv6NextHopParserSerializer();
         regs.add(context.registerNlriParser(Ipv4AddressFamily.class, LabeledUnicastSubsequentAddressFamily.class,
             luNlriParser, ipv4NextHopParser, Ipv4NextHopCase.class));
         regs.add(context.registerNlriParser(Ipv6AddressFamily.class, LabeledUnicastSubsequentAddressFamily.class,
             luNlriParser, ipv6NextHopParser, Ipv6NextHopCase.class));
 
         regs.add(context.registerNlriSerializer(LabeledUnicastRoutes.class, luNlriParser));
+
+        final LabelIndexTlvParser labelHandler = new LabelIndexTlvParser();
+        final OriginatorSrgbTlvParser originatorHandler = new OriginatorSrgbTlvParser();
+        regs.add(context.registerBgpPrefixSidTlvParser(labelHandler.getType(), labelHandler));
+        regs.add(context.registerBgpPrefixSidTlvParser(labelHandler.getType(), originatorHandler));
+        regs.add(context.registerBgpPrefixSidTlvSerializer(LuLabelIndexTlv.class, labelHandler));
+        regs.add(context.registerBgpPrefixSidTlvSerializer(LuOriginatorSrgbTlv.class, originatorHandler));
 
         return regs;
     }
