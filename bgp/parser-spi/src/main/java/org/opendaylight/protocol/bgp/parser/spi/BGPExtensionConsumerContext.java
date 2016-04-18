9e9134c8ca2abe7deb9502ce5508181bb7bd74df
Iveta Halanova <ihalanov@cisco.com>
2016-04-18 10:26:20 +0200

    BUG-5441: BGP SR prefix SID extension
    ref.: https://tools.ietf.org/html/draft-ietf-idr-bgp-prefix-sid-02
    
    Updated and added yang modules in parser, LU and inet projects.
    Implemented individual handlers and registered.
    Created parser, serialized and registry interfaces.
    Updated and add unit tests.
    
    Change-Id: If39bfc6c06bf65687d20caa800e9a891ce11a12e
    Signed-off-by: Iveta Halanova <ihalanov@cisco.com>

--- bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/BGPExtensionConsumerContext.java
+++ bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/BGPExtensionConsumerContext.java
@@ -29,4 +29,6 @@ public interface BGPExtensionConsumerContext {
     SubsequentAddressFamilyRegistry getSubsequentAddressFamilyRegistry();
 
     ExtendedCommunityRegistry getExtendedCommunityReistry();
+
+    BgpPrefixSidTlvRegistry getBgpPrefixSidTlvRegistry();
 }
