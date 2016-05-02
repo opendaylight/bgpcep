dbaa4b4138a454842c3d296cbf5b423c2cb52f16
Kevin Wang <kevixw@gmail.com>
2016-05-02 23:48:25 +0000

    Support for dissemination of L3VPN flow spec
    Change-Id: Iecc0908ac55cf64414a472fe1dcc5e78034090c0
    Signed-off-by: Kevin Wang <kevixw@gmail.com>

--- bgp/flowspec/src/main/java/org/opendaylight/protocol/bgp/flowspec/handlers/ipv6/FSIpv6FragmentHandler.java
+++ bgp/flowspec/src/main/java/org/opendaylight/protocol/bgp/flowspec/handlers/ipv6/FSIpv6FragmentHandler.java
@@ -5,23 +5,24 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.protocol.bgp.flowspec.handlers;
+package org.opendaylight.protocol.bgp.flowspec.handlers.ipv6;
 
+import org.opendaylight.protocol.bgp.flowspec.handlers.AbstractFSFragmentHandler;
 import org.opendaylight.protocol.util.BitArray;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.flowspec.rev150807.Fragment;
 
-public class FSIpv4FragmentHandler extends AbstractFSFragmentHandler {
+public class FSIpv6FragmentHandler extends AbstractFSFragmentHandler {
 
     @Override
     protected final Fragment parseFragment(final byte fragment) {
         final BitArray bs = BitArray.valueOf(fragment);
-        return new Fragment(bs.get(DONT_FRAGMENT), bs.get(FIRST_FRAGMENT), bs.get(IS_A_FRAGMENT), bs.get(LAST_FRAGMENT));
+        return new Fragment(Boolean.FALSE, bs.get(FIRST_FRAGMENT), bs.get(IS_A_FRAGMENT), bs.get(LAST_FRAGMENT));
     }
 
     @Override
     protected final byte serializeFragment(final Fragment fragment) {
         final BitArray bs = new BitArray(Byte.SIZE);
-        bs.set(DONT_FRAGMENT, fragment.isDoNot());
+        bs.set(DONT_FRAGMENT, Boolean.FALSE);
         bs.set(FIRST_FRAGMENT, fragment.isFirst());
         bs.set(IS_A_FRAGMENT, fragment.isIsA());
         bs.set(LAST_FRAGMENT, fragment.isLast());
