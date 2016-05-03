ab3bb82636a70cde71ce4492f40876dc4718a9c6
Claudio D. Gasparini <cgaspari@cisco.com>
2016-05-03 17:44:44 +0200

    BUG-5790: BGP Test tool
    - Simulate multiple neighbours
    - Active/Passive session initiation
    - Log level
    - Send X number of prefixes
    - Count routes received
    - Fix missing check null
    - MpPath Ipv4 support
    
    Change-Id: I5150f8f9348a2e26fe3b07424e1ec9e72672cdf2
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/testtool/src/main/java/org/opendaylight/protocol/bgp/testtool/Arguments.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.testtool;
+
+import static org.opendaylight.protocol.util.InetSocketAddressUtil.getInetSocketAddress;
+
+import ch.qos.logback.classic.Level;
+import com.google.common.net.InetAddresses;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.Argument;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.ArgumentType;
+import net.sourceforge.argparse4j.inf.Namespace;
+import org.opendaylight.protocol.util.ArgumentsInput;
+import org.opendaylight.protocol.util.InetSocketAddressUtil;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.AsNumber;
+
+final class Arguments implements ArgumentsInput {
+    private static final String PROGRAM_NAME = "BGP testing tool.";
+    private static final String ARG_PREFIX = "--";
+    private static final int DEFAULT_LOCAL_PORT = 0;
+    private static final int DEFAULT_REMOTE_PORT = 1790;
+    private static final InetAddress LOCALHOST = InetAddresses.forString("127.0.0.1");
+    private static final InetSocketAddress REMOTE_ADDRESS = new InetSocketAddress(LOCALHOST, DEFAULT_REMOTE_PORT);
+    private static final InetSocketAddress LOCAL_ADDRESS = new InetSocketAddress(LOCALHOST, DEFAULT_LOCAL_PORT);
+    private static final int INITIAL_HOLD_TIME = 90;
+    private static final String LOG_LEVEL = "log_level";
+    private static final String REMOTE_ADDRESS_PARAMETER = "remoteAddress";
+    private static final String REMOTE_ADDRESS_PARAMETER_HELP = "IP address of remote BGP peer, which the tool can accept or initiate connect to that " +
+        "address (based on the mode)";
+    private static final String LOCAL_ADDRESS_PARAMETER = "localAddress";
+    private static final String LOCAL_ADDRESS_PARAMETER_HELP = "IP address of BGP speakers which the tools simulates";
+    private static final String EXTENDED_COMMUNITIES_PARAMETER = "extended_communities";
+    private static final String EXTENDED_COMMUNITIES_PARAMETER_HELP = "Extended communities to be send. Format: x,x,x where x is each extended " +
+        "community from bgp-types.yang as as-4-generic-spec-extended-community, link-bandwidth-extended-community, ...";
+    private static final String ACTIVE_CONNECTION_PARAMETER = "active";
+    private static final String ACTIVE_CONNECTION_HELP = "Active initialization of the connection, by default false";
+    private static final String HOLD_TIMER_PARAMETER = "holdtimer";
+    private static final String INITIAL_HOLD_TIME_HELP = "In seconds, value of the desired holdtimer. According to RFC4271, recommended value for " +
+        "deadtimer is 90 seconds(set by default)";
+    private static final String PREFIXES_PARAMETER = "prefixes";
+    private static final String PREFIXES_PARAMETER_HELP = "Number of prefixes to be sent";
+    private static final String MULTIPATH_PARAMETER = "multiPathSupport";
+    private static final String MULTIPATH_PARAMETER_HELP = "Active Multipart support";
+    private static final String AS_PARAMETER = "as";
+    private static final String AS_PARAMETER_HELP = "Value of AS in the initial open message";
+    private static final String SPEAKERS_COUNT = "speakersCount";
+    private static final String SPEAKERS_COUNT_HELP = "Number of simulated BGP speakers, when creating each speaker, use incremented " +
+        "local-address for binding";
+    private static final ArgumentParser ARGUMENT_PARSER = initializeArgumentParser();
+    private final Namespace parseArgs;
+
+    private Arguments(final Namespace parseArgs) {
+        this.parseArgs = parseArgs;
+    }
+
+    private interface ArgumentTypeTool<T> extends ArgumentType<T> {
+        default T convert(ArgumentParser var1, Argument var2, String input) throws ArgumentParserException {
+            return convert(input);
+        }
+
+        T convert(String input) throws ArgumentParserException;
+    }
+
+    static Arguments parseArguments(final String[] args) {
+        try {
+            final Namespace namespace = ARGUMENT_PARSER.parseArgs(args);
+            return new Arguments(namespace);
+        } catch (final ArgumentParserException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static ArgumentParser initializeArgumentParser() {
+        final ArgumentParser parser = ArgumentParsers.newArgumentParser(PROGRAM_NAME);
+
+        parser.addArgument("-i", toArgName(ACTIVE_CONNECTION_PARAMETER)).type(Boolean.class).setDefault(false).help(ACTIVE_CONNECTION_HELP);
+        parser.addArgument("-ho", toArgName(HOLD_TIMER_PARAMETER)).type(Integer.class).setDefault(INITIAL_HOLD_TIME).help(INITIAL_HOLD_TIME_HELP);
+        parser.addArgument("-pr", toArgName(PREFIXES_PARAMETER)).type(Integer.class).setDefault(0).help(PREFIXES_PARAMETER_HELP);
+        parser.addArgument("-sc", toArgName(SPEAKERS_COUNT)).type(Integer.class).setDefault(0).help(SPEAKERS_COUNT_HELP);
+        parser.addArgument("-mp", toArgName(MULTIPATH_PARAMETER)).type(Boolean.class).setDefault(false).help(MULTIPATH_PARAMETER_HELP);
+        parser.addArgument("-" + AS_PARAMETER, toArgName(AS_PARAMETER)).type((ArgumentTypeTool<AsNumber>) as -> new AsNumber(Long.valueOf(as)))
+            .setDefault(new AsNumber(64496L)).help(AS_PARAMETER_HELP);
+        parser.addArgument("-ec", toArgName(EXTENDED_COMMUNITIES_PARAMETER)).type((ArgumentTypeTool<List<String>>) extComInput ->
+            Arrays.asList(extComInput.split(","))).setDefault(Collections.emptyList()).help(EXTENDED_COMMUNITIES_PARAMETER_HELP);
+        parser.addArgument("-ll", toArgName(LOG_LEVEL)).type((ArgumentTypeTool<Level>) Level::toLevel).setDefault(Level.INFO).help("log levels");
+        parser.addArgument("-ra", toArgName(REMOTE_ADDRESS_PARAMETER)).type((ArgumentTypeTool<List<InetSocketAddress>>) input ->
+            InetSocketAddressUtil.parseAddresses(input, DEFAULT_REMOTE_PORT)).setDefault(Collections.singletonList(REMOTE_ADDRESS))
+            .help(REMOTE_ADDRESS_PARAMETER_HELP);
+        parser.addArgument("-la", toArgName(LOCAL_ADDRESS_PARAMETER)).type((ArgumentTypeTool<InetSocketAddress>) input ->
+            getInetSocketAddress(input, DEFAULT_LOCAL_PORT)).setDefault(LOCAL_ADDRESS).help(LOCAL_ADDRESS_PARAMETER_HELP);
+        return parser;
+    }
+
+
+    private static String toArgName(final String dst) {
+        return ARG_PREFIX + dst;
+    }
+
+    @Override
+    public Level getLogLevel() {
+        return this.parseArgs.get(LOG_LEVEL);
+    }
+
+    AsNumber getAs() {
+        return this.parseArgs.get("as");
+    }
+
+    InetSocketAddress getLocalAddresses() {
+        return this.parseArgs.get(LOCAL_ADDRESS_PARAMETER);
+    }
+
+    List<InetSocketAddress> getRemoteAddresses() {
+        return this.parseArgs.get(REMOTE_ADDRESS_PARAMETER);
+    }
+
+    int getNumberOfPrefixes() {
+        return this.parseArgs.get(PREFIXES_PARAMETER);
+    }
+
+    List<String> getExtendedCommunities() {
+        return this.parseArgs.get(EXTENDED_COMMUNITIES_PARAMETER);
+    }
+
+    int getHoldTimer() {
+        return this.parseArgs.get(HOLD_TIMER_PARAMETER);
+    }
+
+    boolean getInitiateConnection() {
+        return this.parseArgs.get(ACTIVE_CONNECTION_PARAMETER);
+    }
+
+    boolean getMultiPathSupport() {
+        return this.parseArgs.get(MULTIPATH_PARAMETER);
+    }
+
+    int getSpeakerCount() {
+        return this.parseArgs.get(SPEAKERS_COUNT);
+    }
+}
--- bgp/testtool/src/main/java/org/opendaylight/protocol/bgp/testtool/Main.java
+++ bgp/testtool/src/main/java/org/opendaylight/protocol/bgp/testtool/Main.java
@@ -7,35 +7,10 @@
  */
 package org.opendaylight.protocol.bgp.testtool;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.Lists;
-import io.netty.channel.nio.NioEventLoopGroup;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
-import java.util.Collections;
-import java.util.List;
-import org.opendaylight.protocol.bgp.parser.spi.pojo.ServiceLoaderBGPExtensionProviderContext;
-import org.opendaylight.protocol.bgp.rib.impl.BGPDispatcherImpl;
-import org.opendaylight.protocol.bgp.rib.impl.StrictBGPPeerRegistry;
-import org.opendaylight.protocol.bgp.rib.impl.spi.BGPSessionPreferences;
-import org.opendaylight.protocol.bgp.rib.spi.BGPSessionListener;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.AsNumber;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.LinkstateAddressFamily;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.LinkstateSubsequentAddressFamily;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.open.message.BgpParameters;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.open.message.BgpParametersBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.open.message.bgp.parameters.OptionalCapabilities;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.open.message.bgp.parameters.OptionalCapabilitiesBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev130919.open.message.bgp.parameters.optional.capabilities.CParametersBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.CParameters1;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.CParameters1Builder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.mp.capabilities.MultiprotocolCapabilityBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.AddressFamily;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv4AddressFamily;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.SubsequentAddressFamily;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.UnicastSubsequentAddressFamily;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.opendaylight.protocol.util.LoggerUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,92 +18,23 @@ import org.slf4j.LoggerFactory;
  * Starter class for testing.
  */
 public final class Main {
-
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-    private static final String USAGE = "DESCRIPTION:\n"
-        + "\tCreates a server with given parameters. As long as it runs, it accepts connections " + "from PCCs.\n" + "USAGE:\n"
-        + "\t-a, --address\n" + "\t\tthe ip address to which is this server bound.\n"
-        + "\t\tFormat: x.x.x.x:y where y is port number.\n\n"
-        + "\t\tThis IP address will appear in BGP Open message as BGP Identifier of the server.\n" +
-
-            "\t-as\n" + "\t\t value of AS in the initial open message\n\n" +
-
-            "\t-h, --holdtimer\n" + "\t\tin seconds, value of the desired holdtimer\n"
-            + "\t\tAccording to RFC4271, recommended value for deadtimer is 90 seconds.\n"
-            + "\t\tIf not set, this will be the default value.\n\n" +
-
-            "\t--help\n" + "\t\tdisplay this help and exits\n\n" +
-
-            "With no parameters, this help is printed.";
-
-    private final BGPDispatcherImpl dispatcher;
-
-    private static final int INITIAL_HOLD_TIME = 90;
-
-    private static final int RETRY_TIMER = 10;
-
     private Main() {
-        this.dispatcher = new BGPDispatcherImpl(ServiceLoaderBGPExtensionProviderContext.getSingletonInstance().getMessageRegistry(), new NioEventLoopGroup(), new NioEventLoopGroup());
+        throw new UnsupportedOperationException();
     }
 
-    public static void main(final String[] args) throws UnknownHostException {
-        if (args.length == 0 || (args.length == 1 && args[0].equalsIgnoreCase("--help"))) {
-            LOG.info(Main.USAGE);
-            return;
+    public static void main(final String[] args) throws IOException {
+        final Arguments arguments = Arguments.parseArguments(args);
+        LoggerUtil.initiateLogger(arguments);
+
+        final BGPTestTool bgpTestTool = new BGPTestTool();
+        final InputStreamReader isr = new InputStreamReader(System.in);
+        final BufferedReader br = new BufferedReader(isr);
+        bgpTestTool.start(arguments);
+        for (;;) {
+            LOG.info("Insert local address:");
+            bgpTestTool.printCount(br.readLine());
         }
-
-        InetSocketAddress address = null;
-        int holdTimerValue = INITIAL_HOLD_TIME;
-        AsNumber as = null;
-
-        int i = 0;
-        while (i < args.length) {
-            if (args[i].equalsIgnoreCase("-a") || args[i].equalsIgnoreCase("--address")) {
-                final String[] ip = args[i + 1].split(":");
-                address = new InetSocketAddress(InetAddress.getByName(ip[0]), Integer.parseInt(ip[1]));
-                i++;
-            } else if (args[i].equalsIgnoreCase("-h") || args[i].equalsIgnoreCase("--holdtimer")) {
-                holdTimerValue = Integer.valueOf(args[i + 1]);
-                i++;
-            } else if (args[i].equalsIgnoreCase("-as")) {
-                as = new AsNumber(Long.valueOf(args[i + 1]));
-                i++;
-            } else {
-                LOG.error("WARNING: Unrecognized argument: {}", args[i]);
-            }
-            i++;
-        }
-
-        final Main m = new Main();
-
-        final BGPSessionListener sessionListener = new TestingListener();
-
-        final BgpParameters bgpParameters = createBgpParameters(Lists.newArrayList(
-                createMPCapability(Ipv4AddressFamily.class, UnicastSubsequentAddressFamily.class),
-                createMPCapability(LinkstateAddressFamily.class, LinkstateSubsequentAddressFamily.class)));
-
-        final BGPSessionPreferences proposal = new BGPSessionPreferences(as, holdTimerValue, new Ipv4Address("25.25.25.2"), as,
-                Collections.singletonList(bgpParameters));
-
-        LOG.debug("{} {} {}", address, sessionListener, proposal);
-
-        final InetSocketAddress addr = address;
-        final StrictBGPPeerRegistry strictBGPPeerRegistry = new StrictBGPPeerRegistry();
-        strictBGPPeerRegistry.addPeer(StrictBGPPeerRegistry.getIpAddress(address), sessionListener, proposal);
-
-        m.dispatcher.createClient(addr, strictBGPPeerRegistry, RETRY_TIMER);
-    }
-
-    @VisibleForTesting
-    protected static BgpParameters createBgpParameters(final List<OptionalCapabilities> optionalCapabilities) {
-        return new BgpParametersBuilder().setOptionalCapabilities(optionalCapabilities).build();
-    }
-
-    @VisibleForTesting
-    protected static OptionalCapabilities createMPCapability(final Class<? extends AddressFamily> afi, final Class<? extends SubsequentAddressFamily> safi) {
-        return new OptionalCapabilitiesBuilder().setCParameters(new CParametersBuilder().addAugmentation(CParameters1.class,
-                new CParameters1Builder().setMultiprotocolCapability(
-                        new MultiprotocolCapabilityBuilder().setAfi(afi).setSafi(safi).build()).build()).build()).build();
     }
 }
