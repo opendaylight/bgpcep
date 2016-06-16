ddd100aefda1e38ee5d08a71fabe751088b6f737
Tom Pantelis <tpanteli@brocade.com>
2016-06-16 11:47:18 -0400

    Convert bmp extenstions to blueprint
    Added blueprint XML file, bmp-spi.xml, to the spi bundle that Utilizes
    the new blueprint extension, "specific-reference-list" (added by
    https://git.opendaylight.org/gerrit/#/c/40267/), to obtain the
    specific list of BmpExtensionProviderActivator OSGi service instances.
    The specific expected services are announced via
    META-INF/services/org.opendaylight.protocol.bmp.spi.registry.BmpExtensionProviderActivator
    resources in the provider bundles which the "specific-reference-list"
    extension scan so it knows which services to look for. Once all expected
    services are obtained, the BmpExtensionProviderActivator instance List
    bean is created and injected into the SimpleBmpExtensionProviderContextActivator
    bean instance which calls start on each.
    
    The only BmpExtensionProviderActivator instance wired in 32-bmp.xml is
    the BmpActivator in bmp-impl. This instance is now created via the
    blueprint XML, bgp-bmp.xml, and the config yang and associated Module
    class were removed.
    
    The SimpleBmpExtensionProviderContextModule was deprecated but still
    remains for now to provide the BmpExtensionProviderContext instance
    created via blueprint to the config system so it can be injected into
    other users. Once the other user(s) are converted to blueprint then the
    config yang, Module classes can be removed.
    
    Change-Id: I96205d078d9a7ec5fa58eae7c0f66a88227f7bc5
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

---
+++ bgp/bmp-spi/src/main/java/org/opendaylight/protocol/bmp/spi/registry/SimpleBmpExtensionProviderContextActivator.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bmp.spi.registry;
+
+import com.google.common.base.Preconditions;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Starts and stops BmpExtensionProviderActivator instances for a BmpExtensionProviderContext.
+ *
+ * @author Thomas Pantelis
+ */
+public class SimpleBmpExtensionProviderContextActivator implements AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleBmpExtensionProviderContextActivator.class);
+
+    private final BmpExtensionProviderContext providerContext;
+    private final List<BmpExtensionProviderActivator> extensionActivators;
+
+    public SimpleBmpExtensionProviderContextActivator(final BmpExtensionProviderContext providerContext,
+            final List<BmpExtensionProviderActivator> extensionActivators) {
+        this.providerContext = Preconditions.checkNotNull(providerContext);
+        this.extensionActivators = Preconditions.checkNotNull(extensionActivators);
+    }
+
+    public void start() {
+        LOG.debug("Starting {} BmpExtensionProviderActivator instances", extensionActivators.size());
+
+        for(final BmpExtensionProviderActivator e : extensionActivators) {
+            e.start(providerContext);
+        }
+    }
+
+    @Override
+    public void close() {
+        LOG.debug("Stopping {} BmpExtensionProviderActivator instances", extensionActivators.size());
+
+        for(final BmpExtensionProviderActivator e : extensionActivators) {
+            e.stop();
+        }
+    }
+
+}
