f2ca148da7dae9c49c1dd51eccb1b7470b324496
Claudio <cgaspari@cisco.com>
2016-07-01 09:18:56 +0200

    BUG-6114: Improve log for creation/close of Modules instances
    - Improve log for creation/close of Modules instances
    - Close PDM strategy under PSM Module
    
    Change-Id: Ib3be5d0a3507f4eb2cb0884d912760be959f0495
    Signed-off-by: Claudio <cgaspari@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/controller/config/yang/bgp/rib/impl/BGPPeerModule.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/controller/config/yang/bgp/rib/impl/BGPPeerModule.java
@@ -126,6 +126,8 @@ public final class BGPPeerModule extends org.opendaylight.controller.config.yang
 
     @Override
     public java.lang.AutoCloseable createInstance() {
+        LOG.trace("Create BGPPeer Instance");
+
         final RIB r = getRibDependency();
 
         final List<BgpParameters> tlvs = getTlvs(r);
@@ -149,6 +151,7 @@ public final class BGPPeerModule extends org.opendaylight.controller.config.yang
         final CloseableNoEx peerCloseable = new CloseableNoEx() {
             @Override
             public void close() {
+                LOG.trace("Close BGPPeer Instance");
                 bgpClientPeer.close();
                 getPeerRegistryBackwards().removePeer(host);
                 moduleTracker.onInstanceClose();
@@ -269,7 +272,7 @@ public final class BGPPeerModule extends org.opendaylight.controller.config.yang
         private final BGPOpenconfigMapper<BGPPeerInstanceConfiguration> neighborProvider;
         private final BGPPeerInstanceConfiguration bgpPeerInstanceConfiguration;
 
-        public BGPPeerModuleTracker(final Optional<BGPOpenConfigProvider> openconfigProvider) {
+        BGPPeerModuleTracker(final Optional<BGPOpenConfigProvider> openconfigProvider) {
             if (openconfigProvider.isPresent()) {
                 this.neighborProvider = openconfigProvider.get().getOpenConfigMapper(BGPPeerInstanceConfiguration.class);
             } else {
@@ -285,6 +288,7 @@ public final class BGPPeerModule extends org.opendaylight.controller.config.yang
         @Override
         public void onInstanceCreate() {
             if (this.neighborProvider != null) {
+                LOG.trace("Creating Peer Module Tracker Instance");
                 this.neighborProvider.writeConfiguration(this.bgpPeerInstanceConfiguration);
             }
         }
@@ -292,6 +296,7 @@ public final class BGPPeerModule extends org.opendaylight.controller.config.yang
         @Override
         public void onInstanceClose() {
             if (this.neighborProvider != null) {
+                LOG.trace("Closing Application Peer Module Tracker Instance");
                 this.neighborProvider.removeConfiguration(this.bgpPeerInstanceConfiguration);
             }
         }
