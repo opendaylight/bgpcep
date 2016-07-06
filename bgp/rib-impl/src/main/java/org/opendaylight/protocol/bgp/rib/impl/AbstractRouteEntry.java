08adbaae0bc60b614e11093f2d3270b11114eb27
Ajay <ajayl.bro@gmail.com>
2016-07-06 18:58:37 +0000

    Bug-6083: use Beryllium-SR2, for one route,bgp maybe advertising not best one to peer - if best path is getting changed, ensure that corresponding route is removed from AdjRibOut of other peers, so that it gets withdrawn
    Change-Id: If0c16422f0ff44e58c4c1bf293dadc8d5bf3145e
    Signed-off-by: Ajay <ajayl.bro@gmail.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AbstractRouteEntry.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AbstractRouteEntry.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.protocol.bgp.rib.impl;
 
+import com.google.common.base.Optional;
 import com.google.common.primitives.UnsignedInteger;
 import javax.annotation.concurrent.NotThreadSafe;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
@@ -35,6 +36,7 @@ abstract class AbstractRouteEntry {
     private OffsetMap offsets = OffsetMap.EMPTY;
     private ContainerNode[] values = EMPTY_ATTRIBUTES;
     private BestPath bestPath;
+    private Optional<BestPath> removedBestPath = Optional.absent();
 
     private int addRoute(final UnsignedInteger routerId, final ContainerNode attributes) {
         int offset = this.offsets.offsetOf(routerId);
@@ -91,10 +93,13 @@ abstract class AbstractRouteEntry {
 
         // Get the newly-selected best path.
         final BestPath newBestPath = selector.result();
-        final boolean ret = !newBestPath.equals(this.bestPath);
-        LOG.trace("Previous best {}, current best {}, result {}", this.bestPath, newBestPath, ret);
-        this.bestPath = newBestPath;
-        return ret;
+        final boolean modified = !newBestPath.equals(this.bestPath);
+        if (modified) {
+            this.removedBestPath = Optional.fromNullable(this.bestPath);
+            LOG.trace("Previous best {}, current best {}", this.bestPath, newBestPath);
+            this.bestPath = newBestPath;
+        }
+        return modified;
     }
 
     final ContainerNode attributes() {
@@ -107,6 +112,14 @@ abstract class AbstractRouteEntry {
 
     protected final UnsignedInteger getBestRouterId() {
         return this.bestPath.getRouterId();
+    }
+
+    protected void clearRemovedBestPath() {
+        this.removedBestPath = Optional.absent();
+    }
+
+    protected final UnsignedInteger getRemovedBestRouterId() {
+        return this.removedBestPath.isPresent() ? this.removedBestPath.get().getRouterId() : null;
     }
 
     abstract boolean removeRoute(final UnsignedInteger routerId);
