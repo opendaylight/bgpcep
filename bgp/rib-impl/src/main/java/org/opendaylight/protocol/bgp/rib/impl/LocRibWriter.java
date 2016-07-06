4d0466cbdaa5a1bf5d00d0885e7282f3f0e31ca6
Ajay <ajayl.bro@gmail.com>
2016-07-06 18:58:37 +0000

    Bug-6083: use Beryllium-SR2, for one route,bgp maybe advertising not best one to peer - if best path is getting changed, ensure that corresponding route is removed from AdjRibOut of other peers, so that it gets withdrawn
    Change-Id: If0c16422f0ff44e58c4c1bf293dadc8d5bf3145e
    Signed-off-by: Ajay <ajayl.bro@gmail.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/LocRibWriter.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/LocRibWriter.java
@@ -272,8 +272,15 @@ final class LocRibWriter implements AutoCloseable, DOMDataTreeChangeListener {
             if (entry != null) {
                 if (!entry.selectBest(this.ourAs)) {
                     // Best path has not changed, no need to do anything else. Proceed to next route.
-                    LOG.trace("Continuing");
+                    LOG.trace("Best path has not changed, continuing");
                     continue;
+                }
+                final UnsignedInteger removedBestRouterId = entry.getRemovedBestRouterId();
+                if (removedBestRouterId != null) {
+                    routePeerId = RouterIds.createPeerId(removedBestRouterId);
+                    fillLocRib(tx, entry, null, routeId);
+                    fillAdjRibsOut(tx, null, null, routeId, routePeerId);
+                    entry.clearRemovedBestPath();
                 }
                 routePeerId = RouterIds.createPeerId(entry.getBestRouterId());
                 value = entry.createValue(routeId);
