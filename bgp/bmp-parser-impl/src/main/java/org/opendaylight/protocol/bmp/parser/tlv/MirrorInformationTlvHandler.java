26590e0c5f27528487fe4a5d494488b83458b96f
Tom Pantelis <tpanteli@brocade.com>
2016-07-06 21:20:27 -0400

    Add blueprint XML wiring for the BmpDispatcher
    Added blueprint wiring for the BGPDispatcher. The BmpDispatcherImplModule
    was deprecated but still remains for now to provide the BmpDispatcher
    instance created via blueprint to the config system so it can be
    injected into other users. Once the other user(s) are converted to
    blueprint then the config yang and Module classes can be removed.
    
    There was one complication. The BmpActivator protcol extension is in the
    bmp-impl bundle and the service is consumed by the bmp-spi bundle.
    The BmpExtensionConsumerContext is provided by the bmp-spi bundle and is
    consumed by the bmp-impl bundle. This causes a circular service
    dependency which causes a deadlock with blueprint. To avoid this I moved
    the BmpActivator and related message and tlv classes (which are only used by
    BmpActivator) to a new bundle, bmp-parser-ext. This bundle now provides
    the BmpActivator extension provider to the bmp-spi bundle thereby
    breaking the circular dependency.
    
    Change-Id: Idbee9a7c2632d1e26c76314213389f83ff8ae5c1
    Signed-off-by: Tom Pantelis <tpanteli@brocade.com>

---
+++ bgp/bmp-parser-impl/src/main/java/org/opendaylight/protocol/bmp/parser/tlv/MirrorInformationTlvHandler.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bmp.parser.tlv;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bmp.spi.parser.BmpDeserializationException;
+import org.opendaylight.protocol.bmp.spi.parser.BmpTlvParser;
+import org.opendaylight.protocol.bmp.spi.parser.BmpTlvSerializer;
+import org.opendaylight.protocol.bmp.spi.parser.TlvUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.MirrorInformationCode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.Tlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.mirror.information.tlv.MirrorInformationTlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev150512.mirror.information.tlv.MirrorInformationTlvBuilder;
+
+public class MirrorInformationTlvHandler implements BmpTlvParser, BmpTlvSerializer {
+
+    public static final int TYPE = 1;
+
+    @Override
+    public void serializeTlv(final Tlv tlv, final ByteBuf output) {
+        Preconditions.checkArgument(tlv instanceof MirrorInformationTlv, "MirrorInformationTlv is mandatory.");
+        TlvUtil.formatTlvShort16(TYPE, ((MirrorInformationTlv) tlv).getCode().getIntValue(), output);
+    }
+
+    @Override
+    public Tlv parseTlv(final ByteBuf buffer) throws BmpDeserializationException {
+        if (buffer == null) {
+            return null;
+        }
+        return new MirrorInformationTlvBuilder().setCode(MirrorInformationCode.forValue(buffer.readUnsignedShort())).build();
+    }
+}
