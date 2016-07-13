aa9134a63696a1408f47148f87f5f636cb829cc4
Claudio <cgaspari@cisco.com>
2016-07-13 15:03:17 +0200

    BUG-6120: Revert Ignore test change
    Revert ignore test change introded by bug-6120.
    Improve some test, removing thread.sleep were possible.
    
    Change-Id: I084a015db8d4b6be2b210f8c5d6298aaddc9c69a
    Signed-off-by: Claudio <cgaspari@cisco.com>

--- pcep/pcc-mock/src/test/java/org/opendaylight/protocol/pcep/pcc/mock/PCCDispatcherImplTest.java
+++ pcep/pcc-mock/src/test/java/org/opendaylight/protocol/pcep/pcc/mock/PCCDispatcherImplTest.java
@@ -8,7 +8,11 @@
 
 package org.opendaylight.protocol.pcep.pcc.mock;
 
+import static org.opendaylight.protocol.pcep.pcc.mock.PCCMockCommon.checkSessionListenerNotNull;
+import static org.opendaylight.protocol.pcep.pcc.mock.WaitForFutureSucces.waitFutureSuccess;
+
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.util.concurrent.Future;
@@ -20,7 +24,6 @@ import java.util.concurrent.ExecutionException;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.opendaylight.protocol.pcep.PCEPCapability;
 import org.opendaylight.protocol.pcep.PCEPDispatcher;
@@ -32,20 +35,15 @@ import org.opendaylight.protocol.pcep.impl.PCEPDispatcherImpl;
 import org.opendaylight.protocol.pcep.pcc.mock.protocol.PCCDispatcherImpl;
 import org.opendaylight.protocol.pcep.spi.pojo.ServiceLoaderPCEPExtensionProviderContext;
 
-// This test is set to ignore for now as it's failing frequently on jenkins due to 
-// infrastructure issues.
-@Ignore
 public class PCCDispatcherImplTest {
 
     private static final List<PCEPCapability> CAPS = new ArrayList<>();
     private static final PCEPSessionProposalFactory PROPOSAL = new BasePCEPSessionProposalFactory(30, 120, CAPS);
-
-    private PCCDispatcherImpl dispatcher;
     private final DefaultPCEPSessionNegotiatorFactory nf = new DefaultPCEPSessionNegotiatorFactory(PROPOSAL, 0);
+    private PCCDispatcherImpl dispatcher;
     private PCEPDispatcher pcepDispatcher;
     private InetSocketAddress serverAddress;
     private InetSocketAddress clientAddress;
-    private final Random random = new Random();
     private EventLoopGroup workerGroup;
     private EventLoopGroup bossGroup;
 
@@ -55,9 +53,9 @@ public class PCCDispatcherImplTest {
         this.bossGroup = new NioEventLoopGroup();
         this.dispatcher = new PCCDispatcherImpl(ServiceLoaderPCEPExtensionProviderContext.getSingletonInstance().getMessageHandlerRegistry());
         this.pcepDispatcher = new PCEPDispatcherImpl(ServiceLoaderPCEPExtensionProviderContext.getSingletonInstance().getMessageHandlerRegistry(),
-                this.nf, this.bossGroup, this.workerGroup);
-        this.serverAddress = new InetSocketAddress("127.0.5.0", getRandomPort());
-        this.clientAddress = new InetSocketAddress("127.0.4.0", getRandomPort());
+            this.nf, this.bossGroup, this.workerGroup);
+        this.serverAddress = new InetSocketAddress("127.0.5.0", 4561);
+        this.clientAddress = new InetSocketAddress("127.0.4.0", 4562);
     }
 
     @After
@@ -70,13 +68,17 @@ public class PCCDispatcherImplTest {
     @Test
     public void testClientReconnect() throws Exception {
         final Future<PCEPSession> futureSession = this.dispatcher.createClient(this.serverAddress, 1, new TestingSessionListenerFactory(),
-                this.nf, null, this.clientAddress);
-
+            this.nf, null, this.clientAddress);
+        waitFutureSuccess(futureSession);
         final TestingSessionListenerFactory slf = new TestingSessionListenerFactory();
-        final Channel channel = this.pcepDispatcher.createServer(this.serverAddress, slf, null).channel();
+        final ChannelFuture future = this.pcepDispatcher.createServer(this.serverAddress, slf, null);
+        waitFutureSuccess(future);
+        final Channel channel = future.channel();
         Assert.assertNotNull(futureSession.get());
+        checkSessionListenerNotNull(slf, "127.0.4.0");
         final TestingSessionListener sl = slf.getSessionListenerByRemoteAddress(this.clientAddress.getAddress());
         Assert.assertNotNull(sl);
+        Assert.assertNotNull(sl.getSession());
         Assert.assertTrue(sl.isUp());
 
         channel.close().get();
@@ -86,20 +88,16 @@ public class PCCDispatcherImplTest {
         this.workerGroup = new NioEventLoopGroup();
         this.bossGroup = new NioEventLoopGroup();
         this.pcepDispatcher = new PCEPDispatcherImpl(ServiceLoaderPCEPExtensionProviderContext.getSingletonInstance().getMessageHandlerRegistry(),
-                this.nf, this.bossGroup, this.workerGroup);
+            this.nf, this.bossGroup, this.workerGroup);
 
         final TestingSessionListenerFactory slf2 = new TestingSessionListenerFactory();
-        this.pcepDispatcher.createServer(this.serverAddress, slf2, null).channel();
-        // sleep for bit more than retry time of 1 sec.
+        final ChannelFuture future2 = this.pcepDispatcher.createServer(this.serverAddress, slf2, null);
+        waitFutureSuccess(future2);
+        final Channel channel2 = future2.channel();
         Thread.sleep(1500);
-
         final TestingSessionListener sl2 = slf2.getSessionListenerByRemoteAddress(this.clientAddress.getAddress());
         Assert.assertNotNull(sl2);
+        Assert.assertNotNull(sl2.getSession());
         Assert.assertTrue(sl2.isUp());
     }
-
-    private int getRandomPort() {
-        return this.random.nextInt(4000) + 1024;
-    }
-
 }
