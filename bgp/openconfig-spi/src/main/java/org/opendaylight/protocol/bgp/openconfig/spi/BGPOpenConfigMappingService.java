bbcd0c6fd9f786d05bda6c589a6cea814e5e364b
Milos Fabian <milfabia@cisco.com>
2016-07-15 10:15:37 +0200

    Bug 5061: Integrate RIBImplModule with blueprint
    Instead of instance creation in RIBImplModule, write
    BGP OpenConfig configuration into the config DS and
    BGP deployer takes care of instance creation.
    Remove configuration on module instance close.
    Add mapping function RIBImplModule -> OpenConfig BGP to
    BGPOpenConfigMappingService.
    
    Change-Id: I34533ca6e4593244948cd109c95f105d4c171bdb
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

--- bgp/openconfig-spi/src/main/java/org/opendaylight/protocol/bgp/openconfig/spi/BGPOpenConfigMappingService.java
+++ bgp/openconfig-spi/src/main/java/org/opendaylight/protocol/bgp/openconfig/spi/BGPOpenConfigMappingService.java
@@ -13,9 +13,15 @@ import java.util.Map;
 import org.opendaylight.protocol.bgp.mode.api.PathSelectionMode;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.AfiSafi;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbors.Neighbor;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.network.instance.rev151018.network.instance.top.network.instances.network.instance.protocols.Protocol;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.AsNumber;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.BgpTableType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.mp.capabilities.add.path.capability.AddressFamilies;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.PeerRole;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.RibId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.TablesKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.BgpId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.ClusterIdentifier;
 
 public interface BGPOpenConfigMappingService {
 
@@ -28,4 +34,7 @@ public interface BGPOpenConfigMappingService {
     boolean isApplicationPeer(Neighbor neighbor);
 
     PeerRole toPeerRole(Neighbor neighbor);
+
+    Protocol fromRib(BgpId bgpId, ClusterIdentifier clusterIdentifier, RibId ribId, AsNumber localAs, List<BgpTableType> localTables,
+            Map<TablesKey, PathSelectionMode> pathSelectionStrategies);
 }
