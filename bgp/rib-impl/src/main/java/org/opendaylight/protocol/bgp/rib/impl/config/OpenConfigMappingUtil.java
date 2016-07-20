b37022b8f561fa46a9c5e37250b61ea9a0d6bfb0
Milos Fabian <milfabia@cisco.com>
2016-07-20 16:20:39 +0200

    Bug 5061: Bgp Peer deployer
    Extends BGP deployer to be capable to
    instantiate BGPPeer instances based on
    current OpenConfig Neighbor configurations.
    
    Change-Id: If27e4551b7fc6db4c3c5dcf07028e5563d61e0dd
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/OpenConfigMappingUtil.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/OpenConfigMappingUtil.java
@@ -8,7 +8,16 @@
 
 package org.opendaylight.protocol.bgp.rib.impl.config;
 
+import static org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IetfInetUtil.INSTANCE;
+
+import org.opendaylight.protocol.bgp.rib.impl.spi.RIB;
+import org.opendaylight.protocol.concepts.KeyMapping;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbors.Neighbor;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbors.NeighborKey;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.top.Bgp;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.top.bgp.Neighbors;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.network.instance.rev151018.network.instance.top.network.instances.network.instance.protocols.Protocol;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.AsNumber;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 final class OpenConfigMappingUtil {
@@ -19,6 +28,39 @@ final class OpenConfigMappingUtil {
 
     public static String getRibInstanceName(final InstanceIdentifier<?> rootIdentifier) {
         return rootIdentifier.firstKeyOf(Protocol.class).getName();
+    }
+
+    public static int getHoldTimer(final Neighbor neighbor) {
+        return neighbor.getTimers().getConfig().getHoldTime().intValue();
+    }
+
+    public static AsNumber getPeerAs(final Neighbor neighbor, final RIB rib) {
+        final AsNumber peerAs = neighbor.getConfig().getPeerAs();
+        if (peerAs != null) {
+            return peerAs;
+        }
+        return rib.getLocalAs();
+    }
+
+    public static boolean isActive(final Neighbor neighbor) {
+        return !neighbor.getTransport().getConfig().isPassiveMode();
+    }
+
+    public static int getRetryTimer(final Neighbor neighbor) {
+        return neighbor.getTimers().getConfig().getConnectRetry().intValue();
+    }
+
+    public static KeyMapping getNeighborKey(final Neighbor neighbor) {
+        final String authPassword = neighbor.getConfig().getAuthPassword();
+        if (authPassword != null) {
+            KeyMapping.getKeyMapping(INSTANCE.inetAddressFor(neighbor.getNeighborAddress()), authPassword);
+        }
+        return null;
+    }
+
+    public static InstanceIdentifier<Neighbor> getNeighborInstanceIdentifier(final InstanceIdentifier<Bgp> rootIdentifier,
+            final NeighborKey neighborKey) {
+        return rootIdentifier.child(Neighbors.class).child(Neighbor.class, neighborKey);
     }
 
 }
