7d51f7728ee90bd516965cf5e2ebcbb3152a2916
Milos Fabian <milfabia@cisco.com>
2016-07-25 17:48:25 +0200

    Bug 5061: Move BGP topology providers configuration to config DS
    Allow to configure BGP topology provider from config DS Topology subtree.
    The BGP topology provider configuration consists of topology-id,
    rib-id (where topology provider is listening) and topology-type (identifies the type
    of the BGP topology provider - IPv4/6 reachability, linkstate).
    Services are wired with Blueprint, while CSS module based configuration
    is still preserved.
    
    Change-Id: I94e27c6cdf7f3f5c55b6972324ddb486d2ac185d
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/topology-provider/src/main/java/org/opendaylight/bgpcep/bgp/topology/provider/spi/BgpTopologyProvider.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.bgpcep.bgp.topology.provider.spi;
+
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+
+
+public interface BgpTopologyProvider {
+
+    boolean topologyTypeFilter(Topology topology);
+
+    void onTopologyBuilderRemoved(Topology topology);
+
+    void onTopologyBuilderCreated(Topology topology);
+
+}
