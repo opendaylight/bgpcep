183480e507b82dbbd76eed8c3d29756e1f5b37b0
Claudio D. Gasparini <cgaspari@cisco.com>
2016-08-04 12:49:01 +0200

    BUG-6237: Topology freezes or slows down due to java.util.concurrent.TimeoutException
    High consupmtion of memory is observed caused by high LoadingCache
    per OffsetMap instance.
    Fix by use
    -singleton empty constructor for LoadCache.
    -use null instead of optional
    
    Change-Id: Ia6edc0fc732ac286af4f3ef83d02add572fb5bf1
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- bgp/path-selection-mode/src/main/java/org/opendaylight/protocol/bgp/mode/impl/add/all/paths/ComplexRouteEntry.java
+++ bgp/path-selection-mode/src/main/java/org/opendaylight/protocol/bgp/mode/impl/add/all/paths/ComplexRouteEntry.java
@@ -10,8 +10,8 @@ package org.opendaylight.protocol.bgp.mode.impl.add.all.paths;
 
 import com.google.common.primitives.UnsignedInteger;
 import org.opendaylight.protocol.bgp.mode.api.BestPath;
-import org.opendaylight.protocol.bgp.mode.impl.OffsetMap;
 import org.opendaylight.protocol.bgp.mode.impl.add.AddPathBestPath;
+import org.opendaylight.protocol.bgp.mode.impl.add.OffsetMap;
 import org.opendaylight.protocol.bgp.mode.impl.add.RouteKey;
 import org.opendaylight.protocol.bgp.mode.spi.RouteEntryUtil;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
@@ -25,7 +25,7 @@ final class ComplexRouteEntry extends AbstractAllPathsRouteEntry {
     @Override
     public boolean removeRoute(final UnsignedInteger routerId, final Long remotePathId) {
         final RouteKey key = new RouteKey(routerId, remotePathId);
-        final OffsetMap<RouteKey> map = getOffsets();
+        final OffsetMap map = getOffsets();
         final int offset = map.offsetOf(key);
         this.values = map.removeValue(this.values, offset);
         return removeRoute(key, offset);
@@ -33,16 +33,16 @@ final class ComplexRouteEntry extends AbstractAllPathsRouteEntry {
 
     @Override
     public MapEntryNode createValue(final YangInstanceIdentifier.PathArgument routeId, final BestPath path) {
-        final OffsetMap<RouteKey> map = getOffsets();
+        final OffsetMap map = getOffsets();
         final MapEntryNode mapValues = map.getValue(this.values, map.offsetOf(((AddPathBestPath) path).getRouteKey()));
         return RouteEntryUtil.createComplexRouteValue(routeId, path, mapValues);
     }
 
     @Override
     public int addRoute(final UnsignedInteger routerId, final Long remotePathId, final YangInstanceIdentifier.NodeIdentifier attII, final NormalizedNode<?, ?> data) {
-        final OffsetMap<RouteKey> oldMap = getOffsets();
+        final OffsetMap oldMap = getOffsets();
         final int offset = addRoute(new RouteKey(routerId, remotePathId), attII, data);
-        final OffsetMap<RouteKey> newMap = getOffsets();
+        final OffsetMap newMap = getOffsets();
 
         if (!newMap.equals(oldMap)) {
             this.values = newMap.expand(oldMap, this.values, offset);
