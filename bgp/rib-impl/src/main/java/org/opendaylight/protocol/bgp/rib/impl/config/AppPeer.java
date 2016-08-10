ff6f162a257a423d8a0c99271f8b0c52d84d2e57
Claudio D. Gasparini <cgaspari@cisco.com>
2016-08-10 14:53:28 +0200

    BUG-5685: Register BGP Application Peer Cluster Singleton Service
    Register BGP Application Peer Cluster Singleton Service.
    When running on clustering, it was need it to have different
    configuration per instance to avoid conflicts.
    Now all instance can have same configuration,
    since cluster service provider take cares of make active
    the one is running on Leader Cluster. If Leader goes down,
    the instance from the new Leader will become active.
    
    Change-Id: I5316a50b5ceaf54c838018860baefc5ae326465b
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>
    (cherry picked from commit 0aabee8976574d2765248f134d4292e345adcb83)

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/AppPeer.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/AppPeer.java
@@ -10,38 +10,40 @@ package org.opendaylight.protocol.bgp.rib.impl.config;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataTreeChangeService;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataTreeIdentifier;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonService;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceRegistration;
+import org.opendaylight.mdsal.singleton.common.api.ServiceGroupIdentifier;
 import org.opendaylight.protocol.bgp.openconfig.spi.BGPOpenConfigMappingService;
 import org.opendaylight.protocol.bgp.rib.impl.ApplicationPeer;
 import org.opendaylight.protocol.bgp.rib.impl.spi.BgpDeployer.WriteConfiguration;
 import org.opendaylight.protocol.bgp.rib.impl.spi.RIB;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.Config;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbors.Neighbor;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.ApplicationRib;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.ApplicationRibId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.Tables;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AppPeer implements PeerBean {
-
+    private static final Logger LOG = LoggerFactory.getLogger(AppPeer.class);
     private static final QName APP_ID_QNAME = QName.create(ApplicationRib.QNAME, "id").intern();
-
-    private ApplicationPeer applicationPeer;
-    private ListenerRegistration<ApplicationPeer> registration;
     private Neighbor currentConfiguration;
+    private BgpAppPeerSingletonService bgpAppPeerSingletonService;
 
     @Override
     public void start(final RIB rib, final Neighbor neighbor, final BGPOpenConfigMappingService mappingService, final WriteConfiguration configurationWriter) {
-        this.currentConfiguration = Preconditions.checkNotNull(neighbor);
-        final ApplicationRibId appRibId = createAppRibId(neighbor);
-        this.applicationPeer = new ApplicationPeer(appRibId, neighbor.getNeighborAddress().getIpv4Address(), rib);
-        final YangInstanceIdentifier yangIId = YangInstanceIdentifier.builder().node(ApplicationRib.QNAME)
-                .nodeWithKey(ApplicationRib.QNAME, APP_ID_QNAME, appRibId.getValue()).node(Tables.QNAME).node(Tables.QNAME).build();
-        this.registration = rib.getService().registerDataTreeChangeListener(new DOMDataTreeIdentifier(LogicalDatastoreType.CONFIGURATION, yangIId),
-                this.applicationPeer);
+        this.currentConfiguration = neighbor;
+        this.bgpAppPeerSingletonService = new BgpAppPeerSingletonService(rib, createAppRibId(neighbor), neighbor.getNeighborAddress().getIpv4Address());
     }
 
     @Override
@@ -52,9 +54,10 @@ public class AppPeer implements PeerBean {
 
     @Override
     public void close() {
-        if (this.applicationPeer != null) {
-            this.registration.close();
-            this.applicationPeer.close();
+        try {
+            this.bgpAppPeerSingletonService.close();
+        } catch (final Exception e) {
+            LOG.warn("Failed to close application peer instance", e);
         }
     }
 
@@ -71,4 +74,52 @@ public class AppPeer implements PeerBean {
         return new ApplicationRibId(neighbor.getNeighborAddress().getIpv4Address().getValue());
     }
 
+    private final class BgpAppPeerSingletonService implements ClusterSingletonService, AutoCloseable {
+        private final ApplicationPeer applicationPeer;
+        private final DOMDataTreeChangeService dataTreeChangeService;
+        private final ApplicationRibId appRibId;
+        private ClusterSingletonServiceRegistration singletonServiceRegistration;
+        private ListenerRegistration<ApplicationPeer> registration;
+        private final ServiceGroupIdentifier serviceGroupIdentifier;
+
+        BgpAppPeerSingletonService(final RIB rib, final ApplicationRibId appRibId, final Ipv4Address neighborAddress) {
+            this.applicationPeer = new ApplicationPeer(appRibId, neighborAddress, rib);
+            this.appRibId = appRibId;
+            this.dataTreeChangeService = rib.getService();
+            this.serviceGroupIdentifier = rib.getRibIServiceGroupIdentifier();
+            LOG.info("Application Peer Singleton Service {} registered", getIdentifier());
+            this.singletonServiceRegistration = rib.registerClusterSingletonService(this);
+        }
+
+        @Override
+        public void close() throws Exception {
+            if (this.singletonServiceRegistration != null) {
+                this.singletonServiceRegistration.close();
+                this.singletonServiceRegistration = null;
+            }
+        }
+
+        @Override
+        public void instantiateServiceInstance() {
+            LOG.info("Application Peer Singleton Service {} instantiated", getIdentifier());
+            final YangInstanceIdentifier yangIId = YangInstanceIdentifier.builder().node(ApplicationRib.QNAME)
+                .nodeWithKey(ApplicationRib.QNAME, APP_ID_QNAME, appRibId.getValue()).node(Tables.QNAME).node(Tables.QNAME).build();
+            this.applicationPeer.instantiateServiceInstance();
+            this.registration = this.dataTreeChangeService
+                .registerDataTreeChangeListener(new DOMDataTreeIdentifier(LogicalDatastoreType.CONFIGURATION, yangIId), this.applicationPeer);
+        }
+
+        @Override
+        public ListenableFuture<Void> closeServiceInstance() {
+            LOG.info("Application Peer Singleton Service {} instance closed", getIdentifier());
+            this.registration.close();
+            this.applicationPeer.close();
+            return Futures.immediateFuture(null);
+        }
+
+        @Override
+        public ServiceGroupIdentifier getIdentifier() {
+            return this.serviceGroupIdentifier;
+        }
+    }
 }
