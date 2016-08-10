0aabee8976574d2765248f134d4292e345adcb83
Claudio D. Gasparini <cgaspari@cisco.com>
2016-08-10 14:53:28 +0200

    BUG-5685: Register BGP Application Peer Cluster Singleton Service
    Register BGP Application Peer Cluster Singleton Service.
    When running on clustering, it was need it to have different
    configuration per instance to avoid conflicts.
    Now all instance can have same configuration,
    since cluster service provider take cares of make active
    the one is running on Leader Cluster. If Leader goes down,
    the instance from the new Leader will become active.
    
    Change-Id: I5316a50b5ceaf54c838018860baefc5ae326465b
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- bgp/rib-impl/src/test/java/org/opendaylight/protocol/bgp/rib/impl/PeerTest.java
+++ bgp/rib-impl/src/test/java/org/opendaylight/protocol/bgp/rib/impl/PeerTest.java
@@ -127,6 +127,7 @@ public class PeerTest extends AbstractRIBTestSetup {
         final Ipv4Prefix second = new Ipv4Prefix("127.0.0.1/32");
         final Ipv4Prefix third = new Ipv4Prefix("127.0.0.3/32");
         this.peer = new ApplicationPeer(new ApplicationRibId("t"), new Ipv4Address("127.0.0.1"), getRib());
+        this.peer.instantiateServiceInstance();
         final YangInstanceIdentifier base = getRib().getYangRibId().node(LocRib.QNAME).node(Tables.QNAME).node(RibSupportUtils.toYangTablesKey(KEY));
         this.peer.onDataTreeChanged(ipv4Input(base, ModificationType.WRITE, first, second, third));
         assertEquals(3, this.routes.size());
