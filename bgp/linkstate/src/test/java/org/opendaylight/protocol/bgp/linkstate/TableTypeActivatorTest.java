ba317b4d54b7e72f26cf3b66b3279024572bac00
Milos Fabian <milfabia@cisco.com>
2016-10-27 12:41:34 +0200

    Register Link-state AFI/SAFI
    Change-Id: I7c5c6ccafa23e555d10a0f8461513200c4173d6d
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/linkstate/src/test/java/org/opendaylight/protocol/bgp/linkstate/TableTypeActivatorTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.linkstate;
+
+import java.util.Optional;
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.protocol.bgp.linkstate.impl.TableTypeActivator;
+import org.opendaylight.protocol.bgp.openconfig.spi.SimpleBGPTableTypeRegistryProvider;
+import org.opendaylight.protocol.bgp.parser.BgpTableTypeImpl;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009.AfiSafiType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.LinkstateAddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.linkstate.rev150210.LinkstateSubsequentAddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.BgpTableType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev160614.LINKSTATE;
+
+public class TableTypeActivatorTest {
+
+    private static final BgpTableType LINKSTATE = new BgpTableTypeImpl(LinkstateAddressFamily.class, LinkstateSubsequentAddressFamily.class);
+
+    @Test
+    public void testActivator() {
+        final TableTypeActivator tableTypeActivator = new TableTypeActivator();
+        final SimpleBGPTableTypeRegistryProvider registry = new SimpleBGPTableTypeRegistryProvider();
+        tableTypeActivator.startBGPTableTypeRegistryProvider(registry);
+
+        final Optional<Class<? extends AfiSafiType>> afiSafiType = registry.getAfiSafiType(LINKSTATE);
+        Assert.assertEquals(LINKSTATE.class, afiSafiType.get());
+        final Optional<BgpTableType> tableType = registry.getTableType(LINKSTATE.class);
+        Assert.assertEquals(LINKSTATE, tableType.get());
+
+        tableTypeActivator.stopBGPTableTypeRegistryProvider();
+        tableTypeActivator.close();
+    }
+
+}
