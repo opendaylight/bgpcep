f76d462c4b8d474aebc1b14e05f8650f46eec505
Milos Fabian <milfabia@cisco.com>
2016-10-27 14:18:59 +0200

    Register L2VPN EVPN AFI/SAFI
    Change-Id: If2f3c18606053df53c9e5c1c69e976c3b8ea16df
    Signed-off-by: Milos Fabian <milfabia@cisco.com>

---
+++ bgp/evpn/src/main/java/org/opendaylight/protocol/bgp/evpn/impl/TableTypeActivator.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.evpn.impl;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+import org.opendaylight.protocol.bgp.openconfig.spi.AbstractBGPTableTypeRegistryProviderActivator;
+import org.opendaylight.protocol.bgp.openconfig.spi.BGPTableTypeRegistryProvider;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009.L2VPNEVPN;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.EvpnSubsequentAddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.evpn.rev160321.L2vpnAddressFamily;
+import org.opendaylight.yangtools.concepts.AbstractRegistration;
+
+public final class TableTypeActivator extends AbstractBGPTableTypeRegistryProviderActivator {
+
+    @Override
+    protected List<AbstractRegistration> startBGPTableTypeRegistryProviderImpl(final BGPTableTypeRegistryProvider provider) {
+        return Lists.newArrayList(provider.registerBGPTableType(L2vpnAddressFamily.class, EvpnSubsequentAddressFamily.class, L2VPNEVPN.class));
+    }
+
+}
