0ab7e50af15c4969dc3940b7d871d97b146f104f
Claudio D. Gasparini <cgaspari@cisco.com>
2016-11-03 11:53:33 +0100

    BUG-7006: Unit-tests sometimes hangs during execution
    Fix sporadically hangs during unit-tests execution
    
    Change-Id: Ia339af6d9181a7be8e4b3e6335bba250aaecd822
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- bgp/rib-impl/src/test/java/org/opendaylight/protocol/bgp/rib/impl/AbstractBGPDispatcherTest.java
+++ bgp/rib-impl/src/test/java/org/opendaylight/protocol/bgp/rib/impl/AbstractBGPDispatcherTest.java
@@ -5,7 +5,6 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.protocol.bgp.rib.impl;
 
 import static org.opendaylight.protocol.bgp.rib.impl.CheckUtil.waitFutureSuccess;
@@ -14,24 +13,18 @@ import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.Uninterruptibles;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.Epoll;
-import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
 import java.net.InetSocketAddress;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
 import org.opendaylight.protocol.bgp.parser.BgpExtendedMessageUtil;
 import org.opendaylight.protocol.bgp.parser.BgpTableTypeImpl;
@@ -58,112 +51,60 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.type
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.UnicastSubsequentAddressFamily;
 import org.slf4j.LoggerFactory;
 
-public class BGPDispatcherImplTest {
+public class AbstractBGPDispatcherTest {
+    protected static final AsNumber AS_NUMBER = new AsNumber(30L);
+    protected static final int RETRY_TIMER = 1;
+    protected static final BgpTableType IPV_4_TT = new BgpTableTypeImpl(Ipv4AddressFamily.class, UnicastSubsequentAddressFamily.class);
     private static final short HOLD_TIMER = 30;
-    private static final AsNumber AS_NUMBER = new AsNumber(30L);
-    private static final int RETRY_TIMER = 1;
-    private static final BgpTableType IPV_4_TT = new BgpTableTypeImpl(Ipv4AddressFamily.class, UnicastSubsequentAddressFamily.class);
-    private BGPDispatcherImpl serverDispatcher;
-    private BGPDispatcherImpl clientDispatcher;
-    private BGPPeerRegistry registry;
-    private SimpleSessionListener clientListener;
-    private SimpleSessionListener serverListener;
+    protected BGPDispatcherImpl clientDispatcher;
+    protected BGPPeerRegistry registry;
+    protected SimpleSessionListener clientListener;
+    protected BGPDispatcherImpl serverDispatcher;
+    protected SimpleSessionListener serverListener;
+    protected InetSocketAddress clientAddress;
     private EventLoopGroup boss;
     private EventLoopGroup worker;
-    private InetSocketAddress clientAddress;
 
     @Before
     public void setUp() throws BGPDocumentedException {
-        if (Epoll.isAvailable()) {
-            this.boss = new EpollEventLoopGroup();
-            this.worker = new EpollEventLoopGroup();
-        } else {
+        if (!Epoll.isAvailable()) {
             this.boss = new NioEventLoopGroup();
             this.worker = new NioEventLoopGroup();
         }
         this.registry = new StrictBGPPeerRegistry();
         this.clientListener = new SimpleSessionListener();
+        this.serverListener = new SimpleSessionListener();
         final BGPExtensionProviderContext ctx = ServiceLoaderBGPExtensionProviderContext.getSingletonInstance();
         this.serverDispatcher = new BGPDispatcherImpl(ctx.getMessageRegistry(), this.boss, this.worker);
-        configureClient(ctx);
-    }
 
-    public static void checkIdleState (final SimpleSessionListener listener){
-        Stopwatch sw = Stopwatch.createStarted();
-        while(sw.elapsed(TimeUnit.SECONDS) <= 10) {
-            if (BGPSessionImpl.State.IDLE != listener.getState()){
-                Uninterruptibles.sleepUninterruptibly(50, TimeUnit.MILLISECONDS);
-            } else {
-                return;
-            }
-        }
-        Assert.fail();
-    }
-
-    private void configureClient(final BGPExtensionProviderContext ctx) {
         this.clientAddress = InetSocketAddressUtil.getRandomLoopbackInetSocketAddress();
-        final IpAddress clientPeerIp = new IpAddress(new Ipv4Address(this.clientAddress.getAddress().getHostAddress()));
-        this.registry.addPeer(clientPeerIp, this.clientListener, createPreferences(this.clientAddress));
+        final IpAddress clientPeerIp = new IpAddress(new Ipv4Address(clientAddress.getAddress().getHostAddress()));
+        this.registry.addPeer(clientPeerIp, this.clientListener, createPreferences(clientAddress));
         this.clientDispatcher = new BGPDispatcherImpl(ctx.getMessageRegistry(), this.boss, this.worker);
-    }
-
-    private Channel createServer(final InetSocketAddress serverAddress) throws InterruptedException {
-        this.serverListener = new SimpleSessionListener();
-        this.registry.addPeer(new IpAddress(new Ipv4Address(serverAddress.getAddress().getHostAddress())), this.serverListener, createPreferences(serverAddress));
-        LoggerFactory.getLogger(BGPDispatcherImplTest.class).info("createServer");
-        final ChannelFuture future = this.serverDispatcher.createServer(this.registry, serverAddress);
-        future.addListener(new GenericFutureListener<Future<Void>>() {
-            @Override
-            public void operationComplete(final Future<Void> future) {
-                Preconditions.checkArgument(future.isSuccess(), "Unable to start bgp server on %s", future.cause());
-            }
-        });
-        waitFutureSuccess(future);
-        return future.channel();
     }
 
     @After
     public void tearDown() throws Exception {
         this.serverDispatcher.close();
         this.registry.close();
-        this.worker.shutdownGracefully().awaitUninterruptibly();
-        this.boss.shutdownGracefully().awaitUninterruptibly();
+        if (!Epoll.isAvailable()) {
+            this.worker.shutdownGracefully().awaitUninterruptibly();
+            this.boss.shutdownGracefully().awaitUninterruptibly();
+        }
     }
 
-    @Test
-    public void testCreateClient() throws InterruptedException, ExecutionException {
-        final InetSocketAddress serverAddress = InetSocketAddressUtil.getRandomLoopbackInetSocketAddress();
-        final Channel serverChannel = createServer(serverAddress);
-        Thread.sleep(1000);
-        final Future<BGPSessionImpl> futureClient = this.clientDispatcher.createClient(this.clientAddress, serverAddress, this.registry, 2, true);
-        waitFutureSuccess(futureClient);
-        final BGPSessionImpl session = futureClient.get();
-        Assert.assertEquals(BGPSessionImpl.State.UP, this.clientListener.getState());
-        Assert.assertEquals(BGPSessionImpl.State.UP, this.serverListener.getState());
-        Assert.assertEquals(AS_NUMBER, session.getAsNumber());
-        Assert.assertEquals(Sets.newHashSet(IPV_4_TT), session.getAdvertisedTableTypes());
-        Assert.assertTrue(serverChannel.isWritable());
-        session.close();
-        this.serverListener.releaseConnection();
-        checkIdleState(this.clientListener);
-        checkIdleState(this.serverListener);
+    private void configureClient(final BGPExtensionProviderContext ctx) {
+        final InetSocketAddress clientAddress = InetSocketAddressUtil.getRandomLoopbackInetSocketAddress();
+        final IpAddress clientPeerIp = new IpAddress(new Ipv4Address(clientAddress.getAddress().getHostAddress()));
+        this.registry.addPeer(clientPeerIp, this.clientListener, createPreferences(clientAddress));
+        this.clientDispatcher = new BGPDispatcherImpl(ctx.getMessageRegistry(), this.boss, this.worker);
+        if (!Epoll.isAvailable()) {
+            this.worker.shutdownGracefully().awaitUninterruptibly();
+            this.boss.shutdownGracefully().awaitUninterruptibly();
+        }
     }
 
-    @Test
-    public void testCreateReconnectingClient() throws Exception {
-        final InetSocketAddress serverAddress = InetSocketAddressUtil.getRandomLoopbackInetSocketAddress();
-        final Future<Void> future = this.clientDispatcher.createReconnectingClient(serverAddress, this.registry, RETRY_TIMER, Optional.absent(),
-            this.clientAddress, true);
-        waitFutureSuccess(future);
-        final Channel serverChannel = createServer(serverAddress);
-        Assert.assertEquals(BGPSessionImpl.State.UP, this.serverListener.getState());
-        Assert.assertTrue(serverChannel.isWritable());
-        future.cancel(true);
-        this.serverListener.releaseConnection();
-        checkIdleState(this.serverListener);
-    }
-
-    private BGPSessionPreferences createPreferences(final InetSocketAddress socketAddress) {
+    protected BGPSessionPreferences createPreferences(final InetSocketAddress socketAddress) {
         final List<BgpParameters> tlvs = Lists.newArrayList();
         final List<OptionalCapabilities> capas = Lists.newArrayList();
         capas.add(new OptionalCapabilitiesBuilder().setCParameters(new CParametersBuilder().addAugmentation(
@@ -175,5 +116,26 @@ public class BGPDispatcherImplTest {
         tlvs.add(new BgpParametersBuilder().setOptionalCapabilities(capas).build());
         final BgpId bgpId = new BgpId(new Ipv4Address(socketAddress.getAddress().getHostAddress()));
         return new BGPSessionPreferences(AS_NUMBER, HOLD_TIMER, bgpId, AS_NUMBER, tlvs, Optional.absent());
+    }
+
+    public static void checkIdleState(final SimpleSessionListener listener) {
+        Stopwatch sw = Stopwatch.createStarted();
+        while (sw.elapsed(TimeUnit.SECONDS) <= 10) {
+            if (BGPSessionImpl.State.IDLE != listener.getState()) {
+                Uninterruptibles.sleepUninterruptibly(50, TimeUnit.MILLISECONDS);
+            } else {
+                return;
+            }
+        }
+        Assert.fail();
+    }
+
+    protected Channel createServer(final InetSocketAddress serverAddress) throws InterruptedException {
+        this.registry.addPeer(new IpAddress(new Ipv4Address(serverAddress.getAddress().getHostAddress())), this.serverListener, createPreferences(serverAddress));
+        LoggerFactory.getLogger(AbstractBGPDispatcherTest.class).info("createServer");
+        final ChannelFuture future = this.serverDispatcher.createServer(this.registry, serverAddress);
+        future.addListener(future1 -> Preconditions.checkArgument(future1.isSuccess(), "Unable to start bgp server on %s", future1.cause()));
+        waitFutureSuccess(future);
+        return future.channel();
     }
 }
