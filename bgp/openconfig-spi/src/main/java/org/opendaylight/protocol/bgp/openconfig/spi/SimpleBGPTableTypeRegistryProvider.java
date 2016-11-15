3227a5df7e376a430a7b3d9239aa8a55ca8e0950
Claudio D. Gasparini <cgaspari@cisco.com>
2016-11-15 13:17:17 +0100

    BUG-5032: BGP Openconfig State Implementation
    Introduce service on charge of store
    BGP Operational State as an Openconfig
    Operational State format.
    Introduce Utilities to convert BGP Operational
    State to Openconfig State.
    
    Intruce a cache TableType-AfiSafi under
    BGPTableTypeRegistry. Table type will be stored
    as they are required.
    
    Change-Id: I3cb546f7f332e7f9eb7bba0a45d366fca0c4d0e5
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

--- bgp/openconfig-spi/src/main/java/org/opendaylight/protocol/bgp/openconfig/spi/SimpleBGPTableTypeRegistryProvider.java
+++ bgp/openconfig-spi/src/main/java/org/opendaylight/protocol/bgp/openconfig/spi/SimpleBGPTableTypeRegistryProvider.java
@@ -12,10 +12,12 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import java.util.Optional;
+import javax.annotation.Nonnull;
 import javax.annotation.concurrent.GuardedBy;
 import org.opendaylight.protocol.bgp.parser.BgpTableTypeImpl;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009.AfiSafiType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev130919.BgpTableType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.TablesKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.AddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.SubsequentAddressFamily;
 import org.opendaylight.yangtools.concepts.AbstractRegistration;
@@ -24,6 +26,8 @@ public final class SimpleBGPTableTypeRegistryProvider implements BGPTableTypeReg
 
     @GuardedBy("this")
     private final BiMap<BgpTableType, Class<? extends AfiSafiType>> tableTypes = HashBiMap.create();
+    @GuardedBy("this")
+    private final BiMap<TablesKey, Class<? extends AfiSafiType>> tableKeys = HashBiMap.create();
 
     @Override
     public synchronized AbstractRegistration registerBGPTableType(final Class<? extends AddressFamily> afi,
@@ -31,11 +35,15 @@ public final class SimpleBGPTableTypeRegistryProvider implements BGPTableTypeReg
         final BgpTableType tableType = new BgpTableTypeImpl(afi, safi);
         final Class<? extends AfiSafiType> prev = this.tableTypes.putIfAbsent(tableType, afiSafiType);
         Preconditions.checkState(prev == null, "AFI %s SAFI %s is already registered with %s", afi, safi, prev);
+        final TablesKey tableKey = new TablesKey(tableType.getAfi(), tableType.getSafi());
+        this.tableKeys.put(tableKey, afiSafiType);
+
         return new AbstractRegistration() {
             @Override
             protected void removeRegistration() {
                 synchronized (SimpleBGPTableTypeRegistryProvider.this) {
                     SimpleBGPTableTypeRegistryProvider.this.tableTypes.remove(tableType);
+                    SimpleBGPTableTypeRegistryProvider.this.tableKeys.remove(tableKey);
                 }
             }
         };
@@ -47,9 +55,23 @@ public final class SimpleBGPTableTypeRegistryProvider implements BGPTableTypeReg
         return Optional.ofNullable(tableType);
     }
 
+    @Nonnull
+    @Override
+    public Optional<TablesKey> getTableKey(@Nonnull final Class<? extends AfiSafiType> afiSafiType) {
+        final TablesKey tableKey = this.tableKeys.inverse().get(afiSafiType);
+        return Optional.ofNullable(tableKey);
+    }
+
     @Override
     public synchronized Optional<Class<? extends AfiSafiType>> getAfiSafiType(final BgpTableType bgpTableType) {
         final Class<? extends AfiSafiType> afiSafi = this.tableTypes.get(bgpTableType);
+        return Optional.ofNullable(afiSafi);
+    }
+
+    @Nonnull
+    @Override
+    public Optional<Class<? extends AfiSafiType>> getAfiSafiType(@Nonnull final TablesKey tablesKey) {
+        final Class<? extends AfiSafiType> afiSafi = this.tableKeys.get(tablesKey);
         return Optional.ofNullable(afiSafi);
     }
 
---
+++ bgp/openconfig-state/src/main/java/org/opendaylight/protocol/bgp/state/GlobalUtil.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.state;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import org.opendaylight.protocol.bgp.openconfig.spi.BGPTableTypeRegistryConsumer;
+import org.opendaylight.protocol.bgp.rib.spi.state.BGPRIBState;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.AfiSafi;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.AfiSafiBuilder;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.afi.safi.State;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.afi.safi.StateBuilder;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.global.base.AfiSafisBuilder;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.top.bgp.Global;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.top.bgp.GlobalBuilder;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009.AfiSafiType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev160614.GlobalAfiSafiStateAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev160614.GlobalAfiSafiStateAugmentationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.TablesKey;
+
+public final class GlobalUtil {
+    private GlobalUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Build Openconfig Global containing RIB group stats from a list of BGP RIB State
+     *
+     * @param ribState containing RIb Operational State
+     * @param bgpTableTypeRegistry BGP TableType Registry
+     * @return
+     */
+    @Nonnull
+    public static Global buildGlobal(final BGPRIBState ribState,
+        final BGPTableTypeRegistryConsumer bgpTableTypeRegistry) {
+        return  new GlobalBuilder().setState(buildState(ribState))
+            .setAfiSafis(new AfiSafisBuilder().setAfiSafi(buildAfisSafis(ribState, bgpTableTypeRegistry)).build())
+            .build();
+    }
+
+    /**
+     * Build per AFI SAFI Openconfig Global State containing RIB group stats from a list of BGP RIB State
+     * @param ribState containing RIb Operational State
+     * @param bgpTableTypeRegistry BGP TableType Registry
+     * @return List containing per afi/safi operational state
+     */
+    public static List<AfiSafi> buildAfisSafis(final BGPRIBState ribState,
+        final BGPTableTypeRegistryConsumer bgpTableTypeRegistry) {
+        return ribState.getPathsCount().keySet().stream()
+            .map(tk -> buildAfiSafi(ribState, tk, bgpTableTypeRegistry))
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Build Openconfig Global State
+     * @param ribState containing RIb Operational State
+     * @return Openconfig Global State
+     */
+    public static org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.global.base.State buildState(
+        final BGPRIBState ribState) {
+        return new org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.global.base.StateBuilder()
+            .setAs(ribState.getAs())
+            .setRouterId(ribState.getRouteId())
+            .setTotalPaths(ribState.getTotalPathsCount())
+            .setTotalPrefixes(ribState.getTotalPrefixesCount())
+            .build();
+    }
+
+    /**
+     *
+     * @param ribState containing RIb Operational State
+     * @param tablesKey table Key
+     * @param bgpTableTypeRegistry BGP TableType Registry
+     * @return Afi Safi Operational State
+     */
+    public static AfiSafi buildAfiSafi(final BGPRIBState ribState, final TablesKey tablesKey,
+        final BGPTableTypeRegistryConsumer bgpTableTypeRegistry) {
+        final Optional<Class<? extends AfiSafiType>> optAfiSafi = bgpTableTypeRegistry.getAfiSafiType(tablesKey);
+        if (!optAfiSafi.isPresent()) {
+            return null;
+        }
+        final State state = new StateBuilder()
+            .addAugmentation(GlobalAfiSafiStateAugmentation.class, new GlobalAfiSafiStateAugmentationBuilder()
+                .setTotalPaths(ribState.getPathCount(tablesKey)).setTotalPrefixes(ribState.getPrefixesCount(tablesKey))
+                .build()).build();
+        return new AfiSafiBuilder().setAfiSafiName(optAfiSafi.get()).setState(state).build();
+    }
+}
