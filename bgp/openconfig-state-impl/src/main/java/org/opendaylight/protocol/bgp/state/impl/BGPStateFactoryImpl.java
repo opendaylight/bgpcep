6b7c3668a34958a08838f8fa7ffb25f6303b1920
Claudio D. Gasparini <cgaspari@cisco.com>
2016-11-15 13:17:17 +0100

    BUG-5032: BGP Openconfig Implementation
    Write on Data Store BGP Openconfig State for
    - Global
    - Peer Groups
    - Neighbors
    
    Representation of multiple state information
    as number of paths/ prefixes per
    neighbor/group/global, per Afi Safi.
    status, configuration, etc..
    
    Change-Id: I36d283c7950b68608b66ab53814b0fba08eb6b32
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/openconfig-state-impl/src/main/java/org/opendaylight/protocol/bgp/state/impl/BGPStateFactoryImpl.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.state.impl;
+
+import java.util.Set;
+import javax.annotation.Nonnull;
+import org.opendaylight.protocol.bgp.state.impl.global.BGPGlobalStateImpl;
+import org.opendaylight.protocol.bgp.state.impl.neighbor.BGPNeighborStateImpl;
+import org.opendaylight.protocol.bgp.state.spi.BGPStateFactory;
+import org.opendaylight.protocol.bgp.state.spi.state.BGPGlobalState;
+import org.opendaylight.protocol.bgp.state.spi.state.BGPNeighborState;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.top.Bgp;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009.AfiSafiType;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.AsNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.BgpId;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public final class BGPStateFactoryImpl implements BGPStateFactory {
+    @Override
+    public BGPGlobalState createBGPGlobalState(final Set<Class<? extends AfiSafiType>> announcedAfiSafis,
+        final String ribId, final BgpId routeId, final AsNumber localAs,
+        final InstanceIdentifier<Bgp> bgpIId) {
+        return new BGPGlobalStateImpl(announcedAfiSafis, ribId, routeId, localAs, bgpIId);
+    }
+
+    @Override
+    public BGPNeighborState createBGPNeighborState(@Nonnull final IpAddress neighborAddress,
+        final Set<Class<? extends AfiSafiType>> afiSafisAdvertized,
+        final Set<Class<? extends AfiSafiType>> afiSafisGracefulAdvertized) {
+        return new BGPNeighborStateImpl(neighborAddress, afiSafisAdvertized, afiSafisGracefulAdvertized);
+    }
+}
