c3567ed4d396791bf0f19840eb757adc083b4a5c
Claudio D. Gasparini <cgaspari@cisco.com>
2016-12-19 20:16:29 +0100

    BUG-5032: Introduce BGP Operational State
    Introduce interfaces for expose BGP Operational State for:
    -RIB
    -PeerGroup
    -Neighbor
    -BGPState Collector
    
    Change-Id: Ib6b053f4e7e734eb3f4525f7de4643c9b89c9a31
    Signed-off-by: Claudio D. Gasparini <cgaspari@cisco.com>

---
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/state/BGPSessionState.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.rib.spi.state;
+
+import org.opendaylight.protocol.bgp.rib.spi.State;
+
+/**
+ * BGP Operational Session State
+ */
+public interface BGPSessionState {
+    /**
+     * Internal session state
+     *
+     * @return Internal session state
+     */
+    State getSessionState();
+
+    /**
+     * Additional Path capability
+     *
+     * @return true if supported
+     */
+    boolean isAddPathCapabilitySupported();
+
+    /**
+     * AS 4 Bytes capability
+     *
+     * @return true if supported
+     */
+    boolean isAsn32CapabilitySupported();
+
+    /**
+     * Graceful Restart
+     *
+     * @return true if supported
+     */
+    boolean isGracefulRestartCapabilitySupported();
+
+    /**
+     * Multiprotocol capability
+     *
+     * @return true if supported
+     */
+    boolean isMultiProtocolCapabilitySupported();
+
+    /**
+     * Router Refresh Capability
+     *
+     * @return true if supported
+     */
+    boolean isRouterRefreshCapabilitySupported();
+
+    /**
+     * Update Messages Sent count
+     *
+     * @return count
+     */
+    long getUpdateMessagesSentCount();
+
+    /**
+     * Notification Messages Sent count
+     *
+     * @return count
+     */
+    long getNotificationMessagesSentCount();
+
+    /**
+     * Update Messages Received count
+     *
+     * @return count
+     */
+    long getUpdateMessagesReceivedCount();
+
+    /**
+     * Notification Update Messages Received count
+     *
+     * @return count
+     */
+    long getNotificationMessagesReceivedCount();
+}
---
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/state/BGPTransportState.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+
+package org.opendaylight.protocol.bgp.rib.spi.state;
+
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
+
+/**
+ * BGP Operational Transport State
+ */
+public interface BGPTransportState {
+    /**
+     * Local Port
+     *
+     * @return port
+     */
+    @Nonnull
+    PortNumber getLocalPort();
+
+    /**
+     * Remote Address
+     *
+     * @return IpAddress
+     */
+    @Nonnull
+    IpAddress getRemoteAddress();
+
+    /**
+     * Remote Port
+     *
+     * @return port
+     */
+    @Nonnull
+    PortNumber getRemotePort();
+}
