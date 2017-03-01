29e70d618dbb019748e11b8a95e3fda43e22b7a2
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2017-03-01 17:25:53 +0100

    BUG-6975: Integrate Programming with Cluster Singleton Service
    Integrate Programming with Cluster Singleton Service
    
    Change-Id: I95e3c8c1dc1e596c060256d3f59fec9cfe882456
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

--- programming/spi/src/main/java/org/opendaylight/bgpcep/programming/spi/InstructionScheduler.java
+++ programming/spi/src/main/java/org/opendaylight/bgpcep/programming/spi/InstructionScheduler.java
@@ -8,9 +8,12 @@
 package org.opendaylight.bgpcep.programming.spi;
 
 import com.google.common.util.concurrent.ListenableFuture;
+import javax.annotation.Nonnull;
+import org.opendaylight.mdsal.singleton.common.api.ServiceGroupIdentifier;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.programming.rev150720.SubmitInstructionInput;
+import org.opendaylight.yangtools.concepts.Identifiable;
 
-public interface InstructionScheduler {
+public interface InstructionScheduler extends Identifiable<ServiceGroupIdentifier> {
     /**
      * Schedule a new instruction for execution. This method tries to enqueue an instruction. It will return a Future
      * which represents the scheduling progress. When the future becomes successful, the requestor is expected to start
@@ -18,8 +21,15 @@ public interface InstructionScheduler {
      *
      * @param input Instruction scheduling information
      * @return Scheduling future.
-     *
      * @throws SchedulerException if a failure to schedule the instruction occurs.
      */
     ListenableFuture<Instruction> scheduleInstruction(SubmitInstructionInput input) throws SchedulerException;
+
+    /**
+     * Returns InstructionID
+     *
+     * @return Instruction ID
+     */
+    @Nonnull
+    String getInstructionID();
 }
