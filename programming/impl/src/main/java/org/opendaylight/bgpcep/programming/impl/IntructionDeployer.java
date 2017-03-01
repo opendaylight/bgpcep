a2d4f4ffb1ae59a1f6ab80c1cf28cdb833dcc4ff
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2017-03-01 13:01:20 +0100

    BUG-6973: Implement Programming config via DS
    -Expose programming's configuration in config DS
    -Allow multiple programming configuration
    
    Change-Id: Ib6de3355dae6533367890ef87c504edbdca04654
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

--- programming/impl/src/main/java/org/opendaylight/bgpcep/programming/impl/IntructionDeployer.java
+++ programming/impl/src/main/java/org/opendaylight/bgpcep/programming/impl/IntructionDeployer.java
@@ -8,20 +8,38 @@
 package org.opendaylight.bgpcep.programming.impl;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Instruction Scheduler Deployer
  */
 public interface IntructionDeployer {
+    interface UpdateConfiguration {
+        void writeConfiguration();
+        void removeConfiguration();
+    }
+
     /**
      * Creates Instruction Scheduler
      * @param instructionId Instruction Scheduler Id
      */
-    void createInstruction(@Nonnull String instructionId);
+    void createInstruction(@Nonnull String instructionId, @Nullable UpdateConfiguration writeConfiguration);
 
     /**
      * Remove Instruction Scheduler
      * @param instructionId Instruction Scheduler Id
      */
     void removeInstruction(@Nonnull String instructionId);
+
+    /**
+     * Write instruction configuration on DS
+     * @param instructionId Instruction Scheduler Id
+     */
+    void writeConfiguration(String instructionId);
+
+    /**
+     * Remove instruction configuration on DS
+     * @param instructionId Instruction Scheduler Id
+     */
+    void removeConfiguration(String instructionId);
 }
