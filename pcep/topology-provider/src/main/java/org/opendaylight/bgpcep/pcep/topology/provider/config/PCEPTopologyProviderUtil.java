9d66958460bb36990770f3a6471dd6d35cb1c2e6
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2017-03-02 12:22:47 +0100

    BUG-6973: Wire topology-provider with BP
    -Wire topology-provider with BP
    -Preserve backwards compatibility
    
    Change-Id: Ibc459117b1656a0efe5e34365b9fc131479bdaea
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/config/PCEPTopologyProviderUtil.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.pcep.topology.provider.config;
+
+import com.google.common.base.Optional;
+import com.google.common.net.InetAddresses;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.opendaylight.controller.config.yang.pcep.topology.provider.Client;
+import org.opendaylight.protocol.concepts.KeyMapping;
+import org.opendaylight.protocol.util.Ipv4Util;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.rfc2385.cfg.rev160324.Rfc2385Key;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class PCEPTopologyProviderUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(PCEPTopologyProviderUtil.class);
+
+    private PCEPTopologyProviderUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Optional<KeyMapping> contructKeys(final List<Client> clients) {
+        KeyMapping ret = null;
+
+        if (clients != null && !clients.isEmpty()) {
+            ret = KeyMapping.getKeyMapping();
+            for (final Client c : clients) {
+                if (c.getAddress() == null) {
+                    LOG.warn("Client {} does not have an address skipping it", c);
+                    continue;
+                }
+                final Rfc2385Key rfc2385KeyPassword = c.getPassword();
+                if (rfc2385KeyPassword != null && !rfc2385KeyPassword.getValue().isEmpty()) {
+                    final String s = Ipv4Util.toStringIP(c.getAddress());
+                    ret.put(InetAddresses.forString(s), rfc2385KeyPassword.getValue().getBytes(StandardCharsets.US_ASCII));
+                }
+            }
+        }
+        return Optional.fromNullable(ret);
+    }
+}
---
+++ pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/config/PcepTopologyProvider.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.pcep.topology.provider.config;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import java.net.InetSocketAddress;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import org.opendaylight.bgpcep.pcep.topology.provider.PCEPTopologyProvider;
+import org.opendaylight.bgpcep.pcep.topology.provider.TopologySessionListenerFactory;
+import org.opendaylight.bgpcep.programming.spi.InstructionScheduler;
+import org.opendaylight.bgpcep.topology.DefaultTopologyReference;
+import org.opendaylight.controller.config.yang.pcep.topology.provider.PCEPTopologyProviderRuntimeRegistrator;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.protocol.concepts.KeyMapping;
+import org.opendaylight.protocol.pcep.PCEPCapability;
+import org.opendaylight.protocol.pcep.PCEPDispatcher;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class PcepTopologyProvider implements AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(PcepTopologyProvider.class);
+
+    private static final String STATEFUL_NOT_DEFINED = "Stateful capability not defined, aborting PCEP Topology " +
+        "Deployer instantiation";
+    private final PCEPDispatcher pcepDispatcher;
+    private final DataBroker dataBroker;
+    private final TopologySessionListenerFactory sessionListenerFactory;
+    private final RpcProviderRegistry rpcProviderRegistry;
+    private final BundleContext bundleContext;
+    private PCEPTopologyProvider pcepTopoProvider;
+
+    public PcepTopologyProvider(final BundleContext bundleContext, final DataBroker dataBroker,
+        final PCEPDispatcher pcepDispatcher, final RpcProviderRegistry rpcProviderRegistry,
+        final TopologySessionListenerFactory sessionListenerFactory) {
+        this.bundleContext = Preconditions.checkNotNull(bundleContext);
+        this.pcepDispatcher = Preconditions.checkNotNull(pcepDispatcher);
+        this.dataBroker = Preconditions.checkNotNull(dataBroker);
+        this.sessionListenerFactory = Preconditions.checkNotNull(sessionListenerFactory);
+        this.rpcProviderRegistry = Preconditions.checkNotNull(rpcProviderRegistry);
+        final List<PCEPCapability> capabilities = this.pcepDispatcher.getPCEPSessionNegotiatorFactory()
+            .getPCEPSessionProposalFactory().getCapabilities();
+        final boolean statefulCapability = capabilities.stream().anyMatch(PCEPCapability::isStateful);
+        if (!statefulCapability) {
+            throw new IllegalStateException(STATEFUL_NOT_DEFINED);
+        }
+    }
+
+    @Override
+    public void close() {
+        if (this.pcepTopoProvider != null) {
+            this.pcepTopoProvider.close();
+        }
+    }
+
+    public void start(final InetSocketAddress inetSocketAddress, final Optional<KeyMapping> keys,
+        final InstructionScheduler schedulerDependency, final TopologyId topologyId,
+        final Optional<PCEPTopologyProviderRuntimeRegistrator> runtime, final short rpcTimeout) {
+        Preconditions.checkState(this.pcepTopoProvider == null,
+            "Previous instance %s was not closed.", this);
+        try {
+            this.pcepTopoProvider = PCEPTopologyProvider.create(this.pcepDispatcher,
+                inetSocketAddress, keys, schedulerDependency, this.dataBroker, this.rpcProviderRegistry, topologyId,
+                this.sessionListenerFactory, runtime, rpcTimeout);
+
+            final Dictionary<String, String> properties = new Hashtable<>();
+            properties.put(PCEPTopologyProvider.class.getName(), topologyId.getValue());
+            final ServiceRegistration<?> serviceRegistration = this.bundleContext
+                .registerService(DefaultTopologyReference.class.getName(), this.pcepTopoProvider, properties);
+            this.pcepTopoProvider.setServiceRegistration(serviceRegistration);
+        } catch (final Exception e) {
+            LOG.debug("Failed to create PCEPTopologyProvider {}", topologyId.getValue());
+        }
+    }
+}
