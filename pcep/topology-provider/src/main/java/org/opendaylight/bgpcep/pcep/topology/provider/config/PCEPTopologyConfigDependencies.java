a06c7fa5c877466790cca068aa7eedac5b40ccf1
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2017-03-11 23:28:26 +0100

    BUG-6975: Integrate Topology Provider with CS Service
    Integrate Topology Provider with CS Service
    
    Change-Id: I0e9c42aa3538e0936b74120535fee369db1ad9d5
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/config/PCEPTopologyConfigDependencies.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2017 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.pcep.topology.provider.config;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.base.Optional;
+import java.net.InetSocketAddress;
+import org.opendaylight.bgpcep.programming.spi.InstructionScheduler;
+import org.opendaylight.controller.config.yang.pcep.topology.provider.PCEPTopologyProviderRuntimeRegistrator;
+import org.opendaylight.protocol.concepts.KeyMapping;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+
+public final class PCEPTopologyConfigDependencies {
+    private final InetSocketAddress address;
+    private final Optional<KeyMapping> keys;
+    private final InstructionScheduler scheduler;
+    private final TopologyId topologyId;
+    private final Optional<PCEPTopologyProviderRuntimeRegistrator> runtime;
+    private final int rpcTimeout;
+
+    public PCEPTopologyConfigDependencies(final InetSocketAddress address, final Optional<KeyMapping> keys,
+        final InstructionScheduler scheduler, final TopologyId topologyId,
+        final Optional<PCEPTopologyProviderRuntimeRegistrator> runtime, final short rpcTimeout) {
+        this.address = checkNotNull(address);
+        this.keys = checkNotNull(keys);
+        this.scheduler = checkNotNull(scheduler);
+        this.topologyId = checkNotNull(topologyId);
+        this.runtime = checkNotNull(runtime);
+        this.rpcTimeout = checkNotNull(rpcTimeout);
+    }
+
+    public TopologyId getTopologyId() {
+        return this.topologyId;
+    }
+
+    public InstructionScheduler getSchedulerDependency() {
+        return this.scheduler;
+    }
+
+    public int getRpcTimeout() {
+        return this.rpcTimeout;
+    }
+
+    public Optional<PCEPTopologyProviderRuntimeRegistrator> getRuntimeRootRegistrator() {
+        return this.runtime;
+    }
+
+    public InetSocketAddress getAddress() {
+        return this.address;
+    }
+
+    public Optional<KeyMapping> getKeys() {
+        return this.keys;
+    }
+}
