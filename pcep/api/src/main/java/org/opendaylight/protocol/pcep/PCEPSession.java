b1ed7d95b9e76074399040a7fff9d64291f08a3c
Kevin Wang <kevixw@gmail.com>
2017-04-04 18:08:07 -0700

    BUG-8156 Terminate PCEP session properly when ServerSessionManager is closed
    When the PCEP topology provider is loading the configuration initially,
    the ServerSessionManager will be restarted.  Any new PCEP connection
    coming in during this period will be rejected by the ServerSessionManager.
    
    This patch is to terminate these rejected PCEP session properly when above
    situation happens, so that the session can be established successfully
    in the next retry.
    
    Change-Id: Ifa3366c97025a0b31fb7d3ee50b1142f63a6209c
    Signed-off-by: Kevin Wang <kevixw@gmail.com>

--- pcep/api/src/main/java/org/opendaylight/protocol/pcep/PCEPSession.java
+++ pcep/api/src/main/java/org/opendaylight/protocol/pcep/PCEPSession.java
@@ -31,7 +31,22 @@ public interface PCEPSession extends AutoCloseable, PcepSessionState {
      */
     Future<Void> sendMessage(Message message);
 
+    /**
+     * Closes PCEP session, cancels all timers, returns to state Idle, sends the Close Message. KeepAlive and DeadTimer
+     * are cancelled if the state of the session changes to IDLE. This method is used to close the PCEP session from
+     * inside the session or from the listener, therefore the parent of this session should be informed.
+     * @param reason
+     */
     void close(TerminationReason reason);
+
+    /**
+     * Terminate a PCEP session.  A CLOSE message with given reason will be sent to remote peer by invoking
+     * {@link #close(TerminationReason)} method.
+     *
+     * It triggers onSessionTerminated(TerminationReason) after closing.
+     * @param reason
+     */
+    void terminate(TerminationReason reason);
 
     Tlvs getRemoteTlvs();
 
