9b49653469e73493eed1d43070ebf9419e0a65de
Kevin Wang <kevixw@gmail.com>
2017-04-04 18:08:07 -0700

    BUG-8156 Terminate PCEP session properly when ServerSessionManager is closed
    When the PCEP topology provider is loading the configuration initially,
    the ServerSessionManager will be restarted.  Any new PCEP connection
    coming in during this period will be rejected by the ServerSessionManager.
    
    This patch is to terminate these rejected PCEP session properly when above
    situation happens, so that the session can be established successfully
    in the next retry.
    
    Change-Id: Ifa3366c97025a0b31fb7d3ee50b1142f63a6209c
    Signed-off-by: Kevin Wang <kevixw@gmail.com>
    (cherry picked from commit 361265e3d13003bc3bfe75c888c40713ecbb36de)

--- pcep/api/src/main/java/org/opendaylight/protocol/pcep/PCEPSession.java
+++ pcep/api/src/main/java/org/opendaylight/protocol/pcep/PCEPSession.java
@@ -9,6 +9,8 @@ package org.opendaylight.protocol.pcep;
 
 import io.netty.util.concurrent.Future;
 import java.net.InetAddress;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.pcep.stats.rev141006.PcepSessionState;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.Message;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev131005.open.object.open.Tlvs;
@@ -31,7 +33,24 @@ public interface PCEPSession extends AutoCloseable, PcepSessionState {
      */
     Future<Void> sendMessage(Message message);
 
-    void close(TerminationReason reason);
+    /**
+     * Closes PCEP session, cancels all timers, returns to state Idle, sends the Close Message. KeepAlive and DeadTimer
+     * are cancelled if the state of the session changes to IDLE. This method is used to close the PCEP session from
+     * inside the session or from the listener, therefore the parent of this session should be informed.
+     * @param reason The {@link TerminationReason} to be wrapped in a PCEP CLOSE message and sent to the remote peer.
+     *               When the reason provided is null, no CLOSE message will be sent.
+     */
+    void close(@Nullable TerminationReason reason);
+
+    /**
+     * Terminate a PCEP session.  A CLOSE message with given reason will be sent to remote peer by invoking
+     * {@link #close(TerminationReason)} method.
+     *
+     * It triggers {@link PCEPSessionListener#onSessionTerminated(PCEPSession, PCEPTerminationReason)} after closing.
+     * @param reason The {@link TerminationReason} to be wrapped in a PCEP CLOSE message and sent to the remote peer.
+     *               The reason cannot be null.
+     */
+    void terminate(@Nonnull TerminationReason reason);
 
     Tlvs getRemoteTlvs();
 
