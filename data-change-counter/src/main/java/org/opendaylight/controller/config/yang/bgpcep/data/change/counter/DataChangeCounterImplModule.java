09d939541de17af3575d2bb38963490e7d399b8b
Dana Kutenicsova <dana.kutenics@gmail.com>
2017-04-25 17:16:06 +0200

    BUG-7027 : wired data-change-counter with Blueprint
    Deprecate existing config modules, add BP xml with custom config yang file.
    
    Change-Id: I2b0408310605c9b96388585582d2e7a9f63007cb
    Signed-off-by: Dana Kutenicsova <dana.kutenics@gmail.com>

--- data-change-counter/src/main/java/org/opendaylight/controller/config/yang/bgpcep/data/change/counter/DataChangeCounterImplModule.java
+++ data-change-counter/src/main/java/org/opendaylight/controller/config/yang/bgpcep/data/change/counter/DataChangeCounterImplModule.java
@@ -9,16 +9,12 @@
 package org.opendaylight.controller.config.yang.bgpcep.data.change.counter;
 
 import org.opendaylight.controller.config.api.JmxAttributeValidationException;
-import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.protocol.data.change.counter.TopologyDataChangeCounter;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
+/**
+ * @deprecated Replaced by blueprint wiring but remains for backwards compatibility until downstream users
+ *             of the provided config system service are converted to blueprint.
+ */
 public class DataChangeCounterImplModule extends org.opendaylight.controller.config.yang.bgpcep.data.change.counter.AbstractDataChangeCounterImplModule {
 
     public DataChangeCounterImplModule(final org.opendaylight.controller.config.api.ModuleIdentifier identifier,
@@ -41,30 +37,6 @@ public class DataChangeCounterImplModule extends org.opendaylight.controller.con
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        final TopologyDataChangeCounter counter = new TopologyDataChangeCounter(getDataProviderDependency(), getCounterId());
-        final InstanceIdentifier<Topology> topoIId = InstanceIdentifier.builder(NetworkTopology.class)
-                .child(Topology.class, new TopologyKey(new TopologyId(getTopologyName()))).build();
-        final ListenerRegistration<TopologyDataChangeCounter> registration = getDataProviderDependency().registerDataTreeChangeListener(
-            new DataTreeIdentifier<>(LogicalDatastoreType.OPERATIONAL, topoIId), counter);
-        return new DataChangeCounterCloseable(counter, registration);
+        return new TopologyDataChangeCounter(getDataProviderDependency(), getCounterId(), getTopologyName());
     }
-
-    private static final class DataChangeCounterCloseable implements AutoCloseable {
-
-        private final TopologyDataChangeCounter inner;
-        private final ListenerRegistration<TopologyDataChangeCounter> registration;
-
-        public DataChangeCounterCloseable(final TopologyDataChangeCounter inner,
-                final ListenerRegistration<TopologyDataChangeCounter> registration) {
-            this.inner = inner;
-            this.registration = registration;
-        }
-
-        @Override
-        public void close() {
-            this.registration.close();
-            this.inner.close();
-        }
-    }
-
 }
