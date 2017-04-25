292d2ee0611b82808571b31d5df1dc371a232874
Dana Kutenicsova <dana.kutenics@gmail.com>
2017-04-25 17:16:06 +0200

    BUG-7027 : wired data-change-counter with Blueprint
    Deprecate existing config modules, add BP xml with custom config yang file.
    
    Change-Id: I2b0408310605c9b96388585582d2e7a9f63007cb
    Signed-off-by: Dana Kutenicsova <dana.kutenics@gmail.com>

--- data-change-counter/src/main/java/org/opendaylight/protocol/data/change/counter/TopologyDataChangeCounter.java
+++ data-change-counter/src/main/java/org/opendaylight/protocol/data/change/counter/TopologyDataChangeCounter.java
@@ -10,9 +10,11 @@ package org.opendaylight.protocol.data.change.counter;
 
 import java.util.Collection;
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.opendaylight.controller.md.sal.binding.api.BindingTransactionChain;
 import org.opendaylight.controller.md.sal.binding.api.ClusteredDataTreeChangeListener;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
 import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncTransaction;
@@ -24,12 +26,16 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.data
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.data.change.counter.rev160315.data.change.counter.Counter;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.data.change.counter.rev160315.data.change.counter.CounterBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.data.change.counter.rev160315.data.change.counter.CounterKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TopologyDataChangeCounter implements ClusteredDataTreeChangeListener<Topology>, TransactionChainListener {
+public class TopologyDataChangeCounter implements ClusteredDataTreeChangeListener<Topology>, TransactionChainListener, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(TopologyDataChangeCounter.class);
 
@@ -38,15 +44,23 @@ public class TopologyDataChangeCounter implements ClusteredDataTreeChangeListene
     private final InstanceIdentifier<Counter> counterInstanceId;
     private final BindingTransactionChain chain;
     private final AtomicLong count;
+    private ListenerRegistration<TopologyDataChangeCounter> registration;
+    private final InstanceIdentifier<Topology> topoIId;
 
-    public TopologyDataChangeCounter(final DataBroker dataBroker, final String counterId) {
+    public TopologyDataChangeCounter(final DataBroker dataBroker, final String counterId, final String topologyName) {
         this.dataBroker = dataBroker;
         this.chain = this.dataBroker.createTransactionChain(this);
         this.counterId = counterId;
         this.counterInstanceId = InstanceIdentifier.builder(DataChangeCounter.class)
-                .child(Counter.class, new CounterKey(this.counterId)).build();
+            .child(Counter.class, new CounterKey(this.counterId)).build();
         this.count = new AtomicLong(0);
         putCount(this.count.get());
+        this.topoIId = InstanceIdentifier.builder(NetworkTopology.class)
+            .child(Topology.class, new TopologyKey(new TopologyId(topologyName))).build();
+    }
+
+    public void register() {
+        this.registration = dataBroker.registerDataTreeChangeListener(new DataTreeIdentifier<>(LogicalDatastoreType.OPERATIONAL, topoIId), this);
         LOG.debug("Data change counter {} initiated", this.counterId);
     }
 
@@ -56,7 +70,11 @@ public class TopologyDataChangeCounter implements ClusteredDataTreeChangeListene
         LOG.debug("Data change #{} for counter {}", this.count.get(), this.counterId);
     }
 
+    @Override
     public void close() {
+        if (this.registration != null) {
+            this.registration.close();
+        }
         final WriteTransaction wTx = this.dataBroker.newWriteOnlyTransaction();
         wTx.delete(LogicalDatastoreType.OPERATIONAL, this.counterInstanceId);
         try {
