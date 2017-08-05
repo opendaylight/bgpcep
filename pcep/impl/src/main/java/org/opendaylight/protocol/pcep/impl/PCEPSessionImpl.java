10286effe4969a0444c3387f3b60d99f419f2200
Dana Kutenicsova <dana.kutenics@gmail.com>
2017-08-06 01:00:49 +0200

    BUG-8156 : duplicate session up fixed
    In case of session up twice, the second session gets
    deleted from pcep and controller.
    
    Change-Id: I192a13e29ccb69bb869a9ed344d6f504d80100af
    Signed-off-by: Dana Kutenicsova <dana.kutenics@gmail.com>

--- pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/PCEPSessionImpl.java
+++ pcep/impl/src/main/java/org/opendaylight/protocol/pcep/impl/PCEPSessionImpl.java
@@ -25,6 +25,7 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.concurrent.GuardedBy;
 import org.opendaylight.protocol.pcep.PCEPCloseTermination;
 import org.opendaylight.protocol.pcep.PCEPSession;
 import org.opendaylight.protocol.pcep.PCEPSessionListener;
@@ -88,6 +89,7 @@ public class PCEPSessionImpl extends SimpleChannelInboundHandler<Message> implem
     private int maxUnknownMessages;
 
     // True if the listener should not be notified about events
+    @GuardedBy("this")
     private boolean closed = false;
 
     private final Channel channel;
@@ -206,12 +208,18 @@ public class PCEPSessionImpl extends SimpleChannelInboundHandler<Message> implem
         return this.channel.close();
     }
 
+    @VisibleForTesting
+    public synchronized boolean isClosed() {
+        return this.closed;
+    }
+
     /**
      * Closes PCEP session without sending a Close message, as the channel is no longer active.
      */
     @Override
-    public void close() {
-        LOG.info("Closing PCEP session: {}", this);
+    public synchronized void close() {
+        LOG.info("Closing PCEP session channel: {}", this);
+        this.closed = true;
         closeChannel();
     }
 
@@ -222,6 +230,10 @@ public class PCEPSessionImpl extends SimpleChannelInboundHandler<Message> implem
      */
     @Override
     public synchronized void close(final TerminationReason reason) {
+        if (this.closed) {
+            LOG.info("Session is already closed.");
+            return;
+        }
         LOG.info("Closing PCEP session: {}", this);
         this.closed = true;
         this.sendMessage(new CloseBuilder().setCCloseMessage(
@@ -247,6 +259,10 @@ public class PCEPSessionImpl extends SimpleChannelInboundHandler<Message> implem
     }
 
     private synchronized void terminate(final TerminationReason reason) {
+        if (this.closed) {
+            LOG.info("Session is already closed.");
+            return;
+        }
         LOG.info("Local PCEP session termination : {}", reason);
         this.listener.onSessionTerminated(this, new PCEPCloseTermination(reason));
         this.closed = true;
