d434c00633f38dabf482e398ee210ce4ad049f73
Dana Kutenicsova <dana.kutenics@gmail.com>
2017-08-06 01:00:49 +0200

    BUG-8156 : duplicate session up fixed
    In case of session up twice, the second session gets
    deleted from pcep and controller.
    
    Change-Id: I192a13e29ccb69bb869a9ed344d6f504d80100af
    Signed-off-by: Dana Kutenicsova <dana.kutenics@gmail.com>

--- pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/AbstractTopologySessionListener.java
+++ pcep/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/AbstractTopologySessionListener.java
@@ -251,6 +251,11 @@ public abstract class AbstractTopologySessionListener<S, L> implements PCEPSessi
         this.serverSessionManager.releaseNodeState(this.nodeState, session, isLspDbPersisted());
         this.nodeState = null;
         this.session = null;
+        try {
+            session.close();
+        } catch (Exception e) {
+            LOG.error("Session {} cannot be closed.", session, e);
+        }
         this.syncOptimization = null;
         unregister();
 
