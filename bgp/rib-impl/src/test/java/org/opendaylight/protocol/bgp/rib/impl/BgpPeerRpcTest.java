cae072b7664cd59ae076d6c70e7418341e75bff2
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2017-10-27 14:45:46 +0200

    BGPCEP-685: Add Peer Release session rpc
    old reset session rpc has been removed by
    removal of cfg models. Therefore create a new
    rpc providing the same service.
    Change-Id: If17936e78642b5718126e79b6c197209af5ed268
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

--- bgp/rib-impl/src/test/java/org/opendaylight/protocol/bgp/rib/impl/BgpPeerRpcTest.java
+++ bgp/rib-impl/src/test/java/org/opendaylight/protocol/bgp/rib/impl/BgpPeerRpcTest.java
@@ -9,17 +9,23 @@ package org.opendaylight.protocol.bgp.rib.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import com.google.common.util.concurrent.Futures;
 import io.netty.channel.ChannelFuture;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.peer.rpc.rev160322.PeerRef;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.peer.rpc.rev160322.RouteRefreshRequestInput;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.peer.rpc.rev160322.RouteRefreshRequestInputBuilder;
+import org.mockito.MockitoAnnotations;
+import org.opendaylight.protocol.bgp.rib.spi.PeerRPCs;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.peer.rpc.rev171027.PeerRef;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.peer.rpc.rev171027.ReleaseConnectionInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.peer.rpc.rev171027.ReleaseConnectionInputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.peer.rpc.rev171027.RouteRefreshRequestInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.peer.rpc.rev171027.RouteRefreshRequestInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev130925.rib.TablesKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv4AddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.Ipv6AddressFamily;
@@ -27,21 +33,25 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.type
 import org.opendaylight.yangtools.yang.binding.Notification;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 
-public class BgpPeerRpcTest {
-
-    private final Set<TablesKey> supportedFamilies = new HashSet<>();
-    private final BGPSessionImpl session = Mockito.mock(BGPSessionImpl.class);
-    private final BgpPeerRpc rpc = new BgpPeerRpc(this.session, this.supportedFamilies);
-    private final PeerRef peer = Mockito.mock(PeerRef.class);
-
-    private final ChannelOutputLimiter limiter = new ChannelOutputLimiter(this.session);
-    private final ChannelFuture future = Mockito.mock(ChannelFuture.class);
+public final class BgpPeerRpcTest {
+    @Mock
+    private BGPSessionImpl session;
+    @Mock
+    private PeerRPCs peerRpcs;
+    @Mock
+    private PeerRef peer;
+    @Mock
+    private ChannelFuture future;
+    private BgpPeerRpc rpc;
 
     @Before
     public void setUp() throws InterruptedException, ExecutionException {
-        this.supportedFamilies.add(new TablesKey(Ipv4AddressFamily.class, SubsequentAddressFamily.class));
+        MockitoAnnotations.initMocks(this);
+        this.rpc = new BgpPeerRpc(this.peerRpcs, this.session,
+                Collections.singleton(new TablesKey(Ipv4AddressFamily.class, SubsequentAddressFamily.class)));
+        final ChannelOutputLimiter limiter = new ChannelOutputLimiter(this.session);
 
-        Mockito.doReturn(this.limiter).when(this.session).getLimiter();
+        Mockito.doReturn(limiter).when(this.session).getLimiter();
         Mockito.doReturn(this.future).when(this.session).writeAndFlush(Mockito.any(Notification.class));
         Mockito.doReturn(true).when(this.future).isDone();
         Mockito.doReturn(null).when(this.future).get();
@@ -49,23 +59,33 @@ public class BgpPeerRpcTest {
     }
 
     @Test
-    public void testSuccessRequest() throws InterruptedException, ExecutionException {
+    public void testRouteRefreshRequestSuccessRequest() throws InterruptedException, ExecutionException {
         final RouteRefreshRequestInput input = new RouteRefreshRequestInputBuilder()
-            .setAfi(Ipv4AddressFamily.class)
-            .setSafi(SubsequentAddressFamily.class)
-            .setPeerRef(this.peer).build();
+                .setAfi(Ipv4AddressFamily.class)
+                .setSafi(SubsequentAddressFamily.class)
+                .setPeerRef(this.peer).build();
         final Future<RpcResult<Void>> result = this.rpc.routeRefreshRequest(input);
         assertTrue(result.get().getErrors().isEmpty());
     }
 
     @Test
-    public void testFailedRequest() throws InterruptedException, ExecutionException {
+    public void testRouteRefreshRequestFailedRequest() throws InterruptedException, ExecutionException {
         final RouteRefreshRequestInput input = new RouteRefreshRequestInputBuilder()
-            .setAfi(Ipv6AddressFamily.class)
-            .setSafi(SubsequentAddressFamily.class)
-            .setPeerRef(this.peer).build();
+                .setAfi(Ipv6AddressFamily.class)
+                .setSafi(SubsequentAddressFamily.class)
+                .setPeerRef(this.peer).build();
         final Future<RpcResult<Void>> result = this.rpc.routeRefreshRequest(input);
         assertEquals(1, result.get().getErrors().size());
-        assertEquals("Failed to send Route Refresh message due to unsupported address families.", result.get().getErrors().iterator().next().getMessage());
+        assertEquals("Failed to send Route Refresh message due to unsupported address families.",
+                result.get().getErrors().iterator().next().getMessage());
+    }
+
+    @Test
+    public void testResetSessionRequestSuccessRequest() throws InterruptedException, ExecutionException {
+        Mockito.doReturn(Futures.immediateFuture(null)).when(this.peerRpcs).releaseConnection();
+        final ReleaseConnectionInput input = new ReleaseConnectionInputBuilder()
+                .setPeerRef(this.peer).build();
+        final Future<RpcResult<Void>> result = this.rpc.releaseConnection(input);
+        assertTrue(result.get().getErrors().isEmpty());
     }
 }
