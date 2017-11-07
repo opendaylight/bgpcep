3dc4090f5d10b4af1448b271081513ce755a2240
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2017-11-07 11:31:58 +0100

    Remove deprecated methods under BMP API
    Change-Id: I6ffe7f210331f08539a260cb0871be77b7f1ec8a
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

--- bmp/bmp-spi/src/main/java/org/opendaylight/protocol/bmp/api/BmpDispatcher.java
+++ bmp/bmp-spi/src/main/java/org/opendaylight/protocol/bmp/api/BmpDispatcher.java
@@ -8,16 +8,15 @@
 
 package org.opendaylight.protocol.bmp.api;
 
-import com.google.common.base.Optional;
 import io.netty.channel.ChannelFuture;
 import java.net.InetSocketAddress;
+import javax.annotation.Nonnull;
 import org.opendaylight.protocol.concepts.KeyMapping;
 
 /**
  * Dispatcher class for creating servers and clients.
  */
 public interface BmpDispatcher extends AutoCloseable {
-
     /**
      * Creates server. Each server needs three factories to pass their instances to client sessions.
      *
@@ -26,39 +25,8 @@ public interface BmpDispatcher extends AutoCloseable {
      * @param keys    RFC2385 key mapping
      * @return instance of BmpServer
      */
-    @Deprecated
-    default ChannelFuture createServer(InetSocketAddress address, BmpSessionListenerFactory slf, Optional<KeyMapping> keys) {
-        if(keys.isPresent()) {
-            return createServer(address, slf, keys.get());
-        }
-        return createServer(address, slf, KeyMapping.getKeyMapping());
-    }
-
-     /**
-     * Creates reconnect clients. Make connection to all active monitored-routers.
-     *
-     * @param address bmp client to connect to
-     * @param slf     bmp session listener factory
-     * @param keys    RFC2385 key mapping
-     * @return        void
-     */
-     @Deprecated
-     default ChannelFuture createClient(InetSocketAddress address, BmpSessionListenerFactory slf, Optional<KeyMapping> keys) {
-         if(keys.isPresent()) {
-             return createClient(address, slf, keys.get());
-         }
-         return createClient(address, slf, KeyMapping.getKeyMapping());
-     }
-
-    /**
-     * Creates server. Each server needs three factories to pass their instances to client sessions.
-     *
-     * @param address to be bound with the server
-     * @param slf     bmp session listener factory
-     * @param keys    RFC2385 key mapping
-     * @return instance of BmpServer
-     */
-    ChannelFuture createServer(InetSocketAddress address, BmpSessionListenerFactory slf, KeyMapping keys);
+    ChannelFuture createServer(@Nonnull InetSocketAddress address, @Nonnull BmpSessionListenerFactory slf,
+            @Nonnull KeyMapping keys);
 
     /**
      * Creates reconnect clients. Make connection to all active monitored-routers.
@@ -66,7 +34,8 @@ public interface BmpDispatcher extends AutoCloseable {
      * @param address bmp client to connect to
      * @param slf     bmp session listener factory
      * @param keys    RFC2385 key mapping
-     * @return        void
+     * @return void
      */
-    ChannelFuture createClient(InetSocketAddress address, BmpSessionListenerFactory slf, KeyMapping keys) ;
+    ChannelFuture createClient(@Nonnull InetSocketAddress address, @Nonnull BmpSessionListenerFactory slf,
+            @Nonnull KeyMapping keys);
 }
