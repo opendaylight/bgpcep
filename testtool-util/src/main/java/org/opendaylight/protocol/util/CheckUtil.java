b44d1a8971a167acbf995f02b92d17d697139458
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2017-12-01 09:33:16 +0100

    Improve CheckUtil testss
    -enforce checkstyle
    -enforce findbug
    
    Change-Id: I9d3dd4dc6b6b6192d8015f0d9f11a4c7082e7297
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
    (cherry picked from commit 3fc26db07a4eadd2c6ed4d89b6beb42fce9cb568)

--- testtool-util/src/main/java/org/opendaylight/protocol/util/CheckUtil.java
+++ testtool-util/src/main/java/org/opendaylight/protocol/util/CheckUtil.java
@@ -10,6 +10,7 @@ package org.opendaylight.protocol.util;
 import static org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType.CONFIGURATION;
 import static org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType.OPERATIONAL;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
 import com.google.common.base.Stopwatch;
 import com.google.common.base.Verify;
@@ -43,21 +44,35 @@ public final class CheckUtil {
         Verify.verify(future.isSuccess());
     }
 
-    public static <R, T extends DataObject> R readDataOperational(final DataBroker dataBroker, final InstanceIdentifier<T> iid,
-        final Function<T, R> function) throws ReadFailedException {
-        return readData(dataBroker, OPERATIONAL, iid, function);
+    public static <R, T extends DataObject> R readDataOperational(final DataBroker dataBroker,
+            final InstanceIdentifier<T> iid, final Function<T, R> function) throws ReadFailedException {
+        return readDataOperational(dataBroker, iid, function, TIMEOUT);
     }
 
-    public static <R, T extends DataObject> R readDataConfiguration(final DataBroker dataBroker, final InstanceIdentifier<T> iid,
-        final Function<T, R> function) throws ReadFailedException {
-        return readData(dataBroker, CONFIGURATION, iid, function);
+    @VisibleForTesting
+    static <R, T extends DataObject> R readDataOperational(final DataBroker dataBroker,
+            final InstanceIdentifier<T> iid, final Function<T, R> function, final int timeout)
+            throws ReadFailedException {
+        return readData(dataBroker, OPERATIONAL, iid, function, timeout);
+    }
+
+    public static <R, T extends DataObject> R readDataConfiguration(final DataBroker dataBroker,
+            final InstanceIdentifier<T> iid, final Function<T, R> function) throws ReadFailedException {
+        return readDataConfiguration(dataBroker, iid, function, TIMEOUT);
+    }
+
+    @VisibleForTesting
+    static <R, T extends DataObject> R readDataConfiguration(final DataBroker dataBroker,
+            final InstanceIdentifier<T> iid, final Function<T, R> function, final int timeout) throws ReadFailedException {
+        return readData(dataBroker, CONFIGURATION, iid, function, timeout);
     }
 
     private static <R, T extends DataObject> R readData(final DataBroker dataBroker, final LogicalDatastoreType ldt,
-        final InstanceIdentifier<T> iid, final Function<T, R> function) throws ReadFailedException {
+            final InstanceIdentifier<T> iid, final Function<T, R> function, final int timeout)
+            throws ReadFailedException {
         AssertionError lastError = null;
         final Stopwatch sw = Stopwatch.createStarted();
-        while (sw.elapsed(TimeUnit.SECONDS) <= TIMEOUT) {
+        while (sw.elapsed(TimeUnit.SECONDS) <= timeout) {
             try (final ReadOnlyTransaction tx = dataBroker.newReadOnlyTransaction()) {
                 final Optional<T> data = tx.read(ldt, iid).checkedGet();
                 if (data.isPresent()) {
@@ -73,28 +88,28 @@ public final class CheckUtil {
         throw lastError;
     }
 
-    public static <T extends DataObject> T checkPresentOperational(final DataBroker dataBroker, final InstanceIdentifier<T> iid)
-        throws ReadFailedException {
-        return readData(dataBroker, OPERATIONAL, iid, bgpRib -> bgpRib);
+    public static <T extends DataObject> T checkPresentOperational(final DataBroker dataBroker,
+            final InstanceIdentifier<T> iid) throws ReadFailedException {
+        return readData(dataBroker, OPERATIONAL, iid, bgpRib -> bgpRib, TIMEOUT);
     }
 
     public static <T extends DataObject> T checkPresentConfiguration(final DataBroker dataBroker,
-        final InstanceIdentifier<T> iid) throws ReadFailedException {
-        return readData(dataBroker, CONFIGURATION, iid, bgpRib -> bgpRib);
+            final InstanceIdentifier<T> iid) throws ReadFailedException {
+        return readData(dataBroker, CONFIGURATION, iid, bgpRib -> bgpRib, TIMEOUT);
     }
 
     public static <T extends DataObject> void checkNotPresentOperational(final DataBroker dataBroker,
-        final InstanceIdentifier<T> iid) throws ReadFailedException {
+            final InstanceIdentifier<T> iid) throws ReadFailedException {
         checkNotPresent(dataBroker, OPERATIONAL, iid);
     }
 
     public static <T extends DataObject> void checkNotPresentConfiguration(final DataBroker dataBroker,
-        final InstanceIdentifier<T> iid) throws ReadFailedException {
+            final InstanceIdentifier<T> iid) throws ReadFailedException {
         checkNotPresent(dataBroker, CONFIGURATION, iid);
     }
 
     private static <T extends DataObject> void checkNotPresent(final DataBroker dataBroker,
-        final LogicalDatastoreType ldt, final InstanceIdentifier<T> iid) throws ReadFailedException {
+            final LogicalDatastoreType ldt, final InstanceIdentifier<T> iid) throws ReadFailedException {
         AssertionError lastError = null;
         final Stopwatch sw = Stopwatch.createStarted();
         while (sw.elapsed(TimeUnit.SECONDS) <= 10) {
@@ -113,9 +128,13 @@ public final class CheckUtil {
     }
 
     public static void checkEquals(final CheckEquals function) throws Exception {
+        checkEquals(function, TIMEOUT);
+    }
+
+    public static void checkEquals(final CheckEquals function, final int timeout) throws Exception {
         AssertionError lastError = null;
         final Stopwatch sw = Stopwatch.createStarted();
-        while (sw.elapsed(TimeUnit.SECONDS) <= TIMEOUT) {
+        while (sw.elapsed(TimeUnit.SECONDS) <= timeout) {
             try {
                 function.check();
                 return;
@@ -128,9 +147,15 @@ public final class CheckUtil {
     }
 
     public static void checkReceivedMessages(final ListenerCheck listener, final int numberOfMessages)
-        throws ReadFailedException {
+            throws ReadFailedException {
+        checkReceivedMessages(listener, numberOfMessages, TIMEOUT);
+    }
+
+    @VisibleForTesting
+    static void checkReceivedMessages(final ListenerCheck listener, final int numberOfMessages,
+            final int timeout) throws ReadFailedException {
         final Stopwatch sw = Stopwatch.createStarted();
-        while (sw.elapsed(TimeUnit.SECONDS) <= TIMEOUT) {
+        while (sw.elapsed(TimeUnit.SECONDS) <= timeout) {
             if (listener.getListMessageSize() != numberOfMessages) {
                 Uninterruptibles.sleepUninterruptibly(SLEEP_FOR, TimeUnit.MILLISECONDS);
             } else {
@@ -138,12 +163,13 @@ public final class CheckUtil {
             }
         }
         throw new AssertionError("Expected " + numberOfMessages + " but received "
-            + listener.getListMessageSize());
+                + listener.getListMessageSize());
     }
 
     public interface ListenerCheck {
         int getListMessageSize();
     }
+
     @FunctionalInterface
     public interface CheckEquals {
         void check() throws ExecutionException, InterruptedException;
