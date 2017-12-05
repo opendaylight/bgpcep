c557227f4904dbe5aae8fda8513e9d37d5d0fbb6
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2017-12-05 14:26:33 +0100

    Migrate deprecated netty methods
    Change-Id: I8b4b28c2f7d9acdd2ebd436be48260d3505f04af
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

--- pcep/pcc-mock/src/main/java/org/opendaylight/protocol/pcep/pcc/mock/protocol/PCCDispatcherImpl.java
+++ pcep/pcc-mock/src/main/java/org/opendaylight/protocol/pcep/pcc/mock/protocol/PCCDispatcherImpl.java
@@ -74,7 +74,7 @@ public final class PCCDispatcherImpl implements PCCDispatcher, AutoCloseable {
         setChannelFactory(b, optionalKey);
         b.option(ChannelOption.SO_KEEPALIVE, true);
         b.option(ChannelOption.SO_REUSEADDR, true);
-        b.option(ChannelOption.MAX_MESSAGES_PER_READ, 1);
+        b.option(ChannelOption.RCVBUF_ALLOCATOR, new io.netty.channel.FixedRecvByteBufAllocator(1));
         final long retryTimer = reconnectTime == -1 ? 0 : reconnectTime;
         final PCCReconnectPromise promise = new PCCReconnectPromise(remoteAddress, (int) retryTimer, CONNECT_TIMEOUT, b);
         final ChannelInitializer<SocketChannel> channelInitializer = new ChannelInitializer<SocketChannel>() {
