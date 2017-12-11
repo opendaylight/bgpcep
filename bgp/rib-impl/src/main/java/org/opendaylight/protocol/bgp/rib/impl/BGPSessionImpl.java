d6c6da5508a67fff59fbf5ddcd07d6497735de65
Kevin Wang <kevixw@gmail.com>
2017-12-11 13:58:17 -0800

    BGPCEP-724 Make BGP Session recoverable
    This patch make BGP Session recoverable when anything wrong
    happens during netty session negotiation. When an exception
    is captured, the BGP session will be terminated.
    
    Change-Id: Id3fc7ec282a2151e3bd6bdbfa8930e141b3ceaff
    Signed-off-by: Kevin Wang <kevixw@gmail.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/BGPSessionImpl.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/BGPSessionImpl.java
@@ -201,9 +201,11 @@ public class BGPSessionImpl extends SimpleChannelInboundHandler<Notification> im
 
     @Override
     public synchronized void close() {
-        if (this.state != State.IDLE && !this.terminationReasonNotified) {
-            this.writeAndFlush(new NotifyBuilder().setErrorCode(BGPError.CEASE.getCode())
-                    .setErrorSubcode(BGPError.CEASE.getSubcode()).build());
+        if (this.state != State.IDLE) {
+            if (!this.terminationReasonNotified) {
+                this.writeAndFlush(new NotifyBuilder().setErrorCode(BGPError.CEASE.getCode())
+                        .setErrorSubcode(BGPError.CEASE.getSubcode()).build());
+            }
             this.closeWithoutMessage();
         }
     }
@@ -304,14 +306,15 @@ public class BGPSessionImpl extends SimpleChannelInboundHandler<Notification> im
         return this.channel.newFailedFuture(new NonWritableChannelException());
     }
 
-    private synchronized void closeWithoutMessage() {
+    @VisibleForTesting
+    synchronized void closeWithoutMessage() {
         if (this.state == State.IDLE) {
             return;
         }
         LOG.info("Closing session: {}", this);
         this.channel.close().addListener((ChannelFutureListener) future -> Preconditions.checkArgument(future.isSuccess(), "Channel failed to close: %s", future.cause()));
-        this.state = State.IDLE;
         removePeerSession();
+        this.state = State.IDLE;
         this.sessionState.setSessionState(this.state);
     }
 
@@ -409,9 +412,16 @@ public class BGPSessionImpl extends SimpleChannelInboundHandler<Notification> im
 
     @VisibleForTesting
     synchronized void sessionUp() {
+        // MUST set the state to up first, otherwise close() won't be invoked later when exception
+        // is thrown
         this.state = State.UP;
-        this.sessionState.setSessionState(this.state);
-        this.listener.onSessionUp(this);
+        try {
+            this.sessionState.setSessionState(this.state);
+            this.listener.onSessionUp(this);
+        } catch (final Exception e) {
+            handleException(e);
+            throw e;
+        }
     }
 
     public synchronized State getState() {
@@ -461,7 +471,16 @@ public class BGPSessionImpl extends SimpleChannelInboundHandler<Notification> im
 
     @Override
     public synchronized void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
-        LOG.warn("BGP session encountered error", cause);
+        handleException(cause);
+    }
+
+    /**
+     * Handle exception occurred in the PCEP session. The session in error state should be closed
+     * properly so that it can be restored later.
+     */
+    @VisibleForTesting
+    void handleException(final Throwable cause) {
+        LOG.warn("BGP session {} encountered error", this, cause);
         if (cause.getCause() instanceof BGPDocumentedException) {
             this.terminate((BGPDocumentedException) cause.getCause());
         } else {
