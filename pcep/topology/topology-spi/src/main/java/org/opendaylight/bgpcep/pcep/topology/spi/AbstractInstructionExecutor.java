c64f71165f29881c7258778c0d3a9d4b1027293e
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2017-12-11 12:38:34 +0100

    Enforce checkstyle and findbug for PCEP
    Topology modules
    
    - api
    - spi
    
    Change-Id: Ib1d2092520aab6fca58dbbbc022bee9d864d956f
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

--- pcep/topology/topology-spi/src/main/java/org/opendaylight/bgpcep/pcep/topology/spi/AbstractInstructionExecutor.java
+++ pcep/topology/topology-spi/src/main/java/org/opendaylight/bgpcep/pcep/topology/spi/AbstractInstructionExecutor.java
@@ -13,6 +13,7 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
+import javax.annotation.Nonnull;
 import org.opendaylight.bgpcep.programming.spi.Instruction;
 import org.opendaylight.bgpcep.programming.spi.InstructionScheduler;
 import org.opendaylight.bgpcep.programming.spi.SchedulerException;
@@ -26,27 +27,66 @@ import org.slf4j.LoggerFactory;
 
 public abstract class AbstractInstructionExecutor implements FutureCallback<Instruction> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractInstructionExecutor.class);
+
+    private final SubmitInstructionInput input;
+
+    protected AbstractInstructionExecutor(@Nonnull final SubmitInstructionInput input) {
+        this.input = requireNonNull(input);
+    }
+
+    public static FailureCase schedule(final InstructionScheduler scheduler, final AbstractInstructionExecutor fwd) {
+        final ListenableFuture<Instruction> listenableFuture;
+        try {
+            listenableFuture = scheduler.scheduleInstruction(fwd.getInput());
+        } catch (final SchedulerException e) {
+            LOG.info("Instuction {} failed to schedule", e.getMessage(), e);
+            return new FailureCaseBuilder().setFailure(e.getFailure()).build();
+        }
+        Futures.addCallback(listenableFuture, fwd, MoreExecutors.directExecutor());
+        return null;
+    }
+
+    public final SubmitInstructionInput getInput() {
+        return this.input;
+    }
+
+    protected abstract ListenableFuture<OperationResult> invokeOperation();
+
+    @Override
+    public void onSuccess(final Instruction insn) {
+        if (insn != null && insn.checkedExecutionStart()) {
+            final ListenableFuture<OperationResult> listenableFuture = invokeOperation();
+            Futures.addCallback(listenableFuture, new InstructionCallback(insn), MoreExecutors.directExecutor());
+        }
+    }
+
+    @Override
+    public void onFailure(final Throwable throwable) {
+        LOG.debug("Instruction {} cancelled", this.input, throwable);
+    }
+
     private static final class InstructionCallback implements FutureCallback<OperationResult> {
 
         private final Instruction insn;
 
-        public InstructionCallback(final Instruction insn) {
-            this.insn = insn;
+        InstructionCallback(@Nonnull final Instruction insn) {
+            this.insn = requireNonNull(insn);
         }
 
         @Override
         public void onSuccess(final OperationResult result) {
-            if (result.getFailure() != null) {
+            if (result != null && result.getFailure() != null) {
                 switch (result.getFailure()) {
-                case Failed:
-                case NoAck:
-                    this.insn.executionCompleted(InstructionStatus.Failed, null);
-                    break;
-                case Unsent:
-                    this.insn.executionCompleted(InstructionStatus.Cancelled, null);
-                    break;
-                default:
-                    break;
+                    case Failed:
+                    case NoAck:
+                        this.insn.executionCompleted(InstructionStatus.Failed, null);
+                        break;
+                    case Unsent:
+                        this.insn.executionCompleted(InstructionStatus.Cancelled, null);
+                        break;
+                    default:
+                        break;
                 }
             } else {
                 this.insn.executionCompleted(InstructionStatus.Successful, null);
@@ -54,46 +94,8 @@ public abstract class AbstractInstructionExecutor implements FutureCallback<Inst
         }
 
         @Override
-        public void onFailure(final Throwable t) {
+        public void onFailure(final Throwable throwable) {
             this.insn.executionCompleted(InstructionStatus.Failed, null);
         }
-    }
-
-    private static final Logger LOG = LoggerFactory.getLogger(AbstractInstructionExecutor.class);
-    private final SubmitInstructionInput input;
-
-    protected AbstractInstructionExecutor(final SubmitInstructionInput input) {
-        this.input = requireNonNull(input);
-    }
-
-    public final SubmitInstructionInput getInput() {
-        return this.input;
-    }
-
-    public static FailureCase schedule(final InstructionScheduler scheduler, final AbstractInstructionExecutor fwd) {
-        final ListenableFuture<Instruction> s;
-        try {
-            s = scheduler.scheduleInstruction(fwd.getInput());
-        } catch (final SchedulerException e) {
-            LOG.info("Instuction {} failed to schedule", e.getMessage(), e);
-            return new FailureCaseBuilder().setFailure(e.getFailure()).build();
-        }
-        Futures.addCallback(s, fwd, MoreExecutors.directExecutor());
-        return null;
-    }
-
-    protected abstract ListenableFuture<OperationResult> invokeOperation();
-
-    @Override
-    public void onSuccess(final Instruction insn) {
-        if (insn.checkedExecutionStart()) {
-            final ListenableFuture<OperationResult> s = invokeOperation();
-            Futures.addCallback(s, new InstructionCallback(insn), MoreExecutors.directExecutor());
-        }
-    }
-
-    @Override
-    public void onFailure(final Throwable t) {
-        LOG.debug("Instruction {} cancelled", this.input, t);
     }
 }
--- pcep/topology/topology-spi/src/main/java/org/opendaylight/bgpcep/pcep/topology/spi/stats/TopologySessionStatsRegistry.java
+++ pcep/topology/topology-spi/src/main/java/org/opendaylight/bgpcep/pcep/topology/spi/stats/TopologySessionStatsRegistry.java
@@ -8,6 +8,7 @@
 
 package org.opendaylight.bgpcep.pcep.topology.spi.stats;
 
+import javax.annotation.Nonnull;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.stats.rev171113.PcepSessionState;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
@@ -18,17 +19,17 @@ import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
  */
 public interface TopologySessionStatsRegistry {
     /**
-     * Register session to Session stats Registry handler
+     * Register session to Session stats Registry handler.
      *
      * @param nodeId       Identifier of the topology node where it will be stored session stats under DS
      * @param sessionState containing all Stats Session information
      */
-    void bind(final KeyedInstanceIdentifier<Node, NodeKey> nodeId, PcepSessionState sessionState);
+    void bind(@Nonnull KeyedInstanceIdentifier<Node, NodeKey> nodeId, @Nonnull PcepSessionState sessionState);
 
     /**
-     * Unregister Node from Stats Registry handler
+     * Unregister Node from Stats Registry handler.
      *
      * @param nodeId Identifier of the topology node to be removed from registry
      */
-    void unbind(final KeyedInstanceIdentifier<Node, NodeKey> nodeId);
+    void unbind(@Nonnull KeyedInstanceIdentifier<Node, NodeKey> nodeId);
 }
