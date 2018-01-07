13b85eb3d23ecf7a37d8bd491781e320b5f21e09
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-01-07 22:35:25 +0100

    Reduce number of parameters for path selection
    -enforce findbug.
    
    Change-Id: Iff9c6c8296c4ff08f8e2b80b18b85212f93dc094
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/entry/RouteEntryDependenciesContainer.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.spi.entry;
+
+import javax.annotation.Nonnull;
+import org.opendaylight.protocol.bgp.rib.spi.ExportPolicyPeerTracker;
+import org.opendaylight.protocol.bgp.rib.spi.RIBSupport;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev171207.rib.TablesKey;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+/**
+ * Container wrapper for all dependencies related to Route Entry, required for process and storage.
+ */
+public interface RouteEntryDependenciesContainer {
+    /**
+     * Returns rib support.
+     *
+     * @return RIBSupport
+     */
+    @Nonnull
+    RIBSupport getRibSupport();
+
+
+    /**
+     * Returns the table key(AFI/SAFI) corresponding to the Route Entry.
+     *
+     * @return TablesKey
+     */
+    @Nonnull
+    TablesKey getLocalTablesKey();
+
+
+    /**
+     * Returns the loc-rib table to be updated and to which  corresponds this Route Entry.
+     *
+     * @return YangInstanceIdentifier containing the path to loc-rib table.
+     */
+    @Nonnull
+    YangInstanceIdentifier getLocRibTableTarget();
+
+    /**
+     * Return the ExportPolicyPeerTracker which tracks peers.
+     *
+     * @return ExportPolicyPeerTracker
+     */
+    @Nonnull
+    ExportPolicyPeerTracker getExportPolicyPeerTracker();
+}
