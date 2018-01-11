d8da2e1e14ffd323d1275b2f216f65c539be64ad
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-01-11 13:06:36 +0100

    BUG-6966: Openconfig Routing Policy SPI
    Interfaces for
    
    - OpenconfigRIBRoutingPolicy Consumer
      Creates RIB RP from Openconfig config
    - BGPStatement Provider/Consumer
      Registry per (Condition/Action)
      Openconfig Statement Handlers.
      Expose Handlers to Consumers
    
    Implementation of
    
    - Openconfig Statement handlers registry
    
    Feature creation
    
    Change-Id: I11f68a97ae001537e1a6cf5a092eea7c0848cc2f
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ bgp/openconfig-rp-spi/src/main/java/org/opendaylight/protocol/bgp/openconfig/routing/policy/spi/registry/StatementRegistry.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.registry;
+
+import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.PolicyRIBBaseParameters;
+import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.policy.action.ActionPolicy;
+import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.policy.action.IgpActionPolicy;
+import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.policy.condition.ConditionPolicy;
+import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.policy.condition.IGPConditionPolicy;
+import org.opendaylight.protocol.bgp.rib.spi.policy.BGPRouteEntryExportParameters;
+import org.opendaylight.protocol.bgp.rib.spi.policy.BGPRouteEntryImportParameters;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.routing.policy.rev151009.igp.actions.IgpActions;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.routing.policy.rev151009.igp.conditions.IgpConditions;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.routing.policy.rev151009.routing.policy.top.routing.policy.policy.definitions.policy.definition.statements.Statement;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.routing.policy.rev151009.routing.policy.top.routing.policy.policy.definitions.policy.definition.statements.statement.Actions;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.routing.policy.rev151009.routing.policy.top.routing.policy.policy.definitions.policy.definition.statements.statement.Conditions;
+import org.opendaylight.yangtools.concepts.AbstractRegistration;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+
+public final class StatementRegistry implements StatementRegistryConsumer, StatementRegistryProvider {
+    private final ConditionsRegistryImpl conditionsRegistry;
+    private final ActionsRegistryImpl actionsRegistry;
+
+    public StatementRegistry() {
+        this.conditionsRegistry = new ConditionsRegistryImpl();
+        this.actionsRegistry = new ActionsRegistryImpl();
+    }
+
+    @Override
+    public RouteAttributeContainer applyExportStatement(
+            final OpenconfigPolicyConsumer policyConsumer,
+            final PolicyRIBBaseParameters basePolicyParameters,
+            final BGPRouteEntryExportParameters routeEntryExportParameters,
+            final RouteAttributeContainer attributes,
+            final Statement statement) {
+        final Actions actions = statement.getActions();
+        if (!this.conditionsRegistry.matchExportConditions(
+                policyConsumer,
+                basePolicyParameters,
+                routeEntryExportParameters,
+                attributes.getAttributes(),
+                statement.getConditions())) {
+            return attributes;
+        }
+        return new RouteAttributeContainer(this.actionsRegistry.applyExportAction(
+                basePolicyParameters,
+                routeEntryExportParameters,
+                attributes.getAttributes(),
+                actions),
+                true);
+    }
+
+    @Override
+    public RouteAttributeContainer applyImportStatement(
+            final OpenconfigPolicyConsumer policyConsumer,
+            final PolicyRIBBaseParameters basePolicyParameters,
+            final BGPRouteEntryImportParameters routeEntryImportParameters,
+            final RouteAttributeContainer attributes,
+            final Statement statement) {
+        final ContainerNode att = attributes.getAttributes();
+        if (att == null || !this.conditionsRegistry.matchImportConditions(
+                policyConsumer,
+                basePolicyParameters,
+                routeEntryImportParameters,
+                attributes.getAttributes(),
+                statement.getConditions())) {
+            return attributes;
+        }
+        return new RouteAttributeContainer(this.actionsRegistry.applyImportAction(
+                basePolicyParameters,
+                routeEntryImportParameters,
+                att,
+                statement.getActions()),
+                true);
+    }
+
+    @Override
+    public AbstractRegistration registerConditionPolicy(
+            final Class<? extends Augmentation<Conditions>> conditionPolicyClass,
+            final ConditionPolicy conditionPolicy) {
+        return this.conditionsRegistry.registerConditionPolicy(conditionPolicyClass, conditionPolicy);
+    }
+
+    @Override
+    public AbstractRegistration registerIGPConditionPolicy(
+            final Class<? extends Augmentation<IgpConditions>> igpConditionClass,
+            final IGPConditionPolicy igpConditionPolicy) {
+        return this.conditionsRegistry.registerIGPConditionPolicy(igpConditionClass, igpConditionPolicy);
+    }
+
+    @Override
+    public AbstractRegistration registerActionPolicy(
+            final Class<? extends Augmentation<Actions>> actionPolicyClass,
+            final ActionPolicy actionPolicy) {
+        return this.actionsRegistry.registerActionPolicy(actionPolicyClass, actionPolicy);
+    }
+
+    @Override
+    public AbstractRegistration registerIGPActionPolicy(
+            final Class<? extends Augmentation<IgpActions>> igpActionPolicyClass,
+            final IgpActionPolicy igpActionPolicy) {
+        return this.actionsRegistry.registerIGPActionPolicy(igpActionPolicyClass, igpActionPolicy);
+    }
+}
