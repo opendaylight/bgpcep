1b9c28d0d92e943399d51a33f4f95369afc43e89
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-01-11 22:23:24 +0100

    BUG-6966: Openconfig policy config file loader.
    Change-Id: Iac74537d39f41970472018520a7c1a0144333f3a
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ config-loader/routing-policy-config-loader/src/main/java/org/opendaylight/bgpcep/config/loader/routing/policy/OpenconfigRoutingPolicyLoader.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.bgpcep.config.loader.routing.policy;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.util.concurrent.ExecutionException;
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.GuardedBy;
+import org.opendaylight.bgpcep.config.loader.spi.ConfigFileProcessor;
+import org.opendaylight.bgpcep.config.loader.spi.ConfigLoader;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.routing.policy.rev151009.routing.policy.top.RoutingPolicy;
+import org.opendaylight.yangtools.concepts.AbstractRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.model.api.SchemaPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class OpenconfigRoutingPolicyLoader implements ConfigFileProcessor, AutoCloseable {
+    @VisibleForTesting
+    static final InstanceIdentifier<RoutingPolicy> ROUTING_POLICY_IID = InstanceIdentifier.create(RoutingPolicy.class);
+    private static final Logger LOG = LoggerFactory.getLogger(OpenconfigRoutingPolicyLoader.class);
+    private static final SchemaPath POLICY_SCHEMA_PATH = SchemaPath.create(true, RoutingPolicy.QNAME);
+    private final BindingNormalizedNodeSerializer bindingSerializer;
+    private final ConfigLoader configLoader;
+    private final DataBroker dataBroker;
+    private final YangInstanceIdentifier routingPolicyYiid;
+    @GuardedBy("this")
+    private AbstractRegistration registration;
+
+    public OpenconfigRoutingPolicyLoader(final ConfigLoader configLoader, final DataBroker dataBroker) {
+        requireNonNull(configLoader);
+        this.configLoader = requireNonNull(configLoader);
+        this.dataBroker = requireNonNull(dataBroker);
+        this.bindingSerializer = configLoader.getBindingNormalizedNodeSerializer();
+        this.routingPolicyYiid = this.bindingSerializer.toYangInstanceIdentifier(ROUTING_POLICY_IID);
+    }
+
+    public synchronized void init() {
+        this.registration = this.configLoader.registerConfigFile(this);
+        LOG.info("Routing Policy Config Loader service initiated");
+    }
+
+    @Override
+    public void close() {
+        if (this.registration != null) {
+            this.registration.close();
+            this.registration = null;
+        }
+    }
+
+    @Override
+    public SchemaPath getSchemaPath() {
+        return POLICY_SCHEMA_PATH;
+    }
+
+    @Override
+    public synchronized void loadConfiguration(@Nonnull final NormalizedNode<?, ?> dto) {
+        final RoutingPolicy routingPolicy = (RoutingPolicy) this.bindingSerializer
+                .fromNormalizedNode(this.routingPolicyYiid, dto).getValue();
+        final WriteTransaction wtx = this.dataBroker.newWriteOnlyTransaction();
+        wtx.merge(LogicalDatastoreType.CONFIGURATION, ROUTING_POLICY_IID, routingPolicy,
+                WriteTransaction.CREATE_MISSING_PARENTS);
+
+        try {
+            wtx.submit().get();
+        } catch (final ExecutionException | InterruptedException e) {
+            LOG.warn("Failed to create Routing Policy config", e);
+        }
+    }
+}
---
+++ config-loader/routing-policy-config-loader/src/test/java/org/opendaylight/bgpcep/config/loader/routing/policy/AbstractOpenconfigRoutingPolicyLoaderTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.config.loader.routing.policy;
+
+import static org.junit.Assert.assertNotNull;
+import static org.opendaylight.bgpcep.config.loader.routing.policy.OpenconfigRoutingPolicyLoader.ROUTING_POLICY_IID;
+import static org.opendaylight.protocol.util.CheckUtil.checkNotPresentConfiguration;
+import static org.opendaylight.protocol.util.CheckUtil.checkPresentConfiguration;
+
+import org.junit.After;
+import org.junit.Before;
+import org.opendaylight.bgpcep.config.loader.impl.AbstractConfigLoader;
+
+public class AbstractOpenconfigRoutingPolicyLoaderTest extends AbstractConfigLoader {
+    OpenconfigRoutingPolicyLoader policyLoader;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        checkNotPresentConfiguration(getDataBroker(), ROUTING_POLICY_IID);
+        assertNotNull(ClassLoader.getSystemClassLoader().getResource("initial/routing-policy-config.xml"));
+        this.policyLoader = new OpenconfigRoutingPolicyLoader(this.configLoader, getDataBroker());
+        this.policyLoader.init();
+        checkPresentConfiguration(getDataBroker(), ROUTING_POLICY_IID);
+        this.policyLoader.close();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
