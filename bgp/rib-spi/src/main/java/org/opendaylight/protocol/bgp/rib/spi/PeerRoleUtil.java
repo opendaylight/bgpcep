02ec31ff1c1a3b4de683c789f2e0fcf6613d68a8
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-01-12 12:26:26 +0100

    Remove no longer required method
    Change-Id: Ic987e86f09cff5df35f81c13a7385866727f30ed
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

--- bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/PeerRoleUtil.java
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/PeerRoleUtil.java
@@ -8,29 +8,12 @@
 
 package org.opendaylight.protocol.bgp.rib.spi;
 
-import com.google.common.base.Optional;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev171207.PeerRole;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev171207.bgp.rib.rib.Peer;
-import org.opendaylight.yangtools.yang.binding.BindingMapping;
-import org.opendaylight.yangtools.yang.common.QName;
-import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier.NodeIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
-import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
 
 public final class PeerRoleUtil {
-    public static final NodeIdentifier PEER_ROLE_NID = new NodeIdentifier(QName.create(Peer.QNAME,
-            "peer-role").intern());
 
     private PeerRoleUtil() {
         throw new UnsupportedOperationException();
-    }
-
-    public static PeerRole roleForChange(final Optional<NormalizedNode<?, ?>> maybePeerRole) {
-        if (maybePeerRole.isPresent()) {
-            final LeafNode<?> peerRoleLeaf = (LeafNode<?>) maybePeerRole.get();
-            return PeerRole.valueOf(BindingMapping.getClassName((String) peerRoleLeaf.getValue()));
-        }
-        return null;
     }
 
     public static String roleForString(final PeerRole role) {
