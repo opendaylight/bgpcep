ebd0b77a63a172a2c4efca019c016fceb17ca37c
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-01-17 17:05:58 +0100

    BUG-6966: Introduce BGPPeerTracker interfaces
    as replacement for static Export group
    
    Change-Id: Ifcc1253873d5d05ecbec632f41c8abee3cc1f721
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/PeerTrackerInformation.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.spi;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev171207.SendReceive;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev171207.PeerId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev171207.PeerRole;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev171207.rib.TablesKey;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+
+/**
+ * Exposes information required from peer to PeerTracker.
+ */
+public interface PeerTrackerInformation {
+
+    /**
+     * Returns PeerID
+     *
+     * @return PeerID
+     */
+    @Nonnull
+    PeerId getPeerId();
+
+    /**
+     * Returns if peer supports Additional Path for specific table.
+     *
+     * @param tableKey table
+     * @return true if Additional Path is supported for defined table
+     */
+    default boolean supportsAddPathSupported(@Nonnull TablesKey tableKey) {
+        final SendReceive sendReceive = getSupportedAddPathTables(tableKey);
+        return sendReceive != null && (sendReceive.equals(SendReceive.Both) || sendReceive.equals(SendReceive.Receive));
+    }
+
+    /**
+     * Returns AddPath support configuration if supported, otherwise null.
+     *
+     * @param tableKey table
+     * @return AddPath support configuration if supported, otherwise null
+     */
+    @Nullable
+    SendReceive getSupportedAddPathTables(@Nonnull TablesKey tableKey);
+
+    /**
+     * Returns if peer supports table.
+     *
+     * @param tableKey table
+     * @return true if Additional Path is supported for defined table
+     */
+    boolean supportsTable(@Nonnull TablesKey tableKey);
+
+    /**
+     * Returns YangInstanceIdentifier pointing peer under specific rib.
+     *
+     * @return Peer YangInstanceIdentifier
+     */
+    @Nullable
+    YangInstanceIdentifier getPeerRibInstanceIdentifier();
+
+    /**
+     * Returns Peer Role.
+     *
+     * @return PeerRole
+     */
+    @Nonnull
+    PeerRole getRole();
+}
