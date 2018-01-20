3bbce940bec60685f75892dff148617a36a953f9
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-01-20 13:08:18 +0100

    BGPCEP-672: Fix key storage un adj-rib-out
    list key must not be null, therefore
    path-id should always be included.
    Reserving PATH-ID 0 for non-supporting
    add-path.
    
    Change-Id: I9b0af92e91e7c0b1c7281230a804c7adeff2dbca
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/LocRibWriter.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/LocRibWriter.java
@@ -192,6 +192,9 @@ final class LocRibWriter implements AutoCloseable, TotalPrefixesCounter, TotalPa
         if (!table.getDataBefore().isPresent() && this.exportPolicyPeerTracker.isTableSupported(peerIdOfNewPeer)) {
             this.exportPolicyPeerTracker.registerPeerAsInitialized(peerIdOfNewPeer);
             LOG.debug("Peer {} table has been created, inserting existent routes", peerIdOfNewPeer);
+            if (this.routeEntries.isEmpty()) {
+                return;
+            }
             final PeerRole newPeerRole = this.exportPolicyPeerTracker.getRole(IdentifierUtils.peerPath(rootPath));
             final PeerExportGroup peerGroup = this.exportPolicyPeerTracker.getPeerGroup(newPeerRole);
             this.routeEntries.entrySet().forEach(entry -> entry.getValue().writeRoute(peerIdOfNewPeer, entry.getKey(),
