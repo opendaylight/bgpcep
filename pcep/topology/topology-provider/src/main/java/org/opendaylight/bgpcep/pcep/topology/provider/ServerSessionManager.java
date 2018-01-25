9f643da52142b2ae78336bb7acf85da4f6a478d0
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-01-25 11:31:32 +0100

    BGPCEP-739: Fix  "raced with transaction PingPongTransaction"
    under TopologyNodeState. Raced call to putTopologyNode
    will cause an override node and therefore PathComputationClient
    was being removed and ending on exception when trying to update
    it after such change.
    
    Change-Id: I1d5b44c383d7159669bec7475afff3713f1370a0
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

--- pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/ServerSessionManager.java
+++ pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/ServerSessionManager.java
@@ -19,6 +19,7 @@ import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import javax.annotation.concurrent.GuardedBy;
@@ -98,7 +99,7 @@ final class ServerSessionManager implements PCEPSessionListenerFactory, Topology
     /**
      * Create Base Topology.
      */
-    synchronized ListenableFuture<Void> instantiateServiceInstance() {
+    synchronized void instantiateServiceInstance() {
         final TopologyKey key = InstanceIdentifier.keyOf(this.topology);
         final TopologyId topologyId = key.getTopologyId();
         final WriteTransaction tx = this.dependenciesProvider.getDataBroker().newWriteOnlyTransaction();
@@ -107,21 +108,16 @@ final class ServerSessionManager implements PCEPSessionListenerFactory, Topology
                         .addAugmentation(TopologyTypes1.class, new TopologyTypes1Builder().setTopologyPcep(
                                 new TopologyPcepBuilder().build()).build()).build())
                 .setNode(new ArrayList<>()).build(), true);
-        final ListenableFuture<Void> future = tx.submit();
-        Futures.addCallback(future, new FutureCallback<Void>() {
-            @Override
-            public void onSuccess(final Void result) {
-                LOG.debug("PCEP Topology {} created successfully.", topologyId.getValue());
-                ServerSessionManager.this.isClosed.set(false);
-            }
+        LOG.trace("Create PCEP Topology {}", topologyId.getValue());
 
-            @Override
-            public void onFailure(final Throwable throwable) {
-                LOG.error("Failed to create PCEP Topology {}.", topologyId.getValue(), throwable);
-                ServerSessionManager.this.isClosed.set(true);
-            }
-        }, MoreExecutors.directExecutor());
-        return future;
+        try {
+            tx.submit().get();
+            LOG.info("PCEP Topology {} created successfully.", topologyId.getValue());
+            ServerSessionManager.this.isClosed.set(false);
+        } catch (final ExecutionException | InterruptedException throwable) {
+            LOG.error("Failed to create PCEP Topology {}.", topologyId.getValue(), throwable);
+            ServerSessionManager.this.isClosed.set(true);
+        }
     }
 
     synchronized void releaseNodeState(final TopologyNodeState nodeState, final PCEPSession session,
@@ -147,6 +143,13 @@ final class ServerSessionManager implements PCEPSessionListenerFactory, Topology
         }
 
         LOG.debug("Node {} requested by listener {}", id, sessionListener);
+        // if another listener requests the same session, close it
+        final TopologySessionListener existingSessionListener = this.nodes.get(id);
+        if (existingSessionListener != null && !sessionListener.equals(existingSessionListener)) {
+            LOG.error("New session listener {} is in conflict with existing session listener {} on node {},"
+                    + " closing the existing one.", existingSessionListener, sessionListener, id);
+            existingSessionListener.close();
+        }
         TopologyNodeState ret = this.state.get(id);
 
         if (ret == null) {
@@ -154,13 +157,6 @@ final class ServerSessionManager implements PCEPSessionListenerFactory, Topology
                     DEFAULT_HOLD_STATE_NANOS);
             LOG.debug("Created topology node {} for id {} at {}", ret, id, ret.getNodeId());
             this.state.put(id, ret);
-        }
-        // if another listener requests the same session, close it
-        final TopologySessionListener existingSessionListener = this.nodes.get(id);
-        if (existingSessionListener != null && !sessionListener.equals(existingSessionListener)) {
-            LOG.error("New session listener {} is in conflict with existing session listener {} on node {},"
-                    + " closing the existing one.", existingSessionListener, sessionListener, id);
-            existingSessionListener.close();
         }
         ret.taken(retrieveNode);
         this.nodes.put(id, sessionListener);
