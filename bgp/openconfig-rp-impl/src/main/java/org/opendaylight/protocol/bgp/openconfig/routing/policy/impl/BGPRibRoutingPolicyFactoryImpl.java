cff234409659f562e675257e71e299624eb07058
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-01-31 16:53:27 +0100

    BGPCEP-754: Openconfig Routing Policy Impl
    Implementation of:
    - BGP Openconfig Rib Policy Service on charge
      of build BGPRIBPolicy from Openconfig Rib
      Policy configuration.
    - BGP Openconfig Routing Policy Registry.
      Listens for Openconfig RP configurations and
      keeps track of defined policies.
    
    Change-Id: Id5521811a23b65ad1ae5e4e20eb41a61d78beb95
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ bgp/openconfig-rp-impl/src/main/java/org/opendaylight/protocol/bgp/openconfig/routing/policy/impl/BGPRibRoutingPolicyFactoryImpl.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.openconfig.routing.policy.impl;
+
+import static java.util.Objects.requireNonNull;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.BGPRibRoutingPolicyFactory;
+import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.registry.StatementRegistryConsumer;
+import org.opendaylight.protocol.bgp.rib.spi.policy.BGPRibRoutingPolicy;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.routing.policy.rev151009.apply.policy.group.apply.policy.Config;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.ClusterIdentifier;
+
+public final class BGPRibRoutingPolicyFactoryImpl implements BGPRibRoutingPolicyFactory {
+    private final StatementRegistryConsumer statementRegistryConsumer;
+    private final DataBroker databroker;
+
+    public BGPRibRoutingPolicyFactoryImpl(
+            final DataBroker databroker,
+            final StatementRegistryConsumer statementRegistryConsumer) {
+        this.databroker = requireNonNull(databroker);
+        this.statementRegistryConsumer = requireNonNull(statementRegistryConsumer);
+    }
+
+    @Override
+    public BGPRibRoutingPolicy buildBGPRibPolicy(final long localAs, final Ipv4Address bgpId,
+            final ClusterIdentifier clusterId, final Config policyConfig) {
+        requireNonNull(policyConfig);
+        return new BGPRibPolicyImpl(this.databroker, this.statementRegistryConsumer,
+                localAs, bgpId, clusterId, policyConfig);
+    }
+}
---
+++ bgp/openconfig-rp-impl/src/main/java/org/opendaylight/protocol/bgp/openconfig/routing/policy/impl/PolicyRIBBaseParametersImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.openconfig.routing.policy.impl;
+
+import static java.util.Objects.requireNonNull;
+
+import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.RouteEntryBaseAttributes;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev130919.ClusterIdentifier;
+
+public final class PolicyRIBBaseParametersImpl implements RouteEntryBaseAttributes {
+    private final Long localAs;
+    private final Ipv4Address originatorId;
+    private final ClusterIdentifier clusterId;
+
+    public PolicyRIBBaseParametersImpl(final long localAs, final Ipv4Address originatorId,
+            final ClusterIdentifier clusterId) {
+        this.localAs = localAs;
+        this.originatorId = requireNonNull(originatorId);
+        this.clusterId = requireNonNull(clusterId);
+    }
+
+    @Override
+    public long getLocalAs() {
+        return this.localAs;
+    }
+
+    @Override
+    public Ipv4Address getOriginatorId() {
+        return this.originatorId;
+    }
+
+    @Override
+    public ClusterIdentifier getClusterId() {
+        return this.clusterId;
+    }
+}
