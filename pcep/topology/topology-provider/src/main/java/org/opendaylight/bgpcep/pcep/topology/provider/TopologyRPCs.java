369beaf0cc69dffa952be9f4c437ae2c43691740
Robert Varga <robert.varga@pantheon.tech>
2018-04-23 18:35:39 +0200

    Adjust to RPC method signature update
    Input/Output structures are always present and we need to return
    ListenableFuture.
    
    Change-Id: Idf91f4321be2c339a7752c177bb000ffdd2e5021
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/TopologyRPCs.java
+++ pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/TopologyRPCs.java
@@ -10,8 +10,8 @@ package org.opendaylight.bgpcep.pcep.topology.provider;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
-import java.util.concurrent.Future;
 import org.opendaylight.bgpcep.programming.spi.SuccessfulRpcResult;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.AddLspInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.AddLspOutput;
@@ -24,6 +24,8 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.RemoveLspOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.RemoveLspOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.TearDownSessionInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.TearDownSessionOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.TearDownSessionOutputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.TriggerSyncInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.TriggerSyncOutput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev171025.TriggerSyncOutputBuilder;
@@ -40,43 +42,45 @@ final class TopologyRPCs implements NetworkTopologyPcepService {
     }
 
     @Override
-    public Future<RpcResult<AddLspOutput>> addLsp(final AddLspInput input) {
+    public ListenableFuture<RpcResult<AddLspOutput>> addLsp(final AddLspInput input) {
         return Futures.transform(this.manager.addLsp(input),
             input1 -> SuccessfulRpcResult.create(new AddLspOutputBuilder(input1).build()),
             MoreExecutors.directExecutor());
     }
 
     @Override
-    public Future<RpcResult<RemoveLspOutput>> removeLsp(final RemoveLspInput input) {
+    public ListenableFuture<RpcResult<RemoveLspOutput>> removeLsp(final RemoveLspInput input) {
         return Futures.transform(this.manager.removeLsp(input),
             input1 -> SuccessfulRpcResult.create(new RemoveLspOutputBuilder(input1).build()),
             MoreExecutors.directExecutor());
     }
 
     @Override
-    public Future<RpcResult<TriggerSyncOutput>> triggerSync(final TriggerSyncInput input) {
+    public ListenableFuture<RpcResult<TriggerSyncOutput>> triggerSync(final TriggerSyncInput input) {
         return Futures.transform(this.manager.triggerSync(input),
             input1 -> SuccessfulRpcResult.create(new TriggerSyncOutputBuilder(input1).build()),
             MoreExecutors.directExecutor());
     }
 
     @Override
-    public Future<RpcResult<UpdateLspOutput>> updateLsp(final UpdateLspInput input) {
+    public ListenableFuture<RpcResult<UpdateLspOutput>> updateLsp(final UpdateLspInput input) {
         return Futures.transform(this.manager.updateLsp(input),
             input1 -> SuccessfulRpcResult.create(new UpdateLspOutputBuilder(input1).build()),
             MoreExecutors.directExecutor());
     }
 
     @Override
-    public Future<RpcResult<EnsureLspOperationalOutput>> ensureLspOperational(final EnsureLspOperationalInput input) {
+    public ListenableFuture<RpcResult<EnsureLspOperationalOutput>> ensureLspOperational(
+            final EnsureLspOperationalInput input) {
         return Futures.transform(this.manager.ensureLspOperational(input),
             input1 -> SuccessfulRpcResult.create(new EnsureLspOperationalOutputBuilder(input1).build()),
             MoreExecutors.directExecutor());
     }
 
     @Override
-    public Future<RpcResult<Void>> tearDownSession(final TearDownSessionInput input) {
+    public ListenableFuture<RpcResult<TearDownSessionOutput>> tearDownSession(final TearDownSessionInput input) {
         return Futures.transform(this.manager.tearDownSession(input),
-            input1 -> SuccessfulRpcResult.create(null), MoreExecutors.directExecutor());
+            input1 -> SuccessfulRpcResult.create(new TearDownSessionOutputBuilder().build()),
+            MoreExecutors.directExecutor());
     }
 }
