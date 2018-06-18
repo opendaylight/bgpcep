88154ec4a645fd99c1ddc1ecedb795f36b784e0b
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-06-18 16:24:26 +0200

    Route Target constrains Extension
    https://tools.ietf.org/html/rfc4684
    
    JIRA: BGPCEP-527
    Change-Id: Idcb79c3072caa19775a48fbf6980cc8b89504eea
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ bgp/extensions/route-target/src/main/java/org/opendaylight/protocol/bgp/route/target/spi/nlri/RouteTargetConstrainSerializer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.route.target.spi.nlri;
+
+import io.netty.buffer.ByteBuf;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.route.target.constrain.rev180618.route.target.constrain.RouteTargetConstrainChoice;
+
+public interface RouteTargetConstrainSerializer<T extends RouteTargetConstrainChoice> {
+    /**
+     * Serialize routeTarget.
+     *
+     * @param routeTarget routeTarget
+     * @return Encode routeTarget to output buffer
+     */
+    @Nonnull
+    ByteBuf serializeRouteTargetConstrain(@Nonnull T routeTarget);
+
+    /**
+     * returns class of RouteTargetChoice handled by serializer.
+     *
+     * @return RouteTargetChoice Class
+     */
+    Class<T> getClazz();
+
+    /**
+     * Returns NlriType handled by parser.
+     *
+     * @return NlriType
+     */
+    Integer getType();
+}
