0f1a77ccd5028253ba5f8bce367f251681d1b642
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-06-18 16:24:26 +0200

    Route Target constrains Extension
    https://tools.ietf.org/html/rfc4684
    
    JIRA: BGPCEP-527
    Change-Id: Idcb79c3072caa19775a48fbf6980cc8b89504eea
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ bgp/extensions/route-target/src/main/java/org/opendaylight/protocol/bgp/route/target/impl/nlri/AbstractRouteTargetHandler.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.protocol.bgp.route.target.impl.nlri;
+
+import org.opendaylight.protocol.bgp.route.target.spi.nlri.RouteTargetConstrainParser;
+import org.opendaylight.protocol.bgp.route.target.spi.nlri.RouteTargetConstrainSerializer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.route.target.constrain.rev180618.route.target.constrain.RouteTargetConstrainChoice;
+
+abstract class AbstractRouteTargetHandler<T extends RouteTargetConstrainChoice>
+        implements RouteTargetConstrainParser<T>, RouteTargetConstrainSerializer<T> {
+}
