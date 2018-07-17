fc0d3539cbc4ff7930c2d1f7f47f11a34f51e2c3
Ajay Lele <ajayslele@gmail.com>
2018-07-17 14:09:14 -0700

    Fix calculation and roll-over of BGP operational neighbor uptime value.
    Change-Id: I15211d925664f275f1980df863cf6cefce8c0ba4
    JIRA: BGPCEP-813
    Signed-off-by: Ajay Lele <ajayslele@gmail.com>

--- bgp/openconfig-state/src/test/java/org/opendaylight/protocol/bgp/state/NeighborUtilTest.java
+++ bgp/openconfig-state/src/test/java/org/opendaylight/protocol/bgp/state/NeighborUtilTest.java
@@ -13,8 +13,10 @@ import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.opendaylight.protocol.bgp.state.StateProviderImplTest.TABLES_KEY;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.Optional;
 import org.junit.Before;
@@ -25,19 +27,25 @@ import org.opendaylight.protocol.bgp.openconfig.spi.BGPTableTypeRegistryConsumer
 import org.opendaylight.protocol.bgp.rib.spi.State;
 import org.opendaylight.protocol.bgp.rib.spi.state.BGPAfiSafiState;
 import org.opendaylight.protocol.bgp.rib.spi.state.BGPSessionState;
+import org.opendaylight.protocol.bgp.rib.spi.state.BGPTimersState;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.AfiSafi;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.AfiSafiBuilder;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.afi.safi.GracefulRestart;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.afi.safi.GracefulRestartBuilder;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.afi.safi.graceful.restart.StateBuilder;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.operational.rev151009.BgpNeighborState.SessionState;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.Timers;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.TimersBuilder;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009.AfiSafiType;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009.IPV4UNICAST;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Timeticks;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev180329.NeighborAfiSafiGracefulRestartStateAugmentation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev180329.NeighborAfiSafiGracefulRestartStateAugmentationBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev180329.NeighborAfiSafiStateAugmentation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev180329.NeighborAfiSafiStateAugmentationBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev180329.NeighborStateAugmentationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev180329.NeighborTimersStateAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev180329.NeighborTimersStateAugmentationBuilder;
 
 public class NeighborUtilTest {
     @Mock
@@ -65,8 +73,6 @@ public class NeighborUtilTest {
         doReturn(false).when(this.bgpAfiSafiState).isAfiSafiSupported(eq(TABLES_KEY));
         doReturn(false).when(this.bgpAfiSafiState).isGracefulRestartAdvertized(eq(TABLES_KEY));
         doReturn(false).when(this.bgpAfiSafiState).isGracefulRestartReceived(eq(TABLES_KEY));
-
-
     }
 
     @Test
@@ -82,8 +88,38 @@ public class NeighborUtilTest {
     }
 
     @Test
-    public void testBuildTimer() {
+    public void testBuildTimerNullValue() {
         assertNull(NeighborUtil.buildTimer(null));
+    }
+
+    @Test
+    public void testBuildTimerNormalValue() {
+        final BGPTimersState timerState = mock(BGPTimersState.class);
+        doReturn(90L).when(timerState).getNegotiatedHoldTime();
+        doReturn(5000L).when(timerState).getUpTime();
+
+        final NeighborTimersStateAugmentation timerStateAug = new NeighborTimersStateAugmentationBuilder()
+                .setNegotiatedHoldTime(BigDecimal.valueOf(90L)).setUptime(new Timeticks(500L)).build();
+        final Timers expectedTimers = new TimersBuilder().setState(
+                new org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.timers
+                .StateBuilder().addAugmentation(NeighborTimersStateAugmentation.class, timerStateAug).build())
+                .build();
+        assertEquals(expectedTimers, NeighborUtil.buildTimer(timerState));
+    }
+
+    @Test
+    public void testBuildTimerRollOverValue() {
+        final BGPTimersState timerState = mock(BGPTimersState.class);
+        doReturn(90L).when(timerState).getNegotiatedHoldTime();
+        doReturn(42949673015L).when(timerState).getUpTime();
+
+        final NeighborTimersStateAugmentation timerStateAug = new NeighborTimersStateAugmentationBuilder()
+                .setNegotiatedHoldTime(BigDecimal.valueOf(90L)).setUptime(new Timeticks(5L)).build();
+        final Timers expectedTimers = new TimersBuilder().setState(
+                new org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.timers
+                .StateBuilder().addAugmentation(NeighborTimersStateAugmentation.class, timerStateAug).build())
+                .build();
+        assertEquals(expectedTimers, NeighborUtil.buildTimer(timerState));
     }
 
     @Test
@@ -92,7 +128,7 @@ public class NeighborUtilTest {
     }
 
     @Test
-    public void testBuildNeighborStatet() {
+    public void testBuildNeighborState() {
         assertNull(NeighborUtil.buildNeighborState(null, null));
     }
 
