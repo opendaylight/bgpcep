97608195c15ae78cda064e137ec61bbcba540518
Matej Perina <matej.perina@pantheon.tech>
2018-11-05 12:30:19 +0100

    send EOR marker after initial route exchange
    JIRA: BGPCEP-807
    
    Change-Id: I374826c8faaa94e8ba8394588b7de0a00acf101f
    Signed-off-by: Matej Perina <matej.perina@pantheon.tech>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/BGPPeer.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/BGPPeer.java
@@ -45,6 +45,7 @@ import org.opendaylight.protocol.bgp.parser.BGPError;
 import org.opendaylight.protocol.bgp.parser.impl.message.update.LocalPreferenceAttributeParser;
 import org.opendaylight.protocol.bgp.parser.spi.MessageUtil;
 import org.opendaylight.protocol.bgp.rib.impl.config.BgpPeer;
+import org.opendaylight.protocol.bgp.rib.impl.config.BgpPeerUtil;
 import org.opendaylight.protocol.bgp.rib.impl.config.GracefulRestartUtil;
 import org.opendaylight.protocol.bgp.rib.impl.spi.BGPSessionPreferences;
 import org.opendaylight.protocol.bgp.rib.impl.spi.RIB;
@@ -66,13 +67,10 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.inet
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.inet.rev180329.ipv4.prefixes.destination.ipv4.Ipv4PrefixesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.inet.rev180329.update.attributes.mp.reach.nlri.advertized.routes.destination.type.DestinationIpv4CaseBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.Update;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.UpdateMessage;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.open.message.BgpParameters;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.Attributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.AttributesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.update.message.Nlri;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.Attributes1;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.Attributes2;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.BgpAddPathTableType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.BgpTableType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.RouteRefresh;
@@ -327,7 +325,7 @@ public class BGPPeer extends AbstractPeer implements BGPSessionListener {
         } else {
             mpUnreach = MessageUtil.getMpUnreachNlri(attrs);
         }
-        final boolean endOfRib = isEndOfRib(message);
+        final boolean endOfRib = BgpPeerUtil.isEndOfRib(message);
         if (mpUnreach != null) {
             if (endOfRib) {
                 final TablesKey tablesKey = new TablesKey(mpUnreach.getAfi(), mpUnreach.getSafi());
@@ -597,25 +595,6 @@ public class BGPPeer extends AbstractPeer implements BGPSessionListener {
         }
         new ScheduledThreadPoolExecutor(1)
                 .schedule(this::handleSelectionReferralTimer, timerExpire - ct, TimeUnit.NANOSECONDS);
-    }
-
-    private boolean isEndOfRib(final UpdateMessage msg) {
-        if (msg.getNlri() == null && msg.getWithdrawnRoutes() == null) {
-            if (msg.getAttributes() != null) {
-                final Attributes2 pa = msg.getAttributes().augmentation(Attributes2.class);
-                if (msg.getAttributes().augmentation(Attributes1.class) == null && pa != null) {
-                    //only MP_UNREACH_NLRI allowed in EOR
-                    if (pa.getMpUnreachNlri() != null && pa.getMpUnreachNlri().getWithdrawnRoutes() == null) {
-                        // EOR message contains only MPUnreach attribute and no NLRI
-                        return true;
-                    }
-                }
-            } else {
-                // true for empty IPv4 Unicast
-                return true;
-            }
-        }
-        return false;
     }
 
     private void releaseConnectionGracefully() {
---
+++ bgp/rib-impl/src/test/java/org/opendaylight/protocol/bgp/rib/impl/config/BgpPeerUtilTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.impl.config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collections;
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.Update;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.UpdateBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.Attributes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.AttributesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.Attributes2;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.Attributes2Builder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.update.attributes.MpUnreachNlri;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.update.attributes.MpUnreachNlriBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.update.attributes.mp.unreach.nlri.WithdrawnRoutesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.TablesKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev180329.Ipv4AddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev180329.Ipv6AddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev180329.UnicastSubsequentAddressFamily;
+
+public class BgpPeerUtilTest {
+
+    private final TablesKey IPV4_TABLE_KEY = new TablesKey(Ipv4AddressFamily.class,
+            UnicastSubsequentAddressFamily.class);
+    private final TablesKey IPV6_TABLE_KEY = new TablesKey(Ipv6AddressFamily.class,
+            UnicastSubsequentAddressFamily.class);
+
+    @Test
+    public void createIpv4EORTest() {
+        final Update endOfRib = BgpPeerUtil.createEndOfRib(IPV4_TABLE_KEY);
+        assertNull(endOfRib.getNlri());
+        assertNull(endOfRib.getWithdrawnRoutes());
+        assertNull(endOfRib.getAttributes());
+    }
+
+    @Test
+    public void createNonIpv4EORTest() {
+        final Update endOfRib = BgpPeerUtil.createEndOfRib(IPV6_TABLE_KEY);
+        assertNull(endOfRib.getNlri());
+        assertNull(endOfRib.getWithdrawnRoutes());
+        final Attributes attributes = endOfRib.getAttributes();
+        assertNotNull(attributes);
+        final Attributes2 augmentation = attributes.augmentation(Attributes2.class);
+        assertNotNull(augmentation);
+        final MpUnreachNlri mpUnreachNlri = augmentation.getMpUnreachNlri();
+        assertNotNull(mpUnreachNlri);
+        assertEquals(IPV6_TABLE_KEY.getAfi(), mpUnreachNlri.getAfi());
+        assertEquals(IPV6_TABLE_KEY.getSafi(), mpUnreachNlri.getSafi());
+        assertNull(mpUnreachNlri.getWithdrawnRoutes());
+    }
+
+    @Test
+    public void isEndOfTableTest() {
+        final Update ipv4EOT = new UpdateBuilder().build();
+        final MpUnreachNlri ipv6EOTnlri = new MpUnreachNlriBuilder()
+                .setAfi(IPV6_TABLE_KEY.getAfi())
+                .setSafi(IPV6_TABLE_KEY.getSafi())
+                .build();
+        final Update ipv6EOT = new UpdateBuilder()
+                .setAttributes(new AttributesBuilder()
+                        .addAugmentation(Attributes2.class, new Attributes2Builder()
+                                .setMpUnreachNlri(ipv6EOTnlri)
+                                .build())
+                        .build())
+                .build();
+
+        assertTrue(BgpPeerUtil.isEndOfRib(ipv4EOT));
+        assertTrue(BgpPeerUtil.isEndOfRib(ipv6EOT));
+
+        final Update ipv4NonEOT = new UpdateBuilder()
+                .setNlri(Collections.emptyList())
+                .build();
+        final MpUnreachNlri ipv6NonEOTnlri = new MpUnreachNlriBuilder(ipv6EOTnlri)
+                .setWithdrawnRoutes(new WithdrawnRoutesBuilder().build())
+                .build();
+        final Update ipv6NonEOT = new UpdateBuilder()
+                .setAttributes(new AttributesBuilder()
+                        .addAugmentation(Attributes2.class, new Attributes2Builder()
+                                .setMpUnreachNlri(ipv6NonEOTnlri)
+                                .build())
+                        .build())
+                .build();
+
+        assertFalse(BgpPeerUtil.isEndOfRib(ipv4NonEOT));
+        assertFalse(BgpPeerUtil.isEndOfRib(ipv6NonEOT));
+    }
+}
