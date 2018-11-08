b58a92e809bc9c84cc03440152ede4d80a619771
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-11-08 10:21:16 +0100

    P2MP path computation capability
    https://tools.ietf.org/html/rfc8306#section-3.1.2
    
    JIRA: BGPCEP-406
    Change-Id: Idee90c040d1cf242c3b3146722097f8c68a8b9f8
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ pcep/ietf-p2mp-te-lsp/src/main/java/org/opendaylight/protocol/pcep/p2mp/te/lsp/Activator.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.pcep.p2mp.te.lsp;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.protocol.pcep.spi.PCEPExtensionProviderContext;
+import org.opendaylight.protocol.pcep.spi.pojo.AbstractPCEPExtensionProviderActivator;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.p2mp.te.lsp.rev181109.p2mp.pce.capability.tlv.P2mpPceCapability;
+
+public final class Activator extends AbstractPCEPExtensionProviderActivator {
+    @Override
+    protected List<AutoCloseable> startImpl(final PCEPExtensionProviderContext context) {
+        final List<AutoCloseable> regs = new ArrayList<>(2);
+
+        final P2MPTeLspCapabilityParser p2mpCapabilityParser = new P2MPTeLspCapabilityParser();
+        regs.add(context.registerTlvParser(P2MPTeLspCapabilityParser.TYPE, p2mpCapabilityParser));
+        regs.add(context.registerTlvSerializer(P2mpPceCapability.class, p2mpCapabilityParser));
+        return regs;
+    }
+}
