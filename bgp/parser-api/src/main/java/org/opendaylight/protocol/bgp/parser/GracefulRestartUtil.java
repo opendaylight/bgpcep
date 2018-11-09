888de9b2543b4a283a1fef36c60f7d75763a6d0a
Matej Perina <matej.perina@pantheon.tech>
2018-11-09 16:19:45 +0100

    Add Long-Lived Graceful Restart support
    This patch adds the final bits of LLGR support, handling
    both ingress and egress parts of the mechanics.
    
    Main parts:
    AbstractPeer: Regulate propagating LLGR_STALE routes to AdjRibOut
    table. If peer don't support LLGR dont propagate such routes
    and withdraw routes if they switch to LLGR_STALE.
    
    BGPPeer: Handle LLGR stale timers.
    
    EffectiveRibInWriter: Handle adding LLGR_STALE community addition
    to routes and removal of routes with this community present.
    In case of adding LLGR_STALE to route with NO_LLGR removal is needed.
    
    JIRA: BGPCEP-495
    Change-Id: I8f2092d68e4a0f986b816f1eee7cac0e92803e50
    Signed-off-by: Matej Perina <matej.perina@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/parser-api/src/main/java/org/opendaylight/protocol/bgp/parser/GracefulRestartUtil.java
+++ bgp/parser-api/src/main/java/org/opendaylight/protocol/bgp/parser/GracefulRestartUtil.java
@@ -8,11 +8,21 @@
 package org.opendaylight.protocol.bgp.parser;
 
 import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.open.message.bgp.parameters.optional.capabilities.CParameters;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.open.message.bgp.parameters.optional.capabilities.CParametersBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.CParameters1;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.CParameters1Builder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.GracefulRestartCapability;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.GracefulRestartCapability.RestartFlags;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.GracefulRestartCapabilityBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.LlGracefulRestartCapability;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.LlGracefulRestartCapabilityBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.graceful.restart.capability.Tables;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.graceful.restart.capability.Tables.AfiFlags;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.graceful.restart.capability.TablesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev180329.AddressFamily;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev180329.SubsequentAddressFamily;
 
 /**
  * Utility class for dealing with Graceful Restart.
@@ -36,5 +46,21 @@ public final class GracefulRestartUtil {
 
     private GracefulRestartUtil() {
 
+    }
+
+    public static Tables gracefulRestartTable(final Class<? extends AddressFamily> afi,
+            final Class<? extends SubsequentAddressFamily> safi, final boolean forwardingState) {
+        return new TablesBuilder().setAfi(afi).setSafi(safi).setAfiFlags(new AfiFlags(forwardingState)).build();
+    }
+
+    public static CParameters gracefulRestartCapability(final List<Tables> tables, final int restartTime,
+            final boolean localRestarting) {
+        return new CParametersBuilder().addAugmentation(CParameters1.class, new CParameters1Builder()
+            .setGracefulRestartCapability(new GracefulRestartCapabilityBuilder()
+                .setRestartFlags(new RestartFlags(localRestarting))
+                .setRestartTime(restartTime)
+                .setTables(tables)
+                .build())
+            .build()).build();
     }
 }
