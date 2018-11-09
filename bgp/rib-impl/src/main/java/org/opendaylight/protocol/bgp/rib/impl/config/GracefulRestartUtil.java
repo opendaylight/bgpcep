ba5943466b5314b97e34c27df2a3164f2226b222
Matej Perina <matej.perina@pantheon.tech>
2018-11-09 16:19:45 +0100

    long-lived graceful restart support
    - introdce long-live graceful restart procedures according to
    https://tools.ietf.org/html/draft-uttaro-idr-bgp-persistence-04
    - introduce 2 new BGP well-known communities LLGR_STALE, NO_LLGR
    
    JIRA: BGPCEP-495
    
    Change-Id: I8f2092d68e4a0f986b816f1eee7cac0e92803e50
    Signed-off-by: Matej Perina <matej.perina@pantheon.tech>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/GracefulRestartUtil.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/GracefulRestartUtil.java
@@ -17,8 +17,14 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.opendaylight.protocol.bgp.openconfig.spi.BGPTableTypeRegistryConsumer;
+import org.opendaylight.protocol.bgp.rib.impl.BgpPeerUtil;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.AfiSafi;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.afi.safi.GracefulRestart;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.afi.safi.graceful.restart.Config;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009.AfiSafiType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.ll.graceful.restart.rev181112.Config1;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.ll.graceful.restart.rev181112.Config2;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.ll.graceful.restart.rev181112.afi.safi.ll.graceful.restart.LlGracefulRestart;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.open.message.BgpParameters;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.open.message.BgpParametersBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.open.message.bgp.parameters.OptionalCapabilities;
@@ -29,23 +35,32 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.mult
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.CParameters1Builder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.GracefulRestartCapability;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.GracefulRestartCapabilityBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.LlGracefulRestartCapability;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.LlGracefulRestartCapabilityBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.graceful.restart.capability.Tables;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.graceful.restart.capability.TablesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.TablesKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class GracefulRestartUtil {
 
+    private static final Logger LOG = LoggerFactory.getLogger(GracefulRestartUtil.class);
     public static final GracefulRestartCapability EMPTY_GRACEFUL_CAPABILITY = new GracefulRestartCapabilityBuilder()
             .setRestartTime(0)
             .setTables(Collections.emptyList())
             .setRestartFlags(new GracefulRestartCapability.RestartFlags(false))
             .build();
+    public static final LlGracefulRestartCapability EMPTY_LL_GRACEFUL_CAPABILITY =
+            new LlGracefulRestartCapabilityBuilder()
+                    .setTables(Collections.emptyList())
+                    .build();
 
     private GracefulRestartUtil() {
         throw new UnsupportedOperationException();
     }
 
-    static CParameters getGracefulCapability(final Map<TablesKey, Boolean> tables,
+    public static CParameters getGracefulCapability(final Map<TablesKey, Boolean> tables,
                                                     final int restartTime,
                                                     final boolean localRestarting) {
         final List<Tables> tablesList = tables.entrySet().stream()
@@ -61,6 +76,25 @@ public final class GracefulRestartUtil {
                         .setRestartTime(restartTime)
                         .setTables(tablesList)
                .build()).build()).build();
+    }
+
+    public static CParameters getLlGracefulCapability(final Set<BgpPeerUtil.LlGracefulRestartDTO> llGracefulRestarts) {
+        final List<org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.ll.graceful.restart.capability.Tables> tablesList =
+                llGracefulRestarts.stream()
+                .map(dto -> new org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.ll.graceful.restart.capability.TablesBuilder()
+                        .setAfi(dto.getTableKey().getAfi())
+                        .setSafi(dto.getTableKey().getSafi())
+                        .setAfiFlags(
+                                new org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.ll.graceful.restart.capability.Tables.AfiFlags(
+                                        dto.isForwarding()))
+                        .setLongLiveStaleTime((long) dto.getStaleTime())
+                        .build())
+                .collect(Collectors.toList());
+        return new CParametersBuilder().addAugmentation(CParameters1.class,
+                new CParameters1Builder().setLlGracefulRestartCapability(
+                        new LlGracefulRestartCapabilityBuilder()
+                        .setTables(tablesList)
+                        .build()).build()).build();
     }
 
     static Set<TablesKey> getGracefulTables(final List<AfiSafi> afiSafis,
@@ -83,17 +117,62 @@ public final class GracefulRestartUtil {
         return gracefulTables;
     }
 
+    static Map<TablesKey, Integer> getLlGracefulTimers(final List<AfiSafi> afiSafis,
+                                                       final BGPTableTypeRegistryConsumer tableTypeRegistry) {
+        final Map<TablesKey, Integer> timers = new HashMap<>();
+        afiSafis.forEach(afiSafi -> {
+            final GracefulRestart gracefulRestart = afiSafi.getGracefulRestart();
+            if (gracefulRestart != null) {
+                final Config gracefulRestartConfig = gracefulRestart.getConfig();
+                if (gracefulRestartConfig != null) {
+                    final LlGracefulRestart llGracefulRestart;
+                    final Config1 peerAug = gracefulRestartConfig.augmentation(Config1.class);
+                    if (peerAug != null) {
+                       llGracefulRestart = peerAug.getLlGracefulRestart();
+                    } else {
+                        final Config2 neighborAug = gracefulRestartConfig.augmentation(Config2.class);
+                        if (neighborAug != null) {
+                            llGracefulRestart = neighborAug.getLlGracefulRestart();
+                        } else {
+                            return;
+                        }
+                    }
+                    if (llGracefulRestart != null) {
+                        final org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.ll.graceful.restart
+                                .rev181112.afi.safi.ll.graceful.restart.ll.graceful.restart.Config config =
+                                llGracefulRestart.getConfig();
+                        if(config != null) {
+                            final Long staleTime = config.getLongLiveStaleTime();
+                            if (staleTime != null && staleTime > 0) {
+                                final Optional<TablesKey> key = tableTypeRegistry.getTableKey(afiSafi.getAfiSafiName());
+                                if (key.isPresent()) {
+                                    timers.put(key.get(), staleTime.intValue());
+                                } else {
+                                    LOG.debug("Skipping unsupported afi-safi {}",afiSafi.getAfiSafiName());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        return timers;
+    }
+
     public static BgpParameters getGracefulBgpParameters(final List<OptionalCapabilities> fixedCapabilities,
                                                          final Set<TablesKey> gracefulTables,
                                                          final Set<TablesKey> preservedTables,
                                                          final int gracefulRestartTimer,
-                                                         final boolean localRestarting) {
+                                                         final boolean localRestarting,
+                                                         final Set<BgpPeerUtil.LlGracefulRestartDTO> llGracefulDTOs) {
         final List<OptionalCapabilities> capabilities = new ArrayList<>(fixedCapabilities);
         final Map<TablesKey, Boolean> gracefulMap = new HashMap<>();
         gracefulTables.forEach(table -> gracefulMap.put(table, preservedTables.contains(table)));
-        final CParameters gracefulCapability = getGracefulCapability(gracefulMap,
-                gracefulRestartTimer, localRestarting);
+        final CParameters gracefulCapability = getGracefulCapability(gracefulMap, gracefulRestartTimer,
+                localRestarting);
         capabilities.add(new OptionalCapabilitiesBuilder().setCParameters(gracefulCapability).build());
+        final CParameters llGracefulCapability = getLlGracefulCapability(llGracefulDTOs);
+        capabilities.add(new OptionalCapabilitiesBuilder().setCParameters(llGracefulCapability).build());
 
         return new BgpParametersBuilder().setOptionalCapabilities(capabilities).build();
     }
