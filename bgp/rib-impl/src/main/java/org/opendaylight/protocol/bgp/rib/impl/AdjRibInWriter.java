920170e47f80695a9163520cfd12af1af1257601
Matej Perina <matej.perina@pantheon.tech>
2018-11-09 16:19:45 +0100

    WIP: Add Long-Lived Graceful Restart support
    This patch adds the final bits of LLGR support, handling
    both ingress and egress parts of the mechanics.
    
    FIXME: correctly flip adj-rib-in attributes:
          - it needs to be set when LLGR becomes effective
          - it needs to be deleted when LLGR becomes dormant, i.e.
            when we received all routes from the peer
    
    JIRA: BGPCEP-495
    Change-Id: I8f2092d68e4a0f986b816f1eee7cac0e92803e50
    Signed-off-by: Matej Perina <matej.perina@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibInWriter.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/AdjRibInWriter.java
@@ -13,10 +13,10 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
-import com.google.common.util.concurrent.CheckedFuture;
 import com.google.common.util.concurrent.FluentFuture;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 import java.util.Collection;
 import java.util.Collections;
@@ -26,18 +26,18 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.NotThreadSafe;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataReadWriteTransaction;
 import org.opendaylight.controller.md.sal.dom.api.DOMDataWriteTransaction;
 import org.opendaylight.mdsal.common.api.CommitInfo;
 import org.opendaylight.protocol.bgp.rib.impl.ApplicationPeer.RegisterAppPeerListener;
@@ -118,6 +118,8 @@ final class AdjRibInWriter {
     private final PeerRole role;
     @GuardedBy("this")
     private final Map<TablesKey, Collection<NodeIdentifierWithPredicates>> staleRoutesRegistry = new HashMap<>();
+    @GuardedBy("this")
+    private final Map<TablesKey, Collection<NodeIdentifierWithPredicates>> llStaleRoutesRegistry = new HashMap<>();
     @GuardedBy("this")
     private FluentFuture<? extends CommitInfo> submitted;
 
@@ -299,8 +301,12 @@ final class AdjRibInWriter {
         final DOMDataWriteTransaction tx = this.chain.getDomChain().newWriteOnlyTransaction();
         final Collection<NodeIdentifierWithPredicates> routeKeys = ctx.writeRoutes(tx, nlri, attributes);
         final Collection<NodeIdentifierWithPredicates> staleRoutes = this.staleRoutesRegistry.get(key);
+        final Collection<NodeIdentifierWithPredicates> llStaleRoutes = this.llStaleRoutesRegistry.get(key);
         if (staleRoutes != null) {
             staleRoutes.removeAll(routeKeys);
+        }
+        if (llStaleRoutes != null) {
+            llStaleRoutes.removeAll(routeKeys);
         }
         LOG.trace("Write routes {}", nlri);
         final FluentFuture<? extends CommitInfo> future = tx.commit();
@@ -353,9 +359,9 @@ final class AdjRibInWriter {
         }
     }
 
-    final void storeStaleRoutes(final Set<TablesKey> gracefulTables) {
+    void storeStaleRoutes(final Set<TablesKey> gracefulTables) {
         try (final DOMDataReadOnlyTransaction tx = this.chain.getDomChain().newReadOnlyTransaction()) {
-            final Map<TablesKey, Future> readFutures = new HashMap<>();
+            final Map<TablesKey, ListenableFuture<Optional<NormalizedNode<?, ?>>>> readFutures = new HashMap<>();
             final ExecutorService threadPool = Executors.newCachedThreadPool();
             gracefulTables.forEach(tablesKey -> {
                 final TableContext ctx = this.tables.get(tablesKey);
@@ -364,7 +370,7 @@ final class AdjRibInWriter {
                     return;
                 }
                 final YangInstanceIdentifier iid = ctx.routesPath();
-                final CheckedFuture<Optional<NormalizedNode<?, ?>>, ReadFailedException> readFuture =
+                final ListenableFuture<Optional<NormalizedNode<?, ?>>> readFuture =
                         tx.read(LogicalDatastoreType.OPERATIONAL, iid);
                 readFutures.put(tablesKey, readFuture);
                 Futures.addCallback(readFuture, new FutureCallback<Optional<NormalizedNode<?, ?>>>() {
@@ -396,16 +402,93 @@ final class AdjRibInWriter {
                     LOG.warn("Failed to store stale routes for table {}", entry.getKey(), e);
                 }
             });
+            threadPool.shutdown();
         }
     }
 
-    final void removeStaleRoutes(final TablesKey tableKey) {
+    void storeLlStaleRoutes(final Set<TablesKey> llGracefulTables) {
+        final DOMDataReadWriteTransaction tx = this.chain.getDomChain().newReadWriteTransaction();
+        final CountDownLatch latch = new CountDownLatch(llGracefulTables.size());
+
+        for (TablesKey tablesKey : llGracefulTables) {
+            final TableContext ctx = this.tables.get(tablesKey);
+            if (ctx == null) {
+                LOG.error("Trying to save routes from missing table {}", tablesKey);
+                latch.countDown();
+                continue;
+            }
+
+            // FIXME: write llgr-stale container
+
+            final YangInstanceIdentifier iid = ctx.routesPath();
+            Futures.addCallback(tx.read(LogicalDatastoreType.OPERATIONAL, iid),
+                new FutureCallback<Optional<NormalizedNode<?, ?>>>() {
+                @Override
+                public void onSuccess(final Optional<NormalizedNode<?, ?>> result) {
+                    try {
+                        if (result.isPresent()) {
+                            final MapNode routesNode = (MapNode) result.get();
+                            final Collection<MapEntryNode> routes = routesNode.getValue();
+                            if (!routes.isEmpty()) {
+                                final Set<NodeIdentifierWithPredicates> staleIds = routesNode.getValue().stream()
+                                        .map(MapEntryNode::getIdentifier).collect(Collectors.toSet());
+                                synchronized (AdjRibInWriter.this.llStaleRoutesRegistry) {
+                                    AdjRibInWriter.this.llStaleRoutesRegistry.put(tablesKey, staleIds);
+                                }
+                            }
+                        }
+                    } finally {
+                        latch.countDown();
+                    }
+                }
+
+                @Override
+                public void onFailure(final Throwable failure) {
+                    LOG.warn("Reading stale routes for table {} failed", tablesKey, failure);
+                    latch.countDown();
+                }
+            }, MoreExecutors.directExecutor());
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            LOG.error("Interrupted while waiting to apply {} LLGR_STALE update tasks of {} to finish", latch.getCount(),
+                llGracefulTables, e);
+            tx.cancel();
+            return;
+        }
+
+        tx.commit().addCallback(new FutureCallback<CommitInfo>() {
+            @Override
+            public void onSuccess(final CommitInfo commitInfo) {
+                LOG.debug("Routes from tables {} updated with LLGR_STALE community.", llGracefulTables);
+            }
+
+            @Override
+            public void onFailure(final Throwable throwable) {
+                LOG.error("Failed to update routes from tables {} with LLGR_STALE community", llGracefulTables,
+                        throwable);
+            }
+        }, MoreExecutors.directExecutor());
+    }
+
+    void removeStaleRoutes(final TablesKey tableKey) {
+        removeStaleRoutes(tableKey, this.staleRoutesRegistry);
+    }
+
+    void removeLlStaleRoutes(final TablesKey tableKey) {
+        removeStaleRoutes(tableKey, this.llStaleRoutesRegistry);
+    }
+
+    private void removeStaleRoutes(final TablesKey tableKey,
+                            final Map<TablesKey, Collection<NodeIdentifierWithPredicates>> routeRegistry) {
         final TableContext ctx = this.tables.get(tableKey);
         if (ctx == null) {
             LOG.debug("No table for {}, not removing any stale routes", tableKey);
             return;
         }
-        final Collection<NodeIdentifierWithPredicates> routeKeys = this.staleRoutesRegistry.get(tableKey);
+        final Collection<NodeIdentifierWithPredicates> routeKeys = routeRegistry.get(tableKey);
         if (routeKeys == null || routeKeys.isEmpty()) {
             LOG.debug("No stale routes present in table {}", tableKey);
             return;
@@ -421,8 +504,8 @@ final class AdjRibInWriter {
             @Override
             public void onSuccess(final CommitInfo result) {
                 LOG.trace("Removing routes {}, succeed", routeKeys);
-                synchronized (AdjRibInWriter.this.staleRoutesRegistry) {
-                    staleRoutesRegistry.remove(tableKey);
+                synchronized (routeRegistry) {
+                    routeRegistry.remove(tableKey);
                 }
             }
 
@@ -433,7 +516,7 @@ final class AdjRibInWriter {
         }, MoreExecutors.directExecutor());
     }
 
-    final FluentFuture<? extends CommitInfo> clearTables(final Set<TablesKey> tablesToClear) {
+    FluentFuture<? extends CommitInfo> clearTables(final Set<TablesKey> tablesToClear) {
         if (tablesToClear == null || tablesToClear.isEmpty()) {
             return CommitInfo.emptyFluentFuture();
         }
