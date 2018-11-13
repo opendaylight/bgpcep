e6ea27768ff7221b38b3bec585dad7d167e5391a
Claudio D. Gasparini <claudio.gasparini@pantheon.tech>
2018-11-13 16:48:07 +0100

    UNREACH-DESTINATION Object
    https://tools.ietf.org/html/rfc8306#section-3.14
    
    JIRA: BGPCEP-406
    Change-Id: Ie7ef63e7f166059655ff558623c686f087b6302f
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@pantheon.tech>

---
+++ pcep/base-parser/src/main/java/org/opendaylight/protocol/pcep/parser/object/unreach/PCEPIpv4UnreachDestinationParser.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.pcep.parser.object.unreach;
+
+import static org.opendaylight.protocol.util.ByteBufWriteUtil.writeIpv4Address;
+
+import com.google.common.base.Preconditions;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import java.util.ArrayList;
+import java.util.List;
+import org.opendaylight.protocol.pcep.spi.CommonObjectParser;
+import org.opendaylight.protocol.pcep.spi.ObjectUtil;
+import org.opendaylight.protocol.pcep.spi.PCEPDeserializerException;
+import org.opendaylight.protocol.util.Ipv4Util;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4AddressNoZone;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.ObjectHeader;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.unreach.destination.object.UnreachDestinationObj;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.unreach.destination.object.UnreachDestinationObjBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.unreach.destination.object.unreach.destination.obj.destination.Ipv4DestinationCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.unreach.destination.object.unreach.destination.obj.destination.Ipv4DestinationCaseBuilder;
+
+public final class PCEPIpv4UnreachDestinationParser extends CommonObjectParser {
+    private static final int CLASS = 28;
+    private static final int TYPE = 1;
+
+    public PCEPIpv4UnreachDestinationParser() {
+        super(CLASS, TYPE);
+    }
+
+    public static void serializeObject(
+        final Boolean processing,
+        final Boolean ignore,
+        final Ipv4DestinationCase ipv4Case,
+        final ByteBuf buffer) {
+        final List<Ipv4AddressNoZone> dest = ipv4Case.getDestinationIpv4Address();
+        Preconditions.checkArgument(dest != null, "DestinationIpv4Address is mandatory.");
+        final ByteBuf body = Unpooled.buffer(Ipv4Util.IP4_LENGTH * dest.size());
+        dest.forEach(ipv4 -> writeIpv4Address(ipv4, body));
+        ObjectUtil.formatSubobject(TYPE, CLASS, processing, ignore, body, buffer);
+    }
+
+    @Override
+    public UnreachDestinationObj parseObject(final ObjectHeader header, final ByteBuf bytes)
+        throws PCEPDeserializerException {
+        Preconditions.checkArgument(bytes != null && bytes.isReadable(),
+            "Array of bytes is mandatory. Can't be null or empty.");
+        final UnreachDestinationObjBuilder builder = new UnreachDestinationObjBuilder();
+        if (bytes.readableBytes() % Ipv4Util.IP4_LENGTH != 0) {
+            throw new PCEPDeserializerException("Wrong length of array of bytes.");
+        }
+        builder.setIgnore(header.isIgnore());
+        builder.setProcessingRule(header.isProcessingRule());
+        List<Ipv4AddressNoZone> dest = new ArrayList<>();
+        while (bytes.isReadable()) {
+            dest.add(Ipv4Util.noZoneAddressForByteBuf(bytes));
+        }
+        builder.setDestination(new Ipv4DestinationCaseBuilder().setDestinationIpv4Address(dest).build());
+        return builder.build();
+    }
+}
