cba2d31c8975239a6ad8ab51ffac8c173698f454
Robert Varga <robert.varga@pantheon.tech>
2018-11-21 14:53:06 +0100

    WIP: add revised error handling interfaces
    This is a raw patch.
    
    Change-Id: I383571360f41a3cf874c55e13e6c4212f31522ff
    JIRA: BGPCEP-359
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/MultiExitDiscriminatorAttributeParser.java
+++ bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/MultiExitDiscriminatorAttributeParser.java
@@ -9,10 +9,13 @@ package org.opendaylight.protocol.bgp.parser.impl.message.update;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
+import org.opendaylight.protocol.bgp.parser.BGPError;
 import org.opendaylight.protocol.bgp.parser.spi.AttributeParser;
 import org.opendaylight.protocol.bgp.parser.spi.AttributeSerializer;
 import org.opendaylight.protocol.bgp.parser.spi.AttributeUtil;
 import org.opendaylight.protocol.bgp.parser.spi.PeerSpecificParserConstraint;
+import org.opendaylight.protocol.bgp.parser.spi.RevisedErrorHandling;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.Attributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.AttributesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.attributes.MultiExitDisc;
@@ -24,8 +27,16 @@ public final class MultiExitDiscriminatorAttributeParser implements AttributePar
 
     @Override
     public void parseAttribute(final ByteBuf buffer, final AttributesBuilder builder,
-            final PeerSpecificParserConstraint constraint) {
-        // FIXME: BGPCEP-359: check if length == 4, if not treat-as-withdraw
+            final PeerSpecificParserConstraint constraint) throws BGPDocumentedException {
+        final int readable = buffer.readableBytes();
+        if (readable != 4) {
+            if (RevisedErrorHandling.from(constraint) != RevisedErrorHandling.NONE) {
+                // FIXME: BGPCEP-359: treat-as-withdraw
+            }
+
+            throw new BGPDocumentedException("MULTI_EXIT_DISC has to have length 4, but has " + readable,
+                BGPError.ATTR_LENGTH_ERROR);
+        }
         builder.setMultiExitDisc(new MultiExitDiscBuilder().setMed(buffer.readUnsignedInt()).build());
     }
 
