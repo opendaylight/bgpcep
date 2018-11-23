0bbf6a58ecc6783d4915baa70b6b163b55623ca2
Matej Perina <matej.perina@pantheon.tech>
2018-11-23 10:32:16 +0100

    operational state of (LL)GR procedures
    State of (LL)GR operations (restart time, enabled afi-safi...)
    is now accessible in operational datastore.
    
    JIRA: BGPCEP-809
    Change-Id: If03b74e67750970a08cfdc34ae402cbe8283da15
    Signed-off-by: Matej Perina <matej.perina@pantheon.tech>

--- bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/state/BGPGracelfulRestartState.java
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/state/BGPGracelfulRestartState.java
@@ -9,6 +9,7 @@
 package org.opendaylight.protocol.bgp.rib.spi.state;
 
 import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.operational.rev151009.BgpAfiSafiGracefulRestartState.Mode;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.TablesKey;
 
 /**
@@ -53,4 +54,12 @@ public interface BGPGracelfulRestartState {
      * @return peer is restarting
      */
     boolean isPeerRestarting();
+
+    /**
+     * Returns operational mode of graceful restart. Result depends on advertising
+     * and receiving graceful restart capability to/from peer.
+     *
+     * @return graceful restart operational mode
+     */
+    Mode getMode();
 }
