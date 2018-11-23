c2cea1811648cf280ea84b7213f585070e56f497
Matej Perina <matej.perina@pantheon.tech>
2018-11-23 10:32:16 +0100

    operational state of (LL)GR procedures
    State of (LL)GR operations (restart time, enabled afi-safi...)
    is now accessible in operational datastore.
    
    
    JIRA: BGPCEP-809
    Change-Id: If03b74e67750970a08cfdc34ae402cbe8283da15
    Signed-off-by: Matej Perina <matej.perina@pantheon.tech>

---
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/state/BGPLlGracelfulRestartState.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.rib.spi.state;
+
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.TablesKey;
+
+/**
+ * BGP Operational Graceful Restart State.
+ */
+public interface BGPLlGracelfulRestartState {
+    /**
+     * is Long-lived Graceful Restart Support advertised to neighbor.
+     *
+     * @param tablesKey tables Key
+     * @return true if Afi Safi was advertised to neighbor
+     */
+    boolean isLlGracefulRestartAdvertised(@Nonnull TablesKey tablesKey);
+
+    /**
+     * is Long-lived Graceful Restart Support advertised by neighbor.
+     *
+     * @param tablesKey tables Key
+     * @return true if Afi Safi was advertised by neighbor
+     */
+    boolean isLlGracefulRestartReceived(@Nonnull TablesKey tablesKey);
+
+    /**
+     * If table is both advertised and received return timer with lower value.
+     * If table is not advertised or received return zero.
+     *
+     * @param tablesKey tables key
+     * @return effective value of timer in seconds
+     */
+    int getLlGracefulRestartTimer(@Nonnull TablesKey tablesKey);
+}
