ae6526e089b50ead21c2efbc4639c746c2432cbc
Robert Varga <robert.varga@pantheon.tech>
2018-12-03 00:44:54 +0100

    Add RevisedErrorHandlingSupport ParserConstraint
    We need to communicate to the parser whether RFC7606 is enabled
    and whether the peer is internal or external. This patch adds
    the interface to do that.
    
    JIRA: BGPCEP-359
    Change-Id: Id123c4d25019bd1be1ec7b228cd010c81186a4a4
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/pojo/RevisedErrorHandlingSupportImpl.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.parser.spi.pojo;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.opendaylight.protocol.bgp.parser.spi.RevisedErrorHandlingSupport;
+
+@NonNullByDefault
+public final class RevisedErrorHandlingSupportImpl implements RevisedErrorHandlingSupport {
+    private static final RevisedErrorHandlingSupportImpl EXTERNAL_PEER = new RevisedErrorHandlingSupportImpl(true);
+    private static final RevisedErrorHandlingSupportImpl INTERNAL_PEER = new RevisedErrorHandlingSupportImpl(false);
+
+    private final boolean externalPeer;
+
+    private RevisedErrorHandlingSupportImpl(final boolean externalPeer) {
+        this.externalPeer = externalPeer;
+    }
+
+    public static RevisedErrorHandlingSupportImpl forInternalPeer() {
+        return INTERNAL_PEER;
+    }
+
+    public static RevisedErrorHandlingSupportImpl forExternalPeer() {
+        return EXTERNAL_PEER;
+    }
+
+    @Override
+    public boolean isExternalPeer() {
+        return externalPeer;
+    }
+}
