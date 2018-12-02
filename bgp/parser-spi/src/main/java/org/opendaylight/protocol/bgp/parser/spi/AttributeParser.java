6bf8df2b3f16a8c7aff285d27a2a3dce6b462d60
Robert Varga <robert.varga@pantheon.tech>
2018-12-03 00:49:24 +0100

    Distinguish between ignored and erroring attributes
    MP_REACH/MP_UNREACH attributes need to cause a BGPDocumentedException
    to be thrown when in RFC7606 mode. This adds that capability and
    makes parser report it.
    
    Change-Id: Iabbd16e64332ad9ffd823e942262b7ba324ad328
    JIRA: MDSAL-359
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/AttributeParser.java
+++ bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/AttributeParser.java
@@ -11,6 +11,7 @@ import io.netty.buffer.ByteBuf;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
+import org.opendaylight.protocol.bgp.parser.BGPError;
 import org.opendaylight.protocol.bgp.parser.BGPParsingException;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.AttributesBuilder;
 
@@ -28,4 +29,16 @@ public interface AttributeParser {
      */
     void parseAttribute(@Nonnull ByteBuf buffer, @Nonnull AttributesBuilder builder,
             @Nullable PeerSpecificParserConstraint constraint) throws BGPDocumentedException, BGPParsingException;
+
+    /**
+     * Determine whether a duplicate attribute should be ignored or {@link BGPError#MALFORMED_ATTR_LIST} should be
+     * raised. This is useful for MP_REACH/MP_UNREACH attributes, which need to emit a Notification under RFC7606
+     * rules.
+     *
+     * @param errorHandling Revised error handling type
+     * @return True if the duplicate attribute should be ignored, false if a BGPError should be raised.
+     */
+    default boolean ignoreDuplicates(final @Nonnull RevisedErrorHandling errorHandling) {
+        return true;
+    }
 }
