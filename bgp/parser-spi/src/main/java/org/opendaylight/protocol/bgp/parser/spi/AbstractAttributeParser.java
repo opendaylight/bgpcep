78bacae2d3a90f48ddb77727465517146778d5c4
Robert Varga <robert.varga@pantheon.tech>
2018-12-03 02:27:51 +0100

    Add AbstractAttributeParser
    While AttributeParser makes revised error handling optional, this
    class makes it mandatory, providing a common baseline for parsers
    dealing with attributes which have revised error handling specified.
    
    Change-Id: I47de7426f811962f6771a5925486b133986f0a44
    JIRA: BGPCEP-359
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/AbstractAttributeParser.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.parser.spi;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
+import org.opendaylight.protocol.bgp.parser.BGPParsingException;
+import org.opendaylight.protocol.bgp.parser.BGPTreatAsWithdrawException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.AttributesBuilder;
+
+/**
+ * Abstract base class for AttributeParsers which are aware of {@link RevisedErrorHandling}. Inheriting from this class
+ * requires that {@link #parseAttribute(ByteBuf, AttributesBuilder, RevisedErrorHandling, PeerSpecificParserConstraint)}
+ * is provided and the simplified {@link #parseAttribute(ByteBuf, AttributesBuilder, PeerSpecificParserConstraint)}
+ * is implemented by this class.
+ *
+ * @author Robert Varga
+ */
+public abstract class AbstractAttributeParser implements AttributeParser {
+    @Override
+    public final void parseAttribute(final ByteBuf buffer, final AttributesBuilder builder,
+            final PeerSpecificParserConstraint constraint) throws BGPDocumentedException, BGPParsingException {
+        try {
+            parseAttribute(buffer, builder, RevisedErrorHandling.NONE, constraint);
+        } catch (BGPTreatAsWithdrawException e) {
+            throw e.toDocumentedException();
+        }
+    }
+
+    @Override
+    public abstract void parseAttribute(ByteBuf buffer, AttributesBuilder builder, RevisedErrorHandling errorHandling,
+            PeerSpecificParserConstraint constraint) throws BGPDocumentedException, BGPParsingException,
+            BGPTreatAsWithdrawException;
+}
