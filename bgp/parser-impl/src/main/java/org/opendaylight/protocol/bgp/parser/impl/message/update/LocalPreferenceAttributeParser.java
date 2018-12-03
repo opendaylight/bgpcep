084dd105995f9b8cdec93c4983cdefde4f848946
Robert Varga <robert.varga@pantheon.tech>
2018-12-03 02:27:51 +0100

    Add AbstractAttributeParser
    While AttributeParser makes revised error handling optional, this
    class makes it mandatory, providing a common baseline for parsers
    dealing with attributes which have revised error handling specified.
    
    Also migrate parsers for following attributes to use it:
    - ORIGIN (Section 7.1)
    - AS_PATH (Section 7.2)
    - NEXT_HOP (Section 7.3)
    - MULTI_EXIT_DISC (Section 7.4)
    - LOCAL_PREF (Section 7.5)
    - ATOMIC_AGGREGATE (Section 7.6)
    - AGGREGATOR (Section 7.7)
    - Community (Section 7.8)
    - ORIGINATOR_ID (Section 7.9)
    - CLUSTER_LIST (Section 7.10)
    - MP_REACH (Section 7.11)
    - MP_UNREACH (Section 7.12)
    - Extended Community (Section 7.14)
    
    JIRA: BGPCEP-359
    Change-Id: I47de7426f811962f6771a5925486b133986f0a44
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/LocalPreferenceAttributeParser.java
+++ bgp/parser-impl/src/main/java/org/opendaylight/protocol/bgp/parser/impl/message/update/LocalPreferenceAttributeParser.java
@@ -9,25 +9,41 @@ package org.opendaylight.protocol.bgp.parser.impl.message.update;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import org.opendaylight.protocol.bgp.parser.spi.AttributeParser;
+import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
+import org.opendaylight.protocol.bgp.parser.BGPError;
+import org.opendaylight.protocol.bgp.parser.BGPTreatAsWithdrawException;
+import org.opendaylight.protocol.bgp.parser.spi.AbstractAttributeParser;
 import org.opendaylight.protocol.bgp.parser.spi.AttributeSerializer;
 import org.opendaylight.protocol.bgp.parser.spi.AttributeUtil;
 import org.opendaylight.protocol.bgp.parser.spi.PeerSpecificParserConstraint;
+import org.opendaylight.protocol.bgp.parser.spi.RevisedErrorHandling;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.Attributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.AttributesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.attributes.LocalPref;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.attributes.LocalPrefBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public final class LocalPreferenceAttributeParser implements AttributeParser, AttributeSerializer {
+public final class LocalPreferenceAttributeParser extends AbstractAttributeParser implements AttributeSerializer {
+    private static final Logger LOG = LoggerFactory.getLogger(LocalPreferenceAttributeParser.class);
 
     public static final int TYPE = 5;
 
     @Override
     public void parseAttribute(final ByteBuf buffer, final AttributesBuilder builder,
-            final PeerSpecificParserConstraint constraint) {
-        // FIXME: BGPCEP-359:
-        //        - if peer is external: attribute discard
-        //        - if peer is internal: treat-as-withdraw if length != 4
+            final RevisedErrorHandling errorHandling, final PeerSpecificParserConstraint constraint)
+                    throws BGPDocumentedException, BGPTreatAsWithdrawException {
+        if (errorHandling == RevisedErrorHandling.EXTERNAL) {
+            // RFC7606 section 7.7
+            LOG.debug("Discarded LOCAL_PREF attribute from external peer");
+            return;
+        }
+
+        final int readable = buffer.readableBytes();
+        if (readable != 4) {
+            throw errorHandling.reportError(BGPError.ATTR_LENGTH_ERROR, "Expected 4 bytes, have %s", readable);
+        }
+
         builder.setLocalPref(new LocalPrefBuilder().setPref(buffer.readUnsignedInt()).build());
     }
 
