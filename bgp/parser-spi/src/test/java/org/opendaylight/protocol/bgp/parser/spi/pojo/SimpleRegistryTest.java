9e08242a43b53c870d8a4c96002f3c531ec180a0
Robert Varga <robert.varga@pantheon.tech>
2018-12-03 02:18:21 +0100

    Migrate Matchers.any()
    This method has been moved to ArgumentMatchers, make the switch.
    
    Change-Id: I710791942f2f342e636cc77713e8867b3a75cd32
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/parser-spi/src/test/java/org/opendaylight/protocol/bgp/parser/spi/pojo/SimpleRegistryTest.java
+++ bgp/parser-spi/src/test/java/org/opendaylight/protocol/bgp/parser/spi/pojo/SimpleRegistryTest.java
@@ -10,7 +10,7 @@ package org.opendaylight.protocol.bgp.parser.spi.pojo;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
