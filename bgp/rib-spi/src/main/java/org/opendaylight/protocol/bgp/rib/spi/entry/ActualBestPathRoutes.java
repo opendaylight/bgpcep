28fa5d1095933a7b5763bd23142963072d1d5cce
Robert Varga <robert.varga@pantheon.tech>
2018-12-05 22:22:27 +0100

    Merge ActualBestPathRoutes and AdvertizedRoute
    These two classes are almost identical, extract the common bits
    to a common superclass, reducing complexity and allowing them
    us to share codepaths.
    
    Change-Id: Ib6bbf1e4285ed0f1376d932367c8cb0d3f6f2662
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/entry/ActualBestPathRoutes.java
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/entry/ActualBestPathRoutes.java
@@ -8,8 +8,6 @@
 
 package org.opendaylight.protocol.bgp.rib.spi.entry;
 
-import static org.opendaylight.protocol.bgp.parser.spi.PathIdUtil.NON_PATH_ID_VALUE;
-
 import org.opendaylight.protocol.bgp.rib.spi.RIBSupport;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.path.attributes.Attributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.PeerId;
@@ -28,43 +26,11 @@ import org.opendaylight.yangtools.yang.binding.Identifier;
  * @author Claudio D. Gasparini
  */
 public final class ActualBestPathRoutes<C extends Routes & DataObject & ChoiceIn<Tables>,
-        S extends ChildOf<? super C>,
-        R extends Route & ChildOf<? super S> & Identifiable<I>,
-        I extends Identifier<R>> implements RouteKeyIdentifier<R,I> {
-    private final PeerId fromPeerId;
-    private final R route;
-    private final Attributes attributes;
-    private final I nonAddPathRouteKeyIdentifier;
-    private final I addPathRouteKeyIdentifier;
+        S extends ChildOf<? super C>, R extends Route & ChildOf<? super S> & Identifiable<I>,
+        I extends Identifier<R>> extends AbstractAdvertizedRoute<C, S, R, I> {
 
     public ActualBestPathRoutes(final RIBSupport<C, S, R, I> ribSupport, final R route, final PeerId fromPeerId,
             final Attributes attributes) {
-        this.fromPeerId = fromPeerId;
-        this.route = route;
-        this.attributes = attributes;
-        this.nonAddPathRouteKeyIdentifier
-                = ribSupport.createRouteListKey(NON_PATH_ID_VALUE, route.getRouteKey());
-        this.addPathRouteKeyIdentifier
-                = ribSupport.createRouteListKey(route.getPathId().getValue(), route.getRouteKey());
-    }
-
-    public PeerId getFromPeerId() {
-        return this.fromPeerId;
-    }
-
-    public R getRoute() {
-        return route;
-    }
-
-    public Attributes getAttributes() {
-        return attributes;
-    }
-
-    public I getNonAddPathRouteKeyIdentifier() {
-        return this.nonAddPathRouteKeyIdentifier;
-    }
-
-    public I getAddPathRouteKeyIdentifier() {
-        return this.addPathRouteKeyIdentifier;
+        super(ribSupport, route, fromPeerId, attributes);
     }
 }
