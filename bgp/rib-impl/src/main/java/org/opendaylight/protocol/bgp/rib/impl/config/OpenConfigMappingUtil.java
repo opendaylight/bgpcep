70c5d43cc5b46ca91e1901affe02d2e3d58e2426
Matej Perina <matej.perina@pantheon.tech>
2018-12-09 11:15:13 +0100

    add decoder constraint based on treat-as-withdrawn configuration
    Uses openconfig peer/peer-group -> error-handling ->
    treat-as-withdrawn for configuring whether treat-as-withdrawn
    procedures should take place.
    
    JIRA: BGPCEP-359
    Change-Id: I0fd34df7fbb500b4c7fa7012b2f0cfbef8c1f5e1
    Signed-off-by: Matej Perina <matej.perina@pantheon.tech>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/OpenConfigMappingUtil.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/OpenConfigMappingUtil.java
@@ -25,6 +25,8 @@ import org.opendaylight.protocol.bgp.mode.api.PathSelectionMode;
 import org.opendaylight.protocol.bgp.mode.impl.add.all.paths.AllPathSelection;
 import org.opendaylight.protocol.bgp.mode.impl.add.n.paths.AddPathBestNPathSelection;
 import org.opendaylight.protocol.bgp.openconfig.spi.BGPTableTypeRegistryConsumer;
+import org.opendaylight.protocol.bgp.parser.spi.RevisedErrorHandlingSupport;
+import org.opendaylight.protocol.bgp.parser.spi.pojo.RevisedErrorHandlingSupportImpl;
 import org.opendaylight.protocol.concepts.KeyMapping;
 import org.opendaylight.protocol.util.Ipv4Util;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.BgpCommonAfiSafiList;
@@ -34,6 +36,7 @@ import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.BgpNe
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.BgpNeighborGroup;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.BgpNeighborTransportConfig;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.graceful.restart.GracefulRestart;
+import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.ErrorHandling;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.RouteReflector;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.Timers;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.Transport;
@@ -475,6 +478,45 @@ final class OpenConfigMappingUtil {
             final BgpNeighborTransportConfig.LocalAddress localAddress = transport.getConfig().getLocalAddress();
             if (localAddress != null ) {
                 return localAddress.getIpAddress();
+            }
+        }
+        return null;
+    }
+
+    @Nullable
+    static RevisedErrorHandlingSupport getRevisedErrorHandling(final PeerRole role,final PeerGroup peerGroup,
+            final Neighbor neighbor) {
+        org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.error.handling.Config
+            config = null;
+        if (neighbor != null) {
+            final ErrorHandling errorHandling = neighbor.getErrorHandling();
+            if (errorHandling != null) {
+                config = errorHandling.getConfig();
+            }
+        }
+        if (config == null && peerGroup != null) {
+            final ErrorHandling errorHandling = peerGroup.getErrorHandling();
+            if (errorHandling != null) {
+                config = errorHandling.getConfig();
+            }
+        }
+
+        return config == null ? null : getRevisedErrorHandling(role, config);
+    }
+
+    @Nullable
+    private static RevisedErrorHandlingSupport getRevisedErrorHandling(final PeerRole role, final org.opendaylight.yang
+            .gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.error.handling.Config config) {
+        if (config.isTreatAsWithdraw()) {
+            switch (role) {
+                case Ebgp:
+                    return RevisedErrorHandlingSupportImpl.forExternalPeer();
+                case Ibgp:
+                case Internal:
+                case RrClient:
+                    return RevisedErrorHandlingSupportImpl.forInternalPeer();
+                default:
+                    throw new IllegalStateException("Unhandled role " + role);
             }
         }
         return null;
