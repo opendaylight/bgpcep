3179ee14b7bdb4c1d1a631db52bd9e114ed4187e
Robert Varga <robert.varga@pantheon.tech>
2018-12-13 21:14:30 +0100

    Cleanup bgp-openconfig-api helpers
    This cleans up the custom-coded helpers to work more efficiently,
    fixing eclipse warnings at the same time.
    
    Change-Id: I3724a478b73e27382bc1f49766603e16a92396e5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/openconfig-api/src/main/java/org/opendaylight/yang/gen/v1/http/openconfig/net/yang/network/instance/types/rev151018/RouteDistinguisherBuilder.java
+++ bgp/openconfig-api/src/main/java/org/opendaylight/yang/gen/v1/http/openconfig/net/yang/network/instance/types/rev151018/RouteDistinguisherBuilder.java
@@ -5,40 +5,26 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.yang.gen.v1.http.openconfig.net.yang.network.instance.types.rev151018;
 
 import java.util.regex.Pattern;
+import org.opendaylight.yangtools.yang.binding.CodeHelpers;
 
 /**
- *
  * Helper builder utility for {@code RouteDistinguisher} union type.
- *
  */
 public final class RouteDistinguisherBuilder {
+    private static final Pattern[] PATTERNS = CodeHelpers.compilePatterns(RouteDistinguisher.PATTERN_CONSTANTS);
 
     private RouteDistinguisherBuilder() {
         throw new UnsupportedOperationException();
     }
 
-    private static final Pattern[] PATTERNS;
-
-    static {
-        final Pattern a[] = new Pattern[RouteDistinguisher.PATTERN_CONSTANTS.size()];
-        int i = 0;
-        for (final String regEx : RouteDistinguisher.PATTERN_CONSTANTS) {
-            a[i++] = Pattern.compile(regEx);
-        }
-
-        PATTERNS = a;
-    }
-
     public static RouteDistinguisher getDefaultInstance(final String defaultValue) {
         if (anyMatch(defaultValue)) {
             return new RouteDistinguisher(defaultValue);
-        } else {
-            throw new IllegalArgumentException("Cannot create RouteDistinguisher from " + defaultValue);
         }
+        throw new IllegalArgumentException("Cannot create RouteDistinguisher from " + defaultValue);
     }
 
     private static boolean anyMatch(final String defaultValue) {
