b806b5fab9aff352b0132fbe1b65237bf68bfff5
Robert Varga <robert.varga@pantheon.tech>
2018-12-13 21:14:30 +0100

    Cleanup bgp-openconfig-api helpers
    This cleans up the custom-coded helpers to work more efficiently,
    fixing eclipse warnings at the same time.
    
    Change-Id: I3724a478b73e27382bc1f49766603e16a92396e5
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/openconfig-api/src/main/java/org/opendaylight/yang/gen/v1/http/openconfig/net/yang/bgp/types/rev151009/BgpStdCommunityTypeBuilder.java
+++ bgp/openconfig-api/src/main/java/org/opendaylight/yang/gen/v1/http/openconfig/net/yang/bgp/types/rev151009/BgpStdCommunityTypeBuilder.java
@@ -5,39 +5,26 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009;
 
-import com.google.common.primitives.UnsignedInts;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
- * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
- * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
- *
- * The reason behind putting it under src/main/java is:
- * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
- * loss of user code.
- *
+ * Customized handler for instantiating {@link BgpStdCommunityType} from a String.
  */
-public class BgpStdCommunityTypeBuilder {
-
-    private static final Pattern COMM_TYPE_PATTERN = Pattern.compile("^([0-9]+:[0-9]+)$");
-
-    public static BgpStdCommunityType getDefaultInstance(final java.lang.String defaultValue) {
-        final Matcher commMatcher = COMM_TYPE_PATTERN.matcher(defaultValue);
-
-        if (commMatcher.matches()) {
-            return new BgpStdCommunityType(defaultValue);
-        } else {
-            try {
-                final long parseUnsignedInt = UnsignedInts.parseUnsignedInt(defaultValue);
-                return new BgpStdCommunityType(parseUnsignedInt);
-            } catch (final NumberFormatException e) {
-                throw new IllegalArgumentException("Cannot create BgpStdCommunityType from " + defaultValue);
-            }
-        }
+public final class BgpStdCommunityTypeBuilder {
+    private BgpStdCommunityTypeBuilder() {
+        // Hidden
     }
 
+    public static BgpStdCommunityType getDefaultInstance(final String defaultValue) {
+        // uints cannot have a ':', which is allowed for four-octets
+        if (defaultValue.indexOf(':') != -1) {
+            return new BgpStdCommunityType(defaultValue);
+        }
+
+        try {
+            return new BgpStdCommunityType(Integer.toUnsignedLong(Integer.parseUnsignedInt(defaultValue)));
+        } catch (final NumberFormatException e) {
+            throw new IllegalArgumentException("Cannot create BgpStdCommunityType from " + defaultValue, e);
+        }
+    }
 }
