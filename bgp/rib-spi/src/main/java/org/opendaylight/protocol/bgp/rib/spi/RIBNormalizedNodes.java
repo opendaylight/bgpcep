ace7f23fbc5f098dcf52540df272e15891e915ae
Robert Varga <robert.varga@pantheon.tech>
2018-12-19 19:47:51 +0100

    Add adj-rib-in llgr-stale flag
    Add the ability to signal tables to be LLGR-stale, so that
    effective-rib-in can propagate this state properly towards
    local rib.
    
    This flag is picked up by before we are applying input policies,
    so that routes are transiently marked as LLGR_STALE. The policy
    can then decide whether to filter the route or modify its
    attributes.
    
    Once the policy has been applied and the flag is set, we check
    if NO_LLGR flag is present, and if that is the case, we remove
    that route.
    
    This mechanics is required to correctly allow policies to
    see LLGR_STALE routes as well as potentially remove the NO_LLGR
    flag.
    
    JIRA: BGPCEP-495
    Change-Id: Iac9815b29e97aee99a81d015194cd16fb362387d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/RIBNormalizedNodes.java
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/RIBNormalizedNodes.java
@@ -7,10 +7,13 @@
  */
 package org.opendaylight.protocol.bgp.rib.spi;
 
+import static org.opendaylight.protocol.bgp.rib.spi.RIBNodeIdentifiers.ATTRIBUTES_NID;
 import static org.opendaylight.protocol.bgp.rib.spi.RIBNodeIdentifiers.UPTODATE_NID;
 
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
 import org.opendaylight.yangtools.yang.data.api.schema.LeafNode;
 import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+import org.opendaylight.yangtools.yang.data.impl.schema.Builders;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNodes;
 
 /**
@@ -21,6 +24,11 @@ public final class RIBNormalizedNodes {
         UPTODATE_NID, Boolean.FALSE);
     public static final LeafNode<Boolean> ATTRIBUTES_UPTODATE_TRUE = ImmutableNodes.leafNode(
         UPTODATE_NID, Boolean.TRUE);
+
+    public static final ContainerNode NOT_UPTODATE_ATTRIBUTES = Builders.containerBuilder()
+            .withNodeIdentifier(ATTRIBUTES_NID).withChild(ATTRIBUTES_UPTODATE_FALSE).build();
+    public static final ContainerNode UPTODATE_ATTRIBUTES = Builders.containerBuilder()
+            .withNodeIdentifier(ATTRIBUTES_NID).withChild(ATTRIBUTES_UPTODATE_TRUE).build();
 
     private RIBNormalizedNodes() {
 
