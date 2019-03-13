eea9e865b82f92e204b48286b27f7b685c53b696
Robert Varga <robert.varga@pantheon.tech>
2019-03-13 12:48:43 +0100

    Fix checkstyle in TableTypeActivator
    Simple case of long lines, reformat.
    
    Change-Id: I45c3d1eb5892cf5ff37f3630aa7f8a8847e9c322
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/extensions/flowspec/src/main/java/org/opendaylight/protocol/bgp/flowspec/TableTypeActivator.java
+++ bgp/extensions/flowspec/src/main/java/org/opendaylight/protocol/bgp/flowspec/TableTypeActivator.java
@@ -5,10 +5,9 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.protocol.bgp.flowspec;
 
-import com.google.common.collect.Lists;
+import java.util.Arrays;
 import java.util.List;
 import org.opendaylight.protocol.bgp.openconfig.spi.AbstractBGPTableTypeRegistryProviderActivator;
 import org.opendaylight.protocol.bgp.openconfig.spi.BGPTableTypeRegistryProvider;
@@ -23,14 +22,17 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.type
 import org.opendaylight.yangtools.concepts.AbstractRegistration;
 
 public final class TableTypeActivator extends AbstractBGPTableTypeRegistryProviderActivator {
-
     @Override
-    protected List<AbstractRegistration> startBGPTableTypeRegistryProviderImpl(final BGPTableTypeRegistryProvider provider) {
-        return Lists.newArrayList(
-                provider.registerBGPTableType(Ipv4AddressFamily.class, FlowspecSubsequentAddressFamily.class, IPV4FLOW.class),
-                provider.registerBGPTableType(Ipv6AddressFamily.class, FlowspecSubsequentAddressFamily.class, IPV6FLOW.class),
-                provider.registerBGPTableType(Ipv4AddressFamily.class, FlowspecL3vpnSubsequentAddressFamily.class, IPV4L3VPNFLOW.class),
-                provider.registerBGPTableType(Ipv6AddressFamily.class, FlowspecL3vpnSubsequentAddressFamily.class, IPV6L3VPNFLOW.class));
+    protected List<AbstractRegistration> startBGPTableTypeRegistryProviderImpl(
+            final BGPTableTypeRegistryProvider provider) {
+        return Arrays.asList(
+            provider.registerBGPTableType(Ipv4AddressFamily.class, FlowspecSubsequentAddressFamily.class,
+                IPV4FLOW.class),
+            provider.registerBGPTableType(Ipv6AddressFamily.class, FlowspecSubsequentAddressFamily.class,
+                IPV6FLOW.class),
+            provider.registerBGPTableType(Ipv4AddressFamily.class, FlowspecL3vpnSubsequentAddressFamily.class,
+                IPV4L3VPNFLOW.class),
+            provider.registerBGPTableType(Ipv6AddressFamily.class, FlowspecL3vpnSubsequentAddressFamily.class,
+                IPV6L3VPNFLOW.class));
     }
-
 }
