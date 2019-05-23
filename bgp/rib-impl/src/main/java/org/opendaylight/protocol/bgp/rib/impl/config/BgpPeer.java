7ca9f2a314561d23e22171766f37266332172580
Luis Gomez <ecelgp@gmail.com>
2019-05-23 03:15:55 +0000

    Revert "remove OSGi dependency from bgp modules"
    Due to bug BGPCEP-873
    
    This reverts commit 69986c1d0f73bdbde96441e6709f98e3ad7f37a5.
    
    Change-Id: I2b306edb22f10012279197b0331c62fab013c1d0
    Signed-off-by: Luis Gomez <ecelgp@gmail.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/BgpPeer.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/BgpPeer.java
@@ -63,6 +63,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.TablesKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev180329.ClusterIdentifier;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.osgi.framework.ServiceRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -71,6 +72,8 @@ public final class BgpPeer implements PeerBean, BGPPeerStateConsumer {
     private static final Logger LOG = LoggerFactory.getLogger(BgpPeer.class);
 
     private final RpcProviderRegistry rpcRegistry;
+    @GuardedBy("this")
+    private ServiceRegistration<?> serviceRegistration;
     @GuardedBy("this")
     private Neighbor currentConfiguration;
     @GuardedBy("this")
@@ -145,7 +148,12 @@ public final class BgpPeer implements PeerBean, BGPPeerStateConsumer {
     }
 
     @Override
-    public synchronized void close() {}
+    public synchronized void close() {
+        if (this.serviceRegistration != null) {
+            this.serviceRegistration.unregister();
+            this.serviceRegistration = null;
+        }
+    }
 
     @Override
     public synchronized void instantiateServiceInstance() {
@@ -195,6 +203,10 @@ public final class BgpPeer implements PeerBean, BGPPeerStateConsumer {
             return null;
         }
         return this.bgpPeerSingletonService.getPeerState();
+    }
+
+    synchronized void setServiceRegistration(final ServiceRegistration<?> serviceRegistration) {
+        this.serviceRegistration = serviceRegistration;
     }
 
     synchronized void removePeer(final BGPPeerRegistry bgpPeerRegistry) {
