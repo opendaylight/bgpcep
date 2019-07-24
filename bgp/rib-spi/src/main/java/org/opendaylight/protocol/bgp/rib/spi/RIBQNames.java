962318816ffb4e9674041591b59c97d153853957
Robert Varga <robert.varga@pantheon.tech>
2019-07-24 12:38:30 +0200

    WIP: convert LocRibWriter to DOM APIs
    This is a preliminary conversion, pushing down the changes.
    
    // FIXME: track down UT failures
    // FIXME: minimize RIBSupport methods
    // FIXME: RIBSupport should lose most of its args
    
    Change-Id: If6b22adf8f9a15c744ed873a5e114a251023c7a8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/RIBQNames.java
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/RIBQNames.java
@@ -7,19 +7,24 @@
  */
 package org.opendaylight.protocol.bgp.rib.spi;
 
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.bgp.rib.rib.Peer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.Tables;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.tables.Attributes;
 import org.opendaylight.yangtools.yang.common.QName;
 
 /**
  * Utility constant {@link QName}s for various RIB constructs which are not covered by generated DTOs' QNAME constants.
  */
+@NonNullByDefault
 public final class RIBQNames {
     public static final QName UPTODATE_QNAME = QName.create(Attributes.QNAME, "uptodate").intern();
     public static final QName PEER_ID_QNAME = QName.create(Peer.QNAME, "peer-id").intern();
     public static final QName LLGR_STALE_QNAME = QName.create(Attributes.QNAME, "llgr-stale").intern();
+    public static final QName AFI_QNAME = QName.create(Tables.QNAME, "afi").intern();
+    public static final QName SAFI_QNAME = QName.create(Tables.QNAME, "safi").intern();
 
     private RIBQNames() {
-
+        // Hidden on purpose
     }
 }
