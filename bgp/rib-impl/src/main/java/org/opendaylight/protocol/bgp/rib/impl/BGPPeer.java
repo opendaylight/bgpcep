73c80c579b9759b3c20fa00d537975f3378633e4
Ajay Lele <alele@luminanetworks.com>
2019-08-29 14:37:58 -0700

    Fix NPE while accessing DomTxChain when bgp/app peer singleton service is restarted
    Due to cluster partition/heal, singleton service associated with bgp/app
    peer instance can get restarted. Create/close of DomTxChain instance used
    by the service was not being handled properly resulting in NPE and failure
    to register the peer which caused the BGP connection to not get reestablished.
    
    JIRA: BGPCEP-878
    Change-Id: I436ad0877db19c65463bf7b7e09faa3b2b42e5a0
    Signed-off-by: Ajay Lele <ajayslele@gmail.com>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/BGPPeer.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/BGPPeer.java
@@ -235,6 +235,7 @@ public class BGPPeer extends AbstractPeer implements BGPSessionListener {
     }
 
     public synchronized void instantiateServiceInstance() {
+        createDomChain();
         this.ribWriter = AdjRibInWriter.create(this.rib.getYangRibId(), this.peerRole, this);
         setActive(true);
     }
