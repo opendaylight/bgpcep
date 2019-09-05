1b9dd3a4ce4a7f103544ec713e3383fcc1fc21e9
Robert Varga <robert.varga@pantheon.tech>
2019-09-05 18:36:01 +0200

    Bump odlparent to 6.0.0
    This bumps odlparent to 6.0.0 as part of Magnesium MRI.
    
    Change-Id: I19505dd80b832cd6ed8b68daf471d9e90d3dfdd2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/openconfig-rp-statement/src/main/java/org/opendaylight/protocol/bgp/openconfig/routing/policy/statement/conditions/MatchBgpNeighborSetHandler.java
+++ bgp/openconfig-rp-statement/src/main/java/org/opendaylight/protocol/bgp/openconfig/routing/policy/statement/conditions/MatchBgpNeighborSetHandler.java
@@ -12,6 +12,7 @@ import static java.util.Objects.requireNonNull;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -62,6 +63,8 @@ public final class MatchBgpNeighborSetHandler
         this.dataBroker = requireNonNull(dataBroker);
     }
 
+    @SuppressFBWarnings(value = "UPM_UNCALLED_PRIVATE_METHOD",
+            justification = "https://github.com/spotbugs/spotbugs/issues/811")
     private List<PeerId> loadRoleSets(final String key) throws ExecutionException, InterruptedException {
         final ReadTransaction tr = this.dataBroker.newReadOnlyTransaction();
         final Optional<NeighborSet> result = tr.read(LogicalDatastoreType.CONFIGURATION,
