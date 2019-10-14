7571dd52d5330a9385842cf10806fd5fa3b16f4c
Robert Varga <robert.varga@pantheon.tech>
2019-10-14 10:56:20 +0200

    WIP: Add ByteBufUtil
    This adds proper helpers to interface Uint types and ByteBuf. This
    supersedes ByteBufWriteUtil.
    
    Change-Id: I8466dbff77f8469278df4eb92a2aaebcbdeeb6b8
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ util/src/main/java/org/opendaylight/protocol/util/ByteBufUtil.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.util;
+
+import io.netty.buffer.ByteBuf;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yangtools.yang.common.Uint16;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.opendaylight.yangtools.yang.common.Uint64;
+import org.opendaylight.yangtools.yang.common.Uint8;
+
+/**
+ * Utility methods from interacting ByteBufs.
+ */
+public final class ByteBufUtil {
+    private ByteBufUtil() {
+
+    }
+
+    public static @NonNull Uint8 readUint8(final ByteBuf buf) {
+        return Uint8.fromByteBits(buf.readByte());
+    }
+
+    public static @NonNull Uint16 readUint16(final ByteBuf buf) {
+        return Uint16.fromShortBits(buf.readShort());
+    }
+
+    public static @NonNull Uint32 readUint32(final ByteBuf buf) {
+        return Uint32.fromIntBits(buf.readInt());
+    }
+
+    public static @NonNull Uint64 readUint64(final ByteBuf buf) {
+        return Uint64.fromLongBits(buf.readLong());
+    }
+
+    public static void writeNonNull(final ByteBuf buf, final Uint8 uint) {
+        buf.writeByte(uint.byteValue());
+    }
+
+    public static void writeNonNull(final ByteBuf buf, final Uint16 uint) {
+        buf.writeShort(uint.shortValue());
+    }
+
+    public static void writeNonNull(final ByteBuf buf, final Uint32 uint) {
+        buf.writeInt(uint.intValue());
+    }
+
+    public static void writeNonNull(final ByteBuf buf, final Uint64 uint) {
+        buf.writeLong(uint.longValue());
+    }
+}
