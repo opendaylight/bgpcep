265383c5a2fb407c13bb807ab2e35cc62d07fa42
Robert Varga <robert.varga@pantheon.tech>
2019-10-21 10:20:07 +0200

    Fix rsvp.yang
    This model is not valid yang, as it is defining overlapping leaves
    in choice/case statements. This fixes the violations by providing
    encapsulating containers, i.e. the content within each case is also
    put into a container.
    
    JIRA: BGPCEP-880
    Change-Id: I55d1ac7491b2985a29558ec88f90919771b2a753
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit e119dd71b0559dd35abb8dc8b0e3675c14db7aec)

--- rsvp/impl/src/main/java/org/opendaylight/protocol/rsvp/parser/impl/te/ReoptimizationBandwidthObjectParser.java
+++ rsvp/impl/src/main/java/org/opendaylight/protocol/rsvp/parser/impl/te/ReoptimizationBandwidthObjectParser.java
@@ -5,7 +5,6 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.protocol.rsvp.parser.impl.te;
 
 import com.google.common.base.Preconditions;
@@ -16,8 +15,8 @@ import org.opendaylight.protocol.rsvp.parser.spi.subobjects.AbstractRSVPObjectPa
 import org.opendaylight.protocol.util.ByteArray;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.concepts.rev131125.Bandwidth;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.RsvpTeObject;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.bandwidth.object.bandwidth.object.ReoptimizationBandwidthObject;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.bandwidth.object.bandwidth.object.ReoptimizationBandwidthObjectBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.bandwidth.object.bandwidth.object.reoptimization.bandwidth.object._case.ReoptimizationBandwidthObject;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.bandwidth.object.bandwidth.object.reoptimization.bandwidth.object._case.ReoptimizationBandwidthObjectBuilder;
 
 public final class ReoptimizationBandwidthObjectParser extends AbstractRSVPObjectParser {
     public static final short CLASS_NUM = 5;
