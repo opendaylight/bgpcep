b140e9eda2293c4478cbb7eaf3e3f1d591bd425b
Robert Varga <robert.varga@pantheon.tech>
2019-11-26 16:13:59 +0100

    Switch TerminationReason to use Uint8
    As binding has switched to more expressive Uint types, embrace them
    to allow smaller number of conversions.
    
    Change-Id: I353750e1ed9fb8904b1f20be44f25d9c09592213
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/api/src/main/java/org/opendaylight/protocol/pcep/TerminationReason.java
+++ pcep/api/src/main/java/org/opendaylight/protocol/pcep/TerminationReason.java
@@ -10,6 +10,7 @@ package org.opendaylight.protocol.pcep;
 import static com.google.common.base.Verify.verify;
 
 import java.util.Arrays;
+import org.opendaylight.yangtools.yang.common.Uint8;
 
 public enum TerminationReason {
     UNKNOWN(1),
@@ -25,19 +26,20 @@ public enum TerminationReason {
         final TerminationReason[] reasons = TerminationReason.values();
         verify(reasons.length > 0);
 
-        final short highest = Arrays.stream(reasons).map(TerminationReason::getShortValue).max(Short::compareTo).get();
+        final short highest = Arrays.stream(reasons).map(TerminationReason::getUintValue).max(Uint8::compareTo).get()
+                .toJava();
         final TerminationReason[] init = new TerminationReason[highest + 1];
         for (TerminationReason reason : reasons) {
-            init[reason.getShortValue()] = reason;
+            init[reason.getUintValue().toJava()] = reason;
         }
 
         REASONS = init;
     }
 
-    private short value;
+    private final Uint8 value;
 
     TerminationReason(final int value) {
-        this.value = (short) value;
+        this.value = Uint8.valueOf(value);
     }
 
     /**
@@ -45,7 +47,7 @@ public enum TerminationReason {
      *
      * @return short value
      */
-    public short getShortValue() {
+    public Uint8 getUintValue() {
         return value;
     }
 
@@ -55,7 +57,8 @@ public enum TerminationReason {
      * @param valueArg corresponding to Termination reason
      * @return corresponding TerminationReason item
      */
-    public static TerminationReason forValue(final short valueArg) {
-        return valueArg < 0 || valueArg >= REASONS.length ? null : REASONS[valueArg];
+    public static TerminationReason forValue(final Uint8 valueArg) {
+        final short value = valueArg.toJava();
+        return value >= REASONS.length ? null : REASONS[value];
     }
 }
