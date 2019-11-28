5077f764cb0fbf84a1a8f90b02ab6bcbf944393f
Olivier Dugeon <olivier.dugeon@orange.com>
2019-11-28 15:40:07 +0100

    Graph modelisation for Path Computation Algorithm
    Initial commit of Graph Model.
    
    This is the patch set 1/3 to implement the Path Computation Algorithms
    to implement a full feature PCE server in conformity to RFC5440.
    
    Details information about the yang model, REST and Java API are
    provided in docs/graph directory.
    
    JIRA: BGPCEP-858
    Change-Id: Icf7b8320185f9d94377c76cdaecba836b7ef3bc5
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>
    Co-authored-by: Philippe Niger <philippe.niger@orange.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ graph/graph-api/src/main/java/org/opendaylight/graph/ConnectedEdge.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019 Orange.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.graph;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.Edge;
+
+/**
+ * Connected Edge class is the connected version of the Edge class from the graph yang model.
+ *
+ * <p>
+ * It is composed of a reference to the associated Edge class from the Graph class,
+ * a unique Key identifier in the associated Connected Graph,
+ * and two references to the associated Connected Vertex in the connected Graph: source and destination.
+ * <pre>
+ * {@code
+ * ---------------------------                        --------------------------------
+ * | Source Connected Vertex |---- Connected Edge --->| Destination Connected Vertex |
+ * ---------------------------                        --------------------------------
+ * }
+ * </pre>
+ *
+ * @author Olivier Dugeon
+ * @author Philippe Niger
+ */
+public interface ConnectedEdge {
+    /**
+     * Returns unique key associated to this Connected Edge.
+     *
+     * @return Edge Key
+     */
+    long getKey();
+
+    /**
+     * Returns the Edge from the Graph associated to this connection.
+     *
+     * @return Edge associated to this connection
+     */
+    @NonNull Edge getEdge();
+
+    /**
+     * Returns the source Connected Vertex from the Connected Graph associated to this Connected Edge.
+     *
+     * @return Source Connected Vertex
+     */
+    ConnectedVertex getSource();
+
+    /**
+     * Returns the destination Connected Vertex from the Connected Graph associated to this Connected Edge.
+     *
+     * @return Destination Connected Vertex
+     */
+    ConnectedVertex getDestination();
+
+    String toString();
+}
---
+++ graph/graph-api/src/main/java/org/opendaylight/graph/ConnectedGraph.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) 2019 Orange.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.graph;
+
+import java.util.ArrayList;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpPrefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.Graph;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.Edge;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.EdgeKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.Vertex;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.VertexKey;
+
+/**
+ * Connected Graph class is the connected version of the Graph class from the graph yang model.
+ *
+ * <p>
+ * Connected Graph is composed by Connected Vertex, Connected Edges and Prefix. It models Unidirectional Connected
+ * Graph (see graph theory). So, Edge and Connected Edge are unidirectional, thus to connect bidirectionally 2 vertices
+ * Va and Vb, it is necessary to setup 2 edges: Va to Vb and Vb to Va.
+ * <pre>
+ * {@code
+ *        --------------     ---------------------------    --------------
+ *        | Connected  |---->| Connected Edge Va to Vb |--->| Connected  |
+ *   ---->|  Vertex    |     ---------------------------    |  Vertex    |---->
+ *        |            |                                    |            |
+ *        | - Key (Va) |                                    | - Key (Vb) |
+ *   <----| - Vertex   |     ---------------------------    | - Vertex   |<----
+ *        |            |<----| Connected Edge Vb to Va |<---|            |
+ *        --------------     ---------------------------    --------------
+ * }
+ * </pre>
+ *
+ * @author Olivier Dugeon
+ * @author Philippe Niger
+ */
+public interface ConnectedGraph {
+
+    /**
+     * Returns the Graph associated to this Connected Graph.
+     *
+     * @return Graph
+     */
+    Graph getGraph();
+
+    /**
+     * Returns the list of Connected Vertices that form this graph.
+     *
+     * @return list of Connected Vertices
+     */
+    ArrayList<ConnectedVertex> getVertices();
+
+    /**
+     * Returns the Vertex associated to the given key.
+     *
+     * @param key Unique Vertex Identifier
+     * @return Vertex or null if there is no Vertex associated to the given key in this graph
+     */
+    ConnectedVertex getConnectedVertex(long key);
+
+    /**
+     * Returns the Vertex associated to the given IP address.
+     *
+     * @param address IP address of the Loopback of the Vertex
+     *
+     * @return Vertex or null if there is no Vertex associated to the given IP address in this graph
+     */
+    ConnectedVertex getConnectedVertex(IpAddress address);
+
+    /**
+     * Add Vertex in the Connected Graph. This action will automatically create the associated Connected Vertex and
+     * update the Graph in the DataStore.
+     *
+     * @param vertex Vertex to be added
+     *
+     * @return Connected Vertex associated to the given Vertex
+     */
+    ConnectedVertex addVertex(Vertex vertex);
+
+    /**
+     * Remove the Vertex in the Connected Graph. This action will automatically remove the associated Connected Vertex
+     * and update the Graph in the DataStore.
+     *
+     * @param vertexKey Unique Vertex Identifier
+     */
+    void deleteVertex(VertexKey vertexKey);
+
+    /**
+     * Returns the number of Vertices in the graph.
+     *
+     * @return number of vertices
+     */
+    int getVerticesSize();
+
+    /**
+     * Returns the list of Connected Edges that form this graph.
+     *
+     * @return list of Connected Edges
+     */
+    ArrayList<ConnectedEdge> getEdges();
+
+    /**
+     * Returns the Edge associated to the given key.
+     *
+     * @param key Unique Edge Identifier
+     * @return Edge or null if there is no Edge associated to the given key in this graph
+     */
+    ConnectedEdge getConnectedEdge(long key);
+
+    /**
+     * Returns the Edge associated to the given IP address.
+     *
+     * @param address IP address of the Edge
+     *
+     * @return Edge or null if there is no Edge associated to the given IP address in this graph
+     */
+    ConnectedEdge getConnectedEdge(IpAddress address);
+
+    /**
+     * Add Edge in the Connected Graph. This action will automatically create the associated Connected Edge and
+     * update the Graph in the DataStore.
+     *
+     * @param edge Edge to be added
+     *
+     * @return Connected Edge associated to the given Edge
+     */
+    ConnectedEdge addEdge(Edge edge);
+
+    /**
+     * Remove the Edge in the Connected Graph. This action will automatically remove the associated Connected Edge
+     * and update the Graph in the DataStore.
+     *
+     * @param edgeKey Unique Edge Identifier
+     */
+    void deleteEdge(EdgeKey edgeKey);
+
+    /**
+     * Returns the number of Edges in the graph.
+     *
+     * @return number of edges
+     */
+    int getEdgesSize();
+
+    /**
+     * Returns the list of Prefix that are stored in this graph.
+     *
+     * @return list of Prefix
+     */
+    ArrayList<Prefix> getPrefixes();
+
+    /**
+     * Returns the Prefix associated to the given IP prefix.
+     *
+     * @param ippfx IPv4 or IPv6 prefix
+     *
+     * @return Prefix that match the given IPv4 or IPv6 prefix
+     */
+    Prefix getPrefix(IpPrefix ippfx);
+
+    /**
+     * Add Prefix in the Connected Graph. This action will automatically update the Graph in the DataStore.
+     *
+     * @param prefix Prefix to be added
+     *
+     */
+    void addPrefix(Prefix prefix);
+
+    /**
+     * Remove the Prefix in the Connected Graph. This action will automatically update the Graph in the DataStore.
+     *
+     * @param ippfx IPv4 or IPv6 prefix
+     */
+    void deletePrefix(IpPrefix ippfx);
+
+    /**
+     * Clear the Connected Graph by removing all Vertices, Edges and Prefixes. This also remove the associated Graph
+     * in the Datastore.
+     *
+     */
+    void clear();
+
+    /**
+     * Returns the summary of the graph characteristics: number of Vertices, Edges and Prefix.
+     *
+     * @return characteristics of the Graph as a string
+     */
+    String getSummary();
+
+    String toString();
+}
