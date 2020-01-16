37572ce9c210b66d5f29a42acdbb468bfa0ca265
Olivier Dugeon <olivier.dugeon@orange.com>
2020-01-16 15:53:43 +0100

    Path Computation Server
    Initial commit of Path Computation Server.
    
    This is the 3/3 Patch Set to provide Path Computation Element (PCE)
    conform to RFC5440. This integrates the Graph model and Algorithms
    features. Graph is fulfilled from BGP-LinkState if available otherwise
    a graph must be provided.
    
    Details information about how to use the PCE server will be provided
    in docs/pcep/pcep-user-guide-pce-server.rst
    
    JIRA: BGPCEP-858
    
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>
    Change-Id: Ic0552b4c7ad856fe7a7cca214bbaf81f659d876c

---
+++ bgp/topology-provider/src/main/java/org/opendaylight/bgpcep/bgp/topology/provider/config/LinkstateGraphProvider.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Orange. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.bgp.topology.provider.config;
+
+import org.opendaylight.bgpcep.bgp.topology.provider.AbstractTopologyBuilder;
+import org.opendaylight.bgpcep.bgp.topology.provider.LinkstateGraphBuilder;
+import org.opendaylight.bgpcep.bgp.topology.provider.spi.BgpTopologyDeployer;
+import org.opendaylight.graph.ConnectedGraphProvider;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.protocol.bgp.rib.RibReference;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.odl.bgp.topology.types.rev160524.TopologyTypes1;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+
+public final class LinkstateGraphProvider extends AbstractBgpTopologyProvider {
+
+    ConnectedGraphProvider graphProvider;
+
+    public LinkstateGraphProvider(final BgpTopologyDeployer deployer, final ConnectedGraphProvider graphProvider) {
+        super(deployer);
+        this.graphProvider = graphProvider;
+    }
+
+    @Override
+    AbstractTopologyBuilder<?> createTopologyBuilder(final DataBroker dataProvider, final RibReference locRibReference,
+            final TopologyId topologyId) {
+        return new LinkstateGraphBuilder(dataProvider, locRibReference, topologyId, this.graphProvider);
+    }
+
+    @Override
+    public boolean topologyTypeFilter(final Topology topology) {
+        final TopologyTypes1 topoAug = getTopologyAug(topology);
+        return topoAug != null && topoAug.getBgpLinkstateTopology() != null;
+    }
+}
