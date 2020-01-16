d90d3fe1ac8ae1e281cda3dc759da4b8b62d80d9
Olivier Dugeon <olivier.dugeon@orange.com>
2020-01-16 16:03:08 +0100

    Path Computation Algorithms
    Initial commit of Path Computation Algorithms implementation.
    
    This is the 2/3 Patch Set to implement a full featured PCE server
    in conformity to RFC5440. It provides three different algorithms
    able to compute paths between end points by taking into account
    different constraints:
     - A simple Shortest Path First that takes into account only
       standard IGP metric
     - A Constrained Shortest Path First (CSPF) that takes into
       account the TE Metric and Bandwidth for constraints
     - SAMCRA algorithm that takes into account TE Metric, Delay,
       Loss and Bandwidth for constraints
    
    Details information about how the various algorithms are implemented
    and how to use them will be provided in docs/algo directory.
    
    JIRA: BGPCEP-858
    
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>
    Co-authored-by: Philippe Niger <philippe.niger@orange.com>
    Co-authored-by: Philippe Cadro <philippe.cadro@orange.com>
    Change-Id: Ifbcf2a65aef08c3fa95a2be54eb53ffdf55dd417

---
+++ algo/algo-impl/src/main/java/org/opendaylight/algo/impl/PathComputationServer.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2020 Orange. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.algo.impl;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.algo.PathComputationAlgorithm;
+import org.opendaylight.algo.PathComputationProvider;
+import org.opendaylight.graph.ConnectedGraph;
+import org.opendaylight.graph.ConnectedGraphProvider;
+import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.VertexKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.AlgorithmType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ComputationStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ConstrainedPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.GetConstrainedPathInput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.GetConstrainedPathOutput;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.GetConstrainedPathOutputBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.PathComputationService;
+import org.opendaylight.yangtools.concepts.ObjectRegistration;
+import org.opendaylight.yangtools.yang.common.RpcError;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+
+/**
+ * Path Computation Algorithms provider.
+ *
+ * @author Olivier Dugeon
+ */
+public class PathComputationServer implements AutoCloseable, PathComputationService, PathComputationProvider {
+
+    private ObjectRegistration<PathComputationService> pathService;
+    private RpcProviderService rpcProviderRegistry;
+    private ConnectedGraphProvider graphProvider;
+
+    private ConnectedGraph cgraph;
+
+    public PathComputationServer(final RpcProviderService rpcService, final ConnectedGraphProvider graphProvider) {
+        checkArgument(rpcService != null);
+        checkArgument(graphProvider != null);
+        this.rpcProviderRegistry = rpcService;
+        this.graphProvider = graphProvider;
+    }
+
+    public void init() {
+        pathService = this.rpcProviderRegistry.registerRpcImplementation(PathComputationService.class, this);
+    }
+
+    @Override
+    public ListenableFuture<RpcResult<GetConstrainedPathOutput>> getConstrainedPath(
+            final GetConstrainedPathInput input) {
+        final GetConstrainedPathOutputBuilder output = new GetConstrainedPathOutputBuilder();
+
+        /* First, get graph */
+        this.cgraph = graphProvider.getConnectedGraph(input.getGraphName());
+        if (this.cgraph == null) {
+            output.setStatus(ComputationStatus.Failed);
+            return RpcResultBuilder.<GetConstrainedPathOutput>failed()
+                    .withError(RpcError.ErrorType.RPC, "Unknown Graph Name").buildFuture();
+        }
+
+        /* get a new Path Computation Algorithm according to Input choice */
+        PathComputationAlgorithm algo = getPathComputationAlgorithm(this.cgraph, input.getAlgorithm());
+        if (algo == null) {
+            output.setStatus(ComputationStatus.Failed);
+            return RpcResultBuilder.<GetConstrainedPathOutput>failed()
+                    .withError(RpcError.ErrorType.RPC, "Unknown Path Computation Algorithm").buildFuture();
+        }
+
+        /*
+         * Request Path Computation for given source, destination and
+         * constraints
+         */
+        final VertexKey source = new VertexKey(input.getSource());
+        final VertexKey destination = new VertexKey(input.getSource());
+        final ConstrainedPath cpath = algo.computeP2pPath(source, destination, input.getConstraints());
+
+        /* Send back the Computed Path */
+        output.setPathDescription(cpath.getPathDescription()).setStatus(cpath.getStatus())
+                .setComputedMetric(cpath.getMetric()).setComputedTeMetric(cpath.getTeMetric())
+                .setComputedDelay(cpath.getDelay());
+
+        return RpcResultBuilder.success(output.build()).buildFuture();
+    }
+
+    @Override
+    public void close() throws Exception {
+        pathService.close();
+    }
+
+    @Override
+    public PathComputationAlgorithm getPathComputationAlgorithm(ConnectedGraph runningGraph,
+            AlgorithmType algorithmType) {
+        PathComputationAlgorithm algo = null;
+        switch (algorithmType) {
+            case Spf:
+                algo = new ShortestPathFirst(runningGraph);
+                break;
+            case Cspf:
+                algo = new ConstrainedShortestPathFirst(runningGraph);
+                break;
+            case Samcra:
+                algo = new Samcra(runningGraph);
+                break;
+            default:
+                break;
+        }
+        return algo;
+    }
+}
