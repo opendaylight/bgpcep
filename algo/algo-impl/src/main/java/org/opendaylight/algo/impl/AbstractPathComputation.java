85d6957be3b8b260e7bbaeba846c80c29958ea0e
Olivier Dugeon <olivier.dugeon@orange.com>
2020-01-16 16:03:08 +0100

    Path Computation Algorithms
    Initial commit of Path Computation Algorithms implementation.
    
    This is the 2/3 Patch Set to implement a full featured PCE server
    in conformity to RFC5440. It provides three different algorithms
    able to compute paths between end points by taking into account
    different constraints:
     - A simple Shortest Path First that takes into account only
       standard IGP metric
     - A Constrained Shortest Path First (CSPF) that takes into
       account the TE Metric and Bandwidth for constraints
     - SAMCRA algorithm that takes into account TE Metric, Delay,
       Loss and Bandwidth for constraints
    
    Details information about how the various algorithms are implemented
    and how to use them will be provided in docs/algo directory.
    
    JIRA: BGPCEP-858
    
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>
    Co-authored-by: Philippe Niger <philippe.niger@orange.com>
    Co-authored-by: Philippe Cadro <philippe.cadro@orange.com>
    Change-Id: Ifbcf2a65aef08c3fa95a2be54eb53ffdf55dd417

---
+++ algo/algo-impl/src/main/java/org/opendaylight/algo/impl/AbstractPathComputation.java
@@ -0,0 +1,365 @@
+/*
+ * Copyright (c) 2020 Orange. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.algo.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.algo.PathComputationAlgorithm;
+import org.opendaylight.graph.ConnectedEdge;
+import org.opendaylight.graph.ConnectedGraph;
+import org.opendaylight.graph.ConnectedVertex;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.edge.EdgeAttributes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.VertexKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.concepts.rev131125.MplsLabel;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ComputationStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ConstrainedPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ConstrainedPathBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.PathConstraints;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.path.descriptions.PathDescription;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.path.descriptions.PathDescriptionBuilder;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractPathComputation implements PathComputationAlgorithm {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractPathComputation.class);
+
+    protected final ConnectedGraph graph;
+
+    /* Source and Destination of the path */
+    protected CspfPath pathSource = null;
+    protected CspfPath pathDestination = null;
+
+    /* Constraints that the path must respect */
+    protected PathConstraints constraints = null;
+
+    /* Priority Queue and HashMap to manage path computation */
+    protected PriorityQueue<CspfPath> priorityQueue;
+    protected HashMap<Long, CspfPath> processedPath;
+
+    protected AbstractPathComputation(ConnectedGraph graph) {
+        this.graph = graph;
+        priorityQueue = new PriorityQueue<CspfPath>();
+        processedPath = new HashMap<Long, CspfPath>();
+    }
+
+    /**
+     * Initialize the various parameters for Path Computation, in particular the
+     * Source and Destination CspfPath.
+     *
+     * @param src
+     *            Source Vertex Identifier in the Connected Graph
+     * @param dst
+     *            Destination Vertex Identifier in the Connected Graph
+     *
+     * @return Constrained Path Builder with status set to 'OnGoing' if
+     *         initialization success, 'Failed' otherwise
+     */
+    protected ConstrainedPathBuilder initializePathComputation(VertexKey src, VertexKey dst) {
+        ConstrainedPathBuilder cpathBuilder = new ConstrainedPathBuilder().setStatus(ComputationStatus.InProgress);
+
+        /* Check that source and destination vertexKey are not identical */
+        if (src.equals(dst)) {
+            LOG.warn("Source and Destination are equal: Abort!");
+            cpathBuilder.setStatus(ComputationStatus.Failed);
+            return cpathBuilder;
+        }
+
+        /*
+         * Get the Connected Vertex from the Graph to initialize the source of
+         * the Cspf Path
+         */
+        ConnectedVertex vertex = null;
+        vertex = graph.getConnectedVertex(src.getVertexId().longValue());
+        if (vertex == null) {
+            LOG.warn("Found no source for Vertex Key {}", src);
+            cpathBuilder.setStatus(ComputationStatus.Failed);
+            return cpathBuilder;
+        }
+        LOG.debug("Create Path Source with Vertex {}", vertex.toString());
+        pathSource = new CspfPath(vertex).setCost(0).setDelay(0);
+        cpathBuilder.setSource(vertex.getVertex().getVertexId());
+
+        /*
+         * Get the Connected Vertex from the Graph to initialize the destination
+         * of the Cspf Path
+         */
+        vertex = graph.getConnectedVertex(dst.getVertexId().longValue());
+        if (vertex == null) {
+            LOG.warn("Found no destination for Vertex Key {}", src);
+            cpathBuilder.setStatus(ComputationStatus.Failed);
+            return cpathBuilder;
+        }
+        LOG.debug("Create Path Destination with Vertex {}", vertex.toString());
+        pathDestination = new CspfPath(vertex);
+        cpathBuilder.setDestination(vertex.getVertex().getVertexId());
+
+        /* Initialize the Priority Queue, HashMap */
+        priorityQueue.clear();
+        priorityQueue.add(pathSource);
+        processedPath.clear();
+        processedPath.put(pathSource.getVertexKey(), pathSource);
+        processedPath.put(pathDestination.getVertexKey(), pathDestination);
+
+        return cpathBuilder;
+    }
+
+    /**
+     * Check if Edge need to be prune regarding all constraints including
+     * address family.
+     *
+     * @return True if Edge must be prune, False if Edge must be keep
+     */
+    protected boolean pruneEdge(ConnectedEdge edge, CspfPath path) {
+        /* Check that Constraints are initialized */
+        if (constraints == null) {
+            LOG.warn("Constraints not set");
+            return true;
+        }
+
+        /* Edge could point to an unknown Vertex e.g. with inter-domain link */
+        if ((edge.getDestination() == null) || (edge.getDestination().getVertex() == null)) {
+            LOG.debug("No Destination");
+            return true;
+        }
+
+        /* Check that Edge have attributes */
+        EdgeAttributes attributes = edge.getEdge() != null ? edge.getEdge().getEdgeAttributes() : null;
+        if (attributes == null) {
+            LOG.debug("No attributes");
+            return true;
+        }
+
+        /* Check that Edge belongs to the requested address family */
+        switch (constraints.getAddressFamily()) {
+            case Ipv4:
+                if ((attributes.getRemoteAddress() == null)
+                        || (attributes.getRemoteAddress().getIpv4Address() == null)) {
+                    LOG.debug("No Ipv4 address");
+                    return true;
+                }
+                break;
+            case Ipv6:
+                if ((attributes.getRemoteAddress() == null)
+                        || (attributes.getRemoteAddress().getIpv6Address() == null)) {
+                    LOG.debug("No Ipv6 address");
+                    return true;
+                }
+                break;
+            case SrIpv4:
+                if (getIpv4NodeSid(edge.getDestination()) == null) {
+                    LOG.debug("No SR-Ipv4 SID");
+                    return true;
+                }
+                break;
+            case SrIpv6:
+                if (getIpv6NodeSid(edge.getDestination()) == null) {
+                    LOG.debug("No SR-Ipv6 SID");
+                    return true;
+                }
+                break;
+            default:
+                return true;
+        }
+
+        /* Skip checking other Constraints for simple SPF algorithm */
+        if (this instanceof ShortestPathFirst) {
+            LOG.trace("Edge {} is valid for Simple Path Computation", edge.toString());
+            return false;
+        }
+
+        /*
+         * If specified, check that total TE Metric up to this edge respects the
+         * initial constraints
+         */
+        if (constraints.getTeMetric() != null) {
+            if (attributes.getTeMetric() == null) {
+                return true;
+            } else {
+                int totalCost = attributes.getTeMetric().intValue() + path.getCost();
+                if (totalCost > constraints.getTeMetric().intValue()) {
+                    LOG.debug("TeMetric {} exceed constraint {}", totalCost, constraints.getTeMetric().intValue());
+                    return true;
+                }
+            }
+        }
+
+        /*
+         * If specified, check that total Delay up to this edge respects the
+         * initial constraints
+         */
+        if (constraints.getDelay() != null) {
+            if (attributes.getDelay() == null) {
+                return true;
+            } else {
+                int totalDelay = attributes.getDelay().getValue().intValue() + path.getDelay();
+                if (totalDelay > constraints.getDelay().getValue().intValue()) {
+                    LOG.debug("Delay {} exceed constraint {}", totalDelay,
+                            constraints.getDelay().getValue().intValue());
+                    return true;
+                }
+            }
+        }
+
+        /* Check that Edge respect Loss constraint */
+        if (constraints.getLoss() != null) {
+            if ((attributes.getLoss() == null)
+                    || (attributes.getLoss().getValue().intValue() > constraints.getLoss().getValue().intValue())) {
+                return true;
+            }
+        }
+
+        /* Check that Edge meet Bandwidth constraint */
+        if (constraints.getBandwidth() != null) {
+            if ((attributes.getMaxLinkBandwidth() == null) || (attributes.getMaxResvLinkBandwidth() == null)
+                    || (attributes.getUnreservedBandwidth() == null)
+                    || (attributes.getUnreservedBandwidth().get(constraints.getClassType().intValue()) == null)) {
+                return true;
+            } else {
+                Long bandwidth = constraints.getBandwidth().getValue().longValue();
+                if (attributes.getUnreservedBandwidth().get(constraints.getClassType().intValue()).getBandwidth()
+                        .getValue().longValue() < bandwidth
+                        || attributes.getMaxLinkBandwidth().getValue().longValue() < bandwidth
+                        || attributes.getMaxResvLinkBandwidth().getValue().longValue() < bandwidth) {
+                    LOG.debug("Bandwidth constraint is not met");
+                    return true;
+                }
+            }
+        }
+
+        /* Check that Edge belongs to admin group */
+        if ((constraints.getAdminGroup() != null)
+                && !(constraints.getAdminGroup().equals(attributes.getAdminGroup()))) {
+            LOG.debug("Not in the requested admin-group");
+            return true;
+        }
+
+        /*
+         * OK. All is fine. We can consider this Edge valid, so not to be prune
+         */
+        LOG.trace("Edge {} is valid for Constrained Path Computation", edge.toString());
+        return false;
+    }
+
+    /**
+     * Return the MPLS Label corresponding to the Node SID for IPv4 when the
+     * Connected Vertex is Segment Routing aware.
+     *
+     * @return MPLS Label if Connected Vertex is Segment Routing aware, Null
+     *         otherwise
+     */
+    protected @Nullable MplsLabel getIpv4NodeSid(ConnectedVertex cvertex) {
+        /*
+         * Check that Vertex is Segment Routing aware
+         */
+        if ((cvertex.getVertex() == null) || (cvertex.getVertex().getSrgb() == null)) {
+            return null;
+        }
+        /*
+         * Find in Prefix List Node SID attached to the IPv4 of the next Vertex
+         * and return the MPLS Label that corresponds to the index in the SRGB
+         */
+        if (cvertex.getPrefixes() == null) {
+            return null;
+        }
+        for (Prefix prefix : cvertex.getPrefixes()) {
+            if ((prefix.getPrefixSid() == null) || (prefix.isNodeSid() == null)) {
+                continue;
+            }
+            if (prefix.isNodeSid() && prefix.getPrefix().getIpv4Prefix() != null) {
+                return new MplsLabel(Uint32.valueOf(prefix.getPrefixSid().intValue()));
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Return the MPLS Label corresponding to the Node SID for IPv6 when the
+     * Connected Vertex is Segment Routing aware.
+     *
+     * @return MPLS Label if Connected Vertex is Segment Routing aware, Null
+     *         otherwise
+     */
+    protected @Nullable MplsLabel getIpv6NodeSid(ConnectedVertex cvertex) {
+        /*
+         * Check that Vertex is Segment Routing aware
+         */
+        if ((cvertex.getVertex() == null) || (cvertex.getVertex().getSrgb() == null)) {
+            return null;
+        }
+        /*
+         * Find in Prefix List Node SID attached to the IPv6 of the next Vertex
+         * and return the MPLS Label that corresponds to the index in the SRGB
+         */
+        if (cvertex.getPrefixes() == null) {
+            return null;
+        }
+        for (Prefix prefix : cvertex.getPrefixes()) {
+            if ((prefix.getPrefixSid() == null) || (prefix.isNodeSid() == null)) {
+                continue;
+            }
+            if (prefix.isNodeSid() && prefix.getPrefix().getIpv6Prefix() != null) {
+                return new MplsLabel(Uint32.valueOf(prefix.getPrefixSid().intValue()));
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Convert List of Connected Edges into a Path Description as a List of
+     * IPv4, IPv6 or MPLS Label depending of the requested Address Family.
+     *
+     * @param edges
+     *            List of Connected Edges
+     *
+     * @return Path Description
+     */
+    protected List<PathDescription> getPathDescription(List<ConnectedEdge> edges) {
+        ArrayList<PathDescription> list = new ArrayList<PathDescription>();
+
+        for (ConnectedEdge edge : edges) {
+            PathDescription pathDesc = null;
+            switch (constraints.getAddressFamily()) {
+                case Ipv4:
+                    pathDesc = new PathDescriptionBuilder()
+                            .setIpv4(edge.getEdge().getEdgeAttributes().getRemoteAddress().getIpv4Address()).build();
+                    break;
+                case Ipv6:
+                    pathDesc = new PathDescriptionBuilder()
+                            .setIpv6(edge.getEdge().getEdgeAttributes().getRemoteAddress().getIpv6Address()).build();
+                    break;
+                case SrIpv4:
+                    pathDesc = new PathDescriptionBuilder()
+                            .setIpv4(edge.getDestination().getVertex().getRouterId().getIpv4Address())
+                            .setLabel(getIpv4NodeSid(edge.getDestination()))
+                            .build();
+                    break;
+                case SrIpv6:
+                    pathDesc = new PathDescriptionBuilder()
+                            .setIpv6(edge.getDestination().getVertex().getRouterId().getIpv6Address())
+                            .setLabel(getIpv6NodeSid(edge.getDestination()))
+                            .build();
+                    break;
+                default:
+                    break;
+            }
+            list.add(pathDesc);
+        }
+        return list;
+    }
+
+    @Override
+    public abstract ConstrainedPath computeP2pPath(VertexKey source, VertexKey destination,
+            PathConstraints constraints);
+
+}
---
+++ algo/algo-impl/src/main/java/org/opendaylight/algo/impl/ConstrainedShortestPathFirst.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2016 Orange. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.algo.impl;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.opendaylight.graph.ConnectedEdge;
+import org.opendaylight.graph.ConnectedGraph;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.VertexKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ComputationStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ConstrainedPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ConstrainedPathBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.PathConstraints;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * This Class implements a simple Constrained Shortest Path First path computation algorithm that take into account
+ * Bandwidth and TE Metric as constraints.
+ *
+ * @author Olivier Dugeon
+ * @author Philippe Niger
+ * @author Philippe Cadro
+ *
+ */
+public class ConstrainedShortestPathFirst extends AbstractPathComputation {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ConstrainedShortestPathFirst.class);
+
+    private HashMap<Long, CspfPath> visitedVertices;
+
+    public ConstrainedShortestPathFirst(ConnectedGraph graph) {
+        super(graph);
+        visitedVertices = new HashMap<Long, CspfPath>();
+    }
+
+    public ConstrainedPath computeP2pPath(VertexKey src, VertexKey dst, PathConstraints cts) {
+        ConstrainedPathBuilder cpathBuilder;
+        List<ConnectedEdge> edges;
+        CspfPath currentPath;
+        int currentCost = Integer.MAX_VALUE;
+
+        LOG.info("Start CSPF Path Computation from {} to {} with constraints {}", src, dst, cts);
+
+        /* Initialize algorithm */
+        this.constraints = cts;
+        cpathBuilder = initializePathComputation(src, dst);
+        if (cpathBuilder.getStatus() == ComputationStatus.Failed) {
+            return cpathBuilder.build();
+        }
+
+        cpathBuilder.setBandwidth(cts.getBandwidth()).setClassType(cts.getClassType());
+
+        visitedVertices.clear();
+
+        /* Process all Connected Vertex until priority queue becomes empty. Connected Vertices are added into the
+         * priority queue when processing the next Connected Vertex: see relaxMC() method */
+        while (priorityQueue.size() != 0) {
+            currentPath = priorityQueue.poll();
+            visitedVertices.put(currentPath.getVertexKey(), currentPath);
+            LOG.debug("Got path to Vertex {} from Priority Queue", currentPath.getVertex().toString());
+            edges = currentPath.getVertex().getOutputConnectedEdges();
+
+            for (ConnectedEdge edge : edges) {
+                /* Skip Connected Edges that must be prune i.e. Edges that not satisfy the given constraints,
+                 * in particular the Bandwidth, TE Metric and Delay. */
+                if (pruneEdge(edge, currentPath)) {
+                    LOG.trace("  Prune Edge {}", edge.toString());
+                    continue;
+                }
+                if ((relaxMultiConstraints(edge, currentPath)) && (pathDestination.getCost() < currentCost)) {
+                    currentCost = pathDestination.getCost();
+                    cpathBuilder.setPathDescription(getPathDescription(pathDestination.getPath()))
+                            .setMetric(Uint32.valueOf(pathDestination.getCost()))
+                            .setStatus(ComputationStatus.Active);
+                    LOG.debug("  Found a valid path up to destination {}", cpathBuilder.getPathDescription());
+                }
+            }
+        }
+        /* The priority queue is empty => all the possible (vertex, path) elements have been explored
+         * The "ConstrainedPathBuilder" object contains the optimal path if it exists
+         * Otherwise an empty path with status failed is returned
+         */
+        if ((cpathBuilder.getStatus() == ComputationStatus.InProgress)
+                || (cpathBuilder.getPathDescription().size() == 0)) {
+            cpathBuilder.setStatus(ComputationStatus.Failed);
+        } else {
+            cpathBuilder.setStatus(ComputationStatus.Completed);
+        }
+        return cpathBuilder.build();
+    }
+
+    private boolean relaxMultiConstraints(ConnectedEdge edge, CspfPath currentPath) {
+        LOG.debug("    Start relaxing Multi Constraints on Edge {} to Vertex {}",
+                edge.toString(), edge.getDestination().toString());
+        final Long nextVertexKey = edge.getDestination().getKey();
+
+        /* Verify if we have not visited this Vertex to avoid loop */
+        if (visitedVertices.containsKey(nextVertexKey)) {
+            return false;
+        }
+
+        /* Get Next Vertex from processedPath or create a new one if it has not yet processed */
+        CspfPath nextPath = processedPath.get(nextVertexKey);
+        if (nextPath == null) {
+            nextPath = new CspfPath(edge.getDestination());
+            processedPath.put(nextPath.getVertexKey(), nextPath);
+        }
+
+        /* Add or update the CspfPath in the Priority Queue if total path Cost is lower than cost associated
+         * to this next Vertex. This could occurs if we process a Vertex that as not yet been visited in the Graph
+         * or if we found a shortest path up to this Vertex. */
+        int totalCost = edge.getEdge().getEdgeAttributes().getTeMetric().intValue() + currentPath.getCost();
+        if (totalCost < nextPath.getCost()) {
+            nextPath.setCost(totalCost)
+                    .replacePath(currentPath.getPath())
+                    .addConnectedEdge(edge);
+            /* It is not possible to directly update the CspfPath in the Priority Queue. Indeed, if we modify the path
+             * weight, the Priority Queue must be re-ordered. So, we need fist to remove the CspfPath if it is present
+             * in the Priority Queue, then, update the Path Weight, and finally (re-)insert it in the Priority Queue.
+             */
+            priorityQueue.removeIf((path) -> path.getVertexKey().equals(nextVertexKey));
+            nextPath.setKey(totalCost);
+            priorityQueue.add(nextPath);
+            LOG.debug("    Added path to Vertex {} in the Priority Queue", nextPath.getVertex().toString());
+        }
+        /* Return True if we reach the destination, false otherwise */
+        return pathDestination.equals(nextPath);
+    }
+}
