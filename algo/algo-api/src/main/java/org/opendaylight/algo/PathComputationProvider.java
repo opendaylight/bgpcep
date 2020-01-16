3f9872bc019888b6e703d1c83498f839aa348376
Olivier Dugeon <olivier.dugeon@orange.com>
2020-01-16 16:03:08 +0100

    Path Computation Algorithms
    Initial commit of Path Computation Algorithms implementation.
    
    This is the 2/3 Patch Set to implement a full featured PCE server
    in conformity to RFC5440. It provides three different algorithms
    able to compute paths between end points by taking into account
    different constraints:
     - A simple Shortest Path First that takes into account only
       standard IGP metric
     - A Constrained Shortest Path First (CSPF) that takes into
       account the TE Metric and Bandwidth for constraints
     - SAMCRA algorithm that takes into account TE Metric, Delay,
       Loss and Bandwidth for constraints
    
    Details information about how the various algorithms are implemented
    and how to use them will be provided in docs/algo directory.
    
    JIRA: BGPCEP-858
    
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>
    Co-authored-by: Philippe Niger <philippe.niger@orange.com>
    Co-authored-by: Philippe Cadro <philippe.cadro@orange.com>
    Change-Id: Ifbcf2a65aef08c3fa95a2be54eb53ffdf55dd417

---
+++ algo/algo-api/src/main/java/org/opendaylight/algo/PathComputationProvider.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 Orange. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.algo;
+
+import org.opendaylight.graph.ConnectedGraph;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.AlgorithmType;
+
+/**
+ * This class provides access to Path Computation Algorithms.
+ *
+ * @author Olivier Dugeon
+ *
+ */
+public interface PathComputationProvider {
+
+    /**
+     * Return Path Computation Algorithm object that corresponds to the requested type.
+     *
+     * @param cgraph         Connected Graph on which path computation will run
+     * @param algorithmType  Algorithm supported types are: 'SPF', 'CSPF' and 'SAMCRA'
+     *
+     * @return PathComputationAlgorithm
+     */
+    PathComputationAlgorithm getPathComputationAlgorithm(ConnectedGraph cgraph, AlgorithmType algorithmType);
+
+}
---
+++ algo/algo-impl/src/main/java/org/opendaylight/algo/impl/ShortestPathFirst.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2016 Orange. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.algo.impl;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.opendaylight.graph.ConnectedEdge;
+import org.opendaylight.graph.ConnectedGraph;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.graph.VertexKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ComputationStatus;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ConstrainedPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ConstrainedPathBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.PathConstraints;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * This Class implements a simple Shortest Path First path computation algorithm based on standard IGP Metric.
+ *
+ * @author Olivier Dugeon
+ * @author Philippe Niger
+ * @author Philippe Cadro
+ *
+ */
+public class ShortestPathFirst extends AbstractPathComputation {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ShortestPathFirst.class);
+
+    private HashMap<Long, CspfPath> visitedVertices;
+
+    public ShortestPathFirst(ConnectedGraph graph) {
+        super(graph);
+        visitedVertices = new HashMap<Long, CspfPath>();
+    }
+
+    public ConstrainedPath computeP2pPath(VertexKey src, VertexKey dst, PathConstraints cts) {
+        ConstrainedPathBuilder cpathBuilder;
+        List<ConnectedEdge> edges;
+        CspfPath currentPath;
+        int currentCost = Integer.MAX_VALUE;
+
+        LOG.info("Start SPF Path Computation from {} to {} with constraints {}", src, dst, cts);
+
+        /* Initialize algorithm */
+        this.constraints = cts;
+        cpathBuilder = initializePathComputation(src, dst);
+        if (cpathBuilder.getStatus() == ComputationStatus.Failed) {
+            LOG.warn("Initial configurations are not met. Abort!");
+            return cpathBuilder.build();
+        }
+
+        visitedVertices.clear();
+
+        while (priorityQueue.size() != 0) {
+            currentPath = priorityQueue.poll();
+            visitedVertices.put(currentPath.getVertexKey(), currentPath);
+            LOG.debug("Process path to Vertex {} from Priority Queue", currentPath.getVertex().toString());
+            edges = currentPath.getVertex().getOutputConnectedEdges();
+
+            for (ConnectedEdge edge : edges) {
+                /* Check that Edge point to a valid Vertex and is suitable for the Constraint Address Family */
+                if (pruneEdge(edge, currentPath)) {
+                    LOG.trace("  Prune Edge {}", edge.toString());
+                    continue;
+                }
+                if ((relax(edge, currentPath)) && (pathDestination.getCost() < currentCost)) {
+                    currentCost = pathDestination.getCost();
+                    cpathBuilder.setPathDescription(getPathDescription(pathDestination.getPath()))
+                            .setMetric(Uint32.valueOf(pathDestination.getCost()))
+                            .setStatus(ComputationStatus.Active);
+                    LOG.debug("  Found a valid path up to destination {}", cpathBuilder.getPathDescription());
+                }
+            }
+        }
+        /* The priority queue is empty => all the possible (vertex, path) elements have been explored
+         * The "ConstrainedPathBuilder" object contains the optimal path if it exists
+         * Otherwise an empty path with status failed is returned
+         */
+        if ((cpathBuilder.getStatus() == ComputationStatus.InProgress)
+                || (cpathBuilder.getPathDescription().size() == 0)) {
+            cpathBuilder.setStatus(ComputationStatus.Failed);
+        } else {
+            cpathBuilder.setStatus(ComputationStatus.Completed);
+        }
+        return cpathBuilder.build();
+    }
+
+    private boolean relax(ConnectedEdge edge, CspfPath currentPath) {
+        LOG.debug("    Start relaxing Edge {} to Vertex {}", edge.toString(), edge.getDestination().toString());
+        final Long nextVertexKey = edge.getDestination().getKey();
+
+        /* Verify if we have not visited this Vertex to avoid loop */
+        if (visitedVertices.containsKey(nextVertexKey)) {
+            return false;
+        }
+
+        /* Get Next Vertex from processedPath or create a new one if it has not yet processed */
+        CspfPath nextPath = processedPath.get(nextVertexKey);
+        if (nextPath == null) {
+            nextPath = new CspfPath(edge.getDestination());
+            processedPath.put(nextPath.getVertexKey(), nextPath);
+        }
+
+        /* Compute Cost from source to this next Vertex and add or update it in the Priority Queue
+         * if total path Cost is lower than cost associated to this next Vertex.
+         * This could occurs if we process a Vertex that as not yet been visited in the Graph
+         * or if we found a shortest path up to this Vertex. */
+        int totalCost = edge.getEdge().getEdgeAttributes().getMetric().intValue() + currentPath.getCost();
+        if (nextPath.getCost() > totalCost) {
+            nextPath.setCost(totalCost)
+                    .replacePath(currentPath.getPath())
+                    .addConnectedEdge(edge);
+            /* It is not possible to directly update the CspfPath in the Priority Queue. Indeed, if we modify the path
+             * weight, the Priority Queue must be re-ordered. So, we need fist to remove the CspfPath if it is present
+             * in the Priority Queue, then, update the Path Weight, and finally (re-)insert it in the Priority Queue.
+             */
+            priorityQueue.removeIf((path) -> path.getVertexKey().equals(nextVertexKey));
+            nextPath.setKey(totalCost);
+            priorityQueue.add(nextPath);
+            LOG.debug("    Added path to Vertex {} in the Priority Queue with weight {}",
+                    nextPath.getVertex().toString(), nextPath.getKey());
+        }
+        /* Return True if we reach the destination, false otherwise */
+        return pathDestination.equals(nextPath);
+    }
+}
