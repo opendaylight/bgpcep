e9a7fe954c92452e3d64aca9e84aec36d8bfd43a
Olivier Dugeon <olivier.dugeon@orange.com>
2020-01-16 15:53:43 +0100

    Path Computation Server
    Initial commit of Path Computation Server.
    
    This is the 3/3 Patch Set to provide Path Computation Element (PCE)
    conform to RFC5440. This integrates the Graph model and Algorithms
    features. Graph is fulfilled from BGP-LinkState if available otherwise
    a graph must be provided.
    
    Details information about how to use the PCE server will be provided
    in docs/pcep/pcep-user-guide-pce-server.rst
    
    JIRA: BGPCEP-858
    
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>
    Change-Id: Ic0552b4c7ad856fe7a7cca214bbaf81f659d876c

---
+++ pcep/server/server-provider/src/main/java/org/opendaylight/bgpcep/pcep/server/provider/MessagesUtil.java
@@ -0,0 +1,274 @@
+/*
+ * Copyright (c) 2020 Orange.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.bgpcep.pcep.server.provider;
+
+import com.google.common.collect.Lists;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.eclipse.jdt.annotation.NonNull;
+import org.opendaylight.protocol.pcep.spi.PCEPErrors;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddressNoZone;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4AddressNoZone;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4Prefix;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv6AddressNoZone;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv6Prefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ieee754.rev130819.Float32;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.network.concepts.rev131125.Bandwidth;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.ConstrainedPath;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.path.computation.rev200120.path.descriptions.PathDescription;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.message.rev181109.Pcerr;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.message.rev181109.PcerrBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.message.rev181109.Pcrep;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.message.rev181109.PcrepBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev181109.SidType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev181109.pcrep.pcrep.message.replies.result.success._case.success.paths.ero.subobject.subobject.type.SrEroType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev181109.pcrep.pcrep.message.replies.result.success._case.success.paths.ero.subobject.subobject.type.SrEroTypeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev181109.sr.subobject.nai.IpNodeIdBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.NoPathVectorTlv.Flags;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.RequestId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.bandwidth.object.BandwidthBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.classtype.object.ClassTypeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.explicit.route.object.Ero;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.explicit.route.object.EroBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.explicit.route.object.ero.Subobject;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.explicit.route.object.ero.SubobjectBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.lsp.attributes.Metrics;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.lsp.attributes.MetricsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.metric.object.MetricBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcep.error.object.ErrorObjectBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcerr.message.PcerrMessageBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcerr.message.pcerr.message.ErrorsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcerr.message.pcerr.message.error.type.RequestCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcerr.message.pcerr.message.error.type.request._case.RequestBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcerr.message.pcerr.message.error.type.request._case.request.RpsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.PcrepMessageBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.pcrep.message.RepliesBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.pcrep.message.replies.result.FailureCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.pcrep.message.replies.result.SuccessCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.pcrep.message.replies.result.failure._case.NoPathBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.pcrep.message.replies.result.failure._case.no.path.TlvsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.pcrep.message.replies.result.failure._case.no.path.tlvs.NoPathVectorBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.pcrep.message.replies.result.success._case.SuccessBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.pcrep.message.replies.result.success._case.success.Paths;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcrep.message.pcrep.message.replies.result.success._case.success.PathsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.pcreq.message.pcreq.message.requests.segment.computation.P2p;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.rp.object.Rp;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.rp.object.RpBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.basic.explicit.route.subobjects.subobject.type.IpPrefixCase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.basic.explicit.route.subobjects.subobject.type.IpPrefixCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.basic.explicit.route.subobjects.subobject.type.ip.prefix._case.IpPrefix;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.rsvp.rev150820.basic.explicit.route.subobjects.subobject.type.ip.prefix._case.IpPrefixBuilder;
+import org.opendaylight.yangtools.yang.common.Uint32;
+import org.opendaylight.yangtools.yang.common.Uint8;
+
+public final class MessagesUtil {
+
+    public static final byte NO_PATH = 0x0;
+    public static final byte UNKNOWN_SOURCE = 0x1;
+    public static final byte UNKNOWN_DESTINATION = 0x0;
+
+    /*
+     * See
+     * https://www.iana.org/assignments/pcep/pcep.xhtml#metric-object-ni-field
+     */
+    public static final int IGP_METRIC = 1;
+    public static final int TE_METRIC = 2;
+    public static final int PATH_DELAY = 12;
+
+    private MessagesUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Ero getEro(List<PathDescription> pathDescriptions) {
+        /* Prepare ERO */
+        final EroBuilder eroBuilder = new EroBuilder();
+        eroBuilder.setIgnore(false);
+        eroBuilder.setProcessingRule(true);
+        final List<Subobject> eroSubs = new ArrayList<>();
+
+        /* Fulfill ERO sublist */
+        for (PathDescription path : pathDescriptions) {
+            Subobject sb = null;
+            if (path.getLabel() == null) {
+                IpPrefix ipPref = null;
+                /* Prepare SubObject for IPv4 or IPv6 address */
+                if (path.getIpv4() != null) {
+                    final Ipv4Prefix ipv4Pref = new Ipv4Prefix(path.getIpv4().getValue() + "/32");
+                    ipPref = new IpPrefixBuilder().setIpPrefix(
+                            new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715
+                                    .IpPrefix(ipv4Pref))
+                            .build();
+                }
+                if (path.getIpv6() != null) {
+                    final Ipv6Prefix ipv6Pref = new Ipv6Prefix(path.getIpv6().getValue() + "/128");
+                    ipPref = new IpPrefixBuilder().setIpPrefix(
+                            new org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715
+                                    .IpPrefix(ipv6Pref))
+                            .build();
+                }
+                if (ipPref != null) {
+                    final IpPrefixCase ipPrefCase = new IpPrefixCaseBuilder().setIpPrefix(ipPref).build();
+                    sb = new SubobjectBuilder().setSubobjectType(ipPrefCase).setLoose(false).build();
+                }
+            } else {
+                /* Prepare SubObject for Segment Routing */
+                SrEroType srEro = null;
+                if (path.getIpv4() != null) {
+                    srEro = new SrEroTypeBuilder()
+                            .setSidType(SidType.Ipv4NodeId)
+                            .setSid(path.getLabel().getValue())
+                            .setCFlag(false)
+                            .setMFlag(false)
+                            .setNai(new IpNodeIdBuilder()
+                                    .setIpAddress(new IpAddressNoZone(new Ipv4AddressNoZone(path.getIpv4().getValue())))
+                                    .build())
+                            .build();
+                }
+                if (path.getIpv6() != null) {
+                    srEro = new SrEroTypeBuilder()
+                            .setSidType(SidType.Ipv6NodeId)
+                            .setSid(path.getLabel().getValue())
+                            .setCFlag(false)
+                            .setMFlag(false)
+                            .setNai(new IpNodeIdBuilder()
+                                    .setIpAddress(new IpAddressNoZone(new Ipv6AddressNoZone(path.getIpv6().getValue())))
+                                    .build())
+                            .build();
+                }
+                if (srEro != null) {
+                    sb = new SubobjectBuilder().setSubobjectType(srEro).setLoose(false).build();
+                }
+            }
+
+            /* Add corresponding SubObject to the ERO List */
+            if (sb != null) {
+                eroSubs.add(sb);
+            }
+        }
+        /* Set ERO sublist */
+        eroBuilder.setSubobject(eroSubs);
+
+        return eroBuilder.build();
+    }
+
+    private static PathsBuilder buildPath(ConstrainedPath cpath) {
+        final PathsBuilder pathBuilder = new PathsBuilder();
+
+        /* Get ERO from Path Description */
+        pathBuilder.setEro(getEro(cpath.getPathDescription()));
+
+        /* Fulfill Computed Metrics if available */
+        final ArrayList<Metrics> metrics = new ArrayList<Metrics>();
+        if (cpath.getMetric() != null) {
+            final MetricBuilder metricBuilder = new MetricBuilder().setComputed(true)
+                    .setMetricType(Uint8.valueOf(IGP_METRIC)).setValue(new Float32(
+                            ByteBuffer.allocate(4).putFloat(Float.valueOf(cpath.getMetric().floatValue())).array()));
+            metrics.add(new MetricsBuilder().setMetric(metricBuilder.build()).build());
+        }
+        if (cpath.getTeMetric() != null) {
+            final MetricBuilder metricBuilder = new MetricBuilder().setComputed(true)
+                    .setMetricType(Uint8.valueOf(TE_METRIC)).setValue(new Float32(
+                            ByteBuffer.allocate(4).putFloat(Float.valueOf(cpath.getTeMetric().floatValue())).array()));
+            metrics.add(new MetricsBuilder().setMetric(metricBuilder.build()).build());
+        }
+        if (cpath.getDelay() != null) {
+            final MetricBuilder metricBuilder = new MetricBuilder().setComputed(true)
+                    .setMetricType(Uint8.valueOf(PATH_DELAY)).setValue(new Float32(ByteBuffer.allocate(4)
+                            .putFloat(Float.valueOf(cpath.getDelay().getValue().floatValue())).array()));
+            metrics.add(new MetricsBuilder().setMetric(metricBuilder.build()).build());
+        }
+        if (!metrics.isEmpty()) {
+            pathBuilder.setMetrics(metrics);
+        }
+        /* Fulfill Bandwidth and ClassType if set */
+        if (cpath.getBandwidth() != null) {
+            final BandwidthBuilder bwBuilder = new BandwidthBuilder();
+            bwBuilder.setBandwidth(new Bandwidth(new Float32(ByteBuffer.allocate(4)
+                    .putFloat(Float.valueOf(cpath.getBandwidth().getValue().floatValue())).array())));
+            pathBuilder.setBandwidth(bwBuilder.build());
+            if (cpath.getClassType() != null) {
+                pathBuilder.setClassType(new ClassTypeBuilder().setClassType(
+                        new org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109
+                                .ClassType(cpath.getClassType()))
+                        .build());
+            }
+        }
+        return pathBuilder;
+    }
+
+    public static Pcrep createPcRepMessage(Rp rp, P2p p2p, ConstrainedPath cpath) {
+
+        /* Prepare Path Object with ERO and Object from the Request */
+        final ArrayList<Paths> paths = new ArrayList<Paths>();
+        PathsBuilder pathBuilder = buildPath(cpath);
+
+        if (p2p.getLspa() != null) {
+            pathBuilder.setLspa(p2p.getLspa());
+        }
+        if (p2p.getIro() != null) {
+            pathBuilder.setIro(p2p.getIro());
+        }
+        if (p2p.getXro() != null) {
+            pathBuilder.setXro(p2p.getXro());
+        }
+        paths.add(pathBuilder.build());
+
+        /* Prepare Reply with Path Object */
+        final RepliesBuilder replyBuilder = new RepliesBuilder()
+                .setRp(rp)
+                .setResult(new SuccessCaseBuilder().setSuccess(new SuccessBuilder().setPaths(paths).build()).build());
+
+        /* Prepare PcRep Message */
+        final PcrepMessageBuilder msgBuilder = new PcrepMessageBuilder()
+                .setReplies(Lists.newArrayList(replyBuilder.build()));
+        return new PcrepBuilder().setPcrepMessage(msgBuilder.build()).build();
+    }
+
+    public static Pcrep createNoPathMessage(Rp rp, byte reason) {
+
+        /* Prepare NoPath Object */
+        final Flags flags = new Flags(false, false, false, false, false, false,
+                (reason == UNKNOWN_DESTINATION) ? true : false, (reason == UNKNOWN_SOURCE) ? true : false);
+        final NoPathVectorBuilder npvBuilder = new NoPathVectorBuilder().setFlags(flags);
+        final TlvsBuilder tlvsBuilder = new TlvsBuilder().setNoPathVector(npvBuilder.build());
+        final NoPathBuilder npBuilder = new NoPathBuilder()
+                .setProcessingRule(false)
+                .setIgnore(false)
+                .setNatureOfIssue(Uint8.ZERO)
+                .setUnsatisfiedConstraints(true)
+                .setTlvs(tlvsBuilder.build());
+
+        /* Prepare Reply */
+        final RepliesBuilder replyBuilder = new RepliesBuilder()
+                .setRp(rp)
+                .setResult(new FailureCaseBuilder().setNoPath(npBuilder.build()).build());
+
+        /* Prepare PcRep Message */
+        final PcrepMessageBuilder msgBuilder = new PcrepMessageBuilder()
+                .setReplies(Lists.newArrayList(replyBuilder.build()));
+        return new PcrepBuilder().setPcrepMessage(msgBuilder.build()).build();
+    }
+
+    public static Pcerr createErrorMsg(@NonNull final PCEPErrors pcepErrors, final Uint32 reqID) {
+        final PcerrMessageBuilder msgBuilder = new PcerrMessageBuilder();
+        return new PcerrBuilder().setPcerrMessage(msgBuilder
+                .setErrorType(
+                        new RequestCaseBuilder().setRequest(new RequestBuilder()
+                                .setRps(Lists
+                                        .newArrayList(new RpsBuilder().setRp(new RpBuilder().setProcessingRule(false)
+                                                .setIgnore(false).setRequestId(new RequestId(reqID)).build()).build()))
+                                .build()).build())
+                .setErrors(Collections.singletonList(new ErrorsBuilder().setErrorObject(new ErrorObjectBuilder()
+                        .setType(pcepErrors.getErrorType()).setValue(pcepErrors.getErrorValue()).build()).build()))
+                .build()).build();
+    }
+
+}
