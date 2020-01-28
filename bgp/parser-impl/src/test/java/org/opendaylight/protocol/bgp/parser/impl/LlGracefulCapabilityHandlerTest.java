fe472440b0c6c0dcbfbb8a00f51c007205c19cbf
Vikram Singh  Kalakoti <vikramskalakoti@gmail.com>
2020-01-28 12:48:12 +0530

    Fix buffer read for unsupported LLGR Safi
    In LlGracefulCapabilityHandler, once an unsupported safi is encountered
    we should skip all unread bytes in buffer for that Table
    
    Signed-off-by: Vikram Singh Kalakoti <vikramskalakoti@gmail.com>
    Change-Id: Ic37eeb2dfaec8ac411815bc9c1105e6baad197be

--- bgp/parser-impl/src/test/java/org/opendaylight/protocol/bgp/parser/impl/LlGracefulCapabilityHandlerTest.java
+++ bgp/parser-impl/src/test/java/org/opendaylight/protocol/bgp/parser/impl/LlGracefulCapabilityHandlerTest.java
@@ -13,10 +13,15 @@ import java.util.Arrays;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.opendaylight.protocol.bgp.parser.BGPDocumentedException;
 import org.opendaylight.protocol.bgp.parser.BGPParsingException;
 import org.opendaylight.protocol.bgp.parser.impl.message.open.LlGracefulCapabilityHandler;
+import org.opendaylight.protocol.bgp.parser.spi.AddressFamilyRegistry;
 import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionProviderContext;
+import org.opendaylight.protocol.bgp.parser.spi.SubsequentAddressFamilyRegistry;
 import org.opendaylight.protocol.bgp.parser.spi.pojo.ServiceLoaderBGPExtensionProviderContext;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.open.message.bgp.parameters.optional.capabilities.CParameters;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev180329.open.message.bgp.parameters.optional.capabilities.CParametersBuilder;
@@ -33,11 +38,21 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.type
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev180329.UnicastSubsequentAddressFamily;
 
 public class LlGracefulCapabilityHandlerTest {
-
     private LlGracefulCapabilityHandler handler;
+
+    @Mock
+    private AddressFamilyRegistry afir;
+    @Mock
+    private SubsequentAddressFamilyRegistry safir;
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        Mockito.doReturn(Ipv4AddressFamily.class).when(this.afir).classForFamily(1);
+        Mockito.doReturn(Ipv6AddressFamily.class).when(this.afir).classForFamily(2);
+        Mockito.doReturn(null).when(this.afir).classForFamily(256);
+        Mockito.doReturn(UnicastSubsequentAddressFamily.class).when(this.safir).classForFamily(1);
+        Mockito.doReturn(null).when(this.safir).classForFamily(-123);
         final BGPExtensionProviderContext ctx = ServiceLoaderBGPExtensionProviderContext.getSingletonInstance();
         this.handler = new LlGracefulCapabilityHandler(
                 ctx.getAddressFamilyRegistry(), ctx.getSubsequentAddressFamilyRegistry());
@@ -98,6 +113,43 @@ public class LlGracefulCapabilityHandlerTest {
         this.handler.serializeCapability(cParameters, buffer);
     }
 
+    @Test
+    public void testRecvdUnsupportedAfi() {
+        final byte[] capaBytes = {
+            //header
+            (byte) 0x47, (byte) 0x15,
+            // Ipv4 Unicast, afiFlags = false, timer = 10
+            (byte) 0x00, (byte) 0x01, (byte) 0x01,
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x0a,
+            // Unsupported afi, afiFlags = true, timer = 160
+            (byte) 0x01, (byte) 0x00, (byte) 0x01,
+            (byte) 0x80, (byte) 0x00, (byte) 0x00, (byte) 0xa0,
+            //Ipv6  Unicast afiFlags = false, timer = 160
+            (byte) 0x00, (byte) 0x02, (byte) 0x01,
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0xa0
+        };
+        final LlGracefulRestartCapability capability = new LlGracefulRestartCapabilityBuilder()
+                .setTables(Arrays.asList(new TablesBuilder()
+                        .setAfi(Ipv4AddressFamily.class)
+                        .setSafi(UnicastSubsequentAddressFamily.class)
+                        .setAfiFlags(new Tables.AfiFlags(false))
+                        .setLongLivedStaleTime(10L)
+                        .build(),
+                        new TablesBuilder()
+                                .setAfi(Ipv6AddressFamily.class)
+                                .setSafi(UnicastSubsequentAddressFamily.class)
+                                .setAfiFlags(new Tables.AfiFlags(false))
+                                .setLongLivedStaleTime(160L)
+                                .build())).build();
+
+        final CParameters cParameters = new CParametersBuilder().addAugmentation(CParameters1.class,
+                new CParameters1Builder().setLlGracefulRestartCapability(capability).build()).build();
+        LlGracefulCapabilityHandler handler1 = new LlGracefulCapabilityHandler(
+                afir, safir);
+        Assert.assertEquals(cParameters, handler1.parseCapability(Unpooled.wrappedBuffer(capaBytes)
+                .slice(2, capaBytes.length - 2)));
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void testUnsupportedSafi() {
         final LlGracefulRestartCapability capability = new LlGracefulRestartCapabilityBuilder()
@@ -112,6 +164,44 @@ public class LlGracefulCapabilityHandlerTest {
                 new CParameters1Builder().setLlGracefulRestartCapability(capability).build()).build();
         final ByteBuf buffer = Unpooled.buffer();
         this.handler.serializeCapability(cParameters, buffer);
+    }
+
+    @Test
+    public void testRecvdUnsupportedSafi() {
+        final byte[] capaBytes = {
+            //header
+            (byte) 0x47, (byte) 0x15,
+            // Ipv4 Unicast, afiFlags = false, timer = 10
+            (byte) 0x00, (byte) 0x01, (byte) 0x01,
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x0a,
+            // Unsupported safi, afiFlags = true, timer = 160
+            (byte) 0x00, (byte) 0x01, (byte) 0x85,
+            (byte) 0x80, (byte) 0x00, (byte) 0x00, (byte) 0xa0,
+            //Ipv6  Unicast afiFlags = false, timer = 160
+            (byte) 0x00, (byte) 0x02, (byte) 0x01,
+            (byte) 0x80, (byte) 0x00, (byte) 0x00, (byte) 0xa0
+        };
+
+        final LlGracefulRestartCapability capability = new LlGracefulRestartCapabilityBuilder()
+                .setTables(Arrays.asList(new TablesBuilder()
+                        .setAfi(Ipv4AddressFamily.class)
+                        .setSafi(UnicastSubsequentAddressFamily.class)
+                        .setAfiFlags(new Tables.AfiFlags(false))
+                        .setLongLivedStaleTime(10L)
+                        .build(),
+                        new TablesBuilder()
+                                .setAfi(Ipv6AddressFamily.class)
+                                .setSafi(UnicastSubsequentAddressFamily.class)
+                                .setAfiFlags(new Tables.AfiFlags(true))
+                                .setLongLivedStaleTime(160L)
+                                .build())).build();
+
+        final CParameters cParameters = new CParametersBuilder().addAugmentation(CParameters1.class,
+                new CParameters1Builder().setLlGracefulRestartCapability(capability).build()).build();
+        LlGracefulCapabilityHandler handler1 = new LlGracefulCapabilityHandler(
+                afir, safir);
+        Assert.assertEquals(cParameters, handler1.parseCapability(Unpooled.wrappedBuffer(capaBytes)
+                .slice(2, capaBytes.length - 2)));
     }
 
     @Test(expected = IllegalArgumentException.class)
