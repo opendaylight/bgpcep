490da953ae4393f65d7ff7a633dcebdda175bf71
Robert Varga <robert.varga@pantheon.tech>
2020-05-13 22:20:30 +0200

    Convert config-loader to OSGi DS
    The primary task here is getting rid of explicit blueprint while
    retaining autowiring functionality.
    
    This forces us to decompose ConfigLoaderImpl into multiple classes,
    which ends up being a boon for tests, as they can completely forgo
    threading.
    
    First of all FileWatcherImpl was renamed to DefaultFileWatcher and
    is a completely separate component, with proper lifecycle without
    edge case failures.
    
    Next we split out AbstractConfigLoader with all the loading logic
    and AbstractWatchingConfigLoader with the addition of having notion
    of a FileWatcher. This allows us to use synchronous loading in
    test harness, while also providing the ability to have async
    processing.
    
    We then instantiate SimpleConfigLoader and OSGiConfigLoader to
    handle the cases of static vs. dynamic codecs, as well as
    TestConfigLoader to handle unit testing tasks.
    
    We can therefore drop dependency on odl-controller-blueprint, in
    favor of depending on YANG tools codecs instead.
    
    JIRA: BGPCEP-905
    Change-Id: I5549bdfeb372b2f9cb15a1690c399401432f4702
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- config-loader/config-loader-impl/src/main/java/org/opendaylight/bgpcep/config/loader/impl/DefaultFileWatcher.java
+++ config-loader/config-loader-impl/src/main/java/org/opendaylight/bgpcep/config/loader/impl/DefaultFileWatcher.java
@@ -16,35 +16,28 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.WatchService;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Singleton;
+import org.opendaylight.yangtools.concepts.AbstractRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class DefaultFileWatcher implements FileWatcher, AutoCloseable {
+@Singleton
+@Component(immediate = true, service = FileWatcher.class)
+public final class DefaultFileWatcher extends AbstractRegistration implements FileWatcher {
     private static final Logger LOG = LoggerFactory.getLogger(DefaultFileWatcher.class);
-    private static final String INTERRUPTED = "InterruptedException";
     //BGPCEP config folder OS agnostic path
     private static final Path PATH = Paths.get("etc","opendaylight","bgpcep");
+
     private final WatchService watchService;
 
     public DefaultFileWatcher() throws IOException {
-        this.watchService = FileSystems.getDefault().newWatchService();
-        final File file = new File(PATH.toString());
-        if (!file.exists()) {
-            if (!file.mkdirs()) {
-                LOG.warn("Failed to create config directory {}", PATH);
-                return;
-            }
-        }
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                DefaultFileWatcher.this.watchService.close();
-            } catch (final IOException e) {
-                LOG.warn(INTERRUPTED, e);
-            }
-        }));
-        PATH.register(this.watchService, OVERFLOW, ENTRY_CREATE);
-        LOG.info("File Watcher service initiated");
+        watchService = FileSystems.getDefault().newWatchService();
+        Runtime.getRuntime().addShutdownHook(new Thread(this::close));
     }
 
     @Override
@@ -53,14 +46,39 @@ public final class DefaultFileWatcher implements FileWatcher, AutoCloseable {
     }
 
     @Override
-    public synchronized WatchService getWatchService() {
-        return this.watchService;
+    public WatchService getWatchService() {
+        return watchService;
+    }
+
+    @Activate
+    @PostConstruct
+    public void activate() throws IOException {
+        final File file = new File(PATH.toString());
+        if (!file.exists()) {
+            if (!file.mkdirs()) {
+                LOG.warn("Failed to create config directory {}", PATH);
+                return;
+            }
+        }
+
+        PATH.register(this.watchService, OVERFLOW, ENTRY_CREATE);
+        LOG.info("File Watcher service started");
+    }
+
+    @Deactivate
+    @PreDestroy
+    public void deactivate() {
+        // Just route to close(), it will do the right thing
+        close();
     }
 
     @Override
-    public synchronized void close() throws Exception {
-        if (this.watchService != null) {
-            this.watchService.close();
+    protected void removeRegistration() {
+        try {
+            watchService.close();
+        } catch (IOException e) {
+            LOG.warn("Failed to close watch service", e);
         }
+        LOG.info("File Watcher service stopped");
     }
 }
