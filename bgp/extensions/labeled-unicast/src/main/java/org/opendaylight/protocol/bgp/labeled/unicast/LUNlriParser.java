f1af13cb11156e54a9f77213ae4186ae5e516d33
Robert Varga <robert.varga@pantheon.tech>
2020-10-16 23:05:02 +0200

    Update Labeled Unicast parser
    // FIXME: add a description
    
    Change-Id: I49c5128a7915df218b756584db4989160fc269cf
    JIRA: BGPCEP-902
    Signed-off-by: Kailas Deshmukh <kdeshmukh@lumnanetworks.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/extensions/labeled-unicast/src/main/java/org/opendaylight/protocol/bgp/labeled/unicast/LUNlriParser.java
+++ bgp/extensions/labeled-unicast/src/main/java/org/opendaylight/protocol/bgp/labeled/unicast/LUNlriParser.java
@@ -74,7 +74,7 @@ public class LUNlriParser implements NlriParser, NlriSerializer {
                                 .reach.nlri.advertized.routes.destination.type.DestinationLabeledUnicastCase)
                                 destinationType;
                     serializeNlri(labeledUnicastCase.getDestinationLabeledUnicast().getCLabeledUnicastDestination(),
-                        false, byteAggregator);
+                        byteAggregator);
                 } else if (destinationType instanceof org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang
                         .bgp.labeled.unicast.rev180329.update.attributes.mp.reach.nlri.advertized.routes.destination
                         .type.DestinationIpv6LabeledUnicastCase) {
@@ -85,7 +85,7 @@ public class LUNlriParser implements NlriParser, NlriSerializer {
                                 .reach.nlri.advertized.routes.destination.type.DestinationIpv6LabeledUnicastCase)
                                 destinationType;
                     serializeNlri(labeledUnicastCase.getDestinationIpv6LabeledUnicast().getCLabeledUnicastDestination(),
-                        false, byteAggregator);
+                        byteAggregator);
                 }
             }
         } else if (pathAttributes2 != null) {
@@ -98,31 +98,27 @@ public class LUNlriParser implements NlriParser, NlriSerializer {
                     final DestinationLabeledUnicastCase labeledUnicastCase = (DestinationLabeledUnicastCase)
                             destinationType;
                     serializeNlri(labeledUnicastCase.getDestinationLabeledUnicast().getCLabeledUnicastDestination(),
-                        true, byteAggregator);
+                        byteAggregator);
                 } else if (destinationType instanceof DestinationIpv6LabeledUnicastCase) {
                     final DestinationIpv6LabeledUnicastCase labeledUnicastCase = (DestinationIpv6LabeledUnicastCase)
                             destinationType;
                     serializeNlri(labeledUnicastCase.getDestinationIpv6LabeledUnicast().getCLabeledUnicastDestination(),
-                        true, byteAggregator);
+                        byteAggregator);
                 }
             }
         }
     }
 
-    protected static void serializeNlri(final List<CLabeledUnicastDestination> dests, final boolean isUnreachNlri,
-            final ByteBuf buffer) {
+    protected static void serializeNlri(final List<CLabeledUnicastDestination> dests, final ByteBuf buffer) {
         final ByteBuf nlriByteBuf = Unpooled.buffer();
         for (final CLabeledUnicastDestination dest : dests) {
             PathIdUtil.writePathId(dest.getPathId(), buffer);
 
-            final List<LabelStack> labelStack = dest.getLabelStack();
-            final IpPrefix prefix = dest.getPrefix();
             // Serialize the length field
-            // Length field contains one Byte which represents the length of label stack and prefix in bits
-            nlriByteBuf.writeByte((LABEL_LENGTH * (!isUnreachNlri ? labelStack.size() : 1)
-                    + getPrefixLength(prefix)) * Byte.SIZE);
+            // Length field contains prefix in bits
+            final IpPrefix prefix = dest.getPrefix();
+            nlriByteBuf.writeByte(getPrefixLength(prefix) * Byte.SIZE);
 
-            serializeLabelStackEntries(labelStack, isUnreachNlri, nlriByteBuf);
             serializePrefixField(prefix, nlriByteBuf);
         }
         buffer.writeBytes(nlriByteBuf);
