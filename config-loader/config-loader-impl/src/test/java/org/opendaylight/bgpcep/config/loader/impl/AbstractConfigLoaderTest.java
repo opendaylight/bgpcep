3fa019bc21c8b3174333ecc7248465812b586687
Robert Varga <robert.varga@pantheon.tech>
2020-10-17 10:13:43 +0200

    Switch ConfigLoader to mdsal-binding-runtime
    There is no need for the config loader to require Binding/DOM
    translation services, hence it is enough when it bindings to
    mdsal-binding-runtime instead of mdsal-binding-dom-adapter.
    
    This allows speedier start of services with smaller overall
    footprint.
    
    JIRA: BGPCEP-933
    Change-Id: I8aa9569591565dd440ac528bccc413b5aea7e358
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- config-loader/config-loader-impl/src/test/java/org/opendaylight/bgpcep/config/loader/impl/AbstractConfigLoaderTest.java
+++ config-loader/config-loader-impl/src/test/java/org/opendaylight/bgpcep/config/loader/impl/AbstractConfigLoaderTest.java
@@ -15,20 +15,13 @@ import org.junit.Before;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.opendaylight.bgpcep.config.loader.spi.ConfigFileProcessor;
-import org.opendaylight.mdsal.binding.dom.adapter.AdapterContext;
 import org.opendaylight.mdsal.binding.dom.adapter.test.AbstractConcurrentDataBrokerTest;
 import org.opendaylight.mdsal.binding.dom.adapter.test.AbstractDataBrokerTestCustomizer;
-import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
 import org.opendaylight.mdsal.dom.api.DOMSchemaService;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 
 public abstract class AbstractConfigLoaderTest extends AbstractConcurrentDataBrokerTest {
     protected final class TestConfigLoader extends AbstractConfigLoader {
-        @Override
-        public BindingNormalizedNodeSerializer getBindingNormalizedNodeSerializer() {
-            return mappingService.currentSerializer();
-        }
-
         @Override
         File directory() {
             return new File(getResourceFolder());
@@ -52,7 +45,6 @@ public abstract class AbstractConfigLoaderTest extends AbstractConcurrentDataBro
     protected final TestConfigLoader configLoader = new TestConfigLoader();
     @Mock
     ConfigFileProcessor processor;
-    protected AdapterContext mappingService;
     protected DOMSchemaService schemaService;
 
     public AbstractConfigLoaderTest() {
@@ -68,7 +60,6 @@ public abstract class AbstractConfigLoaderTest extends AbstractConcurrentDataBro
     @Override
     protected AbstractDataBrokerTestCustomizer createDataBrokerTestCustomizer() {
         final AbstractDataBrokerTestCustomizer customizer = super.createDataBrokerTestCustomizer();
-        this.mappingService = customizer.getAdapterContext();
         this.schemaService = customizer.getSchemaService();
         return customizer;
     }
