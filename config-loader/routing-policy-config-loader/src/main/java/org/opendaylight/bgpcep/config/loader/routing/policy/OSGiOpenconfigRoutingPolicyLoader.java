1a402fb3e383171beef89ff18de92fd06c0bdc78
Robert Varga <robert.varga@pantheon.tech>
2020-10-17 05:12:01 +0200

    Remove blueprint from routing-policy-config-loader
    This is a straight conversion from blueprint to OSGi DS/inject. The
    patch is a bit larger as it introduces some basic infrastructure
    for reuse across implementations.
    
    JIRA: BGPCEP-928
    Change-Id: Ica5a317cb6f44cf1baec8a9802af85c3b77e1c43
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ config-loader/routing-policy-config-loader/src/main/java/org/opendaylight/bgpcep/config/loader/routing/policy/OSGiOpenconfigRoutingPolicyLoader.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.config.loader.routing.policy;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.opendaylight.bgpcep.config.loader.spi.AbstractOSGiConfigFileProcessor;
+import org.opendaylight.bgpcep.config.loader.spi.ConfigLoader;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(immediate = true)
+// FIXME: merge with OpenconfigRoutingPolicyLoader once we have constructor injection
+public final class OSGiOpenconfigRoutingPolicyLoader extends AbstractOSGiConfigFileProcessor {
+    @Reference
+    DataBroker dataBroker;
+    @Reference
+    ConfigLoader configLoader;
+
+    private @Nullable OpenconfigRoutingPolicyLoader delegate;
+
+    @Activate
+    void activate() {
+        start(new OpenconfigRoutingPolicyLoader(configLoader, dataBroker));
+    }
+
+    @Deactivate
+    void deactivate() {
+        stop();
+    }
+}
