3de49fe8ecc382b5f48471e40539199a2c29a649
Robert Varga <robert.varga@pantheon.tech>
2020-10-17 06:11:13 +0200

    Remove blueprint from topology-config-loader
    This is a straight conversion from blueprint to OSGi DS/inject.
    
    JIRA: BGPCEP-929
    Change-Id: Ie098c51808c8a4aa11357605f3f802894152f6f3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ config-loader/topology-config-loader/src/main/java/org/opendaylight/bgpcep/config/loader/topology/OSGiOpenconfigConfigFileProcessor.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.bgpcep.config.loader.topology;
+
+import org.opendaylight.bgpcep.config.loader.spi.AbstractOSGiConfigFileProcessor;
+import org.opendaylight.bgpcep.config.loader.spi.ConfigLoader;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(immediate = true)
+// FIXME: merge with NetworkTopologyConfigFileProcessor once we have constructor injection
+public final class OSGiOpenconfigConfigFileProcessor extends AbstractOSGiConfigFileProcessor {
+    @Reference
+    DataBroker dataBroker;
+    @Reference
+    ConfigLoader configLoader;
+
+    @Activate
+    void activate() {
+        start(new NetworkTopologyConfigFileProcessor(configLoader, dataBroker));
+    }
+
+    @Deactivate
+    void deactivate() {
+        stop();
+    }
+}
