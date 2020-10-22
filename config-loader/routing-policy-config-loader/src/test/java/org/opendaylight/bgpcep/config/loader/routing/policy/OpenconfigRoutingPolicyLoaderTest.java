72268e66caa85aa0535f79d6d999e0b93f5eb59e
guillaume.lambert <guillaume.lambert@orange.com>
2020-10-22 08:38:45 +0200

    remove some useless empty lines
    Change-Id: I01aad5992f174e82dd5ea31eed69f33a4cc81167
    Co-authored-by: Robert Varga <robert.varga@pantheon.tech>
    Signed-off-by: guillaume.lambert <guillaume.lambert@orange.com>
    (cherry picked from commit 02b111541d5c489538bc4bc835d7084bf766865a)

--- config-loader/routing-policy-config-loader/src/test/java/org/opendaylight/bgpcep/config/loader/routing/policy/OpenconfigRoutingPolicyLoaderTest.java
+++ config-loader/routing-policy-config-loader/src/test/java/org/opendaylight/bgpcep/config/loader/routing/policy/OpenconfigRoutingPolicyLoaderTest.java
@@ -5,7 +5,6 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.bgpcep.config.loader.routing.policy;
 
 import static org.junit.Assert.assertEquals;
