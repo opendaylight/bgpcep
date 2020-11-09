b6909d650d92c84c72c177db322960906eef29f4
Robert Varga <robert.varga@pantheon.tech>
2020-11-09 12:39:10 +0100

    Convert BGP extensions to OSGi DS
    Most of these are straightforward ports.
    bgp-linkstate will need a follow-up to deal with BGPActivator.
    bgp-flowspace will need a follow-up to deal with BGP/RIBActivator.
    
    JIRA: BGPCEP-941
    Change-Id: Ib6ebbb41b3cd1095e1809e91a46fee38ebaf722f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/extensions/l3vpn/src/main/java/org/opendaylight/protocol/bgp/l3vpn/RIBActivator.java
+++ bgp/extensions/l3vpn/src/main/java/org/opendaylight/protocol/bgp/l3vpn/RIBActivator.java
@@ -8,6 +8,8 @@
 package org.opendaylight.protocol.bgp.l3vpn;
 
 import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import org.kohsuke.MetaInfServices;
 import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
 import org.opendaylight.protocol.bgp.l3vpn.mcast.L3VpnMcastIpv4RIBSupport;
@@ -22,26 +24,34 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.type
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev200120.Ipv6AddressFamily;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev200120.MplsLabeledVpnSubsequentAddressFamily;
 import org.opendaylight.yangtools.concepts.Registration;
+import org.osgi.service.component.annotations.Component;
 
 /**
  * RIBActivator.
  *
  * @author Claudio D. Gasparini
  */
+@Singleton
+@Component(immediate = true, service = RIBExtensionProviderActivator.class,
+           property = "type=org.opendaylight.protocol.bgp.l3vpn.RIBActivator")
 @MetaInfServices(value = RIBExtensionProviderActivator.class)
 public final class RIBActivator extends AbstractRIBExtensionProviderActivator {
+    @Inject
+    public RIBActivator() {
+        // Exposed for DI
+    }
+
     @Override
     protected List<Registration> startRIBExtensionProviderImpl(final RIBExtensionProviderContext context,
             final BindingNormalizedNodeSerializer mappingService) {
         return List.of(
-                context.registerRIBSupport(Ipv4AddressFamily.class,
-                        MplsLabeledVpnSubsequentAddressFamily.class, VpnIpv4RIBSupport.getInstance(mappingService)),
-                context.registerRIBSupport(Ipv6AddressFamily.class,
-                        MplsLabeledVpnSubsequentAddressFamily.class, VpnIpv6RIBSupport.getInstance(mappingService)),
-                context.registerRIBSupport(Ipv4AddressFamily.class, McastMplsLabeledVpnSubsequentAddressFamily.class,
-                        L3VpnMcastIpv4RIBSupport.getInstance(mappingService)),
-                context.registerRIBSupport(Ipv6AddressFamily.class, McastMplsLabeledVpnSubsequentAddressFamily.class,
-                        L3VpnMcastIpv6RIBSupport.getInstance(mappingService))
-        );
+            context.registerRIBSupport(Ipv4AddressFamily.class, MplsLabeledVpnSubsequentAddressFamily.class,
+                VpnIpv4RIBSupport.getInstance(mappingService)),
+            context.registerRIBSupport(Ipv6AddressFamily.class, MplsLabeledVpnSubsequentAddressFamily.class,
+                VpnIpv6RIBSupport.getInstance(mappingService)),
+            context.registerRIBSupport(Ipv4AddressFamily.class, McastMplsLabeledVpnSubsequentAddressFamily.class,
+                L3VpnMcastIpv4RIBSupport.getInstance(mappingService)),
+            context.registerRIBSupport(Ipv6AddressFamily.class, McastMplsLabeledVpnSubsequentAddressFamily.class,
+                L3VpnMcastIpv6RIBSupport.getInstance(mappingService)));
     }
 }
