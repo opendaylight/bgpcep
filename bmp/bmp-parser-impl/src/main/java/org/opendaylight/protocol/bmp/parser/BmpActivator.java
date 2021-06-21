bcc80c13ae19437fe94c2431ff639035435bf8e4
Robert Varga <robert.varga@pantheon.tech>
2021-06-21 23:34:57 +0200

    Convert bmp-parser-impl to OSGi DS
    BmpActivator is essentially a simple whiteboard dispatch, let's express
    it that way, eliminating some blueprint wiring.
    
    JIRA: BGPCEP-951
    Change-Id: I77b4d0d3a53fb73adb72c8affb84e0c593919675
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bmp/bmp-parser-impl/src/main/java/org/opendaylight/protocol/bmp/parser/BmpActivator.java
+++ bmp/bmp-parser-impl/src/main/java/org/opendaylight/protocol/bmp/parser/BmpActivator.java
@@ -7,8 +7,9 @@
  */
 package org.opendaylight.protocol.bmp.parser;
 
-import java.util.ArrayList;
 import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import org.kohsuke.MetaInfServices;
 import org.opendaylight.protocol.bgp.parser.spi.AddressFamilyRegistry;
 import org.opendaylight.protocol.bgp.parser.spi.BGPExtensionConsumerContext;
@@ -40,7 +41,6 @@ import org.opendaylight.protocol.bmp.parser.tlv.StatType011TlvHandler;
 import org.opendaylight.protocol.bmp.parser.tlv.StatType012TlvHandler;
 import org.opendaylight.protocol.bmp.parser.tlv.StatType013TlvHandler;
 import org.opendaylight.protocol.bmp.parser.tlv.StringTlvHandler;
-import org.opendaylight.protocol.bmp.spi.registry.AbstractBmpExtensionProviderActivator;
 import org.opendaylight.protocol.bmp.spi.registry.BmpExtensionProviderActivator;
 import org.opendaylight.protocol.bmp.spi.registry.BmpExtensionProviderContext;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev200120.InitiationMessage;
@@ -69,154 +69,148 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.mess
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev200120.stat.tlvs.RejectedPrefixesTlv;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev200120.stat.tlvs.UpdatesTreatedAsWithdrawTlv;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bmp.message.rev200120.string.tlv.StringTlv;
+import org.opendaylight.yangtools.concepts.Registration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
-@MetaInfServices(value = BmpExtensionProviderActivator.class)
-public final class BmpActivator extends AbstractBmpExtensionProviderActivator {
+@Component(immediate = true, property = "type=org.opendaylight.protocol.bmp.parser.BmpActivator")
+@MetaInfServices
+@Singleton
+public final class BmpActivator implements BmpExtensionProviderActivator {
     private final MessageRegistry messageRegistry;
     private final AddressFamilyRegistry afiRegistry;
     private final SubsequentAddressFamilyRegistry safiRegistry;
 
-    // FIXME: this needs to be properly injected
-    public BmpActivator(final BGPExtensionConsumerContext context) {
+    @Inject
+    @Activate
+    public BmpActivator(final @Reference BGPExtensionConsumerContext context) {
         this.messageRegistry = context.getMessageRegistry();
         this.afiRegistry = context.getAddressFamilyRegistry();
         this.safiRegistry = context.getSubsequentAddressFamilyRegistry();
     }
 
     @Override
-    protected List<AutoCloseable> startImpl(final BmpExtensionProviderContext context) {
-        final List<AutoCloseable> regs = new ArrayList<>();
-        registerBmpTlvHandlers(regs, context);
-        registerBmpStatTlvHandlers(regs, context);
-        registerBmpMessageHandlers(regs, context);
-        return regs;
-    }
-
-    private void registerBmpMessageHandlers(final List<AutoCloseable> regs, final BmpExtensionProviderContext context) {
-        final InitiationHandler initiationHandler = new InitiationHandler(context.getBmpInitiationTlvRegistry());
-        regs.add(context.registerBmpMessageParser(initiationHandler.getBmpMessageType(), initiationHandler));
-        regs.add(context.registerBmpMessageSerializer(InitiationMessage.class, initiationHandler));
-
-        final TerminationHandler terminationHandler = new TerminationHandler(context.getBmpTerminationTlvRegistry());
-        regs.add(context.registerBmpMessageParser(terminationHandler.getBmpMessageType(), terminationHandler));
-        regs.add(context.registerBmpMessageSerializer(TerminationMessage.class, terminationHandler));
-
-        final PeerUpHandler peerUpHandler = new PeerUpHandler(this.messageRegistry, context.getBmpPeerUpTlvRegistry());
-        regs.add(context.registerBmpMessageParser(peerUpHandler.getBmpMessageType(), peerUpHandler));
-        regs.add(context.registerBmpMessageSerializer(PeerUpNotification.class, peerUpHandler));
-
-        final PeerDownHandler peerDownHandler = new PeerDownHandler(this.messageRegistry);
-        regs.add(context.registerBmpMessageParser(peerDownHandler.getBmpMessageType(), peerDownHandler));
-        regs.add(context.registerBmpMessageSerializer(PeerDownNotification.class, peerDownHandler));
-
-        final StatisticsReportHandler statisticsReportHandler = new StatisticsReportHandler(this.messageRegistry,
-                context.getBmpStatisticsTlvRegistry());
-        regs.add(context.registerBmpMessageParser(statisticsReportHandler.getBmpMessageType(),
-                statisticsReportHandler));
-        regs.add(context.registerBmpMessageSerializer(StatsReportsMessage.class, statisticsReportHandler));
-
-        final RouteMonitoringMessageHandler routeMonitoringMessageHandler =
-                new RouteMonitoringMessageHandler(this.messageRegistry);
-        regs.add(context.registerBmpMessageParser(routeMonitoringMessageHandler.getBmpMessageType(),
-                routeMonitoringMessageHandler));
-        regs.add(context.registerBmpMessageSerializer(RouteMonitoringMessage.class, routeMonitoringMessageHandler));
-
-        final RouteMirroringMessageHandler routeMirroringMessageHandler =
-                new RouteMirroringMessageHandler(this.messageRegistry, context.getBmpRouteMirroringTlvRegistry());
-        regs.add(context.registerBmpMessageParser(routeMirroringMessageHandler.getBmpMessageType(),
-                routeMirroringMessageHandler));
-        regs.add(context.registerBmpMessageSerializer(RouteMirroringMessage.class, routeMirroringMessageHandler));
-
-    }
-
-    private static void registerBmpTlvHandlers(final List<AutoCloseable> regs,
-            final BmpExtensionProviderContext context) {
+    public List<Registration> start(final BmpExtensionProviderContext context) {
         final DescriptionTlvHandler descriptionTlvHandler = new DescriptionTlvHandler();
-        regs.add(context.registerBmpInitiationTlvParser(DescriptionTlvHandler.TYPE, descriptionTlvHandler));
-        regs.add(context.registerBmpInitiationTlvSerializer(DescriptionTlv.class, descriptionTlvHandler));
-
         final NameTlvHandler nameTlvHandler = new NameTlvHandler();
-        regs.add(context.registerBmpInitiationTlvParser(NameTlvHandler.TYPE, nameTlvHandler));
-        regs.add(context.registerBmpInitiationTlvSerializer(NameTlv.class, nameTlvHandler));
-
         final StringTlvHandler stringTlvHandler = new StringTlvHandler();
-        regs.add(context.registerBmpInitiationTlvParser(StringTlvHandler.TYPE, stringTlvHandler));
-        regs.add(context.registerBmpInitiationTlvSerializer(StringTlv.class, stringTlvHandler));
-        regs.add(context.registerBmpTerminationTlvParser(StringTlvHandler.TYPE, stringTlvHandler));
-        regs.add(context.registerBmpTerminationTlvSerializer(StringTlv.class, stringTlvHandler));
-        regs.add(context.registerBmpPeerUpTlvParser(StringTlvHandler.TYPE, stringTlvHandler));
-        regs.add(context.registerBmpPeerUpTlvSerializer(StringTlv.class, stringTlvHandler));
-
         final ReasonTlvHandler reasonTlvHandler = new ReasonTlvHandler();
-        regs.add(context.registerBmpTerminationTlvParser(ReasonTlvHandler.TYPE, reasonTlvHandler));
-        regs.add(context.registerBmpTerminationTlvSerializer(ReasonTlv.class, reasonTlvHandler));
-
         final MirrorInformationTlvHandler informationTlvHandler = new MirrorInformationTlvHandler();
-        regs.add(context.registerBmpRouteMirroringTlvParser(MirrorInformationTlvHandler.TYPE, informationTlvHandler));
-        regs.add(context.registerBmpRouteMirroringTlvSerializer(MirrorInformationTlv.class, informationTlvHandler));
-    }
-
-    private void registerBmpStatTlvHandlers(final List<AutoCloseable> regs,
-            final BmpExtensionProviderContext context) {
         final StatType000TlvHandler statType000TlvHandler = new StatType000TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType000TlvHandler.TYPE, statType000TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(RejectedPrefixesTlv.class, statType000TlvHandler));
-
         final StatType001TlvHandler statType001TlvHandler = new StatType001TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType001TlvHandler.TYPE, statType001TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(DuplicatePrefixAdvertisementsTlv.class,
-                statType001TlvHandler));
-
         final StatType002TlvHandler statType002TlvHandler = new StatType002TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType002TlvHandler.TYPE, statType002TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(DuplicateWithdrawsTlv.class, statType002TlvHandler));
-
         final StatType003TlvHandler statType003TlvHandler = new StatType003TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType003TlvHandler.TYPE, statType003TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(InvalidatedClusterListLoopTlv.class,
-                statType003TlvHandler));
-
         final StatType004TlvHandler statType004TlvHandler = new StatType004TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType004TlvHandler.TYPE, statType004TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(InvalidatedAsPathLoopTlv.class, statType004TlvHandler));
-
         final StatType005TlvHandler statType005TlvHandler = new StatType005TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType005TlvHandler.TYPE, statType005TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(InvalidatedOriginatorIdTlv.class, statType005TlvHandler));
-
         final StatType006TlvHandler statType006TlvHandler = new StatType006TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType006TlvHandler.TYPE, statType006TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(InvalidatedAsConfedLoopTlv.class, statType006TlvHandler));
-
         final StatType007TlvHandler statType007TlvHandler = new StatType007TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType007TlvHandler.TYPE, statType007TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(AdjRibsInRoutesTlv.class, statType007TlvHandler));
-
         final StatType008TlvHandler statType008TlvHandler = new StatType008TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType008TlvHandler.TYPE, statType008TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(LocRibRoutesTlv.class, statType008TlvHandler));
-
         final StatType009TlvHandler statType009TlvHandler =
-                new StatType009TlvHandler(this.afiRegistry, this.safiRegistry);
-        regs.add(context.registerBmpStatisticsTlvParser(StatType009TlvHandler.TYPE, statType009TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(PerAfiSafiAdjRibInTlv.class, statType009TlvHandler));
-
+            new StatType009TlvHandler(this.afiRegistry, this.safiRegistry);
         final StatType010TlvHandler statType010TlvHandler =
-                new StatType010TlvHandler(this.afiRegistry, this.safiRegistry);
-        regs.add(context.registerBmpStatisticsTlvParser(StatType010TlvHandler.TYPE, statType010TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(PerAfiSafiLocRibTlv.class, statType010TlvHandler));
-
+            new StatType010TlvHandler(this.afiRegistry, this.safiRegistry);
         final StatType011TlvHandler statType011TlvHandler = new StatType011TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType011TlvHandler.TYPE, statType011TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(UpdatesTreatedAsWithdrawTlv.class, statType011TlvHandler));
-
         final StatType012TlvHandler statType012TlvHandler = new StatType012TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType012TlvHandler.TYPE, statType012TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(PrefixesTreatedAsWithdrawTlv.class, statType012TlvHandler));
-
         final StatType013TlvHandler statType013TlvHandler = new StatType013TlvHandler();
-        regs.add(context.registerBmpStatisticsTlvParser(StatType013TlvHandler.TYPE, statType013TlvHandler));
-        regs.add(context.registerBmpStatisticsTlvSerializer(DuplicateUpdatesTlv.class, statType013TlvHandler));
 
+        final InitiationHandler initiationHandler = new InitiationHandler(context.getBmpInitiationTlvRegistry());
+        final TerminationHandler terminationHandler = new TerminationHandler(context.getBmpTerminationTlvRegistry());
+        final PeerUpHandler peerUpHandler = new PeerUpHandler(this.messageRegistry, context.getBmpPeerUpTlvRegistry());
+        final PeerDownHandler peerDownHandler = new PeerDownHandler(this.messageRegistry);
+        final RouteMirroringMessageHandler routeMirroringMessageHandler =
+            new RouteMirroringMessageHandler(this.messageRegistry, context.getBmpRouteMirroringTlvRegistry());
+        final StatisticsReportHandler statisticsReportHandler = new StatisticsReportHandler(this.messageRegistry,
+            context.getBmpStatisticsTlvRegistry());
+        final RouteMonitoringMessageHandler routeMonitoringMessageHandler =
+            new RouteMonitoringMessageHandler(this.messageRegistry);
+
+        // FIXME: Convert to proper whiteboard pattern
+        return List.of(
+            context.registerBmpInitiationTlvParser(DescriptionTlvHandler.TYPE, descriptionTlvHandler),
+            context.registerBmpInitiationTlvSerializer(DescriptionTlv.class, descriptionTlvHandler),
+            context.registerBmpInitiationTlvParser(NameTlvHandler.TYPE, nameTlvHandler),
+            context.registerBmpInitiationTlvSerializer(NameTlv.class, nameTlvHandler),
+
+            context.registerBmpInitiationTlvParser(StringTlvHandler.TYPE, stringTlvHandler),
+            context.registerBmpInitiationTlvSerializer(StringTlv.class, stringTlvHandler),
+            context.registerBmpTerminationTlvParser(StringTlvHandler.TYPE, stringTlvHandler),
+            context.registerBmpTerminationTlvSerializer(StringTlv.class, stringTlvHandler),
+            context.registerBmpPeerUpTlvParser(StringTlvHandler.TYPE, stringTlvHandler),
+            context.registerBmpPeerUpTlvSerializer(StringTlv.class, stringTlvHandler),
+
+            context.registerBmpTerminationTlvParser(ReasonTlvHandler.TYPE, reasonTlvHandler),
+            context.registerBmpTerminationTlvSerializer(ReasonTlv.class, reasonTlvHandler),
+
+            context.registerBmpRouteMirroringTlvParser(MirrorInformationTlvHandler.TYPE, informationTlvHandler),
+            context.registerBmpRouteMirroringTlvSerializer(MirrorInformationTlv.class, informationTlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType000TlvHandler.TYPE, statType000TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(RejectedPrefixesTlv.class, statType000TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType001TlvHandler.TYPE, statType001TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(DuplicatePrefixAdvertisementsTlv.class, statType001TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType002TlvHandler.TYPE, statType002TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(DuplicateWithdrawsTlv.class, statType002TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType003TlvHandler.TYPE, statType003TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(InvalidatedClusterListLoopTlv.class, statType003TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType004TlvHandler.TYPE, statType004TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(InvalidatedAsPathLoopTlv.class, statType004TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType005TlvHandler.TYPE, statType005TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(InvalidatedOriginatorIdTlv.class, statType005TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType006TlvHandler.TYPE, statType006TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(InvalidatedAsConfedLoopTlv.class, statType006TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType007TlvHandler.TYPE, statType007TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(AdjRibsInRoutesTlv.class, statType007TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType008TlvHandler.TYPE, statType008TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(LocRibRoutesTlv.class, statType008TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType009TlvHandler.TYPE, statType009TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(PerAfiSafiAdjRibInTlv.class, statType009TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType010TlvHandler.TYPE, statType010TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(PerAfiSafiLocRibTlv.class, statType010TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType011TlvHandler.TYPE, statType011TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(UpdatesTreatedAsWithdrawTlv.class, statType011TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType012TlvHandler.TYPE, statType012TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(PrefixesTreatedAsWithdrawTlv.class, statType012TlvHandler),
+
+            context.registerBmpStatisticsTlvParser(StatType013TlvHandler.TYPE, statType013TlvHandler),
+            context.registerBmpStatisticsTlvSerializer(DuplicateUpdatesTlv.class, statType013TlvHandler),
+
+
+            context.registerBmpMessageParser(initiationHandler.getBmpMessageType(), initiationHandler),
+            context.registerBmpMessageSerializer(InitiationMessage.class, initiationHandler),
+
+            context.registerBmpMessageParser(terminationHandler.getBmpMessageType(), terminationHandler),
+            context.registerBmpMessageSerializer(TerminationMessage.class, terminationHandler),
+
+            context.registerBmpMessageParser(peerUpHandler.getBmpMessageType(), peerUpHandler),
+            context.registerBmpMessageSerializer(PeerUpNotification.class, peerUpHandler),
+
+            context.registerBmpMessageParser(peerDownHandler.getBmpMessageType(), peerDownHandler),
+            context.registerBmpMessageSerializer(PeerDownNotification.class, peerDownHandler),
+
+            context.registerBmpMessageParser(statisticsReportHandler.getBmpMessageType(),
+                statisticsReportHandler),
+            context.registerBmpMessageSerializer(StatsReportsMessage.class, statisticsReportHandler),
+
+            context.registerBmpMessageParser(routeMonitoringMessageHandler.getBmpMessageType(),
+                routeMonitoringMessageHandler),
+            context.registerBmpMessageSerializer(RouteMonitoringMessage.class, routeMonitoringMessageHandler),
+
+            context.registerBmpMessageParser(routeMirroringMessageHandler.getBmpMessageType(),
+                routeMirroringMessageHandler),
+            context.registerBmpMessageSerializer(RouteMirroringMessage.class, routeMirroringMessageHandler)
+            );
     }
 }
