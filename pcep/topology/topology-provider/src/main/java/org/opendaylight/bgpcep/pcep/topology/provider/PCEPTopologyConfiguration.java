b4cc22e3044e7720386a06d9eddfacc9a371fcab
Robert Varga <robert.varga@pantheon.tech>
2021-11-19 16:05:48 +0100

    Remove SpeakerIdMapping
    Tunnelling this simple map through PCEPTopologyConfiguration is
    completely unnecessary as PCEPStatefulPeerProposal can easily have
    it pushed via a DTCL and keep it close by.
    
    JIRA: BGPCEP-983
    Change-Id: I76abc4e94f3f69c4fab00b39dbaf0cfd6b11e055
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPTopologyConfiguration.java
+++ pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPTopologyConfiguration.java
@@ -9,7 +9,6 @@ package org.opendaylight.bgpcep.pcep.topology.provider;
 
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.net.InetAddresses;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -23,23 +22,20 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.config.rev181109.PcepNodeConfig;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.config.rev181109.PcepTopologyTypeConfig;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.sync.optimizations.config.rev181109.PcepNodeSyncConfig;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
 import org.opendaylight.yangtools.concepts.Immutable;
 
 final class PCEPTopologyConfiguration implements Immutable {
-    private final @NonNull SpeakerIdMapping speakerIds;
     private final @NonNull InetSocketAddress address;
     private final @NonNull KeyMapping keys;
     private final short rpcTimeout;
 
     PCEPTopologyConfiguration(final @NonNull InetSocketAddress address, final short rpcTimeout,
-            final @NonNull KeyMapping keys, final @NonNull SpeakerIdMapping speakerIds) {
+            final @NonNull KeyMapping keys) {
         this.address = requireNonNull(address);
         this.keys = requireNonNull(keys);
-        this.speakerIds = requireNonNull(speakerIds);
         this.rpcTimeout = rpcTimeout;
     }
 
@@ -56,7 +52,7 @@ final class PCEPTopologyConfiguration implements Immutable {
 
         return new PCEPTopologyConfiguration(
             getInetSocketAddress(sessionConfig.getListenAddress(), sessionConfig.getListenPort()),
-            sessionConfig.getRpcTimeout(), constructKeys(topology.getNode()), contructSpeakersId(topology.getNode()));
+            sessionConfig.getRpcTimeout(), constructKeys(topology.getNode()));
     }
 
     short getRpcTimeout() {
@@ -69,10 +65,6 @@ final class PCEPTopologyConfiguration implements Immutable {
 
     @NonNull KeyMapping getKeys() {
         return keys;
-    }
-
-    @NonNull SpeakerIdMapping getSpeakerIds() {
-        return speakerIds;
     }
 
     private static @NonNull KeyMapping constructKeys(final @Nullable Map<NodeKey, Node> nodes) {
@@ -100,33 +92,6 @@ final class PCEPTopologyConfiguration implements Immutable {
         }
 
         return KeyMapping.of(passwords);
-    }
-
-    private static @NonNull SpeakerIdMapping contructSpeakersId(final @Nullable Map<NodeKey, Node> nodes) {
-        if (nodes == null) {
-            return SpeakerIdMapping.of();
-        }
-
-        final var builder = ImmutableMap.<InetAddress, byte[]>builder();
-        for (var node : nodes.values()) {
-            if (node != null) {
-                final var nodeConfig = node.augmentation(PcepNodeConfig.class);
-                if (nodeConfig != null) {
-                    final var sessionConfig = nodeConfig.getSessionConfig();
-                    if (sessionConfig != null) {
-                        final var nodeSyncConfig = sessionConfig.augmentation(PcepNodeSyncConfig.class);
-                        if (nodeSyncConfig != null) {
-                            final var speakerEntityId = nodeSyncConfig.getSpeakerEntityIdValue();
-                            if (speakerEntityId != null) {
-                                builder.put(nodeAddress(node), speakerEntityId);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return SpeakerIdMapping.copyOf(builder.build());
     }
 
     private static InetAddress nodeAddress(final Node node) {
