fd88c45c799f69c6a5c3c8db8894efa11423ad35
Robert Varga <robert.varga@pantheon.tech>
2021-11-19 19:56:40 +0100

    WIP: Use DTCL to maintain LSPDB versions
    Rather than issuing synchronous writes, use a DTCL to acquire
    versions populated in the datastore. This eliminates a failure code
    path and makes connections a tad faster.
    
    // FIXME: make sure the code populating these waits for commit
    //        before responding to the PCC and how the protocol handles
    //        older versions
    
    JIRA: BGPCEP-989
    Change-Id: I90c45dfac9a610f1d153eec1d95fbb50bad86800
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPStatefulPeerProposal.java
+++ pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPStatefulPeerProposal.java
@@ -8,20 +8,17 @@
 package org.opendaylight.bgpcep.pcep.topology.provider;
 
 import static com.google.common.base.Verify.verifyNotNull;
-import static java.util.Objects.requireNonNull;
 
-import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.annotations.VisibleForTesting;
 import java.net.InetSocketAddress;
 import java.util.Collection;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.mdsal.binding.api.ClusteredDataTreeChangeListener;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.mdsal.binding.api.DataTreeIdentifier;
 import org.opendaylight.mdsal.binding.api.DataTreeModification;
-import org.opendaylight.mdsal.binding.api.ReadTransaction;
 import org.opendaylight.mdsal.common.api.LogicalDatastoreType;
 import org.opendaylight.protocol.pcep.PCEPPeerProposal;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.pcep.sync.optimizations.rev200720.PathComputationClient1;
@@ -39,51 +36,83 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
 import org.opendaylight.yangtools.concepts.AbstractRegistration;
 import org.opendaylight.yangtools.concepts.Registration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 final class PCEPStatefulPeerProposal extends AbstractRegistration implements PCEPPeerProposal {
-    private static final class SpeakerIdListener implements ClusteredDataTreeChangeListener<PcepNodeSyncConfig> {
-        final Map<NodeId, byte[]> map = new ConcurrentHashMap<>();
+    @VisibleForTesting
+    private abstract static class AbstractListener<D extends DataObject, V>
+            implements ClusteredDataTreeChangeListener<D> {
+        final Map<NodeId, V> map = new ConcurrentHashMap<>();
         final Registration reg;
 
+        AbstractListener(final DataBroker dataBroker, final @NonNull LogicalDatastoreType datastore,
+                final @NonNull InstanceIdentifier<D> wildcard) {
+            reg = dataBroker.registerDataTreeChangeListener(DataTreeIdentifier.create(datastore, wildcard), this);
+        }
+
+        final void remove(final DataTreeModification<?> modification) {
+            map.remove(extractNodeId(modification));
+        }
+
+        final void update(final DataTreeModification<?> modification, final V value) {
+            final var nodeId = extractNodeId(modification);
+            if (value != null) {
+                map.put(nodeId, value);
+            } else {
+                map.remove(nodeId);
+            }
+        }
+
+        private static @NonNull NodeId extractNodeId(final DataTreeModification<?> modification) {
+            return verifyNotNull(modification.getRootPath().getRootIdentifier().firstKeyOf(Node.class)).getNodeId();
+        }
+    }
+
+    @VisibleForTesting
+    static final class SpeakerIdListener extends AbstractListener<PcepNodeSyncConfig, byte[]> {
         SpeakerIdListener(final DataBroker dataBroker, final InstanceIdentifier<Topology> topologyId) {
-            reg = dataBroker.registerDataTreeChangeListener(DataTreeIdentifier.create(
-                LogicalDatastoreType.CONFIGURATION, topologyId.child(Node.class).augmentation(PcepNodeConfig.class)
-                    .child(SessionConfig.class).augmentation(PcepNodeSyncConfig.class)), this);
+            super(dataBroker, LogicalDatastoreType.CONFIGURATION, topologyId.child(Node.class)
+                .augmentation(PcepNodeConfig.class).child(SessionConfig.class)
+                .augmentation(PcepNodeSyncConfig.class));
         }
 
         @Override
         public void onDataTreeChanged(final Collection<DataTreeModification<PcepNodeSyncConfig>> changes) {
             for (var change : changes) {
-                final var nodeId = verifyNotNull(change.getRootPath().getRootIdentifier().firstKeyOf(Node.class))
-                    .getNodeId();
                 final var config = change.getRootNode().getDataAfter();
                 if (config != null) {
-                    final var speakerEntityId = config.getSpeakerEntityIdValue();
-                    if (speakerEntityId != null) {
-                        map.put(nodeId, speakerEntityId);
-                        continue;
-                    }
+                    update(change, config.getSpeakerEntityIdValue());
+                } else {
+                    remove(change);
                 }
-                map.remove(nodeId);
             }
         }
     }
 
-    private static final Logger LOG = LoggerFactory.getLogger(PCEPStatefulPeerProposal.class);
+    @VisibleForTesting
+    static final class LspDbVersionListener extends AbstractListener<LspDbVersion, LspDbVersion> {
+        LspDbVersionListener(final DataBroker dataBroker, final InstanceIdentifier<Topology> topologyId) {
+            super(dataBroker, LogicalDatastoreType.OPERATIONAL, topologyId.child(Node.class)
+                .augmentation(Node1.class).child(PathComputationClient.class)
+                .augmentation(PathComputationClient1.class).child(LspDbVersion.class));
+        }
 
-    private final InstanceIdentifier<Topology> topologyId;
+        @Override
+        public void onDataTreeChanged(final Collection<DataTreeModification<LspDbVersion>> changes) {
+            for (var change : changes) {
+                update(change, change.getRootNode().getDataAfter());
+            }
+        }
+    }
+
+    private final LspDbVersionListener lspDbVersions;
     private final SpeakerIdListener speakerIds;
-    private final DataBroker dataBroker;
 
     PCEPStatefulPeerProposal(final DataBroker dataBroker, final InstanceIdentifier<Topology> topologyId) {
-        this.dataBroker = requireNonNull(dataBroker);
-        this.topologyId = requireNonNull(topologyId);
+        lspDbVersions = new LspDbVersionListener(dataBroker, topologyId);
         speakerIds = new SpeakerIdListener(dataBroker, topologyId);
     }
 
@@ -99,31 +128,17 @@ final class PCEPStatefulPeerProposal extends AbstractRegistration implements PCE
             return;
         }
 
-        final var addr = address.getAddress();
-        final var nodeId = ServerSessionManager.createNodeId(addr);
-        // FIXME: BGPCEP-989: acquire this information via a DTCL and perform a simple lookup only
-        Optional<LspDbVersion> result = Optional.empty();
-        try (ReadTransaction rTx = dataBroker.newReadOnlyTransaction()) {
-            // FIXME: we should be listening for this configuration and keep a proper cache
-            final ListenableFuture<Optional<LspDbVersion>> future = rTx.read(LogicalDatastoreType.OPERATIONAL,
-                topologyId.child(Node.class, new NodeKey(nodeId))
-                    .augmentation(Node1.class).child(PathComputationClient.class)
-                    .augmentation(PathComputationClient1.class).child(LspDbVersion.class));
-            try {
-                result = future.get();
-            } catch (final InterruptedException | ExecutionException e) {
-                LOG.warn("Failed to read toplogy {}.", InstanceIdentifier.keyOf(topologyId), e);
-            }
-        }
-
+        final var nodeId = ServerSessionManager.createNodeId(address.getAddress());
+        final var dbVersion = lspDbVersions.map.get(nodeId);
         final var speakerId = speakerIds.map.get(nodeId);
-        if (speakerId == null && !result.isPresent()) {
+        if (speakerId == null && dbVersion == null) {
+            // Nothing to add
             return;
         }
 
         final Tlvs3Builder syncBuilder = new Tlvs3Builder();
-        if (result.isPresent()) {
-            syncBuilder.setLspDbVersion(result.get());
+        if (dbVersion != null) {
+            syncBuilder.setLspDbVersion(dbVersion);
         }
         if (speakerId != null) {
             syncBuilder.setSpeakerEntityId(new SpeakerEntityIdBuilder().setSpeakerEntityIdValue(speakerId).build());
@@ -133,6 +148,7 @@ final class PCEPStatefulPeerProposal extends AbstractRegistration implements PCE
 
     @Override
     protected void removeRegistration() {
+        lspDbVersions.reg.close();
         speakerIds.reg.close();
     }
 }
