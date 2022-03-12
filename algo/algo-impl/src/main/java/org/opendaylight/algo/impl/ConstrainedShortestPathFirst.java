947041d05aa480bcd60a6fa93b2710dc947433c6
guillaume.lambert <guillaume.lambert@orange.com>
2022-03-12 14:57:14 +0100

    Algo API impl slight refactoring
    JIRA: BGPCEP-979
    Signed-off-by: Guillaume Lambert <guillaume.lambert@orange.com>
    Change-Id: I7a77c2a1f8073d63e8727d129a4e0f744f0b5836

--- algo/algo-impl/src/main/java/org/opendaylight/algo/impl/ConstrainedShortestPathFirst.java
+++ algo/algo-impl/src/main/java/org/opendaylight/algo/impl/ConstrainedShortestPathFirst.java
@@ -81,13 +81,13 @@ public class ConstrainedShortestPathFirst extends AbstractPathComputation {
          * The "ConstrainedPathBuilder" object contains the optimal path if it exists
          * Otherwise an empty path with status failed is returned
          */
-        if (cpathBuilder.getStatus() == ComputationStatus.InProgress
-                || cpathBuilder.getPathDescription().size() == 0) {
-            cpathBuilder.setStatus(ComputationStatus.NoPath);
-        } else {
-            cpathBuilder.setStatus(ComputationStatus.Completed);
-        }
-        return cpathBuilder.build();
+        return cpathBuilder
+            .setStatus(
+                cpathBuilder.getStatus() == ComputationStatus.InProgress
+                        || cpathBuilder.getPathDescription().size() == 0
+                   ? ComputationStatus.NoPath
+                   : ComputationStatus.Completed)
+            .build();
     }
 
     private boolean relaxMultiConstraints(final ConnectedEdge edge, final CspfPath currentPath) {
