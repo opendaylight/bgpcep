1d5ec67794a22180de073edfecd7c85ae4de6f47
Olivier Dugeon <olivier.dugeon@orange.com>
2022-03-28 18:10:42 +0200

    Add Graph mapping to PCEP topology
    PCEP implementation is able to manage several pcep-topology.
    For each topology, a new PCEP Session Manager is started as
    well as a dedicated PCE server. However, each PCE server
    uses the same Graph Topology for Path Computation which
    could be useless.
    
    This patch add a new key in the pcep-config yang model to
    configure the associated TED i.e. the name of the Connected
    Graph that provides it. This allows a possible mapping
    between different PCEP Topology and Graph Topology.
    
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>
    Change-Id: I90ee70a63c22f2a0ece2f7731ef90dac0c05225e

--- pcep/server/server-api/src/main/java/org/opendaylight/bgpcep/pcep/server/PceServerProvider.java
+++ pcep/server/server-api/src/main/java/org/opendaylight/bgpcep/pcep/server/PceServerProvider.java
@@ -8,6 +8,7 @@
 
 package org.opendaylight.bgpcep.pcep.server;
 
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.graph.rev191125.graph.topology.GraphKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
 import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
@@ -24,9 +25,10 @@ public interface PceServerProvider {
     /**
      * Register PCEP Topology into PCE Server to manage LSP.
      *
-     * @param topology    Configured PCEP Topology
+     * @param topology  Configured PCEP Topology
+     * @param graphKey  Configured Connected Graph Topology as GraphKey
      */
-    void registerPcepTopology(KeyedInstanceIdentifier<Topology, TopologyKey> topology);
+    void registerPcepTopology(KeyedInstanceIdentifier<Topology, TopologyKey> topology, GraphKey graphKey);
 
     /**
      * Un Register current PCEP Topology into PCE Server.
