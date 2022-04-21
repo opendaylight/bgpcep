1020edc87ef73ef662e20ed064018a0dc9f7220d
Robert Varga <robert.varga@pantheon.tech>
2022-04-21 14:40:17 +0200

    Replace per-request j.u.Timer with a Netty Timer
    We are currently spawning a full java.util.Timer for every request we are
    sending out. That is rather wasteful in terms of resources: each such
    timer costs us a thread.
    
    Rather than doing that, use Netty's HashedWheelTimer, which can easily
    cope with a large number of timeouts.
    
    JIRA: BGPCEP-1006
    Change-Id: I4f3b6bf6a435170c8a054aa0e012418c60b17e96
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPRequest.java
+++ pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPRequest.java
@@ -10,7 +10,7 @@ package org.opendaylight.bgpcep.pcep.topology.provider;
 import com.google.common.base.Stopwatch;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
-import java.util.Timer;
+import io.netty.util.Timeout;
 import java.util.concurrent.TimeUnit;
 import org.checkerframework.checker.lock.qual.GuardedBy;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev200120.OperationResult;
@@ -19,9 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 final class PCEPRequest {
-
     private static final Logger LOG = LoggerFactory.getLogger(PCEPRequest.class);
-
     private static final long MINIMUM_ELAPSED_TIME = 1;
 
     enum State {
@@ -30,49 +28,49 @@ final class PCEPRequest {
         DONE,
     }
 
-    private final SettableFuture<OperationResult> future;
+    private final SettableFuture<OperationResult> future = SettableFuture.create();
     private final Metadata metadata;
-    private volatile State state;
+
     @GuardedBy("this")
-    private final Stopwatch stopwatch;
-    private final Timer timer;
+    private final Stopwatch stopwatch = Stopwatch.createStarted();
+    @GuardedBy("this")
+    private Timeout timeout;
 
-    PCEPRequest(final Metadata metadata) {
-        this.future = SettableFuture.create();
+    // FIXME; use atomic ops for this
+    private volatile State state = State.UNSENT;
+
+    PCEPRequest(final Metadata metadata, final Timeout timeout) {
         this.metadata = metadata;
-        this.state = State.UNSENT;
-        this.stopwatch = Stopwatch.createStarted();
-        this.timer = new Timer();
+        this.timeout = timeout;
     }
 
-    protected ListenableFuture<OperationResult> getFuture() {
-        return this.future;
+    ListenableFuture<OperationResult> getFuture() {
+        return future;
     }
 
-    public Metadata getMetadata() {
-        return this.metadata;
+    Metadata getMetadata() {
+        return metadata;
     }
 
-    public State getState() {
-        return this.state;
-    }
-
-    Timer getTimer() {
-        return this.timer;
+    State getState() {
+        return state;
     }
 
     synchronized void done(final OperationResult result) {
-        if (this.state != State.DONE) {
-            LOG.debug("Request went from {} to {}", this.state, State.DONE);
-            this.state = State.DONE;
-            this.timer.cancel();
-            this.future.set(result);
+        if (state != State.DONE) {
+            LOG.debug("Request went from {} to {}", state, State.DONE);
+            state = State.DONE;
+            if (timeout != null) {
+                timeout.cancel();
+                timeout = null;
+            }
+            future.set(result);
         }
     }
 
     synchronized void done() {
         OperationResult result;
-        switch (this.state) {
+        switch (state) {
             case UNSENT:
                 result = OperationResults.UNSENT;
                 break;
@@ -88,18 +86,17 @@ final class PCEPRequest {
     }
 
     synchronized void sent() {
-        if (this.state == State.UNSENT) {
-            LOG.debug("Request went from {} to {}", this.state, State.UNACKED);
-            this.state = State.UNACKED;
+        if (state == State.UNSENT) {
+            LOG.debug("Request went from {} to {}", state, State.UNACKED);
+            state = State.UNACKED;
         }
     }
 
     synchronized long getElapsedMillis() {
-        final long elapsedNanos = this.stopwatch.elapsed().toNanos();
+        final long elapsedNanos = stopwatch.elapsed().toNanos();
         final long elapsedMillis = TimeUnit.NANOSECONDS.toMillis(elapsedNanos);
-        if (elapsedMillis == 0 && elapsedNanos > 0) {
-            return MINIMUM_ELAPSED_TIME;
-        }
-        return elapsedMillis;
+
+        // FIXME: this is weird: it scales (0,1) up to 1, but otherwise scales down
+        return elapsedMillis == 0 && elapsedNanos > 0 ? MINIMUM_ELAPSED_TIME : elapsedMillis;
     }
 }
