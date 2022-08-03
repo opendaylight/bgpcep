bb82f5469250b80aedeafec905c76ca141296742
Robert Varga <robert.varga@pantheon.tech>
2022-08-03 16:06:00 +0200

    Rework statistics updates
    Rather than using an unrelated transaction chain from
    TopologyStatsProvider, call back into the session to perform the update
    on the corresponding transaction chain.
    
    This eliminates the problem of conflicting updates during session churn
    and allows for future evolution.
    
    JIRA: BGPCEP-1005
    Change-Id: I4d70b985d1089266c108798c34a0176826016200
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/SessionStateRegistry.java
+++ pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/SessionStateRegistry.java
@@ -9,24 +9,17 @@ package org.opendaylight.bgpcep.pcep.topology.provider;
 
 import edu.umd.cs.findbugs.annotations.CheckReturnValue;
 import org.eclipse.jdt.annotation.NonNull;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.stats.rev171113.PcepSessionState;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
 import org.opendaylight.yangtools.concepts.ObjectRegistration;
-import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
 
 /**
- * Topology Node Sessions stats handler. Will store Session stats on DS per each Topology Node registered.
+ * Topology Node Sessions stats handler. It is resposible for dispatching update requests to each registrant.
  */
 interface SessionStateRegistry {
     /**
      * Register session to Session stats Registry handler.
      *
-     * @param nodeId       Identifier of the topology node where it will be stored session stats under DS
-     * @param sessionState containing all Stats Session information
+     * @param updater A {@link SessionStateUpdater}
      */
-    // FIXME: BGPCEP-1105: nodeId is a bit superfluous, lifecycle is driven by AbstractTopologySessionListener
     @CheckReturnValue
-    <T extends PcepSessionState> @NonNull ObjectRegistration<T> bind(
-        @NonNull KeyedInstanceIdentifier<Node, NodeKey> nodeId, @NonNull T sessionState);
+    @NonNull ObjectRegistration<SessionStateUpdater> bind(@NonNull SessionStateUpdater updater);
 }
