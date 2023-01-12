b65dd4520b44391bcd3baa117ad357656183f056
Robert Varga <robert.varga@pantheon.tech>
2023-01-12 16:21:17 +0100

    Introduce pcep-session-error-policy
    max-unknown-messages is a common session parameter, tuning the policy
    when faced with errors. Capture this in a grouping, allowing for its
    reuse.
    
    JIRA: BGPCEP-962
    Change-Id: I8e7e582a11bff561b67108825d3376bb59e8c434
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/testtool/src/test/java/org/opendaylight/protocol/pcep/testtool/PCCMock.java
+++ pcep/testtool/src/test/java/org/opendaylight/protocol/pcep/testtool/PCCMock.java
@@ -19,9 +19,23 @@ import org.opendaylight.protocol.pcep.impl.DefaultPCEPSessionNegotiatorFactory;
 import org.opendaylight.protocol.pcep.pcc.mock.protocol.PCCDispatcherImpl;
 import org.opendaylight.protocol.pcep.spi.pojo.DefaultPCEPExtensionConsumerContext;
 import org.opendaylight.protocol.util.InetSocketAddressUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.config.rev230112.PcepSessionErrorPolicy;
+import org.opendaylight.yangtools.yang.common.Uint16;
 import org.opendaylight.yangtools.yang.common.Uint8;
 
 public final class PCCMock {
+    private static final PcepSessionErrorPolicy ERROR_POLICY = new PcepSessionErrorPolicy() {
+        @Override
+        public Uint16 getMaxUnknownMessages() {
+            return Uint16.ZERO;
+        }
+
+        @Override
+        public Class<? extends PcepSessionErrorPolicy> implementedInterface() {
+            throw new UnsupportedOperationException();
+        }
+    };
+
     private PCCMock() {
         // Hidden on purpose
     }
@@ -29,7 +43,7 @@ public final class PCCMock {
     public static void main(final String[] args) throws InterruptedException, ExecutionException {
         checkArgument(args.length > 0, "Host and port of server must be provided.");
         final var proposal = new BasePCEPSessionProposalFactory(Uint8.valueOf(120), Uint8.valueOf(30), List.of());
-        final var snf = new DefaultPCEPSessionNegotiatorFactory(proposal, 0);
+        final var snf = new DefaultPCEPSessionNegotiatorFactory(proposal, ERROR_POLICY);
         final var serverHostAndPort = HostAndPort.fromString(args[0]);
         final var serverAddr = new InetSocketAddress(serverHostAndPort.getHost(),
             serverHostAndPort.getPortOrDefault(12345));
