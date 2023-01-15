6459d6edfa7d0dd61f78bac4a38cca5800984c5e
Robert Varga <robert.varga@pantheon.tech>
2023-01-15 21:40:02 +0100

    Eliminate blueprint from ietf-p2mp-te-lsp
    Blueprint here is completely superfluous: services exposed by it are not
    used. Furthermore the tuneable here is bogus -- we either announce it or
    we do not. Remove this complexity.
    
    JIRA: BGPCEP-968
    Change-Id: I0f7d857a1c1563eee6f13e47b8caa6183093888a
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/ietf-p2mp-te-lsp/src/main/java/org/opendaylight/protocol/pcep/p2mp/te/lsp/P2MPTeLspCapability.java
+++ pcep/ietf-p2mp-te-lsp/src/main/java/org/opendaylight/protocol/pcep/p2mp/te/lsp/P2MPTeLspCapability.java
@@ -11,33 +11,17 @@ import com.google.common.base.MoreObjects;
 import java.net.InetSocketAddress;
 import org.kohsuke.MetaInfServices;
 import org.opendaylight.protocol.pcep.PCEPCapability;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.p2mp.te.lsp.rev181109.TlvsP2mpCapabilityAug;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.p2mp.te.lsp.rev181109.TlvsP2mpCapabilityAugBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.p2mp.te.lsp.rev181109.p2mp.pce.capability.tlv.P2mpPceCapabilityBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.open.object.open.TlvsBuilder;
 
 @MetaInfServices
 public final class P2MPTeLspCapability implements PCEPCapability {
-    private static final TlvsP2mpCapabilityAug PATH_COMPUTATION_CAP_AUG = new TlvsP2mpCapabilityAugBuilder()
-        .setP2mpPceCapability(new P2mpPceCapabilityBuilder().build())
-        .build();
-
-    private final boolean supportsPathComputation;
-
-    public P2MPTeLspCapability() {
-        this(true);
-    }
-
-    // FIXME: this should return distinct implementations
-    public P2MPTeLspCapability(final boolean supportsP2MPTeLspPathComputation) {
-        this.supportsPathComputation = supportsP2MPTeLspPathComputation;
-    }
-
     @Override
     public void setCapabilityProposal(final InetSocketAddress address, final TlvsBuilder builder) {
-        if (supportsPathComputation) {
-            builder.addAugmentation(PATH_COMPUTATION_CAP_AUG);
-        }
+        builder.addAugmentation(new TlvsP2mpCapabilityAugBuilder()
+            .setP2mpPceCapability(new P2mpPceCapabilityBuilder().build())
+            .build());
     }
 
     @Override
@@ -47,6 +31,6 @@ public final class P2MPTeLspCapability implements PCEPCapability {
 
     @Override
     public String toString() {
-        return MoreObjects.toStringHelper(this).add("pathComputation", supportsPathComputation).toString();
+        return MoreObjects.toStringHelper(this).toString();
     }
 }
