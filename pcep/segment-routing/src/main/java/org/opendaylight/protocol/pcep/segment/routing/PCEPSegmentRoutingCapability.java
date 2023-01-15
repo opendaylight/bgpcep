16d8062e52852f7fe0825b2e29a18197c64358ba
Robert Varga <robert.varga@pantheon.tech>
2023-01-15 22:22:36 +0100

    Remove sr-capable leaf
    The bit with advertizement is not used anywhere, as we always end up
    loading the default configuration. Remove the unused leaf.
    
    JIRA: BGPCEP-967
    Change-Id: I03b9da1af231d495dc359f309984079f88b6a0e6
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/PCEPSegmentRoutingCapability.java
+++ pcep/segment-routing/src/main/java/org/opendaylight/protocol/pcep/segment/routing/PCEPSegmentRoutingCapability.java
@@ -9,10 +9,8 @@ package org.opendaylight.protocol.pcep.segment.routing;
 
 import com.google.common.base.MoreObjects;
 import java.net.InetSocketAddress;
-import org.eclipse.jdt.annotation.NonNull;
 import org.kohsuke.MetaInfServices;
 import org.opendaylight.protocol.pcep.PCEPCapability;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev200720.Tlvs1;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev200720.Tlvs1Builder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev200720.sr.pce.capability.tlv.SrPceCapabilityBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.open.object.open.TlvsBuilder;
@@ -20,33 +18,15 @@ import org.opendaylight.yangtools.yang.common.Uint8;
 
 @MetaInfServices
 public class PCEPSegmentRoutingCapability implements PCEPCapability {
-    private static final @NonNull Tlvs1 AUGMENTATION = new Tlvs1Builder()
-        .setSrPceCapability(new SrPceCapabilityBuilder()
-            .setNFlag(Boolean.FALSE)
-            .setXFlag(Boolean.FALSE)
-            .setMsd(Uint8.ZERO)
-            .build())
-        .build();
-
-    private final boolean isSegmentRoutingCapable;
-
-    public PCEPSegmentRoutingCapability() {
-        this(true);
-    }
-
-    public PCEPSegmentRoutingCapability(final boolean isSegmentRoutingCapable) {
-        this.isSegmentRoutingCapable = isSegmentRoutingCapable;
-    }
-
     @Override
     public void setCapabilityProposal(final InetSocketAddress address, final TlvsBuilder builder) {
-        if (isSegmentRoutingCapable) {
-            builder.addAugmentation(AUGMENTATION);
-        }
-    }
-
-    public boolean isSegmentRoutingCapable() {
-        return isSegmentRoutingCapable;
+        builder.addAugmentation(new Tlvs1Builder()
+            .setSrPceCapability(new SrPceCapabilityBuilder()
+                .setNFlag(Boolean.FALSE)
+                .setXFlag(Boolean.FALSE)
+                .setMsd(Uint8.ZERO)
+                .build())
+            .build());
     }
 
     @Override
@@ -56,6 +36,6 @@ public class PCEPSegmentRoutingCapability implements PCEPCapability {
 
     @Override
     public String toString() {
-        return MoreObjects.toStringHelper(this).add("srCapable", isSegmentRoutingCapable).toString();
+        return MoreObjects.toStringHelper(this).toString();
     }
 }
