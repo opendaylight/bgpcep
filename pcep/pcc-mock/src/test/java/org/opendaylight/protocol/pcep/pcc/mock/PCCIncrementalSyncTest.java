48bdef9516c96fa65ce60d87a28012428f64c270
Robert Varga <robert.varga@pantheon.tech>
2023-01-16 13:32:11 +0100

    Remove PCEPCapability.isStateful()
    This is a useless method leaking a particular capability. Remove it and
    adjust users.
    
    JIRA: BGPCEP-960
    Change-Id: Iedf491ef8f276ea27acca9ac40892200c857dc23
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/pcc-mock/src/test/java/org/opendaylight/protocol/pcep/pcc/mock/PCCIncrementalSyncTest.java
+++ pcep/pcc-mock/src/test/java/org/opendaylight/protocol/pcep/pcc/mock/PCCIncrementalSyncTest.java
@@ -8,8 +8,6 @@
 package org.opendaylight.protocol.pcep.pcc.mock;
 
 import io.netty.channel.Channel;
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.junit.Test;
@@ -20,24 +18,22 @@ import org.opendaylight.protocol.util.InetSocketAddressUtil;
 import org.opendaylight.yangtools.yang.common.Uint64;
 
 public class PCCIncrementalSyncTest extends PCCMockCommon {
-
-    private final BigInteger lsp = BigInteger.valueOf(8);
     /**
      * Test Incremental Synchronization
      * Create 8 lsp, then it disconnects after 5 sec and then after 5 sec reconnects with Pcc DBVersion 10
      * After reconnection PCE has DBVersion 10, therefore there is 9 changes missed. 9 Pcrt + 1 Pcrt-Sync
      */
-    private final String[] mainInputIncrementalSync = new String[]{"--local-address", this.localAddress.getHostString(),
-        "--remote-address", InetSocketAddressUtil.toHostAndPort(this.remoteAddress).toString(), "--pcc", "1", "--lsp",
-        this.lsp.toString(), "--log-level", "DEBUG", "-ka", "30", "-d", "120", "--reconnect", "-1",
+    private final String[] mainInputIncrementalSync = new String[]{"--local-address", localAddress.getHostString(),
+        "--remote-address", InetSocketAddressUtil.toHostAndPort(remoteAddress).toString(), "--pcc", "1", "--lsp",
+        "8", "--log-level", "DEBUG", "-ka", "30", "-d", "120", "--reconnect", "-1",
         "--redelegation-timeout", "0", "--state-timeout", "-1", "--incremental-sync-procedure", "10", "5", "5"};
 
     @Test
     public void testSessionIncrementalSyncEstablishment() throws Exception {
         final TestingSessionListenerFactory factory = new TestingSessionListenerFactory();
         final Uint64 numberOflspAndDBv = Uint64.valueOf(8);
-        final Channel channel = createServer(factory, this.remoteAddress, new PCCServerPeerProposal(numberOflspAndDBv));
-        Main.main(this.mainInputIncrementalSync);
+        final Channel channel = createServer(factory, remoteAddress, new PCCServerPeerProposal(numberOflspAndDBv));
+        Main.main(mainInputIncrementalSync);
         final TestingSessionListener pceSessionListener = getListener(factory);
         checkSynchronizedSession(8, pceSessionListener, numberOflspAndDBv);
         Thread.sleep(6000);
@@ -51,8 +47,6 @@ public class PCCIncrementalSyncTest extends PCCMockCommon {
 
     @Override
     protected List<PCEPCapability> getCapabilities() {
-        final List<PCEPCapability> caps = new ArrayList<>();
-        caps.add(new PCEPStatefulCapability(true, true, true, false, false, true, true));
-        return caps;
+        return List.of(new PCEPStatefulCapability(true, true, false, false, true, true));
     }
 }
