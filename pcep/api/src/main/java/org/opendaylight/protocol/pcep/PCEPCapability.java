640f9ac8dc39402628f4ace86a97e7b8a87d6c87
Robert Varga <robert.varga@pantheon.tech>
2023-01-16 14:08:42 +0100

    Turn PCEPCapability into an abstract class
    We want to force uniform treatment, equality and useful toString(). All
    that requires an explicit abstract superclass.
    
    JIRA: BGPCEP-960
    Change-Id: I4522732290429c298bc7d2109ce65e272e909d86
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/api/src/main/java/org/opendaylight/protocol/pcep/PCEPCapability.java
+++ pcep/api/src/main/java/org/opendaylight/protocol/pcep/PCEPCapability.java
@@ -7,18 +7,38 @@
  */
 package org.opendaylight.protocol.pcep;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.MoreObjects.ToStringHelper;
 import java.net.InetSocketAddress;
+import org.eclipse.jdt.annotation.NonNull;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.open.object.open.TlvsBuilder;
+import org.opendaylight.yangtools.concepts.Immutable;
 
 /**
- * Stores usability for available capabilities.
+ * Stores usability for available capabilities. All implementations are required to be effectively immutable.
  */
-public interface PCEPCapability {
+public abstract class PCEPCapability implements Immutable {
     /**
      * Sets stateful capabilities tlv in incoming builder.
      *
      * @param address peer address to assign capability proposal
      * @param builder for TLVs included in PCEPOpenObject
      */
-    void setCapabilityProposal(InetSocketAddress address, TlvsBuilder builder);
+    public abstract void setCapabilityProposal(InetSocketAddress address, TlvsBuilder builder);
+
+    @Override
+    public abstract int hashCode();
+
+    @Override
+    public abstract boolean equals(Object obj);
+
+    @Override
+    public final String toString() {
+        return addToStringAttributes(MoreObjects.toStringHelper(this).omitNullValues()).toString();
+    }
+
+    protected @NonNull ToStringHelper addToStringAttributes(final @NonNull ToStringHelper helper) {
+        // No-op by default
+        return helper;
+    }
 }
