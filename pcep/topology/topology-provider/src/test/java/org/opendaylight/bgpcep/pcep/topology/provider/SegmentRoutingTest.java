f58fdef941d00ade5e67db6f7414c98e5b7a4955
Robert Varga <robert.varga@pantheon.tech>
2023-01-16 18:03:02 +0100

    Invert pcep-{segment-routing,topology-provider} dependencies
    pcep-topology-provider needs to depend on pcep-segment-routing,
    but a test is wrecking that. Rehost the test and flip the dependencies.
    
    JIRA: BGPCEP-960
    Change-Id: I763b9244f4ecb2ac288dc7b8cb93c2708e1d3b3d
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/topology/topology-provider/src/test/java/org/opendaylight/bgpcep/pcep/topology/provider/SegmentRoutingTest.java
+++ pcep/topology/topology-provider/src/test/java/org/opendaylight/bgpcep/pcep/topology/provider/SegmentRoutingTest.java
@@ -5,21 +5,18 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.protocol.pcep.segment.routing;
+package org.opendaylight.bgpcep.pcep.topology.provider;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.opendaylight.protocol.util.CheckTestUtil.readDataOperational;
 
-import com.google.common.collect.Lists;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import org.junit.Before;
 import org.junit.Test;
-import org.opendaylight.bgpcep.pcep.topology.provider.AbstractPCEPSessionTest;
-import org.opendaylight.bgpcep.pcep.topology.provider.AbstractTopologySessionListener;
 import org.opendaylight.protocol.pcep.PCEPSession;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddressNoZone;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.Ipv4AddressNoZone;
@@ -55,8 +52,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology
 import org.opendaylight.yangtools.yang.common.Uint32;
 import org.opendaylight.yangtools.yang.common.Uint8;
 
-public class TopologyProviderTest extends AbstractPCEPSessionTest {
-
+public class SegmentRoutingTest extends AbstractPCEPSessionTest {
     private AbstractTopologySessionListener listener;
     private PCEPSession session;
 
@@ -131,33 +127,48 @@ public class TopologyProviderTest extends AbstractPCEPSessionTest {
         if (hasLspIdTlv) {
             lspTlvBuilder.setLspIdentifiers(new LspIdentifiersBuilder().setLspId(new LspId(plspId)).build());
         }
-        return new PcrptBuilder().setPcrptMessage(new PcrptMessageBuilder().setReports(Lists.newArrayList(
-            new ReportsBuilder().setLsp(new LspBuilder().setPlspId(new PlspId(plspId)).setRemove(false).setSync(true)
-                .setAdministrative(true).setDelegate(true)
-                .setTlvs(lspTlvBuilder.setSymbolicPathName(new SymbolicPathNameBuilder()
-                    .setPathName(new SymbolicPathName(pathName.getBytes(StandardCharsets.UTF_8))).build()).build())
+        return new PcrptBuilder()
+            .setPcrptMessage(new PcrptMessageBuilder()
+                .setReports(List.of(new ReportsBuilder()
+                    .setLsp(new LspBuilder()
+                        .setPlspId(new PlspId(plspId))
+                        .setRemove(false)
+                        .setSync(true)
+                        .setAdministrative(true)
+                        .setDelegate(true)
+                        .setTlvs(lspTlvBuilder
+                            .setSymbolicPathName(new SymbolicPathNameBuilder()
+                                .setPathName(new SymbolicPathName(pathName.getBytes(StandardCharsets.UTF_8)))
+                                .build())
+                            .build())
+                        .build())
+                    .setSrp(new SrpBuilder()
+                        .setOperationId(new SrpIdNumber(Uint32.ZERO))
+                        .setTlvs(new org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful
+                            .rev200720.srp.object.srp.TlvsBuilder()
+                            .setPathSetupType(new PathSetupTypeBuilder().setPst(Uint8.ONE).build())
+                            .build())
+                        .build())
+                    .setPath(new PathBuilder().setEro(createSrEroObject(nai)).build())
+                    .build()))
                 .build())
-            .setSrp(new SrpBuilder().setOperationId(new SrpIdNumber(Uint32.ZERO)).setTlvs(
-                    new org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev200720
-                    .srp.object.srp.TlvsBuilder()
-                        .setPathSetupType(new PathSetupTypeBuilder().setPst(Uint8.ONE).build()).build()).build())
-            .setPath(new PathBuilder().setEro(createSrEroObject(nai)).build())
-            .build())).build()).build();
+            .build();
     }
 
     private static Ero createSrEroObject(final String nai) {
-        final SrEroTypeBuilder srEroBuilder = new SrEroTypeBuilder()
-                .setCFlag(false)
-                .setMFlag(false)
-                .setNaiType(NaiType.Ipv4NodeId)
-                .setSid(Uint32.valueOf(123456))
-                .setNai(new IpNodeIdBuilder().setIpAddress(new IpAddressNoZone(new Ipv4AddressNoZone(nai)))
-                    .build());
-        final SubobjectBuilder subobjBuilder = new SubobjectBuilder().setSubobjectType(srEroBuilder.build())
-                .setLoose(false);
-
-        final List<Subobject> subobjects = Lists.newArrayList(subobjBuilder.build());
-        return new EroBuilder().setProcessingRule(false).setIgnore(false).setSubobject(subobjects).build();
+        return new EroBuilder()
+            .setProcessingRule(false)
+            .setIgnore(false)
+            .setSubobject(List.of(new SubobjectBuilder()
+                .setSubobjectType(new SrEroTypeBuilder()
+                    .setCFlag(false)
+                    .setMFlag(false)
+                    .setNaiType(NaiType.Ipv4NodeId)
+                    .setSid(Uint32.valueOf(123456))
+                    .setNai(new IpNodeIdBuilder().setIpAddress(new IpAddressNoZone(new Ipv4AddressNoZone(nai))).build())
+                    .build())
+                .setLoose(false)
+                .build()))
+            .build();
     }
-
 }
