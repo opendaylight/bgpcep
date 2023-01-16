79bc77a1eeb31cb98ec795852b0432849f694a2e
Robert Varga <robert.varga@pantheon.tech>
2023-01-16 21:59:49 +0100

    Convert pcep-auto-bandwidth-extension to OSGi DS
    This is an experimenter option, this patch brings persistence out of the
    picture and shifts configuration to OSGi DS (or whatever else external).
    
    JIRA: BGPCEP-1018
    Change-Id: I122d5a9c41c78b5ac8523bc4a1d5ed54d7b166b3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/auto-bandwidth-extension/src/main/java/org/opendaylight/protocol/pcep/auto/bandwidth/extension/Activator.java
+++ pcep/auto-bandwidth-extension/src/main/java/org/opendaylight/protocol/pcep/auto/bandwidth/extension/Activator.java
@@ -7,32 +7,66 @@
  */
 package org.opendaylight.protocol.pcep.auto.bandwidth.extension;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import com.google.common.base.MoreObjects;
 import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import org.kohsuke.MetaInfServices;
 import org.opendaylight.protocol.pcep.spi.PCEPExtensionProviderActivator;
 import org.opendaylight.protocol.pcep.spi.PCEPExtensionProviderContext;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.pcep.auto.bandwidth.rev181109.bandwidth.usage.object.BandwidthUsage;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.ietf.stateful.rev200720.Pcrpt;
 import org.opendaylight.yangtools.concepts.Registration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
+@Singleton
 @MetaInfServices
+@Component(immediate = true)
 public class Activator implements PCEPExtensionProviderActivator {
-    private final int bandwidthUsageObjectType;
-
-    public Activator() {
-        this(5);
+    /**
+     * This implementation is bound to
+     * <a href="https://datatracker.ietf.org/doc/html/draft-dhody-pce-stateful-pce-auto-bandwidth-06#section-8.4">
+     * draft-dhody-pce-stateful-pce-auto-bandwidth-06</a>. Since there may be varying success with experimenting with
+     * compatibility, the type we bind to is configurable via {@link #bandwidthObjectClass()}.
+     */
+    @ObjectClassDefinition
+    public @interface Configuration {
+        @AttributeDefinition(min = "3", max = "15", description = "Object Class Value as per RFC5440")
+        byte bandwidthObjectClass() default DEFAULT_BANDWIDTH_OBJECT_TYPE;
     }
 
-    public Activator(final int bandwidthUsageObjectType) {
-        this.bandwidthUsageObjectType = bandwidthUsageObjectType;
+    /**
+     * {@code BANDWITH object} as per {@code draft-dhody-pce-stateful-pce-auto-bandwidth-06 section 8.4}.
+     */
+    private static final byte DEFAULT_BANDWIDTH_OBJECT_TYPE = 5;
+
+    private final int bandwidthObjectClass;
+
+    @Inject
+    public Activator() {
+        this(DEFAULT_BANDWIDTH_OBJECT_TYPE);
+    }
+
+    @Activate
+    public Activator(final Configuration config) {
+        this(config.bandwidthObjectClass());
+    }
+
+    public Activator(final byte bandwidthObjectType) {
+        checkArgument(bandwidthObjectType >= 3 && bandwidthObjectType <= 15,
+            "Object type %s is not in range [[3..15]]", bandwidthObjectType);
+        bandwidthObjectClass = bandwidthObjectType;
     }
 
     @Override
     public List<Registration> start(final PCEPExtensionProviderContext context) {
-        final BandwidthUsageObjectCodec bandwidthUsageObjectCodec =
-                new BandwidthUsageObjectCodec(bandwidthUsageObjectType);
-        final PcRptMessageCodec pcRptMessageCodec = new PcRptMessageCodec(context.getObjectHandlerRegistry());
+        final var bandwidthUsageObjectCodec = new BandwidthUsageObjectCodec(bandwidthObjectClass);
+        final var pcRptMessageCodec = new PcRptMessageCodec(context.getObjectHandlerRegistry());
 
         return List.of(
             context.registerObjectParser(bandwidthUsageObjectCodec),
@@ -43,6 +77,6 @@ public class Activator implements PCEPExtensionProviderActivator {
 
     @Override
     public String toString() {
-        return MoreObjects.toStringHelper(this).add("bandwithUsage", bandwidthUsageObjectType).toString();
+        return MoreObjects.toStringHelper(this).add("bandwithUsage", bandwidthObjectClass).toString();
     }
 }
