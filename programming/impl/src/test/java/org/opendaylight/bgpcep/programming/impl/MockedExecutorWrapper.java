1affcebafa8741097d4023b8bbd3c4a3a5221c08
Robert Varga <robert.varga@pantheon.tech>
2023-07-04 21:08:15 +0200

    Ditch ListeningExecutorService
    We have Futures.submit() at our disposal, hence we can use plain
    Executor/ExecutorService to manage our tasks.
    
    Change-Id: I18c26b52603dc6cca11b8feb42f8ac0c6f62bf14
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- programming/impl/src/test/java/org/opendaylight/bgpcep/programming/impl/MockedExecutorWrapper.java
+++ programming/impl/src/test/java/org/opendaylight/bgpcep/programming/impl/MockedExecutorWrapper.java
@@ -8,49 +8,26 @@
 package org.opendaylight.bgpcep.programming.impl;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.ListeningExecutorService;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import org.mockito.stubbing.Answer;
+import java.util.concurrent.Executor;
 
 final class MockedExecutorWrapper {
-
-    private final List<Object> submittedTasksToExecutor;
+    private final List<Runnable> submittedTasksToExecutor;
 
     MockedExecutorWrapper() {
-        this.submittedTasksToExecutor = new ArrayList<>();
+        submittedTasksToExecutor = new ArrayList<>();
     }
 
-    ListeningExecutorService getMockedExecutor() {
-        final ListeningExecutorService mockedExecutor = mock(ListeningExecutorService.class);
-        final Answer<ListenableFuture<?>> submitAnswer = invocation -> {
-            final Object task = invocation.getArguments()[0];
-            this.submittedTasksToExecutor.add(task);
-
-            Object result = null;
-            if (task instanceof Runnable) {
-                ((Runnable) task).run();
-            } else if (task instanceof Callable) {
-                result = ((Callable<?>) task).call();
-            }
-
-            final ListenableFuture<?> mockedFuture = mock(ListenableFuture.class);
-            doReturn(result).when(mockedFuture).get();
-            return mockedFuture;
+    Executor getMockedExecutor() {
+        return runnable -> {
+            submittedTasksToExecutor.add(runnable);
+            runnable.run();
         };
-        doAnswer(submitAnswer).when(mockedExecutor).submit(any(Runnable.class));
-        doAnswer(submitAnswer).when(mockedExecutor).submit(any(Callable.class));
-        return mockedExecutor;
     }
 
     void assertSubmittedTasksSize(final int taskCount) {
-        assertEquals(taskCount, this.submittedTasksToExecutor.size());
+        assertEquals(taskCount, submittedTasksToExecutor.size());
     }
 }
