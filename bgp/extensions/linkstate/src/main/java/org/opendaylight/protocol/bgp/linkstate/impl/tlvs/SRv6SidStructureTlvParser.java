836aa1b2248b5df0c30362bdd60b99f19fd704c9
Olivier Dugeon <olivier.dugeon@orange.com>
2024-10-24 10:57:23 +0200

    Update BGP-LS with latest RFCs
    Many new RFCs have been published since the latest modifications to
    BGP-LS. This patch propose an update of modified features and
    implementation of new TLVs as per the following RFCs:
    
    - RFC9552 which replace original RFC7752
    - RFC9085 - Segment Routing
    - RFC9514 - SRv6
    - RFC9351 - Flex-Algo
    - RFC9104 - Extended Admin Group
    - RFC8814 - Node & Link MSD
    - RFC9086 - Peer Node
    - RFC9294 - ASLA
    
    bgp-linkstate and bgp-segment-routing yang models have been first
    updated. Implementation of parsers and serialisers have been update
    or created accordingly.
    
    Note that the TLV 1160 has been used for the Binding SID in current
    implementation while TLV 1160 code point is now allocated for IS-IS
    flood redirection as per IANA allocation. In fact, the TLV 1160 -
    Binding SID corresponds to the individual draft prior to RFC9085
    (see rfc9085 historical https://datatracker.ietf.org/doc/rfc9085/)
    and has been remove in version 2 of the WG draft. In fact, what was
    named Binding SID in the individual draft has been renamed to "Range
    Prefix" in the WG draft and then RFC9085. The Binding-SID Prefix has
    been only kept in the IS-IS extension for Segment Routing i.e.
    RFC8667. Range is also the name used in OSPFv2 and OSPFv3 extension
    for Segment Routing a.k.a. RFC8665 and RFC8666.
    
    The newly Binding SID name coming from RFC9256 is now managed by the
    draft draft-ietf-idr- sr-policy which will be implemented latter.
    
    All old TE-LSP part has also been removed has they will be replaced
    by the new draft draft-ietf-idr-bgp-ls-te-path and implemented latter.
    
    A list of supported TLVs has been added to the documentation.
    
    JIRA: BGPCEP-1033
    Change-Id: I6ad6a0c1c3add5b00d6edd2ee438e58e9653b85b
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ bgp/extensions/linkstate/src/main/java/org/opendaylight/protocol/bgp/linkstate/impl/tlvs/SRv6SidStructureTlvParser.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 Orange. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.bgp.linkstate.impl.tlvs;
+
+import static org.opendaylight.yangtools.yang.common.netty.ByteBufUtils.readUint8;
+import static org.opendaylight.yangtools.yang.common.netty.ByteBufUtils.writeUint8;
+
+import io.netty.buffer.ByteBuf;
+import org.opendaylight.protocol.bgp.linkstate.spi.LinkstateTlvParser;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev200120.srv6.sid.subtlvs.Srv6SidStructure;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev200120.srv6.sid.subtlvs.Srv6SidStructureBuilder;
+import org.opendaylight.yang.svc.v1.urn.opendaylight.params.xml.ns.yang.bgp.segment.routing.rev200120.YangModuleInfoImpl;
+import org.opendaylight.yangtools.yang.common.QName;
+
+public class SRv6SidStructureTlvParser implements LinkstateTlvParser<Srv6SidStructure>,
+    LinkstateTlvParser.LinkstateTlvSerializer<Srv6SidStructure> {
+
+    private static final int SRV6_SID_STRUCTURE = 1252;
+
+    public static final QName SRV6_SID_STRUCTURE_QNAME = YangModuleInfoImpl.qnameOf("srv6-sid-structure");
+
+    @Override
+    public void serializeTlvBody(final Srv6SidStructure tlv, final ByteBuf body) {
+        writeUint8(body, tlv.getLocatorBlockLength());
+        writeUint8(body, tlv.getLocatorNodeLength());
+        writeUint8(body, tlv.getFunctionLength());
+        writeUint8(body, tlv.getArgumentLength());
+    }
+
+    @Override
+    public Srv6SidStructure parseTlvBody(final ByteBuf value) {
+        final Srv6SidStructureBuilder builder = new Srv6SidStructureBuilder();
+        builder.setLocatorBlockLength(readUint8(value));
+        builder.setLocatorNodeLength(readUint8(value));
+        builder.setFunctionLength(readUint8(value));
+        builder.setArgumentLength(readUint8(value));
+        return builder.build();
+    }
+
+    @Override
+    public int getType() {
+        return SRV6_SID_STRUCTURE;
+    }
+
+    @Override
+    public QName getTlvQName() {
+        return SRV6_SID_STRUCTURE_QNAME;
+    }
+}
