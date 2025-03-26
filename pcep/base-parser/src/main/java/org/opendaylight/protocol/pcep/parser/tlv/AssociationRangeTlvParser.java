63a714c53cd2ff5f72d170dc51ce0baeee69bde1
Olivier Dugeon <olivier.dugeon@orange.com>
2025-03-26 19:01:27 +0100

    PCEP: Add support to Association Group
    This patch adds support to Association Group as follow:
     - RFC8697: Base Association Group Object and TLVs definition
     - RFC8745: Path Protection Association
     - RFC8800: Disjoint Association
     - RFC9005: Policy Association
     - RFC9059: Bidirectional LSP Association
    
    This includes modification of pcep-type yang model and new parser
    and serializer for this new Association Group Object and TLVs.
    
    JIRA: BGPCEP-1034
    Change-Id: Ie071ae7934e1dee0c0256f446d1ec9ce45cca798
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>

---
+++ pcep/base-parser/src/main/java/org/opendaylight/protocol/pcep/parser/tlv/AssociationRangeTlvParser.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2025 Orange.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.protocol.pcep.parser.tlv;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import java.util.ArrayList;
+import org.opendaylight.protocol.pcep.PCEPDeserializerException;
+import org.opendaylight.protocol.pcep.spi.TlvParser;
+import org.opendaylight.protocol.pcep.spi.TlvSerializer;
+import org.opendaylight.protocol.pcep.spi.TlvUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.AssociationType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.Tlv;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.association.range.tlv.AssociationRange;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.association.range.tlv.AssociationRangeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.association.range.tlv.association.range.AssociationRanges;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.types.rev181109.association.range.tlv.association.range.AssociationRangesBuilder;
+import org.opendaylight.yangtools.yang.common.netty.ByteBufUtils;
+
+/**
+ * Parser for {@link AssociationRange}.
+ */
+public class AssociationRangeTlvParser implements TlvParser, TlvSerializer {
+    public static final int TYPE = 29;
+
+    private static final int OF_CODE_ELEMENT_LENGTH = 8;
+    private static final int RESERVED = 2;
+
+    @Override
+    public AssociationRange parseTlv(final ByteBuf buffer) throws PCEPDeserializerException {
+        if (buffer == null) {
+            return null;
+        }
+        if (buffer.readableBytes() % OF_CODE_ELEMENT_LENGTH != 0) {
+            throw new PCEPDeserializerException("Wrong length of array of bytes. Passed: " + buffer.readableBytes()
+                + ".");
+        }
+        final var assocRangeList = new ArrayList<AssociationRanges>();
+        while (buffer.isReadable()) {
+            buffer.skipBytes(RESERVED);
+            final var type = AssociationType.forValue(buffer.readShort());
+            final var start = ByteBufUtils.readUint16(buffer);
+            final var range = ByteBufUtils.readUint16(buffer);
+            if (type != null
+                && (start.intValue() > 0 && start.intValue() < 0xffff)
+                && range.intValue() > 0
+                && (start.intValue() + range.intValue() < 0xfff)) {
+                assocRangeList.add(new AssociationRangesBuilder()
+                    .setAssociationType(type)
+                    .setAssociationIdStart(start)
+                    .setRange(range)
+                    .build());
+            } else {
+                // FIXME: Should Trigger a PCEP Error as per https://tools.ietf.org/html/rfc8697#section-5.1
+                throw new PCEPDeserializerException("Wrong Association Range type: " + type + " start: "
+                   + start + "range: " + range + ".");
+            }
+        }
+        return new AssociationRangeBuilder().setAssociationRanges(assocRangeList).build();
+    }
+
+    @Override
+    public void serializeTlv(final Tlv tlv, final ByteBuf buffer) {
+        checkArgument(tlv instanceof AssociationRange, "AssociationRange TLV is mandatory.");
+        final AssociationRange arl = (AssociationRange) tlv;
+        final ByteBuf body = Unpooled.buffer();
+        arl.getAssociationRanges().forEach(id -> {
+            body.writeZero(RESERVED);
+            body.writeShort(id.getAssociationType().getIntValue());
+            ByteBufUtils.write(body, id.getAssociationIdStart());
+            ByteBufUtils.write(body, id.getRange());
+        });
+        TlvUtil.formatTlv(TYPE, body, buffer);
+    }
+
+}
--- pcep/base-parser/src/main/java/org/opendaylight/protocol/pcep/parser/tlv/NoPathVectorTlvParser.java
+++ pcep/base-parser/src/main/java/org/opendaylight/protocol/pcep/parser/tlv/NoPathVectorTlvParser.java
@@ -30,6 +30,8 @@ public class NoPathVectorTlvParser implements TlvParser, TlvSerializer {
 
     private static final int FLAGS_SIZE = 32;
 
+    private static final int NO_DISJOINT_COMPUTATION = 10;
+    private static final int NO_DISJOINT_PATH = 11;
     private static final int REACHABLITY_PROBLEM = 24;
     private static final int NO_GCO_SOLUTION = 25;
     private static final int NO_GCO_MIGRATION_PATH = 26;
@@ -50,6 +52,7 @@ public class NoPathVectorTlvParser implements TlvParser, TlvSerializer {
         }
         final BitArray flags = BitArray.valueOf(buffer, FLAGS_SIZE);
         return new NoPathVectorBuilder().setFlags(new Flags(flags.get(CHAIN_UNAVAILABLE),
+                flags.get(NO_DISJOINT_COMPUTATION), flags.get(NO_DISJOINT_PATH),
                 flags.get(NO_GCO_MIGRATION_PATH), flags.get(NO_GCO_SOLUTION), flags.get(REACHABLITY_PROBLEM),
                 flags.get(PATH_KEY), flags.get(PCE_UNAVAILABLE), flags.get(UNKNOWN_DEST), flags.get(UNKNOWN_SRC)))
                 .build();
@@ -70,6 +73,8 @@ public class NoPathVectorTlvParser implements TlvParser, TlvSerializer {
         flags.set(UNKNOWN_SRC, f.getUnknownSource());
         flags.set(UNKNOWN_DEST, f.getUnknownDestination());
         flags.set(PCE_UNAVAILABLE, f.getPceUnavailable());
+        flags.set(NO_DISJOINT_COMPUTATION, f.getNoDisjointCompute());
+        flags.set(NO_DISJOINT_PATH, f.getNoDisjointPath());
         flags.toByteBuf(body);
         TlvUtil.formatTlv(TYPE, body, buffer);
     }
