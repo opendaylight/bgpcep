b11f63f99f4256898ed30c0efa2206366dfb12fc
Robert Varga <robert.varga@pantheon.tech>
2025-06-13 19:59:46 +0200

    Use virtual threads in DefaultInstructionSchedulerFactory
    There are two kinds of threads we are creating:
    - a thread to back HashedWheelTimer
    - a thread to back DefaultInstructionScheduler
    
    Both of these do not need a platform thread, a virtual thread will do
    just fine.
    
    JIRA: BGPCEP-1040
    Change-Id: I943754a26ded7857d034c53bb5b6c8aac8039942
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- programming/impl/src/main/java/org/opendaylight/bgpcep/programming/impl/DefaultInstructionSchedulerFactory.java
+++ programming/impl/src/main/java/org/opendaylight/bgpcep/programming/impl/DefaultInstructionSchedulerFactory.java
@@ -9,7 +9,6 @@ package org.opendaylight.bgpcep.programming.impl;
 
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.netty.util.HashedWheelTimer;
 import io.netty.util.Timer;
 import java.util.concurrent.ExecutorService;
@@ -18,6 +17,7 @@ import java.util.concurrent.ThreadFactory;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.bgpcep.programming.spi.InstructionScheduler;
 import org.opendaylight.bgpcep.programming.spi.InstructionSchedulerFactory;
 import org.opendaylight.mdsal.binding.api.DataBroker;
@@ -31,24 +31,23 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component(immediate = true, service = InstructionSchedulerFactory.class)
 @Singleton
+@Component(immediate = true, service = InstructionSchedulerFactory.class)
 public final class DefaultInstructionSchedulerFactory implements InstructionSchedulerFactory, AutoCloseable {
     private static final Logger LOG = LoggerFactory.getLogger(DefaultInstructionSchedulerFactory.class);
-    private static final ThreadFactory THREAD_FACTORY = new ThreadFactoryBuilder()
-        .setNameFormat("programming-timer-%d")
-        .setDaemon(true)
-        .build();
+    private static final ThreadFactory TIMER_TF = Thread.ofVirtual().name("odl-programming-timer-", 0).factory();
+    private static final ThreadFactory EXECUTOR_TF = Thread.ofVirtual().name("odl-programming-timer-", 0).factory();
 
-    private final ExecutorService exec = Executors.newSingleThreadExecutor();
-    private final Timer timer = new HashedWheelTimer(THREAD_FACTORY);
     private final DataBroker dataProvider;
     private final NotificationPublishService notifs;
     private final RpcProviderService rpcProviderRegistry;
     private final ClusterSingletonServiceProvider cssp;
+    private final ExecutorService executor;
+    private final Timer timer;
 
     @Inject
     @Activate
+    @NonNullByDefault
     public DefaultInstructionSchedulerFactory(@Reference final DataBroker dataProvider,
             @Reference final RpcProviderService rpcProviderRegistry,
             @Reference final NotificationPublishService notifs,
@@ -57,12 +56,15 @@ public final class DefaultInstructionSchedulerFactory implements InstructionSche
         this.notifs = requireNonNull(notifs);
         this.rpcProviderRegistry = requireNonNull(rpcProviderRegistry);
         this.cssp = requireNonNull(cssp);
+
+        executor = Executors.newSingleThreadExecutor(EXECUTOR_TF);
+        timer = new HashedWheelTimer(TIMER_TF);
     }
 
     @Override
     public InstructionScheduler createInstructionScheduler(final String instructionId) {
         LOG.info("Creating Instruction Scheduler {}.", instructionId);
-        return new DefaultInstructionScheduler(dataProvider, notifs, exec, rpcProviderRegistry, cssp, timer,
+        return new DefaultInstructionScheduler(dataProvider, notifs, executor, rpcProviderRegistry, cssp, timer,
             instructionId);
     }
 
@@ -70,8 +72,10 @@ public final class DefaultInstructionSchedulerFactory implements InstructionSche
     @PreDestroy
     @Override
     public void close() {
-        // FIXME: This can have weird effects: should we keep track of all schedulers and refcount?
-        exec.shutdown();
+        // FIXME: this can have weird effects:
+        //        - should we keep track of all schedulers and refcount?
+        //        - or just close() here?
+        executor.shutdown();
         timer.stop();
     }
 }
