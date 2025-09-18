ca7898fec2898112ba0daa78a543db91414ee40a
Martin Balaz <martin.balaz@pantheon.tech>
2025-09-18 13:41:10 +0200

    Port CSIT bgp_ingest_mixed test suite to pytest
    As part of moving from RobotFramework to pytest port CSIT integration
    tests from integration-tests project directly to bgpcep project.
    Continue migration with BGP ingest mixed test suite from robot tests.
    
    JIRA: INTTEST-161
    Change-Id: I7894ef198b8feb5b669541ea0158f434a59c353a
    Signed-off-by: Martin Balaz <martin.balaz@pantheon.tech>

--- tests/test_suites/base_test_singlepeer_prefix_count.py
+++ tests/test_suites/base_test_singlepeer_prefix_count.py
@@ -5,55 +5,44 @@
 # terms of the Eclipse Public License v1.0 which accompanies this distribution,
 # and is available at http://www.eclipse.org/legal/epl-v10.html
 #
-# BGP performance of ingesting from 1 iBGP peer, data change counter
-# is used. This suite uses play.py as single iBGP peer. The suite
-# only looks at example-ipv4-topology, so RIB is not examined.
-# This suite requires odl-bgpcep-data-change-counter to be installed
-# so make sure it is added to "install-features" of any jobs
-# that are going to invoke it.The suite consists of two halves,
-# differing on which side initiates BGP connection.
-# Data change counter is a lightweight way to detect
-# "work is being done". change_counter.py provide a nice function
-# to wait for stability, but it needs initial value, that is why
-# "TestSinglePeerChangeCount.last_change_count_single = change_counter.get_change_count()"
-# appears just before work-inducing action. The time for
-# test_wait_for_stable_* cases to finish is the main performance
-# metric.After waiting for stability is done, full check on
+# This suite uses play.py as single iBGP peer. The suite only looks at
+# example-ipv4-topology, so RIB is not examined.
+# The suite consists of two halves, differing on which side initiates BGP
+# connection. State of "work is being done" is detected by increasing value of
+# prefixes in topology. The time for Wait_For_Stable_* cases to finish is the
+# main performance metric. After waiting for stability is done, full check on
 # number of prefixes present is performed.
 # Brief description how to configure BGP peer can be found here:
 # https://wiki.opendaylight.org/view/BGP_LS_PCEP:User_Guide#BGP_Peer
 # http://docs.opendaylight.org/en/stable-boron/user-guide/bgp-user-guide.html#bgp-peering
-
+# TODO: Currently, if a bug causes prefix count to remain at zero, affected
+# test cases will wait for max time. Reconsider. If zero is allowed as stable,
+# higher period or repetitions would be required.
+# The prefix counting is quite heavyweight and may induce large variation in
+# time. Try the other version of the suite (test_singlepeer_change_count.py)
+# to get better precision.
 
 import logging
 import os
-import pytest
 
 from lib import bgp
-from lib import change_counter
 from lib import infra
 from lib import ip_topology
 from lib import utils
 
 
-PREFIXES_COUNT = 600_000
 TEST_DURATION_MULTIPLIER = int(os.environ["TEST_DURATION_MULTIPLIER"])
-BGP_FILLING_TIMEOUT = TEST_DURATION_MULTIPLIER * (PREFIXES_COUNT * 9.0 / 10000 + 20)
-BGP_EMPTYING_TIMEOUT = BGP_FILLING_TIMEOUT * 3 / 4
 
 log = logging.getLogger(__name__)
 
 
-@pytest.mark.usefixtures("preconditions")
-@pytest.mark.usefixtures("log_test_suite_start_end_to_karaf")
-@pytest.mark.usefixtures("log_test_case_start_end_to_karaf")
-@pytest.mark.usefixtures("teardown_kill_all_running_play_script_processes")
-@pytest.mark.run(order=3)
-class TestSinglePeerChangeCount:
+class BaseTestSinglePeerPrefixCount:
     bgp_speaker_process = None
-    last_change_count_single = 1
 
-    def test_single_peer_change_count(self, allure_step_with_separate_logging):
+    def test_single_peer_prefix_count(self, allure_step_with_separate_logging, prefixes_count, insert, withdraw, prefill):
+
+        bgp_filling_timeout = TEST_DURATION_MULTIPLIER * (prefixes_count * 9.0 / 10000 + 20)
+        bgp_emptying_timeout = bgp_filling_timeout * 3 / 4
 
         with allure_step_with_separate_logging(
             "step_check_for_empty_topology_before_talking"
@@ -73,42 +62,28 @@ class TestSinglePeerChangeCount:
             """Configure BGP peer module with initiate-connection set to false."""
             bgp.set_bgp_neighbour(ip="127.0.0.1", passive_mode=True)
 
-        with allure_step_with_separate_logging("step_recofigure_data_change_counter"):
-            """Configure data change counter to count transactions in
-            example-ipv4-topology instead of example-linkstate-topology."""
-            change_counter.set_change_counter(topology_name="example-ipv4-topology")
-
-        with allure_step_with_separate_logging("step_check_data_change_counter_ready"):
-            """Data change counter might have been slower to start than ipv4
-            topology, wait for it."""
-            utils.wait_until_function_pass(5, 1, change_counter.get_change_count)
-
         with allure_step_with_separate_logging("step_start_talking_bgp_speaker"):
             """Start Python speaker to connect to ODL."""
-            TestSinglePeerChangeCount.last_change_count_single = (
-                change_counter.get_change_count()
-            )
-            TestSinglePeerChangeCount.bgp_speaker_process = bgp.start_bgp_speaker(
-                ammount=PREFIXES_COUNT,
-                insert=1,
-                withdraw=0,
-                prefill=0,
+            self.bgp_speaker_process = bgp.start_bgp_speaker(
+                ammount=prefixes_count,
+                insert=insert,
+                withdraw=withdraw,
+                prefill=prefill,
                 update="single",
                 listen=False,
                 log_level="info",
             )
             assert infra.is_process_still_running(
-                TestSinglePeerChangeCount.bgp_speaker_process
+                self.bgp_speaker_process
             ), "Bgp speaker process is not running"
 
         with allure_step_with_separate_logging(
             "step_wait_for_stable_talking_ip_topology"
         ):
             """Wait until example-ipv4-topology becomes stable. This is done by
-            checking the change counter."""
-            change_counter.wait_for_change_count_to_become_stable(
-                minimum_value=self.last_change_count_single + 1,
-                timeout=BGP_FILLING_TIMEOUT,
+            checking stability of prefix count."""
+            ip_topology.wait_for_ipv4_topology_prefixes_to_become_stable(
+                excluded_value=0, timeout=bgp_filling_timeout
             )
 
         with allure_step_with_separate_logging("step_check_talking_ip_topology_count"):
@@ -116,26 +91,23 @@ class TestSinglePeerChangeCount:
             not correct."""
             topology_count = ip_topology.get_ipv4_topology_prefixes_count()
             assert (
-                topology_count == PREFIXES_COUNT
-            ), f"Ipv4 topology does not contain all {PREFIXES_COUNT} expected advertised prefixes, but only {topology_count}"
+                topology_count == prefixes_count
+            ), f"Ipv4 topology does not contain all {prefixes_count} expected advertised prefixes, but only {topology_count}"
 
         with allure_step_with_separate_logging("step_kill_talking_bgp_speaker"):
             """Abort the Python speaker."""
-            TestSinglePeerChangeCount.last_change_count_single = (
-                change_counter.get_change_count()
-            )
             bgp.stop_bgp_speaker(self.bgp_speaker_process)
 
         with allure_step_with_separate_logging(
             "step_store_result_for_talking_bgp_speaker"
         ):
-            """Store results for plotting."""
+            """Store results for debugging."""
             infra.shell(
-                "mv totals-bgp.csv results/changecount-talking-totals-bgp.csv",
+                "mv totals-bgp.csv results/prefixcount-talking-totals-bgp.csv",
                 check_rc=True,
             )
             infra.shell(
-                "mv performance-bgp.csv results/changecount-talking-performance-bgp.csv",
+                "mv performance-bgp.csv results/prefixcount-talking-performance-bgp.csv",
                 check_rc=True,
             )
 
@@ -143,15 +115,13 @@ class TestSinglePeerChangeCount:
             "step_wait_for_stable_ip_topology_after_talking"
         ):
             """Wait until example-ipv4-topology becomes stable again."""
-            change_counter.wait_for_change_count_to_become_stable(
-                minimum_value=self.last_change_count_single + 1,
-                timeout=BGP_EMPTYING_TIMEOUT,
+            ip_topology.wait_for_ipv4_topology_prefixes_to_become_stable(
+                excluded_value=prefixes_count, timeout=bgp_emptying_timeout
             )
 
         with allure_step_with_separate_logging(
             "step_check_for_empty_ip_topology_after_talking"
         ):
-            """Example-ipv4-topology should be empty now."""
             topology_count = ip_topology.get_ipv4_topology_prefixes_count()
             assert (
                 topology_count == 0
@@ -159,17 +129,17 @@ class TestSinglePeerChangeCount:
 
         with allure_step_with_separate_logging("step_start_listening_bgp_speaker"):
             """Start Python speaker in listening mode."""
-            TestSinglePeerChangeCount.bgp_speaker_process = bgp.start_bgp_speaker(
-                ammount=PREFIXES_COUNT,
-                insert=1,
-                withdraw=0,
-                prefill=0,
+            self.bgp_speaker_process = bgp.start_bgp_speaker(
+                ammount=prefixes_count,
+                insert=insert,
+                withdraw=withdraw,
+                prefill=prefill,
                 update="single",
                 listen=True,
                 log_level="info",
             )
             assert infra.is_process_still_running(
-                TestSinglePeerChangeCount.bgp_speaker_process
+                self.bgp_speaker_process
             ), "Bgp speaker process is not running"
 
         with allure_step_with_separate_logging(
@@ -177,45 +147,40 @@ class TestSinglePeerChangeCount:
         ):
             """Replace BGP peer config module, now with initiate-connection set to
             true."""
-            TestSinglePeerChangeCount.last_change_count_single = (
-                change_counter.get_change_count()
-            )
             bgp.set_bgp_neighbour(ip="127.0.0.1", passive_mode=False)
 
         with allure_step_with_separate_logging(
             "step_wait_for_stable_listening_ip_topology"
         ):
             """Wait until example-ipv4-topology becomes stable."""
-            change_counter.wait_for_change_count_to_become_stable(
-                minimum_value=self.last_change_count_single + 1,
-                timeout=BGP_FILLING_TIMEOUT,
+            ip_topology.wait_for_ipv4_topology_prefixes_to_become_stable(
+                excluded_value=0, timeout=bgp_filling_timeout
             )
 
-        with allure_step_with_separate_logging("step_listening_ip_topology_count"):
+        with allure_step_with_separate_logging(
+            "step_check_listening_ip_topology_count"
+        ):
             """Count the routes in example-ipv4-topology and fail if the count is
             not correct."""
             topology_count = ip_topology.get_ipv4_topology_prefixes_count()
             assert (
-                topology_count == PREFIXES_COUNT
-            ), f"Ipv4 topology does not contain all {PREFIXES_COUNT} expected advertised prefixes, but only {topology_count}"
+                topology_count == prefixes_count
+            ), f"Ipv4 topology does not contain all {prefixes_count} expected advertised prefixes, but only {topology_count}"
 
         with allure_step_with_separate_logging("step_kill_listening_bgp_speaker"):
-            """Abort the Python speaker. Also, attempt to stop failing fast."""
-            TestSinglePeerChangeCount.last_change_count_single = (
-                change_counter.get_change_count()
-            )
+            """Abort the Python speaker."""
             bgp.stop_bgp_speaker(self.bgp_speaker_process)
 
         with allure_step_with_separate_logging(
             "step_store_result_for_listening_bgp_speaker"
         ):
-            """Store results for plotting."""
+            """Store results for debugging."""
             infra.shell(
-                "mv totals-bgp.csv results/changecount-listening-totals-bgp.csv",
+                "mv totals-bgp.csv results/prefixcount-listening-totals-bgp.csv",
                 check_rc=True,
             )
             infra.shell(
-                "mv performance-bgp.csv results/changecount-listening-performance-bgp.csv",
+                "mv performance-bgp.csv results/prefixcount-listening-performance-bgp.csv",
                 check_rc=True,
             )
 
@@ -223,9 +188,8 @@ class TestSinglePeerChangeCount:
             "step_wait_for_stable_ip_topology_after_listening"
         ):
             """Wait until example-ipv4-topology becomes stable again."""
-            change_counter.wait_for_change_count_to_become_stable(
-                minimum_value=self.last_change_count_single + 1,
-                timeout=BGP_EMPTYING_TIMEOUT,
+            ip_topology.wait_for_ipv4_topology_prefixes_to_become_stable(
+                excluded_value=prefixes_count, timeout=bgp_emptying_timeout
             )
 
         with allure_step_with_separate_logging(
@@ -236,15 +200,6 @@ class TestSinglePeerChangeCount:
             assert (
                 topology_count == 0
             ), f"Ipv4 topology is not empty as expected, there are {topology_count} prefixes present."
-
-        with allure_step_with_separate_logging(
-            "step_restore_data_change_counter_configuration"
-        ):
-            """Configure data change counter back to count transactions affecting
-            example-linkstate-topology."""
-            change_counter.set_change_counter(
-                topology_name="example-linkstate-topology"
-            )
 
         with allure_step_with_separate_logging("step_delete_bgp_peer_configuartion"):
             """Revert the BGP configuration to the original state: without any
