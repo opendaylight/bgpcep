f78ff1c4e345ceb8df7b1fda86f59abccafab0fd
Martin Balaz <martin.balaz@pantheon.tech>
2025-09-23 09:26:04 +0200

    Port CSIT bgp_throughpcep test suite to pytest
    As part of moving from RobotFramework to pytest port CSIT integration
    tests from integration-tests project directly to bgpcep project.
    Continue migration with BGP througpcep test suite from robot tests.
    
    JIRA: INTTEST-162
    Change-Id: I5003236ff27648d0e943b2fbfc95307fa3048aed
    Signed-off-by: Martin Balaz <martin.balaz@pantheon.tech>

--- tests/libraries/variables.py
+++ tests/libraries/variables.py
@@ -6,7 +6,8 @@ class Variables(BaseSettings):
     BGP_TOOL_PORT: int = 17900
     ODL_BGP_PORT: int = 1790
     ODL_IP: str = "127.0.0.1"
-    TOOLS_IP: str = "127.0.0.2"
+    ODL_USER: str = "admin"
+    ODL_PASSWORD: str = "admin"
     RESTCONF_PORT: int = 8181
     RESTCONF_ROOT: str = "rests"
 
@@ -15,6 +16,9 @@ class Variables(BaseSettings):
     def REST_API(self) -> str:
         return f"{self.RESTCONF_ROOT}/data"
 
+    TOOLS_IP: str = "127.0.1.0"
+    TOOLS_USER: str = "admin"
+    TOOLS_PASSWORD: str = "admin"
     KARAF_LOG_LEVEL: str = "INFO"
     TEST_DURATION_MULTIPLIER: int = 1
 
