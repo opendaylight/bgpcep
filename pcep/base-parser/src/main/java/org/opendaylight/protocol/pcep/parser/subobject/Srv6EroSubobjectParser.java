7aa3e523e1c0b96c9b9326a4b35dafc2cb221c20
Olivier Dugeon <olivier.dugeon@orange.com>
2025-09-26 16:45:40 +0200

    PCEP: Migrate Segment Routing yang model
    This is the migration of Segment Routing yang model according to
    the new design of PCEP yang models. Definition of TLVs and Object
    for Segment Routing (RFC8664) and SRv6 (RFC9603) are now place
    respectively in odl-pcep-types and odl-pcep-object yang models.
    
    Segment Routing parsers have been also migrated to pcep-base-parser
    and tests to pcep-impl. Other part of codes have been updated
    accordingly.
    
    Change-Id: I02c48ad8493d609dae0ffc21629bb5b9cbb30195
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>

--- pcep/base-parser/src/main/java/org/opendaylight/protocol/pcep/parser/subobject/Srv6EroSubobjectParser.java
+++ pcep/base-parser/src/main/java/org/opendaylight/protocol/pcep/parser/subobject/Srv6EroSubobjectParser.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2025 Orange.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.protocol.pcep.segment.routing;
+package org.opendaylight.protocol.pcep.parser.subobject;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
@@ -14,37 +14,36 @@ import org.opendaylight.protocol.pcep.PCEPDeserializerException;
 import org.opendaylight.protocol.pcep.spi.EROSubobjectParser;
 import org.opendaylight.protocol.pcep.spi.EROSubobjectSerializer;
 import org.opendaylight.protocol.pcep.spi.EROSubobjectUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.object.rev250930.Srv6Subobject;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.object.rev250930.explicit.route.object.ero.Subobject;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.object.rev250930.explicit.route.object.ero.SubobjectBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev250402.SrSubobject;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.segment.routing.rev250402.add.lsp.input.arguments.ero.subobject.subobject.type.SrEroTypeBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.object.rev250930.explicit.route.object.ero.subobject.subobject.type.Srv6EroTypeBuilder;
 
-public class SrEroSubobjectParser extends AbstractSrSubobjectParser implements EROSubobjectParser,
+public class Srv6EroSubobjectParser extends AbstractSrv6SubobjectParser implements EROSubobjectParser,
         EROSubobjectSerializer {
 
-    public static final int TYPE = 36;
+    public static final int TYPE = 40;
 
-    SrEroSubobjectParser() {
+    public Srv6EroSubobjectParser() {
         // Hidden on purpose
     }
 
     @Override
     public void serializeSubobject(final Subobject subobject, final ByteBuf buffer) {
-        checkArgument(subobject.getSubobjectType() instanceof SrSubobject,
-                "Unknown subobject instance. Passed %s. Needed SrSubobject.", subobject.getSubobjectType()
+        checkArgument(subobject.getSubobjectType() instanceof Srv6Subobject,
+                "Unknown subobject instance. Passed %s. Needed Srv6Subobject.", subobject.getSubobjectType()
                         .getClass());
 
-        final SrSubobject srSubobject = (SrSubobject) subobject.getSubobjectType();
-        final ByteBuf body = serializeSubobject(srSubobject);
+        final Srv6Subobject srv6Subobject = (Srv6Subobject) subobject.getSubobjectType();
+        final ByteBuf body = serializeSubobject(srv6Subobject);
         EROSubobjectUtil.formatSubobject(TYPE, subobject.getLoose(), body, buffer);
     }
 
     @Override
     public Subobject parseSubobject(final ByteBuf buffer, final boolean loose) throws PCEPDeserializerException {
-        final SrEroTypeBuilder srEroSubobjectBuilder = new SrEroTypeBuilder(parseSrSubobject(buffer));
-        final SubobjectBuilder subobjectBuilder = new SubobjectBuilder();
-        subobjectBuilder.setLoose(loose);
-        subobjectBuilder.setSubobjectType(srEroSubobjectBuilder.build());
-        return subobjectBuilder.build();
+        return new SubobjectBuilder()
+            .setLoose(loose)
+            .setSubobjectType(new Srv6EroTypeBuilder(parseSrv6Subobject(buffer)).build())
+            .build();
     }
 }
