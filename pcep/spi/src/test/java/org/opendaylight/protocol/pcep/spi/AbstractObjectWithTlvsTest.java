13a50352039368872029a88d94b3b6a974eb1e8c
Robert Varga <robert.varga@pantheon.tech>
2025-10-09 01:09:26 +0200

    Migrate AbstractObjectWithTlvsTest to JUnit5
    This is mostly a straightforward migration, except we ditch the setup
    method as the two test cases are not really sharing anything.
    
    Change-Id: I4a459aa437e0ae8115c10d79fbfce6545ff425b2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- pcep/spi/src/test/java/org/opendaylight/protocol/pcep/spi/AbstractObjectWithTlvsTest.java
+++ pcep/spi/src/test/java/org/opendaylight/protocol/pcep/spi/AbstractObjectWithTlvsTest.java
@@ -7,25 +7,22 @@
  */
 package org.opendaylight.protocol.pcep.spi;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.verify;
 
-import com.google.common.collect.Lists;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.opendaylight.protocol.pcep.PCEPDeserializerException;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.iana.rev130816.EnterpriseNumber;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.object.rev250930.Object;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.object.rev250930.ObjectHeader;
@@ -39,24 +36,10 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.pcep.typ
 import org.opendaylight.yangtools.yang.common.Uint16;
 import org.opendaylight.yangtools.yang.common.Uint32;
 
-@RunWith(MockitoJUnitRunner.StrictStubs.class)
-public class AbstractObjectWithTlvsTest {
-
-    private static final EnterpriseNumber EN = new EnterpriseNumber(Uint32.ZERO);
-
-    private Tlv tlv;
-
-    private VendorInformationTlv viTlv;
-
-    @Mock
-    private TlvRegistry tlvRegistry;
-
-    @Mock
-    private VendorInformationTlvRegistry viTlvRegistry;
-
-    private static class Abs extends AbstractObjectWithTlvsParser<TlvsBuilder> {
-
-        protected Abs(final TlvRegistry tlvReg, final VendorInformationTlvRegistry viTlvReg) {
+@ExtendWith(MockitoExtension.class)
+class AbstractObjectWithTlvsTest {
+    private static final class Abs extends AbstractObjectWithTlvsParser<TlvsBuilder> {
+        Abs(final TlvRegistry tlvReg, final VendorInformationTlvRegistry viTlvReg) {
             super(tlvReg, viTlvReg, 0, 0);
         }
 
@@ -67,6 +50,7 @@ public class AbstractObjectWithTlvsTest {
 
         @Override
         public void serializeObject(final Object object, final ByteBuf buffer) {
+            // No-op
         }
 
         @Override
@@ -80,25 +64,25 @@ public class AbstractObjectWithTlvsTest {
         }
     }
 
-    @Before
-    public void setUp() throws PCEPDeserializerException {
-        tlv = new OfListBuilder().setCodes(Set.of(new OfId(Uint16.TEN))).build();
-        viTlv = new VendorInformationTlvBuilder().setEnterpriseNumber(EN).build();
-        doNothing().when(viTlvRegistry).serializeVendorInformationTlv(any(VendorInformationTlv.class),
-            any(ByteBuf.class));
-        doReturn(Optional.of(viTlv)).when(viTlvRegistry).parseVendorInformationTlv(EN,
-            Unpooled.wrappedBuffer(new byte[0]));
-        doNothing().when(tlvRegistry).serializeTlv(any(Tlv.class), any(ByteBuf.class));
-        doReturn(tlv).when(tlvRegistry).parseTlv(4, Unpooled.wrappedBuffer(new byte[] { 5, 6 }));
-    }
+    private static final EnterpriseNumber EN = new EnterpriseNumber(Uint32.ZERO);
+
+    @Mock
+    private TlvRegistry tlvRegistry;
+    @Mock
+    private VendorInformationTlvRegistry viTlvRegistry;
 
     @Test
-    public void testParseTlvs() throws PCEPDeserializerException {
+    void testParseTlvs() throws Exception {
+        final var tlv = new OfListBuilder().setCodes(Set.of(new OfId(Uint16.TEN))).build();
+
+        doNothing().when(tlvRegistry).serializeTlv(any(), any());
+        doReturn(tlv).when(tlvRegistry).parseTlv(4, Unpooled.wrappedBuffer(new byte[] { 5, 6 }));
+
         Abs abs = new Abs(tlvRegistry, viTlvRegistry);
         ByteBuf buffer = Unpooled.buffer();
         abs.serializeTlv(tlv, buffer);
 
-        verify(tlvRegistry, only()).serializeTlv(any(Tlv.class), any(ByteBuf.class));
+        verify(tlvRegistry, only()).serializeTlv(any(), any());
 
         TlvsBuilder builder = new TlvsBuilder();
         abs.parseTlvs(builder, Unpooled.wrappedBuffer(new byte[] { 0, 4, 0, 2, 5, 6, 0, 0 }));
@@ -107,13 +91,17 @@ public class AbstractObjectWithTlvsTest {
     }
 
     @Test
-    public void testParseVendorInformationTlv() throws PCEPDeserializerException {
+    void testParseVendorInformationTlv() throws Exception {
+        final var viTlv = new VendorInformationTlvBuilder().setEnterpriseNumber(EN).build();
+
+        doNothing().when(viTlvRegistry).serializeVendorInformationTlv(any(), any());
+        doReturn(Optional.of(viTlv)).when(viTlvRegistry).parseVendorInformationTlv(EN, Unpooled.EMPTY_BUFFER);
+
         final Abs parser = new Abs(tlvRegistry, viTlvRegistry);
         final ByteBuf buffer = Unpooled.buffer();
 
-        parser.serializeVendorInformationTlvs(Lists.newArrayList(viTlv), buffer);
-        verify(viTlvRegistry, only()).serializeVendorInformationTlv(any(VendorInformationTlv.class),
-            any(ByteBuf.class));
+        parser.serializeVendorInformationTlvs(List.of(viTlv), buffer);
+        verify(viTlvRegistry, only()).serializeVendorInformationTlv(any(), any());
 
         final TlvsBuilder tlvsBuilder = new TlvsBuilder();
         parser.parseTlvs(tlvsBuilder, Unpooled.wrappedBuffer(
