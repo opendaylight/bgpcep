dc9ce50c0caa2e65fb1c3cb0ce8fb79b60a08d53
Robert Varga <robert.varga@pantheon.tech>
2025-12-05 22:35:03 +0100

    Clean up BgpPeerBean
    Use local variable type inference and initialize fields as soon as
    possible to make things a tad clearer.
    
    JIRA: BGPCEP-1071
    Change-Id: Ieff1d7171c8f7f3121b37658d5f4b866c71b2730
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/BgpPeerBean.java
+++ bgp/rib-impl/src/main/java/org/opendaylight/protocol/bgp/rib/impl/config/BgpPeerBean.java
@@ -16,7 +16,6 @@ import com.google.common.util.concurrent.ListenableFuture;
 import io.netty.util.concurrent.Future;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -41,29 +40,23 @@ import org.opendaylight.protocol.bgp.rib.spi.state.BGPPeerStateProvider;
 import org.opendaylight.protocol.bgp.rib.spi.state.BGPStateProviderRegistry;
 import org.opendaylight.protocol.concepts.KeyMapping;
 import org.opendaylight.protocol.util.Ipv4Util;
-import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.multiprotocol.rev151009.bgp.common.afi.safi.list.AfiSafi;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.AfiSafis;
-import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbor.group.Config;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.neighbors.Neighbor;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.peer.group.PeerGroup;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.rev151009.bgp.top.Bgp;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.AsNumber;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.IpAddressNoZone;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev200120.open.message.BgpParameters;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev200120.open.message.bgp.parameters.OptionalCapabilities;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev200120.open.message.bgp.parameters.OptionalCapabilitiesBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev200120.open.message.bgp.parameters.optional.capabilities.CParametersBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev200120.open.message.bgp.parameters.optional.capabilities.c.parameters.As4BytesCapabilityBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.BgpTableType;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.CParameters1Builder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.AddPathCapabilityBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.MultiprotocolCapabilityBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.multiprotocol.rev180329.mp.capabilities.add.path.capability.AddressFamilies;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.openconfig.extensions.rev180329.network.instance.protocol.NeighborPeerGroupConfig;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.PeerRole;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.TablesKey;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.types.rev200120.ClusterIdentifier;
 import org.opendaylight.yangtools.binding.DataObjectIdentifier;
 import org.opendaylight.yangtools.concepts.Registration;
 import org.slf4j.Logger;
@@ -89,18 +82,19 @@ public final class BgpPeerBean extends PeerBean {
 
     private static List<OptionalCapabilities> getBgpCapabilities(final AfiSafis afiSafis, final RIB rib,
                                                           final BGPTableTypeRegistryConsumer tableTypeRegistry) {
-        final List<OptionalCapabilities> caps = new ArrayList<>();
-        caps.add(new OptionalCapabilitiesBuilder().setCParameters(new CParametersBuilder().setAs4BytesCapability(
-                new As4BytesCapabilityBuilder().setAsNumber(rib.getLocalAs()).build()).build()).build());
-
+        final var caps = new ArrayList<OptionalCapabilities>();
         caps.add(new OptionalCapabilitiesBuilder()
-                .setCParameters(BgpExtendedMessageUtil.EXTENDED_MESSAGE_CAPABILITY).build());
+            .setCParameters(new CParametersBuilder()
+                .setAs4BytesCapability(new As4BytesCapabilityBuilder().setAsNumber(rib.getLocalAs()).build())
+                .build())
+            .build());
         caps.add(new OptionalCapabilitiesBuilder()
-                .setCParameters(MultiprotocolCapabilitiesUtil.RR_CAPABILITY).build());
+            .setCParameters(BgpExtendedMessageUtil.EXTENDED_MESSAGE_CAPABILITY)
+            .build());
+        caps.add(new OptionalCapabilitiesBuilder().setCParameters(MultiprotocolCapabilitiesUtil.RR_CAPABILITY).build());
 
-        final Collection<AfiSafi> afiSafi = OpenConfigMappingUtil.getAfiSafiWithDefault(afiSafis, false).values();
-        final List<AddressFamilies> addPathCapability = OpenConfigMappingUtil.toAddPathCapability(afiSafi,
-            tableTypeRegistry);
+        final var afiSafi = OpenConfigMappingUtil.getAfiSafiWithDefault(afiSafis, false).values();
+        final var addPathCapability = OpenConfigMappingUtil.toAddPathCapability(afiSafi, tableTypeRegistry);
         if (!addPathCapability.isEmpty()) {
             caps.add(new OptionalCapabilitiesBuilder()
                 .setCParameters(new CParametersBuilder()
@@ -113,8 +107,8 @@ public final class BgpPeerBean extends PeerBean {
                 .build());
         }
 
-        final List<BgpTableType> tableTypes = OpenConfigMappingUtil.toTableTypes(afiSafi, tableTypeRegistry);
-        for (final BgpTableType tableType : tableTypes) {
+        final var tableTypes = OpenConfigMappingUtil.toTableTypes(afiSafi, tableTypeRegistry);
+        for (var tableType : tableTypes) {
             if (!rib.getLocalTables().contains(tableType)) {
                 LOG.info("RIB instance does not list {} in its local tables. Incoming data will be dropped.",
                     tableType);
@@ -177,26 +171,24 @@ public final class BgpPeerBean extends PeerBean {
         if (currentConfiguration == null) {
             return false;
         }
-        final AfiSafis actAfiSafi = currentConfiguration.getAfiSafis();
-        final AfiSafis extAfiSafi = neighbor.getAfiSafis();
-        final Collection<AfiSafi> actualSafi = actAfiSafi != null ? actAfiSafi.nonnullAfiSafi().values()
-                : Collections.emptyList();
-        final Collection<AfiSafi> extSafi = extAfiSafi != null ? extAfiSafi.nonnullAfiSafi().values()
-                : Collections.emptyList();
+        final var actAfiSafi = currentConfiguration.getAfiSafis();
+        final var extAfiSafi = neighbor.getAfiSafis();
+        final var actualSafi = actAfiSafi != null ? actAfiSafi.nonnullAfiSafi().values() : List.of();
+        final var extSafi = extAfiSafi != null ? extAfiSafi.nonnullAfiSafi().values() : List.of();
         return actualSafi.containsAll(extSafi) && extSafi.containsAll(actualSafi)
-                && Objects.equals(currentConfiguration.getConfig(), neighbor.getConfig())
-                && Objects.equals(currentConfiguration.getNeighborAddress(), neighbor.getNeighborAddress())
-                && Objects.equals(currentConfiguration.getAddPaths(), neighbor.getAddPaths())
-                && Objects.equals(currentConfiguration.getApplyPolicy(), neighbor.getApplyPolicy())
-                && Objects.equals(currentConfiguration.getAsPathOptions(), neighbor.getAsPathOptions())
-                && Objects.equals(currentConfiguration.getEbgpMultihop(), neighbor.getEbgpMultihop())
-                && Objects.equals(currentConfiguration.getGracefulRestart(), neighbor.getGracefulRestart())
-                && Objects.equals(currentConfiguration.getErrorHandling(), neighbor.getErrorHandling())
-                && Objects.equals(currentConfiguration.getLoggingOptions(), neighbor.getLoggingOptions())
-                && Objects.equals(currentConfiguration.getRouteReflector(), neighbor.getRouteReflector())
-                && Objects.equals(currentConfiguration.getState(), neighbor.getState())
-                && Objects.equals(currentConfiguration.getTimers(), neighbor.getTimers())
-                && Objects.equals(currentConfiguration.getTransport(), neighbor.getTransport());
+            && Objects.equals(currentConfiguration.getConfig(), neighbor.getConfig())
+            && Objects.equals(currentConfiguration.getNeighborAddress(), neighbor.getNeighborAddress())
+            && Objects.equals(currentConfiguration.getAddPaths(), neighbor.getAddPaths())
+            && Objects.equals(currentConfiguration.getApplyPolicy(), neighbor.getApplyPolicy())
+            && Objects.equals(currentConfiguration.getAsPathOptions(), neighbor.getAsPathOptions())
+            && Objects.equals(currentConfiguration.getEbgpMultihop(), neighbor.getEbgpMultihop())
+            && Objects.equals(currentConfiguration.getGracefulRestart(), neighbor.getGracefulRestart())
+            && Objects.equals(currentConfiguration.getErrorHandling(), neighbor.getErrorHandling())
+            && Objects.equals(currentConfiguration.getLoggingOptions(), neighbor.getLoggingOptions())
+            && Objects.equals(currentConfiguration.getRouteReflector(), neighbor.getRouteReflector())
+            && Objects.equals(currentConfiguration.getState(), neighbor.getState())
+            && Objects.equals(currentConfiguration.getTimers(), neighbor.getTimers())
+            && Objects.equals(currentConfiguration.getTransport(), neighbor.getTransport());
     }
 
     @Override
@@ -206,10 +198,7 @@ public final class BgpPeerBean extends PeerBean {
 
     @Override
     public synchronized BGPPeerState getPeerState() {
-        if (bgpPeerSingletonService == null) {
-            return null;
-        }
-        return bgpPeerSingletonService.getPeerState();
+        return bgpPeerSingletonService == null ? null : bgpPeerSingletonService.getPeerState();
     }
 
     synchronized void removePeer(final BGPPeerRegistry bgpPeerRegistry) {
@@ -240,9 +229,9 @@ public final class BgpPeerBean extends PeerBean {
 
             PeerGroup peerGroup = null;
             String peerGroupName = null;
-            final Config neighborConfig = neighbor.getConfig();
+            final var neighborConfig = neighbor.getConfig();
             if (neighborConfig != null) {
-                final NeighborPeerGroupConfig pgConfig = neighborConfig.augmentation(NeighborPeerGroupConfig.class);
+                final var pgConfig = neighborConfig.augmentation(NeighborPeerGroupConfig.class);
                 if (pgConfig != null) {
                     peerGroupName = StringUtils.substringBetween(pgConfig.getPeerGroup(), "=\"", "\"");
                     peerGroup = peerGroupLoader.getPeerGroup(bgpIid, peerGroupName);
@@ -254,29 +243,26 @@ public final class BgpPeerBean extends PeerBean {
             } else {
                 afisSafis = requireNonNull(neighbor.getAfiSafis(), "Missing mandatory AFIs/SAFIs");
             }
+            finalCapabilities = getBgpCapabilities(afisSafis, rib, tableTypeRegistry);
 
-            final Set<TablesKey> afiSafisAdvertized = OpenConfigMappingUtil.toTableKey(afisSafis.getAfiSafi(),
-                tableTypeRegistry);
-            final PeerRole role = OpenConfigMappingUtil.toPeerRole(neighbor, peerGroup);
+            final var afiSafisAdvertized = OpenConfigMappingUtil.toTableKey(afisSafis.getAfiSafi(), tableTypeRegistry);
+            final var role = OpenConfigMappingUtil.toPeerRole(neighbor, peerGroup);
 
-            final ClusterIdentifier clusterId = OpenConfigMappingUtil
-                    .getNeighborClusterIdentifier(neighbor.getRouteReflector(), peerGroup);
+            final var clusterId = OpenConfigMappingUtil.getNeighborClusterIdentifier(neighbor.getRouteReflector(),
+                peerGroup);
             final int hold = OpenConfigMappingUtil.getHoldTimer(neighbor, peerGroup);
             gracefulRestartTimer = OpenConfigMappingUtil.getGracefulRestartTimer(neighbor, peerGroup, hold);
-            final Set<TablesKey> gracefulTables = GracefulRestartUtil.getGracefulTables(
-                afisSafis.nonnullAfiSafi().values(), tableTypeRegistry);
-            final Map<TablesKey, Integer> llGracefulTimers = GracefulRestartUtil.getLlGracefulTimers(
-                afisSafis.nonnullAfiSafi().values(), tableTypeRegistry);
-            finalCapabilities = getBgpCapabilities(afisSafis, rib, tableTypeRegistry);
-            final List<BgpParameters> bgpParameters = getInitialBgpParameters(gracefulTables, llGracefulTimers);
-            final KeyMapping keyMapping = OpenConfigMappingUtil.getNeighborKey(neighbor);
-            final IpAddressNoZone neighborLocalAddress = OpenConfigMappingUtil.getLocalAddress(neighbor.getTransport());
-            final AsNumber globalAs = rib.getLocalAs();
-            final AsNumber neighborRemoteAs = OpenConfigMappingUtil
-                    .getRemotePeerAs(neighbor.getConfig(), peerGroup, globalAs);
+            final var gracefulTables = GracefulRestartUtil.getGracefulTables(afisSafis.nonnullAfiSafi().values(),
+                tableTypeRegistry);
+            final var llGracefulTimers = GracefulRestartUtil.getLlGracefulTimers(afisSafis.nonnullAfiSafi().values(),
+                tableTypeRegistry);
+            final var keyMapping = OpenConfigMappingUtil.getNeighborKey(neighbor);
+            final var neighborLocalAddress = OpenConfigMappingUtil.getLocalAddress(neighbor.getTransport());
+            final var globalAs = rib.getLocalAs();
+            final var neighborRemoteAs = OpenConfigMappingUtil.getRemotePeerAs(neighborConfig, peerGroup, globalAs);
             final AsNumber neighborLocalAs;
             if (role == PeerRole.Ebgp) {
-                neighborLocalAs = OpenConfigMappingUtil.getLocalPeerAs(neighbor.getConfig(), globalAs);
+                neighborLocalAs = OpenConfigMappingUtil.getLocalPeerAs(neighborConfig, globalAs);
             } else {
                 neighborLocalAs = globalAs;
             }
@@ -286,14 +272,14 @@ public final class BgpPeerBean extends PeerBean {
                     OpenConfigMappingUtil.getTreatAsWithdraw(peerGroup, neighbor),
                     BgpPeerBean.this);
             prefs = new BGPSessionPreferences(neighborLocalAs, hold, rib.getBgpIdentifier(),
-                    neighborRemoteAs, bgpParameters,
+                    neighborRemoteAs, getInitialBgpParameters(gracefulTables, llGracefulTimers),
                     keyMapping == null ? Optional.empty()
                         : Optional.of(Iterables.getOnlyElement(keyMapping.asMap().values())));
             activeConnection = OpenConfigMappingUtil.isActive(neighbor, peerGroup);
             retryTimer = OpenConfigMappingUtil.getRetryTimer(neighbor, peerGroup);
             dispatcher = rib.getDispatcher();
 
-            final PortNumber port = OpenConfigMappingUtil.getPort(neighbor, peerGroup);
+            final var port = OpenConfigMappingUtil.getPort(neighbor, peerGroup);
             inetAddress = Ipv4Util.toInetSocketAddress(neighborAddress, port);
             if (neighborLocalAddress != null) {
                 localAddress = Ipv4Util.toInetSocketAddress(neighborLocalAddress, port);
@@ -307,12 +293,12 @@ public final class BgpPeerBean extends PeerBean {
 
         private List<BgpParameters> getInitialBgpParameters(final Set<TablesKey> gracefulTables,
                                                             final Map<TablesKey, Integer> llGracefulTimers) {
-            final Set<BgpPeerUtil.LlGracefulRestartDTO> llGracefulRestarts = llGracefulTimers.entrySet().stream()
+            final var llGracefulRestarts = llGracefulTimers.entrySet().stream()
                     .map(entry -> new BgpPeerUtil.LlGracefulRestartDTO(entry.getKey(), entry.getValue(), false))
                     .collect(Collectors.toSet());
             return Collections.singletonList(
-                    GracefulRestartUtil.getGracefulBgpParameters(finalCapabilities, gracefulTables,
-                            Collections.emptySet(), gracefulRestartTimer, false, llGracefulRestarts));
+                    GracefulRestartUtil.getGracefulBgpParameters(finalCapabilities, gracefulTables, Set.of(),
+                        gracefulRestartTimer, false, llGracefulRestarts));
         }
 
         synchronized void instantiateServiceInstance() {
