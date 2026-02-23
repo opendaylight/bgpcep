835923b4d374782a77a0b653b78ea4afd7302d1a
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2026-02-23 12:53:35 +0100

    Rework updatePccState method
    We are always calling updatePccState method with PcepTriggeredResync
    value for its only parameter - pccSyncState.
    
    Remove it together with unnecessary check for its value.
    
    JIRA: BGPCEP-1073
    Change-Id: Ie984508699654d562e3a49771094eb986e15fce2
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    (cherry picked from commit 3d97ca3b0bfe4ae5892cd0ed377fe17373ef31df)

--- pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPTopologySessionListener.java
+++ pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/PCEPTopologySessionListener.java
@@ -85,7 +85,6 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev250328.LspId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev250328.Node1;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev250328.OperationResult;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev250328.PccSyncState;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev250328.RemoveLspArgs;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev250328.TriggerSyncArgs;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.topology.pcep.rev250328.UpdateLspArgs;
@@ -180,7 +179,7 @@ class PCEPTopologySessionListener extends AbstractTopologySessionListener {
     private ListenableFuture<OperationResult> triggerResyncronization(final TriggerSyncArgs input) {
         LOG.trace("Trigger Resynchronization {}", input);
         markAllLspAsStale();
-        updatePccState(PccSyncState.PcepTriggeredResync);
+        updatePccState();
         final PcupdMessageBuilder pcupdMessageBuilder = new PcupdMessageBuilder(MESSAGE_HEADER);
         final SrpIdNumber srpIdNumber = createUpdateMessageSync(pcupdMessageBuilder);
         final Message msg = new PcupdBuilder().setPcupdMessage(pcupdMessageBuilder.build()).build();
@@ -659,7 +658,7 @@ class PCEPTopologySessionListener extends AbstractTopologySessionListener {
                     staleLsps.add(path1.getLsp().getPlspId());
                 }
             }
-            updatePccState(PccSyncState.PcepTriggeredResync);
+            updatePccState();
             // create PCUpd with mandatory objects and LSP object set to 1
             final SrpBuilder srpBuilder = new SrpBuilder();
             srpBuilder.setOperationId(nextRequest());
