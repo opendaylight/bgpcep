f870da486a225b4c2c53f1191600db25b2a51a70
Martin Balaz <martin.balaz@pantheon.tech>
2026-02-26 21:11:11 +0100

    Increase PCC and LSP count in PCEP stats pytest
    Enhance the PCEP statistics test suite to simulate a higher load of
    connected PCC devices and reported LSPs. This validates ODL's
    capability to successfully process increased volume of data within
    a one second interval.
    
    JIRA: BGPCEP-1077
    Change-Id: Iedcf778b0416e4afc4484c59f68a38341394a110
    Signed-off-by: Martin Balaz <martin.balaz@pantheon.tech>

--- tests/suites/pcep_statistic/test_tc8_large_scale_topology.py
+++ tests/suites/pcep_statistic/test_tc8_large_scale_topology.py
@@ -19,8 +19,8 @@ from libraries.variables import variables
 
 
 DEFAULT_PCEP_STATS_UPDATE_INTERVAL = variables.DEFAULT_PCEP_STATS_UPDATE_INTERVAL
-PCC_COUNT = 70
-LSP_PER_PCC_COUNT = 145
+PCC_COUNT = 100
+LSP_PER_PCC_COUNT = 400
 ODL_IP = variables.ODL_IP
 TOOLS_IP = variables.TOOLS_IP
 
@@ -49,9 +49,11 @@ class TestPcepUser:
             """Update timer value to lowest possible value."""
             pcep.set_stat_timer_value(1)
 
-        with allure_step_with_separate_logging("step_start_pcc_mock_1_pcc_5_lsps"):
-            """Starts PCC mocks simulator with 1 simulated PCC device
-            and 5 reported LSPs."""
+        with allure_step_with_separate_logging(
+            f"step_start_pcc_mock_{PCC_COUNT}_pcc_{LSP_PER_PCC_COUNT}_lsps"
+        ):
+            """Starts PCC mocks simulator with {PCC_COUNT} simulated PCC device
+            and {LSP_PER_PCC_COUNT} reported LSPs."""
             self.pcc_mock_process = pcep.start_pcc_mock(
                 pcc=PCC_COUNT,
                 lsp=LSP_PER_PCC_COUNT,
