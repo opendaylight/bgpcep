8d821e3b8718e204cb48898c5642dcc95b364857
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2026-03-27 14:53:30 +0100

    Improve TopologySessionStats javadocs
    Document remaining method and improve existing javadocs.
    
    JIRA: BGPCEP-1083
    Change-Id: Idf8aef6e9ac43ea769b9ff4174048177fe9d625c
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    (cherry picked from commit 15a0a4c0d3ea04672dad8bb0cca71c2b88e33462)

--- pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/TopologySessionStats.java
+++ pcep/topology/topology-provider/src/main/java/org/opendaylight/bgpcep/pcep/topology/provider/TopologySessionStats.java
@@ -13,39 +13,32 @@ package org.opendaylight.bgpcep.pcep.topology.provider;
  */
 interface TopologySessionStats {
     /**
-     * Returns true if session is synchronized.
-     *
-     * @return status
+     * {@return true if session is synchronized}
      */
     boolean isSessionSynchronized();
 
     /**
-     * return true if Initiation Capability is advertized.
-     *
-     * @return status
+     * {@return true if Initiation Capability is advertised}
      */
     boolean isInitiationCapability();
 
     /**
-     * return true if Stateful Capability is advertized.
-     *
-     * @return status
+     * {@return true if Stateful Capability is advertised}
      */
     boolean isStatefulCapability();
 
     /**
-     * return true if Lsp Update Capability is advertized.
-     *
-     * @return status
+     * {@return true if Lsp Update Capability is advertised}
      */
     boolean isLspUpdateCapability();
 
     /**
-     * return the number of delegated LSPs (tunnels) from PCC.
-     *
-     * @return status
+     * {@return the number of delegated LSPs (tunnels) from PCC}
      */
     int getDelegatedLspsCount();
 
+    /**
+     * {@return the update interval (timer in nanos) to run updates}
+     */
     long updateInterval();
 }
