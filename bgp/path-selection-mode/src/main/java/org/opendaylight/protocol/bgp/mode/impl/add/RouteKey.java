3c64a00625cc7e09f8739636d855f131ecf3e535
Robert Varga <robert.varga@pantheon.tech>
2026-06-25 16:52:44 +0200

    Improve RouteKey.hashCode()
    We have the usual hashCode/multiply cascade generated by Eclipse long
    time ago. Improve it by inlining multiplications, eliminating data
    dependencies between the two constituent hashCode() invocations.
    
    Change-Id: I071ab39a7b6c2e83bfe8483b78bf36a550c990f9
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 5d10647fa5e2a178c9e037d3720423f8c2a6ccec)

--- bgp/path-selection-mode/src/main/java/org/opendaylight/protocol/bgp/mode/impl/add/RouteKey.java
+++ bgp/path-selection-mode/src/main/java/org/opendaylight/protocol/bgp/mode/impl/add/RouteKey.java
@@ -33,11 +33,7 @@ final class RouteKey implements Comparable<RouteKey>, Immutable {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + Long.hashCode(remotePathId);
-        result = prime * result + routerId.hashCode();
-        return result;
+        return 31 * 31 + 31 * Long.hashCode(remotePathId) + routerId.hashCode();
     }
 
     @Override
