5dbe99661feabe7b816b1c6d6ffa67e784f06afb
Matej Sramcik <matej.sramcik@pantheon.tech>
2026-08-07 08:19:10 +0200

    Migrate testtool-util tests to Junit5
    Migrate JUnit4 tests in testtool-util to JUnit5.
    
    JIRA: BGPCEP-1096
    Change-Id: I3790163dbfb61cbfc8b60b991152e542f37b53a4
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- testtool-util/src/test/java/org/opendaylight/protocol/util/InetSocketAddressUtilTest.java
+++ testtool-util/src/test/java/org/opendaylight/protocol/util/InetSocketAddressUtilTest.java
@@ -8,18 +8,17 @@
 
 package org.opendaylight.protocol.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.google.common.net.HostAndPort;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class InetSocketAddressUtilTest {
+class InetSocketAddressUtilTest {
     private static final String ADDRESS1 = "1.1.1.1";
     private static final String ADDRESS2 = "2.2.2.2";
     private static final int PORT1 = 123;
@@ -29,37 +28,37 @@ public class InetSocketAddressUtilTest {
     private static final int DEFAULT_PORT = 179;
 
     @Test
-    public void parseAddresses() throws Exception {
+    void parseAddresses() throws Exception {
         final List<InetSocketAddress> actualResult = InetSocketAddressUtil.parseAddresses(ADDRESSES, DEFAULT_PORT);
-        Assert.assertEquals(Arrays.asList(new InetSocketAddress(ADDRESS1, PORT1),
+        assertEquals(Arrays.asList(new InetSocketAddress(ADDRESS1, PORT1),
                 new InetSocketAddress(ADDRESS2, PORT2)), actualResult);
     }
 
     @Test
-    public void toHostAndPort() throws Exception {
+    void toHostAndPort() throws Exception {
         final HostAndPort actualResult = InetSocketAddressUtil.toHostAndPort(new InetSocketAddress(ADDRESS2, PORT2));
         final HostAndPort expected = HostAndPort.fromString("2.2.2.2:321");
-        Assert.assertEquals(expected, actualResult);
+        assertEquals(expected, actualResult);
     }
 
     @Test
-    public void parseAddressesDefaultPort() throws Exception {
+    void parseAddressesDefaultPort() throws Exception {
         final List<InetSocketAddress> actualResult
                 = InetSocketAddressUtil.parseAddresses(ADDRESSES_WO_PORT, DEFAULT_PORT);
         final List<InetSocketAddress> expected = Arrays.asList(new InetSocketAddress(ADDRESS1, DEFAULT_PORT),
                 new InetSocketAddress(ADDRESS2, DEFAULT_PORT));
-        Assert.assertEquals(expected, actualResult);
+        assertEquals(expected, actualResult);
     }
 
     @Test
-    public void parseAddressesWithoutPort() throws Exception {
+    void parseAddressesWithoutPort() throws Exception {
         final List<InetSocketAddress> actualResult = InetSocketAddressUtil.parseAddresses(ADDRESSES);
-        Assert.assertEquals(Arrays.asList(new InetSocketAddress(ADDRESS1, PORT1),
+        assertEquals(Arrays.asList(new InetSocketAddress(ADDRESS1, PORT1),
                 new InetSocketAddress(ADDRESS2, PORT2)), actualResult);
     }
 
     @Test
-    public void getInetSocketAddress() throws Exception {
+    void getInetSocketAddress() throws Exception {
         assertEquals(new InetSocketAddress(ADDRESS1, PORT1),
                 InetSocketAddressUtil.getInetSocketAddress(ADDRESS1 + ":" + PORT1, DEFAULT_PORT));
         assertEquals(new InetSocketAddress(ADDRESS1, DEFAULT_PORT),
@@ -67,7 +66,7 @@ public class InetSocketAddressUtilTest {
     }
 
     @Test
-    public void getRandomLoopbackInetSocketAddressTest() throws Exception {
+    void getRandomLoopbackInetSocketAddressTest() throws Exception {
         final InetSocketAddress addr1 = InetSocketAddressUtil.getRandomLoopbackInetSocketAddress();
         final InetSocketAddress addr2 = InetSocketAddressUtil.getRandomLoopbackInetSocketAddress();
         assertNotNull(addr1);
