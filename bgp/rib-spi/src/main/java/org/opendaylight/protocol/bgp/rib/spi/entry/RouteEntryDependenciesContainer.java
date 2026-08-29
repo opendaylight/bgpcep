2ca4ffe1448bd5a16cfd2160324b275a96615a6c
Robert Varga <robert.varga@pantheon.tech>
2026-08-29 16:13:07 +0200

    Clean up RouteEntryDependenciesContainer
    The callers do not care about the generic types of getRIBSupport()
    and the way this was retrofitted does not protect against run-time
    failures.
    
    Remove the generics, simplifying things significantly. While we are at
    it, modernize javadocs and use @NonNullByDefault.
    
    JIRA: BGPCEP-1109
    Change-Id: Ie28ec5548f4c768e3ba054dbf0f24a81482c4b98
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/entry/RouteEntryDependenciesContainer.java
+++ bgp/rib-spi/src/main/java/org/opendaylight/protocol/bgp/rib/spi/entry/RouteEntryDependenciesContainer.java
@@ -7,58 +7,43 @@
  */
 package org.opendaylight.protocol.bgp.rib.spi.entry;
 
-import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.opendaylight.protocol.bgp.rib.spi.BGPPeerTracker;
 import org.opendaylight.protocol.bgp.rib.spi.RIBSupport;
 import org.opendaylight.protocol.bgp.rib.spi.policy.BGPRibRoutingPolicy;
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.bgp.types.rev151009.AfiSafiType;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.Tables;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.TablesKey;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.rib.rev180329.rib.tables.Routes;
-import org.opendaylight.yangtools.binding.ChildOf;
-import org.opendaylight.yangtools.binding.ChoiceIn;
-import org.opendaylight.yangtools.binding.DataObject;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
 
 /**
  * Container wrapper for all dependencies related to Route Entry, required for process and storage.
  */
+@NonNullByDefault
 public interface RouteEntryDependenciesContainer {
     /**
-     * Returns rib support.
-     *
-     * @return RIBSupport
+     * {@return RIBSupport}
      */
-    <C extends Routes & DataObject & ChoiceIn<Tables>, S extends ChildOf<? super C>>
-        @NonNull RIBSupport<C, S> getRIBSupport();
+    RIBSupport<?, ?> getRIBSupport();
 
     /**
-     * Returns the table key(AFI/SAFI) corresponding to the Route Entry.
-     *
-     * @return TablesKey
+     * {@return the table key(AFI/SAFI) corresponding to the Route Entry}
      */
-    @NonNull TablesKey getLocalTablesKey();
+    TablesKey getLocalTablesKey();
 
     /**
-     * Returns the AfiSafiType(AFI/SAFI) corresponding to the Route Entry.
-     *
-     * @return TablesKey
+     * {@return the AfiSafiType(AFI/SAFI) corresponding to the Route Entry}
      */
-    @NonNull AfiSafiType getAfiSafType();
+    AfiSafiType getAfiSafType();
 
     /**
-     * Returns the loc-rib table to be updated and to which corresponds this Route Entry.
-     *
-     * @return InstanceIdentifier containing the path to loc-rib table.
+     * {@return the loc-rib table to be updated and to which corresponds this Route Entry}
      */
-    @NonNull YangInstanceIdentifier getLocRibTableTarget();
+    YangInstanceIdentifier getLocRibTableTarget();
 
     /**
-     * Return routing policies defined per RIB.
-     *
-     * @return BGPRibRoutingPolicy
+     * {@return routing policies defined per RIB}
      */
-    @NonNull BGPRibRoutingPolicy getRoutingPolicies();
+    BGPRibRoutingPolicy getRoutingPolicies();
 
-    @NonNull BGPPeerTracker getPeerTracker();
+    BGPPeerTracker getPeerTracker();
 }
