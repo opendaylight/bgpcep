0cc0af2e128676bf2fdaf0ea78aa3b3c99099de0
Robert Varga <robert.varga@pantheon.tech>
2026-09-11 20:26:57 +0200

    Simplify LocalAsPathPrependHandler
    We have two symmetric methods doing the same based on inputs that boil
    down to an optional AsNumber.
    
    Split out a common method and use it at the two call sites.
    
    Change-Id: I71751d336b4695346f966bf41f1bb706147790b0
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/openconfig-rp-statement/src/main/java/org/opendaylight/protocol/bgp/openconfig/routing/policy/statement/actions/LocalAsPathPrependHandler.java
+++ bgp/openconfig-rp-statement/src/main/java/org/opendaylight/protocol/bgp/openconfig/routing/policy/statement/actions/LocalAsPathPrependHandler.java
@@ -5,13 +5,14 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.protocol.bgp.openconfig.routing.policy.statement.actions;
 
+import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.RouteEntryBaseAttributes;
 import org.opendaylight.protocol.bgp.openconfig.routing.policy.spi.policy.action.BgpActionAugPolicy;
 import org.opendaylight.protocol.bgp.rib.spi.policy.BGPRouteEntryExportParameters;
 import org.opendaylight.protocol.bgp.rib.spi.policy.BGPRouteEntryImportParameters;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.AsNumber;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.bgp.message.rev200120.path.attributes.Attributes;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.odl.bgp._default.policy.rev200120.LocalAsPathPrepend;
 
@@ -20,7 +21,7 @@ public final class LocalAsPathPrependHandler extends AbstractPrependAsPath
     private static final LocalAsPathPrependHandler INSTANCE = new LocalAsPathPrependHandler();
 
     private LocalAsPathPrependHandler() {
-
+        // hidden on purpose
     }
 
     public static LocalAsPathPrependHandler getInstance() {
@@ -28,26 +29,20 @@ public final class LocalAsPathPrependHandler extends AbstractPrependAsPath
     }
 
     @Override
-    public Attributes applyImportAction(
-            final RouteEntryBaseAttributes routeEntryInfo,
-            final BGPRouteEntryImportParameters parameters,
-            final Attributes attributes,
+    public Attributes applyImportAction(final RouteEntryBaseAttributes routeEntryInfo,
+            final BGPRouteEntryImportParameters parameters, final Attributes attributes,
             final LocalAsPathPrepend actions) {
-        if (parameters.getFromPeerLocalAs() != null) {
-            return prependAS(attributes, parameters.getFromPeerLocalAs());
-        }
-        return attributes;
+        return prependIfPeerLocalAs(attributes, parameters.getFromPeerLocalAs());
     }
 
     @Override
-    public Attributes applyExportAction(
-            final RouteEntryBaseAttributes routeEntryInfo,
-            final BGPRouteEntryExportParameters parameters,
-            final Attributes attributes,
+    public Attributes applyExportAction(final RouteEntryBaseAttributes routeEntryInfo,
+            final BGPRouteEntryExportParameters parameters, final Attributes attributes,
             final LocalAsPathPrepend actions) {
-        if (parameters.getToPeerLocalAs() != null) {
-            return prependAS(attributes, parameters.getToPeerLocalAs());
-        }
-        return attributes;
+        return prependIfPeerLocalAs(attributes, parameters.getToPeerLocalAs());
+    }
+
+    private static Attributes prependIfPeerLocalAs( final Attributes attributes, final @Nullable AsNumber peerLocalAs) {
+        return peerLocalAs == null ? attributes : prependAS(attributes, peerLocalAs);
     }
 }
