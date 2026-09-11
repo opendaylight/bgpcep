068d8920f2d0d92aee4dd06e5befa4156055607c
Robert Varga <robert.varga@pantheon.tech>
2026-09-11 08:49:49 +0200

    Modernize SimpleNlriRegistry
    Deliver some TLC, namely:
    - use putIfAbsent() with explicit throws to improve population
    - use Map.entry() instead of SimpleEntry
    - use local variable type inference
    - use SimpleNlriRegistry.this instead of explicit lock object
    - use Collections.unmodifiableCollection instead of Iterables
    - use local afi/safi instead of extracting them from builder
    - use builders fluently
    
    Change-Id: I79bd3165c75dc105253c3490797c7c0e6c1fc2c3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/pojo/SimpleNlriRegistry.java
+++ bgp/parser-spi/src/main/java/org/opendaylight/protocol/bgp/parser/spi/pojo/SimpleNlriRegistry.java
@@ -7,17 +7,14 @@
  */
 package org.opendaylight.protocol.bgp.parser.spi.pojo;
 
-import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.collect.Iterables;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import java.util.AbstractMap.SimpleEntry;
-import java.util.Map.Entry;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import org.opendaylight.bgp.concepts.NextHopUtil;
 import org.opendaylight.protocol.bgp.parser.BGPParsingException;
 import org.opendaylight.protocol.bgp.parser.BgpTableTypeImpl;
@@ -44,16 +41,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 final class SimpleNlriRegistry implements NlriRegistry {
-
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleNlriRegistry.class);
     private static final int RESERVED = 1;
     private static final String PARSER_NOT_FOUND = "Nlri parser not found for table type {}";
-    private static final Logger LOG = LoggerFactory.getLogger(SimpleNlriRegistry.class);
 
-    private final ConcurrentMap<BgpTableType, NlriParser> handlers = new ConcurrentHashMap<>();
-    private final ConcurrentMap<Class<? extends DataObject>, NlriSerializer> serializers = new ConcurrentHashMap<>();
-    private final ConcurrentMap<BgpTableType, NextHopParserSerializer> nextHopParsers = new ConcurrentHashMap<>();
-    private final ConcurrentMap<Entry<Class<? extends CNextHop>, BgpTableType>,
-            NextHopParserSerializer> nextHopSerializers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<BgpTableType, NlriParser> handlers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Class<? extends DataObject>, NlriSerializer> serializers =
+        new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<BgpTableType, NextHopParserSerializer> nextHopParsers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Map.Entry<Class<? extends CNextHop>, BgpTableType>, NextHopParserSerializer>
+        nextHopSerializers = new ConcurrentHashMap<>();
     private final SubsequentAddressFamilyRegistry safiReg;
     private final AddressFamilyRegistry afiReg;
 
@@ -68,17 +65,16 @@ final class SimpleNlriRegistry implements NlriRegistry {
 
     synchronized Registration registerNlriSerializer(final Class<? extends DataObject> nlriClass,
             final NlriSerializer serializer) {
-        final NlriSerializer prev = this.serializers.get(nlriClass);
-        checkState(prev == null, "Serializer already bound to class " + prev);
+        final var prev = serializers.putIfAbsent(nlriClass, serializer);
+        if (prev != null) {
+            throw new IllegalStateException("Serializer already bound to class " + prev);
+        }
 
-        this.serializers.put(nlriClass, serializer);
-
-        final Object lock = this;
         return new AbstractRegistration() {
             @Override
             protected void removeRegistration() {
-                synchronized (lock) {
-                    SimpleNlriRegistry.this.serializers.remove(nlriClass);
+                synchronized (SimpleNlriRegistry.this) {
+                    serializers.remove(nlriClass);
                 }
             }
         };
@@ -87,37 +83,30 @@ final class SimpleNlriRegistry implements NlriRegistry {
     synchronized Registration registerNlriParser(final AddressFamily afi, final SubsequentAddressFamily safi,
             final NlriParser parser, final NextHopParserSerializer nextHopSerializer,
             final Class<? extends CNextHop> cnextHopClass, final Class<? extends CNextHop>... cnextHopClassList) {
-        final BgpTableType key = createKey(afi, safi);
-        final NlriParser prev = this.handlers.get(key);
-        checkState(prev == null, "AFI/SAFI is already bound to parser " + prev);
-
-        this.handlers.put(key, parser);
-        this.nextHopParsers.put(key,nextHopSerializer);
+        final var key = createKey(afi, safi);
+        final var prev = handlers.putIfAbsent(key, parser);
+        if (prev != null) {
+            throw new IllegalStateException("AFI/SAFI is already bound to parser " + prev);
+        }
+        nextHopParsers.put(key, nextHopSerializer);
 
         if (cnextHopClass != null) {
-            final Entry<Class<? extends CNextHop>, BgpTableType> nhKey = new SimpleEntry<>(cnextHopClass, key);
-            this.nextHopSerializers.put(nhKey, nextHopSerializer);
-            for (final Class<? extends CNextHop> cnextHop : cnextHopClassList) {
-                final Entry<Class<? extends CNextHop>, BgpTableType> nhKeys = new SimpleEntry<>(cnextHop, key);
-                this.nextHopSerializers.put(nhKeys, nextHopSerializer);
+            nextHopSerializers.put(Map.entry(cnextHopClass, key), nextHopSerializer);
+            for (var cnextHop : cnextHopClassList) {
+                nextHopSerializers.put(Map.entry(cnextHop, key), nextHopSerializer);
             }
         }
 
-        final Object lock = this;
         return new AbstractRegistration() {
             @Override
             protected void removeRegistration() {
-                synchronized (lock) {
-                    SimpleNlriRegistry.this.handlers.remove(key);
-                    SimpleNlriRegistry.this.nextHopParsers.remove(key);
+                synchronized (SimpleNlriRegistry.this) {
+                    handlers.remove(key);
+                    nextHopParsers.remove(key);
                     if (cnextHopClass != null) {
-                        final Entry<Class<? extends CNextHop>, BgpTableType> nhKey
-                                = new SimpleEntry<>(cnextHopClass, key);
-                        SimpleNlriRegistry.this.nextHopSerializers.remove(nhKey);
-                        for (final Class<? extends CNextHop> cnextHop : cnextHopClassList) {
-                            final Entry<Class<? extends CNextHop>, BgpTableType> nhKeys
-                                    = new SimpleEntry<>(cnextHop, key);
-                            SimpleNlriRegistry.this.nextHopSerializers.remove(nhKeys);
+                        nextHopSerializers.remove(Map.entry(cnextHopClass, key));
+                        for (var cnextHop : cnextHopClassList) {
+                            nextHopSerializers.remove(Map.entry(cnextHop, key));
                         }
                     }
                 }
@@ -127,7 +116,7 @@ final class SimpleNlriRegistry implements NlriRegistry {
 
     private AddressFamily getAfi(final ByteBuf buffer) throws BGPParsingException {
         final int afiVal = buffer.readUnsignedShort();
-        final AddressFamily afi = this.afiReg.classForFamily(afiVal);
+        final var afi = afiReg.classForFamily(afiVal);
         if (afi == null) {
             throw new BGPParsingException("Address Family Identifier: '" + afiVal + "' not supported.");
         }
@@ -136,7 +125,7 @@ final class SimpleNlriRegistry implements NlriRegistry {
 
     private SubsequentAddressFamily getSafi(final ByteBuf buffer) throws BGPParsingException {
         final int safiVal = buffer.readUnsignedByte();
-        final SubsequentAddressFamily safi = this.safiReg.classForFamily(safiVal);
+        final var safi = safiReg.classForFamily(safiVal);
         if (safi == null) {
             throw new BGPParsingException("Subsequent Address Family Identifier: '" + safiVal + "' not supported.");
         }
@@ -146,18 +135,20 @@ final class SimpleNlriRegistry implements NlriRegistry {
     @Override
     public MpUnreachNlri parseMpUnreach(final ByteBuf buffer, final PeerSpecificParserConstraint constraint)
             throws BGPParsingException {
-        final MpUnreachNlriBuilder builder = new MpUnreachNlriBuilder();
-        builder.setAfi(getAfi(buffer));
-        builder.setSafi(getSafi(buffer));
+        final var afi = getAfi(buffer);
+        final var safi = getSafi(buffer);
+        final var builder = new MpUnreachNlriBuilder()
+            .setAfi(afi)
+            .setSafi(safi);
 
         if (buffer.isReadable()) {
-            final ByteBuf nlri = buffer.slice();
-            final BgpTableType key = createKey(builder.getAfi(), builder.getSafi());
-            final NlriParser parser = this.handlers.get(key);
-            if (parser == null) {
-                LOG.warn(PARSER_NOT_FOUND, key);
-            } else {
+            final var nlri = buffer.slice();
+            final var key = createKey(afi, safi);
+            final var parser = handlers.get(key);
+            if (parser != null) {
                 parser.parseNlri(nlri, builder, constraint);
+            } else {
+                LOG.warn(PARSER_NOT_FOUND, key);
             }
         }
         return builder.build();
@@ -165,17 +156,16 @@ final class SimpleNlriRegistry implements NlriRegistry {
 
     @Override
     public void serializeMpReach(final MpReachNlri mpReachNlri, final ByteBuf byteAggregator) {
-        final AddressFamily afi = mpReachNlri.getAfi();
-        final SubsequentAddressFamily safi = mpReachNlri.getSafi();
-        byteAggregator.writeShort(this.afiReg.numberForClass(afi));
-        byteAggregator.writeByte(this.safiReg.numberForClass(safi));
+        final var afi = mpReachNlri.getAfi();
+        final var safi = mpReachNlri.getSafi();
+        byteAggregator.writeShort(afiReg.numberForClass(afi));
+        byteAggregator.writeByte(safiReg.numberForClass(safi));
 
-        final CNextHop cNextHop = mpReachNlri.getCNextHop();
+        final var cNextHop = mpReachNlri.getCNextHop();
         if (cNextHop != null) {
-            final Entry<Class<? extends CNextHop>, BgpTableType> key = new SimpleEntry(
-                    cNextHop.implementedInterface(), new BgpTableTypeImpl(afi, safi));
-            final NextHopParserSerializer nextHopSerializer = this.nextHopSerializers.get(key);
-            final ByteBuf nextHopBuffer = Unpooled.buffer();
+            final var nextHopSerializer = nextHopSerializers.get(
+                Map.entry(cNextHop.implementedInterface(), new BgpTableTypeImpl(afi, safi)));
+            final var nextHopBuffer = Unpooled.buffer();
             nextHopSerializer.serializeNextHop(cNextHop, nextHopBuffer);
             byteAggregator.writeByte(nextHopBuffer.writerIndex());
             byteAggregator.writeBytes(nextHopBuffer);
@@ -188,29 +178,28 @@ final class SimpleNlriRegistry implements NlriRegistry {
 
     @Override
     public void serializeMpUnReach(final MpUnreachNlri mpUnreachNlri, final ByteBuf byteAggregator) {
-        byteAggregator.writeShort(this.afiReg.numberForClass(mpUnreachNlri.getAfi()));
-        byteAggregator.writeByte(this.safiReg.numberForClass(mpUnreachNlri.getSafi()));
+        byteAggregator.writeShort(afiReg.numberForClass(mpUnreachNlri.getAfi()));
+        byteAggregator.writeByte(safiReg.numberForClass(mpUnreachNlri.getSafi()));
     }
 
     @Override
     public Iterable<NlriSerializer> getSerializers() {
-        return Iterables.unmodifiableIterable(this.serializers.values());
+        return Collections.unmodifiableCollection(serializers.values());
     }
 
     @Override
     public MpReachNlri parseMpReach(final ByteBuf buffer, final PeerSpecificParserConstraint constraint)
             throws BGPParsingException {
-        final MpReachNlriBuilder builder = new MpReachNlriBuilder();
-        final AddressFamily afi = getAfi(buffer);
-        final SubsequentAddressFamily safi = getSafi(buffer);
-        builder.setAfi(afi);
-        builder.setSafi(safi);
-
-        final BgpTableType key = createKey(builder.getAfi(), builder.getSafi());
+        final var afi = getAfi(buffer);
+        final var safi = getSafi(buffer);
+        final var key = createKey(afi, safi);
+        final var builder = new MpReachNlriBuilder()
+            .setAfi(afi)
+            .setSafi(safi);
 
         final int nextHopLength = buffer.readUnsignedByte();
         if (nextHopLength != 0) {
-            final NextHopParserSerializer nextHopParser = this.nextHopParsers.get(key);
+            final var nextHopParser = nextHopParsers.get(key);
             if (nextHopParser != null) {
                 builder.setCNextHop(nextHopParser.parseNextHop(buffer.readSlice(nextHopLength)));
             } else {
@@ -220,8 +209,8 @@ final class SimpleNlriRegistry implements NlriRegistry {
         }
         buffer.skipBytes(RESERVED);
 
-        final ByteBuf nlri = buffer.slice();
-        final NlriParser parser = this.handlers.get(key);
+        final var nlri = buffer.slice();
+        final var parser = handlers.get(key);
         if (parser == null) {
             LOG.warn(PARSER_NOT_FOUND, key);
         } else {
@@ -235,20 +224,20 @@ final class SimpleNlriRegistry implements NlriRegistry {
             final MpUnreachNlri mpUnreachNlri) {
         if (mpUnreachNlri == null) {
             return Optional.of(new MpUnreachNlriBuilder()
-                    .setWithdrawnRoutes(new WithdrawnRoutesBuilder()
-                            .setDestinationType(mpReachNlri.getAdvertizedRoutes().getDestinationType())
-                            .build())
-                    .build());
+                .setWithdrawnRoutes(new WithdrawnRoutesBuilder()
+                    .setDestinationType(mpReachNlri.getAdvertizedRoutes().getDestinationType())
+                    .build())
+                .build());
         }
 
-        final BgpTableType key = createKey(mpUnreachNlri.getAfi(), mpUnreachNlri.getSafi());
-        final NlriParser parser = this.handlers.get(key);
+        final var key = createKey(mpUnreachNlri.getAfi(), mpUnreachNlri.getSafi());
+        final var parser = handlers.get(key);
         if (parser == null) {
             LOG.debug("Parser for {} not found", key);
             return Optional.empty();
         }
 
-        final MpUnreachNlriBuilder builder = new MpUnreachNlriBuilder(mpUnreachNlri);
+        final var builder = new MpUnreachNlriBuilder(mpUnreachNlri);
         return parser.convertMpReachToMpUnReach(mpReachNlri, builder) ? Optional.of(builder.build()) : Optional.empty();
     }
 }
